2a96f56956d36ae02b2b6b326fe404499ef93b8b
Jean-Philippe Lang <jp_lang@yahoo.fr>
2006-07-30 09:51:14 +0000

    lang files updated
    git-svn-id: http://redmine.rubyforge.org/svn/trunk@16 e93f8b46-1217-0410-a6f0-8f06a7374b81

--- redmine/lang/en_US.rb
+++
@@ -1,6 +0,0 @@
-Localization.define('en', 'English') do |l| 
-  l.store '(date)', lambda { |t| t.strftime('%m/%d/%Y') }
-  l.store '(time)', lambda { |t| t.strftime('%m/%d/%Y %I:%M%p') }
-  
-  l.store '%d errors', ['1 error', '%d errors']
-end 
