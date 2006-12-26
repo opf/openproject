58f8a23d58c5419a158d84888025a7ae930833ca
Jean-Philippe Lang <jp_lang@yahoo.fr>
2006-12-26 16:19:12 +0000

    * news rss feed added * "header_tags" block added to allow inclusion of page specific tags in the html header (stylesheets, javascripts, feeds...)
    git-svn-id: http://redmine.rubyforge.org/svn/trunk@121 e93f8b46-1217-0410-a6f0-8f06a7374b81

---
+++ app/controllers/feeds_controller.rb
@@ -0,0 +1,25 @@
+# redMine - project management software
+# Copyright (C) 2006  Jean-Philippe Lang
+#
+# This program is free software; you can redistribute it and/or
+# modify it under the terms of the GNU General Public License
+# as published by the Free Software Foundation; either version 2
+# of the License, or (at your option) any later version.
+# 
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+# 
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+
+class FeedsController < ApplicationController
+  session :off
+  
+  def news
+    @news = News.find :all, :order => 'news.created_on DESC', :limit => 10, :include => [ :author, :project ]
+    @headers["Content-Type"] = "application/rss+xml"
+  end
+end
