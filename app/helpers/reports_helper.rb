515caa8f37325654ed424aff03b0654cab6ac487
Jean-Philippe Lang <jp_lang@yahoo.fr>
2007-05-28 09:09:37 +0000

    Fixed: open/closed issue counts are always 0 on reports view (postgresql)
    git-svn-id: http://redmine.rubyforge.org/svn/trunk@551 e93f8b46-1217-0410-a6f0-8f06a7374b81

--- app/helpers/reports_helper.rb
+++ app/helpers/reports_helper.rb
@@ -22,7 +22,7 @@ module ReportsHelper
     data.each { |row|
       match = 1
       criteria.each { |k, v|
-        match = 0 unless row[k].to_s == v.to_s
+        match = 0 unless (row[k].to_s == v.to_s) || (k == 'closed' && row[k] == (v == 0 ? "f" : "t"))
       } unless criteria.nil?
       a = a + row["total"].to_i if match == 1
     } unless data.nil?
