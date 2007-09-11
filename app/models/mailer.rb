74ed14f8a2fb93fc940b68ca76737686d1054d9b
Jean-Philippe Lang <jp_lang@yahoo.fr>
2007-09-11 17:12:34 +0000

    Notifications about issues (add/edit) are now sent in plain text and html. Removed lang specific strings in the corresponding mail templates, so that there is only one template for all languages.
    git-svn-id: http://redmine.rubyforge.org/svn/trunk@723 e93f8b46-1217-0410-a6f0-8f06a7374b81

--- app/models/mailer.rb
+++ app/models/mailer.rb
@@ -16,6 +16,7 @@
 # Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
 
 class Mailer < ActionMailer::Base
+  helper ApplicationHelper
   helper IssuesHelper
   
   def account_information(user, password)
