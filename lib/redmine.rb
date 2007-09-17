eb371db5b96ebd2471cef8156bf6e19e9d97bc06
Jean-Philippe Lang <jp_lang@yahoo.fr>
2007-09-17 16:56:11 +0000

    Fixed: RMagick not loaded on case sensitive filesystems.
    git-svn-id: http://redmine.rubyforge.org/svn/trunk@741 e93f8b46-1217-0410-a6f0-8f06a7374b81

--- lib/redmine.rb
+++ lib/redmine.rb
@@ -5,7 +5,7 @@ require 'redmine/acts_as_watchable/init'
 require 'redmine/acts_as_event/init'
 
 begin
-  require_library_or_gem 'rmagick' unless Object.const_defined?(:Magick)
+  require_library_or_gem 'RMagick' unless Object.const_defined?(:Magick)
 rescue LoadError
   # RMagick is not available
 end
