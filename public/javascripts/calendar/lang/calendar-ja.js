dc74a1780fa27a0256afcbdc9f1a4bea5fa84059
Jean-Philippe Lang <jp_lang@yahoo.fr>
2007-09-30 15:26:59 +0000

    Updated Japanese language file for calendar (Go Maeda).
    git-svn-id: http://redmine.rubyforge.org/svn/trunk@777 e93f8b46-1217-0410-a6f0-8f06a7374b81

--- public/javascripts/calendar/lang/calendar-ja.js
+++ public/javascripts/calendar/lang/calendar-ja.js
@@ -25,7 +25,7 @@ Calendar._DN = new Array ("日曜日", "月曜日", "火曜日", "水曜日", "�
 // this feature.
 
 // short day names
-Calendar._SDN = new Array ("日曜日", "月曜日", "火曜日", "水曜日", "木曜日", "金曜日", "土曜日");
+Calendar._SDN = new Array ("日", "月", "火", "水", "木", "金", "土");
 
 // First day of the week. "0" means display Sunday first, "1" means display
 // Monday first, etc.
@@ -39,7 +39,7 @@ Calendar._SMN = new Array ("1月", "2月", "3月", "4月", "5月", "6月", "7月
 
 // tooltips
 Calendar._TT = {};
-Calendar._TT["INFO"] = "About the calendar";
+Calendar._TT["INFO"] = "このカレンダーについて";
 
 Calendar._TT["ABOUT"] =
 "DHTML Date/Time Selector\n" +
@@ -47,41 +47,41 @@ Calendar._TT["ABOUT"] =
 "For latest version visit: http://www.dynarch.com/projects/calendar/\n" +
 "Distributed under GNU LGPL.  See http://gnu.org/licenses/lgpl.html for details." +
 "\n\n" +
-"Date selection:\n" +
-"- Use the \xab, \xbb buttons to select year\n" +
-"- Use the " + String.fromCharCode(0x2039) + ", " + String.fromCharCode(0x203a) + " buttons to select month\n" +
-"- Hold mouse button on any of the above buttons for faster selection.";
+"日付の選択方法:\n" +
+"- \xab, \xbb ボタンで年を選択。\n" +
+"- " + String.fromCharCode(0x2039) + ", " + String.fromCharCode(0x203a) + " ボタンで年を選択。\n" +
+"- 上記ボタンの長押しでメニューから選択。";
 Calendar._TT["ABOUT_TIME"] = "\n\n" +
 "Time selection:\n" +
 "- Click on any of the time parts to increase it\n" +
 "- or Shift-click to decrease it\n" +
 "- or click and drag for faster selection.";
 
-Calendar._TT["PREV_YEAR"] = "Prev. year (hold for menu)";
-Calendar._TT["PREV_MONTH"] = "Prev. month (hold for menu)";
-Calendar._TT["GO_TODAY"] = "Go Today";
-Calendar._TT["NEXT_MONTH"] = "Next month (hold for menu)";
-Calendar._TT["NEXT_YEAR"] = "Next year (hold for menu)";
-Calendar._TT["SEL_DATE"] = "Select date";
-Calendar._TT["DRAG_TO_MOVE"] = "Drag to move";
-Calendar._TT["PART_TODAY"] = " (today)";
+Calendar._TT["PREV_YEAR"] = "前年 (長押しでメニュー表示)";
+Calendar._TT["PREV_MONTH"] = "翌年 (長押しでメニュー表示)";
+Calendar._TT["GO_TODAY"] = "今日の日付を選択";
+Calendar._TT["NEXT_MONTH"] = "前月 (長押しでメニュー表示)";
+Calendar._TT["NEXT_YEAR"] = "翌月 (長押しでメニュー表示)";
+Calendar._TT["SEL_DATE"] = "日付を選択してください";
+Calendar._TT["DRAG_TO_MOVE"] = "ドラッグで移動";
+Calendar._TT["PART_TODAY"] = " (今日)";
 
 // the following is to inform that "%s" is to be the first day of week
 // %s will be replaced with the day name.
-Calendar._TT["DAY_FIRST"] = "Display %s first";
+Calendar._TT["DAY_FIRST"] = "%s始まりで表示";
 
 // This may be locale-dependent.  It specifies the week-end days, as an array
 // of comma-separated numbers.  The numbers are from 0 to 6: 0 means Sunday, 1
 // means Monday, etc.
 Calendar._TT["WEEKEND"] = "0,6";
 
-Calendar._TT["CLOSE"] = "Close";
-Calendar._TT["TODAY"] = "Today";
+Calendar._TT["CLOSE"] = "閉じる";
+Calendar._TT["TODAY"] = "今日";
 Calendar._TT["TIME_PART"] = "(Shift-)Click or drag to change value";
 
 // date formats
 Calendar._TT["DEF_DATE_FORMAT"] = "%Y-%m-%d";
-Calendar._TT["TT_DATE_FORMAT"] = "%a, %b %e";
+Calendar._TT["TT_DATE_FORMAT"] = "%b%e日(%a)";
 
-Calendar._TT["WK"] = "wk";
+Calendar._TT["WK"] = "週";
 Calendar._TT["TIME"] = "Time:";
