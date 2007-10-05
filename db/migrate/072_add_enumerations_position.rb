31467731fa623dbeee05a956469a7f4dbbaefddb
Jean-Philippe Lang <jp_lang@yahoo.fr>
2007-10-05 22:53:26 +0000

    Fixed: Migration 71 broken if run with code >= r803 (Enumeration#before_save relies on an attribute added in migration 72).
    git-svn-id: http://redmine.rubyforge.org/svn/trunk@809 e93f8b46-1217-0410-a6f0-8f06a7374b81

--- db/migrate/072_add_enumerations_position.rb
+++ db/migrate/072_add_enumerations_position.rb
@@ -1,8 +1,11 @@
 class AddEnumerationsPosition < ActiveRecord::Migration
   def self.up
-    add_column :enumerations, :position, :integer, :default => 1, :null => false
+    add_column(:enumerations, :position, :integer, :default => 1, :null => false) unless Enumeration.column_names.include?('position')
     Enumeration.find(:all).group_by(&:opt).each_value  do |enums|
-      enums.each_with_index {|enum, i| enum.update_attribute(:position, i+1)}
+      enums.each_with_index do |enum, i|
+        # do not call model callbacks
+        Enumeration.update_all "position = #{i+1}", {:id => enum.id}
+      end
     end
   end
 
