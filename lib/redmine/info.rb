a53c86f063ad39c2d345ca9a582601befa0d9c82
Jean-Philippe Lang <jp_lang@yahoo.fr>
2007-10-13 10:30:24 +0000

    Removed inconsistent user manual stuff (html/docbook content and controller). Help link now points to the Redmine guide which will be more easy to maintain. Also removed the useless components directory.
    git-svn-id: http://redmine.rubyforge.org/svn/trunk@835 e93f8b46-1217-0410-a6f0-8f06a7374b81

--- lib/redmine/info.rb
+++ lib/redmine/info.rb
@@ -3,6 +3,7 @@ module Redmine
     class << self
       def app_name; 'Redmine' end
       def url; 'http://www.redmine.org/' end
+      def help_url; 'http://www.redmine.org/guide' end
       def versioned_name; "#{app_name} #{Redmine::VERSION}" end
     end
   end
