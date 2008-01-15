ad25d15d6cee6ec7c4aff197473e6b6284f85754
Jean-Philippe Lang <jp_lang@yahoo.fr>
2008-01-15 20:40:59 +0000

    Fixed error on migration 85 with Oracle (index name can not be more than 30 characters long).
    git-svn-id: http://redmine.rubyforge.org/svn/trunk@1069 e93f8b46-1217-0410-a6f0-8f06a7374b81

--- db/migrate/085_add_role_tracker_old_status_index_to_workflows.rb
+++ db/migrate/085_add_role_tracker_old_status_index_to_workflows.rb
@@ -1,9 +1,9 @@
 class AddRoleTrackerOldStatusIndexToWorkflows < ActiveRecord::Migration
   def self.up
-    add_index :workflows, [:role_id, :tracker_id, :old_status_id], :name => :workflows_role_tracker_old_status
+    add_index :workflows, [:role_id, :tracker_id, :old_status_id], :name => :wkfs_role_tracker_old_status
   end
 
   def self.down
-    remove_index :workflows, :name => :workflows_role_tracker_old_status
+    remove_index(:workflows, :name => :wkfs_role_tracker_old_status); rescue
   end
 end
