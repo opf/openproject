97e7432ce6da80dea633cd99e934943925a774b5
Jean-Philippe Lang <jp_lang@yahoo.fr>
2008-01-22 17:17:52 +0000

    Fixed migration 87 (mysql: TEXT column can't have a default value).
    git-svn-id: http://redmine.rubyforge.org/svn/trunk@1093 e93f8b46-1217-0410-a6f0-8f06a7374b81

--- db/migrate/087_change_projects_description_to_text.rb
+++ db/migrate/087_change_projects_description_to_text.rb
@@ -1,6 +1,6 @@
 class ChangeProjectsDescriptionToText < ActiveRecord::Migration
   def self.up
-    change_column :projects, :description, :text, :default => ''
+    change_column :projects, :description, :text
   end
 
   def self.down
