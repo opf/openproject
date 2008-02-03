2920daf2c81a667ea6495cc5426de9a05e0e55e2
Jean-Philippe Lang <jp_lang@yahoo.fr>
2008-02-03 15:34:58 +0000

    Do not use RedCloth's glyphs (#210).
    git-svn-id: http://redmine.rubyforge.org/svn/trunk@1113 e93f8b46-1217-0410-a6f0-8f06a7374b81

--- lib/redcloth.rb
+++ lib/redcloth.rb
@@ -272,7 +272,7 @@ class RedCloth < String
         @shelf = []
         textile_rules = [:refs_textile, :block_textile_table, :block_textile_lists,
                          :block_textile_prefix, :inline_textile_image, :inline_textile_link,
-                         :inline_textile_code, :inline_textile_span, :glyphs_textile]
+                         :inline_textile_code, :inline_textile_span]
         markdown_rules = [:refs_markdown, :block_markdown_setext, :block_markdown_atx, :block_markdown_rule,
                           :block_markdown_bq, :block_markdown_lists, 
                           :inline_markdown_reflink, :inline_markdown_link]
