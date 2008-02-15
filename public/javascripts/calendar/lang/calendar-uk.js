38307c097907e5b4e04c6d4159dc28171251be26
Jean-Philippe Lang <jp_lang@yahoo.fr>
2008-02-15 17:47:19 +0000

    Added Ukrainan translation (Natalia Konovka & Mykhaylo Sorochan). Translation updates: * Lithuanian (Sergej Jegorov) * Russian (Michael Pirogov) * Traditional Chinese (Shortie Lo) * Finnish (Antti Perkiömäki)
    git-svn-id: http://redmine.rubyforge.org/svn/trunk@1149 e93f8b46-1217-0410-a6f0-8f06a7374b81

--- public/javascripts/calendar/lang/calendar-uk.js
+++ public/javascripts/calendar/lang/calendar-uk.js
@@ -1,10 +1,9 @@
 // ** I18N
 
-// Calendar LT language
-// Author: Gediminas Muižis, <gediminas.muizis@elgama.eu>
-// Encoding: UTF-8
+// Calendar EN language
+// Author: Mihai Bazon, <mihai_bazon@yahoo.com>
+// Encoding: any
 // Distributed under the same terms as the calendar itself.
-// Ver: 0.2
 
 // For translators: please use UTF-8 if possible.  We strongly believe that
 // Unicode is the answer to a real internationalized world.  Also please
@@ -12,14 +11,14 @@
 
 // full day names
 Calendar._DN = new Array
-("Sekmadienis",
- "Pirmadienis",
- "Antradienis",
- "Trečiadienis",
- "Ketvirtadienis",
- "Penktadienis",
- "Šeštadienis",
- "Sekmadienis");
+("Sunday",
+ "Monday",
+ "Tuesday",
+ "Wednesday",
+ "Thursday",
+ "Friday",
+ "Saturday",
+ "Sunday");
 
 // Please note that the following array of short day names (and the same goes
 // for short month names, _SMN) isn't absolutely necessary.  We give it here
@@ -35,52 +34,52 @@ Calendar._DN = new Array
 
 // short day names
 Calendar._SDN = new Array
-("Sek",
- "Pir",
- "Ant",
- "Tre",
- "Ket",
- "Pen",
- "Šeš",
- "Sek");
+("Sun",
+ "Mon",
+ "Tue",
+ "Wed",
+ "Thu",
+ "Fri",
+ "Sat",
+ "Sun");
 
 // First day of the week. "0" means display Sunday first, "1" means display
 // Monday first, etc.
-Calendar._FD = 1;
+Calendar._FD = 0;
 
 // full month names
 Calendar._MN = new Array
-("Sausis",
- "Vasaris",
- "Kovas",
- "Balandis",
- "Gegužė",
- "Birželis",
- "Liepa",
- "Rudpjūtis",
- "Rugsėjis",
- "Spalis",
- "Lapkritis",
- "Gruodis");
+("January",
+ "February",
+ "March",
+ "April",
+ "May",
+ "June",
+ "July",
+ "August",
+ "September",
+ "October",
+ "November",
+ "December");
 
 // short month names
 Calendar._SMN = new Array
-("Sau",
- "Vas",
- "Kov",
- "Bal",
- "Geg",
- "Brž",
- "Lie",
- "Rgp",
- "Rgs",
- "Spl",
- "Lap",
- "Grd");
+("Jan",
+ "Feb",
+ "Mar",
+ "Apr",
+ "May",
+ "Jun",
+ "Jul",
+ "Aug",
+ "Sep",
+ "Oct",
+ "Nov",
+ "Dec");
 
 // tooltips
 Calendar._TT = {};
-Calendar._TT["INFO"] = "Apie kalendorių";
+Calendar._TT["INFO"] = "About the calendar";
 
 Calendar._TT["ABOUT"] =
 "DHTML Date/Time Selector\n" +
@@ -88,41 +87,41 @@ Calendar._TT["ABOUT"] =
 "For latest version visit: http://www.dynarch.com/projects/calendar/\n" +
 "Distributed under GNU LGPL.  See http://gnu.org/licenses/lgpl.html for details." +
 "\n\n" +
-"Datos pasirinkimas:\n" +
-"- Naudoti \xab, \xbb mygtukus norint pasirinkti metus\n" +
-"- Naudoti " + String.fromCharCode(0x2039) + ", " + String.fromCharCode(0x203a) + " mygtukus norint pasirinkti mėnesį\n" +
-"- PAlaikykite nuspaudę bet kurį nygtuką norėdami iškviesti greitąjį meniu.";
+"Date selection:\n" +
+"- Use the \xab, \xbb buttons to select year\n" +
+"- Use the " + String.fromCharCode(0x2039) + ", " + String.fromCharCode(0x203a) + " buttons to select month\n" +
+"- Hold mouse button on any of the above buttons for faster selection.";
 Calendar._TT["ABOUT_TIME"] = "\n\n" +
-"Datos pasirinkimas:\n" +
-"- Paspaudus ant valandos ar minutės, jų reikšmės padidėja\n" +
-"- arba Shift-paspaudimas norint sumažinti reikšmę\n" +
-"- arba paspauskite ir tempkite norint greičiau keisti reikšmę.";
+"Time selection:\n" +
+"- Click on any of the time parts to increase it\n" +
+"- or Shift-click to decrease it\n" +
+"- or click and drag for faster selection.";
 
-Calendar._TT["PREV_YEAR"] = "Ankst. metai (laikyti, norint iškviesti meniu)";
-Calendar._TT["PREV_MONTH"] = "Ankst. mėnuo (laikyti, norint iškviesti meniu)";
-Calendar._TT["GO_TODAY"] = "Šiandien";
-Calendar._TT["NEXT_MONTH"] = "Kitas mėnuo (laikyti, norint iškviesti meniu)";
-Calendar._TT["NEXT_YEAR"] = "Kiti metai (laikyti, norint iškviesti meniu)";
-Calendar._TT["SEL_DATE"] = "Pasirinkti datą";
-Calendar._TT["DRAG_TO_MOVE"] = "Perkelkite pėlyte";
-Calendar._TT["PART_TODAY"] = " (šiandien)";
+Calendar._TT["PREV_YEAR"] = "Prev. year (hold for menu)";
+Calendar._TT["PREV_MONTH"] = "Prev. month (hold for menu)";
+Calendar._TT["GO_TODAY"] = "Go Today";
+Calendar._TT["NEXT_MONTH"] = "Next month (hold for menu)";
+Calendar._TT["NEXT_YEAR"] = "Next year (hold for menu)";
+Calendar._TT["SEL_DATE"] = "Select date";
+Calendar._TT["DRAG_TO_MOVE"] = "Drag to move";
+Calendar._TT["PART_TODAY"] = " (today)";
 
 // the following is to inform that "%s" is to be the first day of week
 // %s will be replaced with the day name.
-Calendar._TT["DAY_FIRST"] = "Rodyti %s pirmiau";
+Calendar._TT["DAY_FIRST"] = "Display %s first";
 
 // This may be locale-dependent.  It specifies the week-end days, as an array
 // of comma-separated numbers.  The numbers are from 0 to 6: 0 means Sunday, 1
 // means Monday, etc.
 Calendar._TT["WEEKEND"] = "0,6";
 
-Calendar._TT["CLOSE"] = "Uždaryti";
-Calendar._TT["TODAY"] = "Šiandien";
-Calendar._TT["TIME_PART"] = "(Shift-)Spausti ar tempti, norint pakeisti reikšmę";
+Calendar._TT["CLOSE"] = "Close";
+Calendar._TT["TODAY"] = "Today";
+Calendar._TT["TIME_PART"] = "(Shift-)Click or drag to change value";
 
 // date formats
 Calendar._TT["DEF_DATE_FORMAT"] = "%Y-%m-%d";
 Calendar._TT["TT_DATE_FORMAT"] = "%a, %b %e";
 
-Calendar._TT["WK"] = "sav";
-Calendar._TT["TIME"] = "Laikas:";
+Calendar._TT["WK"] = "wk";
+Calendar._TT["TIME"] = "Time:";
