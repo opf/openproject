4b15dc10c10d9cb54d156457cc7d6823dac55520
Jean-Philippe Lang <jp_lang@yahoo.fr>
2008-02-28 22:00:56 +0000

    Fixed: Multiple "new" statuses in filing new ticket (closes #751).
    git-svn-id: http://redmine.rubyforge.org/svn/trunk@1179 e93f8b46-1217-0410-a6f0-8f06a7374b81

--- app/controllers/issues_controller.rb
+++ app/controllers/issues_controller.rb
@@ -116,7 +116,7 @@ class IssuesController < ApplicationController
       return
     end    
     @issue.status = default_status
-    @allowed_statuses = ([default_status] + default_status.find_new_statuses_allowed_to(User.current.role_for_project(@project), @issue.tracker))
+    @allowed_statuses = ([default_status] + default_status.find_new_statuses_allowed_to(User.current.role_for_project(@project), @issue.tracker)).uniq
     
     if request.get? || request.xhr?
       @issue.start_date ||= Date.today
