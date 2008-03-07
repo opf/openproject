4458a7282b183939594a8a2d345235cfec459938
Jean-Philippe Lang <jp_lang@yahoo.fr>
2008-03-07 17:15:50 +0000

    Translations (closes #789, #790, #798, #803, #809): * Danish added (Mads Vestergaard) * Polish updated (Mariusz Olejnik) * Finnish updated (Antti Perkiömäki) * Simplified Chinese updated (chaoqun zou) * Traditional Chinese updated (shortie lo)
    git-svn-id: http://redmine.rubyforge.org/svn/trunk@1204 e93f8b46-1217-0410-a6f0-8f06a7374b81

---
+++ public/javascripts/jstoolbar/lang/jstoolbar-da.js
@@ -0,0 +1,14 @@
+jsToolBar.strings = {};
+jsToolBar.strings['Strong'] = 'Fed';
+jsToolBar.strings['Italic'] = 'Kursiv';
+jsToolBar.strings['Underline'] = 'Underskrevet';
+jsToolBar.strings['Deleted'] = 'Slettet';
+jsToolBar.strings['Code'] = 'Inline Kode';
+jsToolBar.strings['Heading 1'] = 'Overskrift 1';
+jsToolBar.strings['Heading 2'] = 'Overskrift 2';
+jsToolBar.strings['Heading 3'] = 'Overskrift 3';
+jsToolBar.strings['Unordered list'] = 'Unummereret list';
+jsToolBar.strings['Ordered list'] = 'Nummereret list';
+jsToolBar.strings['Preformatted text'] = 'Preformatteret tekst';
+jsToolBar.strings['Wiki link'] = 'Link til en Wiki side';
+jsToolBar.strings['Image'] = 'Billede';
