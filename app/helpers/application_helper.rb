68fe7856c9218d0bfa5c4cf468e7120cf2c964d4
Jean-Philippe Lang <jp_lang@yahoo.fr>
2008-03-31 21:48:01 +0000

    Move repetitive calendar include code from views into helper (patch #966 by Peter Suschlik).
    git-svn-id: http://redmine.rubyforge.org/svn/trunk@1315 e93f8b46-1217-0410-a6f0-8f06a7374b81

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -463,8 +463,21 @@ module ApplicationHelper
   end
   
   def calendar_for(field_id)
+    include_calendar_headers_tags
     image_tag("calendar.png", {:id => "#{field_id}_trigger",:class => "calendar-trigger"}) +
     javascript_tag("Calendar.setup({inputField : '#{field_id}', ifFormat : '%Y-%m-%d', button : '#{field_id}_trigger' });")
+  end
+
+  def include_calendar_headers_tags
+    unless @calendar_headers_tags_included
+      @calendar_headers_tags_included = true
+      content_for :header_tags do
+        javascript_include_tag('calendar/calendar') +
+        javascript_include_tag("calendar/lang/calendar-#{current_language}.js") +
+        javascript_include_tag('calendar/calendar-setup') +
+        stylesheet_link_tag('calendar')
+      end
+    end
   end
   
   def wikitoolbar_for(field_id)
