c72b53a8fa3c01ff0a80b34317fef80d97ee9c97
Jean-Philippe Lang <jp_lang@yahoo.fr>
2008-04-27 16:34:42 +0000

    Forces Redmine to use rails 2.0.2 gem when vendor/rails is not present. If you don't want to upgrade rails gem, freeze Redmine by running the following command in your Redmine directory:
      rake rails:freeze:edge TAG=rel_2-0-2
    
    git-svn-id: http://redmine.rubyforge.org/svn/trunk@1366 e93f8b46-1217-0410-a6f0-8f06a7374b81

--- config/environment.rb
+++ config/environment.rb
@@ -4,6 +4,9 @@
 # you don't control web/app server and can't set it the proper way
 # ENV['RAILS_ENV'] ||= 'production'
 
+# Specifies gem version of Rails to use when vendor/rails is not present
+RAILS_GEM_VERSION = '2.0.2' unless defined? RAILS_GEM_VERSION
+
 # Bootstrap the Rails environment, frameworks, and default configuration
 require File.join(File.dirname(__FILE__), 'boot')
 
