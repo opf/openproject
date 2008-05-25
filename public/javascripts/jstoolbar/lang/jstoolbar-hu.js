fbafff83630b767f7da735d64ed0feb3be001fe0
Jean-Philippe Lang <jp_lang@yahoo.fr>
2008-05-25 12:18:49 +0000

    Hungarian translation added (#1198 by Gábor Takács).
    git-svn-id: http://redmine.rubyforge.org/svn/trunk@1452 e93f8b46-1217-0410-a6f0-8f06a7374b81

---
+++ public/javascripts/jstoolbar/lang/jstoolbar-hu.js
@@ -0,0 +1,14 @@
+﻿jsToolBar.strings = {};
+jsToolBar.strings['Strong'] = 'Félkövér';
+jsToolBar.strings['Italic'] = 'Dőlt';
+jsToolBar.strings['Underline'] = 'Aláhúzott';
+jsToolBar.strings['Deleted'] = 'Törölt';
+jsToolBar.strings['Code'] = 'Kód sorok';
+jsToolBar.strings['Heading 1'] = 'Fejléc 1';
+jsToolBar.strings['Heading 2'] = 'Fejléc 2';
+jsToolBar.strings['Heading 3'] = 'Fejléc 3';
+jsToolBar.strings['Unordered list'] = 'Felsorolás';
+jsToolBar.strings['Ordered list'] = 'Számozott lista';
+jsToolBar.strings['Preformatted text'] = 'Előreformázott szöveg';
+jsToolBar.strings['Wiki link'] = 'Link egy Wiki oldalra';
+jsToolBar.strings['Image'] = 'Kép';
