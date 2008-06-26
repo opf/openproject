f79f19f84c3b30b47280592a4ab8eb06dafb168f
Jean-Philippe Lang <jp_lang@yahoo.fr>
2008-06-26 19:11:07 +0000

    Fixed: timelog redirects inappropriately when :back_url is blank (#1524).
    git-svn-id: http://redmine.rubyforge.org/svn/trunk@1589 e93f8b46-1217-0410-a6f0-8f06a7374b81

--- app/controllers/timelog_controller.rb
+++ app/controllers/timelog_controller.rb
@@ -180,7 +180,7 @@ class TimelogController < ApplicationController
     @time_entry.attributes = params[:time_entry]
     if request.post? and @time_entry.save
       flash[:notice] = l(:notice_successful_update)
-      redirect_to(params[:back_url] || {:action => 'details', :project_id => @time_entry.project})
+      redirect_to(params[:back_url].blank? ? {:action => 'details', :project_id => @time_entry.project} : params[:back_url])
       return
     end    
     @activities = Enumeration::get_values('ACTI')
