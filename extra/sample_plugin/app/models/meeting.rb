b20281f1515675813ade9e1284c39fd00d261365
Jean-Philippe Lang <jp_lang@yahoo.fr>
2008-07-27 19:18:35 +0000

    Follows r1703.
    git-svn-id: http://redmine.rubyforge.org/svn/trunk@1704 e93f8b46-1217-0410-a6f0-8f06a7374b81

--- extra/sample_plugin/app/models/meeting.rb
+++ extra/sample_plugin/app/models/meeting.rb
@@ -1,5 +1,10 @@
 class Meeting < ActiveRecord::Base
   belongs_to :project
+
+  acts_as_event :title => Proc.new {|o| "#{o.scheduled_on} Meeting"},
+                :datetime => :scheduled_on,
+                :author => nil,
+                :url => Proc.new {|o| {:controller => 'meetings', :action => 'show', :id => o.id}}                
   
   acts_as_activity_provider :timestamp => 'scheduled_on',
                             :find_options => { :include => :project }
