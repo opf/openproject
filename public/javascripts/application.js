6ad989f82887dcd88cf3cf70b658b4ccf82cf770
Jean-Philippe Lang <jp_lang@yahoo.fr>
2008-08-26 16:05:02 +0000

    Adds checkboxes toggle links on permissions report.
    git-svn-id: http://redmine.rubyforge.org/svn/trunk@1770 e93f8b46-1217-0410-a6f0-8f06a7374b81

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -10,6 +10,13 @@ function checkAll (id, checked) {
 	}
 }
 
+function toggleCheckboxesBySelector(selector) {
+	boxes = $$(selector);
+	var all_checked = true;
+	for (i = 0; i < boxes.length; i++) { if (boxes[i].checked == false) { all_checked = false; } }
+	for (i = 0; i < boxes.length; i++) { boxes[i].checked = !all_checked; }
+}
+
 function showAndScrollTo(id, focus) {
 	Element.show(id);
 	if (focus!=null) { Form.Element.focus(focus); }
