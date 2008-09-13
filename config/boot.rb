1f1fd9e034a83ac6ffdd3e8cbda86b92a9326d8b
Jean-Philippe Lang <jp_lang@yahoo.fr>
2008-09-13 18:32:37 +0000

    Expand RAILS_ROOT path on startup (#1892).
    git-svn-id: http://redmine.rubyforge.org/svn/trunk@1820 e93f8b46-1217-0410-a6f0-8f06a7374b81

--- config/boot.rb
+++ config/boot.rb
@@ -1,7 +1,7 @@
 # Don't change this file!
 # Configure your app in config/environment.rb and config/environments/*.rb
 
-RAILS_ROOT = "#{File.dirname(__FILE__)}/.." unless defined?(RAILS_ROOT)
+RAILS_ROOT = File.expand_path("#{File.dirname(__FILE__)}/..") unless defined?(RAILS_ROOT)
 
 module Rails
   class << self
