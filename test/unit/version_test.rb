199428804fd8129559b685a0d2ddc2f73f70deaa
Jean-Philippe Lang <jp_lang@yahoo.fr>
2008-09-21 08:54:02 +0000

    Fixed: invalid effective date (eg. 99999-01-01) causes an error on version edition screen.
    git-svn-id: http://redmine.rubyforge.org/svn/trunk@1888 e93f8b46-1217-0410-a6f0-8f06a7374b81

---
+++ test/unit/version_test.rb
@@ -0,0 +1,36 @@
+# redMine - project management software
+# Copyright (C) 2006-2007  Jean-Philippe Lang
+#
+# This program is free software; you can redistribute it and/or
+# modify it under the terms of the GNU General Public License
+# as published by the Free Software Foundation; either version 2
+# of the License, or (at your option) any later version.
+# 
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+# 
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+
+require File.dirname(__FILE__) + '/../test_helper'
+
+class ProjectTest < Test::Unit::TestCase
+  fixtures :projects, :issues, :issue_statuses, :versions
+
+  def setup
+  end
+  
+  def test_create
+    v = Version.new(:project => Project.find(1), :name => '1.1', :effective_date => '2011-03-25')
+    assert v.save
+  end
+  
+  def test_invalid_effective_date_validation
+    v = Version.new(:project => Project.find(1), :name => '1.1', :effective_date => '99999-01-01')
+    assert !v.save
+    assert_equal 'activerecord_error_not_a_date', v.errors.on(:effective_date)
+  end
+end
