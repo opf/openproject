1d542a5113a39aeecb4fb44f4cbc89d11b147d76
Jean-Philippe Lang <jp_lang@yahoo.fr>
2008-09-21 08:54:50 +0000

    Fixes VersionTest class.
    git-svn-id: http://redmine.rubyforge.org/svn/trunk@1889 e93f8b46-1217-0410-a6f0-8f06a7374b81

--- test/unit/version_test.rb
+++ test/unit/version_test.rb
@@ -1,5 +1,5 @@
-# redMine - project management software
-# Copyright (C) 2006-2007  Jean-Philippe Lang
+# Redmine - project management software
+# Copyright (C) 2006-2008  Jean-Philippe Lang
 #
 # This program is free software; you can redistribute it and/or
 # modify it under the terms of the GNU General Public License
@@ -17,7 +17,7 @@
 
 require File.dirname(__FILE__) + '/../test_helper'
 
-class ProjectTest < Test::Unit::TestCase
+class VersionTest < Test::Unit::TestCase
   fixtures :projects, :issues, :issue_statuses, :versions
 
   def setup
