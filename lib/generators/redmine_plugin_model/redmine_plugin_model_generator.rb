738fc579f0dc95aad9ab0dedc65fbce7af0bb980
Jean-Philippe Lang <jp_lang@yahoo.fr>
2008-10-25 09:35:51 +0000

    Renames template ruby files to erb.
    git-svn-id: svn+ssh://rubyforge.org/var/svn/redmine/trunk@1951 e93f8b46-1217-0410-a6f0-8f06a7374b81

--- lib/generators/redmine_plugin_model/redmine_plugin_model_generator.rb
+++ lib/generators/redmine_plugin_model/redmine_plugin_model_generator.rb
@@ -15,4 +15,30 @@ class RedminePluginModelGenerator < ModelGenerator
   def destination_root
     File.join(RAILS_ROOT, plugin_path)
   end
+  
+  def manifest
+    record do |m|
+      # Check for class naming collisions.
+      m.class_collisions class_path, class_name, "#{class_name}Test"
+
+      # Model, test, and fixture directories.
+      m.directory File.join('app/models', class_path)
+      m.directory File.join('test/unit', class_path)
+      m.directory File.join('test/fixtures', class_path)
+
+      # Model class, unit test, and fixtures.
+      m.template 'model.rb.erb',      File.join('app/models', class_path, "#{file_name}.rb")
+      m.template 'unit_test.rb.erb',  File.join('test/unit', class_path, "#{file_name}_test.rb")
+
+      unless options[:skip_fixture] 
+       	m.template 'fixtures.yml',  File.join('test/fixtures', "#{table_name}.yml")
+      end
+
+      unless options[:skip_migration]
+        m.migration_template 'migration.rb.erb', 'db/migrate', :assigns => {
+          :migration_name => "Create#{class_name.pluralize.gsub(/::/, '')}"
+        }, :migration_file_name => "create_#{file_path.gsub(/\//, '_').pluralize}"
+      end
+    end
+  end
 end
