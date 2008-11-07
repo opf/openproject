4581baa5c0b64eb7d638876fb2e9288d5f71107c
Jean-Philippe Lang <jp_lang@yahoo.fr>
2008-11-07 14:35:18 +0000

    Email address should be lowercased for gravatar (#2145).
    git-svn-id: svn+ssh://rubyforge.org/var/svn/redmine/trunk@1988 e93f8b46-1217-0410-a6f0-8f06a7374b81

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -5,12 +5,12 @@
 # modify it under the terms of the GNU General Public License
 # as published by the Free Software Foundation; either version 2
 # of the License, or (at your option) any later version.
-# 
+#
 # This program is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 # GNU General Public License for more details.
-# 
+#
 # You should have received a copy of the GNU General Public License
 # along with this program; if not, write to the Free Software
 # Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
@@ -28,7 +28,7 @@ module ApplicationHelper
   def current_role
     @current_role ||= User.current.role_for_project(@project)
   end
-  
+
   # Return true if user is authorized for controller/action, otherwise false
   def authorize_for(controller, action)
     User.current.allowed_to?({:controller => controller, :action => action}, @project)
@@ -38,7 +38,7 @@ module ApplicationHelper
   def link_to_if_authorized(name, options = {}, html_options = nil, *parameters_for_method_reference)
     link_to(name, options, html_options, *parameters_for_method_reference) if authorize_for(options[:controller] || params[:controller], options[:action])
   end
-  
+
   # Display a link to remote if user is authorized
   def link_to_remote_if_authorized(name, options = {}, html_options = nil)
     url = options[:url] || {}
@@ -49,14 +49,14 @@ module ApplicationHelper
   def link_to_user(user)
     user ? link_to(user, :controller => 'account', :action => 'show', :id => user) : 'Anonymous'
   end
-  
+
   def link_to_issue(issue, options={})
     options[:class] ||= ''
     options[:class] << ' issue'
     options[:class] << ' closed' if issue.closed?
     link_to "#{issue.tracker.name} ##{issue.id}", {:controller => "issues", :action => "show", :id => issue}, options
   end
-  
+
   # Generates a link to an attachment.
   # Options:
   # * :text - Link text (default to attachment filename)
@@ -64,37 +64,37 @@ module ApplicationHelper
   def link_to_attachment(attachment, options={})
     text = options.delete(:text) || attachment.filename
     action = options.delete(:download) ? 'download' : 'show'
-    
+
     link_to(h(text), {:controller => 'attachments', :action => action, :id => attachment, :filename => attachment.filename }, options)
   end
-  
+
   def toggle_link(name, id, options={})
     onclick = "Element.toggle('#{id}'); "
     onclick << (options[:focus] ? "Form.Element.focus('#{options[:focus]}'); " : "this.blur(); ")
     onclick << "return false;"
     link_to(name, "#", :onclick => onclick)
   end
-  
+
   def image_to_function(name, function, html_options = {})
     html_options.symbolize_keys!
-    tag(:input, html_options.merge({ 
-        :type => "image", :src => image_path(name), 
-        :onclick => (html_options[:onclick] ? "#{html_options[:onclick]}; " : "") + "#{function};" 
+    tag(:input, html_options.merge({
+        :type => "image", :src => image_path(name),
+        :onclick => (html_options[:onclick] ? "#{html_options[:onclick]}; " : "") + "#{function};"
         }))
   end
-  
+
   def prompt_to_remote(name, text, param, url, html_options = {})
     html_options[:onclick] = "promptToRemote('#{text}', '#{param}', '#{url_for(url)}'); return false;"
     link_to name, {}, html_options
   end
-  
+
   def format_date(date)
     return nil unless date
     # "Setting.date_format.size < 2" is a temporary fix (content of date_format setting changed)
     @date_format ||= (Setting.date_format.blank? || Setting.date_format.size < 2 ? l(:general_fmt_date) : Setting.date_format)
     date.strftime(@date_format)
   end
-  
+
   def format_time(time, include_date = true)
     return nil unless time
     time = time.to_time if time.is_a?(String)
@@ -104,44 +104,44 @@ module ApplicationHelper
     @time_format ||= (Setting.time_format.blank? ? l(:general_fmt_time) : Setting.time_format)
     include_date ? local.strftime("#{@date_format} #{@time_format}") : local.strftime(@time_format)
   end
-      
+
   def distance_of_date_in_words(from_date, to_date = 0)
     from_date = from_date.to_date if from_date.respond_to?(:to_date)
     to_date = to_date.to_date if to_date.respond_to?(:to_date)
     distance_in_days = (to_date - from_date).abs
     lwr(:actionview_datehelper_time_in_words_day, distance_in_days)
   end
-  
+
   def due_date_distance_in_words(date)
     if date
       l((date < Date.today ? :label_roadmap_overdue : :label_roadmap_due_in), distance_of_date_in_words(Date.today, date))
     end
   end
-  
+
   # Truncates and returns the string as a single line
   def truncate_single_line(string, *args)
     truncate(string, *args).gsub(%r{[\r\n]+}m, ' ')
   end
-  
+
   def html_hours(text)
     text.gsub(%r{(\d+)\.(\d+)}, '<span class="hours hours-int">\1</span><span class="hours hours-dec">.\2</span>')
   end
-  
+
   def authoring(created, author)
     time_tag = content_tag('acronym', distance_of_time_in_words(Time.now, created), :title => format_time(created))
     author_tag = (author.is_a?(User) && !author.anonymous?) ? link_to(h(author), :controller => 'account', :action => 'show', :id => author) : h(author || 'Anonymous')
     l(:label_added_time_by, author_tag, time_tag)
   end
-  
+
   def l_or_humanize(s, options={})
     k = "#{options[:prefix]}#{s}".to_sym
     l_has_string?(k) ? l(k) : s.to_s.humanize
   end
-  
+
   def day_name(day)
     l(:general_day_names).split(',')[day-1]
   end
-  
+
   def month_name(month)
     l(:actionview_datehelper_select_month_names).split(',')[month-1]
   end
@@ -150,7 +150,7 @@ module ApplicationHelper
     type = CodeRay::FileType[name]
     type ? CodeRay.scan(content, type).html : h(content)
   end
-  
+
   def to_path_param(path)
     path.to_s.split(%r{[/\\]}).select {|p| !p.blank?}
   end
@@ -160,51 +160,51 @@ module ApplicationHelper
     url_param = params.dup
     # don't reuse params if filters are present
     url_param.clear if url_param.has_key?(:set_filter)
-    
-    html = ''    
-    html << link_to_remote(('&#171; ' + l(:label_previous)), 
+
+    html = ''
+    html << link_to_remote(('&#171; ' + l(:label_previous)),
                             {:update => 'content',
                              :url => url_param.merge(page_param => paginator.current.previous),
                              :complete => 'window.scrollTo(0,0)'},
                             {:href => url_for(:params => url_param.merge(page_param => paginator.current.previous))}) + ' ' if paginator.current.previous
-                            
+
     html << (pagination_links_each(paginator, options) do |n|
-      link_to_remote(n.to_s, 
+      link_to_remote(n.to_s,
                       {:url => {:params => url_param.merge(page_param => n)},
                        :update => 'content',
                        :complete => 'window.scrollTo(0,0)'},
                       {:href => url_for(:params => url_param.merge(page_param => n))})
     end || '')
-    
-    html << ' ' + link_to_remote((l(:label_next) + ' &#187;'), 
+
+    html << ' ' + link_to_remote((l(:label_next) + ' &#187;'),
                                  {:update => 'content',
                                   :url => url_param.merge(page_param => paginator.current.next),
                                   :complete => 'window.scrollTo(0,0)'},
                                  {:href => url_for(:params => url_param.merge(page_param => paginator.current.next))}) if paginator.current.next
-    
+
     unless count.nil?
       html << [" (#{paginator.current.first_item}-#{paginator.current.last_item}/#{count})", per_page_links(paginator.items_per_page)].compact.join(' | ')
     end
-    
-    html  
+
+    html
   end
-  
+
   def per_page_links(selected=nil)
     url_param = params.dup
     url_param.clear if url_param.has_key?(:set_filter)
-    
+
     links = Setting.per_page_options_array.collect do |n|
-      n == selected ? n : link_to_remote(n, {:update => "content", :url => params.dup.merge(:per_page => n)}, 
+      n == selected ? n : link_to_remote(n, {:update => "content", :url => params.dup.merge(:per_page => n)},
                                             {:href => url_for(url_param.merge(:per_page => n))})
     end
     links.size > 1 ? l(:label_display_per_page, links.join(', ')) : nil
   end
-  
+
   def breadcrumb(*args)
     elements = args.flatten
     elements.any? ? content_tag('p', args.join(' &#187; ') + ' &#187; ', :class => 'breadcrumb') : nil
   end
-  
+
   def html_title(*args)
     if args.empty?
       title = []
@@ -239,12 +239,12 @@ module ApplicationHelper
       raise ArgumentError, 'invalid arguments to textilizable'
     end
     return '' if text.blank?
-    
+
     only_path = options.delete(:only_path) == false ? false : true
 
     # when using an image link, try to use an attachment, if possible
     attachments = options[:attachments] || (obj && obj.respond_to?(:attachments) ? obj.attachments : nil)
-    
+
     if attachments
       attachments = attachments.sort_by(&:created_on).reverse
       text = text.gsub(/!((\<|\=|\>)?(\([^\)]+\))?(\[[^\]]+\])?(\{[^\}]+\})?)(\S+\.(bmp|gif|jpg|jpeg|png))!/i) do |m|
@@ -262,7 +262,7 @@ module ApplicationHelper
         end
       end
     end
-    
+
     text = Redmine::WikiFormatting.to_html(Setting.text_formatting, text) { |macro, args| exec_macro(macro, obj, args) }
 
     # different methods for formatting wiki links
@@ -276,11 +276,11 @@ module ApplicationHelper
     else
       format_wiki_link = Proc.new {|project, title, anchor| url_for(:only_path => only_path, :controller => 'wiki', :action => 'index', :id => project, :page => title, :anchor => anchor) }
     end
-    
+
     project = options[:project] || @project || (obj && obj.respond_to?(:project) ? obj.project : nil)
-    
+
     # Wiki links
-    # 
+    #
     # Examples:
     #   [[mypage]]
     #   [[mypage|mytext]]
@@ -298,7 +298,7 @@ module ApplicationHelper
           page = $2
           title ||= $1 if page.blank?
         end
-        
+
         if link_project && link_project.wiki
           # extract anchor
           anchor = nil
@@ -319,7 +319,7 @@ module ApplicationHelper
     end
 
     # Redmine links
-    # 
+    #
     # Examples:
     #   Issues:
     #     #52 -> Link to issue #52
@@ -358,7 +358,7 @@ module ApplicationHelper
           oid = oid.to_i
           case prefix
           when nil
-            if issue = Issue.find_by_id(oid, :include => [:project, :status], :conditions => Project.visible_by(User.current))        
+            if issue = Issue.find_by_id(oid, :include => [:project, :status], :conditions => Project.visible_by(User.current))
               link = link_to("##{oid}", {:only_path => only_path, :controller => 'issues', :action => 'show', :id => oid},
                                         :class => (issue.closed? ? 'issue closed' : 'issue'),
                                         :title => "#{truncate(issue.subject, 100)} (#{issue.status.name})")
@@ -426,10 +426,10 @@ module ApplicationHelper
       end
       leading + (link || "#{prefix}#{sep}#{oid}")
     end
-    
+
     text
   end
-  
+
   # Same as Rails' simple_format helper without using paragraphs
   def simple_format_without_paragraph(text)
     text.to_s.
@@ -437,7 +437,7 @@ module ApplicationHelper
       gsub(/\n\n+/, "<br /><br />").          # 2+ newline  -> 2 br
       gsub(/([^\n]\n)(?=[^\n])/, '\1<br />')  # 1 newline   -> br
   end
-  
+
   def error_messages_for(object_name, options = {})
     options = options.symbolize_keys
     object = instance_variable_get("@#{object_name}")
@@ -455,14 +455,14 @@ module ApplicationHelper
       end
       # retrieve custom values error messages
       if object.errors[:custom_values]
-        object.custom_values.each do |v| 
+        object.custom_values.each do |v|
           v.errors.each do |attr, msg|
             next if msg.nil?
             msg = msg.first if msg.is_a? Array
             full_messages << "&#171; " + v.custom_field.name + " &#187; " + l(msg)
           end
         end
-      end      
+      end
       content_tag("div",
         content_tag(
           options[:header_tag] || "span", lwr(:gui_validation_error, full_messages.length) + ":"
@@ -474,34 +474,34 @@ module ApplicationHelper
       ""
     end
   end
-  
+
   def lang_options_for_select(blank=true)
-    (blank ? [["(auto)", ""]] : []) + 
+    (blank ? [["(auto)", ""]] : []) +
       GLoc.valid_languages.collect{|lang| [ ll(lang.to_s, :general_lang_name), lang.to_s]}.sort{|x,y| x.last <=> y.last }
   end
-  
+
   def label_tag_for(name, option_tags = nil, options = {})
     label_text = l(("field_"+field.to_s.gsub(/\_id$/, "")).to_sym) + (options.delete(:required) ? @template.content_tag("span", " *", :class => "required"): "")
     content_tag("label", label_text)
   end
-  
+
   def labelled_tabular_form_for(name, object, options, &proc)
     options[:html] ||= {}
     options[:html][:class] = 'tabular' unless options[:html].has_key?(:class)
     form_for(name, object, options.merge({ :builder => TabularFormBuilder, :lang => current_language}), &proc)
   end
-  
+
   def back_url_hidden_field_tag
     back_url = params[:back_url] || request.env['HTTP_REFERER']
     hidden_field_tag('back_url', back_url) unless back_url.blank?
   end
-  
+
   def check_all_links(form_name)
     link_to_function(l(:button_check_all), "checkAll('#{form_name}', true)") +
     " | " +
-    link_to_function(l(:button_uncheck_all), "checkAll('#{form_name}', false)")   
+    link_to_function(l(:button_uncheck_all), "checkAll('#{form_name}', false)")
   end
-  
+
   def progress_bar(pcts, options={})
     pcts = [pcts, pcts] unless pcts.is_a?(Array)
     pcts[1] = pcts[1] - pcts[0]
@@ -516,7 +516,7 @@ module ApplicationHelper
       ), :class => 'progress', :style => "width: #{width};") +
       content_tag('p', legend, :class => 'pourcent')
   end
-  
+
   def context_menu_link(name, url, options={})
     options[:class] ||= ''
     if options.delete(:selected)
@@ -532,7 +532,7 @@ module ApplicationHelper
     end
     link_to name, url, options
   end
-  
+
   def calendar_for(field_id)
     include_calendar_headers_tags
     image_tag("calendar.png", {:id => "#{field_id}_trigger",:class => "calendar-trigger"}) +
@@ -550,25 +550,25 @@ module ApplicationHelper
       end
     end
   end
-  
+
   def content_for(name, content = nil, &block)
     @has_content ||= {}
     @has_content[name] = true
     super(name, content, &block)
   end
-  
+
   def has_content?(name)
     (@has_content && @has_content[name]) || false
   end
 
   def gravatar_for_mail(mail, options = { })
     if Setting.gravatar_enabled?
-      return gravatar(mail, options) rescue nil
+      return gravatar(mail.to_s.downcase, options) rescue nil
     end
   end
 
   private
-  
+
   def wiki_helper
     helper = Redmine::WikiFormatting.helper_for(Setting.text_formatting)
     extend helper
