858eba2f1ca63f7ee643171e3edd63e60e69901a
winterheart <unknown@unknown>
2008-11-09 00:29:20 +0000

    populating new string, updates for ru.yml and sv.yml (#2126)
    git-svn-id: svn+ssh://rubyforge.org/var/svn/redmine/trunk@1998 e93f8b46-1217-0410-a6f0-8f06a7374b81

--- public/javascripts/calendar/lang/calendar-sk.js
+++ public/javascripts/calendar/lang/calendar-sk.js
@@ -1,87 +1,34 @@
+/* 
+	calendar-sk.js
+	language: Slovak
+	encoding: UTF-8
+	author: Stanislav Pach (stano.pach@seznam.cz)
+*/
+
 // ** I18N
-
-// Calendar Slovak (SK) language
-// Author: Stanislav Pach, <stano.pach@seznam.cz>
-// Encoding: UTF-8
-// Distributed under the same terms as the calendar itself.
-
-// For translators: please use UTF-8 if possible.  We strongly believe that
-// Unicode is the answer to a real internationalized world.  Also please
-// include your contact information in the header, as can be seen above.
-
-// 
-
-// full day names
-Calendar._DN = new Array
-("Nedeľa",
- "Pondelok",
- "Utorok",
- "Streda",
- "Štvrtok,
- "Piatok",
- "Sobota",
- "Nedeľa");
-
-// Please note that the following array of short day names (and the same goes
-// for short month names, _SMN) isn't absolutely necessary.  We give it here
-// for exemplification on how one can customize the short day names, but if
-// they are simply the first N letters of the full name you can simply say:
-//
-//   Calendar._SDN_len = N; // short day name length
-//   Calendar._SMN_len = N; // short month name length
-//
-// If N = 3 then this is not needed either since we assume a value of 3 if not
-// present, to be compatible with translation files that were written before
-// this feature.
-
-// short day names
-Calendar._SDN = new Array
-("Ne",
- "Po",
- "Ut",
- "St",
- "Št",
- "Pi",
- "So",
- "Ne");
+Calendar._DN  = new Array('Nedeľa','Pondelok','Utorok','Streda','Štvrtok','Piatok','Sobota','Nedeľa');
+Calendar._SDN = new Array('Ne','Po','Ut','St','Št','Pi','So','Ne');
+Calendar._MN  = new Array('Január','Február','Marec','Apríl','Máj','Jún','Júl','August','September','Október','November','December');
+Calendar._SMN = new Array('Jan','Feb','Mar','Apr','Máj','Jún','Júl','Aug','Sep','Okt','Nov','Dec');
 
 // First day of the week. "0" means display Sunday first, "1" means display
 // Monday first, etc.
 Calendar._FD = 1;
 
-// full month names
-Calendar._MN = new Array
-("Január",
- "Február",
- "Marec",
- "Apríl",
- "Máj",
- "Jún",
- "Júl",
- "August",
- "September",
- "Október",
- "November",
- "December");
-
-// short month names
-Calendar._SMN = new Array
-("Jan",
- "Feb",
- "Mar",
- "Apr",
- "Maj",
- "Jun",
- "Jul",
- "Aug",
- "Sep",
- "Okt",
- "Nov",
- "Dec");
-
 // tooltips
 Calendar._TT = {};
 Calendar._TT["INFO"] = "O komponente kalendár";
+Calendar._TT["TOGGLE"] = "Zmena prvého dňa v týždni";
+Calendar._TT["PREV_YEAR"] = "Predchádzajúci rok (pridrž pre menu)";
+Calendar._TT["PREV_MONTH"] = "Predchádzajúci mesiac (pridrž pre menu)";
+Calendar._TT["GO_TODAY"] = "Dnešný dátum";
+Calendar._TT["NEXT_MONTH"] = "Ďalší mesiac (pridrž pre menu)";
+Calendar._TT["NEXT_YEAR"] = "Ďalší rok (pridrž pre menu)";
+Calendar._TT["SEL_DATE"] = "Zvoľ dátum";
+Calendar._TT["DRAG_TO_MOVE"] = "Chyť a ťahaj pre presun";
+Calendar._TT["PART_TODAY"] = " (dnes)";
+Calendar._TT["MON_FIRST"] = "Ukáž ako prvný Pondelok";
+//Calendar._TT["SUN_FIRST"] = "Ukaž jako první Neděli";
 
 Calendar._TT["ABOUT"] =
 "DHTML Date/Time Selector\n" +
@@ -89,28 +36,20 @@ Calendar._TT["ABOUT"] =
 "For latest version visit: http://www.dynarch.com/projects/calendar/\n" +
 "Distributed under GNU LGPL.  See http://gnu.org/licenses/lgpl.html for details." +
 "\n\n" +
-"Nastavenie dátumu:\n" +
-"- Použijte klávesy \xab, \xbb pre voľbu roku\n" +
-"- Použijte tlačítka " + String.fromCharCode(0x2039) + ", " + String.fromCharCode(0x203a) + " pre voľbu mesiaca\n" +
-"- Podržte tlačitko myši na hociakej časti týchto tlačítiek pre rychlú voľbu.";
-Calendar._TT["ABOUT_TIME"] = "\n\n" +
-"Nastavenie času:\n" +
-"- Kliknite na hociakú časť času pre jeho zvýšenie\n" +
-"- alebo kliknite spolu so Shiftom, aby ste ho znížil\n" +
-"- alebo kliknite a ťahajte pre rýchlejší výber.";
+"Výber dátumu:\n" +
+"- Použijte tlačítka \xab, \xbb pre voľbu roku\n" +
+"- Použijte tlačítka " + String.fromCharCode(0x2039) + ", " + String.fromCharCode(0x203a) + " pre výber mesiaca\n" +
+"- Podržte tlačítko myši na akomkoľvek z týchto tlačítok pre rýchlejší výber.";
 
-Calendar._TT["PREV_YEAR"] = "Predchádzajúci rok (pridrž pre menu)";
-Calendar._TT["PREV_MONTH"] = "Predchádzajúci mesiac (pridrž pre menu)";
-Calendar._TT["GO_TODAY"] = "Dnešný dátum";
-Calendar._TT["NEXT_MONTH"] = "Daľší mesiac (pridrž pre menu)";
-Calendar._TT["NEXT_YEAR"] = "Daľší rok (pridrž pre menu)";
-Calendar._TT["SEL_DATE"] = "Zvoľ dátum";
-Calendar._TT["DRAG_TO_MOVE"] = "Chyť a ťahaj pre presun";
-Calendar._TT["PART_TODAY"] = " (dnes)";
+Calendar._TT["ABOUT_TIME"] = "\n\n" +
+"Výber času:\n" +
+"- Kliknite na akúkoľvek časť z výberu času pre zvýšenie.\n" +
+"- alebo Shift-klick pre zníženie\n" +
+"- alebo kliknite a ťahajte pre rýchlejší výber.";
 
 // the following is to inform that "%s" is to be the first day of week
 // %s will be replaced with the day name.
-Calendar._TT["DAY_FIRST"] = "Zobraz %s prvý";
+Calendar._TT["DAY_FIRST"] = "Zobraz %s ako prvý";
 
 // This may be locale-dependent.  It specifies the week-end days, as an array
 // of comma-separated numbers.  The numbers are from 0 to 6: 0 means Sunday, 1
@@ -122,8 +61,8 @@ Calendar._TT["TODAY"] = "Dnes";
 Calendar._TT["TIME_PART"] = "(Shift-)Klikni alebo ťahaj pre zmenu hodnoty";
 
 // date formats
-Calendar._TT["DEF_DATE_FORMAT"] = "%Y-%m-%d";
+Calendar._TT["DEF_DATE_FORMAT"] = "d.m.yy";
 Calendar._TT["TT_DATE_FORMAT"] = "%a, %b %e";
 
-Calendar._TT["WK"] = "wk";
+Calendar._TT["WK"] = "týž";
 Calendar._TT["TIME"] = "Čas:";
