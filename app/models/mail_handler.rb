dcf5ba1ea6cc66c49bb6af2e456ee2790707fc9a
Jean-Philippe Lang <jp_lang@yahoo.fr>
2008-12-12 19:11:16 +0000

    Make use of User.find_by_mail
    git-svn-id: svn+ssh://rubyforge.org/var/svn/redmine/trunk@2129 e93f8b46-1217-0410-a6f0-8f06a7374b81

--- app/models/mail_handler.rb
+++ app/models/mail_handler.rb
@@ -39,7 +39,7 @@ class MailHandler < ActionMailer::Base
   # Processes incoming emails
   def receive(email)
     @email = email
-    @user = User.active.find(:first, :conditions => ["LOWER(mail) = ?", email.from.first.to_s.strip.downcase])
+    @user = User.active.find_by_mail(email.from.first.to_s.strip)
     unless @user
       # Unknown user => the email is ignored
       # TODO: ability to create the user's account
