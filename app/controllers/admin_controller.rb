a22f3d6aa7a55416d4a255423a143e5f47c8be99
Jean-Philippe Lang <jp_lang@yahoo.fr>
2008-12-31 11:48:56 +0000

    Admin Info Screen: Display if plugin assets directory is writable (#2425).
    git-svn-id: svn+ssh://rubyforge.org/var/svn/redmine/trunk@2218 e93f8b46-1217-0410-a6f0-8f06a7374b81

--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -86,6 +86,7 @@ class AdminController < ApplicationController
     @flags = {
       :default_admin_changed => User.find(:first, :conditions => ["login=? and hashed_password=?", 'admin', User.hash_password('admin')]).nil?,
       :file_repository_writable => File.writable?(Attachment.storage_path),
+      :plugin_assets_writable => File.writable?(Engines.public_directory),
       :rmagick_available => Object.const_defined?(:Magick)
     }
   end  
