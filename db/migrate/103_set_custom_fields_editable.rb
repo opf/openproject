08304afe547da2460ca24f92474da7f677c21e75
Jean-Philippe Lang <jp_lang@yahoo.fr>
2009-01-18 10:54:08 +0000

    Fixes 103_set_custom_fields_editable migration from r2276 (#2526).
    git-svn-id: svn+ssh://rubyforge.org/var/svn/redmine/trunk@2279 e93f8b46-1217-0410-a6f0-8f06a7374b81

--- db/migrate/103_set_custom_fields_editable.rb
+++ db/migrate/103_set_custom_fields_editable.rb
@@ -1,9 +1,9 @@
 class SetCustomFieldsEditable < ActiveRecord::Migration
   def self.up
-    UserCustomField.update_all('editable = false')
+    UserCustomField.update_all("editable = #{CustomField.connection.quoted_false}")
   end
 
   def self.down
-    UserCustomField.update_all('editable = true')
+    UserCustomField.update_all("editable = #{CustomField.connection.quoted_true}")
   end
 end
