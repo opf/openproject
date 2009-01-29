32d4378198b83a10bab53c8e7cee2e4ec8adb818
Jean-Philippe Lang <jp_lang@yahoo.fr>
2009-01-29 13:54:44 +0000

    Adds rel='nofollow' attribute to other formats download links (#2491).
    git-svn-id: svn+ssh://rubyforge.org/var/svn/redmine/trunk@2334 e93f8b46-1217-0410-a6f0-8f06a7374b81

--- lib/redmine/views/other_formats_builder.rb
+++ lib/redmine/views/other_formats_builder.rb
@@ -25,7 +25,7 @@ module Redmine
       def link_to(name, options={})
         url = { :format => name.to_s.downcase }.merge(options.delete(:url) || {})
         caption = options.delete(:caption) || name
-        html_options = { :class => name.to_s.downcase }.merge(options)
+        html_options = { :class => name.to_s.downcase, :rel => 'nofollow' }.merge(options)
         @view.content_tag('span', @view.link_to(caption, url, html_options))
       end
     end
