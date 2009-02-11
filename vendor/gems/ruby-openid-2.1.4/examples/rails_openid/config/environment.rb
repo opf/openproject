f70be197e0aefd2d352faf3749d0db0d2e7b5c14
Eric Davis <edavis@littlestreamsoftware.com>
2009-02-11 19:06:37 +0000

    Unpacked OpenID gem. #699
    git-svn-id: svn+ssh://rubyforge.org/var/svn/redmine/trunk@2437 e93f8b46-1217-0410-a6f0-8f06a7374b81

--- vendor/gems/ruby-openid-2.1.4/examples/rails_openid/config/environment.rb
+++ vendor/gems/ruby-openid-2.1.4/examples/rails_openid/config/environment.rb
@@ -4,18 +4,8 @@
 # you don't control web/app server and can't set it the proper way
 # ENV['RAILS_ENV'] ||= 'production'
 
-# Specifies gem version of Rails to use when vendor/rails is not present
-RAILS_GEM_VERSION = '2.1.2' unless defined? RAILS_GEM_VERSION
-
 # Bootstrap the Rails environment, frameworks, and default configuration
 require File.join(File.dirname(__FILE__), 'boot')
-
-# Load Engine plugin if available
-begin
-  require File.join(File.dirname(__FILE__), '../vendor/plugins/engines/boot')
-rescue LoadError
-  # Not available
-end
 
 Rails::Initializer.run do |config|
   # Settings in config/environments/* take precedence those specified here
@@ -23,25 +13,23 @@ Rails::Initializer.run do |config|
   # Skip frameworks you're not going to use
   # config.frameworks -= [ :action_web_service, :action_mailer ]
 
-  # Add additional load paths for sweepers
-  config.load_paths += %W( #{RAILS_ROOT}/app/sweepers )
+  # Add additional load paths for your own custom dirs
+  # config.load_paths += %W( #{RAILS_ROOT}/extras )
 
   # Force all environments to use the same logger level 
   # (by default production uses :info, the others :debug)
   # config.log_level = :debug
 
   # Use the database for sessions instead of the file system
-  # (create the session table with 'rake db:sessions:create')
+  # (create the session table with 'rake create_sessions_table')
   # config.action_controller.session_store = :active_record_store
-  config.action_controller.session_store = :PStore
 
   # Enable page/fragment caching by setting a file-based store
   # (remember to create the caching directory and make it readable to the application)
   # config.action_controller.fragment_cache_store = :file_store, "#{RAILS_ROOT}/cache"
-  
+
   # Activate observers that should always be running
   # config.active_record.observers = :cacher, :garbage_collector
-  config.active_record.observers = :message_observer
 
   # Make Active Record use UTC-base instead of local time
   # config.active_record.default_timezone = :utc
@@ -49,11 +37,18 @@ Rails::Initializer.run do |config|
   # Use Active Record's schema dumper instead of SQL when creating the test database
   # (enables use of different database adapters for development and test environments)
   # config.active_record.schema_format = :ruby
-  
-  # Deliveries are disabled by default. Do NOT modify this section.
-  # Define your email configuration in email.yml instead.
-  # It will automatically turn deliveries on
-  config.action_mailer.perform_deliveries = false
-  
-  config.gem 'ruby-openid', :lib => 'openid'
+
+  # See Rails::Configuration for more options
 end
+
+# Add new inflection rules using the following format 
+# (all these examples are active by default):
+# Inflector.inflections do |inflect|
+#   inflect.plural /^(ox)$/i, '\1en'
+#   inflect.singular /^(ox)en/i, '\1'
+#   inflect.irregular 'person', 'people'
+#   inflect.uncountable %w( fish sheep )
+# end
+
+# Include your application configuration below
+ActionController::CgiRequest::DEFAULT_SESSION_OPTIONS[:session_key] = '_session_id_2'
