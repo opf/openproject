4d892fd6d003ca75d1e5c3009002df7a108f55c1
Jean-Philippe Lang <jp_lang@yahoo.fr>
2009-02-27 11:32:43 +0000

    Fixes #2851.
    git-svn-id: svn+ssh://rubyforge.org/var/svn/redmine/trunk@2531 e93f8b46-1217-0410-a6f0-8f06a7374b81

--- lib/tabular_form_builder.rb
+++ lib/tabular_form_builder.rb
@@ -41,7 +41,7 @@ class TabularFormBuilder < ActionView::Helpers::FormBuilder
   # Returns a label tag for the given field
   def label_for_field(field, options = {})
       return '' if options.delete(:no_label)
-      text = l(options[:label]) if options[:label]
+      text = options[:label].is_a?(Symbol) ? l(options[:label]) : options[:label]
       text ||= l(("field_" + field.to_s.gsub(/\_id$/, "")).to_sym)
       text << @template.content_tag("span", " *", :class => "required") if options.delete(:required)
       @template.content_tag("label", text, 
