8d6d9a80d2c327c3f91eb243ba297a2169bd6153
Jean-Philippe Lang <jp_lang@yahoo.fr>
2009-03-28 12:43:21 +0000

    Fixed: News summary field is not searchable (#2998).
    git-svn-id: svn+ssh://rubyforge.org/var/svn/redmine/trunk@2641 e93f8b46-1217-0410-a6f0-8f06a7374b81

--- app/models/news.rb
+++ app/models/news.rb
@@ -24,7 +24,7 @@ class News < ActiveRecord::Base
   validates_length_of :title, :maximum => 60
   validates_length_of :summary, :maximum => 255
 
-  acts_as_searchable :columns => ['title', "#{table_name}.description"], :include => :project
+  acts_as_searchable :columns => ['title', 'summary', "#{table_name}.description"], :include => :project
   acts_as_event :url => Proc.new {|o| {:controller => 'news', :action => 'show', :id => o.id}}
   acts_as_activity_provider :find_options => {:include => [:project, :author]},
                             :author_key => :author_id
