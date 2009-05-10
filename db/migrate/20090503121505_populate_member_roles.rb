bbb5a47b2ab95674c27d837f1279134e4c54b720
Jean-Philippe Lang <jp_lang@yahoo.fr>
2009-05-10 11:18:09 +0000

    Clear member_roles table before populating it.
    git-svn-id: svn+ssh://rubyforge.org/var/svn/redmine/trunk@2729 e93f8b46-1217-0410-a6f0-8f06a7374b81

--- db/migrate/20090503121505_populate_member_roles.rb
+++ db/migrate/20090503121505_populate_member_roles.rb
@@ -1,5 +1,6 @@
 class PopulateMemberRoles < ActiveRecord::Migration
   def self.up
+    MemberRole.delete_all
     Member.find(:all).each do |member|
       MemberRole.create!(:member_id => member.id, :role_id => member.role_id)
     end
