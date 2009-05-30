fbfb34949677c41734531a552547fb216537999f
Eric Davis <edavis@littlestreamsoftware.com>
2009-05-30 23:00:22 +0000

    Added Redmine::Info.issue to get links to specific issue numbers.
    git-svn-id: svn+ssh://rubyforge.org/var/svn/redmine/trunk@2776 e93f8b46-1217-0410-a6f0-8f06a7374b81

--- lib/redmine/info.rb
+++ lib/redmine/info.rb
@@ -5,6 +5,11 @@ module Redmine
       def url; 'http://www.redmine.org/' end
       def help_url; 'http://www.redmine.org/guide' end
       def versioned_name; "#{app_name} #{Redmine::VERSION}" end
+
+      # Creates the url string to a specific Redmine issue
+      def issue(issue_id)
+        url + 'issues/' + issue_id.to_s
+      end
     end
   end
 end
