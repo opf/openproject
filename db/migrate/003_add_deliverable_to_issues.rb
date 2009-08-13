8756c4747f02b057f3d8f55d8adeab10e438a6b4
hjust <hjust@7926756e-e54e-46e6-9721-ed318f58905e>
2009-08-13 13:23:58 +0000

    allow null values for deliverable_id in issues
    git-svn-id: https://dev.finn.de/svn/cockpit/trunk@10 7926756e-e54e-46e6-9721-ed318f58905e

--- db/migrate/003_add_deliverable_to_issues.rb
+++ db/migrate/003_add_deliverable_to_issues.rb
@@ -1,6 +1,6 @@
 class AddDeliverableToIssues < ActiveRecord::Migration
   def self.up
-    add_column :issues, :deliverable_id, :integer, :null => false
+    add_column :issues, :deliverable_id, :integer, :null => true
   end
 
   def self.down
