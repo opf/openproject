71abeb5898f1ee1be88fb3071c2d7e5ea8e8028b
Eric Davis <edavis@littlestreamsoftware.com>
2009-09-01 04:09:32 +0000

    Add some margin to journal notes if Gravatars are enabled. (#3771)
    git-svn-id: svn+ssh://rubyforge.org/var/svn/redmine/trunk@2848 e93f8b46-1217-0410-a6f0-8f06a7374b81

--- app/helpers/journals_helper.rb
+++ app/helpers/journals_helper.rb
@@ -30,7 +30,10 @@ module JournalsHelper
     end
     content << content_tag('div', links.join(' '), :class => 'contextual') unless links.empty?
     content << textilizable(journal, :notes)
-    content_tag('div', content, :id => "journal-#{journal.id}-notes", :class => (editable ? 'wiki editable' : 'wiki'))
+    css_classes = "wiki"
+    css_classes << " editable" if editable
+    css_classes << " gravatar-margin" if Setting.gravatar_enabled?
+    content_tag('div', content, :id => "journal-#{journal.id}-notes", :class => css_classes)
   end
   
   def link_to_in_place_notes_editor(text, field_id, url, options={})
