670e249307c9850a67a1aba0820655987e3dc662
hjust <hjust@7926756e-e54e-46e6-9721-ed318f58905e>
2009-09-17 10:04:31 +0000

    Fixed editinplace Javascript for IE6. Fixes #10619
    git-svn-id: https://dev.finn.de/svn/cockpit/trunk@130 7926756e-e54e-46e6-9721-ed318f58905e

--- assets/javascripts/editinplace.js
+++ assets/javascripts/editinplace.js
@@ -3,14 +3,14 @@ function initialize_editinplace(cancelButtonAttributes) {
 }
 
 function getCurrencyValue(str) {
-  var result = str.trim().match(/^(([0-9]+[.,])+[0-9]+) (.+)/);
+  var result = str.match(/^\s*(([0-9]+[.,])+[0-9]+) (.+)\s*/);
   return result ? new Array(result[1], result[3]) : new Array(str, "");
 }
 
 function makeEditable(id, name){
   var obj = $(id);
   obj.addClassName("inline_editable");
-  Event.observe(id, 'click', function(){edit_and_focus(obj, name)}, false);
+  Event.observe(id, 'click', function(){edit_and_focus(obj, name)});
   
 }
 
@@ -23,21 +23,20 @@ function edit_and_focus(obj, name) {
 
 function edit(obj, name, obj_value) {
   Element.hide(obj);
-  
+
   var obj_value = typeof(obj_value) != 'undefined' ? obj_value : obj.innerHTML;
   var parsed = getCurrencyValue(obj_value);
   var value = parsed[0];
   var currency = parsed[1]
   
-  
   var span_in = '<span id="'+obj.id+'_editor">'
-    var text = '<input id="'+obj.id+'_edit" name="'+name+'" size="7" value="'+value+'" class="currency"/> '+currency;
+    var text = '<input id="'+obj.id+'_edit" name="'+name+'" size="7" value="'+value+'"/> '+currency;
     var button = '<input id="'+obj.id+'_cancel" type="image" '+ _cancelButtonAttributes  +' /> ';
   var span_end = '</span>';
   
   new Insertion.After(obj, span_in+text+button+span_end);
 
-  Event.observe(obj.id+'_cancel', 'click', function(){cleanUp(obj)}, false);
+  Event.observe(obj.id+'_cancel', 'click', function(){cleanUp(obj)});
 }
 
 function cleanUp(obj){
