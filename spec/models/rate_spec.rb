bc6029023b0e22600a9d5e3fef5361282ffead27
rkh <rkh@7926756e-e54e-46e6-9721-ed318f58905e>
2009-10-07 17:29:24 +0000

    added tests (still pending)
    git-svn-id: https://dev.finn.de/svn/cockpit/trunk@163 7926756e-e54e-46e6-9721-ed318f58905e

--- spec/models/rate_spec.rb
+++ spec/models/rate_spec.rb
@@ -1,6 +1,36 @@
 require File.dirname(__FILE__) + '/../spec_helper'
 
 describe Rate do
+
   fixtures :cost_types
   fixtures :rates
+
+  describe "changing valid_from" do
+
+    it "should update all cost_entries correctly when rates change without changing order" do
+      pending
+    end
+
+    it "should update all cost_entries correctly when rates change order" do
+      pending
+    end
+
+    it "should update all cost_entries correctly when a rate is added before all other rates" do
+      pending
+    end
+
+    it "should update all cost_entries correctly when a rate is added after all other rates" do
+      pending
+    end
+
+    it "should update all cost_entries correctly when a rate is added between two other rates" do
+      pending
+    end
+
+    it "should update all cost_entries correctly when a rate is removed" do
+      pending
+    end
+
+  end
+
 end
