df6232171d8f85bdd492d8e74006008dbb23ce4f
rkh <rkh@7926756e-e54e-46e6-9721-ed318f58905e>
2009-10-13 15:54:56 +0000

    more specs, more fixtures. one still failing
    git-svn-id: https://dev.finn.de/svn/cockpit/trunk@189 7926756e-e54e-46e6-9721-ed318f58905e

--- spec/models/cost_entry_spec.rb
+++ spec/models/cost_entry_spec.rb
@@ -2,8 +2,9 @@ require File.expand_path(File.dirname(__FILE__) + '/../spec_helper')
 
 describe CostEntry do
   
-  before do
+  before(:each) do
     User.current = users("admin")
+    @example = cost_entries "example"
   end
 
   fixtures :users
@@ -12,14 +13,34 @@ describe CostEntry do
   fixtures :rates
   fixtures :projects
   fixtures :issues
+  fixtures :trackers
+  fixtures :enumerations
+  fixtures :issue_statuses
 
   it "should always prefer overridden_costs" do
-    example = cost_entries "example"
     value = rand(500)
-    example.overridden_costs = value
-    example.overridden_costs.should == value
-    example.real_costs.should == value
-    example.save!
+    @example.overridden_costs = value
+    @example.overridden_costs.should == value
+    @example.real_costs.should == value
+    @example.save!
+  end
+  
+  it "should return the current costs depending on the number of units" do
+    (0..100).each do |units|
+      @example.units = units
+      @example.save!
+      @example.costs.should == @example.cost_type.rate_at(@example.spent_on).rate * units
+    end
+  end
+  
+  it "should update cost if a new rate is added" do
+    @example.cost_type = cost_types("umbrella")
+    @example.spent_on = Time.now
+    @example.units = 1
+    @example.save!
+    @example.costs.should == rates("cheap_one").rate
+    cheap = CostRate.create! :valid_from => 10.minutes.ago, :rate => 1.0, :cost_type => cost_types("umbrella")
+    @example.costs.should == cheap.rate
   end
 
 end
