af24eae1ac7fbf6d0873284ddfa7688400d0633c
rkh <rkh@7926756e-e54e-46e6-9721-ed318f58905e>
2009-10-13 15:07:32 +0000

    make sure current_user is an admin when running tests
    git-svn-id: https://dev.finn.de/svn/cockpit/trunk@185 7926756e-e54e-46e6-9721-ed318f58905e

--- spec/models/cost_entry_spec.rb
+++ spec/models/cost_entry_spec.rb
@@ -1,6 +1,10 @@
 require File.expand_path(File.dirname(__FILE__) + '/../spec_helper')
 
 describe CostEntry do
+  
+  before do
+    User.current = users("admin")
+  end
 
   fixtures :users
   fixtures :cost_types
