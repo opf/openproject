57f9880b5ceec210436681731f5089499ea30aa0
rkh <rkh@7926756e-e54e-46e6-9721-ed318f58905e>
2009-10-16 11:45:49 +0000

    created time entry spec
    git-svn-id: https://dev.finn.de/svn/cockpit/trunk@197 7926756e-e54e-46e6-9721-ed318f58905e

---
+++ spec/models/time_entry_spec.rb
@@ -0,0 +1,19 @@
+require File.expand_path(File.dirname(__FILE__) + '/../spec_helper')
+
+describe CostEntry do
+
+  before(:each) do
+    User.current = users("admin")
+  end
+
+  fixtures :users
+  fixtures :cost_types
+  fixtures :cost_entries
+  fixtures :rates
+  fixtures :projects
+  fixtures :issues
+  fixtures :trackers
+  fixtures :enumerations
+  fixtures :issue_statuses
+
+end
