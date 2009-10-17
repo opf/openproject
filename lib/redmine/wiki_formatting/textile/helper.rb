07ffad4a7ec566be782ae6e07fb5191d7f3a771c
Eric Davis <edavis@littlestreamsoftware.com>
2009-10-17 21:17:46 +0000

    Rename the wiki toolbar's JavaScript variable, 'toolbar' conflicts with the browser's variable.
    git-svn-id: svn+ssh://rubyforge.org/var/svn/redmine/trunk@2927 e93f8b46-1217-0410-a6f0-8f06a7374b81

--- lib/redmine/wiki_formatting/textile/helper.rb
+++ lib/redmine/wiki_formatting/textile/helper.rb
@@ -30,7 +30,7 @@ module Redmine
           javascript_include_tag('jstoolbar/jstoolbar') +
             javascript_include_tag('jstoolbar/textile') +
             javascript_include_tag("jstoolbar/lang/jstoolbar-#{current_language}") +
-          javascript_tag("var toolbar = new jsToolBar($('#{field_id}')); toolbar.setHelpLink('#{help_link}'); toolbar.draw();")
+          javascript_tag("var wikiToolbar = new jsToolBar($('#{field_id}')); wikiToolbar.setHelpLink('#{help_link}'); wikiToolbar.draw();")
         end
       
         def initial_page_content(page)
