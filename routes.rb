f3f6e294d570c7032bc4a2c3cc23094e23c2312e
rkh <rkh@7926756e-e54e-46e6-9721-ed318f58905e>
2009-11-12 18:03:40 +0000

    fixed routes
    git-svn-id: https://dev.finn.de/svn/cockpit/trunk@296 7926756e-e54e-46e6-9721-ed318f58905e

--- routes.rb
+++
@@ -1,4 +0,0 @@
-map.connect 'projects/:project_id/costlog/:action/:id', :controller => 'costlog', :project_id => /.+/
-map.connect 'projects/:project_id/cost_reports/:action/:id', :controller => 'cost_reports', :project_id => /.+/
-map.connect 'projects/:project_id/cost_objects/:action/:id', :controller => 'cost_objects'
-map.connect 'projects/:project_id/hourly_rates/:action/:id', :controller => 'hourly_rates', :project_id => /.+/
