9b0acce9c938e6c1bbbe2c1a4138105513a08b33
Jean-Philippe Lang <jp_lang@yahoo.fr>
2009-11-24 22:02:14 +0000

    Fixed: error raised when rendering text that contains an email address with textile disabled (#4268).
    git-svn-id: svn+ssh://rubyforge.org/var/svn/redmine/trunk@3088 e93f8b46-1217-0410-a6f0-8f06a7374b81

--- lib/redmine/wiki_formatting.rb
+++ lib/redmine/wiki_formatting.rb
@@ -53,6 +53,7 @@ module Redmine
       class Formatter
         include ActionView::Helpers::TagHelper
         include ActionView::Helpers::TextHelper
+        include ActionView::Helpers::UrlHelper
         
         def initialize(text)
           @text = text
---
+++ test/unit/lib/redmine/wiki_formatting.rb
@@ -0,0 +1,35 @@
+# Redmine - project management software
+# Copyright (C) 2006-2009  Jean-Philippe Lang
+#
+# This program is free software; you can redistribute it and/or
+# modify it under the terms of the GNU General Public License
+# as published by the Free Software Foundation; either version 2
+# of the License, or (at your option) any later version.
+# 
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+# 
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+
+require File.dirname(__FILE__) + '/../../../test_helper'
+
+class Redmine::WikiFormattingTest < ActiveSupport::TestCase
+  
+  def test_should_link_urls_and_email_addresses
+    raw = <<-DIFF
+This is a sample *text* with a link: http://www.redmine.org
+and an email address foo@example.net
+DIFF
+
+    expected = <<-EXPECTED
+<p>This is a sample *text* with a link: <a href="http://www.redmine.org">http://www.redmine.org</a><br />
+and an email address <a href="mailto:foo@example.net">foo@example.net</a></p>
+EXPECTED
+
+    assert_equal expected.gsub(%r{[\r\n\t]}, ''), Redmine::WikiFormatting::NullFormatter::Formatter.new(raw).to_html.gsub(%r{[\r\n\t]}, '')
+  end
+end
