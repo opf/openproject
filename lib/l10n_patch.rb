ab260911ae4b541d18efdaebf36388f4afbc4cf0
hjust <hjust@7926756e-e54e-46e6-9721-ed318f58905e>
2009-11-26 14:50:06 +0000

    Circumvent a bug in Siemens's ruby version which fails to correctly converting a BigDecimal into a Float (#13483)
    git-svn-id: https://dev.finn.de/svn/cockpit/trunk@312 7926756e-e54e-46e6-9721-ed318f58905e

--- lib/l10n_patch.rb
+++ lib/l10n_patch.rb
@@ -8,6 +8,10 @@ module ActionView::Helpers::NumberHelper
     options[:unit] = Setting.plugin_redmine_costs['costs_currency'] unless options[:unit]
     options[:format] = Setting.plugin_redmine_costs['costs_currency_format'] unless options[:format]
     
+    # FIXME: patch ruby instead of this code
+    # this circumvents the broken BigDecimal#to_f on Siemens's ruby
+    number = number.to_s if number.is_a? BigDecimal
+    
     number_to_currency_without_l10n(number, options)
   end
 
