4c2264ee42eceeedc4787b9f3736b0bd19880d7a
Jean-Philippe Lang <jp_lang@yahoo.fr>
2009-11-28 11:59:45 +0000

    Adds 2 buttons to easily reorder selected columns (#4272).
    git-svn-id: svn+ssh://rubyforge.org/var/svn/redmine/trunk@3106 e93f8b46-1217-0410-a6f0-8f06a7374b81

--- public/javascripts/select_list_move.js
+++ public/javascripts/select_list_move.js
@@ -7,6 +7,17 @@ function addOption(theSel, theText, theValue)
   theSel.options[selLength] = newOpt;
 }
 
+function swapOptions(theSel, index1, index2)
+{
+	var text, value;
+  text = theSel.options[index1].text;
+  value = theSel.options[index1].value;
+  theSel.options[index1].text = theSel.options[index2].text;
+  theSel.options[index1].value = theSel.options[index2].value;
+  theSel.options[index2].text = text;
+  theSel.options[index2].value = value;
+}
+
 function deleteOption(theSel, theIndex)
 { 
   var selLength = theSel.length;
@@ -43,6 +54,22 @@ function moveOptions(theSelFrom, theSelTo)
   }
   
   if(NS4) history.go(0);
+}
+
+function moveOptionUp(theSel) {
+	var index = theSel.selectedIndex;
+	if (index > 0) {
+		swapOptions(theSel, index-1, index);
+  	theSel.selectedIndex = index-1;
+	}
+}
+
+function moveOptionDown(theSel) {
+	var index = theSel.selectedIndex;
+	if (index < theSel.length - 1) {
+		swapOptions(theSel, index, index+1);
+  	theSel.selectedIndex = index+1;
+	}
 }
 
 function selectAllOptions(id)
