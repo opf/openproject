715cd02f79542663d9d2c45b7db548bdad067fbb
hjust <hjust@7926756e-e54e-46e6-9721-ed318f58905e>
2009-11-30 18:58:05 +0000

    implemented Project.allowed_to_conditions using the newly implemented User#allowed_to
    git-svn-id: https://dev.finn.de/svn/cockpit/trunk@329 7926756e-e54e-46e6-9721-ed318f58905e

--- lib/project_patch.rb
+++ lib/project_patch.rb
@@ -22,7 +22,7 @@ module ProjectPatch
       
       unless singleton_methods.include? "allowed_to_condition_without_inheritance"
         class << self
-          #alias_method_chain :allowed_to_condition, :inheritance
+          alias_method_chain :allowed_to_condition, :inheritance
         end
       end
       
@@ -32,39 +32,15 @@ module ProjectPatch
 
   module ClassMethods
     def allowed_to_condition_with_inheritance(user, permission, options={})
-      # we just added to user parameter to the calls to role.allowed_to?
-
-      statements = []
-      base_statement = "#{Project.table_name}.status=#{Project::STATUS_ACTIVE}"
-      if perm = Redmine::AccessControl.permission(permission)
-        unless perm.project_module.nil?
-          # If the permission belongs to a project module, make sure the module is enabled
-          base_statement << " AND #{Project.table_name}.id IN (SELECT em.project_id FROM #{EnabledModule.table_name} em WHERE em.name='#{perm.project_module}')"
-        end
-      end
       if options[:project]
-        project_statement = "#{Project.table_name}.id = #{options[:project].id}"
-        project_statement << " OR (#{Project.table_name}.lft > #{options[:project].lft} AND #{Project.table_name}.rgt < #{options[:project].rgt})" if options[:with_subprojects]
-        base_statement = "(#{project_statement}) AND (#{base_statement})"
-      end
-      if user.admin?
-        # no restriction
+        projects = [options[:project]]
       else
-        statements << "1=0"
-        if user.logged?
-          if Role.non_member.allowed_to?(permission, user) && !options[:member]
-            statements << "#{Project.table_name}.is_public = #{connection.quoted_true}"
-          end
-          allowed_project_ids = user.memberships.select {|m| m.roles.detect {|role| role.allowed_to?(permission, user)}}.collect {|m| m.project_id}
-          statements << "#{Project.table_name}.id IN (#{allowed_project_ids.join(',')})" if allowed_project_ids.any?
-        else
-          if Role.anonymous.allowed_to?(permission, user) && !options[:member]
-            # anonymous user allowed on public project
-            statements << "#{Project.table_name}.is_public = #{connection.quoted_true}"
-          end 
-        end
+        projects = Project.active.all(:conditions => Project.visible_by(user))
       end
-      statements.empty? ? base_statement : "((#{base_statement}) AND (#{statements.join(' OR ')}))"
+
+      projects = projects.collect(&:children).flatten.uniq if options[:with_subprojects]
+      
+      user.allowed_for(permission, projects)
     end
 
   end
