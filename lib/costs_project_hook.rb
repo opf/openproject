1483df22944727cd70f158a673cbbdac83fb12c0
hjust <hjust@7926756e-e54e-46e6-9721-ed318f58905e>
2009-12-03 19:14:27 +0000

    Setting of rates now woks with Redmine 0.9
    git-svn-id: https://dev.finn.de/svn/cockpit/trunk@361 7926756e-e54e-46e6-9721-ed318f58905e

--- lib/costs_project_hook.rb
+++ lib/costs_project_hook.rb
@@ -18,12 +18,16 @@ class CostsProjectHook < Redmine::Hook::ViewListener
     result
   end
  
-  # Renders an AJAX from to update the member's billing rate
-  #
+  # Renders an AJAX form to update the member's billing rate
   # Context:
   # * :project => Current project
   # * :member => Current Member record
   render_on :view_projects_settings_members_table_row, :partial => 'hooks/view_projects_settings_members_table_row'
 
+  # Renders table headers to update the member's billing rate
+  # Context:
+  # * :project => Current project
+  render_on :view_projects_settings_members_table_header, :partial => 'hooks/view_projects_settings_members_table_header'
+    
   # TODO: implement  model_project_copy_before_save
 end
