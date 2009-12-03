9be41a19f1f0b317a32277b843c7f39d3eccb299
hjust <hjust@7926756e-e54e-46e6-9721-ed318f58905e>
2009-12-03 13:10:52 +0000

    Added additional routes for a more fine granular routing.
    git-svn-id: https://dev.finn.de/svn/cockpit/trunk@355 7926756e-e54e-46e6-9721-ed318f58905e

--- config/routes.rb
+++ config/routes.rb
@@ -17,7 +17,41 @@ ActionController::Routing::Routes.draw do |map|
     end
   end
   
+  map.with_options :controller => 'cost_objects' do |cost_objects|
+    cost_objects.with_options :conditions => {:method => :get} do |co_views|
+      co_views.connect 'cost_objects', :action => 'index'
+      co_views.connect 'cost_objects.:format', :action => 'index'
+      co_views.connect 'projects/:project_id/cost_objects', :action => 'index'
+      co_views.connect 'projects/:project_id/cost_objects.:format', :action => 'index'
+      
+      co_views.connect 'projects/:project_id/cost_objects/new', :action => 'new'
+      co_views.connect 'projects/:project_id/cost_objects/:copy_from/copy', :action => 'new'
+      co_views.connect 'cost_objects/:id', :action => 'show', :id => /\d+/
+      co_views.connect 'cost_objects/:id.:format', :action => 'show', :id => /\d+/
+      co_views.connect 'cost_objects/:id/edit', :action => 'edit', :id => /\d+/
+      co_views.connect 'cost_objects/:id/move', :action => 'move', :id => /\d+/
+    end
+    cost_objects.with_options :conditions => {:method => :post} do |co_actions|
+      co_actions.connect 'projects/:project_id/cost_objects', :action => 'new'
+      co_actions.connect 'cost_objects/:id/:action', :action => /edit|move|destroy/, :id => /\d+/
+    end
+    cost_objects.connect 'cost_objects/:action'
+  end
+  
+  map.with_options :controller => 'hourly_rates' do |hourly_rates|
+    hourly_rates.with_options :conditions => {:method => :get} do |hr_views|
+      hr_views.connect 'users/:id/default_rates', :action => 'show'
+      hr_views.connect 'users/:id/default_rates/:action', :action => 'edit'
+
+      hr_views.connect 'projects/:project_id/hourly_rates/:id', :action => 'show'
+      hr_views.connect 'projects/:project_id/hourly_rates/:id/:action', :action => 'edit'
+    end
+    hourly_rates.with_options :conditions => {:method => :post} do |hr_actions|
+      hr_actions.connect 'users/:id/default_rates/:action', :action => 'edit'
+      hr_actions.connect 'projects/:project_id/hourly_rates/:id/:action', :action => 'edit'
+    end
+  end
+  
   map.connect 'projects/:project_id/cost_reports/:action/:id', :controller => 'cost_reports', :project_id => /.+/
-  map.connect 'projects/:project_id/cost_objects/:action/:id', :controller => 'cost_objects'
-  map.connect 'projects/:project_id/hourly_rates/:action/:id', :controller => 'hourly_rates', :project_id => /.+/
+#  map.connect 'projects/:project_id/hourly_rates/:action/:id', :controller => 'hourly_rates', :project_id => /.+/
 end
