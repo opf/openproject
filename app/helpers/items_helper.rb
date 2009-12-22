1776d15beef3cec8401de6af4f9011865716a47f
Nate Lowrie <nate@finelineautomation.com>
2009-12-22 15:34:58 -0500

    Added some code to determine if a subject was long enough to require 2 lines and if so, set the appropriate CSS class.  Not that this is an approximation and not an exact calculation, but it works pretty darn well.

--- app/helpers/items_helper.rb
+++ app/helpers/items_helper.rb
@@ -1,49 +1,57 @@
 module ItemsHelper
   unloadable
-  
+
   def assigned_to_id_or_empty(item)
     item.new_record? ? "" : item.issue.assigned_to_id
   end
-  
+
   def assignee_name_or_empty(item)
     issue = item.issue
     issue.nil? || issue.assigned_to.nil? ? "" : "#{issue.assigned_to.firstname} #{issue.assigned_to.lastname}"
   end
-  
+
   def description_or_empty(item)
     item.new_record? ? "" : textilizable(item.issue, :description)
   end
-  
+
   def element_id_or_empty(item)
     item.new_record? ? "" : "item_#{item.id}"
   end
-  
+
   def issue_id_or_default(item)
-    item.new_record? ? "<span class='issue_id'>0</span>" : 
+    item.new_record? ? "<span class='issue_id'>0</span>" :
                        link_to("<span class='issue_id'>#{item.issue.id}</span>", :controller => "issues", :action => "show", :id => item.issue.id)
   end
-  
+
   def mark_if_closed(item)
     !item.new_record? && item.issue.status.is_closed? ? "closed" : ""
   end
-  
+
   def mark_if_task(item)
     item.parent_id == 0 ? "" : "task"
   end
-  
-  
+
+  def one_or_two_line_height(item)
+    if item.backlog_id.nil? || item.backlog_id == 0
+      maxLength = 50
+    else
+      maxLength = 65
+    end
+    item.subject.length > maxLength ? "item_double" : ""
+  end
+
   def points_or_empty(item)
     item.points.nil? ? 0 : item.points
   end
-  
+
   def record_id_or_empty(item)
     item.new_record? ? "" : item.id
   end
-  
+
   def status_id_or_default(item)
     item.new_record? ? IssueStatus.find(:first, :order => "position ASC").id : item.issue.status.id
   end
-  
+
   def status_label_or_default(item)
     item.new_record? ? IssueStatus.find(:first, :order => "position ASC").name : item.issue.status.name
   end
