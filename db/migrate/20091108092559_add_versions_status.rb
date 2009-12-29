ac5cfe7a62288bfde7e5ee795518cdb5aa02de15
Jean-Philippe Lang <jp_lang@yahoo.fr>
2009-12-29 14:53:29 +0000

    Set status of existing versions to 'open' (#4504).
    git-svn-id: svn+ssh://rubyforge.org/var/svn/redmine/trunk@3259 e93f8b46-1217-0410-a6f0-8f06a7374b81

--- db/migrate/20091108092559_add_versions_status.rb
+++ db/migrate/20091108092559_add_versions_status.rb
@@ -1,6 +1,7 @@
 class AddVersionsStatus < ActiveRecord::Migration
   def self.up
     add_column :versions, :status, :string, :default => 'open'
+    Version.update_all("status = 'open'")
   end
 
   def self.down
