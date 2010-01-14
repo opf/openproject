64f4b50139a7ef9e33fc7df00caf4fab028e1cef
Azamat Hackimov <azamat.hackimov@gmail.com>
2010-01-14 16:10:56 +0000

    New Croatian language in Redmine! Thanks to Igor P. for translation (#4526) Also, updates for: * es.yml (#4578) * zh.yml (#4545)
    git-svn-id: svn+ssh://rubyforge.org/var/svn/redmine/trunk@3312 e93f8b46-1217-0410-a6f0-8f06a7374b81

---
+++ public/javascripts/jstoolbar/lang/jstoolbar-hr.js
@@ -0,0 +1,16 @@
+jsToolBar.strings = {};
+jsToolBar.strings['Strong'] = 'Podebljano';
+jsToolBar.strings['Italic'] = 'Kurziv';
+jsToolBar.strings['Underline'] = 'Podcrtano';
+jsToolBar.strings['Deleted'] = 'Obrisano';
+jsToolBar.strings['Code'] = 'Inline Code';
+jsToolBar.strings['Heading 1'] = 'Naslov 1';
+jsToolBar.strings['Heading 2'] = 'Naslov 2';
+jsToolBar.strings['Heading 3'] = 'Naslov 3';
+jsToolBar.strings['Unordered list'] = 'Graficke oznake';
+jsToolBar.strings['Ordered list'] = 'Numeriranje';
+jsToolBar.strings['Quote'] = 'Citat';
+jsToolBar.strings['Unquote'] = 'Ukloni citat';
+jsToolBar.strings['Preformatted text'] = 'Izveden tekst';
+jsToolBar.strings['Wiki link'] = 'Link na Wiki stranicu';
+jsToolBar.strings['Image'] = 'Slika';
