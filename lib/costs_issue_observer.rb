dd969ce3a951361b3d00a6cf5d4606afc29145c5
hjust <hjust@7926756e-e54e-46e6-9721-ed318f58905e>
2010-02-15 18:02:17 +0000

    typo
    git-svn-id: https://dev.finn.de/svn/cockpit/trunk@516 7926756e-e54e-46e6-9721-ed318f58905e

--- lib/costs_issue_observer.rb
+++ lib/costs_issue_observer.rb
@@ -2,7 +2,7 @@ class CostsIssueObserver < ActiveRecord::Observer
   observe :issue
 
   def after_update(issue)
-    if issue.project_changed?
+    if issue.project_id_changed?
       CostEntry.update_all({:project_id => issue.project_id}, {:issue_id => id})
     end 
   end
