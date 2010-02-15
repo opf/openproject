d22b00a541697c0e4e6a30ed98392bc0f1d6aa3e
hjust <hjust@7926756e-e54e-46e6-9721-ed318f58905e>
2010-02-15 17:53:40 +0000

    Add Issue observer to update cost entries if issue is moved.
    git-svn-id: https://dev.finn.de/svn/cockpit/trunk@514 7926756e-e54e-46e6-9721-ed318f58905e

---
+++ lib/costs_issue_observer.rb
@@ -0,0 +1,10 @@
+class CostsIssueObserver < ActiveRecord::Observer
+  observe :issue
+
+  def after_update(issue)
+    if issue.project_changed?
+      CostEntry.update_all({:project_id => issue.project_id}, {:issue_id => id})
+    end 
+  end
+end
+    
