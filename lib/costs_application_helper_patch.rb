3535ec9260380c1d148421c99a2683c6c285fa11
hjust <hjust@7926756e-e54e-46e6-9721-ed318f58905e>
2010-02-15 17:58:45 +0000

    fixed patch for applocation_helper
    git-svn-id: https://dev.finn.de/svn/cockpit/trunk@515 7926756e-e54e-46e6-9721-ed318f58905e

--- lib/costs_application_helper_patch.rb
+++ lib/costs_application_helper_patch.rb
@@ -2,31 +2,26 @@ require_dependency 'application_helper'
 
 module CostsApplicationHelperPatch
   def self.included(base) # :nodoc:
-    base.send(:include, InstanceMethods)
-
     # Same as typing in the class 
     base.class_eval do
-    end
-  end
-  
-  module InstanceMethods
-    def link_to_cost_object(cost_object, options={})
-      title = nil
-      subject = nil
-      if options[:subject] == false
-        subject = "#{l(:label_cost_object)} ##{cost_object.id}"
-        title = truncate(cost_object.subject, :length => 60)
-      else
-        subject = cost_object.subject
-        if options[:truncate]
-          subject = truncate(subject, :length => options[:truncate])
+      def link_to_cost_object(cost_object, options={})
+        title = nil
+        subject = nil
+        if options[:subject] == false
+          subject = "#{l(:label_cost_object)} ##{cost_object.id}"
+          title = truncate(cost_object.subject, :length => 60)
+        else
+          subject = cost_object.subject
+          if options[:truncate]
+            subject = truncate(subject, :length => options[:truncate])
+          end
         end
+        s = link_to subject, {:controller => "cost_objects", :action => "show", :id => cost_object}, 
+                                                     :class => cost_object.css_classes,
+                                                     :title => title
+        s = "#{h cost_object.project} - " + s if options[:project]
+        s
       end
-      s = link_to subject, {:controller => "cost_objects", :action => "show", :id => cost_object}, 
-                                                   :class => cost_object.css_classes,
-                                                   :title => title
-      s = "#{h cost_object.project} - " + s if options[:project]
-      s
     end
   end
 end
