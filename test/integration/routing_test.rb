c88a467ddf15f5a02a4ab3a8712e76142e3cca5c
Jean-Philippe Lang <jp_lang@yahoo.fr>
2010-03-28 15:18:09 +0000

    Fixes an error when running tests coverage.
    git-svn-id: svn+ssh://rubyforge.org/var/svn/redmine/trunk@3624 e93f8b46-1217-0410-a6f0-8f06a7374b81

--- test/integration/routing_test.rb
+++ test/integration/routing_test.rb
@@ -15,7 +15,7 @@
 # along with this program; if not, write to the Free Software
 # Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
 
-require "test_helper"
+require "#{File.dirname(__FILE__)}/../test_helper"
 
 class RoutingTest < ActionController::IntegrationTest
   context "issues" do
