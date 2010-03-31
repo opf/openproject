e116d5b5624df64d5cbeaa1b8d6477530394933e
friflaj <friflaj@ajellito.com>
2010-03-31 16:33:10 +0200

    Migration cleanup

--- db/migrate/009_create_stories_tasks_and_sprints.rb
+++ db/migrate/009_create_stories_tasks_and_sprints.rb
@@ -29,41 +29,49 @@ class CreateStoriesTasksAndSprints < ActiveRecord::Migration
     end
 
     if backlogs_present
-        res = execute "select issue_id, version_id, position, version_id, parent_id, points from items left join backlogs on backlog_id = backlogs.id"
-        res.each { |row|
-            issue, version, position, parent, points = row
+        say_with_time "Migrating Backlogs data..." do
+            res = execute "select issue_id, version_id, position, version_id, parent_id, points from items left join backlogs on backlog_id = backlogs.id"
+            res.each { |row|
+                issue, version, position, parent, points = row
 
-            if not version.nil? and version != 0
-                execute "update issues set fixed_version_id = #{version} where id = #{issue}"
-            end
+                if not version.nil? and version != 0
+                    execute "update issues set fixed_version_id = #{version} where id = #{issue}"
+                end
 
-            execute "update issues set position = #{position} where id = #{issue}"
+                execute "update issues set position = #{position} where id = #{issue}"
 
-            if not points.nil? and points != 0
-                execute "update issues set story_points = #{points} where id = #{issue}"
-            end
+                if not points.nil? and points != 0
+                    execute "update issues set story_points = #{points} where id = #{issue}"
+                end
 
-            if not parent.nil? and parent != 0
-                execute "update issues set parent_id = (select issue_id from items where id = #{parent})"
-            end
-        }
+                if not parent.nil? and parent != 0
+                    execute "update issues set parent_id = (select issue_id from items where id = #{parent})"
+                end
+            }
 
-        res = execute "select version_id, start_date, is_closed from backlogs"
-        res.each { |row|
-            version, start_date, is_closed = row
-            status = is_closed ? 'closed' : 'open'
+            res = execute "select version_id, start_date, is_closed from backlogs"
+            res.each { |row|
+                version, start_date, is_closed = row
+                status = is_closed ? 'closed' : 'open'
 
-            if not start_date.nil?
-                execute "update versions set start_date = '#{start_date}' where id = #{version}"
-            end
-            execute "update versions set status = '#{status}' where id = #{version}"
-        }
+                if not start_date.nil?
+                    execute "update versions set start_date = '#{start_date}' where id = #{version}"
+                end
+                execute "update versions set status = '#{status}' where id = #{version}"
+            }
+        end
     end
 
-    # force rebuild
-    execute "update issues set lft = NULL, rgt = NULL"
-    Issue.reset_column_information
-    Issue.rebuild!
+    # RM core started needing this... I'm not agreeing, but I need to
+    # get the migration working
+    execute "update issues set start_date = NULL where due_date < start_date"
+
+    say_with_time "Rebuilding issues tree..." do
+        # force rebuild
+        execute "update issues set lft = NULL, rgt = NULL"
+        Issue.reset_column_information
+        Issue.rebuild!
+    end
   end
 
   def self.down
