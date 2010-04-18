6fffc89a148d9cf1330c8357bbd24b29d4899314
Mark Maglana <mmaglana@gmail.com>
2010-04-18 19:25:07 +0800

    Rename is_sprint helper method to is_sprint? for clarity

--- app/helpers/backlogs_helper.rb
+++ app/helpers/backlogs_helper.rb
@@ -1,15 +1,15 @@
 module BacklogsHelper
   
   def backlog_html_class(backlog)
-    is_sprint(backlog) ? "sprint backlog" : "product backlog"
+    is_sprint?(backlog) ? "sprint backlog" : "product backlog"
   end
   
   def backlog_html_id(backlog)
-    is_sprint(backlog) ? "sprint_#{backlog.id}" : "product_backlog"
+    is_sprint?(backlog) ? "sprint_#{backlog.id}" : "product_backlog"
   end
 
   def backlog_id_or_empty(backlog)
-    is_sprint(backlog) ? backlog.id : ""
+    is_sprint?(backlog) ? backlog.id : ""
   end
   
   def date_or_nil(date)
@@ -17,15 +17,15 @@ module BacklogsHelper
   end
   
   def editable_if_sprint(backlog)
-    "editable" if is_sprint(backlog)
+    "editable" if is_sprint?(backlog)
   end
   
-  def is_sprint(backlog)
+  def is_sprint?(backlog)
     backlog.class.to_s.downcase=='sprint'
   end
   
   def name_or_default(backlog)
-    is_sprint(backlog) ? backlog.name : "Product Backlog"
+    is_sprint?(backlog) ? backlog.name : "Product Backlog"
   end
   
   def stories(backlog)
