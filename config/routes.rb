e95c76c0516100a1e234234b145b8285bbda7505
friflaj <friflaj@ajellito.com>
2010-04-28 10:28:46 +0200

    RESTful routes

---
+++ config/routes.rb
@@ -0,0 +1,9 @@
+ActionController::Routing::Routes.draw do |map|
+    map.connect 'backlogs/:project_id/:sprint_id/burndown', :controller => 'backlogs', :action => 'burndown'
+    map.connect 'backlogs/:project_id/issues', :controller => 'backlogs', :action => 'select_issues'
+    map.connect 'backlogs/:project_id/:sprint_id/issues', :controller => 'backlogs', :action => 'select_issues'
+    map.connect 'backlogs/:project_id/:sprint_id/wiki', :controller => 'backlogs', :action => 'wiki'
+    map.connect 'backlogs/:project_id/:sprint_id/cards', :controller => 'backlogs', :action => 'taskboard_cards'
+    map.connect 'backlogs/:project_id/cards', :controller => 'backlogs', :action => 'product_backlog_cards'
+end
+
