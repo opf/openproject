fb75d0a66c8c16bdfd6cb179eac659b5706ea43d
rkh <rkh@7926756e-e54e-46e6-9721-ed318f58905e>
2010-04-29 16:08:35 +0000

    clear routes
    git-svn-id: https://dev.finn.de/svn/cockpit/trunk@997 7926756e-e54e-46e6-9721-ed318f58905e

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,2 @@
-ActionController::Routing::Routes.draw do |map| 
-  map.connect 'projects/:project_id/costlog/:action/:id', :controller => 'costlog', :project_id => /.+/
-  map.connect 'projects/:project_id/cost_reports/:action/:id', :controller => 'cost_reports', :project_id => /.+/
-  map.connect 'projects/:project_id/cost_objects/:action/:id', :controller => 'cost_objects'
-  map.connect 'projects/:project_id/hourly_rates/:action/:id', :controller => 'hourly_rates', :project_id => /.+/
+ActionController::Routing::Routes.draw do |map|
 end
