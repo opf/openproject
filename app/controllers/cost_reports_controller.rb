702618567b964f609ea84327bb2a24753a821e97
p.tessenow <p.tessenow@7926756e-e54e-46e6-9721-ed318f58905e>
2010-05-14 19:48:10 +0000

    added skeleton to cost_reports_controler
    git-svn-id: https://dev.finn.de/svn/cockpit/branches/reporting_merge@1086 7926756e-e54e-46e6-9721-ed318f58905e

--- app/controllers/cost_reports_controller.rb
+++ app/controllers/cost_reports_controller.rb
@@ -1,2 +1,25 @@
 class CostReportsController < ApplicationController
+  def list
+  end
+
+  def show
+  end
+
+  def new
+  end
+
+  def create
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def delete
+  end
+
+  def index
+  end
 end
