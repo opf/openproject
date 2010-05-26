258f0ff6e9aa808c8bfe3004eaef687616c6bb95
Mark Maglana <mmaglana@gmail.com>
2010-05-26 10:00:57 +0800

    Remove unecessary helper

--- app/helpers/tasks_helper.rb
+++
@@ -1,8 +0,0 @@
-module TasksHelper
-  unloadable
-  
-  def issue_link_or_empty(task)
-    task.new_record? ? "" : link_to(task.id, {:controller => "issues", :action => "show", :id => task}, {:target => "_blank"})
-  end
-  
-end
