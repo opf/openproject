0ad646c1be45a7db8a8f827d27567de409b1caa2
p.tessenow <p.tessenow@7926756e-e54e-46e6-9721-ed318f58905e>
2010-06-03 11:47:04 +0000

    moved reporting routes from costs plugin to reporting plugin
    git-svn-id: https://dev.finn.de/svn/cockpit/branches/reporting_merge@1167 7926756e-e54e-46e6-9721-ed318f58905e

--- config/routes.rb
+++ config/routes.rb
@@ -59,8 +59,6 @@ ActionController::Routing::Routes.draw do |map|
     end
   end
   
-  map.connect 'projects/:project_id/cost_reports.:format', :controller => 'cost_reports', :project_id => /.+/, :action => 'index'
-  map.connect 'projects/:project_id/cost_reports/:action/:id', :controller => 'cost_reports', :project_id => /.+/
   map.connect 'projects/:project_id/costlog/:action/:id', :controller => 'costlog', :project_id => /.+/
 
 #  map.connect 'projects/:project_id/hourly_rates/:action/:id', :controller => 'hourly_rates', :project_id => /.+/
