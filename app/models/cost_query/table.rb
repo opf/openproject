4ad44da2f11562661eb4f17a69a78e063fb5550e
rkh <rkh@7926756e-e54e-46e6-9721-ed318f58905e>
2010-06-10 09:20:08 +0000

    making table instanciatable
    git-svn-id: https://dev.finn.de/svn/cockpit/branches/reporting_merge@1190 7926756e-e54e-46e6-9721-ed318f58905e

--- app/models/cost_query/table.rb
+++ app/models/cost_query/table.rb
@@ -1,5 +1,19 @@
 # encoding: UTF-8
+
+##
+# @example
+#   CostQuery::Table.new query, :rows => [:project_id, :user_id], :columns => [:spent_on, :tweak]
 class CostQuery::Table
+  attr_accessor :query
+
+  def initialize(query, options = {})
+    options = options.with_indifferent_access.merge :query => query
+    options.each do |k,v|
+      k = "#{k}=" if respond_to? "#{k}="
+      send(k, *v)
+    end
+  end
+
   def rows(*rows)
     return @rows if rows.empty?
     @rows = rows
