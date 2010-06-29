5b8a2f5c573126b0fbb75c01ff48912649553c04
p.tessenow <p.tessenow@7926756e-e54e-46e6-9721-ed318f58905e>
2010-06-29 20:03:59 +0000

    added all filter where we can select objects, to be displayed in our view
    git-svn-id: https://dev.finn.de/svn/cockpit/branches/reporting_merge@1253 7926756e-e54e-46e6-9721-ed318f58905e

--- app/models/cost_query/filter.rb
+++ app/models/cost_query/filter.rb
@@ -3,7 +3,18 @@ require "set"
 module CostQuery::Filter
   def self.all
     @all ||= Set[
-      CostQuery::Filter::ActivityId]
+      CostQuery::Filter::ActivityId,
+      CostQuery::Filter::AssignedToId,
+      CostQuery::Filter::CategoryId,
+      CostQuery::Filter::CostTypeId,
+      CostQuery::Filter::FixedVersionId,
+      CostQuery::Filter::IssueId,
+      CostQuery::Filter::PriorityId,
+      CostQuery::Filter::ProjectId,
+      CostQuery::Filter::StatusId,
+      CostQuery::Filter::TrackerId,
+      CostQuery::Filter::UserId
+      ]
   end
 
   def self.from_hash
