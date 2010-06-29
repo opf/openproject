b940e0d4d104c7a3c46df620b43dc80f1ee040e6
p.tessenow <p.tessenow@7926756e-e54e-46e6-9721-ed318f58905e>
2010-06-29 12:13:56 +0000

    fixed bug in activity_id filter available_values
    git-svn-id: https://dev.finn.de/svn/cockpit/branches/reporting_merge@1234 7926756e-e54e-46e6-9721-ed318f58905e

--- app/models/cost_query/filter/activity_id.rb
+++ app/models/cost_query/filter/activity_id.rb
@@ -1,5 +1,5 @@
 class CostQuery::Filter::ActivityId < CostQuery::Filter::Base
-  def available_values
-    Activity.all.map { |a| [a.name, a.id] }
+  def available_values(query=nil)
+    TimeEntryActivity.all.map { |a| [a.name, a.id] }
   end
 end
