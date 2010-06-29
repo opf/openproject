b5df44cfacaa8614d60485137cd26cef0a17167c
p.tessenow <p.tessenow@7926756e-e54e-46e6-9721-ed318f58905e>
2010-06-29 20:03:03 +0000

    renamed parameter, fixed comment in reporting helper
    git-svn-id: https://dev.finn.de/svn/cockpit/branches/reporting_merge@1242 7926756e-e54e-46e6-9721-ed318f58905e

--- app/helpers/reporting_helper.rb
+++ app/helpers/reporting_helper.rb
@@ -1,10 +1,10 @@
 module ReportingHelper
   ##
-  # For a given CostQuery::Filter f, return an array of hashes, that contain
+  # For a given CostQuery::Filter filter, return an array of hashes, that contain
   # the partials that should be rendered (:name) for that filter and necessary
   # parameters.
-  # @param [CostQuery::Filter] the filter which we want render
-  def html_elements(f)
+  # @param [CostQuery::Filter] the filter we want to render
+  def html_elements(filter)
     [
       {:name=>'activate_filter'},
       {:name=>'text', :text=>'foo'}]
