8124c459fb84176c81d84581f1be78c5baa3f2b9
t.felgentreff <t.felgentreff@7926756e-e54e-46e6-9721-ed318f58905e>
2010-06-30 13:04:46 +0000

    start implementing filter application
    git-svn-id: https://dev.finn.de/svn/cockpit/branches/reporting_merge@1262 7926756e-e54e-46e6-9721-ed318f58905e

--- app/controllers/cost_reports_controller.rb
+++ app/controllers/cost_reports_controller.rb
@@ -2,6 +2,63 @@ class CostReportsController < ApplicationController
   before_filter :find_optional_project, :only => [:index]
 
   helper :reporting
+  include ReportingHelper  
+  
+  ##
+  # Determines if the request contains filters to set
+  def set_filter?
+    params[:set_filter].to_i == 1
+  end
+  
+  ## 
+  # Determines whether the session-saved query can to be used
+  def session_query?
+    !session[:cost_query].nil?
+  end
+  
+  # FIXME: Remove € symbol
+  def walker(query)
+    walker = CostQuery::Walker.new(query)
+    walker
+  end
+
+  ##
+  # Find a query to search on
+  def query_params
+    filters = session[:cost_query] if session_query?
+    if set_filter?
+      filters = {}
+      filters[:operators] = params[:operators]
+      filters[:values] = params[:values]
+    end
+    filters ||= {:operators => {:user_id => "=", :tweek => "="},
+      :values => {:user_id => [User.current.id], :tweek => [Date.today.cweek]}}      
+  end
+
+  ##
+  # Build the query from the current request and save it to 
+  # the session.
+  def query
+    session[:cost_query] = query_params
+    CostQuery.new.tap do |q|
+      (session[:cost_query][:operators] || []).each do |filter, operator|
+        unless (session[:cost_query][:values] || {})[filter].nil?
+          require 'ruby-debug'; debugger
+          q.filter(filter.to_sym,
+          :operator => operator,
+          :values => session[:cost_query][:values][filter])
+        end
+      end
+    end.
+    column(:tweek).column(:tyear).
+    row(:project_id).row(:user_id)
+  end
+  
+  def index
+    @query = query
+    @walker = walker(@query)
+    render :layout => !request.xhr?
+  end
 
   private
   def find_optional_project
