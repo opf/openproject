d0d0ceb8d0f9b3a315b8af5c7ed4f9b63c77c981
p.tessenow <p.tessenow@7926756e-e54e-46e6-9721-ed318f58905e>
2010-06-30 09:32:52 +0000

    added full 'add-filter' support
    git-svn-id: https://dev.finn.de/svn/cockpit/branches/reporting_merge@1258 7926756e-e54e-46e6-9721-ed318f58905e

--- assets/javascripts/reporting.js
+++ assets/javascripts/reporting.js
@@ -20,3 +20,20 @@ function toggle_multi_select(field) {
 function operator_changed(field) {
   //TODO: this method should update the fields after the id="operator_field"-element depending on the selected operator
 }
+
+function add_filter() {
+    select = $('add_filter_select');
+    field = select.value
+    Element.show('tr_' +  field);
+    check_box = $('cb_' + field);
+    check_box.checked = true;
+    toggle_filter(field);
+    select.selectedIndex = 0;
+
+    for (i=0; i<select.options.length; i++) {
+        if (select.options[i].value == field) {
+            select.options[i].disabled = true;
+            break;
+        }
+    }
+}
