2f363264bf71ec50e977102cc9c4e7dad17ab29d
p.tessenow <p.tessenow@7926756e-e54e-46e6-9721-ed318f58905e>
2010-06-30 10:16:14 +0000

    return << me >> as an available value when we need users
    git-svn-id: https://dev.finn.de/svn/cockpit/branches/reporting_merge@1260 7926756e-e54e-46e6-9721-ed318f58905e

--- app/models/cost_query/filter/user_id.rb
+++ app/models/cost_query/filter/user_id.rb
@@ -2,6 +2,8 @@ class CostQuery::Filter::UserId < CostQuery::Filter::Base
   label :field_user
 
   def self.available_values
-    User.all.map { |u| [u.name, u.id] }
+    values = User.all.map { |u| [u.name, u.id] }
+    values.unshift ["<< #{l(:label_me)} >>", User.current.id.to_s] if User.current.logged?
+    values
   end
 end
