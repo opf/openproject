d4303ae2640f4a5d4738cda34e0c509249d6ac53
p.tessenow <p.tessenow@7926756e-e54e-46e6-9721-ed318f58905e>
2010-06-30 15:50:39 +0000

    give issue filters a longer description in the view
    git-svn-id: https://dev.finn.de/svn/cockpit/branches/reporting_merge@1275 7926756e-e54e-46e6-9721-ed318f58905e

--- app/models/cost_query/filter/issue_id.rb
+++ app/models/cost_query/filter/issue_id.rb
@@ -2,6 +2,6 @@ class CostQuery::Filter::IssueId < CostQuery::Filter::Base
   label :field_issue
 
   def self.available_values
-    Issue.all.map { |i| ["##{i.id} #{i.subject.length>24 ? i.subject.first(20)+'...': i.subject}", i.id] }
+    Issue.all.map { |i| ["##{i.id} #{i.subject.length>30 ? i.subject.first(26)+'...': i.subject}", i.id] }
   end
 end
