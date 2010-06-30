549ae1e9856e7cc24d1d3fad3c125bffc4b20158
p.tessenow <p.tessenow@7926756e-e54e-46e6-9721-ed318f58905e>
2010-06-30 16:29:51 +0000

    added method Filter.all_grouped which returns an array grouped by 'applied_for' used this method in the view
    git-svn-id: https://dev.finn.de/svn/cockpit/branches/reporting_merge@1279 7926756e-e54e-46e6-9721-ed318f58905e

--- app/models/cost_query/filter.rb
+++ app/models/cost_query/filter.rb
@@ -17,6 +17,10 @@ module CostQuery::Filter
       ]
   end
 
+  def self.all_grouped
+    CostQuery::Filter.all.group_by { |f| f.applies_for }.to_a.sort { |a,b| a.first.to_s <=> b.first.to_s }
+  end
+
   def self.from_hash
     raise NotImplementedError
   end
