81348ddb8867d2344b3f43dbe89b5616e66a18e8
p.tessenow <p.tessenow@7926756e-e54e-46e6-9721-ed318f58905e>
2010-07-08 16:47:50 +0000

    added all and all_grouped method to GroupBy
    git-svn-id: https://dev.finn.de/svn/cockpit/branches/reporting_merge@1363 7926756e-e54e-46e6-9721-ed318f58905e

--- app/models/cost_query/group_by.rb
+++ app/models/cost_query/group_by.rb
@@ -2,7 +2,23 @@ require "set"
 
 module CostQuery::GroupBy
   def self.all
-    @all ||= Set.new
+    @all ||= Set[
+      CostQuery::GroupBy::ActivityId,
+      CostQuery::GroupBy::CostObjectId,
+      CostQuery::GroupBy::CostTypeId,
+      CostQuery::GroupBy::IssueId,
+      CostQuery::GroupBy::ProjectId,
+      CostQuery::GroupBy::SpentOn,
+      CostQuery::GroupBy::Tmonth,
+      CostQuery::GroupBy::TrackerId,
+      CostQuery::GroupBy::Tweek,
+      CostQuery::GroupBy::Tyear,
+      CostQuery::GroupBy::UserId
+      ]
+  end
+
+  def self.all_grouped
+    all.group_by { |f| f.applies_for }.to_a.sort { |a,b| a.first.to_s <=> b.first.to_s }
   end
 
   def self.from_hash
