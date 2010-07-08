3a105ac4044e3c5110f08e1e017bff0fa80b8392
t.felgentreff <t.felgentreff@7926756e-e54e-46e6-9721-ed318f58905e>
2010-07-08 13:28:00 +0000

    add CI reporter to spec_helpers
    git-svn-id: https://dev.finn.de/svn/cockpit/trunk@1357 7926756e-e54e-46e6-9721-ed318f58905e

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,10 +1,23 @@
-RAILS_ENV ||= "test"
+ENV["RAILS_ENV"] ||= "test"
 
 # prevent case where we are using rubygems and test-unit 2.x is installed
 begin
   require 'rubygems'
   gem "test-unit", "~> 1.2.3"
 rescue LoadError
+end
+
+begin
+  require 'ci/reporter/rake/rspec'     # use this if you're using RSpec
+  require 'ci/reporter/rake/test_unit' # use this if you're using Test::Unit
+rescue LoadError
+  puts <<-EOS
+    Missing the CI Reporter gem. This is not fatal.
+    If you want XML output for the CI, execute
+
+        gem install ci_reporter
+
+  EOS
 end
 
 begin
