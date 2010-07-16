2c16c783fb850c4fb539a13a8cef635dfaa81d61
p.tessenow <p.tessenow@7926756e-e54e-46e6-9721-ed318f58905e>
2010-07-16 09:21:19 +0000

    rewritten the filter-select-part of the reporting view. filters operators and values) are set correctly now affter applying a query
    git-svn-id: https://dev.finn.de/svn/cockpit/branches/reporting_merge@1435 7926756e-e54e-46e6-9721-ed318f58905e

--- assets/javascripts/reporting.js
+++ assets/javascripts/reporting.js
@@ -8,8 +8,7 @@ function toggle_filter(field) {
       to_toggle.invoke('hide');
 }
 
-function toggle_multi_select(field) {
-    select = $('values_' + field);
+function toggle_multi_select(select) {
     if (select.multiple === true) {
         select.multiple = false;
     } else {
@@ -82,6 +81,59 @@ function show_group_by(group_by, target) {
     return;
   // move the option-tag to the taget select-box while keepings its data
   target.options[target.length] = group_option;
+}
+
+function select_operator(field, operator) {
+  select = $("operators_" + field);
+  for (var i=0; i<select.options.length; i++) {
+    if (select.options[i].value == operator) {
+      select.selectedIndex = i;
+      break;
+    }
+  }
+  operator_changed(field, select);
+}
+
+function restore_select_values(select, values) {
+  if (values.length > 1)
+      select.multiple = true;
+  for (var i = 0; i < values.length; i++)
+    for (var j = 0; j < select.options.length; j++)
+      if (select.options[j].value == values[i])
+        select.options[j].selected = true;
+}
+
+function find_arguments(field) {
+  var arguments = new Array();
+  var arg_count = 0;
+  var arg = null;
+  while (arg = $(field + '_arg_' + (arg_count + 1) + '_val')) {
+    arguments[arguments.length] = arg;
+    arg_count++;
+  }
+  return arguments;
+}
+
+function restore_values(field, values) {
+  var op_select = $("operators_" + field);
+  var op_arity = op_select.options[op_select.selectedIndex].getAttribute("data-arity");
+  var arguments = find_arguments(field);
+
+  if (!Object.isArray(values))
+    values = [values];
+  if (op_arity < 0)
+    restore_select_values(arguments[0], values);
+  else
+    for (var i = 0; i < values.length && i < arguments.length; i++)
+      arguments[i].setValue(values[i]);
+}
+
+function restore_filter(field, operator, values) {
+  select_operator(field, operator);
+  if(typeof(values) != "undefined")
+    restore_values(field, values);
+  disable_select_option($("add_filter_select"), field);
+  show_filter(field);
 }
 
 function show_group_by_column(group_by) {
