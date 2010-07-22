eca99914f28f64e476d2f76e048ac38dc0faaf1e
rkh <rkh@7926756e-e54e-46e6-9721-ed318f58905e>
2010-07-22 16:13:21 +0000

    reports without group-bys
    git-svn-id: https://dev.finn.de/svn/cockpit/branches/reporting_merge@1468 7926756e-e54e-46e6-9721-ed318f58905e

--- app/helpers/reporting_helper.rb
+++ app/helpers/reporting_helper.rb
@@ -1,4 +1,6 @@
 module ReportingHelper
+  include QueriesHelper
+
   ##
   # For a given CostQuery::Filter filter, return an array of hashes, that contain
   # the partials that should be rendered (:name) for that filter and necessary
@@ -41,35 +43,41 @@ module ReportingHelper
     klass.find(id).name
   end
 
+  def label_for(field)
+    l(CostQuery::Filter.const_get(field.to_s.camelcase).label)
+  end
+
   def debug_fields(result, prefix = ", ")
     prefix << result.fields.inspect << ", " << result.key.inspect if params[:debug]
+  end
+
+  def show_field(key, value)
+    @show_row ||= Hash.new { |h,k| h[k] = {}}
+    @show_row[key][value] ||= begin
+      return "" if value.blank?
+      case key.to_sym
+      when :activity_id               then mapped value, Enumeration, :caption_material_costs
+      when :project_id                then link_to_project Project.find(value.to_i)
+      when :user_id, :assigned_to_id  then link_to_user User.find(value.to_i)
+      when :tyear                     then value
+      when :tweek                     then "#{l(:label_week)} ##{value}"
+      when :tmonth                    then month_name(value.to_i)
+      when :category_id               then IssueCategory.find(value.to_i).name
+      when :cost_type_id              then mapped value, CostType, :caption_labor
+      when :cost_object               then CostObject.find(value.to_i).subject
+      when :issue_id                  then link_to_issue Issue.find(value.to_i)
+      when :spent_on                  then format_date(value.to_date)
+      when :tracker_id                then Tracker.find(value.to_i)
+      when :week                      then "#{l(:label_week)} #%s" % value.to_i.modulo(100)
+      else value.inspect
+      end
+    end
   end
 
   ##
   # For a given row, determine how to render it's contents according to usability and
   # localization rules
   def show_row(row)
-    @show_row ||= Hash.new { |h,k| h[k] = {}}
-    row.render do |key, value|
-      @show_row[key][value] ||= begin
-        return "" if value.blank?
-        case key.to_sym
-        when :activity_id               then mapped value, Enumeration, :caption_material_costs
-        when :project_id                then link_to_project Project.find(value.to_i)
-        when :user_id, :assigned_to_id  then link_to_user User.find(value.to_i)
-        when :tyear                     then value
-        when :tweek                     then "#{l(:label_week)} ##{value}"
-        when :tmonth                    then month_name(value.to_i)
-        when :category_id               then IssueCategory.find(value.to_i).name
-        when :cost_type_id              then mapped value, CostType, :caption_labor
-        when :cost_object               then CostObject.find(value.to_i).subject
-        when :issue_id                  then link_to_issue Issue.find(value.to_i)
-        when :spent_on                  then format_date(value.to_date)
-        when :tracker_id                then Tracker.find(value.to_i)
-        when :week                      then "#{l(:label_week)} #%s" % value.to_i.modulo(100)
-        else value.inspect
-        end
-      end
-    end
+    row.render { |k,v| show_field(k,v) }
   end
 end
