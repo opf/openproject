a628b0f186cf4d182ce5cee1a497ad42c5246406
Eric Davis <edavis@littlestreamsoftware.com>
2010-07-24 00:19:32 +0000

    Force-clear the default value for migration from :string to :text. #5846 #5771
    This is a special case for MySQL in Win32.
    
    Contributed by Holger Just.
    
    git-svn-id: svn+ssh://rubyforge.org/var/svn/redmine/trunk@3860 e93f8b46-1217-0410-a6f0-8f06a7374b81

--- app/models/change.rb
+++ app/models/change.rb
@@ -19,12 +19,13 @@ class Change < ActiveRecord::Base
   belongs_to :changeset
   
   validates_presence_of :changeset_id, :action, :path
+  before_save :init_path
   
   def relative_path
     changeset.repository.relative_path(path)
   end
   
-  def before_save
-    path ||= ""
+  def init_path
+    self.path ||= ""
   end
 end
--- db/migrate/20100705164950_change_changes_path_length_limit.rb
+++ db/migrate/20100705164950_change_changes_path_length_limit.rb
@@ -1,6 +1,9 @@
 class ChangeChangesPathLengthLimit < ActiveRecord::Migration
   def self.up
+    # these are two steps to please MySQL 5 on Win32
+    change_column :changes, :path, :text, :default => nil, :null => true
     change_column :changes, :path, :text, :null => false
+    
     change_column :changes, :from_path, :text
   end
 
