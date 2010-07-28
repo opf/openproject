e3b17d91c6d051288bd25b152ff06ed05efe21e7
Mark Maglana <mmaglana@gmail.com>
2010-07-28 16:24:20 +0800

    Prevent server from displaying warnings

--- lib/backlogs_issue_patch.rb
+++ lib/backlogs_issue_patch.rb
@@ -34,12 +34,12 @@ module Backlogs
   
       def journalized_update_attributes!(attribs)
         self.init_journal(User.current)
-        return self.update_attributes! attribs
+        return self.update_attributes!(attribs)
       end
   
       def journalized_update_attributes(attribs)
         self.init_journal(User.current)
-        return self.update_attributes attribs
+        return self.update_attributes(attribs)
       end
   
       def journalized_update_attribute(attrib, v)
