6f3ae29522f49c6ac536a9118657d79bb2cef600
Eric Davis <edavis@littlestreamsoftware.com>
2010-08-04 00:38:22 +0000

    Use the MenuManager for the Administration menu. #6008
    Contributed by Jean-Baptiste Barth.
    
    git-svn-id: svn+ssh://rubyforge.org/var/svn/redmine/trunk@3909 e93f8b46-1217-0410-a6f0-8f06a7374b81

--- lib/redmine.rb
+++ lib/redmine.rb
@@ -157,7 +157,22 @@ Redmine::MenuManager.map :application_menu do |menu|
 end
 
 Redmine::MenuManager.map :admin_menu do |menu|
-  # Empty
+  menu.push :projects, {:controller => 'admin', :action => 'projects'}, :caption => :label_project_plural
+  menu.push :users, {:controller => 'users'}, :caption => :label_user_plural
+  menu.push :groups, {:controller => 'groups'}, :caption => :label_group_plural
+  menu.push :roles, {:controller => 'roles'}, :caption => :label_role_and_permissions
+  menu.push :trackers, {:controller => 'trackers'}, :caption => :label_tracker_plural
+  menu.push :issue_statuses, {:controller => 'issue_statuses'}, :caption => :label_issue_status_plural,
+            :html => {:class => 'issue_statuses'}
+  menu.push :workflows, {:controller => 'workflows', :action => 'edit'}, :caption => :label_workflow
+  menu.push :custom_fields, {:controller => 'custom_fields'},  :caption => :label_custom_field_plural,
+            :html => {:class => 'custom_fields'}
+  menu.push :enumerations, {:controller => 'enumerations'}
+  menu.push :settings, {:controller => 'settings'}
+  menu.push :ldap_authentication, {:controller => 'ldap_auth_sources', :action => 'index'},
+            :html => {:class => 'server_authentication'}
+  menu.push :plugins, {:controller => 'admin', :action => 'plugins'}, :last => true
+  menu.push :info, {:controller => 'admin', :action => 'info'}, :caption => :label_information_plural, :last => true
 end
 
 Redmine::MenuManager.map :project_menu do |menu|
