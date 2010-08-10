587903ba085c69ce505b54c08596893588f560a7
friflaj <friflaj@ajellito.com>
2010-08-10 20:35:18 +0200

    Move task order to nested_set
    The issues list will indent stories/tasks neatly if they use the
    nested_set ordering. It's functionally equivalent for us.

--- app/models/task.rb
+++ app/models/task.rb
@@ -6,4 +6,14 @@ class Task < Issue
         return nil if task_tracker.nil? or task_tracker == ''
         return Integer(task_tracker)
     end
+
+    # assumes the task is already under the same story as 'id'
+    def move_after(id)
+      if id.nil?
+        sib = self.siblings
+        move_to_left_of sib[0].id if sib.any?
+      else
+        move_to_right_of id
+      end
+    end
 end
