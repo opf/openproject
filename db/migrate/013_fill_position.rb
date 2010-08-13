5985c77f7d6ea9ef74fb2c21905602367ef7598d
friflaj <friflaj@ajellito.com>
2010-08-13 11:43:44 +0200

    Looks like PG doesn't return the same row structure as mysql, explicitly picking columns now

--- db/migrate/013_fill_position.rb
+++ db/migrate/013_fill_position.rb
@@ -2,8 +2,8 @@ class FillPosition < ActiveRecord::Migration
   def self.up
     pos = execute "select project_id, max(position) from issues where parent_id is null group by project_id"
     pos.each do |row|
-      project_id, position = row
-      position = position.to_i
+      project_id = row[0].to_i
+      position = row[1].to_i
 
       stories = execute "select id from issues where project_id = #{project_id} and parent_id is null and position is null order by created_on"
       stories.each do |id|
