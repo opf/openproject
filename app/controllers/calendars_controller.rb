bc57078b128cf994495dfa35a5df0cbcc875ff19
Eric Davis <edavis@littlestreamsoftware.com>
2010-08-19 04:34:02 +0000

    Merged r3944 from trunk.
    git-svn-id: svn+ssh://rubyforge.org/var/svn/redmine/branches/1.0-stable@3994 e93f8b46-1217-0410-a6f0-8f06a7374b81

--- app/controllers/calendars_controller.rb
+++ app/controllers/calendars_controller.rb
@@ -1,4 +1,5 @@
 class CalendarsController < ApplicationController
+  menu_item :calendar
   before_filter :find_optional_project
 
   rescue_from Query::StatementInvalid, :with => :query_statement_invalid
