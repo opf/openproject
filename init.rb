0e33abe1279147ba98104f068c9b644d9e99eaa3
Mark Maglana <mmaglana@gmail.com>
2010-08-28 23:58:04 +0800

    Return to unstable version

--- init.rb
+++ init.rb
@@ -22,7 +22,7 @@ Redmine::Plugin.register :redmine_backlogs do
   name 'Redmine Backlogs Plugin'
   author 'relaxdiego, friflaj'
   description 'A plugin for scrum/agile teams'
-  version 'v0.2.1'
+  version 'master branch (unstable)'
 
   settings :default => { :story_trackers => nil, :task_tracker => nil, :card_spec => nil }, :partial => 'settings/backlogs_settings'
 
