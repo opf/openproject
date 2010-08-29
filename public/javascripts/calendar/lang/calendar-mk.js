1ebb0d9caec7cdc8d07c53d4d2fff416e41408aa
Azamat Hackimov <azamat.hackimov@gmail.com>
2010-08-29 12:54:04 +0000

    Translation updates * mk (#6208) * sv (#6210)
    git-svn-id: svn+ssh://rubyforge.org/var/svn/redmine/trunk@4049 e93f8b46-1217-0410-a6f0-8f06a7374b81

--- public/javascripts/calendar/lang/calendar-mk.js
+++ public/javascripts/calendar/lang/calendar-mk.js
@@ -1,7 +1,7 @@
 // ** I18N
 
 // Calendar МК language
-// Author: Илин Татабитовски, <ilin@slobodensoftver.org.mk>
+// Author: Ilin Tatabitovski, <itatabitovski@gmail.com>
 // Encoding: UTF-8
 // Distributed under the same terms as the calendar itself.
 
@@ -84,26 +84,26 @@ Calendar._TT["INFO"] = "За календарот";
 Calendar._TT["ABOUT"] =
 "DHTML Date/Time Selector\n" +
 "(c) dynarch.com 2002-2005 / Author: Mihai Bazon\n" + // don't translate this this ;-)
-"For latest version visit: http://www.dynarch.com/projects/calendar/\n" +
-"Distributed under GNU LGPL.  See http://gnu.org/licenses/lgpl.html for details." +
+"За последна верзија посети: http://www.dynarch.com/projects/calendar/\n" +
+"Дистрибуирано под GNU LGPL.  Види http://gnu.org/licenses/lgpl.html за детали." +
 "\n\n" +
-"Date selection:\n" +
-"- Use the \xab, \xbb buttons to select year\n" +
-"- Use the " + String.fromCharCode(0x2039) + ", " + String.fromCharCode(0x203a) + " buttons to select month\n" +
-"- Hold mouse button on any of the above buttons for faster selection.";
+"Бирање на дата:\n" +
+"- Користи ги \xab, \xbb копчињата за да избереш година\n" +
+"- Користи ги " + String.fromCharCode(0x2039) + ", " + String.fromCharCode(0x203a) + " копчињата за да избере месеци\n" +
+"- Држи го притиснато копчето на глувчето на било кое копче за побрзо бирање.";
 Calendar._TT["ABOUT_TIME"] = "\n\n" +
-"Time selection:\n" +
-"- Click on any of the time parts to increase it\n" +
-"- or Shift-click to decrease it\n" +
-"- or click and drag for faster selection.";
+"Бирање на време:\n" +
+"- Клик на временските делови за да го зголемиш\n" +
+"- или Shift-клик да го намалиш\n" +
+"- или клик и влечи за побрзо бирање.";
 
-Calendar._TT["PREV_YEAR"] = "Претходна година (hold for menu)";
-Calendar._TT["PREV_MONTH"] = "Претходен месец (hold for menu)";
+Calendar._TT["PREV_YEAR"] = "Претходна година (држи за мени)";
+Calendar._TT["PREV_MONTH"] = "Претходен месец (држи за мени)";
 Calendar._TT["GO_TODAY"] = "Go Today";
-Calendar._TT["NEXT_MONTH"] = "Следен месец (hold for menu)";
-Calendar._TT["NEXT_YEAR"] = "Следна година (hold for menu)";
-Calendar._TT["SEL_DATE"] = "Изберете дата";
-Calendar._TT["DRAG_TO_MOVE"] = "Drag to move";
+Calendar._TT["NEXT_MONTH"] = "Следен месец (држи за мени)";
+Calendar._TT["NEXT_YEAR"] = "Следна година (држи за мени)";
+Calendar._TT["SEL_DATE"] = "Избери дата";
+Calendar._TT["DRAG_TO_MOVE"] = "Влечи да поместиш";
 Calendar._TT["PART_TODAY"] = " (денес)";
 
 // the following is to inform that "%s" is to be the first day of week
@@ -117,7 +117,7 @@ Calendar._TT["WEEKEND"] = "0,6";
 
 Calendar._TT["CLOSE"] = "Затвори";
 Calendar._TT["TODAY"] = "Денес";
-Calendar._TT["TIME_PART"] = "(Shift-)Click or drag to change value";
+Calendar._TT["TIME_PART"] = "(Shift-)Клик или влечи за да промениш вредност";
 
 // date formats
 Calendar._TT["DEF_DATE_FORMAT"] = "%d-%m-%Y";
@@ -125,3 +125,4 @@ Calendar._TT["TT_DATE_FORMAT"] = "%a, %e %b";
 
 Calendar._TT["WK"] = "нед";
 Calendar._TT["TIME"] = "Време:";
+
