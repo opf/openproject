16103b8fc3385859bb493e01cc2307c8070fe187
friflaj <friflaj@ajellito.com>
2010-09-01 14:21:14 +0200

    Treat task as story so we can remove it from the stories list

--- db/migrate/016_remove_task_position.rb
+++ db/migrate/016_remove_task_position.rb
@@ -1,7 +1,10 @@
 class RemoveTaskPosition < ActiveRecord::Migration
   def self.up
     ActiveRecord::Base.transaction do
-      Task.find(:all, :conditions => "id <> root_id and not position is null").each do |t|
+      # this intentionally loads tasks as stories so we can issue
+      # remove_from_list, which does more than just nilling the
+      # position
+      Story.find(:all, :conditions => "id <> root_id and not position is null").each do |t|
         t.remove_from_list
       end
     end
