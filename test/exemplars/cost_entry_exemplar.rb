37f8d55f74555ac83e67a2e49b01fe5348c79868
p.tessenow <p.tessenow@7926756e-e54e-46e6-9721-ed318f58905e>
2010-09-02 13:38:00 +0000

    cost entry are now generated as if they were created 'yesterday'
    git-svn-id: https://dev.finn.de/svn/cockpit/branches/reporting_merge@1626 7926756e-e54e-46e6-9721-ed318f58905e

--- test/exemplars/cost_entry_exemplar.rb
+++ test/exemplars/cost_entry_exemplar.rb
@@ -4,16 +4,16 @@ CostEntry.class_eval do
   generator_for :cost_type, :method => :next_cost_type
   generator_for :rate, :method => :next_cost_rate
   generator_for :units, rand(1000)
-  generator_for :spent_on, 1.year.ago
+  generator_for :spent_on, 1.day.ago
   generator_for :comments, "Some comment"
   generator_for :issue, :method => :next_issue
-  generator_for :created_on, 1.year.ago
+  generator_for :created_on, 1.day.ago
   generator_for :updated_on, Date.today
   generator_for :blocked, false
   generator_for :costs, 20
-  generator_for :tyear, 2010
-  generator_for :tmonth, 3
-  generator_for :tweek, 10
+  generator_for :tyear, 1.day.ago.year
+  generator_for :tmonth, 1.day.ago.month
+  generator_for :tweek, 1.day.ago.to_date.cweek
 
   def self.next_project
     Project.last || Project.generate!
