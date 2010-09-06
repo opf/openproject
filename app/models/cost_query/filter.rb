77a43874acce87de025ff9d3a55aec51e97fe514
p.tessenow <p.tessenow@7926756e-e54e-46e6-9721-ed318f58905e>
2010-09-06 06:17:35 +0000

    add permission filter
    git-svn-id: https://dev.finn.de/svn/cockpit/branches/reporting_merge@1646 7926756e-e54e-46e6-9721-ed318f58905e

--- app/models/cost_query/filter.rb
+++ app/models/cost_query/filter.rb
@@ -24,6 +24,7 @@ module CostQuery::Filter
       CostQuery::Filter::UpdatedOn,
       CostQuery::Filter::UserId,
       #CostQuery::Filter::Week,
+      CostQuery::Filter::PermissionFilter
     ]
   end
 
