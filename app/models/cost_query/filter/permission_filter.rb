cb724d02ea31feb92335609bf82f1e6b46c82d6c
p.tessenow <p.tessenow@7926756e-e54e-46e6-9721-ed318f58905e>
2010-09-06 13:36:02 +0000

    added permission filter class.
    git-svn-id: https://dev.finn.de/svn/cockpit/branches/reporting_merge@1648 7926756e-e54e-46e6-9721-ed318f58905e

---
+++ app/models/cost_query/filter/permission_filter.rb
@@ -0,0 +1,20 @@
+class CostQuery::Filter::PermissionFilter < CostQuery::Filter::Base
+  dont_display!
+  not_selectable!
+  db_field ""
+
+  initialize_query_with { |query| query.filter self.to_s.demodulize.to_sym }
+
+  def permission_statement(permission)
+    User.current.allowed_for(permission).gsub(/(user|project)s?\.id/, 'entries.\1_id')
+  end
+
+  def sql_statement
+    super.tap do |query|
+      query.where "(entries.type != \"TimeEntry\" OR #{permission_statement :view_own_time_entries} OR #{permission_statement :view_time_entries})"
+      query.where "(entries.type != \"CostEntry\" OR #{permission_statement :view_own_cost_entries} OR #{permission_statement :view_cost_entries})"
+      query.select "*"
+      query.select :display_costs => "(#{permission_statement :view_own_cost_entries} OR #{permission_statement :view_cost_entries})"
+    end
+  end
+end
