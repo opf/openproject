3ad742ce267a007d499196f2b1342400f95c622d
friflaj <friflaj@ajellito.com>
2010-09-09 22:06:18 +0200

    updated_on is nil if there's no story

--- app/controllers/rb_master_backlogs_controller.rb
+++ app/controllers/rb_master_backlogs_controller.rb
@@ -7,11 +7,12 @@ class RbMasterBacklogsController < RbApplicationController
     product_backlog_stories = Story.product_backlog(@project)
     sprints = Sprint.open_sprints(@project)
     
-    @last_update = Story.find(
+    last_story = Story.find(
                           :first, 
                           :conditions => ["project_id=? AND tracker_id in (?)", @project, Story.trackers],
                           :order => "updated_on DESC"
-                          ).updated_on
+                          )
+    @last_update = (last_story ? last_story.updated_on : nil)
     @product_backlog = { :sprint => nil, :stories => product_backlog_stories }
     @sprint_backlogs = sprints.map{ |s| { :sprint => s, :stories => s.stories } }
 
