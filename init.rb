cbdf4032b85660876a4907fcd32af787fac7be82
p.tessenow <p.tessenow@7926756e-e54e-46e6-9721-ed318f58905e>
2010-09-09 11:19:17 +0000

    fixed permission problems
    git-svn-id: https://dev.finn.de/svn/cockpit/branches/reporting_merge@1718 7926756e-e54e-46e6-9721-ed318f58905e

--- init.rb
+++ init.rb
@@ -35,8 +35,8 @@ Redmine::Plugin.register :redmine_reporting do
   menu :top_menu, :cost_reports_global, {:controller => 'cost_reports', :action => 'index'},
     :caption => :cost_reports_title,
     :if => Proc.new {
-      ( User.current.allowed_to?(:view_own_cost_entries, nil, :global => true) ||
-        User.current.allowed_to?(:view_own_time_entries, nil, :global => true)
+      ( User.current.allowed_to?(:view_cost_entries, nil, :global => true, :for => User.current ) ||
+        User.current.allowed_to?(:view_time_entries, nil, :global => true, :for => User.current )
       )
     }
 
