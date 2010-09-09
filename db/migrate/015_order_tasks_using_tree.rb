00ff75ec5913ffd3eec69a6251d9232911cfb483
friflaj <friflaj@ajellito.com>
2010-09-09 22:01:33 +0200

    Task reordering must be done ordered by parent,position

--- db/migrate/015_order_tasks_using_tree.rb
+++ db/migrate/015_order_tasks_using_tree.rb
@@ -2,7 +2,7 @@ class OrderTasksUsingTree < ActiveRecord::Migration
   def self.up
     last_task = {}
     ActiveRecord::Base.transaction do
-      Task.find(:all, :conditions => "id <> root_id", :order => "project_id ASC, fixed_version_id ASC, position ASC").each do |t|
+      Task.find(:all, :conditions => "not parent_id is NULL", :order => "project_id ASC, parent_id ASC, position ASC").each do |t|
         t.move_after last_task[t.parent_id] if last_task[t.parent_id]
 
         last_task[t.parent_id] = t.id
