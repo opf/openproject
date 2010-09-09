557072f2cc6da456c962518d2da77dbd7c6d8f4e
p.tessenow <p.tessenow@7926756e-e54e-46e6-9721-ed318f58905e>
2010-09-09 12:21:01 +0000

    the user filter now only shows user who belong to a project the current user has permissions to see
    git-svn-id: https://dev.finn.de/svn/cockpit/branches/reporting_merge@1728 7926756e-e54e-46e6-9721-ed318f58905e

--- app/models/cost_query/filter/user_id.rb
+++ app/models/cost_query/filter/user_id.rb
@@ -2,7 +2,8 @@ class CostQuery::Filter::UserId < CostQuery::Filter::Base
   label :field_user
 
   def self.available_values(user)
-    values = User.all.map { |u| [u.name, u.id] }
+    users = user.projects.collect {|p| p.users}.flatten.uniq.sort
+    values = users.map { |u| [u.name, u.id] }
     values.delete_if { |u| (u.first.include? "Redmine Admin") || (u.first.include? "Anonymous")}
     values.sort!
     values.unshift ["<< #{l(:label_me)} >>", User.current.id.to_s] if User.current.logged?
