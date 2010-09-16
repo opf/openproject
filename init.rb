e7b88c43de5a64d34522087e0f282dc9a6e97702
j.wollert <j.wollert@7926756e-e54e-46e6-9721-ed318f58905e>
2010-09-16 07:59:31 +0000

    remove hwia for now
    git-svn-id: https://dev.finn.de/svn/cockpit/branches/reporting_merge@1762 7926756e-e54e-46e6-9721-ed318f58905e

--- init.rb
+++ init.rb
@@ -1,7 +1,7 @@
 require 'redmine'
 
 fail "upgrade ruby version, ruby < 1.8.7 suffers from Hash#hash bug" if {:a => 10}.hash != {:a => 10}.hash
-require "hwia_rails"
+#require "hwia_rails"
 
 require 'big_decimal_patch'
 require 'to_date_patch'
