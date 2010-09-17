eb1f58f9624a22d4b5a22f1510c858fa6d5cf7cb
Eric Davis <edavis@littlestreamsoftware.com>
2010-09-17 04:31:17 +0000

    Added some documentation about the Rails Logger. #6135
    Contributed by Felix Schäfer
    
    git-svn-id: svn+ssh://rubyforge.org/var/svn/redmine/trunk@4096 e93f8b46-1217-0410-a6f0-8f06a7374b81

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -4,9 +4,14 @@
 # Code is not reloaded between requests
 config.cache_classes = true
 
+#####
+# Customize the default logger (http://ruby-doc.org/core/classes/Logger.html)
+#
 # Use a different logger for distributed setups
 # config.logger        = SyslogLogger.new
-
+#
+# Rotate logs bigger than 1MB, keeps no more than 7 rotated logs around.
+# config.logger = Logger.new(config.log_path, 7, 1048576)
 
 # Full error reports are disabled and caching is turned on
 config.action_controller.consider_all_requests_local = false
