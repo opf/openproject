853bcf8726c0f14207967512e1255911eba3e295
t.felgentreff <t.felgentreff@7926756e-e54e-46e6-9721-ed318f58905e>
2010-09-22 13:36:34 +0000

    for multiply applying cost_type_id filters, allow setting it to dont_display on a per-instance basis
    git-svn-id: https://dev.finn.de/svn/cockpit/branches/reporting_merge@1776 7926756e-e54e-46e6-9721-ed318f58905e

--- app/models/cost_query/filter/cost_type_id.rb
+++ app/models/cost_query/filter/cost_type_id.rb
@@ -1,6 +1,15 @@
 class CostQuery::Filter::CostTypeId < CostQuery::Filter::Base
-  dont_display!
   label :field_cost_type
+
+  def initialize(child = nil, options = {})
+    @display = options.delete(:display)
+    super
+  end
+
+  def display?
+    return super if @display.nil?
+    @display
+  end
 
   def self.available_values(user)
     ([[l(:caption_labor), -1]] + CostType.find(:all, :order => 'name').map { |t| [t.name, t.id] })
