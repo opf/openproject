a1028f00bcd2ed46cd432bb31241d3a77c44ec00
Mark Maglana <mmaglana@gmail.com>
2010-09-24 15:53:59 +0800

    No choice but to use js workaround

--- assets/javascripts/master_backlog.js
+++ assets/javascripts/master_backlog.js
@@ -6,4 +6,11 @@ $(function() {
   });
   // $("#project_info").bind('click', function(){ $("#velocity").dialog({ modal: true, title: "Project Info"}); });
   RB.BacklogsUpdater.start();
+
+  // Workaround for IE7
+  var z = 2000;
+  $('.backlog, .header').each(function(){
+    $(this).css('z-index', z);
+    z--;
+  });
 });
