a2893fe1990a6a33758986ed6a3181d13101b6b2
p.tessenow <p.tessenow@7926756e-e54e-46e6-9721-ed318f58905e>
2010-09-30 06:51:56 +0000

    fixed reporting.js indendation
    git-svn-id: https://dev.finn.de/svn/cockpit/branches/reporting_merge@1825 7926756e-e54e-46e6-9721-ed318f58905e

--- assets/javascripts/reporting.js
+++ assets/javascripts/reporting.js
@@ -471,27 +471,27 @@ function init_group_bys() {
 }
 
 function load_available_values_for_filter(filter_name, callback_func) {
-  var select;
-  select = $('' + filter_name + '_arg_1_val');
-  if (select.readAttribute('data-loading') == "ajax" && select.childElements().length == 0) {
-    new Ajax.Updater({ success: select }, '/cost_reports/available_values', {
-      parameters: { filter_name: filter_name },
-      insertion: 'bottom',
-      evalScripts: false,
-      onCreate: function (a,b) {
-        $('operators_' + filter_name).disable();
-        $('' + filter_name + '_arg_1_val').disable();
-      },
-      onComplete: function (a,b) {
-        $('operators_' + filter_name).enable();
-        $('' + filter_name + '_arg_1_val').enable();
+    var select;
+    select = $('' + filter_name + '_arg_1_val');
+    if (select.readAttribute('data-loading') == "ajax" && select.childElements().length == 0) {
+        new Ajax.Updater({ success: select }, '/cost_reports/available_values', {
+            parameters: { filter_name: filter_name },
+            insertion: 'bottom',
+            evalScripts: false,
+            onCreate: function (a,b) {
+                $('operators_' + filter_name).disable();
+                $('' + filter_name + '_arg_1_val').disable();
+            },
+            onComplete: function (a,b) {
+                $('operators_' + filter_name).enable();
+                $('' + filter_name + '_arg_1_val').enable();
+                callback_func();
+            }
+        });
+    }
+    else {
         callback_func();
-      }
-    });
-  }
-  else {
-    callback_func();
-  }
+    }
 }
 
 function defineElementGetter() {
