d1f0ff79ad8b1516385d32d41d220135f829bb4f
p.tessenow <p.tessenow@7926756e-e54e-46e6-9721-ed318f58905e>
2010-10-04 09:34:09 +0000

    renamed validation 'numbers' to 'integers' and added validation code
    git-svn-id: https://dev.finn.de/svn/cockpit/branches/reporting_merge@1844 7926756e-e54e-46e6-9721-ed318f58905e

--- app/models/cost_query/validation/numbers.rb
+++
@@ -1,7 +0,0 @@
-module CostQuery::Validation
-  module Numbers
-    def validate_numbers(values = [])
-      raise NotImplementedError, "Haven't done number validation just yet!"
-    end
-  end
-end
