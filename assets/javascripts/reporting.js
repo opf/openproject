36c6484049f7b49159e700aed90d2206d6c358e8
p.tessenow <p.tessenow@7926756e-e54e-46e6-9721-ed318f58905e>
2010-10-15 08:23:56 +0000

    did some jslint on reporting.js
    git-svn-id: https://dev.finn.de/svn/cockpit/branches/deployment_merge@1992 7926756e-e54e-46e6-9721-ed318f58905e

--- assets/javascripts/reporting.js
+++ assets/javascripts/reporting.js
@@ -1,7 +1,7 @@
 /*global $, selectAllOptions, moveOptions */
 
 function toggle_filter(field) {
-    var remove, to_toggle;
+    var to_toggle, label;
     label = $('label_' + field);
     to_toggle = label.up().siblings();
     if (label.visible()) {
@@ -33,9 +33,9 @@ function change_argument_visibility(field, arg_nr) {
     var params, i;
     params = [$(field + '_arg_1'), $(field + '_arg_2')];
 
-    for (i = 0; i < 2; i++) {
+    for (i = 0; i < 2; i += 1) {
         if (params[i] !== null) {
-            if (arg_nr >= (i + 1) || arg_nr <= (-1 - i) ) {
+            if (arg_nr >= (i + 1) || arg_nr <= (-1 - i)) {
                 params[i].show();
             }
             else {
@@ -48,7 +48,7 @@ function change_argument_visibility(field, arg_nr) {
 function operator_changed(field, select) {
     var option_tag, arity;
     option_tag = select.options[select.selectedIndex];
-    arity = parseInt(option_tag.getAttribute("data-arity"));
+    arity = parseInt(option_tag.getAttribute("data-arity"), 10);
     change_argument_visibility(field, arity);
 }
 
@@ -67,9 +67,9 @@ function hide_category(tr_field) {
 }
 
 function set_remove_button_visibility(field, value) {
-    remove = $('rm_' + field);
+    var remove = $('rm_' + field);
     if (remove !== null) {
-        if (value == true) {
+        if (value === true) {
             remove.show();
         } else {
             remove.hide();
@@ -77,8 +77,29 @@ function set_remove_button_visibility(field, value) {
     }
 }
 
-function show_filter(field) {
-    show_filter_callback(field, function(){;});
+function load_available_values_for_filter(filter_name, callback_func) {
+    var select;
+    select = $('' + filter_name + '_arg_1_val');
+    if (select.readAttribute('data-loading') === "ajax" && select.childElements().length === 0) {
+        new Ajax.Updater({ success: select }, '/cost_reports/available_values', {
+            parameters: { filter_name: filter_name },
+            insertion: 'bottom',
+            evalScripts: false,
+            onCreate: function (a, b) {
+                $('operators_' + filter_name).disable();
+                $('' + filter_name + '_arg_1_val').disable();
+            },
+            onComplete: function (a, b) {
+                $('operators_' + filter_name).enable();
+                $('' + filter_name + '_arg_1_val').enable();
+                callback_func();
+            }
+        });
+        make_select_accept_single_value(select);
+    }
+    else {
+        callback_func();
+    }
 }
 
 function show_filter_callback(field, callback_func) {
@@ -94,6 +115,22 @@ function show_filter_callback(field, callback_func) {
     }
 }
 
+function show_filter(field) {
+    show_filter_callback(field, function () {});
+}
+
+function occupied_category(tr_field) {
+    var i, data_label, filters;
+    data_label = tr_field.getAttribute("data-label");
+    filters = document.getElementsByClassName('filter');
+    for (i = 0; i < filters.length; i += 1) {
+        if (filters[i].visible() && filters[i].getAttribute("data-label") === data_label) {
+            return true;
+        }
+    }
+    return false; //not hit
+}
+
 function hide_filter(field) {
     var field_el = $('tr_' +  field);
     if (field_el !== null) {
@@ -107,20 +144,9 @@ function hide_filter(field) {
     }
 }
 
-function occupied_category(tr_field) {
-    var i, hit = false, data_label = tr_field.getAttribute("data-label");
-    filters = document.getElementsByClassName('filter');
-    for (i = 0; i < filters.length; i++) {
-        if (filters[i].visible() && filters[i].getAttribute("data-label") == data_label) {
-            return hit = true;
-        }
-    }
-    return hit;
-}
-
 function disable_select_option(select, field) {
-    for (var i = 0; i < select.options.length; i++) {
-        if (select.options[i].value == field) {
+    for (var i = 0; i < select.options.length; i += 1) {
+        if (select.options[i].value === field) {
             select.options[i].disabled = true;
             break;
         }
@@ -128,8 +154,8 @@ function disable_select_option(select, field) {
 }
 
 function enable_select_option(select, field) {
-    for (var i = 0; i < select.options.length; i++) {
-        if (select.options[i].value == field) {
+    for (var i = 0; i < select.options.length; i += 1) {
+        if (select.options[i].value === field) {
             select.options[i].disabled = false;
             break;
         }
@@ -154,8 +180,8 @@ function show_group_by(group_by, target) {
     source = $("group_by_container");
     group_option = null;
     // find group_by option-tag in target select-box
-    for (i = 0; i < source.options.length; i++) {
-        if (source.options[i].value == group_by) {
+    for (i = 0; i < source.options.length; i += 1) {
+        if (source.options[i].value === group_by) {
             group_option = source.options[i];
             source.options[i] = null;
             break;
@@ -172,8 +198,8 @@ function show_group_by(group_by, target) {
 function select_operator(field, operator) {
     var select, i;
     select = $("operators_" + field);
-    for (i = 0; i < select.options.length; i++) {
-        if (select.options[i].value == operator) {
+    for (i = 0; i < select.options.length; i += 1) {
+        if (select.options[i].value === operator) {
             select.selectedIndex = i;
             break;
         }
@@ -188,9 +214,9 @@ function restore_select_values(select, values) {
     } else {
         make_select_accept_single_value(select);
     }
-    for (i = 0; i < values.length; i++) {
-        for (j = 0; j < select.options.length; j++) {
-            if (select.options[j].value == values[i]) {
+    for (i = 0; i < values.length; i += 1) {
+        for (j = 0; j < select.options.length; j += 1) {
+            if (select.options[j].value === values[i]) {
                 select.options[j].selected = true;
             }
         }
@@ -202,7 +228,7 @@ function find_arguments(field) {
     arg = $(field + '_arg_' + (arg_count + 1) + '_val');
     while (arg !== null) {
         args[args.length] = arg;
-        arg_count++;
+        arg_count = arg_count + 1;
         arg = $(field + '_arg_' + (arg_count + 1) + '_val');
     }
     return args;
@@ -219,7 +245,7 @@ function restore_values(field, values) {
     if (op_arity < 0 && !(args[0].type.empty()) && args[0].type.include('select')) {
         restore_select_values(args[0], values);
     } else {
-        for (i = 0; i < values.length && i < args.length; i++) {
+        for (i = 0; i < values.length && i < args.length; i += 1) {
             args[i].setValue(values[i]);
         }
     }
@@ -228,8 +254,8 @@ function restore_values(field, values) {
 function restore_filter(field, operator, values) {
     select_operator(field, operator);
     disable_select_option($("add_filter_select"), field);
-    show_filter_callback(field, function() {
-        if (typeof(values) != "undefined") {
+    show_filter_callback(field, function () {
+        if (typeof(values) !== "undefined") {
             restore_values(field, values);
         }
     });
@@ -247,7 +273,7 @@ function disable_all_filters() {
     $('filter_table').down().childElements().each(function (e) {
         var field, possible_select;
         e.hide();
-        if (e.readAttribute('class') == 'filter') {
+        if (e.readAttribute('class') === 'filter') {
             field = e.id.gsub('tr_', '');
             hide_filter(field);
             enable_select_option($('add_filter_select'), field);
@@ -269,13 +295,14 @@ function disable_all_group_bys() {
 }
 
 function serialize_filter_and_group_by() {
-    var ret_str = Form.serialize('query_form');
-    var rows = Sortable.serialize('group_rows');
-    var columns = Sortable.serialize('group_columns');
-    if (rows !== null && rows != "") {
+    var ret_str, rows, columns;
+    ret_str = Form.serialize('query_form');
+    rows = Sortable.serialize('group_rows');
+    columns = Sortable.serialize('group_columns');
+    if (rows !== null && rows !== "") {
         ret_str += "&" + rows;
     }
-    if(columns !== null && columns != "") {
+    if (columns !== null && columns !== "") {
         ret_str += "&" + columns;
     }
     return ret_str;
@@ -283,10 +310,10 @@ function serialize_filter_and_group_by() {
 
 function init_group_bys() {
     var options = {
-        tag:'span',
-        overlap:'horizontal',
-        constraint:'horizontal',
-        containment: ['group_columns','group_rows'],
+        tag: 'span',
+        overlap: 'horizontal',
+        constraint: 'horizontal',
+        containment: ['group_columns', 'group_rows'],
         //only: "group_by",
         dropOnEmpty: true,
         format: /^(.*)$/,
@@ -299,16 +326,16 @@ function init_group_bys() {
 function load_available_values_for_filter(filter_name, callback_func) {
     var select;
     select = $('' + filter_name + '_arg_1_val');
-    if (select.readAttribute('data-loading') == "ajax" && select.childElements().length == 0) {
+    if (select.readAttribute('data-loading') === "ajax" && select.childElements().length === 0) {
         new Ajax.Updater({ success: select }, '/cost_reports/available_values', {
             parameters: { filter_name: filter_name },
             insertion: 'bottom',
             evalScripts: false,
-            onCreate: function (a,b) {
+            onCreate: function (a, b) {
                 $('operators_' + filter_name).disable();
                 $('' + filter_name + '_arg_1_val').disable();
             },
-            onComplete: function (a,b) {
+            onComplete: function (a, b) {
                 $('operators_' + filter_name).enable();
                 $('' + filter_name + '_arg_1_val').enable();
                 callback_func();
@@ -322,22 +349,21 @@ function load_available_values_for_filter(filter_name, callback_func) {
 }
 
 function defineElementGetter() {
-    if (document.getElementsByClassName == undefined) {
-        document.getElementsByClassName = function(className)
+    if (document.getElementsByClassName === undefined) {
+        document.getElementsByClassName = function (className)
         {
-            var hasClassName = new RegExp("(?:^|\\s)" + className + "(?:$|\\s)");
-            var allElements = document.getElementsByTagName("*");
-            var results = [];
-
-            var element;
-            for (var i = 0; (element = allElements[i]) != null; i++) {
-                var elementClass = element.className;
-                if (elementClass && elementClass.indexOf(className) != -1 && hasClassName.test(elementClass))
-                results.push(element);
+            var hasClassName, allElements, results, element, elementClass, i;
+            hasClassName = new RegExp("(?:^|\\s)" + className + "(?:$|\\s)");
+            allElements = document.getElementsByTagName("*");
+            results = [];
+            for (i = 0; (element = allElements[i]) !== null; i += 1) {
+                elementClass = element.className;
+                if (elementClass && elementClass.indexOf(className) !== -1 && hasClassName.test(elementClass)) {
+                    results.push(element);
+                }
             }
-
             return results;
-        }
+        };
     }
 }
 
