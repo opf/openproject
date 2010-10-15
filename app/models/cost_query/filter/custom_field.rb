22a3ea07d94ddb6073a328186a642d126642f809
rkh <rkh@7926756e-e54e-46e6-9721-ed318f58905e>
2010-10-15 07:08:10 +0000

    Display custom fields as issue properties in filter UI.
    git-svn-id: https://dev.finn.de/svn/cockpit/branches/deployment_merge@1989 7926756e-e54e-46e6-9721-ed318f58905e

--- app/models/cost_query/filter/custom_field.rb
+++ app/models/cost_query/filter/custom_field.rb
@@ -3,6 +3,7 @@ module CostQuery::Filter
     extend CostQuery::CustomFieldMixin
 
     on_prepare do
+      applies_for :label_issue_attributes
       # redmine internals just suck
       case custom_field.field_format
       when 'string', 'text' then use :string_operators
