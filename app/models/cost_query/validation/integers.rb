750204e7085086cfd6ca22182ea6af24aa4c826d
j.wollert <j.wollert@7926756e-e54e-46e6-9721-ed318f58905e>
2010-10-26 13:58:06 +0000

    typo in int validation error message
    git-svn-id: https://dev.finn.de/svn/cockpit/branches/deployment_merge@2087 7926756e-e54e-46e6-9721-ed318f58905e

--- app/models/cost_query/validation/integers.rb
+++ app/models/cost_query/validation/integers.rb
@@ -5,7 +5,7 @@ module CostQuery::Validation
       values.all? do |val|
         if val.to_i.to_s != val.to_s
           values_passed = false
-          errors << "\'#{val}\' is not a valid date!"
+          errors << "\'#{val}\' is not a valid number!"
         end
       end
       values_passed
