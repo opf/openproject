af26a053b3e6ba0362b40499ac3687cbf207943d
rkh <rkh@7926756e-e54e-46e6-9721-ed318f58905e>
2010-10-29 09:29:31 +0000

    display custom field group bys as issue attributes
    git-svn-id: https://dev.finn.de/svn/cockpit/branches/deployment_merge@2124 7926756e-e54e-46e6-9721-ed318f58905e

--- app/models/cost_query/group_by/custom_field.rb
+++ app/models/cost_query/group_by/custom_field.rb
@@ -1,5 +1,6 @@
 module CostQuery::GroupBy
   class CustomField < Base
+    applies_for :label_issue_attributes
     extend CostQuery::CustomFieldMixin
     on_prepare { group_fields table_name }
   end
