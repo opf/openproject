c8f28e2f4475fa90c2dfd2dd6045dce6667679d3
rkh <rkh@7926756e-e54e-46e6-9721-ed318f58905e>
2010-10-29 10:02:03 +0000

    Add more issue fields to group bys. Fixes #3666.
    git-svn-id: https://dev.finn.de/svn/cockpit/branches/deployment_merge@2125 7926756e-e54e-46e6-9721-ed318f58905e

---
+++ app/models/cost_query/group_by/author_id.rb
@@ -0,0 +1,7 @@
+module CostQuery::GroupBy
+  class AuthorId < Base
+    join_table Issue
+    applies_for :label_issue_attributes
+    label :field_author
+  end
+end
---
+++ app/models/cost_query/group_by/category_id.rb
@@ -0,0 +1,7 @@
+module CostQuery::GroupBy
+  class CategoryId < Base
+    join_table Issue
+    applies_for :label_issue_attributes
+    label :field_category
+  end
+end
