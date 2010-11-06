c50b611f851303f97c83c8eb080f3c051c957395
Jean-Philippe Lang <jp_lang@yahoo.fr>
2010-11-06 11:44:38 +0000

    Adds a unique index on members (#6826).
    git-svn-id: svn+ssh://rubyforge.org/var/svn/redmine/trunk@4369 e93f8b46-1217-0410-a6f0-8f06a7374b81

---
+++ db/migrate/20101104182107_add_unique_index_on_members.rb
@@ -0,0 +1,20 @@
+class AddUniqueIndexOnMembers < ActiveRecord::Migration
+  def self.up
+    # Reassign MemberRole rows if needed
+    MemberRole.update_all("member_id =" +
+      " (SELECT min(m2.id) FROM #{Member.table_name} m1, #{Member.table_name} m2" +
+      " WHERE m1.user_id = m2.user_id AND m1.project_id = m2.project_id" +
+      " AND m1.id = #{MemberRole.table_name}.member_id)")
+    # Remove duplicates
+    Member.connection.select_values("SELECT m.id FROM #{Member.table_name} m" +
+      " WHERE m.id > (SELECT min(m1.id) FROM #{Member.table_name} m1 WHERE m1.user_id = m.user_id AND m1.project_id = m.project_id)").each do |i|
+        Member.delete_all(["id = ?", i])
+      end
+    
+    # Then add a unique index
+    add_index :members, [:user_id, :project_id], :unique => true
+  end
+
+  def self.down
+  end
+end
