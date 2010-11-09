48c3f341671993b30667bf3b09112eeabf622a60
jwollert <johannes.wollert@gmail.com>
2010-11-09 15:38:34 +0100

    some more cuces for filtering

--- features/step_definitions/custom_steps.rb
+++ features/step_definitions/custom_steps.rb
@@ -47,3 +47,24 @@ Given /^there is a standard permission test project named "([^\"]*)"$/ do |name|
       | cost type    | ten       |
   }
 end
+
+Given /^I set the filter "([^"]*)" to "([^"]*)" with the operator "([^"]*)"$/ do |filter, value, operator|
+  locate :xpath, "//body"
+  page.evaluate_script("restore_filter(\"#{filter}\", \"#{operator}\", \"#{value}\")")
+end
+
+Then /^filter "([^"]*)" should (not )?be visible$/ do |filter, negative|
+  bool = negative ? false : true
+  page.evaluate_script("$('tr_#{filter}').visible()") =~ /^#{bool}$/
+end
+
+Given /^I group (rows|columns) by "([^"]*)"/ do |target, group|
+  destination = target == "rows" ? "moveLeft" : "moveUp"
+  When %{I select "#{group}" from "group_by_container"}
+  find(:xpath, "//input[@class='buttons group_by move #{destination}']").click
+end
+
+When /^I send the query$/ do
+  find(:xpath, '//p[@class="buttons"]/a[@class="button apply"]').click
+end
+
