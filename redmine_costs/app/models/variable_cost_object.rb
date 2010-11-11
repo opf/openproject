5305ed536d6298c29e4d3231707b40121674980c
Jens Ulferts <j.ulferts@finn.de>
2010-11-11 11:32:56 +0100

    addresses #3720: implicit BigDecimal.to_s conversion removed

--- redmine_costs/app/models/variable_cost_object.rb
+++ redmine_costs/app/models/variable_cost_object.rb
@@ -3,25 +3,25 @@ class VariableCostObject < CostObject
 
   has_many :material_budget_items, :include => :cost_type, :foreign_key => 'cost_object_id', :dependent => :destroy
   has_many :labor_budget_items, :include => :user, :foreign_key => 'cost_object_id', :dependent => :destroy
-  
+
   validates_associated :material_budget_items
   validates_associated :labor_budget_items
-  
+
   after_update :save_material_budget_items
   after_update :save_labor_budget_items
-  
+
   def copy_from(arg)
     cost_object = arg.is_a?(VariableCostObject) ? arg : VariableCostObject.find(arg)
     self.attributes = cost_object.attributes.dup
     self.material_budget_items = cost_object.material_budget_items.collect {|v| v.clone}
     self.labor_budget_items = cost_object.labor_budget_items.collect {|v| v.clone}
   end
-  
+
   # Label of the current cost_object type for display in GUI.
   def type_label
     return l(:label_variable_cost_object)
   end
-  
+
   def material_budget
     @material_budget ||= material_budget_items.inject(BigDecimal.new("0.0000")){|sum, i| sum += i.costs}
   end
@@ -29,49 +29,49 @@ class VariableCostObject < CostObject
   def labor_budget
     @labor_budget ||= labor_budget_items.inject(BigDecimal.new("0.0000")){|sum, i| sum += i.costs}
   end
-  
+
   def spent
     spent_material + spent_labor
   end
-  
+
   def spent_material
     @spent_material ||= begin
       if cost_entries.blank?
         BigDecimal.new("0.0000")
       else
-        BigDecimal.new(cost_entries.visible_costs(User.current, self.project).sum("CASE
+        cost_entries.visible_costs(User.current, self.project).sum("CASE
           WHEN #{CostEntry.table_name}.overridden_costs IS NULL THEN
             #{CostEntry.table_name}.costs
           ELSE
-            #{CostEntry.table_name}.overridden_costs END"))
+            #{CostEntry.table_name}.overridden_costs END")
       end
     end
   end
-  
+
   def spent_labor
     @spent_labor ||= begin
       if time_entries.blank?
         BigDecimal.new("0.0000")
       else
-        BigDecimal.new(time_entries.visible_costs(User.current, self.project).sum("CASE
+        time_entries.visible_costs(User.current, self.project).sum("CASE
           WHEN #{TimeEntry.table_name}.overridden_costs IS NULL THEN
             #{TimeEntry.table_name}.costs
           ELSE
-            #{TimeEntry.table_name}.overridden_costs END"))
+            #{TimeEntry.table_name}.overridden_costs END")
       end
     end
   end
-  
+
   def new_material_budget_item_attributes=(material_budget_item_attributes)
     material_budget_item_attributes.each do |index, attributes|
       material_budget_items.build(attributes) if attributes[:units].to_i > 0
     end
   end
-  
+
   def existing_material_budget_item_attributes=(material_budget_item_attributes)
     material_budget_items.reject(&:new_record?).each do |material_budget_item|
       attributes = material_budget_item_attributes[material_budget_item.id.to_s]
-      
+
       if attributes && attributes[:units].to_i > 0
         attributes[:budget] = Rate.clean_currency(attributes[:budget])
         material_budget_item.attributes = attributes
@@ -80,23 +80,23 @@ class VariableCostObject < CostObject
       end
     end
   end
-  
+
   def save_material_budget_items
     material_budget_items.each do |material_budget_item|
       material_budget_item.save(false)
     end
   end
-  
+
   def new_labor_budget_item_attributes=(labor_budget_item_attributes)
     labor_budget_item_attributes.each do |index, attributes|
       labor_budget_items.build(attributes) if attributes[:hours].to_i > 0 && attributes[:user_id].to_i > 0
     end
   end
-  
+
   def existing_labor_budget_item_attributes=(labor_budget_item_attributes)
     labor_budget_items.reject(&:new_record?).each do |labor_budget_item|
       attributes = labor_budget_item_attributes[labor_budget_item.id.to_s]
-      
+
       if attributes && attributes[:hours].to_i > 0 && attributes[:user_id].to_i > 0
         attributes[:budget] = Rate.clean_currency(attributes[:budget])
         labor_budget_item.attributes = attributes
@@ -105,7 +105,7 @@ class VariableCostObject < CostObject
       end
     end
   end
-  
+
   def save_labor_budget_items
     labor_budget_items.each do |labor_budget_item|
       labor_budget_item.save(false)
