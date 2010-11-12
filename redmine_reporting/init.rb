52c53093967fb4085524ace97fde7254d535a8de
jwollert <johannes.wollert@gmail.com>
2010-11-12 14:39:14 +0100

    delete obsolete patches

--- redmine_reporting/init.rb
+++ redmine_reporting/init.rb
@@ -1,10 +1,7 @@
 require 'redmine'
 
-fail "upgrade ruby version, ruby < 1.8.7 suffers from Hash#hash bug" if {:a => 10}.hash != {:a => 10}.hash
-#require "hwia_rails"
-
-require 'big_decimal_patch'
-require 'to_date_patch'
+#FIXME
+#require_plugin 'reporting_engine'
 
 # Hooks
 require 'view_projects_show_sidebar_bottom_hook'
