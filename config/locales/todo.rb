c54df5795479d07cb3a3a0611c92cd55d4b69790
friflaj <friflaj@ajellito.com>
2010-11-14 11:17:03 +0100

    Don't update translations, just report errors (potential character encoding problems)

---
+++ config/locales/todo.rb
@@ -0,0 +1,33 @@
+#!/usr/bin/env ruby
+
+require 'yaml'
+
+langdir = File.dirname(__FILE__)
+
+template_file = "#{langdir}/en.yml"
+template = YAML::load_file(template_file)['en']
+
+errors = false
+
+Dir.glob("#{langdir}/*.yml").each {|lang_file|
+  next if lang_file == template_file
+
+  lang = YAML::load_file(lang_file)
+  l = lang.keys[0]
+
+  template.each_pair {|key, txt|
+    if ! lang[l][key]
+      puts "missing: ${l}: #{key}"
+      errors = true
+    end
+  }
+
+  lang[l].keys.each {|k|
+    if !template[k]
+      puts "obsolete: #{l}: #{k}"
+      errors = true
+    end
+  }
+}
+
+puts "All OK!" unless errors
