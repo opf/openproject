427ec05c8b7931d0096c00d451e3b36ccfa6c969
Jean-Philippe Lang <jp_lang@yahoo.fr>
2010-11-20 09:55:06 +0000

    Fixed: Migration from boolean to varchar fails on PostgreSQL 8.1 (#6943).
    git-svn-id: svn+ssh://rubyforge.org/var/svn/redmine/trunk@4413 e93f8b46-1217-0410-a6f0-8f06a7374b81

--- db/migrate/20100129193402_change_users_mail_notification_to_string.rb
+++ db/migrate/20100129193402_change_users_mail_notification_to_string.rb
@@ -1,9 +1,17 @@
 class ChangeUsersMailNotificationToString < ActiveRecord::Migration
   def self.up
-    change_column :users, :mail_notification, :string, :default => '', :null => false
+    rename_column :users, :mail_notification, :mail_notification_bool
+    add_column :users, :mail_notification, :string, :default => '', :null => false
+    User.update_all("mail_notification = 'all'", "mail_notification_bool = #{connection.quoted_true}")
+    User.update_all("mail_notification = 'selected'", "EXISTS (SELECT 1 FROM #{Member.table_name} WHERE #{Member.table_name}.mail_notification = #{connection.quoted_true} AND #{Member.table_name}.user_id = #{User.table_name}.id)")
+    User.update_all("mail_notification = 'only_my_events'", "mail_notification NOT IN ('all', 'selected')")
+    remove_column :users, :mail_notification_bool
   end
 
   def self.down
-    change_column :users, :mail_notification, :boolean, :default => true, :null => false
+    rename_column :users, :mail_notification, :mail_notification_char
+    add_column :users, :mail_notification, :boolean, :default => true, :null => false
+    User.update_all("mail_notification = #{connection.quoted_false}", "mail_notification_char <> 'all'")
+    remove_column :users, :mail_notification_char
   end
 end
--- db/migrate/20100129193813_update_mail_notification_values.rb
+++ db/migrate/20100129193813_update_mail_notification_values.rb
@@ -1,9 +1,8 @@
 # Patch the data from a boolean change.
 class UpdateMailNotificationValues < ActiveRecord::Migration
   def self.up
-    User.update_all("mail_notification = 'all'", "mail_notification IN ('1', 't')")
-    User.update_all("mail_notification = 'selected'", "EXISTS (SELECT 1 FROM #{Member.table_name} WHERE #{Member.table_name}.mail_notification = #{connection.quoted_true} AND #{Member.table_name}.user_id = #{User.table_name}.id)")
-    User.update_all("mail_notification = 'only_my_events'", "mail_notification NOT IN ('all', 'selected')")
+    # No-op
+    # See 20100129193402_change_users_mail_notification_to_string.rb
   end
 
   def self.down
