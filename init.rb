aff38bcedce885e5ec7211a91b8837009767b650
Jens Ulferts <j.ulferts@finn.de>
2010-11-22 16:12:23 +0100

    removed comment, no longer needed

--- init.rb
+++ init.rb
@@ -7,6 +7,7 @@ Dispatcher.to_prepare do
   require_dependency 'global_roles/permission_patch'
   require_dependency 'global_roles/access_control_patch'
   require_dependency 'global_roles/role_patch'
+  require_dependency 'global_roles/users_helper_patch'
 end
 
 Redmine::Plugin.register :redmine_global_roles do
@@ -16,10 +17,6 @@ Redmine::Plugin.register :redmine_global_roles do
   version '0.0.1'
 
   if RAILS_ENV != "test"
-    #patches are loaded after permissions are set
-    #hence it is not possible to define an option :public => true
-    #we must therefore take the less obvious option to say :require => :global
-    #an option ala :public => true would only be possible when the plugin has been moved to core
     require_or_load 'global_roles/permission_patch'
 
     project_module :user do
