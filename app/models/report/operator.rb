87c8769d7d4582dcce63e845961cad59f1823afe
jwollert <johannes.wollert@gmail.com>
2010-11-25 16:39:38 +0100

    removes redmine specific operators

--- app/models/report/operator.rb
+++ app/models/report/operator.rb
@@ -69,22 +69,6 @@ class Report::Operator
       end
     end
 
-    new "c", :arity => 0 do
-      def modify(query, field, *values)
-        raise "wrong field" if field.to_s.split('.').last != "status_id"
-        query.where "(#{IssueStatus.table_name}.is_closed = #{quoted_true})"
-        query
-      end
-    end
-
-    new "o", :arity => 0 do
-      def modify(query, field, *values)
-        raise "wrong field" if field.to_s.split('.').last != "status_id"
-        query.where "(#{IssueStatus.table_name}.is_closed = #{quoted_false})"
-        query
-      end
-    end
-
     new "!~", :arity => 1 do
       def modify(query, field, *values)
         value = values.first || ''
@@ -187,30 +171,6 @@ class Report::Operator
       def modify(query, field, value)
         return query if value.to_s.empty?
         "=".to_operator.modify query, field, quoted_date(value)
-      end
-    end
-
-    new "=_child_projects", :validate => :integers, :label => :label_is_project_with_subprojects do
-      def modify(query, field, *values)
-        p_ids = []
-        values.each do |value|
-          p_ids += ([value] << Project.find(value).descendants.map{ |p| p.id })
-        end
-        "=".to_operator.modify query, field, p_ids
-      rescue ActiveRecord::RecordNotFound
-        query
-      end
-    end
-
-    new "!_child_projects", :validate => :integers, :label => :label_is_not_project_with_subprojects do
-      def modify(query, field, *values)
-        p_ids = []
-        values.each do |value|
-          p_ids += ([value] << Project.find(value).descendants.map{ |p| p.id })
-        end
-        "!".to_operator.modify query, field, p_ids
-      rescue ActiveRecord::RecordNotFound
-        query
       end
     end
 
