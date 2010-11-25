5dac615726247465d96506025b4d6d1b28904a7c
jwollert <johannes.wollert@gmail.com>
2010-11-25 16:42:16 +0100

    adjusts init.rb so it doesn't require patches that went into the engine

--- init.rb
+++ init.rb
@@ -1,11 +1,5 @@
 require 'redmine'
 
-fail "upgrade ruby version, ruby < 1.8.7 suffers from Hash#hash bug" if {:a => 10}.hash != {:a => 10}.hash
-#require "hwia_rails"
-
-require 'big_decimal_patch'
-require 'to_date_patch'
-
 # Hooks
 require 'view_projects_show_sidebar_bottom_hook'
 
