8bf8dac8865f8ac9f6c42f837918aead556364e5
Philipp Tessenow <philipp.tessenow@student.hpi.uni-potsdam.de>
2010-11-26 14:43:02 +0100

    some minor magic to make CostQuery::Chainable lookup work

--- app/models/report/inherited_namespace.rb
+++ app/models/report/inherited_namespace.rb
@@ -8,16 +8,21 @@ module Report::InheritedNamespace
   def const_missing(name, *)
     super
   rescue NameError => error
-    raise error unless respond_to? superclass and superclass != self
+    raise error unless respond_to? :superclass and superclass != self
     begin
       zuper = superclass.const_get(name)
       case zuper
-      when Class  then const_set name, Class.new(zuper).extend(InheritedNamespace)
-      when Module then const_set name, Module.new { include zuper }.extend(InheritedNamespace)
+      when Class  then const_set name, Class.new(zuper).extend(Report::InheritedNamespace)
+      when Module then const_set name, Module.new { include zuper }.extend(Report::InheritedNamespace)
       else const_set name, zuper
       end
     rescue NameError
       raise error
     end
+  end
+
+  def inherited(klass)
+    super
+    klass.extend Report::InheritedNamespace
   end
 end
