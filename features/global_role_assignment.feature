357b3aefec951f3f54d80d828b01652288f0642f
Jens Ulferts <j.ulferts@finn.de>
2010-12-21 10:37:59 +0100

    added cuke for global role assignment

--- features/global_role_assignment.feature
+++ features/global_role_assignment.feature
@@ -1,9 +1,11 @@
 Feature: Global Role Assignment
 
   @javascript
-  Scenario: Assigning Role to user
+  Scenario: Assigning a global role to a user
     Given there is the global permission "global1" of the module "global"
-    And there is the global permission "global2" of the module "global"
+    And there is a global role "global_role"
+    And the global role "global_role" may have the following rights:
+      | global1 |
     And there is 1 User with:
       | Login | bob |
       | Firstname | Bob |
@@ -11,7 +13,7 @@ Feature: Global Role Assignment
     And I am admin
     When I go to the edit page of the user called "bob"
     And I click on "tab-global_roles"
-    And I click on "principal_role_role_ids_0"
+    And I select the available role "global_role"
     And I click on "Add"
-    Then I should see "global1" within "#table_principal_roles"
-    And I should not see "global1" within ".principal_role_option"
+    Then I should see "global_role" within "#table_principal_roles"
+    And I should not see "global_role" within "#available_principal_roles"
--- features/step_definitions/global_roles.rb
+++ features/step_definitions/global_roles.rb
@@ -9,7 +9,39 @@ end
 Given /^the global permission "(.+)?" of the module "(.+)?" is defined$/ do |perm_name, perm_module|
   as_admin do
     permissions = Redmine::AccessControl.modules_permissions(perm_module)
-    debugger
     permissions.detect{|p| p.name == perm_name.to_sym && p.global?}.should_not be_nil
   end
+end
+
+Given /^there is a global [rR]ole "([^\"]*)"$/ do |name|
+  GlobalRole.spawn.tap { |r| r.name = name }.save! unless GlobalRole.find_by_name(name)
+end
+
+Given /^the global [rR]ole "([^\"]*)" may have the following [rR]ights:$/ do |role, table|
+  r = GlobalRole.find_by_name(role)
+  raise "No such role was defined: #{role}" unless r
+  as_admin do
+    available_perms = Redmine::AccessControl.permissions.collect(&:name)
+    r.permissions = []
+
+    table.raw.each do |_perm|
+      perm = _perm.first
+      unless perm.blank?
+        perm = perm.gsub(" ", "_").underscore.to_sym
+        if available_perms.include?(:"#{perm}")
+          r.permissions << perm
+        end
+      end
+    end
+
+    r.save!
+  end
+end
+
+When /^I select the available role (.+)$/ do |role|
+  r = GlobalRole.find_by_name(role.gsub("\"", ""))
+  raise "No such role was defined: #{role}" unless r
+  steps %Q{
+    When I check "principal_role_role_ids_#{r.id}"
+  }
 end
