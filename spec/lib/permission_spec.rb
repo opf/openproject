98272d7db9c56c07527e1fee926a9ec27282a8b4
Jens Ulferts <j.ulferts@finn.de>
2010-12-23 11:42:20 +0100

    removed spec as it was implemented in a different place

--- spec/lib/permission_spec.rb
+++ spec/lib/permission_spec.rb
@@ -3,17 +3,9 @@ require File.expand_path(File.dirname(__FILE__) + "/../spec_helper")
 require 'redmine/access_control'
 
 describe Redmine::AccessControl::Permission do
-  describe "setting global" do
+  describe "WHEN setting global permission" do
     describe "creating with", :new do
       before {@permission = Redmine::AccessControl::Permission.new(:perm, {:cont => [:action]}, {:global => true})}
-      describe :global? do
-        it {@permission.global?.should be_true}
-      end
-    end
-
-    describe "name is :add_project", :new do
-      before {@permission = Redmine::AccessControl::Permission.new(:add_project, {:cont => [:action]}, :project_module => :project)}
-
       describe :global? do
         it {@permission.global?.should be_true}
       end
