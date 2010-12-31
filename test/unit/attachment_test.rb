92d34234b01f7d0e7850b50ccd2559f1e63cac5d
Jean-Philippe Lang <jp_lang@yahoo.fr>
2010-12-31 15:24:42 +0000

    Safer assertion.
    git-svn-id: svn+ssh://rubyforge.org/var/svn/redmine/trunk@4598 e93f8b46-1217-0410-a6f0-8f06a7374b81

--- test/unit/attachment_test.rb
+++ test/unit/attachment_test.rb
@@ -34,7 +34,7 @@ class AttachmentTest < ActiveSupport::TestCase
     assert_equal 59, a.filesize
     assert_equal 'text/plain', a.content_type
     assert_equal 0, a.downloads
-    assert_equal Digest::MD5.hexdigest(uploaded_test_file("testfile.txt", "text/plain").read), a.digest
+    assert_equal '1478adae0d4eb06d35897518540e25d6', a.digest
     assert File.exist?(a.diskfile)
   end
   
