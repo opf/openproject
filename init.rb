7af153e0f0a12c9dfae3c73e848034b38ddb2d5e
Felix Schäfer <f.schaefer@finn.de>
2011-01-06 23:30:00 +0100

    Plugin information

--- init.rb
+++ init.rb
@@ -1,8 +1,12 @@
 require 'redmine'
 
 Redmine::Plugin.register :redmine_meeting do
-  name 'Redmine Meeting plugin'
-  author 'Author name'
-  description 'This is a plugin for Redmine'
+  name 'Redmine Meeting'
+  author 'Felix Schäfer'
+  author_url 'http://finn.de/team'
+  description 'This plugin adds a meeting module with functionality to plan and save the minutes of a meeting.'
+  url 'http://finn.de'
   version '0.0.1'
+  
+  requires_redmine :version_or_higher => '0.9'
 end
