e55404eaf98ac9e32a0363aae36fd593eae5b02b
Felix Schäfer <f.schaefer@finn.de>
2011-01-06 21:44:54 +0100

    Bare redmine plugin

---
+++ init.rb
@@ -0,0 +1,8 @@
+require 'redmine'
+
+Redmine::Plugin.register :redmine_meeting do
+  name 'Redmine Meeting plugin'
+  author 'Author name'
+  description 'This is a plugin for Redmine'
+  version '0.0.1'
+end
