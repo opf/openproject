2a0da1629aa4015fda7e4da8eafefa28ff17eba1
Jens Ulferts <j.ulferts@finn.de>
2011-01-07 08:54:55 +0100

    removed global roles allowed_to?

--- app/models/global_role.rb
+++ app/models/global_role.rb
@@ -3,10 +3,6 @@ class GlobalRole < Role
   has_many :principal_roles, :foreign_key => :role_id, :dependent => :destroy
   has_many :principals, :through => :principal_roles
 
-  def allowed_to?(action)
-     has_permission?(action)
-  end
-
   def permissions=(perms) #Why is this not inherited?
     perms = perms.collect {|p| p.to_sym unless p.blank? }.compact.uniq if perms
     write_attribute(:permissions, perms)
