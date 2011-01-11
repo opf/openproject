6d3d23d680a18403c8838e36e5ab03d97948cb81
Felix Schäfer <f.schaefer@finn.de>
2011-01-11 11:20:24 +0100

    tentative routes, agenda/minutes need more work

---
+++ config/routes.rb
@@ -0,0 +1,9 @@
+#custom routes for this plugin
+ActionController::Routing::Routes.draw do |map|
+  map.resources :projects, :only => [] do |project|
+    project.resources :meetings, :shallow => true do |meeting|
+      meeting.resource :meeting_agenda, :as => 'agenda', :only => [:create, :update]
+      meeting.resource :meeting_minutes, :as => 'minutes', :only => [:create, :update]
+    end
+  end
+end
