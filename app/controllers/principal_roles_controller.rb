ff79891ce5108ff2cbbef33e706c7baef814884c
Jens Ulferts <j.ulferts@finn.de>
2011-01-11 11:53:47 +0100

    some refactoring to be extensible, e.g. for privacy_plugin

--- app/controllers/principal_roles_controller.rb
+++ app/controllers/principal_roles_controller.rb
@@ -1,34 +1,88 @@
 class PrincipalRolesController < ApplicationController
   unloadable (PrincipalRolesController)
 
-  def create
-    principal_roles = create_principal_roles_from_params
+  #before_filter :authorize
 
-    respond_to do |format|
-      format.js do
-        render(:update) do |page|
-          principal_roles.each do |role|
-            page.remove "principal_role_option_#{role.role_id}"
-            page.insert_html :top, 'table_principal_roles_body',
-                             :partial => "principal_roles/show_table_row",
-                             :locals => {:principal_role => role}
-          end
-        end
-      end
-    end
+  def create
+    @principal_roles = new_principal_roles_from_params
+
+    call_hook :principal_roles_controller_create_before_save,
+              {:principal_roles => @principal_roles}
+
+    @principal_roles.each{ |pr| pr.save } unless performed?
+
+    call_hook :principal_roles_controller_create_before_respond,
+              {:principal_roles => @principal_roles}
+
+    respond_to_create @principal_roles unless performed?
   end
 
   def update
     @principal_role = PrincipalRole.find(params[:principal_role][:id])
     @principal_role.update_attributes(params[:principal_role])
 
+    respond_to_update @principal_role
+  end
+
+  def destroy
+    @principal_role = PrincipalRole.find(params[:id])
+    @user = Principal.find(@principal_role.principal_id)
+    @global_roles = GlobalRole.all
+
+    @principal_role.destroy
+
+    respond_to_destroy @principal_role, @user, @global_roles
+  end
+
+  def respond_with=(value)
+    @respond_with = value
+  end
+
+  def respond_with
+    @respond_with
+  end
+
+  private
+
+  def new_principal_roles_from_params
+    pr_params = params[:principal_role].dup
+    role_ids = pr_params[:role_id] ? [pr_params.delete(:role_id)] : pr_params.delete(:role_ids)
+    roles = Role.find role_ids
+    principal_roles = []
+    role_ids.map(&:to_i).each do |role_id|
+      role = PrincipalRole.new(pr_params)
+      role.role = roles.detect {|r| r.id == role_id}
+      principal_roles << role
+    end
+    principal_roles
+  end
+
+  def respond_to_create roles
     respond_to do |format|
       format.js do
         render(:update) do |page|
-          if @principal_role.valid?
-            page.replace "principal_role-#{@principal_role.id}",
+          roles.each do |role|
+            page.remove "principal_role_option_#{role.role_id}"
+            page.insert_html :top, 'table_principal_roles_body',
+                             :partial => "principal_roles/show_table_row",
+                             :locals => {:principal_role => role}
+
+            call_hook :principal_roles_controller_create_respond_js_role,
+                      {:page => page, :principal_role => role}
+          end
+        end
+      end
+    end
+  end
+
+  def respond_to_update role
+    respond_to do |format|
+      format.js do
+        render(:update) do |page|
+          if role.valid?
+            page.replace "principal_role-#{role.id}",
                           :partial => "principal_roles/show_table_row",
-                          :locals => {:principal_role => @principal_role}
+                          :locals => {:principal_role => role}
           else
             page.insert_html :top, "tab-content-global_roles", :partial => 'errors'
           end
@@ -37,37 +91,16 @@ class PrincipalRolesController < ApplicationController
     end
   end
 
-  def destroy
-    principal_role = PrincipalRole.find(params[:id])
-    user = Principal.find(principal_role.principal_id)
-    global_roles = GlobalRole.all
-
-    principal_role.destroy
-
+  def respond_to_destroy principal_role, user, global_roles
     respond_to do |format|
       format.js do
         render(:update) do |page|
-          page.remove "principal_role-#{params[:id]}"
+          page.remove "principal_role-#{principal_role.id}"
           page.replace "available_principal_roles",
                         :partial => "users/available_global_roles",
                         :locals => {:user => user, :global_roles => global_roles}
         end
       end
     end
-  end
-
-  private
-
-  def create_principal_roles_from_params
-    role_ids = params[:principal_role][:role_id] ? [params[:principal_role].delete(:role_id)] : params[:principal_role].delete(:role_ids)
-    roles = Role.find role_ids
-    principal_roles = []
-    role_ids.map(&:to_i).each do |role_id|
-      role = PrincipalRole.new(params[:principal_role])
-      role.role = roles.detect {|r| r.id == role_id}
-      role.save
-      principal_roles << role
-    end
-    principal_roles
   end
 end
