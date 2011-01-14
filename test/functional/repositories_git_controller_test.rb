61a517179d49062b566ea98918ef5b70e0cb4ae3
Toshi MARUYAMA <marutosijp2@yahoo.co.jp>
2011-01-14 23:10:13 +0000

    scm: git: fix id at functional test_empty_revision test(#7307).
    git-svn-id: svn+ssh://rubyforge.org/var/svn/redmine/trunk@4713 e93f8b46-1217-0410-a6f0-8f06a7374b81

--- test/functional/repositories_git_controller_test.rb
+++ test/functional/repositories_git_controller_test.rb
@@ -191,7 +191,7 @@ class RepositoriesGitControllerTest < ActionController::TestCase
       @repository.fetch_changesets
       @repository.reload
       ['', ' ', nil].each do |r|
-        get :revision, :id => 1, :rev => r
+        get :revision, :id => 3, :rev => r
         assert_response 404
         assert_error_tag :content => /was not found/
       end
