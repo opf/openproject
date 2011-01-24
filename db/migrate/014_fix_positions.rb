b54b17a8d2cf532b6b351f6d28e85189f0888e79
friflaj <friflaj@ajellito.com>
2011-01-24 19:50:13 +0100

    Stupid rails

--- db/migrate/014_fix_positions.rb
+++ db/migrate/014_fix_positions.rb
@@ -1,11 +1,20 @@
 class FixPositions < ActiveRecord::Migration
   def self.up
-    errors = 0
-    Issue.find(:all, :conditions => "subject is NULL").each do |issue|
-      errors += 1
-      puts "Issue #{issue.id} does not have a subject"
+    errors = []
+    Issue.find(:all, :conditions => "subject is NULL or (start_date is null and not due_date is null) or start_date > due_date or updated_on < created_on or start_date > created_on").each do |issue|
+      errors << issue.id
     end
-    raise "Errors found in your database, aborting migration" if errors > 0
+
+    if errors.size > 0
+      puts "You have issues in your database that have one or more of the following problems:"
+      puts "* subject is missing"
+      puts "* due date is set, but start dat is not"
+      puts "* start date is later than due date"
+      puts "* updated-date is before created-date"
+      puts "* start date is after created-date"
+      puts errors.inspect
+      raise "Please fix these and retry the migration"
+    end
 
     ActiveRecord::Base.transaction do
       Story.find(:all, :conditions => "parent_id is NULL", :order => "project_id ASC, fixed_version_id ASC, position ASC").each_with_index do |s,i|
