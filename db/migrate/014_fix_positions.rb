35919d5bd516af096c82af6f0c343d04066389a8
friflaj <friflaj@ajellito.com>
2011-01-24 18:24:53 +0100

    Stupid rails

--- db/migrate/014_fix_positions.rb
+++ db/migrate/014_fix_positions.rb
@@ -1,5 +1,12 @@
 class FixPositions < ActiveRecord::Migration
   def self.up
+    errors = 0
+    Issue.find(:all, :conditions => "subject is NULL").each do |issue|
+      errors += 1
+      puts "Issue #{issue.id} does not have a subject"
+    end
+    raise "Errors found in your database, aborting migration" if errors > 0
+
     ActiveRecord::Base.transaction do
       Story.find(:all, :conditions => "parent_id is NULL", :order => "project_id ASC, fixed_version_id ASC, position ASC").each_with_index do |s,i|
         s.position=i+1
