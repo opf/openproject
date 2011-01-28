1ecc7e48e6409c93c46cedae463d0eee5d567835
Tim Felgentreff <timfelgentreff@gmail.com>
2011-01-28 15:00:35 +0100

    stub table widget

--- lib/widget/table.rb
+++ lib/widget/table.rb
@@ -1,2 +1,22 @@
 class Widget::Table < Widget::Base
+  def render
+    if @query.group_bys.empty?
+      widget = Widget::Table::DetailedTable
+    else
+      if @query.depth_of(:column) == 0
+        @query.column(:singleton_value)
+      elsif @query.depth_of(:row) == 0
+        @query.row(:singleton_value)
+      end
+      widget = Widget::Table::ReportTable
+    end
+
+    content_tag :div, :id => "result-table" do
+      if @query.result.count > 0
+        render_widget widget, @query
+      else
+        content_tag :p, l(:label_no_data), :class => "nodata"
+      end
+    end
+  end
 end
