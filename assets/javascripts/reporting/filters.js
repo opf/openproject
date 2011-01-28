b3487fef348a52e6e5654029f55ecd394de923fd
Tim Felgentreff <timfelgentreff@gmail.com>
2011-01-28 15:51:14 +0100

    move and lint more JS

--- assets/javascripts/reporting/filters.js
+++ assets/javascripts/reporting/filters.js
@@ -1,23 +1,109 @@
+/*jslint white: false, nomen: true, devel: true, on: true, debug: false, evil: true, onevar: false, browser: true, white: false, indent: 2 */
+/*global window, $, $$, Reporting, Effect, Ajax */
+
 Reporting.Filters = {
-  function show_filter(field, slowly, callback_func) {
-    if (callback_func == undefined) {
-      callback_func = function() {};
+  load_available_values_for_filter:  function  (filter_name, callback_func) {
+    var select;
+    select = $('' + filter_name + '_arg_1_val');
+    if (select !== null && select.readAttribute('data-loading') === "ajax" && select.childElements().length === 0) {
+      Ajax.Updater({ success: select }, window.global_prefix + '/cost_reports/available_values', {
+        parameters: { filter_name: filter_name },
+        insertion: 'bottom',
+        evalScripts: false,
+        onCreate: function (a, b) {
+          $('operators_' + filter_name).disable();
+          $('' + filter_name + '_arg_1_val').disable();
+        },
+        onComplete: function (a, b) {
+          $('operators_' + filter_name).enable();
+          $('' + filter_name + '_arg_1_val').enable();
+          callback_func();
+        }
+      });
+      Reporting.Filters.multi_select(select, false);
+    } else {
+      callback_func();
     }
-    if (slowly == undefined) {
+  },
+
+  show_filter: function (field, slowly, callback_func) {
+    if (callback_func === undefined) {
+      callback_func = function () {};
+    }
+    if (slowly === undefined) {
       slowly = true;
     }
     var field_el = $('tr_' +  field);
     if (field_el !== null) {
-        load_available_values_for_filter(field, callback_func);
-        // the following command might be included into the callback_function (which is called after the ajax request) later
-        $('rm_' + field).value = field;
-        if (slowly) {
-            new Effect.Appear(field_el);
-        } else {
-            field_el.show();
-        }
-        operator_changed(field, $("operators_" + field));
-        display_category(field_el);
+      Reporting.Filters.load_available_values_for_filter(field, callback_func);
+      // the following command might be included into the callback_function (which is called after the ajax request) later
+      $('rm_' + field).value = field;
+      if (slowly) {
+        Effect.Appear(field_el);
+      } else {
+        field_el.show();
+      }
+      Reporting.Filters.operator_changed(field, $("operators_" + field));
+      Reporting.Filters.display_category(field_el);
     }
+  },
+
+  display_category: function (tr_field) {
+    var label = $(tr_field.getAttribute("data-label"));
+    if (label !== null) {
+      label.show();
+    }
+  },
+
+  operator_changed: function (field, select) {
+    var option_tag, arity;
+    if (select === null) {
+      return;
+    }
+    option_tag = select.options[select.selectedIndex];
+    arity = parseInt(option_tag.getAttribute("data-arity"), 10);
+    Reporting.Filters.change_argument_visibility(field, arity);
+  },
+
+  change_argument_visibility: function (field, arg_nr) {
+    var params, i;
+    params = [$(field + '_arg_1'), $(field + '_arg_2')];
+
+    for (i = 0; i < 2; i += 1) {
+      if (params[i] !== null) {
+        if (arg_nr >= (i + 1) || arg_nr <= (-1 - i)) {
+          params[i].show();
+        } else {
+          params[i].hide();
+        }
+      }
+    }
+  },
+
+  add_filter: function (select) {
+    var field;
+    field = select.value;
+    Reporting.Filters.show_filter(field);
+    select.selectedIndex = 0;
+    Reporting.Filters.select_option_enabled(select, field, false);
+  },
+
+  select_option_enabled: function (box, value, state) {
+    box.select("[value='" + value + "']").first().disabled = state;
+  },
+
+  multi_select: function (select, multi) {
+    select.multiple = multi;
+    if (multi) {
+      select.size = 4;
+      // deselect first option
+      select.options[0].selected = false;
+    } else {
+      select.size = 1;
+    }
+  },
+
+  toggle_multi_select: function (select) {
+    Reporting.Filters.multi_select(select, !select.multiple);
   }
-}
+};
--- assets/javascripts/reporting/restore_query.js
+++ assets/javascripts/reporting/restore_query.js
@@ -1,81 +1,77 @@
-/*
-jslint Reporting: false, nomen: true, debug: false, evil: false,
-    onevar: false, browser: true, white: false, indent: 0
-*/
+/*jslint white: false, nomen: true, devel: true, on: true, debug: false, evil: true, onevar: false, browser: true, white: false, indent: 2 */
+/*global window, $, $$, Reporting, Effect, Ajax */
 
 Reporting.RestoreQuery = {
-  restore_filters: function() {
-    group_bys = $('filter_table').select("tr").select(function(row) {
-      return $(row).getAttribute("data-selected") == "true";
-    }).each(function(row) {
+  restore_filters: function () {
+    var group_bys = $('filter_table').select("tr").select(function (row) {
+      return $(row).getAttribute("data-selected") === "true";
+    }).each(function (row) {
       var field = row.className;
       Reporting.Filters.show_filter(field);
-      disable_select_option($("add_filter_select"), field);
+      Reporting.Filters.select_option_enabled($("add_filter_select"), field, false);
     });
   },
 
-  select_operator: function(field, operator) {
+  select_operator: function (field, operator) {
     var select, i;
     select = $("operators_" + field);
     if (select === null) {
-        return; // there is no such operator select field
+      return; // there is no such operator select field
     }
     for (i = 0; i < select.options.length; i += 1) {
-        if (select.options[i].value === operator) {
-            select.selectedIndex = i;
-            break;
-        }
+      if (select.options[i].value === operator) {
+        select.selectedIndex = i;
+        break;
+      }
     }
-    operator_changed(field, select);
+    Reporting.Filters.operator_changed(field, select);
   },
 
-  disable_select_option: function(select, field) {
+  disable_select_option: function (select, field) {
     for (var i = 0; i < select.options.length; i += 1) {
-        if (select.options[i].value === field) {
-            select.options[i].disabled = true;
-            break;
-        }
+      if (select.options[i].value === field) {
+        select.options[i].disabled = true;
+        break;
+      }
     }
   },
 
-  show_group_by: function(group_by, target) {
-    $('group_by_container').select("")
+  show_group_by: function (group_by, target) {
+    $('group_by_container').select("");
     var source, group_option, i;
     source = $("group_by_container");
     group_option = null;
     // find group_by option-tag in target select-box
-    for (i = 0; i < source.options.length; i++) {
-        if (source.options[i].value == group_by) {
-            group_option = source.options[i];
-            source.options[i] = null;
-            break;
-        }
+    for (i = 0; i < source.options.length; i += 1) {
+      if (source.options[i].value === group_by) {
+        group_option = source.options[i];
+        source.options[i] = null;
+        break;
+      }
     }
     // die if the appropriate option-tag can not be found
     if (group_option === null) {
-        return;
+      return;
     }
     // move the option-tag to the taget select-box while keepings its data
     target.options[target.length] = group_option;
   },
 
-  restore_group_bys: function() {
+  restore_group_bys: function () {
     // Activate recent group_bys on loading
-    group_bys = $('group_by_container').childElements().collect(function(og) {
-      return $(og).childElements();
-    }).flatten().select(function(group_by) {
+    var group_bys = $('group_by_container').select("option")
+    .select(function (group_by) {
       return $(group_by).hasAttribute("data-selected-axis");
-    }).sortBy(function(group_by) {
+    }).sortBy(function (group_by) {
       return $(group_by).getAttribute("data-selected-index");
-    }).each(function(group_by) {
+    }).each(function (group_by) {
       var axis = $(group_by).getAttribute("data-selected-axis");
       var name = $(group_by).getAttribute("value");
       Reporting.RestoreQuery.show_group_by(name, $('group_by_' + axis + 's'));
     });
   }
-}
+};
 
-
-Reporting.onload(function() {
+Reporting.onload(function () {
   Reporting.RestoreQuery.restore_group_bys();
 });
