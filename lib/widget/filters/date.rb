59d5d21a7336b99e99a9d9c790b12b222606dd7d
Tim Felgentreff <timfelgentreff@gmail.com>
2011-02-02 16:37:01 +0100

    fix display of Date widget

--- lib/widget/filters/date.rb
+++ lib/widget/filters/date.rb
@@ -25,12 +25,14 @@ class Widget::Filters::Date < Widget::Filters::Base
 
     content_tag :td do
       content_tag :div, :id => "#{id_prefix}arg_1", :class => "filter_values" do
-        text_field_tag name, "", :size => 10, :class => "select-small", :id => "#{id_prefix}arg_1_val"
-        calendar_for("#{id_prefix}arg_1_val")
-        content_tag :span, :id => "#{id_prefix}arg_2", :class => "between_tags" do
-          text_field_tag "#{name}", "", :size => 10, :class => "select-small", :id => "#{id_prefix}arg_2_val"
-          calendar_for "#{id_prefix}arg_2_val"
+        text1 = text_field_tag name, "", :size => 10, :class => "select-small", :id => "#{id_prefix}arg_1_val"
+        cal1 = calendar_for("#{id_prefix}arg_1_val")
+        span = content_tag :span, :id => "#{id_prefix}arg_2", :class => "between_tags" do
+          text2 = text_field_tag "#{name}", "", :size => 10, :class => "select-small", :id => "#{id_prefix}arg_2_val"
+          cal2 = calendar_for "#{id_prefix}arg_2_val"
+          text2 + cal2
         end
+        text1 + cal1 + span
       end
     end
   end
