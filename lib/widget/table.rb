8d4f87b08717663ef33f1fcee99db2d8e1820ef0
jwollert <johannes.wollert@gmail.com>
2011-02-03 10:26:41 +0100

    gets the report_table widget to work

--- lib/widget/table.rb
+++ lib/widget/table.rb
@@ -1,22 +1,52 @@
 class Widget::Table < Widget::Base
+  extend Report::InheritedAttribute
+  include ReportingHelper # FIXME ugs in the ugly. have to think of something else for mapping stuff
+
+  inherited_attribute :debug, :default => false
+  inherited_attribute :mapping, :default => {}
+
+  def initialize(query, options = {})
+    raise ArgumentError, "Tables only work on Reports!" unless query.is_a? Report
+    super(query)
+    debug = options[:debug] || false
+    mapping = options[:mapping] || {}
+  end
+
+  def debug?
+    !!debug
+  end
+
+  def render_with_options(options = {}, &block)
+    if canvas = options[:to]
+      canvas << "\n" << render(&block)
+    else
+      render(&block)
+    end
+  end
+
   def render
     if @query.group_bys.empty?
       widget = Widget::Table::DetailedTable
+    elsif @query.depth_of(:column) + @query.depth_of(:row) == 1
+      widget = Widget::Table::SimpleTable
     else
-      if @query.depth_of(:column) == 0
+      if @query.depth_of(:row) == 0
         @query.column(:singleton_value)
-      elsif @query.depth_of(:row) == 0
+      elsif @query.depth_of(:column) == 0
         @query.row(:singleton_value)
       end
       widget = Widget::Table::ReportTable
     end
-
     content_tag :div, :id => "result-table" do
       if @query.result.count > 0
-        render_widget widget, @query
+        render_widget widget, @query, { :debug => debug? }
       else
         content_tag :p, l(:label_no_data), :class => "nodata"
       end
     end
+  end
+
+  def represent_field
+
   end
 end
--- lib/widget/table/report_table.rb
+++ lib/widget/table/report_table.rb
@@ -1,5 +1,150 @@
-class Widget::Table::ReportTable < Widget::Base
+class Widget::Table::ReportTable < Widget::Table
+
+  attr_accessor :walker
+
+  def initialize(report, options = {})
+    super
+    @walker = @query.walker
+  end
+
   def render
-    "blah"
+    walker.for_final_row do |row, cells|
+      final_row_html = ""
+      final_row_html += content_tag :th, :class => 'normal inner left' do
+        "#{show_row(row)}#{debug_fields(row)}"
+      end
+      final_row_html += cells.join
+      final_row_html += content_tag :th, :class => 'normal inner right' do
+        "#{show_row(row)}#{debug_fields(row)}"
+      end
+      final_row_html
+    end
+
+    walker.for_row do |row, subrows|
+      subrows.flatten!
+      unless row.fields.empty?
+        subrows[0] = ''
+        subrows[0] += content_tag(:th, :class => 'top left', :rowspan => subrows.size) do
+          "#{show_row(row)}#{debug_fields(row)}"
+        end
+        subrows[0].gsub("class='normal'", "class='top'")
+        subrows[0] += content_tag(:th, :class => 'top right', :rowspan => subrows.size) do
+          "#{show_row(row)}#{debug_fields(row)}"
+        end
+      end
+      subrows.last.gsub!("class='normal", "class='bottom")
+      subrows.last.gsub!("class='top", "class='bottom top")
+      subrows
+    end
+
+    walker.for_empty_cell do
+      content_tag(:td, :class =>'normal empty') { "&nbsp;".html_safe }
+    end
+    walker.for_cell do |result|
+      content_tag :td, :class => 'normal right' do
+        "#{show_result(result)}#{debug_fields(result)}"
+      end
+    end
+
+    content = content_tag :table, :class => 'list report' do
+
+      # --------------------
+      # HEADER
+      # --------------------
+      header_content = ""
+      walker.headers do |list, first, first_in_col, last_in_col|
+        header_content += '<tr>'.html_safe if first_in_col
+        if first
+          header_content += content_tag :th, :rowspan => @query.depth_of(:column), :colspan => @query.depth_of(:row) do
+            ""
+          end
+        end
+        list.each do |column|
+          header_content += content_tag :th, :colspan => column.final_number(:column), :class => column.final?(:column) ? "inner" : "" do
+            show_row column
+          end
+        end
+        if first
+          header_content += content_tag :th, :rowspan => @query.depth_of(:column), :colspan => @query.depth_of(:row) do
+            ""
+          end
+        end
+        header_content += '</tr>'.html_safe if last_in_col
+      end
+      head = content_tag :thead, header_content.html_safe
+
+      # --------------------
+      # FOOTER
+      # --------------------
+      reverse_headers = ""
+      walker.reverse_headers do |list, first, first_in_col, last_in_col|
+        if first_in_col
+          reverse_headers += '<tr>'
+          reverse_headers += content_tag :th, :rowspan => @query.depth_of(:column), :colspan => @query.depth_of(:row), :class => 'top' do
+              "&nbsp;".html_safe if first
+          end
+        end
+
+        list.each do |column|
+          reverse_headers += content_tag :th, :colspan => column.final_number(:column), :class => (first ? "inner" : "") do
+            "#{show_result(column)}#{debug_fields(column)}"
+          end
+        end
+        if last_in_col
+          if first
+            reverse_headers += content_tag :th,
+                                         :rowspan => @query.depth_of(:column),
+                                         :colspan => @query.depth_of(:row),
+                                         :class => 'top result' do
+              show_result @query
+            end
+          end
+          reverse_headers += '</tr>'
+        end
+      end
+      foot = content_tag :tfoot, reverse_headers.html_safe
+
+      # --------------------
+      # BODY
+      # --------------------
+      first = true
+      walker_body = ""
+      walker.body do |line|
+        if first
+          line.gsub!("class='normal", "class='top")
+          first = false
+        end
+        walker_body += content_tag :tr, :class => cycle("odd", "even") do
+          line.html_safe
+        end
+      end
+      body = content_tag :tbody, walker_body.html_safe
+      head + foot + body
+    end
+
+    if debug?
+      content += content_tag :pre do
+        debug_pre_content = "[ Query ]" +
+        @query.chain.each do |child|
+          "#{h child.class.inspect}, #{h child.type}"
+        end
+
+        debug_pre_content += "[ RESULT ]"
+        @query.result.recursive_each_with_level do |level, result|
+          debug_pre_content += ">>> " * (level+1)
+          debug_pre_content += h(result.inspect)
+          debug_pre_content += "   " * (level+1)
+          debug_pre_content += h(result.type.inspect)
+          debug_pre_content += "   " * (level+1)
+          debug_pre_content += h(result.fields.inspect)
+        end
+        debug_pre_content += "[ HEADER STACK ]"
+        debug_pre_content += walker.header_stack.each do |l|
+          ">>> #{l.inspect}"
+        end
+      end
+    end
+    content
   end
 end
+
