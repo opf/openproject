927a2846486faa761b5ab71feae686c918099137
jwollert <johannes.wollert@gmail.com>
2011-02-03 17:59:07 +0100

    always render ReportTable for now

--- lib/widget/table.rb
+++ lib/widget/table.rb
@@ -24,18 +24,16 @@ class Widget::Table < Widget::Base
   end
 
   def render
-    if @query.group_bys.empty?
-      widget = Widget::Table::DetailedTable
-    elsif @query.depth_of(:column) + @query.depth_of(:row) == 1
-      widget = Widget::Table::SimpleTable
-    else
-      if @query.depth_of(:row) == 0
-        @query.column(:singleton_value)
-      elsif @query.depth_of(:column) == 0
-        @query.row(:singleton_value)
-      end
-      widget = Widget::Table::ReportTable
-    end
+    # if @query.depth_of(:column) + @query.depth_of(:row) == 1
+    #   widget = Widget::Table::SimpleTable
+    # else
+    #   if @query.depth_of(:row) == 0
+    #     @query.column(:singleton_value)
+    #   elsif @query.depth_of(:column) == 0
+    #     @query.row(:singleton_value)
+    #   end
+    # end
+    widget = Widget::Table::ReportTable
     content_tag :div, :id => "result-table" do
       if @query.result.count > 0
         render_widget widget, @query, { :debug => debug? }
