d0e0e124362d953d63d17afe0d9cd5884b3f9fb0
jwollert <johannes.wollert@gmail.com>
2011-02-03 10:50:58 +0100

    split render method

--- lib/widget/table/report_table.rb
+++ lib/widget/table/report_table.rb
@@ -8,6 +8,14 @@ class Widget::Table::ReportTable < Widget::Table
   end
 
   def render
+    configure_walker
+    content = content_tag :table, :class => 'list report' do
+      header + footer + body
+    end
+    content += (debug_content if debug?)
+  end
+
+  def configure_walker
     walker.for_final_row do |row, cells|
       final_row_html = ""
       final_row_html += content_tag :th, :class => 'normal inner left' do
@@ -40,111 +48,104 @@ class Widget::Table::ReportTable < Widget::Table
     walker.for_empty_cell do
       content_tag(:td, :class =>'normal empty') { "&nbsp;".html_safe }
     end
+
     walker.for_cell do |result|
       content_tag :td, :class => 'normal right' do
         "#{show_result(result)}#{debug_fields(result)}"
       end
     end
+  end
 
-    content = content_tag :table, :class => 'list report' do
-
-      # --------------------
-      # HEADER
-      # --------------------
-      header_content = ""
-      walker.headers do |list, first, first_in_col, last_in_col|
-        header_content += '<tr>'.html_safe if first_in_col
-        if first
-          header_content += content_tag :th, :rowspan => @query.depth_of(:column), :colspan => @query.depth_of(:row) do
-            ""
-          end
-        end
-        list.each do |column|
-          header_content += content_tag :th, :colspan => column.final_number(:column), :class => column.final?(:column) ? "inner" : "" do
-            show_row column
-          end
-        end
-        if first
-          header_content += content_tag :th, :rowspan => @query.depth_of(:column), :colspan => @query.depth_of(:row) do
-            ""
-          end
-        end
-        header_content += '</tr>'.html_safe if last_in_col
-      end
-      head = content_tag :thead, header_content.html_safe
-
-      # --------------------
-      # FOOTER
-      # --------------------
-      reverse_headers = ""
-      walker.reverse_headers do |list, first, first_in_col, last_in_col|
-        if first_in_col
-          reverse_headers += '<tr>'
-          reverse_headers += content_tag :th, :rowspan => @query.depth_of(:column), :colspan => @query.depth_of(:row), :class => 'top' do
-              "&nbsp;".html_safe if first
-          end
-        end
-
-        list.each do |column|
-          reverse_headers += content_tag :th, :colspan => column.final_number(:column), :class => (first ? "inner" : "") do
-            "#{show_result(column)}#{debug_fields(column)}"
-          end
-        end
-        if last_in_col
-          if first
-            reverse_headers += content_tag :th,
-                                         :rowspan => @query.depth_of(:column),
-                                         :colspan => @query.depth_of(:row),
-                                         :class => 'top result' do
-              show_result @query
-            end
-          end
-          reverse_headers += '</tr>'
+  def header
+    header_content = ""
+    walker.headers do |list, first, first_in_col, last_in_col|
+      header_content += '<tr>'.html_safe if first_in_col
+      if first
+        header_content += content_tag :th, :rowspan => @query.depth_of(:column), :colspan => @query.depth_of(:row) do
+          ""
         end
       end
-      foot = content_tag :tfoot, reverse_headers.html_safe
-
-      # --------------------
-      # BODY
-      # --------------------
-      first = true
-      walker_body = ""
-      walker.body do |line|
-        if first
-          line.gsub!("class='normal", "class='top")
-          first = false
-        end
-        walker_body += content_tag :tr, :class => cycle("odd", "even") do
-          line.html_safe
+      list.each do |column|
+        header_content += content_tag :th, :colspan => column.final_number(:column), :class => column.final?(:column) ? "inner" : "" do
+          show_row column
         end
       end
-      body = content_tag :tbody, walker_body.html_safe
-      head + foot + body
+      if first
+        header_content += content_tag :th, :rowspan => @query.depth_of(:column), :colspan => @query.depth_of(:row) do
+          ""
+        end
+      end
+      header_content += '</tr>'.html_safe if last_in_col
     end
+    content_tag :thead, header_content.html_safe
+  end
 
-    if debug?
-      content += content_tag :pre do
-        debug_pre_content = "[ Query ]" +
-        @query.chain.each do |child|
-          "#{h child.class.inspect}, #{h child.type}"
+  def footer
+    reverse_headers = ""
+    walker.reverse_headers do |list, first, first_in_col, last_in_col|
+      if first_in_col
+        reverse_headers += '<tr>'
+        reverse_headers += content_tag :th, :rowspan => @query.depth_of(:column), :colspan => @query.depth_of(:row), :class => 'top' do
+          "&nbsp;".html_safe if first
         end
+      end
 
-        debug_pre_content += "[ RESULT ]"
-        @query.result.recursive_each_with_level do |level, result|
-          debug_pre_content += ">>> " * (level+1)
-          debug_pre_content += h(result.inspect)
-          debug_pre_content += "   " * (level+1)
-          debug_pre_content += h(result.type.inspect)
-          debug_pre_content += "   " * (level+1)
-          debug_pre_content += h(result.fields.inspect)
+      list.each do |column|
+        reverse_headers += content_tag :th, :colspan => column.final_number(:column), :class => (first ? "inner" : "") do
+          "#{show_result(column)}#{debug_fields(column)}"
         end
-        debug_pre_content += "[ HEADER STACK ]"
-        debug_pre_content += walker.header_stack.each do |l|
-          ">>> #{l.inspect}"
+      end
+      if last_in_col
+        if first
+          reverse_headers += content_tag :th,
+          :rowspan => @query.depth_of(:column),
+          :colspan => @query.depth_of(:row),
+          :class => 'top result' do
+            show_result @query
+          end
         end
+        reverse_headers += '</tr>'
       end
     end
-    content
+    content_tag :tfoot, reverse_headers.html_safe
+  end
+
+  def body
+    first = true
+    walker_body = ""
+    walker.body do |line|
+      if first
+        line.gsub!("class='normal", "class='top")
+        first = false
+      end
+      walker_body += content_tag :tr, :class => cycle("odd", "even") do
+        line.html_safe
+      end
+    end
+    content_tag :tbody, walker_body.html_safe
+  end
+
+  def debug_content
+    content_tag :pre do
+      debug_pre_content = "[ Query ]" +
+      @query.chain.each do |child|
+        "#{h child.class.inspect}, #{h child.type}"
+      end
+
+      debug_pre_content += "[ RESULT ]"
+      @query.result.recursive_each_with_level do |level, result|
+        debug_pre_content += ">>> " * (level+1)
+        debug_pre_content += h(result.inspect)
+        debug_pre_content += "   " * (level+1)
+        debug_pre_content += h(result.type.inspect)
+        debug_pre_content += "   " * (level+1)
+        debug_pre_content += h(result.fields.inspect)
+      end
+      debug_pre_content += "[ HEADER STACK ]"
+      debug_pre_content += walker.header_stack.each do |l|
+        ">>> #{l.inspect}"
+      end
+    end
   end
 end
 
