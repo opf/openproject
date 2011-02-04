ff2fdf16a1084f044b3961383af066ccd194d40d
Tim Felgentreff <timfelgentreff@gmail.com>
2011-02-04 12:01:25 +0100

    fix and cleanup in-place-editing

--- assets/javascripts/reporting/controls.js
+++ assets/javascripts/reporting/controls.js
@@ -5,46 +5,42 @@ Reporting.Controls = {
   query_name_editor: function (target_id) {
     var target = $(target_id);
     var isPublic = target.getAttribute("data-is_public") === "true";
-    var updateUrl = target.getAttribute("data-update_url");
+    var updateUrl = target.getAttribute("data-update-url");
     var translations = target.getAttribute("data-translations");
     if (translations.isJSON()) {
       translations = translations.evalJSON(true);
-    } else {
+    }
+    if (translations === undefined) {
       translations = {};
     }
+    if (translations.rename === undefined) {
+      translations.rename = 'ok';
+    }
+    if (translations.cancel === undefined) {
+      translations.cancel = 'cancel';
+    }
+    if (translations.saving === undefined) {
+      translations.saving = 'Saving...';
+    }
+    if (translations.loading === undefined) {
+      translations.loading = 'Loading...';
+    }
+    if (translations.clickToEdit === undefined) {
+      translations.loading = 'Click to edit';
+    }
 
-    Ajax.InPlaceEditor(target_id, updateUrl, {
+    var editor = new Ajax.InPlaceEditor(target_id, updateUrl, {
       callback: function (form, value) {
-        return  'query_is_public=' + (form.query_is_public.checked === true) + '&query_name=' + encodeURIComponent(value);
+        return  'query_name=' + encodeURIComponent(value);
       },
       okControl: 'button',
       cancelControl: 'button',
       externalControl: 'query-name-edit-button',
-      okText: translations.rename === undefined ? 'ok' : translations.rename,
-      cancelText: translations.cancel === undefined ? 'cancel' : translations.cancel,
-      savingText: translations.saving === undefined ? 'Saving...' : translations.saving,
-      loadingText: translations.loading === undefined ? 'Loading...' : translations.loading,
-      clickToEditText: translations.clickToEdit === undefined ? 'Click to edit' : translations.clickToEdit,
-      onFormCustomization: function (ipe, ipeForm) {
-        var label;
-        var checkbox;
-        var chk_id = 'rename_query_is_public';
-
-        checkbox = document.createElement('input');
-        checkbox.value = 1;
-        checkbox.type = 'checkbox';
-        checkbox.id = chk_id;
-        checkbox.name = 'query_is_public';
-        checkbox.checked = isPublic;
-
-        label = document.createElement('label');
-        label.id = 'in_place_save_is_public_question';
-        label.htmlFor = chk_id;
-        label.appendChild(document.createTextNode(translations.isPublic === undefined ? 'public?' : translations.isPublic));
-
-        ipeForm.insert(label);
-        ipeForm.insert(checkbox);
-      }
+      okText: translations.rename,
+      cancelText: translations.cancel,
+      savingText: translations.saving,
+      loadingText: translations.loading,
+      clickToEditText: translations.clickToEdit
     });
   },
 
@@ -78,7 +74,9 @@ Reporting.Controls = {
 };
 
 Reporting.onload(function () {
-  Reporting.Controls.query_name_editor('query_saved_name');
+  if ($('query_saved_name').getAttribute("data-update-url") !== null) {
+    Reporting.Controls.query_name_editor('query_saved_name');
+  }
   $("query-icon-delete").observe("click", Reporting.Controls.toggle_delete_form);
   $("query-icon-save-as").observe("click", Reporting.Controls.toggle_save_as_form);
   $("query-icon-save-as-cancel").observe("click", Reporting.Controls.toggle_save_as_form);
