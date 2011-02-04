4b7f66fcebdfea47a5359f41800d786df55ba81a
Tim Felgentreff <timfelgentreff@gmail.com>
2011-02-04 15:16:21 +0100

    slight fix for control buttons

--- lib/widget/controls/apply.rb
+++ lib/widget/controls/apply.rb
@@ -2,7 +2,7 @@ class Widget::Controls::Apply < Widget::Base
   def render
     link_to content_tag(:span, content_tag(:em, l(:button_apply))), {},
       :href => "#", :id => "query-icon-apply-button",
-      :class => "button apply",
+      :class => "button apply reporting_button",
       :"data-target" => url_for(:action => 'index', :set_filter => '1')
   end
 end
