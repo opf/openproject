e213d81737afafb02179a1bedc6f91b660afed0d
Tim Felgentreff <timfelgentreff@gmail.com>
2011-02-04 09:59:45 +0100

    minor cleanup

--- lib/widget/controls/save.rb
+++ lib/widget/controls/save.rb
@@ -1,13 +1,10 @@
 class Widget::Controls::Save < Widget::Base
   def render
-    unless @query.new_record?
-      link_to content_tag(:span, content_tag(:em, l(:button_save))), {},
-        :href => "#", :id => "query-breadcrumb-save",
-        :class => "breadcrumb_icon icon-save",
-        :title => l(:button_save),
-        :"data-target" => url_for(:action => 'update', :id => @query.id, :set_filter => '1')
-    else
-      ""
-    end
+    return "" if @query.new_record?
+    link_to content_tag(:span, content_tag(:em, l(:button_save))), {},
+      :href => "#", :id => "query-breadcrumb-save",
+      :class => "breadcrumb_icon icon-save",
+      :title => l(:button_save),
+      :"data-target" => url_for(:action => 'update', :id => @query.id, :set_filter => '1')
   end
 end
