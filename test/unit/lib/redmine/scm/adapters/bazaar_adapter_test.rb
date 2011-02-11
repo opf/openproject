6b6a44c83197749908604af45d2dc32317daa2a4
Toshi MARUYAMA <marutosijp2@yahoo.co.jp>
2011-02-11 15:31:11 +0000

    scm: bazaar: prepare version string unit lib test.
    git-svn-id: svn+ssh://rubyforge.org/var/svn/redmine/trunk@4807 e93f8b46-1217-0410-a6f0-8f06a7374b81

---
+++ test/unit/lib/redmine/scm/adapters/bazaar_adapter_test.rb
@@ -0,0 +1,26 @@
+require File.expand_path('../../../../../../test_helper', __FILE__)
+begin
+  require 'mocha'
+
+  class BazaarAdapterTest < ActiveSupport::TestCase
+
+    REPOSITORY_PATH = RAILS_ROOT.gsub(%r{config\/\.\.}, '') + '/tmp/test/bazaar_repository'
+    MODULE_NAME = 'test'
+
+    if File.directory?(REPOSITORY_PATH)  
+      def setup
+        @adapter = Redmine::Scm::Adapters::BazzarAdapter.new(MODULE_NAME, REPOSITORY_PATH)
+      end
+
+    else
+      puts "Bazaar test repository NOT FOUND. Skipping unit tests !!!"
+      def test_fake; assert true end
+    end
+  end
+
+rescue LoadError
+  class BazaarMochaFake < ActiveSupport::TestCase
+    def test_fake; assert(false, "Requires mocha to run those tests")  end
+  end
+end
+
