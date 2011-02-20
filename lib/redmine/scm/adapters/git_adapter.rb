899a35fc8dbe727a73c581e5fccbdbcb532811f2
Toshi MARUYAMA <marutosijp2@yahoo.co.jp>
2011-02-20 01:33:09 +0000

    scm: git: add --no-color option to run "git --version".
    git-svn-id: svn+ssh://rubyforge.org/var/svn/redmine/trunk@4885 e93f8b46-1217-0410-a6f0-8f06a7374b81

--- lib/redmine/scm/adapters/git_adapter.rb
+++ lib/redmine/scm/adapters/git_adapter.rb
@@ -49,7 +49,7 @@ module Redmine
           end
 
           def scm_version_from_command_line
-            shellout("#{sq_bin} --version") { |io| io.read }.to_s
+            shellout("#{sq_bin} --version --no-color") { |io| io.read }.to_s
           end
         end
 
