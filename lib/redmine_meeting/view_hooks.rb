0dd12d3a11275e78b9f1b940dcda822ac90da06b
Jens Ulferts <j.ulferts@finn.de>
2011-02-22 16:31:11 +0100

    requiring stylesheet only where it is really needed

--- lib/redmine_meeting/view_hooks.rb
+++
@@ -1,9 +0,0 @@
-module Plugin
-  module Meeting
-    class ViewHooks < Redmine::Hook::ViewListener
-      def view_layouts_base_html_head(context={})
-        stylesheet_link_tag 'redmine_meeting', :plugin => 'redmine_meeting'
-      end
-    end
-  end
-end
