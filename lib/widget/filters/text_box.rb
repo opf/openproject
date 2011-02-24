91e1f10c10ad3bfc6f37c66c9c5280d1a259de3e
Tim Felgentreff <timfelgentreff@gmail.com>
2011-02-24 12:10:25 +0100

    #18590 - use small width for number text boxes

--- lib/widget/filters/text_box.rb
+++ lib/widget/filters/text_box.rb
@@ -3,6 +3,7 @@ class Widget::Filters::TextBox < Widget::Filters::Base
     content_tag :td do
       content_tag :div, :id => "#{filter_class.underscore_name}_arg_1", :class => "filter_values" do
         text_field_tag("values[#{filter_class.underscore_name}]", "",
+            :size => "6",
             :class => "select-small",
             :id => "#{filter_class.underscore_name}_arg_1_val",
             :'data-filter-name' => filter_class.underscore_name)
