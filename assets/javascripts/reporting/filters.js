opf/openproject

commit 2137a99a9632f485cc6e84b4be1741a594d228ef

fix clear and filter restore JS via className since the class name has changed

assets/javascripts/reporting/filters.js
@@ -134,7 +134,7 @@ Reporting.Filters = {
     return $("filter_table").childElements().first().select('tr').select(function (tr) {
       return tr.visible() === true;
     }).collect(function (filter) {
-      return filter.className;
+      return filter.getAttribute("data-filter-name");
     });
   },
 

assets/javascripts/reporting/restore_query.js
@@ -66,7 +66,9 @@ Reporting.RestoreQuery = {
     // FIXME: rm_xxx values for filters have to be set after re-displaying them
     $$("tr[data-selected=true]").each(function (e) {
       var rm_box = e.select("input[id^=rm]").first();
-      rm_box.value = rm_box.getAttribute("data-filter-name");
+      var filter_name = e.getAttribute("data-filter-name");
+      rm_box.value = filter_name;
+      Reporting.Filters.select_option_enabled($("add_filter_select"), filter_name, false);
     });
   }
 };

lib/widget/filters.rb
@@ -34,7 +34,9 @@ class Widget::Filters < Widget::Base
   def render_filters
     active_filters = @query.filters.select { |f| f.class.display? }
     engine::Filter.all.collect do |filter|
-      opts = {:id => "tr_#{filter.underscore_name}", :class => "#{filter.underscore_name} filter"}
+      opts = {:id => "tr_#{filter.underscore_name}",
+              :class => "#{filter.underscore_name} filter",
+              :"data-filter-name" => filter.underscore_name }
       active_instance = active_filters.detect { |f| f.class == filter }
       if active_instance
         opts[:"data-selected"] = true

lib/widget/filters/remove_button.rb
@@ -3,8 +3,7 @@ class Widget::Filters::RemoveButton < Widget::Filters::Base
     content_tag :td, :width => "25px" do
       tag :input, :id => "rm_#{filter_class.underscore_name}",
         :name => "fields[]", :type => "button", :value => "",
-        :class => "icon filter_rem icon-filter-rem",
-        :"data-filter-name" => filter_class.underscore_name
+        :class => "icon filter_rem icon-filter-rem"
     end
   end
 end

commit 71bbe2f694d132306eabc76d6b1a3f62a8406df4

fix regression introduced with 2137a99

assets/javascripts/reporting/filters.js
@@ -151,7 +151,7 @@ Reporting.onload(function () {
   });
   $$(".filter_rem").each(function (e) {
     e.observe("click", function () {
-      var filter_name = this.getAttribute("data-filter-name");
+      var filter_name = this.up('tr').getAttribute("data-filter-name");
       Reporting.Filters.remove_filter(filter_name);
     });
   });