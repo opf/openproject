e04513ed238511f4f98afc73318681f11e0fe0ed
Eric Davis <edavis@littlestreamsoftware.com>
2011-02-24 14:32:11 -0800

    Reset version to ChiliProject's standards
    * Third version is called Patch
    * Branches are stable, master, or unstable

--- lib/redmine/version.rb
+++ lib/redmine/version.rb
@@ -4,13 +4,16 @@ module Redmine
   module VERSION #:nodoc:
     MAJOR = 1
     MINOR = 1
-    TINY  = 1
+    PATCH = 0
+    TINY  = PATCH # Redmine compat
     
-    # Branch values:
-    # * official release: nil
-    # * stable branch:    stable
-    # * trunk:            devel
-    BRANCH = 'devel'
+    # Branches:
+    # * stable - released version
+    # * master - stable development
+    # * unstable - future development
+    #
+    # Source: https://www.chiliproject.org/projects/chiliproject/wiki/ChiliProject_Repository
+    BRANCH = 'master'
 
     def self.revision
       revision = nil
@@ -34,7 +37,7 @@ module Redmine
     end
 
     REVISION = self.revision
-    ARRAY = [MAJOR, MINOR, TINY, BRANCH, REVISION].compact
+    ARRAY = [MAJOR, MINOR, PATCH, BRANCH, REVISION].compact
     STRING = ARRAY.join('.')
     
     def self.to_a; ARRAY end
