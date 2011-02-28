262433507858b3df2ce529977bdab5e380e990b0
Jens Ulferts <j.ulferts@finn.de>
2011-02-28 15:13:41 +0100

    i18n typo is fixed in chili 1.1.0 - this is now correctly tested for

--- spec/lib/loader_spec.rb
+++ spec/lib/loader_spec.rb
@@ -12,7 +12,13 @@ describe Redmine::DefaultData::Loader do
 
     #describes only the results of load in the db
     it {Role.find_by_name(I18n.t(:default_role_manager)).attributes["type"].should eql "Role"}
-    it {Role.find_by_name(I18n.t(:default_role_developper)).attributes["type"].should eql "Role"} #[sic]
+
+    if Redmine::VERSION::MAJOR < 1
+      it {Role.find_by_name(I18n.t(:default_role_developper)).attributes["type"].should eql "Role"} #[sic]
+    else
+      it {Role.find_by_name(I18n.t(:default_role_developer)).attributes["type"].should eql "Role"} #[sic]
+    end
+
     it {Role.find_by_name(I18n.t(:default_role_reporter)).attributes["type"].should eql "Role"}
   end
 
