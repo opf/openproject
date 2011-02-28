f2e125ff3aee983b453ce2c7434dd25842699fbd
Wieland Lindenthal <w.lindenthal@finn.de>
2011-02-28 11:27:14 +0100

    Added button styling for delete link of reports

--- lib/widget/controls/delete.rb
+++ lib/widget/controls/delete.rb
@@ -8,7 +8,12 @@ class Widget::Controls::Delete < Widget::Base
     popup = content_tag :div, :id => "delete_form", :class => "button_form" do
       question = content_tag :p, l(:label_really_delete_question)
       options = content_tag :p do
-        opt1 = link_to l(:button_delete), url_for(:action => 'delete', :id => @query.id)
+        delete_button = content_tag :span do
+          span = content_tag :em do
+            l(:button_delete)
+          end
+        end
+        opt1 =  link_to delete_button, url_for(:action => 'delete', :id => @query.id), :class => "button apply"
         opt2 = link_to l(:button_cancel), "#", :id => "query-icon-delete-cancel", :class => 'icon icon-cancel'
         opt1 + opt2
       end
