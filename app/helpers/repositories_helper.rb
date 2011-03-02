ce16f4a27873fa1c8d12bbe009ad2540fd3fac65
Toshi MARUYAMA <marutosijp2@yahoo.co.jp>
2011-03-02 07:11:00 +0000

    scm: filesystem: add note "Default: UTF-8" in path encoding setting (#2274).
    git-svn-id: svn+ssh://rubyforge.org/var/svn/redmine/trunk@4988 e93f8b46-1217-0410-a6f0-8f06a7374b81

--- app/helpers/repositories_helper.rb
+++ app/helpers/repositories_helper.rb
@@ -229,7 +229,8 @@ module RepositoriesHelper
   def filesystem_field_tags(form, repository)
     content_tag('p', form.text_field(:url, :label => :label_filesystem_path, :size => 60, :required => true, :disabled => (repository && !repository.root_url.blank?))) +
     content_tag('p', form.select(:path_encoding, [nil] + Setting::ENCODINGS,
-                                 :label => 'Path encoding'))
+                                 :label => 'Path encoding') +
+                                 '<br />Default: UTF-8')
 
   end
 end
