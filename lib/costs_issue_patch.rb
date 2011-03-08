3498e669e79e3eaaf2dab8f40d8229a448b18503
Jens Ulferts <j.ulferts@finn.de>
2011-03-08 12:15:02 +0100

    adding cost_object_id to safe issue attributes

--- lib/costs_issue_patch.rb
+++ lib/costs_issue_patch.rb
@@ -6,34 +6,40 @@ module CostsIssuePatch
 
     base.send(:include, InstanceMethods)
 
-    # Same as typing in the class 
+    # Same as typing in the class
     base.class_eval do
       unloadable
-      
+
       belongs_to :cost_object
       has_many :cost_entries, :dependent => :delete_all
 
       # disabled for now, implements part of ticket blocking
       alias_method_chain :validate, :cost_object
-      
+
       def spent_hours
         # overwritten method
         @spent_hours ||= self.time_entries.visible(User.current).sum(:hours) || 0
       end
+
+      if Redmine::VERSION::MAJOR >= 1
+      #chili 1.1.0 uses safe_attributes. Hence we must proclaim cost_object safe
+        safe_attributes "cost_object_id",
+                        :if => lambda {|issue, user| issue.new_record? || user.allowed_to?(:edit_issues, issue.project) }
+      end
     end
   end
-  
+
   module ClassMethods
-    
+
   end
-  
+
   module InstanceMethods
     def validate_with_cost_object
       if cost_object_id_changed?
         unless (cost_object_id.blank? || project.cost_object_ids.include?(cost_object_id))
           errors.add :cost_object_id, :activerecord_error_invalid
         end
-        
+
         ## disabled for now, implements part of ticket blocking
         # if cost_object_id_was.nil?
         #   # formerly unassigned ticket
@@ -43,10 +49,10 @@ module CostsIssuePatch
         #   errors.add :cost_object_id, :activerecord_error_invalid if old_cost_object.blocked?
         # end
       end
-      
+
       validate_without_cost_object
     end
-    
+
     def material_costs
       @material_costs ||= cost_entries.visible_costs(User.current, self.project).sum("CASE
         WHEN #{CostEntry.table_name}.overridden_costs IS NULL THEN
@@ -54,7 +60,7 @@ module CostsIssuePatch
         ELSE
           #{CostEntry.table_name}.overridden_costs END").to_f
     end
-    
+
     def labor_costs
       @labor_costs ||= time_entries.visible_costs(User.current, self.project).sum("CASE
         WHEN #{TimeEntry.table_name}.overridden_costs IS NULL THEN
@@ -62,19 +68,19 @@ module CostsIssuePatch
         ELSE
           #{TimeEntry.table_name}.overridden_costs END").to_f
     end
-    
+
     def overall_costs
       labor_costs + material_costs
     end
-    
-    # Wraps the association to get the Cost Object subject.  Needed for the 
+
+    # Wraps the association to get the Cost Object subject.  Needed for the
     # Query and filtering
     def cost_object_subject
       unless self.cost_object.nil?
         return self.cost_object.subject
       end
     end
-    
+
     def update_costs!
       # This methods ist referenced from some migrations but does nothing
       # anymore.
