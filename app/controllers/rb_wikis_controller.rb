92997a5f71756e6fee14bf5d82fda3478fce3b9f
Sandro Munda <smu@pcsol.be>
2011-03-11 10:01:44 +0100

    Route fixed wiki link

--- app/controllers/rb_wikis_controller.rb
+++ app/controllers/rb_wikis_controller.rb
@@ -5,13 +5,13 @@ class RbWikisController < RbApplicationController
   # WikiController#index tak care of autorization
   # NOTE: this method does create a template page when called.
   def show
-    redirect_to :controller => 'wiki', :action => 'index', :id => @project.id, :page => @sprint.wiki_page
+    redirect_to :controller => 'wiki', :action => 'index', :project_id => @project.id, :id => @sprint.wiki_page
   end
 
   # NOTE: This method is public (see init.rb). We will let Redmine core's 
   # WikiController#index tak care of autorization
   # NOTE: this method does create a template page when called.
   def edit
-    redirect_to :controller => 'wiki', :action => 'edit', :id => @project.id, :page => @sprint.wiki_page
+    redirect_to :controller => 'wiki', :action => 'edit', :project_id => @project.id, :id => @sprint.wiki_page
   end
 end
