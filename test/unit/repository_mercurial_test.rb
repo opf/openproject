6655727401858a115b5e34c14dfca2e98f6951d9
Toshi MARUYAMA <marutosijp2@yahoo.co.jp>
2011-03-14 08:15:00 +0000

    scm: mercurial: prepare tests of named branch and tag in unit model test (#1981, #7246).
    git-svn-id: svn+ssh://rubyforge.org/var/svn/redmine/trunk@5117 e93f8b46-1217-0410-a6f0-8f06a7374b81

--- test/unit/repository_mercurial_test.rb
+++ test/unit/repository_mercurial_test.rb
@@ -34,8 +34,14 @@ class RepositoryMercurialTest < ActiveSupport::TestCase
                       )
     assert @repository
     @char_1        = CHAR_1_HEX.dup
+    @tag_char_1    = "tag-#{CHAR_1_HEX}-00"
+    @branch_char_0 = "branch-#{CHAR_1_HEX}-00"
+    @branch_char_1 = "branch-#{CHAR_1_HEX}-01"
     if @char_1.respond_to?(:force_encoding)
       @char_1.force_encoding('UTF-8')
+      @tag_char_1.force_encoding('UTF-8')
+      @branch_char_0.force_encoding('UTF-8')
+      @branch_char_1.force_encoding('UTF-8')
     end
   end
 
