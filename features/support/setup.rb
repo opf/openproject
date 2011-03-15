fee9c4d15a173daa5b4a2ed69687c08b1f8ba9e3
Stephan Eckardt <s.eckardt@finn.de>
2011-03-15 18:26:02 +0100

    also load users from fixtures, dev_tools were changed accordingly

--- features/support/setup.rb
+++ features/support/setup.rb
@@ -3,7 +3,5 @@ Before do
   Fixtures.reset_cache
   fixtures_folder = File.join(RAILS_ROOT, 'test', 'fixtures')
   fixtures = Dir[File.join(fixtures_folder, '*.yml')].map {|f| File.basename(f, '.yml') }
-  # Users are created using factory girl in redmine_cucumber
-  fixtures -= ['users']
   Fixtures.create_fixtures(fixtures_folder, fixtures)
 end
