d84519843025d9fd9ba384dce1bac5d7dbc4496a
Gregor Schmidt <ruby@schmidtwisser.de>
2011-03-21 16:47:26 +0100

    making jslint happy
    missing vars, multiple vars, white space, missing semicolons

--- assets/javascripts/story.js
+++ assets/javascripts/story.js
@@ -1,75 +1,81 @@
 /**************************************
   STORY
 ***************************************/
+/*jslint eqeqeq: false, indent: 2, onevar: false*/
+/*globals $, RB, document*/
 RB.Story = RB.Object.create(RB.Issue, RB.EditableInplace, {
-  initialize: function(el){
+  initialize: function (el) {
     var j;  // This ensures that we use a local 'j' variable, not a global one.
     var self = this;
-    
+
     this.$ = j = $(el);
     this.el = el;
-    
+
     // Associate this object with the element for later retrieval
     j.data('this', this);
 
     j.find(".editable").live('mouseup', this.handleClick);
   },
 
-  beforeSave: function(){
+  beforeSave: function () {
     // Do nothing
   },
-  
-  editDialogTitle: function(){
+
+  editDialogTitle: function () {
     return "Story #" + this.getID();
   },
 
-  editorDisplayed: function(editor){
+  editorDisplayed: function (editor) {
     // editor.dialog("option", "position", "center");
   },
-  
-  getPoints: function(){
-    points = parseInt( this.$.find('.story_points').first().text() );
-    return ( isNaN(points) ? 0 : points );
+
+  getPoints: function () {
+    var points = parseInt(this.$.find('.story_points').first().text(), 10);
+    return isNaN(points) ? 0 : points;
   },
 
-  getType: function(){
+  getType: function () {
     return "Story";
   },
 
-  markIfClosed: function(){
+  markIfClosed: function () {
     // Do nothing
   },
-  
-  newDialogTitle: function(){
+
+  newDialogTitle: function () {
     return "New Story";
   },
 
-  saveDirectives: function(){
+  saveDirectives: function () {
+    var url;
     var j = this.$;
     var prev = this.$.prev();
-    var sprint_id = this.$.parents('.backlog').data('this').isSprintBacklog() ? 
+    var sprint_id = this.$.parents('.backlog').data('this').isSprintBacklog() ?
                     this.$.parents('.backlog').data('this').getSprint().data('this').getID() : '';
-        
-    var data = "prev=" + (prev.length==1 ? this.$.prev().data('this').getID() : '') +
+
+    var data = "prev=" + (prev.length == 1 ? this.$.prev().data('this').getID() : '') +
                "&fixed_version_id=" + sprint_id;
-    
-    if(j.find('.editor').length > 0) data += "&" + j.find('.editor').serialize();
-    
-    if( this.isNew() ){
-      var url = RB.urlFor( 'create_story' );
-    } else {
-      var url = RB.urlFor( 'update_story', { id: this.getID() } );
-      data += "&_method=put"
+
+    if (j.find('.editor').length > 0) {
+      data += "&" + j.find('.editor').serialize();
     }
-    
+
+
+    if (this.isNew()) {
+      url = RB.urlFor('create_story');
+    } else {
+      url = RB.urlFor('update_story', {id: this.getID()});
+      data += "&_method=put";
+    }
+
     return {
       url: url,
       data: data
-    }
+    };
   },
 
-  beforeSaveDragResult: function(){
+  beforeSaveDragResult: function () {
     // Do nothing
   }
 });
-  
+
