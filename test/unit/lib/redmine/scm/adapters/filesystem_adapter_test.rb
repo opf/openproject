b06f24e932dff2f6ec73621597d06fac69924c14
Toshi MARUYAMA <marutosijp2@yahoo.co.jp>
2011-03-22 05:46:43 +0000

    scm: filesystem: code clean up unit adapter test.
    git-svn-id: svn+ssh://rubyforge.org/var/svn/redmine/trunk@5191 e93f8b46-1217-0410-a6f0-8f06a7374b81

--- test/unit/lib/redmine/scm/adapters/filesystem_adapter_test.rb
+++ test/unit/lib/redmine/scm/adapters/filesystem_adapter_test.rb
@@ -1,16 +1,15 @@
 
 require File.expand_path('../../../../../../test_helper', __FILE__)
 
-
 class FilesystemAdapterTest < ActiveSupport::TestCase
-  
+
   REPOSITORY_PATH = RAILS_ROOT.gsub(%r{config\/\.\.}, '') + '/tmp/test/filesystem_repository'  
-  
+
   if File.directory?(REPOSITORY_PATH)    
     def setup
       @adapter = Redmine::Scm::Adapters::FilesystemAdapter.new(REPOSITORY_PATH)
     end
-    
+
     def test_entries
       assert_equal 2, @adapter.entries.size
       assert_equal ["dir", "test"], @adapter.entries.collect(&:name)
@@ -21,22 +20,19 @@ class FilesystemAdapterTest < ActiveSupport::TestCase
       end
       # If y try to use "..", the path is ignored
       ["/../","dir/../", "..", "../", "/..", "dir/.."].each do |path|
-        assert_equal ["dir", "test"], @adapter.entries(path).collect(&:name), ".. must be ignored in path argument"
+        assert_equal ["dir", "test"], @adapter.entries(path).collect(&:name),
+             ".. must be ignored in path argument"
       end
     end
-    
+
     def test_cat
       assert_equal "TEST CAT\n", @adapter.cat("test")
       assert_equal "TEST CAT\n", @adapter.cat("/test")
       # Revision number is ignored
       assert_equal "TEST CAT\n", @adapter.cat("/test", 1)
     end
-    
   else
     puts "Filesystem test repository NOT FOUND. Skipping unit tests !!! See doc/RUNNING_TESTS."
     def test_fake; assert true end
   end
-  
 end
-
-
