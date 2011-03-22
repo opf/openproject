476106ac0ff444cd7d6ce575c4d7a48dcba6b32b
Toshi MARUYAMA <marutosijp2@yahoo.co.jp>
2011-03-22 05:47:24 +0000

    scm: filesystem: code clean up unit model test.
    git-svn-id: svn+ssh://rubyforge.org/var/svn/redmine/trunk@5192 e93f8b46-1217-0410-a6f0-8f06a7374b81

--- test/unit/repository_filesystem_test.rb
+++ test/unit/repository_filesystem_test.rb
@@ -19,25 +19,26 @@ require File.expand_path('../../test_helper', __FILE__)
 
 class RepositoryFilesystemTest < ActiveSupport::TestCase
   fixtures :projects
-  
+
   # No '..' in the repository path
   REPOSITORY_PATH = RAILS_ROOT.gsub(%r{config\/\.\.}, '') + '/tmp/test/filesystem_repository'
-  
+
   def setup
     @project = Project.find(1)
     Setting.enabled_scm << 'Filesystem' unless Setting.enabled_scm.include?('Filesystem')
-    assert @repository = Repository::Filesystem.create(:project => @project, :url => REPOSITORY_PATH)
+    assert @repository = Repository::Filesystem.create(
+                            :project => @project, :url => REPOSITORY_PATH)
   end
-  
+
   if File.directory?(REPOSITORY_PATH)  
     def test_fetch_changesets
       @repository.fetch_changesets
       @repository.reload
-      
+
       assert_equal 0, @repository.changesets.count
       assert_equal 0, @repository.changes.count
     end
-        
+
     def test_entries
       assert_equal 2, @repository.entries("", 2).size
       assert_equal 2, @repository.entries("dir", 3).size
