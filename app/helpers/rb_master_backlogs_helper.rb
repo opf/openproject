1567c48bc834433b8ad2556199ad23582f611c8f
Gregor Schmidt <ruby@schmidtwisser.de>
2011-03-22 18:17:26 +0100

    refactoring master_backlogs helper

--- app/helpers/rb_master_backlogs_helper.rb
+++ app/helpers/rb_master_backlogs_helper.rb
@@ -4,88 +4,92 @@ module RbMasterBacklogsHelper
   include Redmine::I18n
 
   def render_backlog_menu(backlog)
-    items = backlog_menu_items_for(backlog)
-    is_sprint = backlog.sprint_backlog?
+    content_tag(:div, :class => 'menu') do
+      [
+        content_tag(:div, '', :class => "icon ui-icon ui-icon-carat-1-s"),
+        content_tag(:ul, :class => 'items') do
 
-    html = %{
-      <div class="menu">
-        <div class="icon ui-icon ui-icon-carat-1-s"></div>
-        <ul class="items">
-    }
-    items.each do |item|
-      item[:condition] = true unless item.has_key?(:condition)
-      if item[:condition] && ( (is_sprint && item[:for] == :sprint) ||
-                               (!is_sprint && item[:for] == :product) ||
-                               (item[:for] == :both) )
-        html += %{ <li class="item">#{item[:item]}</li> }
-      end
+          backlog_menu_items_for(backlog).map do |item|
+            content_tag(:li, item, :class => 'item')
+          end
+
+        end
+      ]
     end
-    html += %{
-        </ul>
-      </div>
-    }
   end
 
   def backlog_menu_items_for(backlog)
+    items = common_backlog_menu_items_for(backlog)
+
+    if backlog.sprint_backlog?
+      items += sprint_backlog_menu_items_for(backlog)
+    else
+      items += owner_backlog_menu_items_for(backlog)
+    end
+
+    items
+  end
+
+  def common_backlog_menu_items_for(backlog)
     [
-      {
-        :item => "<a href='#' class='add_new_story'>New Story</a>",
-        :for  => :both
-      },
-      {
-        :item => link_to(l(:label_task_board), {
-                           :controller => 'rb_taskboards',
-                           :action => 'show',
-                           :sprint_id => backlog.sprint }),
-        :for => :sprint
-      },
-      {
-        :item => "<a href='#' class='show_burndown_chart'>Burndown chart</a>",
-        :for  => :sprint,
-        :condition => backlog.sprint && backlog.sprint.has_burndown
-      },
-      {
-        :item => link_to(l(:label_stories_tasks), {
-                           :controller => 'rb_queries',
-                           :action => 'show',
-                           :project_id => @project,
-                           :sprint_id => backlog.sprint }),
-        :for => :sprint
-      },
-      {
-        :item => link_to(l(:label_stories), {
-                           :controller => 'rb_queries',
-                           :action => 'show',
-                           :project_id => @project }),
-        :for => :product
-      },
-      {
-        :item => link_to(l(:label_sprint_cards), {
-                           :controller => 'rb_stories',
-                           :action => 'index',
-                           :project_id => @project,
-                           :sprint_id => backlog.sprint,
-                           :format => :pdf }),
-        :for => :sprint,
-        :condition => Cards::TaskboardCards.selected_label
-      },
-      {
-        :item => link_to(l(:label_product_cards), {
-                           :controller => 'rb_stories',
-                           :action => 'index',
-                           :project_id => @project,
-                           :format => :pdf }),
-        :for => :product
-      },
-      {
-        :item => link_to(l(:label_wiki), {
-                           :controller => 'rb_wikis',
-                           :action => 'edit',
-                           :project_id => @project.id,
-                           :sprint_id => backlog.sprint }),
-        :for => :sprint,
-        :condition => @project.module_enabled?("wiki")
-      }
+      content_tag(:a, l('backlogs.add_new_story'), :href => '#', :class => 'add_new_story')
     ]
+  end
+
+  def sprint_backlog_menu_items_for(backlog)
+    items = []
+
+    items << link_to(l(:label_task_board),
+                     :controller => 'rb_taskboards',
+                     :action => 'show',
+                     :sprint_id => backlog.sprint)
+
+    if backlog.sprint_backlog? and backlog.sprint.has_burndown
+      items << content_tag(:a, l('backlogs.show_burndown_chart'), :href => '#', :class => 'show_burndown_chart')
+    end
+
+    items << link_to(l(:label_stories_tasks),
+                     :controller => 'rb_queries',
+                     :action => 'show',
+                     :project_id => @project,
+                     :sprint_id => backlog.sprint)
+
+    if Cards::TaskboardCards.selected_label.present?
+      items << link_to(l(:label_sprint_cards),
+                       :controller => 'rb_stories',
+                       :action => 'index',
+                       :project_id => @project,
+                       :sprint_id => backlog.sprint,
+                       :format => :pdf)
+    end
+
+    if @project.module_enabled? "wiki"
+      items << link_to(l(:label_wiki),
+                       :controller => 'rb_wikis',
+                       :action => 'edit',
+                       :project_id => @project.id,
+                       :sprint_id => backlog.sprint)
+    end
+
+    items
+  end
+
+  def owner_backlog_menu_items_for(backlog)
+    items = []
+
+    items << link_to(l(:label_stories),
+                     :controller => 'rb_queries',
+                     :action => 'show',
+                     :project_id => @project)
+
+    if Cards::TaskboardCards.selected_label.present?
+      items << link_to(l(:label_product_cards),
+                       :controller => 'rb_stories',
+                       :action => 'index',
+                       :project_id => @project,
+                       :format => :pdf)
+    end
+
+    items
   end
 end
