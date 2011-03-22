939c42451226d84e16877a4875bad2d2fab50fe0
Markus Kahl <machisuji@googlemail.com>
2011-03-22 16:08:21 +0100

    Tried to fix selection.

--- lib/widget/filters/multi_choice.rb
+++ lib/widget/filters/multi_choice.rb
@@ -4,10 +4,12 @@ class Widget::Filters::MultiChoice < Widget::Filters::Base
     content_tag :td do
       content_tag :div, :id => "#{filter_class.underscore_name}_arg_1", :class => "filter_values" do
         choices = filter_class.available_values.each_with_index.map do |(label, value), i|
-          radio_button = tag :input,
-            :type => "radio", :name => "#{filter_class.underscore_name}_arg_1_val",
-            :id => "#{filter_class.underscore_name}_radio_option_#{i}",
-            :value => value
+          opts = {  :type => "radio",
+                    :name => "#{filter_class.underscore_name}_arg_1_val",
+                    :id => "#{filter_class.underscore_name}_radio_option_#{i}",
+                    :value => value }
+          opts[:checked] = "checked" if filter.values.first.first == label
+          radio_button = tag :input, opts
           content_tag :label, radio_button + label,
             :for => "#{filter_class.underscore_name}_radio_option_#{i}",
             :'data-filter-name' => filter_class.underscore_name,
