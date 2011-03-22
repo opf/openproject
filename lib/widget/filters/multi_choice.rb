1b48d66392ed694f31bcf72585b4e09aa93e3b89
Markus Kahl <machisuji@googlemail.com>
2011-03-22 14:40:50 +0100

    adjusted indentation

--- lib/widget/filters/multi_choice.rb
+++ lib/widget/filters/multi_choice.rb
@@ -5,13 +5,13 @@ class Widget::Filters::MultiChoice < Widget::Filters::Base
       content_tag :div, :id => "#{filter_class.underscore_name}_arg_1", :class => "filter_values" do
         choices = filter_class.available_values.each_with_index.map do |(label, value), i|
           radio_button = tag :input,
-              :type => "radio", :name => "#{filter_class.underscore_name}_arg_1_val",
-              :id => "#{filter_class.underscore_name}_radio_option_#{i}",
-              :class => "#{filter_class.underscore_name}_radio_option",
-              :value => value
+            :type => "radio", :name => "#{filter_class.underscore_name}_arg_1_val",
+            :id => "#{filter_class.underscore_name}_radio_option_#{i}",
+            :class => "#{filter_class.underscore_name}_radio_option",
+            :value => value
           content_tag :label, radio_button + label,
-              :for => "#{filter_class.underscore_name}_radio_option_#{i}",
-              :'data-filter-name' => filter_class.underscore_name
+            :for => "#{filter_class.underscore_name}_radio_option_#{i}",
+            :'data-filter-name' => filter_class.underscore_name
         end
         choices.join(content_tag(:br)).html_safe
       end
