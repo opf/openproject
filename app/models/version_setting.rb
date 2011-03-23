39b72338387eacb82dc741c8a1df0192762cfdf8
Gregor Schmidt <ruby@schmidtwisser.de>
2011-03-23 18:41:49 +0100

    Adding helper methods to version_setting to limit coupling
    ref #2891

--- app/models/version_setting.rb
+++ app/models/version_setting.rb
@@ -6,4 +6,27 @@ class VersionSetting < ActiveRecord::Base
   DISPLAY_LEFT = 2
   DISPLAY_RIGHT = 3
 
+  def display_right?
+    self.display == DISPLAY_RIGHT
+  end
+
+  def display_right!
+    self.display = DISPLAY_RIGHT
+  end
+
+  def display_left?
+    self.display == DISPLAY_LEFT
+  end
+
+  def display_left!
+    self.display = DISPLAY_LEFT
+  end
+
+  def display_none?
+    self.display == DISPLAY_NONE
+  end
+
+  def display_none!
+    self.display = DISPLAY_NONE
+  end
 end
