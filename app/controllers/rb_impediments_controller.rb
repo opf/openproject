fc9c64d3758f848a65a4f3df4ff287de9824e43f
Jens Ulferts <j.ulferts@finn.de>
2011-03-24 12:32:25 +0100

    #3168: updating of impeding task by controller now uses impediment option

--- app/controllers/rb_impediments_controller.rb
+++ app/controllers/rb_impediments_controller.rb
@@ -8,7 +8,7 @@ class RbImpedimentsController < RbApplicationController
     result = @impediment.errors.length
     status = (result == 0 ? 200 : 400)
     @include_meta = true
-    
+
     respond_to do |format|
       format.html { render :partial => "impediment", :object => @impediment, :status => status }
     end
@@ -16,10 +16,10 @@ class RbImpedimentsController < RbApplicationController
 
   def update
     @impediment = Task.find_by_id(params[:id])
-    result = @impediment.update_with_relationships(params)
+    result = @impediment.update_with_relationships(params, true)
     status = (result ? 200 : 400)
     @include_meta = true
-    
+
     respond_to do |format|
       format.html { render :partial => "impediment", :object => @impediment, :status => status }
     end
