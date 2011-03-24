7d8287428fb059176883174d7c487191624dd95a
Gregor Schmidt <ruby@schmidtwisser.de>
2011-03-24 15:06:00 +0100

    Adding Beta badge to Backlogs module and menu
    ref #3146

--- init.rb
+++ init.rb
@@ -91,8 +91,8 @@ Redmine::Plugin.register :redmine_backlogs do
   menu :project_menu,
        :backlogs,
        {:controller => :rb_master_backlogs, :action => :show},
-       :caption => :label_backlogs,
        :after => :issues,
+       :caption => :project_module_backlogs,
        :param => :project_id
   menu :application_menu,
        :backlogs,
