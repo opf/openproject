b6bacddf377208546c604e59ee6c133de4fde03e
Markus Kahl <machisuji@googlemail.com>
2011-03-24 12:34:40 +0100

    Fixed radio name for the Alumni Filer. Enclosed radio button in div which represents 'arg_1_val'.

--- lib/widget/filters/multi_choice.rb
+++ lib/widget/filters/multi_choice.rb
@@ -1,21 +1,25 @@
 class Widget::Filters::MultiChoice < Widget::Filters::Base
 
   def render
+    filterName = filter_class.underscore_name
     content_tag :td do
-      content_tag :div, :id => "#{filter_class.underscore_name}_arg_1", :class => "filter_values" do
+      content_tag :div, :id => "#{filterName}_arg_1", :class => "filter_values" do
         choices = filter_class.available_values.each_with_index.map do |(label, value), i|
-          opts = {  :type => "radio",
-                    :name => "#{filter_class.underscore_name}_arg_1_val",
-                    :id => "#{filter_class.underscore_name}_radio_option_#{i}",
-                    :value => value.to_json }
-          opts[:checked] = "checked" if filter.values.first == value
+          opts = {
+            :type => "radio",
+            :name => "values[#{filterName}][]",
+            :id => "#{filterName}_radio_option_#{i}",
+            :value => value
+          }
+          opts[:checked] = "checked" if filter.values == value
           radio_button = tag :input, opts
           content_tag :label, radio_button + label,
-            :for => "#{filter_class.underscore_name}_radio_option_#{i}",
+            :for => "#{filterName}_radio_option_#{i}",
             :'data-filter-name' => filter_class.underscore_name,
-            :class => "#{filter_class.underscore_name}_radio_option filter_radio_option"
+            :class => "#{filterName}_radio_option filter_radio_option"
         end
-        choices.join.html_safe
+        content_tag :div, choices.join.html_safe,
+          :id => "#{filter_class.underscore_name}_arg_1_val"
       end
     end
   end
