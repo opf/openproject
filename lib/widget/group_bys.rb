e0db7e7930864d14e0bfda2611045ab6bda1c721
Philipp Tessenow <philipp.tessenow@student.hpi.uni-potsdam.de>
2011-03-26 13:30:16 +0100

    work on the group_by rendering, groups are sorted and put in optgroups now, not selectable group are not printed in html-output anymore

--- lib/widget/group_bys.rb
+++ lib/widget/group_bys.rb
@@ -1,29 +1,43 @@
 class Widget::GroupBys < Widget::Base
   extend ProactiveAutoloader
 
-  def render_group(type)
-    content_tag :div, :id => "group_by_#{type}", :class => 'drag_target drag_container' do
-      out = ''
-      #TODO render existing group_bys of type <type>
-      out += content_tag :select, :id => "add_group_by_#{type}", :class => 'select-small' do
-        options  = tag :option, :value => ''
-        engine::GroupBy.all.each do |group_by_class|
-          options += content_tag :option, :value => group_by_class.underscore_name, :'data-label' => "#{l(group_by_class.label)}" do
-            l(group_by_class.label)
-          end
-        end
-        options
+  def render_options(group_by_ary)
+    group_by_ary.sort_by do |group_by|
+      l(group_by.label)
+    end.collect do |group_by|
+      next unless group_by.selectable?
+      content_tag :option, :value => group_by.underscore_name, :'data-label' => "#{l(group_by.label)}" do
+        l(group_by.label)
       end
-      out.html_safe
+    end.join.html_safe
+  end
+
+  def render_group(type, initially_selected)
+    initially_selected = initially_selected.map { |group_by| group_by.class.underscore_name }
+    content_tag :div,
+        :id => "group_by_#{type}",
+        :class => 'drag_target drag_container',
+        :'data-initially-selected' => initially_selected.to_json.gsub('"', "'") do
+      content_tag :select, :id => "add_group_by_#{type}", :class => 'select-small' do
+        content = tag :option, :value => ''
+        content += engine::GroupBy.all_grouped.sort_by do |label, group_by_ary|
+          l(label)
+        end.collect do |label, group_by_ary|
+          content_tag :optgroup, :label => l(label) do
+            render_options group_by_ary
+          end
+        end.join.html_safe
+        content
+      end.html_safe
     end
   end
 
   def render
     content_tag :div, :id => 'group_by_area' do
       out  = l(:label_columns)
-      out += render_group 'columns'
+      out += render_group 'columns', @query.group_bys(:column)
       out += l(:label_rows)
-      out += render_group 'rows'
+      out += render_group 'rows', @query.group_bys(:row)
       out.html_safe
     end
   end
