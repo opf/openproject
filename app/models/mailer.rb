b1c4f0fc4f1ab843661c8dab02b7ac61f7cb6796
Jean-Philippe Lang <jp_lang@yahoo.fr>
2011-03-27 15:12:38 +0000

    Do not send reminders to locked users (#5773).
    git-svn-id: svn+ssh://rubyforge.org/var/svn/redmine/trunk@5225 e93f8b46-1217-0410-a6f0-8f06a7374b81

--- app/models/mailer.rb
+++ app/models/mailer.rb
@@ -359,7 +359,7 @@ class Mailer < ActionMailer::Base
                                           :conditions => s.conditions
                                     ).group_by(&:assigned_to)
     issues_by_assignee.each do |assignee, issues|
-      deliver_reminder(assignee, issues, days) unless assignee.nil?
+      deliver_reminder(assignee, issues, days) if assignee && assignee.active?
     end
   end
   
