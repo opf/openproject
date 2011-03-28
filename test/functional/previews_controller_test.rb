e06b581595a1e4b556a516f28597a6d566a35c6e
Jean-Philippe Lang <jp_lang@yahoo.fr>
2011-03-28 21:45:30 +0000

    Adds support for preview when editing an issue note (#5520).
    git-svn-id: svn+ssh://rubyforge.org/var/svn/redmine/trunk@5246 e93f8b46-1217-0410-a6f0-8f06a7374b81

--- test/functional/previews_controller_test.rb
+++ test/functional/previews_controller_test.rb
@@ -1,3 +1,20 @@
+# Redmine - project management software
+# Copyright (C) 2006-2011  Jean-Philippe Lang
+#
+# This program is free software; you can redistribute it and/or
+# modify it under the terms of the GNU General Public License
+# as published by the Free Software Foundation; either version 2
+# of the License, or (at your option) any later version.
+# 
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+# 
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+
 require File.expand_path('../../test_helper', __FILE__)
 
 class PreviewsControllerTest < ActionController::TestCase
@@ -17,6 +34,15 @@ class PreviewsControllerTest < ActionController::TestCase
     assert_response :success
     assert_template 'preview'
     assert_not_nil assigns(:notes)
+  end
+  
+  def test_preview_journal_notes_for_update
+    @request.session[:user_id] = 2
+    post :issue, :project_id => '1', :id => 1, :notes => 'Foo'
+    assert_response :success
+    assert_template 'preview'
+    assert_not_nil assigns(:notes)
+    assert_tag :p, :content => 'Foo'
   end
 
   def test_news
