610c4efbe932205848810dfa8241a470275fb765
Toshi MARUYAMA <marutosijp2@yahoo.co.jp>
2011-03-30 05:46:58 +0000

    PDF: comment out Mime pdf in vendor/plugins/rfpdf/init.rb (#61).
    git-svn-id: svn+ssh://rubyforge.org/var/svn/redmine/trunk@5257 e93f8b46-1217-0410-a6f0-8f06a7374b81

--- vendor/plugins/rfpdf/init.rb
+++ vendor/plugins/rfpdf/init.rb
@@ -6,5 +6,6 @@ end
 require('cgi')
 require 'rfpdf'
 
-Mime::Type.register "application/pdf", :pdf
+# Mime::Type.register "application/pdf", :pdf
 ActionView::Template::register_template_handler 'rfpdf', RFPDF::TemplateHandlers::Base
+
