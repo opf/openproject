01361027f54f0267a2fa49d0fe470ebb493e8593
Jens Ulferts <j.ulferts@finn.de>
2011-03-31 15:59:02 +0200

    stop using inheritance for factory definitions because the ci can not handle it

--- spec/factories/impediment_factory.rb
+++ spec/factories/impediment_factory.rb
@@ -1,4 +1,7 @@
-Factory.define(:impediment, :parent => :issue, :class => Impediment) do |t|
+Factory.define(:impediment) do |t|
   t.association :tracker, :factory => :tracker_task
   t.subject "Impeding progress"
+  t.description "Unable to print recipes"
+  t.association :priority, :factory => :priority
+  t.association :author, :factory => :user
 end
--- spec/factories/task_factory.rb
+++ spec/factories/task_factory.rb
@@ -1,3 +1,7 @@
-Factory.define(:task, :parent => :issue, :class => Task) do |t|
+Factory.define(:task) do |t|
   t.association :tracker, :factory => :tracker_task
+  t.subject "Printing Recipes"
+  t.description "Unable to print recipes"
+  t.association :priority, :factory => :priority
+  t.association :author, :factory => :user
 end
