d9980d408ceb80c54ba8c2574e1e109627482c05
Markus Kahl <machisuji@googlemail.com>
2011-04-05 18:46:04 +0200

    Enabled help text for Clear Control.

--- lib/widget/controls/clear.rb
+++ lib/widget/controls/clear.rb
@@ -1,5 +1,6 @@
 class Widget::Controls::Clear < Widget::Base
   def render
-    link_to content_tag(:span, content_tag(:em, l(:"button_clear"), :class => "button-icon icon-clear")), '#', :id => 'query-link-clear', :class => 'button secondary'
+    html = link_to content_tag(:span, content_tag(:em, l(:"button_clear"), :class => "button-icon icon-clear")), '#', :id => 'query-link-clear', :class => 'button secondary'
+    maybe_with_help html
   end
 end
