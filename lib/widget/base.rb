390e4d8b00123b7500e3357c0449645fe91746ea
Tim Felgentreff <timfelgentreff@gmail.com>
2011-04-06 10:45:18 +0200

    All widgets now have a default @output canvas they can write to with #write

--- lib/widget/base.rb
+++ lib/widget/base.rb
@@ -1,9 +1,14 @@
 class Widget::Base < Widget
-  attr_reader :engine
+  attr_reader :engine, :output
 
   def initialize(query)
     @query = query
     @engine = query.class
+    @output = "".html_safe
+  end
+
+  def write(str)
+    @output.write str
   end
 
   def render
@@ -12,9 +17,12 @@ class Widget::Base < Widget
 
   def render_with_options(options = {}, &block)
     if canvas = options[:to]
-      canvas << "\n" << render(&block)
-    else
-      render(&block)
+      @output = canvas
     end
+    result = render(&block)
+    if @output.respond_to? :to_str and @output.empty? # FIXME: Transitional support
+      @output += "\n#{result}".html_safe
+    end
+    @output
   end
 end
