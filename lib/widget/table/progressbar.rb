db650ddbb0286f9a47eaff2486c5e8bf16c1b072
jwollert <johannes.wollert@gmail.com>
2011-04-06 12:59:00 +0200

    use streaming widget API and render table widget when we don't hit the progressbar threshhold

--- lib/widget/table/progressbar.rb
+++ lib/widget/table/progressbar.rb
@@ -4,51 +4,43 @@ class Widget::Table::Progressbar < Widget::Base
   def render
     @threshhold ||= 500
     size = @query.size
-      content_tag :div, :id => "progressbar", :class => "form_controls",
-      :"data-query-size" => size do
-        if size > @threshhold
-          content_tag :div, :id => "progressbar-load-table-question", :class => "form_controls" do
-            content = content_tag :span, :id => "progressbar-text", :class => "form_controls" do
-              ::I18n.translate(:label_load_query_question, :size => size)
-             end
+    write (content_tag :div, :id => "progressbar", :class => "form_controls",
+    :"data-query-size" => size do
+      if size >= @threshhold
+        content_tag :div, :id => "progressbar-load-table-question", :class => "form_controls" do
+          content = content_tag :span, :id => "progressbar-text", :class => "form_controls" do
+            ::I18n.translate(:label_load_query_question, :size => size)
+          end
 
-            content += content_tag :p do
-              p_content = content_tag :a, :class => "reporting_button button" do
-                content_tag :span,
-                :id => "progressbar-yes",
-                :'data-load' => 'true',
-                :class => "form_controls",
-                :'data-target' => url_for(:action => 'index', :set_filter => '1', :immediately => true) do
-                  content_tag :em do
-                    ::I18n.t(:label_yes)
-                   end
-                 end
-               end
+          content += content_tag :p, :class => "buttons" do
+            p_content = content_tag :a, :class => "reporting_button button" do
+              content_tag :span,
+              :id => "progressbar-yes",
+              :'data-load' => 'true',
+              :class => "form_controls",
+              :'data-target' => url_for(:action => 'index', :set_filter => '1', :immediately => true) do
+                content_tag :em do
+                  ::I18n.t(:label_yes)
+                end
+              end
+            end
 
-              p_content += content_tag :a, :class => "reporting_button button" do
-                content_tag :span,
-                :id => "progressbar-no",
-                :'data-load' => 'false',
-                :class => "form_controls" do
-                  content_tag :em do
-                    ::I18n.t(:label_no)
-                   end
-                 end
-               end
-             end
-            content
-           end
-        else
+            p_content += content_tag :a, :class => "reporting_button button" do
+              content_tag :span,
+              :id => "progressbar-no",
+              :'data-load' => 'false',
+              :class => "form_controls" do
+                content_tag :em do
+                  ::I18n.t(:label_no)
+                end
+              end
+            end
+          end
+          content
+        end
+      else
+        render_widget Widget::Table::ReportTable, @query, :to => (@output ||= "".html_safe)
       end
-        ## TODO render the table when we don't hit the threshhold
-        # render_widget Widget::Table, @query, {
-        #   :mapping => {
-        #     :show_result => method(:show_result),
-        #     :show_row => method(:show_row),
-        #     :show_field => method(:show_field),
-        #     :debug_fields => method(:debug_fields)
-        #   }
-        # }
-    end
+    end)
   end
 end
