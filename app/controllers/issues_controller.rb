033cef6f53f3bfe37fda3588026af38e60d74731
Felix Schäfer <felix@fachschaften.org>
2011-04-06 18:01:32 +0200

    Change default issue sort to by parent.

--- app/controllers/issues_controller.rb
+++ app/controllers/issues_controller.rb
@@ -50,7 +50,7 @@ class IssuesController < ApplicationController
 
   def index
     retrieve_query
-    sort_init(@query.sort_criteria.empty? ? [['id', 'desc']] : @query.sort_criteria)
+    sort_init(@query.sort_criteria.empty? ? [['parent', 'desc']] : @query.sort_criteria)
     sort_update(@query.sortable_columns)
 
     if @query.valid?
