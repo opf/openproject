626252b8e8631e6ba0319cd11fb383ccd46e3aa2
Jens Ulferts <j.ulferts@finn.de>
2011-04-07 15:25:37 +0200

    fixing costs_objects_controller to only return cost_entries for projects in which the user has the right to view_cost_entries

--- app/controllers/cost_objects_controller.rb
+++ app/controllers/cost_objects_controller.rb
@@ -8,23 +8,23 @@ class CostObjectsController < ApplicationController
     :update_material_budget_item, :update_labor_budget_item
   ]
   before_filter :find_optional_project, :only => :index
-  
+
   before_filter :authorize_global, :only => :index
   before_filter :authorize, :except => [
     :index,
-    
+
     # unrestricted actions
     :preview, :context_menu,
     :update_material_budget_item, :update_labor_budget_item
     ]
-  
+
   verify :method => :post, :only => [:bulk_edit, :destroy],
          :redirect_to => { :action => :index }
-  
+
   helper :sort
   include SortHelper
   helper :projects
-  include ProjectsHelper 
+  include ProjectsHelper
   helper :attachments
   include AttachmentsHelper
   helper :costlog
@@ -32,7 +32,7 @@ class CostObjectsController < ApplicationController
   helper :cost_objects
   include CostObjectsHelper
   include Redmine::Export::PDF
-  
+
   def index
     limit = per_page_option
     respond_to do |format|
@@ -40,7 +40,7 @@ class CostObjectsController < ApplicationController
       format.csv  { limit = Setting.issues_export_limit.to_i }
       format.pdf  { limit = Setting.issues_export_limit.to_i }
     end
-    
+
 
     sort_columns = {'id' => "#{CostObject.table_name}.id",
                     'subject' => "#{CostObject.table_name}.subject",
@@ -49,13 +49,13 @@ class CostObjectsController < ApplicationController
 
     sort_init "id", "desc"
     sort_update sort_columns
-    
-    conditions = @project ? {:project_id => @project} : {}
+
+    conditions = User.current.allowed_for(:view_cost_objects, @project)
 
     @cost_object_count = CostObject.count(:include => [:project], :conditions => conditions)
     @cost_object_pages = Paginator.new self, @cost_object_count, limit, params[:page]
     @cost_objects = CostObject.find :all, :order => sort_clause,
-                                     :include => [:project],
+                                     :include => [:project, :author],
                                      :conditions => conditions,
                                      :limit => limit,
                                      :offset => @cost_object_pages.current.offset
@@ -66,14 +66,14 @@ class CostObjectsController < ApplicationController
       format.pdf  { send_data(cost_objects_to_pdf(@cost_objects, @project), :type => 'application/pdf', :filename => 'export.pdf') }
     end
   end
-  
+
   def show
     @edit_allowed = User.current.allowed_to?(:edit_cost_objects, @project)
     respond_to do |format|
       format.html { render :action => 'show', :layout => !request.xhr?  }
     end
   end
-  
+
   def new
     if params[:cost_object]
       @cost_object = create_cost_object(params[:cost_object].delete(:kind))
@@ -84,19 +84,19 @@ class CostObjectsController < ApplicationController
         @cost_object.copy_from(params[:copy_from])
       end
     end
-    
+
     # FIXME: I forcibly create a VariableCostObject for now. Following Ticket #5360
     @cost_object ||= VariableCostObject.new
-    
+
     @cost_object.project_id = @project.id
-    
+
     # fixed_date must be set before material_budget_items and labor_budget_items
     if params[:cost_object] && params[:cost_object][:fixed_date]
       @cost_object.fixed_date = params[:cost_object].delete(:fixed_date)
     else
       @cost_object.fixed_date = Date.today
     end
-    
+
     @cost_object.attributes = params[:cost_object]
 
     unless request.get? || request.xhr?
@@ -107,53 +107,53 @@ class CostObjectsController < ApplicationController
         return
       end
     end
-    
+
     render :layout => !request.xhr?
   end
-  
+
   # Blacklist of attributes which can not be updated in edit
   EDIT_BLACK_LIST = %w(kind type)
-  
+
   def edit
     if params[:cost_object]
       attrs = params[:cost_object].dup
       attrs.delete_if {|k,v| EDIT_BLACK_LIST.include?(k)}
-      
+
       @cost_object.attributes = attrs
     end
-    
+
     if request.post?
       if @cost_object.save
         flash[:notice] = l(:notice_successful_update)
         redirect_to(params[:back_to] || {:action => 'show', :id => @cost_object})
       end
-    end 
+    end
   rescue ActiveRecord::StaleObjectError
     # Optimistic locking exception
     flash.now[:error] = l(:notice_locking_conflict)
   end
-  
+
   def destroy
     @cost_objects.each(&:destroy)
     flash[:notice] = l(:notice_successful_delete)
     redirect_to :action => 'index', :project_id => @project
   end
-  
+
   def preview
     @cost_object = CostObjects.find_by_id(params[:id]) unless params[:id].blank?
     @text = params[:notes] || (params[:cost_object] ? params[:cost_object][:description] : nil)
-    
+
     render :partial => 'common/preview'
   end
-  
+
   def update_material_budget_item
     element_id = params[:element_id] if params.has_key? :element_id
-    
+
     cost_type = CostType.find(params[:cost_type_id]) if params.has_key? :cost_type_id
-    
+
     units = BigDecimal.new(Rate.clean_currency(params[:units]))
     costs = (units * cost_type.rate_at(params[:fixed_date]).rate rescue 0.0)
-    
+
     if request.xhr?
       render :update do |page|
         if User.current.allowed_to? :view_cost_rates, @project
@@ -165,15 +165,15 @@ class CostObjectsController < ApplicationController
   rescue ActiveRecord::RecordNotFound
     render_404
   end
-  
+
   def update_labor_budget_item
     element_id = params[:element_id] if params.has_key? :element_id
-    
+
     user = User.find(params[:user_id])
-    
+
     hours = params[:hours].to_hours
     costs = hours * user.rate_at(params[:fixed_date], @project).rate rescue 0.0
-    
+
     if request.xhr?
       render :update do |page|
         if User.current.allowed_to?(:view_hourly_rates, @project, :for => user)
@@ -198,7 +198,7 @@ private
       CostObject.new
     end
   end
-  
+
   def find_cost_object
     # This function comes directly from issues_controller.rb (Redmine 0.8.4)
     @cost_object = CostObject.find(params[:id], :include => [:project, :author])
@@ -206,10 +206,10 @@ private
   rescue ActiveRecord::RecordNotFound
     render_404
   end
-  
+
   def find_cost_objects
     # This function comes directly from issues_controller.rb (Redmine 0.8.4)
-    
+
     @cost_objects = CostObject.find_all_by_id(params[:id] || params[:ids])
     raise ActiveRecord::RecordNotFound if @cost_objects.empty?
     projects = @cost_objects.collect(&:project).compact.uniq
@@ -228,7 +228,7 @@ private
   rescue ActiveRecord::RecordNotFound
     render_404
   end
-  
+
   def find_optional_project
     @project = Project.find(params[:project_id]) unless params[:project_id].blank?
   rescue ActiveRecord::RecordNotFound
