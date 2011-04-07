0e8528327d2827e3623787f325bbf783e8753204
Philipp Tessenow <philipp.tessenow@student.hpi.uni-potsdam.de>
2011-04-07 16:00:26 +0200

    fix indendation for dependent filters

--- lib/widget/filters/label.rb
+++ lib/widget/filters/label.rb
@@ -2,7 +2,7 @@ class Widget::Filters::Label < Widget::Filters::Base
   def render
     content_tag :td, :width => 150 do
       options = { :id => filter_class.underscore_name }
-      if (engine::Filter.all.any? {|f| f.dependent == filter_class})
+      if (engine::Filter.all.any? {|f| f.dependents.include?(filter_class)})
         options.merge! :class => 'dependent-filter-label'
       end
       content_tag :label, options do
