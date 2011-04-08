b71bf13c57cfc5d19c9f4d99af23c54d61e8df4a
Markus Kahl <machisuji@googlemail.com>
2011-04-08 10:09:08 +0200

    Use cordinc_tooltip; made icon and diff customizable.

--- lib/widget/controls/help.rb
+++ lib/widget/controls/help.rb
@@ -4,13 +4,15 @@
 # Where :text is a i18n key.
 class Widget::Controls::Help < Widget::Base
   def render
+    id = "tip:#{@query}"
     options = {:icon => {}, :tooltip => {}}
     options.merge!(yield) if block_given?
-    icon = tag :img, :src => '/images/icon_info_red.gif'
-    span = content_tag_string :span, l(@query), options[:tooltip], false
-    hull = content_tag :div, span
-    icon_options = icon_config(options[:icon])
-    content_tag :a, icon + hull, icon_options
+    icon = tag :img, :src => '/images/icon_info_red.gif', :id => "target:#{@query}"
+    tip = content_tag_string :div, l(@query), tip_config(options[:tooltip]), false
+    script = content_tag_string :script, "new Tooltip('target:#{@query}', 'tip:#{@query}', {className: 'tooltip'});",
+      {:type => 'text/javascript'}, false
+    target = content_tag :a, icon + tip, icon_config(options[:icon])
+    target + script
   end
 
   def icon_config(options)
@@ -22,6 +24,17 @@ class Widget::Controls::Help < Widget::Base
       end
     end
     options.mega_merge! :href => '#', :class => add_class
+  end
+
+  def tip_config(options)
+    add_class = lambda do |cl|
+      if cl
+        "#{cl} tooltip"
+      else
+        "tooltip"
+      end
+    end
+    options.mega_merge! :id => "tip:#{@query}", :class => add_class
   end
 end
 
