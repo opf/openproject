063b312aa0de533e23cf3cf8f16822469266d73c
Jens Ulferts <j.ulferts@finn.de>
2011-04-11 15:33:29 +0200

    using project_id param as a default to find the project for the controller, project of sprint is only a fallback

--- app/controllers/rb_application_controller.rb
+++ app/controllers/rb_application_controller.rb
@@ -11,12 +11,12 @@ class RbApplicationController < ApplicationController
   # Loads the project to be used by the authorize filter to
   # determine if User.current has permission to invoke the method in question.
   def load_project
-    @project = if params[:sprint_id]
-                 load_sprint
-                 @sprint.project
-               elsif params[:project_id]
-                 Project.find(params[:project_id])
-               end
+    @project = Project.find(params[:project_id]) if params[:project_id]
+
+    if params[:sprint_id]
+      load_sprint
+      @project = @sprint.project unless @project.present?
+    end
   end
 
   def check_if_plugin_is_configured
