df918b5e1d7ee857f6d080c12d24e1be1200603a
jwollert <johannes.wollert@gmail.com>
2011-04-15 13:55:53 +0200

    modal confirmation dialog for progressbar question

--- assets/javascripts/reporting/progressbar.js
+++ assets/javascripts/reporting/progressbar.js
@@ -3,21 +3,19 @@
 
 Reporting.Progress = {
 
-  attach_listeners: function () {
+  confirm_question: function () {
     if ($('progressbar') !== null && $('progressbar') !== undefined) {
-      $('progressbar').select('span[data-load]').each(function (element) {
-        element.observe("click", function (e) {
-          if (this.getAttribute("data-load") === "true") {
-            Reporting.Controls.send_settings_data(this.getAttribute("data-target"), Reporting.Controls.update_result_table);
-          } else {
-            $('progressbar').toggle();
-          }
-        });
-      });
+      var size = $('progressbar').getAttribute('data-size');
+      var question = $('progressbar').getAttribute('data-translation');
+      if (confirm(question)) {
+        Reporting.Controls.send_settings_data($('progressbar').getAttribute("data-target"), Reporting.Controls.update_result_table);
+      } else {
+        $('progressbar').toggle();
+      }
     }
   }
-};
+}
 
 Reporting.onload(function () {
-  Reporting.Progress.attach_listeners();
+  Reporting.Progress.confirm_question();
 });
--- lib/widget/table/progressbar.rb
+++ lib/widget/table/progressbar.rb
@@ -7,39 +7,12 @@ class Widget::Table::Progressbar < Widget::Base
     if Widget::Table::ReportTable.new(@query).cached? || (size = @query.size) <= THRESHHOLD
       render_widget Widget::Table::ReportTable, @query, :to => (@output ||= "".html_safe)
     else
-      write(content_tag :div, :id => "progressbar", :class => "form_controls",
-      :"data-query-size" => size do
-        content_tag :div, :id => "progressbar-load-table-question", :class => "form_controls" do
-          content = content_tag :span, :id => "progressbar-text", :class => "form_controls" do
-            ::I18n.translate(:label_load_query_question, :size => size)
-          end
-
-          content += content_tag :p, :class => "buttons" do
-            p_content = content_tag :a, :class => "reporting_button button" do
-              content_tag :span,
-              :id => "progressbar-yes",
-              :'data-load' => 'true',
-              :class => "form_controls",
-              :'data-target' => url_for(:action => 'index', :set_filter => '1', :immediately => true) do
-                content_tag :em do
-                  ::I18n.t(:label_yes)
-                end
-              end
-            end
-
-            p_content += content_tag :a, :class => "reporting_button button" do
-              content_tag :span,
-              :id => "progressbar-no",
-              :'data-load' => 'false',
-              :class => "form_controls" do
-                content_tag :em do
-                  ::I18n.t(:label_no)
-                end
-              end
-            end
-          end
-          content
-        end
+      write(content_tag :div,
+        :id => "progressbar",
+        :class => "form_controls",
+        :"data-query-size" => size,
+        :"data-translation" => ::I18n.translate(:label_load_query_question, :size => size),
+        :"data-target" => url_for(:action => 'index', :set_filter => '1', :immediately => true) do
       end)
     end
   end
