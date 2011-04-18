46b7f0496dbea24a63727ac267d557d0bfa96147
Gregor Schmidt <ruby@schmidtwisser.de>
2011-04-18 16:12:33 +0200

    checking for enabled modules in more robust way

--- lib/backlogs_issue_patch.rb
+++ lib/backlogs_issue_patch.rb
@@ -11,14 +11,14 @@ module Backlogs
 
         alias_method_chain :move_to_project_without_transaction, :autolink
         alias_method_chain :recalculate_attributes_for, :remaining_hours
-        before_validation :backlogs_before_validation, :if => lambda {|i| i.project.module_enabled?("backlogs")}
+        before_validation :backlogs_before_validation, :if => lambda {|i| i.project && i.project.module_enabled?("backlogs")}
         after_save  :backlogs_after_save
 
         validates_numericality_of :story_points, :only_integer => true,
                                                  :allow_nil    => true,
                                                  :greater_than => 0,
                                                  :less_than    => 10_000,
-                                                 :if => lambda { |i| i.project.module_enabled? 'backlogs' }
+                                                 :if => lambda { |i| i.project && i.project.module_enabled?('backlogs') }
 
       end
     end
