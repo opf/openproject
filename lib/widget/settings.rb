01cab3618e3d82f04191de8439e0b0f1beee7072
Markus Kahl <machisuji@googlemail.com>
2011-04-19 18:48:24 +0200

    removed hard-coded help texts and made them configurable

--- lib/widget/settings.rb
+++ lib/widget/settings.rb
@@ -4,12 +4,12 @@ class Widget::Settings < Widget::Base
       content_tag :div, :id => "query_form_content" do
 
         fieldsets = render_widget Widget::Settings::Fieldset, @query,
-            { :type => "filter", :help_text => :help_filters } do
+            { :type => "filter", :help_text => self.filter_help } do
           render_widget Widget::Filters, @query
         end
 
         fieldsets += render_widget Widget::Settings::Fieldset, @query,
-            { :type => "group_by", :help_text => :help_group_by } do
+            { :type => "group_by", :help_text => self.group_by_help } do
           render_widget Widget::GroupBys, @query
         end
 
@@ -22,6 +22,22 @@ class Widget::Settings < Widget::Base
         end
         fieldsets + controls
       end
+    end
+  end
+
+  def filter_help
+    if help_text.kind_of?(Array)
+      help_text[0]
+    else
+      nil
+    end
+  end
+
+  def group_by_help
+    if help_text.kind_of?(Array)
+      help_text[1]
+    else
+      nil
     end
   end
 end
