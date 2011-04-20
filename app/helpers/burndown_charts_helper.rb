5b2809d6955beae25882fbcc5a94c5ceda668675
Jens Ulferts <j.ulferts@finn.de>
2011-04-20 22:59:14 +0200

    sorting checkboxes for burndown series selection

--- app/helpers/burndown_charts_helper.rb
+++ app/helpers/burndown_charts_helper.rb
@@ -21,4 +21,12 @@ module BurndownChartsHelper
   def dataseries(burndown)
     burndown.series.collect{|s| "#{s.first.to_s}: {label: '#{l('backlogs.' + s.first.to_s)}', data: [#{s.last.enum_for(:each_with_index).collect{|s, i| "[#{i+1}, #{s}] "}.join(', ')}]} "}
   end
+
+  def burndown_series_checkboxes(burndown)
+    boxes = ""
+    burndown.series(:all).collect{|s| s.first.to_s }.sort.each do |series|
+      boxes += "<input class=\"series_enabled\" type=\"checkbox\" id=\"#{series}\" name=\"#{series}\" value=\"#{series}\" checked>#{l('backlogs.' + series.to_s)}<br/>"
+    end
+    boxes
+  end
 end
