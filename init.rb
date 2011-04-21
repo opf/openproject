ebb24c81a976f81181779139e25c51ed4b4e0e95
Jens Ulferts <j.ulferts@finn.de>
2011-04-21 17:06:17 +0200

    modal boxes have their closing "x" again

--- init.rb
+++ init.rb
@@ -26,7 +26,7 @@ Redmine::Plugin.register :redmine_backlogs do
   name 'Redmine Backlogs'
   author 'relaxdiego, friflaj, Gregor Schmidt, Jens Ulferts'
   description 'A plugin for agile teams'
-  version '0.6.2'
+  version '0.6.3'
 
   settings :default => {
                          :story_trackers  => nil,
