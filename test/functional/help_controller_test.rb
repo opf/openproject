0b7b564a34c84c3046b27fcf215cf63771dd8ece
Gregor Schmidt <ruby@schmidtwisser.de>
2011-05-02 15:24:11 +0200

    adding basic test, to make sure, nothing breaks

--- test/functional/help_controller_test.rb
+++ test/functional/help_controller_test.rb
@@ -1,8 +1,15 @@
 require 'test_helper'
 
 class HelpControllerTest < ActionController::TestCase
-  # Replace this with your real tests.
-  test "the truth" do
-    assert true
+  test "renders wiki_syntax properly" do
+    get "wiki_syntax"
+
+    assert_select "h1", "Wiki Syntax Quick Reference"
+  end
+
+  test "renders wiki_syntax_detailed properly" do
+    get "wiki_syntax_detailed"
+
+    assert_select "h1", "Wiki Formatting"
   end
 end
