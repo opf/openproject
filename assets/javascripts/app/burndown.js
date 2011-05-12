e3002b0b21ed50ff0f994c03616065acf55eb4f2
Jens Ulferts <j.ulferts@finn.de>
2011-05-12 10:01:29 +0200

    #4264: burndown is again available to be called form the issue index page (requires a project and a sprint to be selected)
    * burndown refactored into a separate js class

---
+++ assets/javascripts/app/burndown.js
@@ -0,0 +1,47 @@
+RB.Burndown = (function ($) {
+  return RB.Object.create({
+
+    initialize: function (el) {
+      this.$ = $(el);
+      this.el = el;
+
+      // Associate this object with the element for later retrieval
+      this.$.data('this', this);
+
+      // Observe menu items
+      this.$.click(this.show);
+    },
+
+    setSprintId : function (sprintId) {
+      this.sprintId = sprintId;
+    },
+
+    getSprintId : function (){
+      return this.sprintId;
+    },
+
+    show: function (e) {
+      //var backlogs;
+
+      e.preventDefault();
+
+      //backlogs = $(this).parents('.backlog').data('this');
+
+      if ($("#charts").length === 0) {
+        $('<div id="charts"></div>').appendTo("body");
+      }
+      $('#charts').html("<div class='loading'>" + RB.i18n.generating_graph + "</div>");
+      $('#charts').load(RB.urlFor('show_burndown_chart', { id: $(this).data('this').sprintId,
+                                                           project_id: RB.constants.project_id}));
+      $('#charts').dialog({
+        dialogClass: "rb_dialog",
+        height: 500,
+        width: 710,
+        position: 'center',
+        modal: true,
+        title: RB.i18n.burndown_graph,
+        resizable: false
+      });
+    }
+  });
+}(jQuery));
