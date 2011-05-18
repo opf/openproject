8b07562500db2d2446e7d5241939595bd017988b
Jens Ulferts <j.ulferts@finn.de>
2011-05-18 13:13:12 +0200

    refactor: using new standard way to abbreviate text for the issue description

--- lib/taskboard_card/description.rb
+++ lib/taskboard_card/description.rb
@@ -19,39 +19,20 @@ module TaskboardCard
 
       def render(pdf, issue, options)
         render_bounding_box(pdf, options.merge(:border => true, :margin => margin)) do
-
-          y_offset = pdf.bounds.height
-
           description = issue.description ? issue.description : ""
 
-          description.split("\n").each do |line|
+          r = RedCloth3.new(description)
+          line = r.to_html
+          line = Description.strip_tags(line)
 
-            r = RedCloth3.new(line)
-            line = r.to_html
-            line = Description.strip_tags(line)
-            font_height = 20
-
-            if y_offset - font_height > font_height
-              box = text_box(pdf,
-                             line,
-                             {:height => pdf.height_of(line, :size => font_height),
-                              :at => [0, y_offset],
-                              :size => font_height,
-                              :padding_bottom => 0})
-
-              y_offset -= box.height
-            else
-              text_box(pdf,
-                       "[...]",
-                       {:height => font_height,
-                        :at => [0, y_offset],
-                        :size => font_height})
-              break
-            end
-          end
+          text_box(pdf,
+                   line,
+                   {:height => pdf.bounds.height,
+                    :at => [0, pdf.bounds.height],
+                    :size => 20,
+                    :padding_bottom => 0})
         end
       end
-
     end
   end
 end
