c57bbfa12170e6d31eb9c7dcd48f61b77468e31c
Gregor Schmidt <ruby@schmidtwisser.de>
2011-05-19 10:26:07 +0200

    Adding an edit link, which currently only goes to show, but will eventually become an IPE

--- lib/redmine_backlogs/issue_actions.rb
+++ lib/redmine_backlogs/issue_actions.rb
@@ -6,6 +6,7 @@ class RedmineBacklogs::IssueActions < ChiliProject::Nissue::View
   def render(t)
     css_class = "watcher_link_#{@issue.id}"
     content_tag(:div, [
+        t.link_to_if_authorized(l(:button_update), {:controller => 'issues', :action => 'show', :id => @issue }, :class => 'icon icon-edit'),
         t.watcher_link(@issue, User.current, :class => css_class, :replace => ".#{css_class}")
       ], :class => 'contextual')
   end
