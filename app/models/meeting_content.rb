89394e23161e740ada344624526607feb6b026a4
Felix Schäfer <felix@fachschaften.org>
2011-05-19 17:47:52 +0200

    Meeting journalizing and activity view refinements.

--- app/models/meeting_content.rb
+++ app/models/meeting_content.rb
@@ -30,6 +30,11 @@ class MeetingContent < ActiveRecord::Base
     updated_at
   end
   
+  # Show the project on activity and search views
+  def project
+    meeting.project
+  end
+  
   # Provided for compatibility of the old pre-journalized migration
   def self.create_versioned_table
   end
@@ -43,38 +48,4 @@ class MeetingContent < ActiveRecord::Base
   def comment_to_journal_notes
     init_journal(author, comment) unless changes.empty?
   end
-  
-  # FIXME: Leftover from pre-journalized meeting contents
-  #class Version
-  #  unloadable
-  #  
-  #  belongs_to :author, :class_name => '::User', :foreign_key => 'author_id'
-  #  belongs_to :meeting, :class_name => '::Meeting', :foreign_key => 'meeting_id'
-  #  
-  #  acts_as_event :title => Proc.new {|o| "#{l :"label_#{o.versioned_type.underscore}"}: #{o.meeting.title}"},
-  #                :description => :comment,
-  #                :datetime => :updated_at,
-  #                :type => Proc.new {|o| o.versioned_type.underscore.dasherize},
-  #                :url => Proc.new {|o| {:controller => 'meetings', :action => 'show', :id => o.meeting}}
-  #  
-  #  acts_as_activity_provider :type => 'meetings',
-  #                            :timestamp => "#{MeetingContent.versioned_table_name}.updated_at",
-  #                            :author_key => "#{MeetingContent.versioned_table_name}.author_id",
-  #                            :permission => :view_meetings,
-  #                            :find_options => {:select => "#{MeetingContent.versioned_table_name}.updated_at, #{MeetingContent.versioned_table_name}.comment, " +
-  #                                                         "#{MeetingContent.versioned_table_name}.#{MeetingContent.version_column}, #{Meeting.table_name}.title, " +
-  #                                                         "#{MeetingContent.versioned_table_name}.author_id, #{MeetingContent.versioned_table_name}.id," +
-  #                                                         "#{MeetingContent.versioned_table_name}.meeting_id, #{Meeting.table_name}.project_id, " +
-  #                                                         "#{MeetingContent.versioned_table_name}.versioned_type",
-  #                                              :joins => "LEFT JOIN #{Meeting.table_name} ON #{Meeting.table_name}.id = #{MeetingContent.versioned_table_name}.meeting_id " +
-  #                                                        "LEFT JOIN #{Project.table_name} ON #{Project.table_name}.id = #{Meeting.table_name}.project_id"}
-  #  
-  #  def project
-  #    meeting.project
-  #  end
-  #  
-  #  def editable?
-  #    false
-  #  end
-  #end
 end
--- init.rb
+++ init.rb
@@ -34,7 +34,7 @@ Redmine::Plugin.register :redmine_meeting do
     search.register :meetings
   end
   
-  activity_provider :meetings, :default => false, :class_name => ['MeetingAgenda', 'MeetingMinutes']
+  activity_provider :meetings, :default => false, :class_name => ['Meeting', 'MeetingAgenda', 'MeetingMinutes']
   
   menu :project_menu, :meetings, {:controller => 'meetings', :action => 'index'}, :caption => :project_module_meetings, :param => :project_id, :after => :wiki
 end
