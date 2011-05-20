d7b963e6a60360d0258c112632fdbb853d83cbc2
Felix Schäfer <f.schaefer@finn.de>
2011-05-20 16:10:01 +0200

    Load the SQLite3 lib for the test to work. #112

--- test/unit/lib/chili_project/database_test.rb
+++ test/unit/lib/chili_project/database_test.rb
@@ -16,6 +16,7 @@
 # Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
 
 require File.expand_path('../../../../test_helper', __FILE__)
+require 'sqlite3_api'
 
 class ChiliProject::DatabaseTest < ActiveSupport::TestCase
   setup do
