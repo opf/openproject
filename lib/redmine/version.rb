a428fa60687a55f2e7d45709c2cf8b7f939c0371
Eric Davis <edavis@littlestreamsoftware.com>
2011-05-30 14:22:31 -0700

    Bumping version to 2.0.0RC1

--- lib/redmine/version.rb
+++ lib/redmine/version.rb
@@ -15,8 +15,8 @@ require 'rexml/document'
 
 module Redmine
   module VERSION #:nodoc:
-    MAJOR = 1
-    MINOR = 4
+    MAJOR = 2
+    MINOR = 0
     PATCH = 0
     TINY  = PATCH # Redmine compat
 
