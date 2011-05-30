37f6b75749ebd56a693981ef3c9283cf02becf80
Eric Davis <edavis@littlestreamsoftware.com>
2011-05-29 17:59:51 -0700

    Added VERSION.special to handle special version releases (RC, distributions, etc)

--- lib/redmine/version.rb
+++ lib/redmine/version.rb
@@ -19,6 +19,21 @@ module Redmine
     MINOR = 4
     PATCH = 0 
     TINY  = PATCH # Redmine compat
+
+    # Used by semver to define the special version (if any).
+    # A special version "satify but have a lower precedence than the associated
+    # normal version". So 2.0.0RC1 would be part of the 2.0.0 series but
+    # be considered to be an older version.
+    #
+    #   1.4.0 < 2.0.0RC1 < 2.0.0RC2 < 2.0.0 < 2.1.0
+    #
+    # This method may be overridden by third party code to provide vendor or
+    # distribution specific versions. They may or may not follow semver.org:
+    #
+    #   2.0.0debian-2
+    def self.special
+      'RC1'
+    end
     
     def self.revision
       revision = nil
@@ -48,7 +63,7 @@ module Redmine
     def self.to_a; ARRAY end
     def self.to_s; STRING end    
     def self.to_semver
-      [MAJOR, MINOR, PATCH].join('.')
+      [MAJOR, MINOR, PATCH].join('.') + special
     end
   end
 end
