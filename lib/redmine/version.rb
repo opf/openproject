790bb362c53633cc1b3faa4e7cb2afea287bed04
Eric Davis <edavis@littlestreamsoftware.com>
2011-05-30 14:05:40 -0700

    Fix the merge conflict from 6b8897db5fa5a

--- lib/redmine/version.rb
+++ lib/redmine/version.rb
@@ -20,6 +20,21 @@ module Redmine
     PATCH = 0
     TINY  = PATCH # Redmine compat
 
+    # Used by semver to define the special version (if any).
+    # A special version "satify but have a lower precedence than the associated
+    # normal version". So 2.0.0RC1 would be part of the 2.0.0 series but
+    # be considered to be an older version.
+    #
+    #   1.4.0 < 2.0.0RC1 < 2.0.0RC2 < 2.0.0 < 2.1.0
+    #
+    # This method may be overridden by third party code to provide vendor or
+    # distribution specific versions. They may or may not follow semver.org:
+    #
+    #   2.0.0debian-2
+    def self.special
+      'RC1'
+    end
+    
     def self.revision
       revision = nil
       entries_path = "#{RAILS_ROOT}/.svn/entries"
