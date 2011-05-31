ca5f9c3ada194ee68b253c8f26e340048e37b0b1
Gregor Schmidt <ruby@schmidtwisser.de>
2011-05-31 10:04:54 +0200

    Releasing 0.8.5
    Minor style changes for issue hierarchy

--- init.rb
+++ init.rb
@@ -26,7 +26,7 @@ Redmine::Plugin.register :redmine_backlogs do
   name 'Redmine Backlogs'
   author 'relaxdiego, friflaj, Gregor Schmidt, Jens Ulferts'
   description 'A plugin for agile teams'
-  version '0.8.4'
+  version '0.8.5'
 
   requires_redmine_plugin 'chiliproject_nissue', '0.1.0'
 
