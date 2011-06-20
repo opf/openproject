b9051c1b72ade6fc8a292b48e9697ddb351dbbc5
Gregor Schmidt <ruby@schmidtwisser.de>
2011-06-20 16:43:42 +0200

    Refactoring: removing unused code

--- assets/javascripts/app/show_main.js
+++ assets/javascripts/app/show_main.js
@@ -1,36 +1,14 @@
 /*jslint indent: 2*/
-/*globals window, document, jQuery, RB*/
+/*globals jQuery, RB*/
 
 // Initialize everything after DOM is loaded
 jQuery(function ($) {
-  var showCharts, defaultDialogColor;
-
-  showCharts = function (e) {
-    e.preventDefault();
-
-    if ($("#charts").length === 0) {
-      $("<div id='charts'></div>").appendTo("body");
-    }
-    $('#charts').html("<div class='loading'>" + RB.i18n['generating_graph'] + "</div>");
-    $('#charts').load(RB.urlFor('show_burndown_chart', {id: RB.constants.sprint_id}));
-    $('#charts').dialog({
-      buttons: {
-        Close: function () {
-          $(this).dialog("close");
-        }
-      },
-      modal: true,
-      title: RB.i18n['burndown_graph'],
-      height: 790,
-      width: 710
-    });
-  };
+  var defaultDialogColor; // this var is used as cache for some computation in
+                          // the inner function. -> Do not move to where it
+                          // actually belongs!
 
   RB.Factory.initialize(RB.Taskboard, $('#taskboard'));
   RB.TaskboardUpdater.start();
-
-  // Capture 'click' instead of 'mouseup' so we can preventDefault();
-  $('#show_charts').click(showCharts);
 
   $('#assigned_to_id_options').change(function () {
     var selected = $(this).children(':selected');
