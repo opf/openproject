8ca03f3a2a9da9a0a8e3d540942ba8fa2696b07a
Eric Davis <edavis@littlestreamsoftware.com>
2011-07-11 09:50:30 -0700

    Add ChiliProject::Compatibility to help upgrade third party code

---
+++ lib/chili_project/compatibility.rb
@@ -0,0 +1,24 @@
+#-- copyright
+# ChiliProject is a project management system.
+#
+# Copyright (C) 2010-2011 the ChiliProject Team
+#
+# This program is free software; you can redistribute it and/or
+# modify it under the terms of the GNU General Public License
+# as published by the Free Software Foundation; either version 2
+# of the License, or (at your option) any later version.
+#
+# See doc/COPYRIGHT.rdoc for more details.
+#++
+
+module ChiliProject
+  class Compatibility
+    # Is acts_as_journalized included?
+    #
+    # Released: ChiliProject 2.0.0
+    def self.using_acts_as_journalized?
+      Journal.included_modules.include?(Redmine::Acts::Journalized)
+    end
+
+  end
+end
