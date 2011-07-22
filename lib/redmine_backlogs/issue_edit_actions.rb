eb8f3ef1927e3802ecfc397a547d2c5ef5ed7367
Stephan Eckardt <s.eckardt@finn.de>
2011-07-22 18:21:39 +0200

    render action links in show and edit forms as specified in wireframes

--- lib/redmine_backlogs/issue_edit_actions.rb
+++ lib/redmine_backlogs/issue_edit_actions.rb
@@ -10,14 +10,13 @@ class RedmineBacklogs::IssueEditActions < ChiliProject::Nissue::View
   def render(t)
     css_class = "watcher_link_#{@issue.id}"
     content_tag(:div, [
-        # (t.modal_link_to(l(:button_update) + "(TODO:remove)", {:controller => 'issue_boxes', :action => 'edit', :id => @issue }, :class => 'icon icon-edit') if t.authorize_for('issue_boxes', 'edit')),
-        (t.link_to_remote l(:button_save), 
+        t.link_to_issue_box(l(:button_cancel), @issue, :class => 'icon icon-cancel'),
+        (t.link_to_remote(l(:button_save), 
                            { :url => { :controller => 'issue_boxes', :action => 'update', :id => @issue },
                              :method => 'PUT',
                              :update => @html_id,
                              :with => "Form.serialize('#{@form_id}')"
-                           }, { :class => 'icon icon-save', :accesskey => t.accesskey(:update) } if t.authorize_for('issue_boxes', 'update')),
-        t.watcher_link(@issue, User.current, :class => css_class, :replace => ".#{css_class}")
+                           }, { :class => 'icon icon-save', :accesskey => t.accesskey(:update) }) if t.authorize_for('issue_boxes', 'update'))
       ], :class => 'contextual')
   end
 end
