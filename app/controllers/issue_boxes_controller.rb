1ebffad74a33e2a3c41ca2006040b6cbe091d555
Stephan Eckardt <s.eckardt@finn.de>
2011-07-22 18:17:37 +0200

    less redundancy in issues boxes controller

--- app/controllers/issue_boxes_controller.rb
+++ app/controllers/issue_boxes_controller.rb
@@ -6,9 +6,7 @@ class IssueBoxesController < IssuesController
   def show
     return redirect_to issue_path(params[:id]) unless request.xhr?
 
-    @journals = @issue.journals.find(:all, :include => [:user, :details], :order => "#{Journal.table_name}.created_on ASC")
-    @journals.each_with_index {|j,i| j.indice = i+1}
-    @journals.reverse! if User.current.wants_comments_in_reverse_order?
+    load_journals
     @changesets = @issue.changesets.visible.all
     @changesets.reverse! if User.current.wants_comments_in_reverse_order?
     @relations = @issue.relations.select {|r| r.other_issue(@issue) && r.other_issue(@issue).visible? }
@@ -16,24 +14,19 @@ class IssueBoxesController < IssuesController
     @edit_allowed = User.current.allowed_to?(:edit_issues, @project)
     @priorities = IssuePriority.all
     @time_entry = TimeEntry.new
+
     respond_to do |format|
       format.js   { render :partial => 'show' }
     end
   end
   
   def edit
-    # return redirect_to edit_issue_path(params[:id]) unless request.xhr?
+    return redirect_to edit_issue_path(params[:id]) unless request.xhr?
+    
+    update_issue_from_params
+    load_journals
+    @journal = @issue.current_journal
 
-    @journals = @issue.journals.find(:all, :include => [:user, :details], :order => "#{Journal.table_name}.created_on ASC")
-    @journals.each_with_index {|j,i| j.indice = i+1}
-    @journals.reverse! if User.current.wants_comments_in_reverse_order?
-    @changesets = @issue.changesets.visible.all
-    @changesets.reverse! if User.current.wants_comments_in_reverse_order?
-    @relations = @issue.relations.select {|r| r.other_issue(@issue) && r.other_issue(@issue).visible? }
-    @allowed_statuses = @issue.new_statuses_allowed_to(User.current)
-    @edit_allowed = User.current.allowed_to?(:edit_issues, @project)
-    @priorities = IssuePriority.all
-    @time_entry = TimeEntry.new
     respond_to do |format|
       format.js   { render :partial => 'edit' }
     end
@@ -41,32 +34,26 @@ class IssueBoxesController < IssuesController
   
   def update
     update_issue_from_params
-
+    
     if @issue.save_issue_with_child_records(params, @time_entry)
-      render_attachment_warning_if_needed(@issue)
-      flash[:notice] = l(:notice_successful_update) unless @issue.current_journal.new_record?
-
+      @issue.reload
+      load_journals
       respond_to do |format|
-        format.html   { render :partial => 'show'}
+        format.js   { render :partial => 'show' }
       end
     else
-      render_attachment_warning_if_needed(@issue)
-      flash[:notice] = l(:notice_successful_update) unless @issue.current_journal.new_record?
       @journal = @issue.current_journal
-
       respond_to do |format|
-        format.html { render :partial => 'edit' }
+        format.js { render :partial => 'edit' }
       end
     end
   end
   
   private
-  def update_issue_from_params
-    @time_entry = TimeEntry.new
-    @time_entry.attributes = params[:time_entry]
-    
-    @notes = params[:notes] || (params[:issue].present? ? params[:issue][:notes] : nil)
-    @issue.init_journal(User.current, @notes)
-    @issue.safe_attributes = params[:issue]
+
+  def load_journals
+    @journals = @issue.journals.find(:all, :include => [:user, :details], :order => "#{Journal.table_name}.created_on ASC")
+    @journals.each_with_index {|j,i| j.indice = i+1}
+    @journals.reverse! if User.current.wants_comments_in_reverse_order?
   end
 end
