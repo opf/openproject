f0d271bee3f3336b4a4a91ac5cabff0bd98867eb
Holger Just <h.just@finn.de>
2011-07-25 21:22:13 +0200

    Fix failing tests because of missing journal fixtures and indeterministic behaviour of .find(:first)

--- test/unit/mailer_test.rb
+++ test/unit/mailer_test.rb
@@ -316,7 +316,7 @@ class MailerTest < ActiveSupport::TestCase
   end
 
   def test_wiki_content_added
-    content = WikiContent.find(:first)
+    content = WikiContent.find(1)
     valid_languages.each do |lang|
       Setting.default_language = lang.to_s
       assert_difference 'ActionMailer::Base.deliveries.size' do
@@ -326,7 +326,7 @@ class MailerTest < ActiveSupport::TestCase
   end
 
   def test_wiki_content_updated
-    content = WikiContent.find(:first)
+    content = WikiContent.find(1)
     valid_languages.each do |lang|
       Setting.default_language = lang.to_s
       assert_difference 'ActionMailer::Base.deliveries.size' do
