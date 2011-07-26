faab11d080b37086550a19999c0362baa3ba02c6
Tim Felgentreff <timfelgentreff@gmail.com>
2011-07-26 12:05:18 +0200

    Fix #4412, filter table was rendering incorrectly

--- lib/widget/filters.rb
+++ lib/widget/filters.rb
@@ -5,7 +5,7 @@ class Widget::Filters < Widget::Base
     table = content_tag :table, :width => "100%" do
       content_tag :tr do
         content_tag :td do
-          content_tag :table, :id => "filter_table" do
+          content_tag :table, :id => "filter_table", :cellspacing => '0' do
             render_filters
           end
         end
