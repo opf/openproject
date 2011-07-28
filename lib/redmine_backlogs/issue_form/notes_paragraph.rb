df7b7baa858a407639249a76b3c032ca0f44f649
Stephan Eckardt <s.eckardt@finn.de>
2011-07-28 19:32:26 +0200

    removed superfluous here be hooks message from notes paragraph

--- lib/redmine_backlogs/issue_form/notes_paragraph.rb
+++ lib/redmine_backlogs/issue_form/notes_paragraph.rb
@@ -8,8 +8,7 @@ class RedmineBacklogs::IssueForm::NotesParagraph < ChiliProject::Nissue::IssueVi
     s = content_tag(:fieldset, [
       content_tag(:legend, l(:field_notes)),
       t.text_area_tag('issue[notes]', '', :cols => 60, :rows => 10, :class => 'wiki-edit', :id => html_id),
-      t.wikitoolbar_for(html_id),
-      "HERE BE HOOKS"]
+      t.wikitoolbar_for(html_id) ]
     )
   end
 end
