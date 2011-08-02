5c9100e6c96b90c561aac63beb9d2d37043abbc0
Gregor Schmidt <ruby@schmidtwisser.de>
2011-08-02 14:40:39 +0200

    Only showing backlogs settings when backlogs module is activated

--- lib/projects_helper_patch.rb
+++ lib/projects_helper_patch.rb
@@ -1,13 +1,24 @@
-module ProjectIssueStatus
+module Backlogs
   module ProjectHelperPatch
-    ProjectsHelper.module_eval do
-      def project_settings_tabs_with_project_issue_statuses
-        project_settings_tabs_without_project_issue_statuses <<
-          {:name => 'project_issue_statuses', :action => :manage_project_activities,
-          :partial => 'projects/settings/project_issue_statuses',
-          :label => 'backlogs.backlog_settings'}
+    def self.included(base)
+      base.module_eval do
+        def project_settings_tabs_with_project_issue_statuses
+          project_settings_tabs_without_project_issue_statuses.tap do |settings|
+            if @project.module_enabled? 'backlogs'
+              settings << {
+                :name => 'project_issue_statuses',
+                :action => :manage_project_activities,
+                :partial => 'projects/settings/project_issue_statuses',
+                :label => 'backlogs.backlog_settings'
+              }
+            end
+          end
+        end
+
+        alias_method_chain :project_settings_tabs, :project_issue_statuses
       end
-      alias_method_chain :project_settings_tabs, :project_issue_statuses
     end
   end
 end
+
+ProjectsHelper.send(:include, Backlogs::ProjectHelperPatch)
