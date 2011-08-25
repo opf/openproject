37e023ea620abd9f754729de4a41555baed5868d
jwollert <johannes.wollert@gmail.com>
2011-08-25 15:11:17 +0200

    available values get calculated inside the reportingcontroller module, we just need to provide the route (this should be pushed into the engine some sunny day)

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 ActionController::Routing::Routes.draw do |map|
   map.connect 'projects/:project_id/cost_reports.:format', :controller => 'cost_reports', :project_id => /.+/, :action => 'index'
   map.connect 'projects/:project_id/cost_reports/:action/:id', :controller => 'cost_reports', :project_id => /.+/
+  map.connect 'reporting/available_values', :controller => 'cost_reports', :action => "available_values"
 end
