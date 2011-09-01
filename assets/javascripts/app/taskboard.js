9edbf2406c68057fdac3e6b3e4ffced420403843
jwollert <johannes.wollert@gmail.com>
2011-09-01 11:22:51 +0200

    Revert "don't set width and min-width manually for the task board swimlanes. this is handled via css"
    This reverts commit ae3d7ada7282ae71e62da915279994b956903c1d.

--- assets/javascripts/app/taskboard.js
+++ assets/javascripts/app/taskboard.js
@@ -150,6 +150,7 @@ RB.Taskboard = (function ($) {
       }
       $("#col_width input").val(w);
       RB.UserPreferences.set('taskboardColWidth', w);
+      $(".swimlane").width(this.colWidthUnit * w).css('min-width', this.colWidthUnit * w);
     }
   });
 }(jQuery));
