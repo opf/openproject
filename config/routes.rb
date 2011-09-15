6c6aa3a89dfc70c1b5e0ccc3103632c84cd555ed
jwollert <johannes.wollert@gmail.com>
2011-09-15 13:59:37 +0200

    don't need a special route for available_values

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,4 @@
 ActionController::Routing::Routes.draw do |map|
   map.connect 'projects/:project_id/cost_reports.:format', :controller => 'cost_reports', :project_id => /.+/, :action => 'index'
   map.connect 'projects/:project_id/cost_reports/:action/:id', :controller => 'cost_reports', :project_id => /.+/
-  map.connect 'reporting/available_values', :controller => 'cost_reports', :action => "available_values"
 end
