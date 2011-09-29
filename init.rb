29be4e4ed68316362fbee02de68d27ead6a3bfc8
Jens Ulferts <j.ulferts@finn.de>
2011-09-29 15:44:25 +0200

    sort available issue status in backlogs view by their position

--- init.rb
+++ init.rb
@@ -35,7 +35,7 @@ Redmine::Plugin.register :redmine_backlogs do
   name 'Redmine Backlogs'
   author 'relaxdiego, friflaj, Gregor Schmidt, Jens Ulferts'
   description 'A plugin for agile teams'
-  version '0.11.0'
+  version '0.11.1'
 
   requires_redmine_plugin 'chiliproject_nissue', '0.1.0'
 
