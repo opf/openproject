994132a51ad04c9d2fae3c58a4f2217b3d53a4fb
Holger Just <h.just@finn.de>
2011-10-17 18:47:13 +0200

    [#604] Add nicer error formatting, similar to the old style

--- lib/chili_project/liquid/liquid_ext/context.rb
+++ lib/chili_project/liquid/liquid_ext/context.rb
@@ -4,9 +4,20 @@ module ChiliProject
       module Context
         def self.included(base)
           base.send(:include, InstanceMethods)
+          base.class_eval do
+            alias_method_chain :handle_error, :formatting
+          end
         end
 
         module InstanceMethods
+          def handle_error_with_formatting(e)
+            error = handle_error_without_formatting(e)
+            escaped_error = registers[:view].send(:h, error) rescue CGI::escapeHTML(error)
+
+            html = '<div class="flash error">' + escaped_error + '</div>'
+            html_result(html)
+          end
+
           def html_result(html)
             key = nil
             while key.nil? || html_results.has_key?(key)
