8f335043ef540b72f6f1386b1625b2d44d43e931
Gregor Schmidt <ruby@schmidtwisser.de>
2011-10-26 17:36:23 +0200

    Implementing the rebuilding of positions

--- lib/redmine_backlogs/patches/version_patch.rb
+++ lib/redmine_backlogs/patches/version_patch.rb
@@ -16,8 +16,24 @@ module RedmineBacklogs::Patches::VersionPatch
     def rebuild_positions(project = self.project)
       return unless project.backlogs_enabled?
 
-#      stories = self.fixed_issues.find(:all, :conditions => {:project_id => project, :tracker_id => Story.trackers, :position => nil}, :order => 'id')
-#      stories.sort_by(&:id).each(&:insert_at_top)
+      stories_wo_position = self.fixed_issues.find(:all, :conditions => {:project_id => project, :tracker_id => Story.trackers, :position => nil}, :order => 'id')
+
+      if stories_wo_position.present?
+        stories_w_position = self.fixed_issues.find(:all, :conditions => ['project_id = ? AND tracker_id = ? AND position IS NOT NULL', project, Story.trackers], :order => 'position')
+
+        Issue.transaction do
+          # add issues w/o position to the top of the list
+          # and add issues, that have a position, at the end
+
+          stories_wo_position.each_with_index do |story, index|
+            story.send(:update_attribute_silently, 'position', index + 1)
+          end
+
+          stories_w_position.each_with_index do |story, index|
+            story.send(:update_attribute_silently, 'position', index + 1 + stories_wo_position.size)
+          end
+        end
+      end
 
       nil
     end
--- spec/models/version_spec.rb
+++ spec/models/version_spec.rb
@@ -2,4 +2,72 @@ require File.expand_path(File.dirname(__FILE__) + '/../spec_helper')
 
 describe Version do
   it { should have_many :version_settings }
+
+  describe 'rebuild positions' do
+    def build_issue(options = {})
+      Factory.build(:issue, options.reverse_merge(:fixed_version_id => version.id,
+                                                  :priority_id      => priority.id,
+                                                  :project_id       => project.id,
+                                                  :status_id        => status.id,
+                                                  :tracker_id       => tracker.id))
+    end
+
+    def create_issue(options = {})
+      build_issue(options).tap { |i| i.save! }
+    end
+
+    let(:status)   { Factory.create(:issue_status)    }
+    let(:priority) { Factory.create(:priority_normal) }
+    let(:project)  { Factory.create(:project)         }
+
+    let(:tracker) { Factory.create(:tracker, :name => 'Tracker')    }
+
+    let(:version) { Factory.create(:version, :project_id => project.id, :name => 'Version') }
+
+    before do
+      # had problems while writing these specs, that some elements kept creaping
+      # around between tests. This should be fast enough to not harm anybody
+      # while adding an additional safety net to make sure, that everything runs
+      # in isolation.
+      Issue.delete_all
+      IssuePriority.delete_all
+      IssueStatus.delete_all
+      Project.delete_all
+      Tracker.delete_all
+      Version.delete_all
+
+      # enable and configure backlogs
+      project.enabled_module_names = project.enabled_module_names + ["backlogs"]
+      Setting.plugin_redmine_backlogs = {:story_trackers => [tracker.id],
+                                         :task_tracker   => nil}
+
+      # otherwise the tracker id's from the previous test are still active
+      Issue.instance_variable_set(:@backlogs_trackers, nil)
+
+      project.trackers = [tracker]
+      version
+    end
+
+    it 'rebuilds postions' do
+      i1 = create_issue
+      i2 = create_issue
+      i3 = create_issue
+      i4 = create_issue
+      i5 = create_issue
+
+      [i1, i2, i3, i4, i5].each(&:move_to_bottom)
+
+      [i3, i4].map(&:assume_not_in_list)
+
+      version.rebuild_positions(project)
+
+      issues = version.fixed_issues.find(:all, :conditions => {:project_id => project}, :order => 'position')
+
+      issues.map(&:position).should == [1, 2, 3, 4, 5]
+      issues.map(&:subject).should == [i3, i4, i1, i2, i5].map(&:subject)
+
+      issues.map(&:subject).uniq.size.should == 5 # makes sure, that all issue
+            # subjects are uniq, so that the above assertion works as expected
+    end
+  end
 end
