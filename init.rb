dd1bf5bc478936e0c83e45c6d2c19a0c9f8c9311
Gregor Schmidt <ruby@schmidtwisser.de>
2011-10-27 17:21:45 +0200

    Release 0.12.2
    Now with less syntax errors

--- init.rb
+++ init.rb
@@ -34,7 +34,7 @@ Redmine::Plugin.register :redmine_backlogs do
   name 'Redmine Backlogs'
   author 'relaxdiego, friflaj, Gregor Schmidt, Jens Ulferts'
   description 'A plugin for agile teams'
-  version '0.12.1
+  version '0.12.2'
 
   requires_redmine_plugin 'chiliproject_nissue', '0.1.0'
 
