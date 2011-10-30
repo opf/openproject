21a45b4e52f0fcd80849f3b8ac08e8472eca74bd
Holger Just <h.just@finn.de>
2011-10-30 11:24:11 +0100

    [#676] Enforce UTF-8 encodings on the params hash
    Contributed by Toshi MARUYAMA

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -43,6 +43,24 @@ class ApplicationController < ActionController::Base
     end
   end
 
+  # FIXME: Remove this when all of Rack and Rails have learned how to
+  # properly use encodings
+  before_filter :params_filter
+  def params_filter
+    self.utf8nize!(params) if RUBY_VERSION >= '1.9'
+  end
+  def utf8nize!(obj)
+    if obj.is_a? String
+      obj.respond_to?(:force_encoding) ? obj.force_encoding("UTF-8") : obj
+    elsif obj.is_a? Hash
+      obj.each {|k, v| obj[k] = self.utf8nize!(v)}
+    elsif obj.is_a? Array
+      obj.each {|v| self.utf8nize!(v)}
+    else
+      obj
+    end
+  end
+
   before_filter :user_setup, :check_if_login_required, :set_localization
   filter_parameter_logging :password
 
