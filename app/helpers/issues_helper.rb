a88552632b1a2a044c2d1d1833121d67a00ec5a0
Romano Licker <r.licker@finn.de>
2011-11-07 13:40:35 +0100

    [#667] added labels on issue views

--- app/helpers/issues_helper.rb
+++ app/helpers/issues_helper.rb
@@ -67,7 +67,10 @@ module IssuesHelper
     s = '<form><table class="list issues">'
     issue_list(issue.descendants.sort_by(&:lft)) do |child, level|
       s << content_tag('tr',
-             content_tag('td', check_box_tag("ids[]", child.id, false, :id => nil), :class => 'checkbox') +
+             content_tag('td',
+                         "<label>#{l(:description_select_issue) + " #" + child.id.to_s}" +
+                         check_box_tag('ids[]', child.id, false, :id => nil) + '</label>',
+                         :class => 'checkbox') +
              content_tag('td', link_to_issue(child, :truncate => 60), :class => 'subject') +
              content_tag('td', h(child.status)) +
              content_tag('td', link_to_user(child.assigned_to)) +
