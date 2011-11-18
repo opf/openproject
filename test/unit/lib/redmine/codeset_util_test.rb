711f4e63ee6f3943fa653f597bd1f78806fd70c8
tmaruyama <tmaruyama@e93f8b46-1217-0410-a6f0-8f06a7374b81>
2011-11-18 14:57:58 +0000

    move repositories helper to_utf8 tests to new CodesetUtilTest (#2371)
    git-svn-id: svn://rubyforge.org/var/svn/redmine/trunk@7828 e93f8b46-1217-0410-a6f0-8f06a7374b81
    
    Conflicts:
    
    	test/unit/lib/redmine/codeset_util_test.rb

--- test/unit/lib/redmine/codeset_util_test.rb
+++ test/unit/lib/redmine/codeset_util_test.rb
@@ -9,14 +9,12 @@
 # as published by the Free Software Foundation; either version 2
 # of the License, or (at your option) any later version.
 #
-# See doc/COPYRIGHT.rdoc for more details.
-#++
-require File.expand_path('../../../test_helper', __FILE__)
+require File.expand_path('../../../../test_helper', __FILE__)
 
-class RepositoryHelperTest < HelperTestCase
+class Redmine::CodesetUtilTest < HelperTestCase
   include RepositoriesHelper
-
-  def test_from_latin1_to_utf8_for_repositories
+  
+  def test_to_utf8_by_setting_from_latin1
     with_settings :repositories_encodings => 'UTF-8,ISO-8859-1' do
       s1 = "Texte encod\xc3\xa9"
       s2 = "Texte encod\xe9"
@@ -26,12 +24,12 @@ class RepositoryHelperTest < HelperTestCase
         s2.force_encoding("ASCII-8BIT")
         s3.force_encoding("UTF-8")
       end
-      assert_equal s1, to_utf8_for_repositories(s2)
-      assert_equal s1, to_utf8_for_repositories(s3)
+      assert_equal s1, Redmine::CodesetUtil.to_utf8_by_setting(s2)
+      assert_equal s1, Redmine::CodesetUtil.to_utf8_by_setting(s3)
     end
   end
 
-  def test_from_euc_jp_to_utf8_for_repositories
+  def test_to_utf8_by_setting_from_euc_jp
     with_settings :repositories_encodings => 'UTF-8,EUC-JP' do
       s1 = "\xe3\x83\xac\xe3\x83\x83\xe3\x83\x89\xe3\x83\x9e\xe3\x82\xa4\xe3\x83\xb3"
       s2 = "\xa5\xec\xa5\xc3\xa5\xc9\xa5\xde\xa5\xa4\xa5\xf3"
@@ -41,12 +39,12 @@ class RepositoryHelperTest < HelperTestCase
         s2.force_encoding("ASCII-8BIT")
         s3.force_encoding("UTF-8")
       end
-      assert_equal s1, to_utf8_for_repositories(s2)
-      assert_equal s1, to_utf8_for_repositories(s3)
+      assert_equal s1, Redmine::CodesetUtil.to_utf8_by_setting(s2)
+      assert_equal s1, Redmine::CodesetUtil.to_utf8_by_setting(s3)
     end
   end
 
-  def test_to_utf8_for_repositories_should_be_converted_all_latin1_to_utf8
+  def test_to_utf8_by_setting_should_be_converted_all_latin1
     with_settings :repositories_encodings => 'ISO-8859-1' do
       s1 = "\xc3\x82\xc2\x80"
       s2 = "\xC2\x80"
@@ -56,25 +54,25 @@ class RepositoryHelperTest < HelperTestCase
         s2.force_encoding("ASCII-8BIT")
         s3.force_encoding("UTF-8")
       end
-      assert_equal s1, to_utf8_for_repositories(s2)
-      assert_equal s1, to_utf8_for_repositories(s3)
+      assert_equal s1, Redmine::CodesetUtil.to_utf8_by_setting(s2)
+      assert_equal s1, Redmine::CodesetUtil.to_utf8_by_setting(s3)
     end
   end
 
-  def test_to_utf8_for_repositories_blank_string
-    assert_equal "",  to_utf8_for_repositories("")
-    assert_equal nil, to_utf8_for_repositories(nil)
+  def test_to_utf8_by_setting_blank_string
+    assert_equal "",  Redmine::CodesetUtil.to_utf8_by_setting("")
+    assert_equal nil, Redmine::CodesetUtil.to_utf8_by_setting(nil)
   end
 
-  def test_to_utf8_for_repositories_returns_ascii_as_utf8
+  def test_to_utf8_by_setting_returns_ascii_as_utf8
     s1 = "ASCII"
     s2 = s1.dup
     if s1.respond_to?(:force_encoding)
       s1.force_encoding("UTF-8")
       s2.force_encoding("ISO-8859-1")
     end
-    str1 = to_utf8_for_repositories(s1)
-    str2 = to_utf8_for_repositories(s2)
+    str1 = Redmine::CodesetUtil.to_utf8_by_setting(s1)
+    str2 = Redmine::CodesetUtil.to_utf8_by_setting(s2)
     assert_equal s1, str1
     assert_equal s1, str2
     if s1.respond_to?(:force_encoding)
@@ -83,10 +81,25 @@ class RepositoryHelperTest < HelperTestCase
     end
   end
 
-  def test_to_utf8_for_repositories_invalid_utf8_sequences_should_be_stripped
+  def test_to_utf8_by_setting_invalid_utf8_sequences_should_be_stripped
     with_settings :repositories_encodings => '' do
-      s1 = File.read(Rails.root.join('test/fixtures/encoding/iso-8859-1.txt'))
-      str = to_utf8_for_repositories(s1)
+      # s1 = File.read(Rails.root.join('test/fixtures/encoding/iso-8859-1.txt'))
+      s1 = "Texte encod\xe9 en ISO-8859-1."
+      s1.force_encoding("ASCII-8BIT") if s1.respond_to?(:force_encoding)
+      str = Redmine::CodesetUtil.to_utf8_by_setting(s1)
+      if str.respond_to?(:force_encoding)
+        assert str.valid_encoding?
+        assert_equal "UTF-8", str.encoding.to_s
+      end
+      assert_equal "Texte encod? en ISO-8859-1.", str
+    end
+  end
+
+  def test_to_utf8_by_setting_invalid_utf8_sequences_should_be_stripped_ja_jis
+    with_settings :repositories_encodings => 'ISO-2022-JP' do
+      s1 = "test\xb5\xfetest\xb5\xfe"
+      s1.force_encoding("ASCII-8BIT") if s1.respond_to?(:force_encoding)
+      str = Redmine::CodesetUtil.to_utf8_by_setting(s1)
       if str.respond_to?(:force_encoding)
         assert_equal "Texte encod? en ISO-8859-1.", str
         assert str.valid_encoding?
