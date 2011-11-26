ce5ffed6a3df2391e1704a0f9a10731221db1603
Holger Just <h.just@finn.de>
2011-11-26 12:26:11 +0100

    Require bundler 1.0.6.
    Only that version introduced the mingw platforms we use in our Gemfile.

--- config/preinitializer.rb
+++ config/preinitializer.rb
@@ -19,9 +19,9 @@ rescue LoadError
   raise "Could not load the bundler gem. Install it with `gem install bundler`."
 end
 
-if Gem::Version.new(Bundler::VERSION) <= Gem::Version.new("0.9.24")
-  raise RuntimeError, "Your bundler version is too old for Rails 2.3." +
-   "Run `gem install bundler` to upgrade."
+if Gem::Version.new(Bundler::VERSION) < Gem::Version.new("1.0.6")
+  raise RuntimeError, "Your bundler version is too old. We require " +
+   "at least version 1.0.6. Run `gem install bundler` to upgrade."
 end
 
 begin
@@ -29,6 +29,6 @@ begin
   ENV["BUNDLE_GEMFILE"] = File.expand_path("../../Gemfile", __FILE__)
   Bundler.setup
 rescue Bundler::GemNotFound
-  raise RuntimeError, "Bundler couldn't find some gems." +
+  raise RuntimeError, "Bundler couldn't find some gems. " +
     "Did you run `bundle install`?"
 end
