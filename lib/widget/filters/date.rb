bfdb7269a9206f4adacd6187ba2a969960cc8b1e
Romano Licker <r.licker@finn.de>
2011-11-29 09:24:23 +0100

    [accessibility] add label to 'date'-fields

--- lib/widget/filters/date.rb
+++ lib/widget/filters/date.rb
@@ -10,6 +10,7 @@ class Widget::Filters::Date < Widget::Filters::Base
     id_prefix = "#{filter_class.underscore_name}_"
 
     write(content_tag :td do
+      label1 = label_tag "#{id_prefix}arg_1_val",l(:label_date), :class => 'hidden-for-sighted'
       arg1 = content_tag :span, :id => "#{id_prefix}arg_1", :class => "filter_values" do
         text1 = text_field_tag name, @filter.values.first.to_s,
                                :size => 10,
@@ -17,8 +18,9 @@ class Widget::Filters::Date < Widget::Filters::Base
                                :id => "#{id_prefix}arg_1_val",
                                :'data-type' => 'date'
         cal1 = calendar_for("#{id_prefix}arg_1_val")
-        text1 + cal1
+        label1 + text1 + cal1
       end
+      label2 = label_tag "#{id_prefix}arg_2_val",l(:label_date), :class => 'hidden-for-sighted'
       arg2 = content_tag :span, :id => "#{id_prefix}arg_2", :class => "between_tags" do
         text2 = text_field_tag "#{name}", @filter.values.second.to_s,
                                :size => 10,
@@ -26,7 +28,7 @@ class Widget::Filters::Date < Widget::Filters::Base
                                :id => "#{id_prefix}arg_2_val",
                                :'data-type' => 'date'
         cal2 = calendar_for "#{id_prefix}arg_2_val"
-        text2 + cal2
+        label2 + text2 + cal2
       end
       arg1 + arg2
     end)
