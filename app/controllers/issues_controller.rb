afb4e6968f5b452459d45ff91e36421c93ee17dc
Felix Schäfer <felix@fachschaften.org>
2011-11-29 14:09:47 +0100

    Correct missing query object on IssuesController#all

--- app/controllers/issues_controller.rb
+++ app/controllers/issues_controller.rb
@@ -25,7 +25,7 @@ class IssuesController < ApplicationController
   before_filter :find_optional_project, :only => [:index, :all]
   before_filter :check_for_default_issue_status, :only => [:new, :create]
   before_filter :build_new_issue_from_params, :only => [:new, :create]
-  before_filter :retrieve_query, :only => :index
+  before_filter :retrieve_query, :only => [:index, :all]
 
   accept_key_auth :index, :show, :create, :update, :destroy
 
