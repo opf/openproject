7be0043fb9142ceb8620080285464d1d9769ed54
Tim Felgentreff <timfelgentreff@gmail.com>
2011-12-02 15:56:07 +0100

    license notice for code files, at least

--- app/controllers/my_projects_overviews_controller.rb
+++ app/controllers/my_projects_overviews_controller.rb
@@ -1,14 +1,15 @@
+#-- encoding: UTF-8
 #-- copyright
 # ChiliProject is a project management system.
 #
-# Copyright (C) 2010-2011 the ChiliProject Team
+# Copyright (C) 2010-2011 Finn GmbH
 #
 # This program is free software; you can redistribute it and/or
 # modify it under the terms of the GNU General Public License
 # as published by the Free Software Foundation; either version 2
 # of the License, or (at your option) any later version.
 #
-# See doc/COPYRIGHT.rdoc for more details.
+# See LICENSE for more details.
 #++
 
 class MyProjectsOverviewsController < ApplicationController
--- app/models/my_projects_overview.rb
+++ app/models/my_projects_overview.rb
@@ -1,3 +1,17 @@
+#-- encoding: UTF-8
+#-- copyright
+# ChiliProject is a project management system.
+#
+# Copyright (C) 2010-2011 Finn GmbH
+#
+# This program is free software; you can redistribute it and/or
+# modify it under the terms of the GNU General Public License
+# as published by the Free Software Foundation; either version 2
+# of the License, or (at your option) any later version.
+#
+# See LICENSE for more details.
+#++
+
 class MyProjectsOverview < ActiveRecord::Base
   unloadable
 
