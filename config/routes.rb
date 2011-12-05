b1a6e69931ae3f1fe1353565fb405198b2246655
Gregor Schmidt <g.schmidt@finn.de>
2011-12-05 16:24:29 +0100

    adding route to make additional format work on global issue list

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,4 @@
 ActionController::Routing::Routes.draw do |map|
   map.connect 'projects/:project_id/issues/printable', :controller => 'issues', :action => 'printable'
+  map.connect 'issues/printable', :controller => 'issues', :action => 'printable'
 end
