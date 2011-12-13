b8a7f2923a8f71bac8bde9a7cb04aa2636f663c6
Eric Davis <edavis@littlestreamsoftware.com>
2011-12-13 10:46:50 -0800

    Remove arbitrary limit on the Time Entry Report columns
    Appears it was put in place to prevent wide reports but was actually
    limiting the data, especially when putting a day in each column.

--- app/controllers/time_entry_reports_controller.rb
+++ app/controllers/time_entry_reports_controller.rb
@@ -72,8 +72,7 @@ class TimeEntryReportsController < ApplicationController
       @periods = []
       # Date#at_beginning_of_ not supported in Rails 1.2.x
       date_from = @from.to_time
-      # 100 columns max
-      while date_from <= @to.to_time && @periods.length < 100
+      while date_from <= @to.to_time
         case @columns
         when 'year'
           @periods << "#{date_from.year}"
