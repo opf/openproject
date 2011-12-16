430d9963c33f7639c03077ed02eeccc70d3fba5d
Gregor Schmidt <g.schmidt@finn.de>
2011-12-16 08:27:21 +0100

    Releasing 1.1.0
    Minor Changes
    
    * Tested with the new layout of yet to be released ChiliProject 3.0
    * Loading jQuery only if ChiliProject doesn't
    
    Bug Fixes
    
    * Fixing syntactic ambiguity in Ruby 1.9 (#1)

--- init.rb
+++ init.rb
@@ -38,7 +38,7 @@ Redmine::Plugin.register :backlogs do
   url 'http://github.com/finnlabs/chiliproject_backlogs'
   author_url 'http://www.finn.de/'
 
-  version '1.0.2'
+  version '1.1.0'
 
   requires_redmine_plugin 'chiliproject_nissue', '1.0.0'
 
