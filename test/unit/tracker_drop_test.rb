2f0d13149de027dd3a0830f9a9da34b736bf7878
Eric Davis <edavis@littlestreamsoftware.com>
2011-12-17 15:50:52 -0800

    Copyright header update

--- test/unit/tracker_drop_test.rb
+++ test/unit/tracker_drop_test.rb
@@ -1,3 +1,16 @@
+#-- copyright
+# ChiliProject is a project management system.
+#
+# Copyright (C) 2010-2011 the ChiliProject Team
+#
+# This program is free software; you can redistribute it and/or
+# modify it under the terms of the GNU General Public License
+# as published by the Free Software Foundation; either version 2
+# of the License, or (at your option) any later version.
+#
+# See doc/COPYRIGHT.rdoc for more details.
+#++
+
 require File.expand_path('../../test_helper', __FILE__)
 
 class TrackerDropTest < ActiveSupport::TestCase
