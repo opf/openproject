e0e86c92adc11e6b57a4cfc535afc7b13a16fd87
Gregor Schmidt <g.schmidt@finn.de>
2011-12-20 12:32:15 +0100

    adding join call to all arrays in content_tag
    Array#to_s acts differently on 1.8.x and 1.9

--- lib/backlogs/issue_actions.rb
+++ lib/backlogs/issue_actions.rb
@@ -8,6 +8,6 @@ class Backlogs::IssueActions < ChiliProject::Nissue::View
     content_tag(:div, [
         t.watcher_link(@issue, User.current, :class => css_class, :replace => ".#{css_class}"),
         (t.modal_link_to(l(:button_update), {:controller => 'issue_boxes', :action => 'edit', :id => @issue }, :class => 'icon icon-edit') if t.authorize_for('issue_boxes', 'edit'))
-      ], :class => 'contextual')
+      ].join, :class => 'contextual')
   end
 end
