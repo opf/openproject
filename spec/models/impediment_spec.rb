67142e40bfff1b0f7a2f664e67b1a712ff4d1f7a
Gregor Schmidt <g.schmidt@finn.de>
2011-12-20 11:04:13 +0100

    Clearing cache to make sure, that setting changes are working

--- spec/models/impediment_spec.rb
+++ spec/models/impediment_spec.rb
@@ -43,6 +43,7 @@ describe Impediment do
                                                 :status => issue_status1)}
 
   before(:each) do
+    Rails.cache.clear
     Setting.plugin_backlogs = {:points_burn_direction => "down",
                                :wiki_template => "",
                                :card_spec => "Sattleford VM-5040",
