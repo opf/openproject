ff3602b7c4968cb9eea3e70ca90f8299ddc854ec
Gregor Schmidt <g.schmidt@finn.de>
2012-01-02 16:09:43 +0100

    Releasing 1.2.0
    * Improved support for Ruby 1.9
    * Improved support for proposed fix of Chili #780

--- init.rb
+++ init.rb
@@ -39,7 +39,7 @@ Redmine::Plugin.register :backlogs do
   url 'http://github.com/finnlabs/chiliproject_backlogs'
   author_url 'http://www.finn.de/'
 
-  version '1.1.0'
+  version '1.2.0'
 
   requires_redmine_plugin 'chiliproject_nissue', '1.0.0'
 
