d9a0ac37ebd796a8da621644344e195629a68e93
Holger Just <hjust@meine-er.de>
2012-01-16 13:28:43 +0100

    Move Redmine::Info to ChiliProject::Info

--- lib/redmine/info.rb
+++ lib/redmine/info.rb
@@ -13,19 +13,6 @@
 #++
 
 module Redmine
-  module Info
-    class << self
-      def app_name; 'ChiliProject' end
-      def url; 'https://www.chiliproject.org/' end
-      def help_url
-        "https://www.chiliproject.org/help/v#{ChiliProject::VERSION::MAJOR.to_s}"
-      end
-      def versioned_name; "#{app_name} #{Redmine::VERSION}" end
-
-      # Creates the url string to a specific Redmine issue
-      def issue(issue_id)
-        url + 'issues/' + issue_id.to_s
-      end
-    end
-  end
+  # THIS IS A REDMINE COMPATIBILITY INTERFACE
+  Info = ChiliProject::Info
 end
