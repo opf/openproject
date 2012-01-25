c5b76c2b7b9e5fe04b907e5a1da0037184c6e889
Gregor Schmidt <g.schmidt@finn.de>
2012-01-25 16:01:07 +0100

    Releasing 1.2.4
    * Minor accessibility improvement

--- init.rb
+++ init.rb
@@ -39,7 +39,7 @@ Redmine::Plugin.register :backlogs do
   url 'http://github.com/finnlabs/chiliproject_backlogs'
   author_url 'http://www.finn.de/'
 
-  version '1.2.3'
+  version '1.2.4'
 
   requires_redmine_plugin 'chiliproject_nissue', '1.0.0'
 
