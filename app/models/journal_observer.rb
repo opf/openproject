aa5be5132dd57cb6f58b021c6c9f9e4f2baed1e1
Holger Just <h.just@finn.de>
2012-02-06 14:31:06 +0100

    [#593] Generate wiki content notifications in the JournalObserver - backported for 2.7
    This will prevent the usage of the wrong wiki_content status.
    
    The code is not overly pretty and deserves a thorough refactoring, but at
    least it solves the problem at hand.

--- app/models/journal_observer.rb
+++ app/models/journal_observer.rb
@@ -16,8 +16,24 @@ class JournalObserver < ActiveRecord::Observer
   attr_accessor :send_notification
 
   def after_create(journal)
-    if journal.type == "IssueJournal" and !journal.initial? and send_notification
-      after_create_issue_journal(journal)
+    case journal.type
+    when "IssueJournal"
+      if !journal.initial? && send_notification
+        after_create_issue_journal(journal)
+      end
+    when "WikiContentJournal"
+      wiki_content = journal.journaled
+      wiki_page = wiki_content.page
+
+      if journal.initial?
+        if Setting.notified_events.include?('wiki_content_added')
+          Mailer.deliver_wiki_content_added(wiki_content)
+        end
+      else
+        if Setting.notified_events.include?('wiki_content_updated')
+          Mailer.deliver_wiki_content_updated(wiki_content)
+        end
+      end
     end
     clear_notification
   end
