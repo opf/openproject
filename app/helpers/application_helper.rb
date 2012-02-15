4d87d9a45e10f7b635d3680d22c1dc5601ab46e8
Romano Licker <r.licker@finn.de>
2012-02-15 14:15:03 +0100

    [breadcrumb] show last 3 elements, hide first project

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -426,10 +426,9 @@ module ApplicationHelper
     elements = args.flatten
     cutme_elements = []
     breadcrumb_elements = [content_tag(:li, elements.shift.to_s, :style => 'list-style-image:none;')]
-    breadcrumb_elements << content_tag(:li, elements.shift.to_s) if elements.first
 
     if elements.size > 2
-      normal_elements = elements.pop(2)
+      normal_elements = elements.pop(3)
       cutme_elements = elements
     else
       normal_elements = elements
