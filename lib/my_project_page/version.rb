62aedba495f6573d1819f23c094dcae7d16c8d77
Jens Ulferts <j.ulferts@finn.de>
2012-02-21 13:49:23 +0100

    Releasing 1.0.0

---
+++ lib/my_project_page/version.rb
@@ -0,0 +1,26 @@
+module MyProjectPage
+  module Version
+    def to_s
+      [major, minor, patch].join('.')
+    end
+
+    def full
+      to_s
+    end
+
+    def major
+      1
+    end
+
+    def minor
+      0
+    end
+
+    def patch
+      0
+    end
+
+    extend self
+  end
+end
+
