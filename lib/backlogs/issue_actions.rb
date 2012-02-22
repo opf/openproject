8adbef0ae273b51db9edafa4522b84761da94350
Romano Licker <r.licker@finn.de>
2012-02-22 11:23:18 +0100

    [designfix] switch position of elements in dom tree with reference to the new issue show view

--- lib/backlogs/issue_actions.rb
+++ lib/backlogs/issue_actions.rb
@@ -6,8 +6,9 @@ class Backlogs::IssueActions < ChiliProject::Nissue::View
   def render(t)
     css_class = "watcher_link_#{@issue.id}"
     content_tag(:div, [
-        t.watcher_link(@issue, User.current, :class => css_class, :replace => ".#{css_class}"),
-        (t.modal_link_to(l(:button_update), {:controller => 'issue_boxes', :action => 'edit', :id => @issue }, :class => 'icon icon-edit') if t.authorize_for('issue_boxes', 'edit'))
+        (t.modal_link_to(l(:button_update), {:controller => 'issue_boxes', :action => 'edit', :id => @issue }, :class => 'icon icon-edit') if t.authorize_for('issue_boxes', 'edit')),
+        " ",
+        t.watcher_link(@issue, User.current, :class => css_class, :replace => ".#{css_class}")
       ].join, :class => 'contextual')
   end
 end
