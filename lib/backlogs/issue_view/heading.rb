94919675b5d9c2dcf9de99ecd45d581885c726d9
Romano Licker <r.licker@finn.de>
2012-02-22 11:24:52 +0100

    [designfix] adjust code on the example of new issue show view

--- lib/backlogs/issue_view/heading.rb
+++ lib/backlogs/issue_view/heading.rb
@@ -1,5 +1,5 @@
 class Backlogs::IssueView::Heading < ChiliProject::Nissue::IssueView::Heading
   def render_issue_subject_with_tree(t)
-    content_tag(:div, content_tag('h3', h(@issue.subject)), :class => 'subject')
+    content_tag('h3', h(@issue.subject))
   end
 end
