6b731840553384a9f61fa7acc462b12e8cdbe369
Jens Ulferts <j.ulferts@finn.de>
2012-02-23 17:43:29 +0100

    have cost types rendered as a fieldset with radiobuttons along with the other settings

---
+++ lib/widget/cost_types.rb
@@ -0,0 +1,22 @@
+class Widget::CostTypes < Widget::Base
+  def render_with_options(options, &block)
+    @cost_types = options.delete(:cost_types)
+    @selected_type_id = options.delete(:selected_type_id)
+
+    super(options, &block)
+  end
+
+  def render
+    write contents
+  end
+
+  def contents
+    content_tag :div do
+      available_cost_type_tabs(@subject).map do |id, label|
+        type_selection = radio_button_tag("unit", id, id == @selected_type_id)
+        type_selection += label_tag "unit_#{id}", h(label)
+        type_selection
+      end.join("</br>")
+    end
+  end
+end
