426e372200fba953f84cd488cc7c2fc30b7acfa9
Jens Ulferts <j.ulferts@finn.de>
2012-05-07 23:31:00 +0200

    Releasing 2.1.1
    * mass_assignment adaptations
    * add missing translations
    * save and show status of newly created stories

--- init.rb
+++ init.rb
@@ -39,7 +39,7 @@ Redmine::Plugin.register :backlogs do
   url 'http://github.com/finnlabs/chiliproject_backlogs'
   author_url 'http://www.finn.de/'
 
-  version '2.1.0'
+  version '2.1.1'
 
   requires_redmine_plugin 'chiliproject_nissue', '1.0.0'
 
