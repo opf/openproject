51be0057608b449ba0a4e4d8185e18b1cccfe00f
Romano Licker <r.licker@finn.de>
2012-05-09 14:42:17 +0200

    remove exception which was thrown on mass assign protected attributes
    this method is thrown even if the attributes are
    protected via e.g. attr_protected
    additionally it enforces the use of safe_attributes
    which is not necessary

--- config/initializers/mass_assignment.rb
+++ config/initializers/mass_assignment.rb
@@ -18,19 +18,6 @@ class ActiveRecord::Base
   # end
 
   private
-
-  # this method will be called when there is an attempt to mass assign protected attributes
-  # it will log the tampered attributes and the class name to the logfile
-  # in development mode it will also throw an exception
-  #   unless the protected attributes are active record's defaults because it deals with them nicely (id and type usually)
-  def log_protected_attribute_removal(*attributes)
-    warning = "WARNING: Can't mass-assign these protected attributes: #{attributes.join(', ')} on class #{self.class.name}"
-    logger.debug warning
-    if (attributes.flatten - attributes_protected_by_default).any?
-      raise Exception.new warning unless Rails.env.production?
-    end
-  end
-
   # def protected_association?(association_id)
   #   PROTECTED_ASSOCIATIONS.include? association_id.to_s
   # end
