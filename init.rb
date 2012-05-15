f9d9ddc3fd45a21de26637131f5e41e177515f43
Martin Linkhorst <m.linkhorst@googlemail.com>
2012-05-15 17:17:31 +0200

    allow removal of blocks

--- init.rb
+++ init.rb
@@ -10,6 +10,7 @@ Redmine::Plugin.register :chiliproject_my_project_page do
   Redmine::AccessControl.permission(:view_project).actions << "my_projects_overviews/index"
   Redmine::AccessControl.permission(:edit_project).actions << "my_projects_overviews/page_layout" <<
                                                               "my_projects_overviews/add_block" <<
+                                                              "my_projects_overviews/remove_block" <<
                                                               "my_projects_overviews/update_custom_element" <<
                                                               "my_projects_overviews/order_blocks" <<
                                                               "my_projects_overviews/destroy_attachment"
