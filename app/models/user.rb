5d6c5da9e71501056ee0164f45db24a4cfde2341
Jens Ulferts <j.ulferts@finn.de>
2012-05-18 15:24:45 +0200

    removing User status ANONYMOUS
    * not used anywhere

--- app/models/user.rb
+++ app/models/user.rb
@@ -18,7 +18,6 @@ class User < Principal
   include Redmine::SafeAttributes
 
   # Account statuses
-  STATUS_ANONYMOUS  = 0
   STATUS_ACTIVE     = 1
   STATUS_REGISTERED = 2
   STATUS_LOCKED     = 3
