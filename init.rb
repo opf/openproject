d2233b138e1cb9f6ac87b41ccdbcd5083141f9e6
Romano Licker <r.licker@finn.de>
2012-05-25 13:24:06 +0200

    Releasing 2.1.2

--- init.rb
+++ init.rb
@@ -62,7 +62,7 @@ Redmine::Plugin.register :redmine_costs do
   author 'Holger Just @ finnlabs'
   author_url 'http://finn.de/team#h.just'
   description 'The costs plugin provides basic cost management functionality for Redmine.'
-  version '2.1.1'
+  version '2.1.2'
 
   requires_redmine :version_or_higher => '0.9'
 
