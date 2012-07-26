c55739cdf71170a79a1a2a60c956aab98bcb47df
Martin Linkhorst <m.linkhorst@googlemail.com>
2012-07-26 14:34:40 +0200

    fixed flash messages

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -256,9 +256,9 @@ module ApplicationHelper
   # Renders flash messages
   def render_flash_messages
     if User.current.impaired?
-      flash.map { |k,v| content_tag('div', content_tag('a', v, :href => 'javascript:;'), :class => "flash #{k}") }.join
+      flash.map { |k,v| content_tag('div', content_tag('a', v, :href => 'javascript:;'), :class => "flash #{k}") }.join.html_safe
     else
-      flash.map { |k,v| content_tag('div', v, :class => "flash #{k}") }.join
+      flash.map { |k,v| content_tag('div', v, :class => "flash #{k}") }.join.html_safe
     end
   end
 
