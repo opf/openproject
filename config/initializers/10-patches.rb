12e24f0f47f18330a2670d4b9ea68c93fb0ae0f5
Jens Ulferts <j.ulferts@finn.de>
2012-07-26 08:38:24 +0200

    REVIEW: deactivates alias method chaining of TMail::Unquoter#convert_to
    * get rails 3 to boot first

--- config/initializers/10-patches.rb
+++ config/initializers/10-patches.rb
@@ -235,13 +235,13 @@ ActionMailer::Base.send :include, AsynchronousMailer
 
 # TMail::Unquoter.convert_to_with_fallback_on_iso_8859_1 introduced in TMail 1.2.7
 # triggers a test failure in test_add_issue_with_japanese_keywords(MailHandlerTest)
-module TMail
-  class Unquoter
-    class << self
-      alias_method :convert_to, :convert_to_without_fallback_on_iso_8859_1
-    end
-  end
-end
+# module TMail
+#  class Unquoter
+#    class << self
+#      alias_method :convert_to, :convert_to_without_fallback_on_iso_8859_1
+#    end
+#  end
+# end
 
 module ActionController
   module MimeResponds
