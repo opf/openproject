5e107042d24e16c955ad8da38c4d1f9a267c36c5
Martin Linkhorst <m.linkhorst@googlemail.com>
2012-07-26 16:53:13 +0200

    REVIEW: make wikis work. TODO: why do they show up twice

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -243,14 +243,14 @@ module ApplicationHelper
       content << "<ul class=\"pages-hierarchy\">\n"
       pages[node].each do |page|
         content << "<li>"
-        content << link_to(h(page.pretty_title), {:controller => 'wiki', :action => 'show', :project_id => page.project, :id => page.title},
+        content << link_to(page.pretty_title, project_wiki_path(page.project, page),
                            :title => (options[:timestamp] && page.updated_on ? l(:label_updated_time, distance_of_time_in_words(Time.now, page.updated_on)) : nil))
         content << "\n" + render_page_hierarchy(pages, page.id, options) if pages[page.id]
         content << "</li>\n"
       end
       content << "</ul>\n"
     end
-    content
+    content.html_safe
   end
 
   # Renders flash messages
--- app/models/wiki_page.rb
+++ app/models/wiki_page.rb
@@ -158,6 +158,10 @@ class WikiPage < ActiveRecord::Base
     self.parent = parent_page
   end
 
+  def to_param
+    title
+  end
+
   protected
 
   def validate
