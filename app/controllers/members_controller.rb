89cc1d6c484fb1083c4a93687b08b67638fced1f
Romano Licker <r.licker@finn.de>
2012-07-26 12:11:24 +0200

    remember page on all actions on settings/members
    "stay where you are" shouldnt confuse the user too much

--- app/controllers/members_controller.rb
+++ app/controllers/members_controller.rb
@@ -60,7 +60,7 @@ class MembersController < ApplicationController
       member.save
 
   	 respond_to do |format|
-        format.html { redirect_to :controller => 'projects', :action => 'settings', :tab => 'members', :id => @project }
+        format.html { redirect_to :controller => 'projects', :action => 'settings', :tab => 'members', :id => @project, :page => params[:page] }
         format.js {
           render(:update) { |page|
             page.replace_html "tab-content-members", :partial => 'projects/settings/members'
