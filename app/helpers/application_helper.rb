d3e7c96eb70708d5a4ef97487f5919ce4d32805a
Martin Linkhorst <m.linkhorst@googlemail.com>
2012-07-26 17:51:41 +0200

    fix minimum characters hint

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1115,6 +1115,6 @@ module ApplicationHelper
   end
 
   def password_complexity_requirements
-    "<em>" + l(:text_caracters_minimum, :count => Setting.password_min_length) + "</em>"
+    raw "<em>" + l(:text_caracters_minimum, :count => Setting.password_min_length) + "</em>"
   end
 end
