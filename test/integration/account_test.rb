5112ad35e81f84f46b87cc066a5d227aac2059f8
Jens Ulferts <j.ulferts@finn.de>
2012-08-08 10:08:39 +0200

    grants autologin test more time before failing

--- test/integration/account_test.rb
+++ test/integration/account_test.rb
@@ -71,7 +71,7 @@ class AccountTest < ActionController::IntegrationTest
     assert_template 'my/page'
     assert_equal user.id, session[:user_id]
     assert_not_nil user.reload.last_login_on
-    assert user.last_login_on.utc > 10.second.ago.utc
+    assert user.last_login_on.utc > 20.second.ago.utc
   end
 
   def test_lost_password
