987a12cce80aaa8642ae1eef6912ffa51390cf21
Philipp Tessenow <philipp.tessenow@student.hpi.uni-potsdam.de>
2012-08-09 08:37:08 +0200

    let tinyMCE not overwrite the textarea on save

--- vendor/plugins/wysiwyg_editing/assets/javascripts/app/tiny_mce_configuration.js
+++ vendor/plugins/wysiwyg_editing/assets/javascripts/app/tiny_mce_configuration.js
@@ -11,10 +11,11 @@ tinyMCE.init({
   theme_advanced_toolbar_align : "left",
   theme_advanced_statusbar_location : "bottom",
   theme_advanced_resizing : true,
+  add_form_submit_trigger : false,
 
   // Skin options
   skin : "o2k7",
-  skin_variant : "silver",
+  skin_variant : "silver"
 
   // Example content CSS (should be your site CSS)
   //content_css : "css/example.css",
