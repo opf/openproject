86335e596db21a0c1ce7317cb9a3429c6443d87c
Philipp Tessenow <philipp.tessenow@student.hpi.uni-potsdam.de>
2012-08-13 10:00:23 +0200

    activate visualblocks tinymce plugin and add button for that

--- vendor/plugins/wysiwyg_editing/assets/javascripts/app/tiny_mce_configuration.js
+++ vendor/plugins/wysiwyg_editing/assets/javascripts/app/tiny_mce_configuration.js
@@ -2,10 +2,10 @@ tinyMCE.init({
   // General options
   mode : "none",
   theme : "advanced",
-  plugins : "autolink,lists,spellchecker,style,table,advhr,advimage,advlink,template,iespell,inlinepopups,insertdatetime,media,searchreplace,contextmenu,paste,noneditable,visualchars,nonbreaking,xhtmlxtras",
+  plugins : "visualblocks, autolink,lists,spellchecker,style,table,advhr,advimage,advlink,template,iespell,inlinepopups,insertdatetime,media,searchreplace,contextmenu,paste,noneditable,visualchars,nonbreaking,xhtmlxtras",
 
   // Theme options
-  theme_advanced_buttons1 : "bold,italic,underline,strikethrough,|,justifyleft,justifycenter,justifyright,justifyfull,|,formatselect,|,cut,copy,paste,pastetext,pasteword,|,search,replace,|,bullist,numlist,|,outdent,indent,blockquote,|,undo,redo,|,link,unlink,image,cleanup,code,|,insertdate,inserttime,preview",
+  theme_advanced_buttons1 : "bold,italic,underline,strikethrough,|,justifyleft,justifycenter,justifyright,justifyfull,|,formatselect,|,cut,copy,paste,pastetext,pasteword,|,search,replace,|,bullist,numlist,|,outdent,indent,blockquote,|,undo,redo,|,link,unlink,image,cleanup,visualblocks,code,|,insertdate,inserttime",
   theme_advanced_buttons2 : "forecolor,backcolor,|,tablecontrols,|,hr,removeformat,visualaid,|,sub,sup,|,charmap,iespell,media,advhr,|,spellchecker,|,cite,abbr,acronym,del,ins,attribs,|,visualchars,nonbreaking,template,|,insertfile,insertimage",
   theme_advanced_toolbar_location : "top",
   theme_advanced_toolbar_align : "left",
