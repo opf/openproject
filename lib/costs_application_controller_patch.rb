fa58841f8491e2fc4ff7c509b6025b81e79ce5c3
Jens Ulferts <j.ulferts@finn.de>
2012-08-13 14:13:59 +0200

    check allowed to in projects (Array) index if project context does not exist

--- lib/costs_application_controller_patch.rb
+++ lib/costs_application_controller_patch.rb
@@ -12,7 +12,7 @@ module CostsApplicationControllerPatch
   module InstanceMethods
     # Authorize the user for the requested action
     def authorize_with_for_user(ctrl = params[:controller], action = params[:action], global = false, for_user=@user)
-      allowed = User.current.allowed_to?({:controller => ctrl, :action => action}, @project, :global => global, :for => for_user)
+      allowed = User.current.allowed_to?({:controller => ctrl, :action => action}, @project || @projects, :global => global, :for => for_user)
       allowed ? true : deny_access
     end
   end
