a998a531a204e77f9e9ca481a5ff7156901122d3
Martin Linkhorst <m.linkhorst@googlemail.com>
2012-09-07 18:31:16 +0200

    re-enable the asset pipeline for rails 3.2

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -30,8 +30,8 @@ OpenProject::Application.configure do
   config.active_record.auto_explain_threshold_in_seconds = 0.5
 
   # Do not compress assets
-  # config.assets.compress = false
+  config.assets.compress = false
 
   # Expands the lines which load the assets
-  # config.assets.debug = true
+  config.assets.debug = true
 end
