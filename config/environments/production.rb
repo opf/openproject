9bdb5038a86b63fba8584bf37c2aed030248d13f
Philipp Tessenow <philipp.tessenow@student.hpi.uni-potsdam.de>
2012-10-05 10:54:32 +0200

    add assets to precompile pipeline. may be evaluated later if they should be served within applications.css/.js

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -47,7 +47,7 @@ OpenProject::Application.configure do
   # config.action_controller.asset_host = "http://assets.example.com"
 
   # Precompile additional assets (application.js, application.css, and all non-JS/CSS are already added)
-  # config.assets.precompile += %w( search.js )
+  config.assets.precompile += %w( scm.css select_list_move.js context_menu.js context_menu.css )
 
   # Disable delivery errors, bad email addresses will be ignored
   # config.action_mailer.raise_delivery_errors = false
