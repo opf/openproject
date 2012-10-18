29d5c5f173945f15ca2ec8d36cf8939151d58ade
Martin Linkhorst <m.linkhorst@googlemail.com>
2012-10-18 13:42:37 +0200

    REVIEW: disable whitelisting for now

--- config/application.rb
+++ config/application.rb
@@ -71,7 +71,7 @@ module OpenProject
     # This will create an empty whitelist of attributes available for mass-assignment for all models
     # in your app. As such, your models will need to explicitly whitelist or blacklist accessible
     # parameters by using an attr_accessible or attr_protected declaration.
-    config.active_record.whitelist_attributes = true
+    config.active_record.whitelist_attributes = false
 
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
