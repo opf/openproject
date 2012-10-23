5f89c5497ee297a18a6f66c698111a4c33a359a7
Martin Linkhorst <m.linkhorst@googlemail.com>
2012-10-23 17:56:42 +0200

    use the correct css classes for the new coderay version

--- test/unit/helpers/application_helper_test.rb
+++ test/unit/helpers/application_helper_test.rb
@@ -507,7 +507,7 @@ EXPECTED
 RAW
 
     expected = <<-EXPECTED
-<pre><code class="ruby syntaxhl"><span class=\"CodeRay\"><span class="no">1</span> <span class="c"># Some ruby code here</span></span>
+<pre><code class="ruby syntaxhl"><span class=\"CodeRay\"><span class="line-numbers"><a href=\"#n1\" name=\"n1\">1</a></span><span class="comment"># Some ruby code here</span></span>
 </code></pre>
 EXPECTED
 
