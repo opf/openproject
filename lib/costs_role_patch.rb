e9b2f5d02e37b411298c5c05fcd23f967377367d
Jens Ulferts <j.ulferts@finn.de>
2012-11-15 09:20:37 +0100

    caches allowed_to for roles

--- lib/costs_role_patch.rb
+++ lib/costs_role_patch.rb
@@ -1,21 +1,35 @@
 module CostsRolePatch
   def self.included(base) # :nodoc:
     base.send(:include, InstanceMethods)
-    
-    # Same as typing in the class 
+
+    # Same as typing in the class
     base.class_eval do
       unloadable
-      
+
       alias_method_chain :allowed_to?, :inheritance
     end
   end
-  
+
   module InstanceMethods
     # Return true if the user is allowed to do the specified action on project
     # action can be:
     # * a parameter-like Hash (eg. :controller => 'projects', :action => 'edit')
     # * a permission Symbol (eg. :edit_project)
     def allowed_to_with_inheritance?(action)
+      allowed_to_with_caching(action)
+    end
+
+  private
+
+    def allowed_to_with_caching(action)
+      @allowed_to_with_inheritance ||= {}
+
+      return @allowed_to_with_inheritance[action] if @allowed_to_with_inheritance.has_key?(action)
+
+      @allowed_to_with_inheritance[action] = allowed_to_without_caching(action)
+    end
+
+    def allowed_to_without_caching(action)
       return true if allowed_to_without_inheritance?(action)
 
       if action.is_a? Hash
@@ -26,6 +40,7 @@ module CostsRolePatch
         allowed_inherited_actions.include? "#{action[:controller]}/#{action[:action]}"
       else
         # check, if the role has one of the parent permissions granted
+
         permission = Redmine::AccessControl.permission(action)
 
         return if permission.blank?
@@ -34,18 +49,17 @@ module CostsRolePatch
 
       end
     end
-    
-  #private
+
     def allowed_inherited_permissions
       @allowed_inherited_permissions ||= begin
         all_permissions = allowed_permissions || []
-        (all_permissions | allowed_permissions.collect do |sym| 
+        (all_permissions | allowed_permissions.collect do |sym|
           p = Redmine::AccessControl.permission(sym)
           p ? p.inherits.collect(&:name) : []
         end.flatten).uniq
       end
     end
-    
+
     def allowed_inherited_actions
       @actions_allowed_inherited ||= begin
         allowed_inherited_permissions.inject({}){|actions, p| actions[p] = Redmine::AccessControl.allowed_actions(p); actions}
