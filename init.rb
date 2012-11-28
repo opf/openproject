88e9d267fcb6e904c4c33757e4d237d1e30c1499
Jens Ulferts <j.ulferts@finn.de>
2012-11-28 15:32:54 +0100

    releases 2.4.9
    * minor performance and ui tweaks

--- init.rb
+++ init.rb
@@ -13,7 +13,7 @@ Redmine::Plugin.register :redmine_reporting do
   author 'Konstantin Haase, Philipp Tessenow @ finnlabs'
   author_url 'http://finn.de/team'
   description 'The reporting plugin provides extended reporting functionality for Redmine including Cost Reports.'
-  version '2.4.8'
+  version '2.4.9'
 
   requires_redmine :version_or_higher => '0.9'
   requires_redmine_plugin :redmine_costs, :version_or_higher => '0.3'
