237c0c9a5050fb1e9c2eeb0a38be79a8abe252ac
Gregor Schmidt <schmidt@nach-vorne.eu>
2012-12-12 10:48:20 +0100

    Making i18n-js work for OpenProject
    * Limit i18n keys in JS to the 'js' scope to reduce payload
    * Update i18n gem to make i18n-js work with limited scopes
    * Exporting translations on server start and not on every request to save
      computing resources and avoid impact on response times
    * Configuring i18n-js to use the same fallbacks like the ruby part to ensure
      consistent behavior across languages
    * Removing rake tasks from Rakefile since (a) they do not belong there - should
      have been in lib/tasks - and (b) they are no longer needed, since the export
      happens at server start
    
    NB: public/javascripts/i18n needs to be writeable by the app server user.

---
+++ app/helpers/i18n_js_helper.rb
@@ -0,0 +1,17 @@
+module I18nJsHelper
+  def i18n_js_tags
+    fallbacks = I18n.fallbacks[I18n.locale].tap {|f| f.shift }.map(&:to_s)
+
+    s = ""
+    s << javascript_include_tag("i18n")
+    s << javascript_include_tag("i18n/translations")
+    s << javascript_tag(%Q{
+      I18n.defaultLocale = "#{I18n.default_locale}";
+      I18n.locale        = "#{I18n.locale}";
+      I18n.fallbacks     = true;
+      I18n.fallbackRules = I18n.fallbackRules || {};
+      I18n.fallbackRules['#{I18n.locale}'] = #{fallbacks.to_json};
+    })
+    s
+  end
+end
