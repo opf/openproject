2e8ae37dd21379c2136587fbcb17f010bb98ef0b
Martin Linkhorst <m.linkhorst@googlemail.com>
2012-12-19 14:50:54 +0100

    the hash containing the acts_as_journalized changes got renamed to #changed_data

--- spec/models/user_deletion_spec.rb
+++ spec/models/user_deletion_spec.rb
@@ -65,13 +65,13 @@ describe User, 'deletion' do
     it { associated_instance.journals.first.user.should == user2 }
     it "should update first journal changes" do
       associations.each do |association|
-        associated_instance.journals.first.changes[association.to_s + "_id"].last.should == user2.id
+        associated_instance.journals.first.changed_data[association.to_s + "_id"].last.should == user2.id
       end
     end
     it { associated_instance.journals.last.user.should == substitute_user }
     it "should update second journal changes" do
       associations.each do |association|
-        associated_instance.journals.last.changes[association.to_s + "_id"].last.should == substitute_user.id
+        associated_instance.journals.last.changed_data[association.to_s + "_id"].last.should == substitute_user.id
       end
     end
   end
@@ -123,14 +123,14 @@ describe User, 'deletion' do
     it { associated_instance.journals.first.user.should == substitute_user }
     it "should update the first journal" do
       associations.each do |association|
-        associated_instance.journals.first.changes[association.to_s + "_id"].last.should == substitute_user.id
+        associated_instance.journals.first.changed_data[association.to_s + "_id"].last.should == substitute_user.id
       end
     end
     it { associated_instance.journals.last.user.should == user2 }
     it "should update the last journal" do
       associations.each do |association|
-        associated_instance.journals.last.changes[association.to_s + "_id"].first.should == substitute_user.id
-        associated_instance.journals.last.changes[association.to_s + "_id"].last.should == user2.id
+        associated_instance.journals.last.changed_data[association.to_s + "_id"].first.should == substitute_user.id
+        associated_instance.journals.last.changed_data[association.to_s + "_id"].last.should == user2.id
       end
     end
   end
@@ -192,13 +192,13 @@ describe User, 'deletion' do
     it { associated_instance.journals.first.user.should == user2 }
     it "should update first journal changes" do
       associations.each do |association|
-        associated_instance.journals.first.changes[association.to_s + "_id"].last.should == user2.id
+        associated_instance.journals.first.changed_data[association.to_s + "_id"].last.should == user2.id
       end
     end
     it { associated_instance.journals.last.user.should == substitute_user }
     it "should update second journal changes" do
       associations.each do |association|
-        associated_instance.journals.last.changes[association.to_s + "_id"].last.should == substitute_user.id
+        associated_instance.journals.last.changed_data[association.to_s + "_id"].last.should == substitute_user.id
       end
     end
   end
@@ -390,11 +390,11 @@ describe User, 'deletion' do
     end
     it { associated_instance.journals.first.user.should == user2 }
     it "should update first journal changes" do
-      associated_instance.journals.first.changes["user_id"].last.should == user2.id
+      associated_instance.journals.first.changed_data["user_id"].last.should == user2.id
     end
     it { associated_instance.journals.last.user.should == substitute_user }
     it "should update second journal changes" do
-      associated_instance.journals.last.changes["user_id"].last.should == substitute_user.id
+      associated_instance.journals.last.changed_data["user_id"].last.should == substitute_user.id
     end
   end
 
