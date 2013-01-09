fd3fb42518987438b1029874e5c1350af9c533aa
Martin Linkhorst <m.linkhorst@googlemail.com>
2013-01-09 14:31:25 +0100

    in order to have these tests to always pass, we need to make sure the database is clean before each test run (this actually should be done for every test, but hey…)

--- test/unit/default_data_test.rb
+++ test/unit/default_data_test.rb
@@ -16,24 +16,23 @@ require File.expand_path('../../test_helper', __FILE__)
 class DefaultDataTest < ActiveSupport::TestCase
   include Redmine::I18n
 
-  def test_no_data
+  def setup
+    delete_loaded_data!
     assert Redmine::DefaultData::Loader::no_data?
-    Redmine::DefaultData::Loader::load()
+  end
+
+  def test_no_data
+    Redmine::DefaultData::Loader::load
     assert !Redmine::DefaultData::Loader::no_data?
-    Role.delete_all("builtin = 0")
-    Tracker.delete_all
-    IssueStatus.delete_all
-    Enumeration.delete_all
+
+    delete_loaded_data!
     assert Redmine::DefaultData::Loader::no_data?
   end
 
   def test_load
     valid_languages.each do |lang|
       begin
-        Role.delete_all("builtin = 0")
-        Tracker.delete_all
-        IssueStatus.delete_all
-        Enumeration.delete_all
+        delete_loaded_data!
         assert Redmine::DefaultData::Loader::load(lang)
         assert_not_nil DocumentCategory.first
         assert_not_nil IssuePriority.first
@@ -42,5 +41,14 @@ class DefaultDataTest < ActiveSupport::TestCase
         assert false, ":#{lang} default data is invalid (#{e.message})."
       end
     end
+  end
+
+private
+
+  def delete_loaded_data!
+    Role.delete_all("builtin = 0")
+    Tracker.delete_all
+    IssueStatus.delete_all
+    Enumeration.delete_all
   end
 end
