83fd0e906f16919984c68af84d617ec2dfafe49f
Philipp Tessenow <philipp.tessenow@student.hpi.uni-potsdam.de>
2013-01-11 18:06:28 +0100

    AttachmentFactory creates an author now

--- spec/factories/attachment_factory.rb
+++ spec/factories/attachment_factory.rb
@@ -1,6 +1,7 @@
 FactoryGirl.define do
   factory :attachment do
     container :factory => :document
+    author :factory => :user
     sequence(:filename) { |n| "test#{n}.test" }
     sequence(:disk_filename) { |n| "test#{n}.test" }
   end
