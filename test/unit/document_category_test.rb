9f5aaa06e6ca124c1d108111efd486ad28cc5cbc
Philipp Tessenow <philipp.tessenow@student.hpi.uni-potsdam.de>
2013-01-11 14:27:13 +0100

    remove fixtures and use FactoryGirl in document_category_test

--- test/unit/document_category_test.rb
+++ test/unit/document_category_test.rb
@@ -14,13 +14,16 @@
 require File.expand_path('../../test_helper', __FILE__)
 
 class DocumentCategoryTest < ActiveSupport::TestCase
-  fixtures :enumerations, :documents, :issues
-
   def test_should_be_an_enumeration
     assert DocumentCategory.ancestors.include?(Enumeration)
   end
 
   def test_objects_count
+    project = FactoryGirl.create :project
+    uncategorized = FactoryGirl.create :document_category, :name => "Uncategorized", :project => project
+    FactoryGirl.create :document_category, :name => "User documentation"
+    FactoryGirl.create_list :document, 2, :category => uncategorized, :project => project
+
     assert_equal 2, DocumentCategory.find_by_name("Uncategorized").objects_count
     assert_equal 0, DocumentCategory.find_by_name("User documentation").objects_count
   end
