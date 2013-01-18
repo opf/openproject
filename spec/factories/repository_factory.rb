7b510e81966b785bfd8c484d0a2b098cf68a5c93
Philipp Tessenow <philipp.tessenow@student.hpi.uni-potsdam.de>
2013-01-18 11:17:34 +0100

    give repository_factory a hopefully helpful comment

--- spec/factories/repository_factory.rb
+++ spec/factories/repository_factory.rb
@@ -1,7 +1,7 @@
 FactoryGirl.define do
   factory :repository, :class => Repository::Filesystem do
-    url  'file:///tmp/test_repo'
+    # Setting.enabled_scm should include "Filesystem" to successfully save the created repository
+    url 'file:///tmp/test_repo'
     project
   end
 end
-
