4e4bc603f6b905fe183e432c6377b695a869f5a8
Jens Ulferts <j.ulferts@finn.de>
2013-01-28 22:23:47 +0100

    sets rails to 2.3.16

--- config/environment.rb
+++ config/environment.rb
@@ -19,7 +19,7 @@
 # ENV['RAILS_ENV'] ||= 'production'
 
 # Specifies gem version of Rails to use when vendor/rails is not present
-RAILS_GEM_VERSION = '2.3.15' unless defined? RAILS_GEM_VERSION
+RAILS_GEM_VERSION = '2.3.16' unless defined? RAILS_GEM_VERSION
 
 if RUBY_VERSION >= '1.9'
   Encoding.default_external = 'UTF-8'
