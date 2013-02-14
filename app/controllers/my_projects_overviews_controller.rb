57f2cf0e44852ef1f1f1817668fb789dc5d893e7
Jens Ulferts <j.ulferts@finn.de>
2013-02-14 19:22:42 +0100

    replaces instance variable in view by methods in controller
    Replaces every instance variable in the view layer by a method call to the controller. This is done in order to:
    
    1) better separate concerns.
    2) Lazy loading of objects that might be used by a block. As the purpose of the plugin is to allow the user to choose from a list of available blocks, not all blocks will be used all the times. This also means that not all information is required to be loaded all the time. With this commit, we load only what is required for the current block selection by having each block call a method that loads the information it requires.

--- app/controllers/my_projects_overviews_controller.rb
+++ app/controllers/my_projects_overviews_controller.rb
@@ -18,9 +18,7 @@ class MyProjectsOverviewsController < ApplicationController
 
   unloadable
 
-  before_filter :find_project, :find_user, :find_my_project_overview
-  before_filter :find_page_blocks, :find_project_details
-  before_filter :find_attachments
+  before_filter :find_project, :find_user
   before_filter :authorize
   before_filter :jump_to_project_menu_item, :only => :index
 
@@ -47,9 +45,6 @@ class MyProjectsOverviewsController < ApplicationController
 
   # User's page layout configuration
   def page_layout
-    @block_options = []
-    BLOCKS.each {|k, v| @block_options << [l("my.blocks.#{v}", :default => [v, v.to_s.humanize]), k.dasherize]}
-    @block_options << [l(:label_custom_element), :custom_element]
   end
 
   def update_custom_element
@@ -59,13 +54,13 @@ class MyProjectsOverviewsController < ApplicationController
 
     if params["attachments"]
       # Attach files and save them
-      attachments = Attachment.attach_files(@overview, params["attachments"])
+      attachments = Attachment.attach_files(overview, params["attachments"])
       unless attachments[:unsaved].blank?
         flash[:error] = l(:warning_attachments_not_saved, attachments[:unsaved].size)
       end
     end
 
-    @overview.save_custom_element(block_name, block_title, textile)
+    overview.save_custom_element(block_name, block_title, textile)
 
     redirect_to :back
   end
@@ -77,23 +72,21 @@ class MyProjectsOverviewsController < ApplicationController
     block = params[:block].to_s.underscore
     if (BLOCKS.keys.include? block)
       # remove if already present in a group
-      %w(top left right hidden).each {|f| @overview.send(f).delete block }
+      %w(top left right hidden).each {|f| overview.send(f).delete block }
       # add it hidden
-      @overview.hidden.unshift block
-      @overview.save!
-      render(:partial => "block",
-             :locals => { :user => @user,
-               :project => @project,
-               :block_name => block})
+      overview.hidden.unshift block
+      overview.save!
+      render :partial => "block",
+             :locals => { :block_name => block }
     elsif block == "custom_element"
-      @overview.hidden.unshift @overview.new_custom_element
-      @overview.save!
+      overview.hidden.unshift overview.new_custom_element
+      overview.save!
       render(:partial => "block_textilizable",
-             :locals => { :user => @user,
-               :project => @project,
-               :block_title => l(:label_custom_element),
-               :block_name => @overview.hidden.first.first,
-               :textile => @overview.hidden.first.last})
+             :locals => { :user => user,
+                          :project => project,
+                          :block_title => l(:label_custom_element),
+                          :block_name => overview.hidden.first.first,
+                          :textile => overview.hidden.first.last})
     else
       render :nothing => true
     end
@@ -103,8 +96,8 @@ class MyProjectsOverviewsController < ApplicationController
   # params[:block] : id of the block to remove
   def remove_block
     block = param_to_block(params[:block])
-    %w(top left right hidden).each {|f| @overview.send(f).delete block }
-    @overview.save!
+    %w(top left right hidden).each {|f| overview.send(f).delete block }
+    overview.save!
     render :nothing => true
   end
 
@@ -115,14 +108,14 @@ class MyProjectsOverviewsController < ApplicationController
     group = params[:group]
     if group.is_a?(String)
       group_items = (params["list-#{group}"] || []).collect {|x| param_to_block(x) }
-      unless group_items.size < @overview.send(group).size
+      unless group_items.size < overview.send(group).size
         # We are adding or re-ordering, not removing
         # Remove group blocks if they are presents in other groups
-        @overview.update_attributes('top' => (@overview.top - group_items),
-                                    'left' => (@overview.left - group_items),
-                                    'right' => (@overview.right - group_items),
-                                    'hidden' => (@overview.hidden - group_items))
-        @overview.update_attribute(group, group_items)
+        overview.update_attributes('top' => (overview.top - group_items),
+                                   'left' => (overview.left - group_items),
+                                   'right' => (overview.right - group_items),
+                                   'hidden' => (overview.hidden - group_items))
+        overview.update_attribute(group, group_items)
       end
     end
     render :nothing => true
@@ -131,69 +124,129 @@ class MyProjectsOverviewsController < ApplicationController
   def param_to_block(param)
     block = param.to_s.underscore
     unless (BLOCKS.keys.include? block)
-      block = @overview.custom_elements.detect {|ary| ary.first == block}
+      block = overview.custom_elements.detect {|ary| ary.first == block}
     end
     block
   end
 
   def destroy_attachment
-    if @user.allowed_to?(:edit_project, @project)
+    if user.allowed_to?(:edit_project, project)
       begin
         att = Attachment.find(params[:attachment_id].to_i)
-        @overview.attachments.delete(att)
-        @overview.save
+        overview.attachments.delete(att)
+        overview.save
       rescue ActiveRecord::RecordNotFound
       end
     end
-    @attachments -= [att]
+    attachments -= [att]
     render :partial => 'page_layout_attachments'
+  end
+
+  helper_method :users_by_role,
+                :childprojects,
+                :recent_news,
+                :trackers,
+                :open_issues_by_tracker,
+                :total_issues_by_tracker,
+                :assigned_issues,
+                :total_hours,
+                :project,
+                :user,
+                :blocks,
+                :block_options,
+                :overview,
+                :attachments,
+                :render_block,
+                :object_callback
+
+  def render_block name
+  end
+
+  def users_by_role
+    @users_by_role ||= project.users_by_role
+  end
+
+  def childprojects
+    @childprojects ||= project.children.visible.all
+  end
+
+  def recent_news
+    @news ||= project.news.all :limit => 5,
+                               :include => [ :author, :project ],
+                               :order => "#{News.table_name}.created_on DESC"
+
+  end
+
+  def trackers
+    @trackers ||= project.rolled_up_trackers
+  end
+
+  def open_issues_by_tracker
+    @open_issues_by_tracker ||= Issue.visible.count(:group => :tracker,
+                                                    :include => [:project, :status, :tracker],
+                                                    :conditions => ["(#{subproject_condition}) AND #{IssueStatus.table_name}.is_closed=?", false])
+  end
+
+  def total_issues_by_tracker
+    @total_issues_by_tracker ||= Issue.visible.count(:group => :tracker,
+                                                     :include => [:project, :status, :tracker],
+                                                     :conditions => subproject_condition)
+
+  end
+
+  def assigned_issues
+    @assigned_issues ||= Issue.visible.open.find(:all,
+                                                 :conditions => { :assigned_to_id => User.current.id },
+                                                 :limit => 10,
+                                                 :include => [ :status, :project, :tracker, :priority ],
+                                                 :order => "#{IssuePriority.table_name}.position DESC, #{Issue.table_name}.updated_on DESC")
+  end
+
+  def total_hours
+    if User.current.allowed_to?(:view_time_entries, project)
+      @total_hours ||= TimeEntry.visible.sum(:hours, :include => :project, :conditions => subproject_condition).to_f
+    end
+  end
+
+  def project
+    @project
+  end
+
+  def user
+    @user
+  end
+
+  def blocks
+    @blocks ||= {
+      'top' => overview.top,
+      'left' => overview.left,
+      'right' => overview.right,
+      'hidden' => overview.hidden
+    }
+  end
+
+  def block_options
+    @block_options = []
+    BLOCKS.each {|k, v| @block_options << [l("my.blocks.#{v}", :default => [v, v.to_s.humanize]), k.dasherize]}
+    @block_options << [l(:label_custom_element), :custom_element]
+  end
+
+  def overview
+    @overview ||= MyProjectsOverview.find_or_create_by_project_id(project.id)
+  end
+
+  def attachments
+    @attachments = overview.attachments || []
   end
 
   private
 
-  def find_my_project_overview
-    @overview = MyProjectsOverview.find(:first, :conditions => "project_id = #{@project.id}")
-    # Auto-create missing overviews
-    @overview ||= MyProjectsOverview.create!(:project_id => @project.id)
+  def subproject_condition
+    @subproject_condition ||= project.project_condition(Setting.display_subprojects_issues?)
   end
 
   def find_user
     @user = User.current
-  end
-
-  def find_page_blocks
-    @blocks = {
-      'top' => @overview.top,
-      'left' => @overview.left,
-      'right' => @overview.right,
-      'hidden' => @overview.hidden
-    }
-  end
-
-  def find_project_details
-    @users_by_role = @project.users_by_role
-    @subprojects = @project.children.visible.all
-    @news = @project.news.find(:all, :limit => 5,
-                               :include => [ :author, :project ],
-                               :order => "#{News.table_name}.created_on DESC")
-    @trackers = @project.rolled_up_trackers
-
-    cond = @project.project_condition(Setting.display_subprojects_issues?)
-
-    @open_issues_by_tracker = Issue.visible.count(:group => :tracker,
-                                            :include => [:project, :status, :tracker],
-                                            :conditions => ["(#{cond}) AND #{IssueStatus.table_name}.is_closed=?", false])
-    @total_issues_by_tracker = Issue.visible.count(:group => :tracker,
-                                            :include => [:project, :status, :tracker],
-                                            :conditions => cond)
-
-    if User.current.allowed_to?(:view_time_entries, @project)
-      @total_hours = TimeEntry.visible.sum(:hours, :include => :project, :conditions => cond).to_f
-    end
-  end
-
-  def find_attachments
-    @attachments = @overview.attachments || []
   end
 
   def default_breadcrumb
@@ -203,7 +256,7 @@ class MyProjectsOverviewsController < ApplicationController
   def jump_to_project_menu_item
     if params[:jump]
       # try to redirect to the requested menu item
-      redirect_to_project_menu_item(@project, params[:jump]) && return
+      redirect_to_project_menu_item(project, params[:jump]) && return
     end
   end
 end
