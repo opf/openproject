b4fbac78b5985486c08827e63c591181372293aa
Christian Rijke <c.rijke@finn.de>
2013-03-05 16:38:47 +0100

    move patch files

---
+++ lib/openproject_meeting/patches/project_patch.rb
@@ -0,0 +1,11 @@
+module Meeting
+  module Patches
+    module ProjectPatch
+      def self.included(receiver)
+        receiver.class_eval do
+          has_many :meetings, :include => [:author], :dependent => :destroy
+        end
+      end
+    end
+  end
+end
