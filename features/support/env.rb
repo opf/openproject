97623fc3843997c67acf74c7981ddeff2605b0b0
Hagen Schink <h.schink@finn.de>
2013-03-15 11:55:55 +0100

    Use truncation strategy for database cleaner

--- features/support/env.rb
+++ features/support/env.rb
@@ -45,7 +45,7 @@ ActionController::Base.allow_rescue = false
 # Remove/comment out the lines below if your app doesn't have a database.
 # For some databases (like MongoDB and CouchDB) you may need to use :truncation instead.
 begin
-  DatabaseCleaner.strategy = :transaction
+  DatabaseCleaner.strategy = :truncation
 rescue NameError
   raise "You need to add database_cleaner to your Gemfile (in the :test group) if you wish to use it."
 end
