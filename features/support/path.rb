b4cc8591390a3ca2f0f6e3de8a7e7b1040f335c7
Jens Ulferts <j.ulferts@finn.de>
2013-03-15 09:27:58 +0100

    fixes budget creation
    We need to provide the new labor_budget_item with a cost object. Otherwise checking the can_view_costs permission check will break.

---
+++ features/support/path.rb
@@ -0,0 +1,19 @@
+module CostNavigationHelpers
+  # Maps a name to a path. Used by the
+  #
+  #   When /^I go to (.+)$/ do |page_name|
+  #
+  # step definition in web_steps.rb
+  #
+  def path_to(page_name)
+    case page_name
+    when /^the show page (?:of|for) the budget "(.+)?"$/
+      budget = CostObject.find_by_subject($1)
+      "/cost_objects/#{budget.id}"
+    else
+      super
+    end
+  end
+end
+
+World(CostNavigationHelpers)
