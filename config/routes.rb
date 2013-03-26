8c1d83f2cc76f41b64becf4e48b4e1155199bdf6
Jens Ulferts <j.ulferts@finn.de>
2013-03-26 14:07:24 +0100

    rewrites routes in new syntax - no optimization

--- config/routes.rb
+++ config/routes.rb
@@ -1,26 +1,67 @@
-ActionController::Routing::Routes.draw do |map|
+OpenProject::Application.routes.draw do
 
   # Use rb/ as a URL 'namespace.' We're using a slightly different URL pattern
   # From Redmine so namespacing avoids any further problems down the line
-  map.resource :rb, :only => :none do |rb|
-    rb.resource   :query,            :only => :show,               :controller => :rb_queries,          :as => "queries/:project_id"
-    rb.resource   :wiki,             :only => [:show, :edit],      :controller => :rb_wikis,            :as => "wikis/:sprint_id"
-    rb.resource   :burndown_chart,   :only => :show,               :controller => :rb_burndown_charts,  :as => "projects/:project_id/burndown_charts/:sprint_id"
-    rb.resource   :impediment,       :except => :index,            :controller => :rb_impediments,      :as => "impediment/:id"
-    rb.resources  :impediments,      :only => :index,              :controller => :rb_impediments,      :as => "impediments/:sprint_id"
-    rb.resource   :task,             :except => :index,            :controller => :rb_tasks,            :as => "task/:id"
-    rb.resources  :tasks,            :only => :index,              :controller => :rb_tasks,            :as => "tasks/:story_id"
-    rb.resource   :story,            :except => :index,            :controller => :rb_stories,          :as => "story/:id"
-    rb.resources  :stories,          :only => :index,              :controller => :rb_stories,          :as => "stories/:project_id"
-    rb.resource   :sprint,           :only => [:show, :update],    :controller => :rb_sprints,          :as => "sprints/:sprint_id"
-    rb.resource   :server_variables, :only => :show,               :controller => :rb_server_variables, :as => "server_variables/:project_id"
-    rb.resource   :taskboard,        :only => :show,               :controller => :rb_taskboards,       :as => "taskboards/:sprint_id"
-    rb.resource   :master_backlog,   :only => :show,               :controller => :rb_master_backlogs,  :as => "master_backlogs/:project_id"
-    rb.resources  :issue_boxes,      :only => [:show, :edit, :update]
+  resource :rb, :only => :none do
+
+    scope "queries/:project_id" do
+      resource :query,
+               :only => :show,
+               :controller => :rb_queries
+    end
+
+    scope "wiki/:sprint_id" do
+      resource :wiki, :only => [:show, :edit], :controller => :rb_wikis
+    end
+
+    scope "projects/:project_id/burndown_charts/:sprint_id" do
+      resource   :burndown_chart,   :only => :show,               :controller => :rb_burndown_charts
+    end
+
+    scope "impediment/:id" do
+      resource   :impediment,       :except => :index,            :controller => :rb_impediments
+    end
+
+    scope  "impediments/:sprint_id" do
+      resources  :impediments,      :only => :index,              :controller => :rb_impediments
+    end
+
+    scope "task/:id" do
+      resource   :task,             :except => :index,            :controller => :rb_tasks
+    end
+
+    scope "tasks/:stroy_id" do
+      resources  :tasks,            :only => :index,              :controller => :rb_tasks
+    end
+
+    scope "story/:id" do
+      resource   :story,            :except => :index,            :controller => :rb_stories
+    end
+
+    scope "stories/:project_id" do
+      resources  :stories,          :only => :index,              :controller => :rb_stories
+    end
+
+    scope "sprints/:sprint_id" do
+      resource   :sprint,           :only => [:show, :update],    :controller => :rb_sprints
+    end
+
+    scope "server_variables/:project_id" do
+      resource   :server_variables, :only => :show,               :controller => :rb_server_variables
+    end
+
+    scope "taskboard/:sprint_id" do
+      resource   :taskboard,        :only => :show,               :controller => :rb_taskboards
+    end
+
+    scope "master_backlogs/:project_id" do
+      resource   :master_backlog,   :only => :show,               :controller => :rb_master_backlogs
+    end
+
+    resources  :issue_boxes,      :only => [:show, :edit, :update]
   end
 
-
-  map.connect 'projects/:project_id/versions/:id/edit', :controller => 'version_settings', :action => 'edit'
-  map.connect 'projects/:id/project_issue_statuses', :controller => 'projects', :action => 'project_issue_statuses'
-  map.connect 'projects/:id/rebuild_positions', :controller => 'projects', :action => 'rebuild_positions'
+  get 'projects/:project_id/versions/:id/edit' => 'version_settings#edit'
+  get 'projects/:id/project_issue_statuses' => 'projects#project_issue_statuses'
+  post 'projects/:id/rebuild_positions' => 'projects#rebuild_positions'
 end
