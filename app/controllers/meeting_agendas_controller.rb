5c4e34cbc833e15c7ef47cbe8ec5b9bbbfb253bd
Jens Ulferts <j.ulferts@finn.de>
2013-03-27 09:17:46 +0100

    fixes whitespace

--- app/controllers/meeting_agendas_controller.rb
+++ app/controllers/meeting_agendas_controller.rb
@@ -1,20 +1,20 @@
 class MeetingAgendasController < MeetingContentsController
   unloadable
-  
+
   menu_item :meetings
-  
+
   def close
     @content.lock!
     redirect_back_or_default :controller => 'meetings', :action => 'show', :id => @meeting
   end
-  
+
   def open
     @content.unlock!
     redirect_back_or_default :controller => 'meetings', :action => 'show', :id => @meeting
   end
-  
+
   private
-  
+
   def find_content
     @content = @meeting.agenda || @meeting.build_agenda
     @content_type = "meeting_agenda"
