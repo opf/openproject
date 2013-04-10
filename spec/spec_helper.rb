52eb41842928bafa65c356d70052b492198e2664
Christian Rijke <crijke@gmail.com>
2013-04-10 10:55:54 +0200

    require factories

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,3 +1,8 @@
 # -- load spec_helper from OpenProject core
 require "spec_helper"
 
+require File.dirname(__FILE__) + '/factories/impediment_factory'
+require File.dirname(__FILE__) + '/factories/sprint_factory'
+require File.dirname(__FILE__) + '/factories/story_factory'
+require File.dirname(__FILE__) + '/factories/task_factory'
+require File.dirname(__FILE__) + '/factories/version_setting_factory'
