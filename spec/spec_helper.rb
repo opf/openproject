ddbff4bd3af5739c8f60ab66df51c3ca0103e943
Christian Rijke <crijke@gmail.com>
2013-04-12 14:19:12 +0200

    require prawn for rspec tests

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,6 +1,9 @@
 # -- load spec_helper from OpenProject core
 require "spec_helper"
 
+# load gem dependencies
+require "prawn"
+
 require File.dirname(__FILE__) + '/factories/impediment_factory'
 require File.dirname(__FILE__) + '/factories/sprint_factory'
 require File.dirname(__FILE__) + '/factories/story_factory'
