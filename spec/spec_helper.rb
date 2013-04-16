962b425ab6954dca9e788b06165d23fcf369fa34
Christian Rijke <crijke@gmail.com>
2013-04-16 14:55:21 +0200

    Requiring factories is no longer required.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -4,8 +4,3 @@ require "spec_helper"
 # load gem dependencies
 require "prawn"
 
-require File.dirname(__FILE__) + '/factories/impediment_factory'
-require File.dirname(__FILE__) + '/factories/sprint_factory'
-require File.dirname(__FILE__) + '/factories/story_factory'
-require File.dirname(__FILE__) + '/factories/task_factory'
-require File.dirname(__FILE__) + '/factories/version_setting_factory'
