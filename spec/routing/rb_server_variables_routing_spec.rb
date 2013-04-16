acae0fafdc0e2e998fbdaa6577448d2c4c9315d6
Sebastian Schuster <s.schuster@finn.de>
2013-04-16 16:57:09 +0200

    Added missing format specifier for server_variables routing spec

--- spec/routing/rb_server_variables_routing_spec.rb
+++ spec/routing/rb_server_variables_routing_spec.rb
@@ -4,6 +4,7 @@ describe RbServerVariablesController do
   describe "routing" do
     it { get('/projects/project_42/server_variables.js').should route_to(:controller => 'rb_server_variables',
                                                      :action => 'show',
+                                                     :format => 'js',
                                                      :project_id => 'project_42') }
   end
 end
