c3f349625199822ce294d9a3bdb3b75be4ea8d56
Jens Ulferts <j.ulferts@finn.de>
2013-04-24 12:08:11 +0200

    renames join table changes to the changed standard

--- db/migrate/20091130214149_primary_key_for_groups_users.rb
+++ db/migrate/20091130214149_primary_key_for_groups_users.rb
@@ -1,9 +1,9 @@
 class PrimaryKeyForGroupsUsers < ActiveRecord::Migration
   def self.up
-    add_column :groups_users, :id, :primary_key
+    add_column :group_user, :id, :primary_key
   end
-  
+
   def self.down
-    remove_column :groups_users, :id
+    remove_column :group_user, :id
   end
 end
