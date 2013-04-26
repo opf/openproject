ce0574119b0b74c91d3582a14dea5b76078091bf
jwollert <johannes.wollert@gmail.com>
2013-04-26 14:32:09 +0200

    fixes estimated_hours validation on issues by explicitly making it fail on invalid input strings

--- app/models/issue.rb
+++ app/models/issue.rb
@@ -265,7 +265,8 @@ class Issue < ActiveRecord::Base
   alias_method_chain(:attributes=, :tracker_first) unless method_defined?(:attributes_without_tracker_first=)
 
   def estimated_hours=(h)
-    write_attribute :estimated_hours, (h.is_a?(String) ? h.to_hours : h)
+    converted_hours = (h.is_a?(String) ? h.to_hours : h)
+    write_attribute :estimated_hours, !!converted_hours ? converted_hours : h
   end
 
   safe_attributes 'tracker_id',
