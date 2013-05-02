268e8d5548f4c4fe83934c279e76bb6010177616
Martin Czuchra <martin.czuchra@googlemail.com>
2013-05-02 16:40:04 +0200

    Mostly whitespace changes.
    This file is really messy with all the mixed indentations. This should
    remedy some of the shortcomings.

--- app/assets/javascripts/context_menu.js
+++ app/assets/javascripts/context_menu.js
@@ -2,35 +2,39 @@
    Copyright (C) 2006-2008  Jean-Philippe Lang */
 
 jQuery(document).ready(function($) {
-  $.fn.onClickDropDown = function(){
+  $.fn.onClickDropDown = function() {
     var that = this;
     $('html').click(function() {
-      that.find(" > li.drop-down.open").removeClass("open").find("> ul").mySlide();
-      that.removeClass("hover");
+      that.find(' > li.drop-down.open').removeClass('open').find('> ul').mySlide();
+      that.removeClass('hover');
     });
 
     // Do not close the login window when using it
-    that.find("li li").click(function(event){
+    that.find('li li').click(function(event) {
       $(document).trigger(event); // pass click-event to rails ujs-handler
       event.stopPropagation();
     });
 
-    this.find(" > li.drop-down").click(function(event) {
+    this.find(' > li.drop-down').click(function(event) {
       // if an h2 tag follows the submenu should unfold out at the border
       var menu_start_position;
-      if (that.next().get(0) != undefined && (that.next().get(0).tagName == 'H2')){
-        menu_start_position = that.next().innerHeight() + that.next().position().top;
-        that.find("ul.action_menu_more").css({ top: menu_start_position });
-      }
-      else if(that.next().hasClass("wiki-content") && that.next().children().next().first().get(0) != undefined && that.next().children().next().first().get(0).tagName == 'H1'){
+      if (that.next().get(0) != undefined &&
+          that.next().get(0).tagName == 'H2') {
+        menu_start_position = that.next().innerHeight() +
+                              that.next().position().top;
+        that.find('ul.action_menu_more').css({ top: menu_start_position });
+      } else if (that.next().hasClass('wiki-content') &&
+                 that.next().children().next().first().get(0) != undefined &&
+                 that.next().children().next().first().get(0).tagName == 'H1') {
         var wiki_heading = that.next().children().next().first();
-        menu_start_position = wiki_heading.innerHeight() + wiki_heading.position().top;
-        that.find("ul.action_menu_more").css({ top: menu_start_position });
+        menu_start_position = wiki_heading.innerHeight() +
+                              wiki_heading.position().top;
+        that.find('ul.action_menu_more').css({ top: menu_start_position });
       }
 
-      $(this).toggleSubmenu(that);
-      $(document).trigger(event); // pass click-event to rails ujs-handler
-      return false;
+    $(this).toggleSubmenu(that);
+    $(document).trigger(event); // pass click-event to rails ujs-handler
+    return false;
     });
   };
 });
@@ -39,44 +43,55 @@ var observingContextMenuClick;
 
 ContextMenu = Class.create();
 ContextMenu.prototype = {
-	initialize: function (url) {
-	this.url = url;
-	this.createMenu();
+  initialize: function(url) {
+    this.url = url;
+    this.createMenu();
 
-	if (!observingContextMenuClick) {
-		Event.observe(document, 'click', this.Click.bindAsEventListener(this));
-		Event.observe(document, 'contextmenu', this.RightClick.bindAsEventListener(this));
-		observingContextMenuClick = true;
-	}
-	
-	this.unselectAll();
-	this.lastSelected = null;
-	},
-  
-	RightClick: function(e) {
-		this.hideMenu();
-		// do not show the context menu on links
-		if (Event.element(e).tagName == 'A') { return; }
-		var tr = Event.findElement(e, 'tr');
-		if (tr == document || tr == undefined  || !tr.hasClassName('hascontextmenu')) { return; }
-		Event.stop(e);
-		if (!this.isSelected(tr)) {
-			this.unselectAll();
-			this.addSelection(tr, e);
-			this.lastSelected = tr;
-		}
-		this.showMenu(e);
-	},
+    if (!observingContextMenuClick) {
+      Event.observe(
+        document,
+        'click',
+        this.Click.bindAsEventListener(this)
+      );
+      Event.observe(
+        document,
+        'contextmenu',
+        this.RightClick.bindAsEventListener(this)
+      );
+      observingContextMenuClick = true;
+    }
+
+    this.unselectAll();
+    this.lastSelected = null;
+  },
+
+  RightClick: function(e) {
+    this.hideMenu();
+    // do not show the context menu on links
+    if (Event.element(e).tagName == 'A') { return; }
+    var tr = Event.findElement(e, 'tr');
+    if (tr == document || tr == undefined ||
+        !tr.hasClassName('hascontextmenu')) {
+      return;
+    }
+    Event.stop(e);
+    if (!this.isSelected(tr)) {
+      this.unselectAll();
+      this.addSelection(tr, e);
+      this.lastSelected = tr;
+    }
+    this.showMenu(e);
+  },
 
   Click: function(e) {
-  	this.hideMenu();
-  	if (Event.element(e).tagName == 'A') { return; }
-    if (!Event.isRightClick(e) || (navigator.appVersion.match(/\bMSIE\b/))) {      
+    this.hideMenu();
+    if (Event.element(e).tagName == 'A') { return; }
+    if (!Event.isRightClick(e) || (navigator.appVersion.match(/\bMSIE\b/))) {
       var tr = Event.findElement(e, 'tr');
-      if (tr!=null && tr!=document && tr.hasClassName('hascontextmenu')) {
+      if (tr != null && tr != document && tr.hasClassName('hascontextmenu')) {
         // a row was clicked, check if the click was on checkbox
         var box = Event.findElement(e, 'input');
-        if (box!=document && box!=undefined) {
+        if (box != document && box != undefined) {
           // a checkbox may be clicked
           if (box.checked) {
             tr.addClassName('context-menu-selection');
@@ -90,11 +105,11 @@ ContextMenu.prototype = {
             if (this.lastSelected != null) {
               var toggling = false;
               var rows = $$('.hascontextmenu');
-              for (i=0; i<rows.length; i++) {
-                if (toggling || rows[i]==tr) {
+              for (i = 0; i < rows.length; i++) {
+                if (toggling || rows[i] == tr) {
                   this.addSelection(rows[i], e);
                 }
-                if (rows[i]==tr || rows[i]==this.lastSelected) {
+                if (rows[i] == tr || rows[i] == this.lastSelected) {
                   toggling = !toggling;
                 }
               }
@@ -112,24 +127,23 @@ ContextMenu.prototype = {
         var t = Event.findElement(e, 'a');
         if (t == document || t == undefined) {
           this.unselectAll();
-        } else {
-          if (Element.hasClassName(t, 'disabled') || Element.hasClassName(t, 'submenu')) {
-            Event.stop(e);
-          }
+        } else if (Element.hasClassName(t, 'disabled') ||
+                   Element.hasClassName(t, 'submenu')) {
+          Event.stop(e);
         }
       }
     }
   },
-  
+
   createMenu: function() {
     if (!$('context-menu')) {
-      var menu = document.createElement("div");
-      menu.setAttribute("id", "context-menu");
-      menu.setAttribute("style", "display:none;");
-      document.getElementById("content").appendChild(menu);
+      var menu = document.createElement('div');
+      menu.setAttribute('id', 'context-menu');
+      menu.setAttribute('style', 'display:none;');
+      document.getElementById('content').appendChild(menu);
     }
   },
-  
+
   showMenu: function(e) {
     var mouse_x = Event.pointerX(e);
     var mouse_y = Event.pointerY(e);
@@ -144,48 +158,49 @@ ContextMenu.prototype = {
     var max_height;
 
     $('context-menu').style['left'] = (render_x + 'px');
-    $('context-menu').style['top'] = (render_y + 'px');		
+    $('context-menu').style['top'] = (render_y + 'px');
     Element.update('context-menu', '');
 
-    new Ajax.Updater({success:'context-menu'}, this.url, 
-      {asynchronous:true,
-       method: 'get',
-       evalScripts:true,
-       parameters:Form.serialize(Event.findElement(e, 'form')),
-       onComplete:function(request){
-				 dims = $('context-menu').getDimensions();
-				 menu_width = dims.width;
-				 menu_height = dims.height;
-				 max_width = render_x + 2*menu_width;
-				 max_height = render_y + menu_height;
+    new Ajax.Updater({success: 'context-menu'}, this.url, {
+      asynchronous: true,
+      method: 'get',
+      evalScripts: true,
+      parameters: Form.serialize(Event.findElement(e, 'form')),
+      onComplete: function(request) {
+        dims = $('context-menu').getDimensions();
+        menu_width = dims.width;
+        menu_height = dims.height;
+        max_width = render_x + 2 * menu_width;
+        max_height = render_y + menu_height;
 
-				 var ws = window_size();
-				 window_width = ws.width;
-				 window_height = ws.height;
-			
-				 /* display the menu above and/or to the left of the click if needed */
-				 if (max_width > window_width) {
-				   render_x -= menu_width;
-				   $('context-menu').addClassName('reverse-x');
-				 } else {
-					 $('context-menu').removeClassName('reverse-x');
-				 }
-				 if (max_height > window_height) {
-				   render_y -= menu_height;
-				   $('context-menu').addClassName('reverse-y');
-				 } else {
-					 $('context-menu').removeClassName('reverse-y');
-				 }
-				 if (render_x <= 0) render_x = 1;
-				 if (render_y <= 0) render_y = 1;
-				 $('context-menu').style['left'] = (render_x + 'px');
-				 $('context-menu').style['top'] = (render_y + 'px');
-				 
-         Effect.Appear('context-menu', {duration: 0.20});
-         if (window.parseStylesheets) { window.parseStylesheets(); } // IE
-      }})
+        var ws = window_size();
+        window_width = ws.width;
+        window_height = ws.height;
+
+        /* display the menu above and/or to the left of the click if needed */
+        if (max_width > window_width) {
+          render_x -= menu_width;
+          $('context-menu').addClassName('reverse-x');
+        } else {
+          $('context-menu').removeClassName('reverse-x');
+        }
+        if (max_height > window_height) {
+          render_y -= menu_height;
+          $('context-menu').addClassName('reverse-y');
+        } else {
+          $('context-menu').removeClassName('reverse-y');
+        }
+        if (render_x <= 0) render_x = 1;
+        if (render_y <= 0) render_y = 1;
+        $('context-menu').style['left'] = (render_x + 'px');
+        $('context-menu').style['top'] = (render_y + 'px');
+
+        Effect.Appear('context-menu', {duration: 0.20});
+        if (window.parseStylesheets) { window.parseStylesheets(); } // IE
+      }
+    });
   },
-  
+
   hideMenu: function() {
     Element.hide('context-menu');
   },
@@ -196,73 +211,77 @@ ContextMenu.prototype = {
     this.clearDocumentSelection(e);
   },
 
-  toggleSelection: function(tr,e) {
+  toggleSelection: function(tr, e) {
     if (this.isSelected(tr)) {
       this.removeSelection(tr);
     } else {
       this.addSelection(tr, e);
     }
   },
-  
+
   removeSelection: function(tr) {
     tr.removeClassName('context-menu-selection');
     this.checkSelectionBox(tr, false);
   },
-  
+
   unselectAll: function() {
     var rows = $$('.hascontextmenu');
-    for (i=0; i<rows.length; i++) {
+    for (i = 0; i < rows.length; i++) {
       this.removeSelection(rows[i]);
     }
   },
-  
+
   checkSelectionBox: function(tr, checked) {
-  	var inputs = Element.getElementsBySelector(tr, 'input');
-  	if (inputs.length > 0) { inputs[0].checked = checked; }
+    var inputs = Element.getElementsBySelector(tr, 'input');
+    if (inputs.length > 0) { inputs[0].checked = checked; }
   },
-  
+
   isSelected: function(tr) {
     return Element.hasClassName(tr, 'context-menu-selection');
   },
 
   clearDocumentSelection: function(e) {
     if (document.selection) {
-      if (document.selection.type == "Text" && e.shiftKey) {
+      if (document.selection.type == 'Text' && e.shiftKey) {
         document.selection.empty(); // IE
       }
     } else {
       window.getSelection().removeAllRanges();
     }
   }
-}
+};
 
 function toggleIssuesSelection(el) {
-	var boxes = el.getElementsBySelector('input[type=checkbox]');
-	var all_checked = true;
-	for (i = 0; i < boxes.length; i++) { if (boxes[i].checked == false) { all_checked = false; } }
-	for (i = 0; i < boxes.length; i++) {
-		if (all_checked) {
-			boxes[i].checked = false;
-			boxes[i].up('tr').removeClassName('context-menu-selection');
-		} else if (boxes[i].checked == false) {
-			boxes[i].checked = true;
-			boxes[i].up('tr').addClassName('context-menu-selection');
-		}
-	}
+  var boxes = el.getElementsBySelector('input[type=checkbox]');
+  var all_checked = true;
+  for (i = 0; i < boxes.length; i++) {
+    if (boxes[i].checked == false) {
+      all_checked = false;
+    }
+  }
+  for (i = 0; i < boxes.length; i++) {
+    if (all_checked) {
+      boxes[i].checked = false;
+      boxes[i].up('tr').removeClassName('context-menu-selection');
+    } else if (boxes[i].checked == false) {
+      boxes[i].checked = true;
+      boxes[i].up('tr').addClassName('context-menu-selection');
+    }
+  }
 }
 
 function window_size() {
-    var w;
-    var h;
-    if (window.innerWidth) {
-	w = window.innerWidth;
-	h = window.innerHeight;
-    } else if (document.documentElement) {
-	w = document.documentElement.clientWidth;
-	h = document.documentElement.clientHeight;
-    } else {
-	w = document.body.clientWidth;
-	h = document.body.clientHeight;
-    }
-    return {width: w, height: h};
+  var w;
+  var h;
+  if (window.innerWidth) {
+    w = window.innerWidth;
+    h = window.innerHeight;
+  } else if (document.documentElement) {
+    w = document.documentElement.clientWidth;
+    h = document.documentElement.clientHeight;
+  } else {
+    w = document.body.clientWidth;
+    h = document.body.clientHeight;
+  }
+  return {width: w, height: h};
 }
