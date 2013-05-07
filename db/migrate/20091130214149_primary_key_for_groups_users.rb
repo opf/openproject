8203880b95e7772a9cbf9b9857c3ca68a66244fb
Christian Rijke <crijke@gmail.com>
2013-05-07 14:40:52 +0200

    Update table name group_users in migrations.

--- db/migrate/20091130214149_primary_key_for_groups_users.rb
+++ db/migrate/20091130214149_primary_key_for_groups_users.rb
@@ -1,9 +1,9 @@
 class PrimaryKeyForGroupsUsers < ActiveRecord::Migration
   def self.up
-    add_column :group_user, :id, :primary_key
+    add_column :group_users, :id, :primary_key
   end
 
   def self.down
-    remove_column :group_user, :id
+    remove_column :group_users, :id
   end
 end
