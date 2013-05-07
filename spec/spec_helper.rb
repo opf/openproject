dee98037a6c1d4e8fc285960c7266003e17b0f76
Christian Rijke <crijke@gmail.com>
2013-05-07 16:00:44 +0200

    Remove even more spork stuff.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,9 +1,5 @@
 require 'rubygems'
 
-# Loading more in this block will cause your tests to run faster. However,
-# if you change any configuration or code from libraries loaded here, you'll
-# need to restart spork for it take effect.
-
 # This file is copied to spec/ when you run 'rails generate rspec:install'
 ENV["RAILS_ENV"] ||= 'test'
 require File.expand_path("../../config/environment", __FILE__)
