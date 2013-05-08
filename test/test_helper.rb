759508fcc50c3876900ab756ff5e9ca8c8072e8d
Toshi MARUYAMA <marutosijp2@yahoo.co.jp>
2013-05-08 20:57:33 +0200

    Add coveralls (coverage web service)

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -13,6 +13,10 @@
 #++
 
 ENV["RAILS_ENV"] = "test"
+
+require 'coveralls'
+Coveralls.wear!('rails')
+
 require File.expand_path('../../config/environment', __FILE__)
 require 'rails/test_help'
 require 'fileutils'
