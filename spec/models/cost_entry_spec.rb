f90e188e079619692237d74548064a73e5549e2f
Christian Rijke <crijke@gmail.com>
2013-05-10 15:40:20 +0200

    Remove plugin_helper require (it's already being required from spec_helper)

--- spec/models/cost_entry_spec.rb
+++ spec/models/cost_entry_spec.rb
@@ -1,5 +1,4 @@
 require File.expand_path(File.dirname(__FILE__) + '/../spec_helper')
-require File.expand_path(File.dirname(__FILE__) + '/../plugin_spec_helper')
 
 describe CostEntry do
   include Cost::PluginSpecHelper
