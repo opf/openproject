2b04d0a07d05fc18e17c0fdf7d0a91f4a7585f33
Sebastian Schuster <s.schuster@finn.de>
2013-05-13 09:44:11 +0200

    Removed unused mass assignment whitelisting and corresponding specs

--- lib/open_project/costs/patches/permitted_params_patch.rb
+++ lib/open_project/costs/patches/permitted_params_patch.rb
@@ -34,27 +34,6 @@ module OpenProject::Costs::Patches::PermittedParamsPatch
                                         { new_rate_attributes: [:valid_from, :rate] },
                                         { existing_rate_attributes: [:valid_from, :rate] })
     end
-
-    def labor_budget_item
-      params.require(:labor_budget_item).permit(:hours,
-                                                :comments,
-                                                :budget,
-                                                :user_id)
-    end
-
-    def material_budget_item
-      params.require(:material_budget_item).permit(:units,
-                                                   :comments,
-                                                   :budget,
-                                                   :cost_type,
-                                                   :cost_type_id)
-    end
-
-    def rate
-      params.require(:rate).permit(:rate,
-                                   :project,
-                                   :valid_from)
-    end
   end
 end
 
