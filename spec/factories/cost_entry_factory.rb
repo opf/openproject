982d4154e02db8e308f73cbfd15b52b7b52b087f
Michael Frister <michael+git@frister.net>
2013-05-22 11:23:05 +0200

    Make CostEntry factory pass validation

--- spec/factories/cost_entry_factory.rb
+++ spec/factories/cost_entry_factory.rb
@@ -1,5 +1,11 @@
 FactoryGirl.define do
   factory :cost_entry  do
-    association :cost_type, :factory => :cost_type
+    project
+    user { FactoryGirl.create(:user, :member_in_project => project)}
+    issue { FactoryGirl.create(:issue, :project => project) }
+    cost_type
+    spent_on Date.today
+    units 1
+    comments ''
   end
 end
