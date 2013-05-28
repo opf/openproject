675fef6390e9406c74e82539324ba5e351f27473
Christian Ratz <c.ratz@finn.de>
2013-05-28 17:47:05 +0200

    fixed failing spec

--- spec/models/system_user_spec.rb
+++ spec/models/system_user_spec.rb
@@ -56,7 +56,7 @@ describe SystemUser do
 
     it 'runs block with SystemUser' do
       @u.admin?.should be_false
-      User.current.should == AnonymousUser.first
+      User.current.should == User.anonymous
 
       @u.run_given do
         issue.done_ratio = 50
@@ -66,7 +66,7 @@ describe SystemUser do
       issue.journals.last.user.should == @u
 
       @u.admin?.should be_false
-      User.current.should == AnonymousUser.first
+      User.current.should == User.anonymous
     end
   end
 end
