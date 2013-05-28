8643925b8a85193b2418fee5e33d8c14c3ac82bf
Philipp Tessenow <philipp@tessenow.org>
2013-05-28 17:35:54 +0200

    lets push coverage data to coveralls when all tests pass

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -2,6 +2,10 @@ require 'rubygems'
 
 # This file is copied to spec/ when you run 'rails generate rspec:install'
 ENV["RAILS_ENV"] ||= 'test'
+
+require 'coveralls'
+Coveralls.wear_merged!('rails')
+
 require File.expand_path("../../config/environment", __FILE__)
 require 'rspec/rails'
 
--- test/test_helper.rb
+++ test/test_helper.rb
@@ -15,7 +15,7 @@
 ENV["RAILS_ENV"] = "test"
 
 require 'coveralls'
-Coveralls.wear!('rails')
+Coveralls.wear_merged!('rails')
 
 require File.expand_path('../../config/environment', __FILE__)
 require 'rails/test_help'
