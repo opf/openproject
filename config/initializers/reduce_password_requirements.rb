e520d8ca204d377aa844f00e9f7ef9ad7f8c9344
Michael Frister <michael+git@frister.net>
2013-06-06 09:37:15 +0200

    Fix default minimum password length for test environment

--- config/initializers/reduce_password_requirements.rb
+++ config/initializers/reduce_password_requirements.rb
@@ -2,5 +2,5 @@ if Rails.env == 'test'
   puts 'Reducing minimum password length to 4 characters for test environment'
   # Set default minimum length to 4, this doesn't require writing to the database.
   # Migrations might not have run here.
-  Setting.available_settings['password_min_length'] = 4
+  Setting.available_settings['password_min_length']['default'] = 4
 end
