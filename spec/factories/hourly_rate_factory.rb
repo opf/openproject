7afc8ed97530e954079e95c64bee4ec23b9c9b38
Sebastian Schuster <s.schuster@finn.de>
2013-06-10 15:16:18 +0200

    Used FactoryGirl instead of ObjectDaddy and included associated data to make validations pass after receiving data by send_table_to_object

--- spec/factories/hourly_rate_factory.rb
+++ spec/factories/hourly_rate_factory.rb
@@ -1,5 +1,7 @@
 FactoryGirl.define do
   factory :hourly_rate do
+    association :user, :factory => :user
+    association :project, :factory => :project
     valid_from Date.today
     rate 50.0
   end
