c981f068ec16669e6e9d86f0bdf46591354a8b8d
Christian Rijke <crijke@gmail.com>
2013-06-11 13:56:26 +0200

    Revert to previous non-RESTful routes to fix report button controls.

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 OpenProject::Application.routes.draw do
   scope 'projects/:project_id' do
-    resources :cost_reports, :only => [:index, :delete, :create, :update, :rename]
+    match  'projects/:project_id/cost_reports.:format', :controller => 'cost_reports', :project_id => /.+/, :action => 'index'
+    match 'projects/:project_id/cost_reports/:action/:id', :controller => 'cost_reports', :project_id => /.+/
   end
 end
