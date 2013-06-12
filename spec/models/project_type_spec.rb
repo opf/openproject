2f5387d164b9a29e997eb5e565f0693420bf44a7
Martin Czuchra <martin.czuchra@googlemail.com>
2013-06-12 14:55:57 +0200

    Fixes spec_helper path in model specs.

--- spec/models/project_type_spec.rb
+++ spec/models/project_type_spec.rb
@@ -9,7 +9,7 @@
 # See doc/COPYRIGHT.rdoc for more details.
 #++
 
-require File.expand_path('../../../spec_helper', __FILE__)
+require File.expand_path('../../spec_helper', __FILE__)
 
 describe ProjectType do
   describe '- Relations ' do
