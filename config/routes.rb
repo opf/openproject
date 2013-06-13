501415d94816d0ba32de93d513b624423c40845a
Sebastian Schuster <s.schuster@finn.de>
2013-06-13 16:07:12 +0200

    Fixed double scoping of cost reports routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,4 @@
 OpenProject::Application.routes.draw do
-  scope 'projects/:project_id' do
-    match  'projects/:project_id/cost_reports.:format', :controller => 'cost_reports', :project_id => /.+/, :action => 'index'
-    match 'projects/:project_id/cost_reports/:action/:id', :controller => 'cost_reports', :project_id => /.+/
-  end
+  match 'projects/:project_id/cost_reports', :controller => 'cost_reports', :project_id => /.+/, :action => 'index'
+  match 'projects/:project_id/cost_reports/:action/:id', :controller => 'cost_reports', :project_id => /.+/
 end
