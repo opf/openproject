de11bed7d930cbec574873c8c9170b542fdac87e
Sebastian Schuster <s.schuster@finn.de>
2013-06-20 13:45:18 +0200

    Changed breadcrump to equal the timelog_controller breadcrumb

--- app/helpers/costlog_helper.rb
+++ app/helpers/costlog_helper.rb
@@ -5,7 +5,7 @@ module CostlogHelper
     links = []
     links << link_to(l(:label_project_all), {:project_id => nil, :issue_id => nil})
     links << link_to(h(@project), {:project_id => @project, :issue_id => nil}) if @project
-    links << link_to_issue(@issue) if @issue
+    links << link_to_issue(@issue, :subject => false) if @issue
     breadcrumb links
   end
 
