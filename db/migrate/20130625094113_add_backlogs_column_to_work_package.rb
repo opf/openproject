73deeb78597228af4acef03bc1ad549e376bcb14
Hagen Schink <h.schink@finn.de>
2013-06-25 12:30:41 +0200

    Add migration for work package backlogs extension

---
+++ db/migrate/20130625094113_add_backlogs_column_to_work_package.rb
@@ -0,0 +1,7 @@
+class AddBacklogsColumnToWorkPackage < ActiveRecord::Migration
+  def change
+    add_column :work_packages, :position, :integer
+    add_column :work_packages, :story_points, :integer
+    add_column :work_packages, :remaining_hours, :float
+  end
+end
