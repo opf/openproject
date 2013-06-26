014834f53ed45d06815c799fe7bc5fc0e43a8f43
Sebastian Schuster <s.schuster@finn.de>
2013-06-26 12:24:44 +0200

    Also added patch for users in addition to principals as the has_many relation does not get inherited correctly

---
+++ lib/open_project/global_roles/patches/user_patch.rb
@@ -0,0 +1,14 @@
+module OpenProject::GlobalRoles::Patches
+  module UserPatch
+    def self.included(base) # :nodoc:
+      base.class_eval do
+        unloadable
+
+        has_many :principal_roes, :dependent => :destroy, :foreign_key => 'principal_id'
+        has_many :global_roles, :through => :principal_roles, :source => :role
+      end
+    end
+  end
+end
+
+User.send(:include, OpenProject::GlobalRoles::Patches::UserPatch)
