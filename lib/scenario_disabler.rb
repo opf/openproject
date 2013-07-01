cc5a06d65190e7f69f888d3573cb853763c0f9e3
Sebastian Schuster <s.schuster@finn.de>
2013-07-01 13:03:23 +0200

    Moved back to the old version of disabling scenarios hoping the ci does not complain anymore

--- lib/scenario_disabler.rb
+++ lib/scenario_disabler.rb
@@ -12,7 +12,7 @@
 class ScenarioDisabler
   def self.empty_if_disabled(scenario)
     if self.disabled?(scenario)
-      scenario.skip_invoke!
+      scenario.instance_variable_set(:@steps,::Cucumber::Ast::StepCollection.new([])) 
       true
     else
       false
