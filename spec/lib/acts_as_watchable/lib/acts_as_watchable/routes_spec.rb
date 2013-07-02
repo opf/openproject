9a6a94bddc77b59284ad863b2f6aaf66af7e1b04
Hagen Schink <h.schink@finn.de>
2013-07-02 08:41:32 +0200

    Test watchable route prefixes

--- spec/lib/acts_as_watchable/lib/acts_as_watchable/routes_spec.rb
+++ spec/lib/acts_as_watchable/lib/acts_as_watchable/routes_spec.rb
@@ -39,7 +39,11 @@ describe OpenProject::Acts::Watchable::Routes do
       end
     end
 
-    ['work_packages', 'news', 'news', 'boards', 'messages', 'wikis', 'wiki_pages'].each do |type|
+    class AnWatchableClassWithRoutePrefix < ActiveRecord::Base
+      acts_as_watchable :route_prefix => 'watchable_objects'
+    end
+
+    ['work_packages', 'news', 'news', 'boards', 'messages', 'wikis', 'wiki_pages', 'watchable_objects'].each do |type|
       describe "routing #{type} watches" do
         let(:type) { type }
 
