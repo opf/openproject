44f7e3c27159741714ddd652670d99bbcaafcc0d
Jens Ulferts <j.ulferts@finn.de>
2013-07-08 15:02:14 +0200

    adapts planning_element factory to changed end_date attribute

--- spec/factories/planning_element_factory.rb
+++ spec/factories/planning_element_factory.rb
@@ -42,7 +42,7 @@ FactoryGirl.define do
     sequence(:description) { |n| "Planning Element No. #{n} is the most important part of the project." }
 
     sequence(:start_date) { |n| ((n - 1) * 7).days.since.to_date }
-    sequence(:end_date)   { |n| (n * 7).days.since.to_date }
+    sequence(:due_date)   { |n| (n * 7).days.since.to_date }
 
     association :project
   end
