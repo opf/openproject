edb4ec5b09de1aed80b06ebd450e0c358d86790b
Philipp Tessenow <philipp@tessenow.org>
2013-07-23 18:24:26 +0200

    adding specs for the new activity module
    add newline at end of file

---
+++ spec/controllers/activities_controller_spec.rb
@@ -0,0 +1,51 @@
+#-- copyright
+# OpenProject is a project management system.
+#
+# Copyright (C) 2012-2013 the OpenProject Team
+#
+# This program is free software; you can redistribute it and/or
+# modify it under the terms of the GNU General Public License version 3.
+#
+# See doc/COPYRIGHT.rdoc for more details.
+#++
+
+require 'spec_helper'
+
+describe ActivitiesController do
+  before :each do
+    @controller.stub!(:set_localization)
+
+    admin = FactoryGirl.create(:admin)
+    User.stub!(:current).and_return admin
+
+    @params = {}
+  end
+
+  describe 'index' do
+    describe 'with activated activity module' do
+      before do
+        @project = FactoryGirl.create(:project, :enabled_module_names => %w[activity wiki])
+        @params[:project_id] = @project.id
+      end
+
+      it 'renders activity' do
+        get 'index', @params
+        response.should be_success
+        response.should render_template 'index'
+      end
+    end
+
+    describe 'without activated activity module' do
+      before do
+        @project = FactoryGirl.create(:project, :enabled_module_names => %w[wiki])
+        @params[:project_id] = @project.id
+      end
+
+      it 'renders 403' do
+        get 'index', @params
+        response.status.should == 403
+        response.should render_template 'common/error'
+      end
+    end
+  end
+end
--- spec/controllers/projects_controller_spec.rb
+++ spec/controllers/projects_controller_spec.rb
@@ -27,6 +27,18 @@ describe ProjectsController do
     @params = {}
   end
 
+  def clear_settings_cache
+    Rails.cache.clear
+  end
+
+  # this is the base method for get, post, etc.
+  def process(*args)
+    clear_settings_cache
+    result = super
+    clear_settings_cache
+    result
+  end
+
   describe 'show' do
     render_views
 
@@ -96,6 +108,137 @@ describe ProjectsController do
 
           assert_select "#main-menu a.Sub", 'Sub'
         end
+      end
+    end
+
+    describe 'with activated activity module' do
+      before do
+        @project = FactoryGirl.create(:project, :enabled_module_names => %w[activity])
+        @params[:id] = @project.id
+      end
+
+      it 'renders show' do
+        get 'show', @params
+        response.should be_success
+        response.should render_template 'show'
+      end
+
+      it 'renders main menu with activity tab' do
+        get 'show', @params
+        assert_select '#main-menu a.activity'
+      end
+    end
+
+    describe 'without activated activity module' do
+      before do
+        @project = FactoryGirl.create(:project, :enabled_module_names => %w[wiki])
+        @params[:id] = @project.id
+      end
+
+      it 'renders show' do
+        get 'show', @params
+        response.should be_success
+        response.should render_template 'show'
+      end
+
+      it 'renders main menu without activity tab' do
+        get 'show', @params
+        response.body.should_not have_selector '#main-menu a.activity'
+      end
+    end
+  end
+
+  describe 'new' do
+    render_views
+
+    before(:all) do
+      @previous_projects_modules = Setting.default_projects_modules
+    end
+
+    after(:all) do
+      Setting.default_projects_modules = @previous_projects_modules
+    end
+
+    describe 'with activity in Setting.default_projects_modules' do
+      before do
+        Setting.default_projects_modules = %w[activity wiki]
+      end
+
+      it "renders 'new'" do
+        get 'new', @params
+        response.should be_success
+        response.should render_template 'new'
+      end
+
+      it 'renders available modules list with activity being selected' do
+        get 'new', @params
+
+        response.body.should have_selector "input[@name='project[enabled_module_names][]'][@value='activity'][@checked='checked']"
+        response.body.should have_selector "input[@name='project[enabled_module_names][]'][@value='wiki'][@checked='checked']"
+      end
+    end
+
+    describe 'without activated activity module' do
+      before do
+        Setting.default_projects_modules = %w[wiki]
+      end
+
+      it "renders 'new'" do
+        get 'new', @params
+        response.should be_success
+        response.should render_template 'new'
+      end
+
+      it 'renders available modules list without activity being selected' do
+        get 'new', @params
+
+        response.body.should have_selector "input[@name='project[enabled_module_names][]'][@value='wiki'][@checked='checked']"
+        response.body.should have_selector "input[@name='project[enabled_module_names][]'][@value='activity']"
+        response.body.should_not have_selector "input[@name='project[enabled_module_names][]'][@value='activity'][@checked='checked']"
+      end
+    end
+  end
+
+  describe 'settings' do
+    render_views
+
+    describe 'with activity in Setting.default_projects_modules' do
+      before do
+        @project = FactoryGirl.create(:project, :enabled_module_names => %w[activity wiki])
+        @params[:id] = @project.id
+      end
+
+      it 'renders settings/modules' do
+        get 'settings', @params.merge(:tab => 'modules')
+        response.should be_success
+        response.should render_template 'settings'
+      end
+
+      it 'renders available modules list with activity being selected' do
+        get 'settings', @params.merge(:tab => 'modules')
+        response.body.should have_selector "#modules-form input[@name='enabled_module_names[]'][@value='activity'][@checked='checked']"
+        response.body.should have_selector "#modules-form input[@name='enabled_module_names[]'][@value='wiki'][@checked='checked']"
+      end
+    end
+
+    describe 'without activated activity module' do
+      before do
+        @project = FactoryGirl.create(:project, :enabled_module_names => %w[wiki])
+        @params[:id] = @project.id
+      end
+
+      it 'renders settings/modules' do
+        get 'settings', @params.merge(:tab => 'modules')
+        response.should be_success
+        response.should render_template 'settings'
+      end
+
+      it 'renders available modules list without activity being selected' do
+        get 'settings', @params.merge(:tab => 'modules')
+
+        response.body.should have_selector "#modules-form input[@name='enabled_module_names[]'][@value='wiki'][@checked='checked']"
+        response.body.should have_selector "#modules-form input[@name='enabled_module_names[]'][@value='activity']"
+        response.body.should_not have_selector "#modules-form input[@name='enabled_module_names[]'][@value='activity'][@checked='checked']"
       end
     end
   end
