c09ca172a03bcfbe64b47373078c4ad0a73054df
Philipp Tessenow <philipp@tessenow.org>
2013-07-23 18:22:32 +0200

    hide activity menu item when activity is disabled

--- lib/redmine.rb
+++ lib/redmine.rb
@@ -278,7 +278,8 @@ end
 
 Redmine::MenuManager.map :project_menu do |menu|
   menu.push :overview, { :controller => '/projects', :action => 'show' }
-  menu.push :activity, { :controller => '/activities', :action => 'index' }, :param => :project_id
+  menu.push :activity, { :controller => '/activities', :action => 'index' }, :param => :project_id,
+              :if => Proc.new { |p| p.module_enabled?("activity") }
   menu.push :roadmap, { :controller => '/versions', :action => 'index' }, :param => :project_id,
               :if => Proc.new { |p| p.shared_versions.any? }
 
