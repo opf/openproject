f258bd318e3c754997ab3c5bb3e6d62b090a9410
Martin Czuchra <martin.czuchra@googlemail.com>
2013-07-29 17:03:15 +0200

    Prefixes all tables w/ "legacy_"
    Also removes planning_element_type_id on work_packages.

--- db/migrate/20130729114110_move_planning_element_types_to_legacy_planning_element_types.rb
+++ db/migrate/20130729114110_move_planning_element_types_to_legacy_planning_element_types.rb
@@ -1,8 +1,17 @@
-class MigrateAndRemovePlanningElementTypes < ActiveRecord::Migration
+class MovePlanningElementTypesToLegacyPlanningElementTypes < ActiveRecord::Migration
   def up
+    rename_table :default_planning_element_types, :legacy_default_planning_element_types
+    rename_table :enabled_planning_element_types, :legacy_enabled_planning_element_types
+    rename_table :planning_element_types,         :legacy_planning_element_types
 
+    remove_column :work_packages, :planning_element_type_id
   end
 
   def down
+    rename_table :legacy_default_planning_element_types, :default_planning_element_types
+    rename_table :legacy_enabled_planning_element_types, :enabled_planning_element_types
+    rename_table :legacy_planning_element_types,         :planning_element_types
+
+    add_column :work_packages, :planning_element_type_id, :integer
   end
 end
