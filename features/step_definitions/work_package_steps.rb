231972eedfd16ca8f81d12f671cb7274facda027
Jens Ulferts <j.ulferts@finn.de>
2013-07-30 16:02:01 +0200

    adapts WorkPackage creation in cukes

--- features/step_definitions/work_package_steps.rb
+++ features/step_definitions/work_package_steps.rb
@@ -24,11 +24,7 @@ Given /^the work package "(.*?)" has the following children:$/ do |work_package_
   table.raw.flatten.each do |child_subject|
     child = WorkPackage.find_by_subject(child_subject)
 
-    if child.is_a? Issue
-      child.parent_issue_id = parent.id
-    elsif child.is_a? PlanningElement
-      child.parent_id = parent.id
-    end
+    child.parent_issue_id = parent.id
 
     child.save
   end
