f5784c0d43a7cd50570df290ee13edb8c49d7ae5
Martin Czuchra <martin.czuchra@googlemail.com>
2013-07-30 09:52:07 +0200

    Removes facrories and specs for obsolete models.
    This removes factories and specs for DefaultPlanningElementTypes and
    EnabledPlanningElementTypes.

--- spec/factories/enabled_planning_element_type_factory.rb
+++ spec/factories/enabled_planning_element_type_factory.rb
@@ -9,9 +9,9 @@
 # See doc/COPYRIGHT.rdoc for more details.
 #++
 
-FactoryGirl.define do
-  factory(:enabled_planning_element_type, :class => EnabledPlanningElementType) do
-    project               { |e| e.association(:project) }
-    planning_element_type { |e| e.association(:planning_element_type) }
-  end
-end
+# FactoryGirl.define do
+#   factory(:enabled_planning_element_type, :class => EnabledPlanningElementType) do
+#     project               { |e| e.association(:project) }
+#     planning_element_type { |e| e.association(:planning_element_type) }
+#   end
+# end
--- spec/models/enabled_planning_element_type_spec.rb
+++ spec/models/enabled_planning_element_type_spec.rb
@@ -8,29 +8,29 @@
 #
 # See doc/COPYRIGHT.rdoc for more details.
 #++
-
-require File.expand_path('../../spec_helper', __FILE__)
-
-describe EnabledPlanningElementType do
-  describe '- Relations ' do
-    it 'can read the project w/ the help of the belongs_to association' do
-      project                       = FactoryGirl.create(:project)
-      enabled_planning_element_type = FactoryGirl.create(:enabled_planning_element_type,
-                                                     :project_id => project.id)
-
-      enabled_planning_element_type.reload
-
-      enabled_planning_element_type.project.should == project
-    end
-
-    it 'can read the planning_element_type w/ the help of the belongs_to association' do
-      planning_element_type         = FactoryGirl.create(:planning_element_type)
-      enabled_planning_element_type = FactoryGirl.create(:enabled_planning_element_type,
-                                                     :planning_element_type_id => planning_element_type.id)
-
-      enabled_planning_element_type.reload
-
-      enabled_planning_element_type.planning_element_type.should == planning_element_type
-    end
-  end
-end
+#
+# require File.expand_path('../../spec_helper', __FILE__)
+#
+# describe EnabledPlanningElementType do
+#   describe '- Relations ' do
+#     it 'can read the project w/ the help of the belongs_to association' do
+#       project                       = FactoryGirl.create(:project)
+#       enabled_planning_element_type = FactoryGirl.create(:enabled_planning_element_type,
+#                                                      :project_id => project.id)
+#
+#       enabled_planning_element_type.reload
+#
+#       enabled_planning_element_type.project.should == project
+#     end
+#
+#     it 'can read the planning_element_type w/ the help of the belongs_to association' do
+#       planning_element_type         = FactoryGirl.create(:planning_element_type)
+#       enabled_planning_element_type = FactoryGirl.create(:enabled_planning_element_type,
+#                                                      :planning_element_type_id => planning_element_type.id)
+#
+#       enabled_planning_element_type.reload
+#
+#       enabled_planning_element_type.planning_element_type.should == planning_element_type
+#     end
+#   end
+# end
