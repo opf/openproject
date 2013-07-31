1754eb9338141be32498e697f9fc50eb5eaf59cf
Hagen Schink <h.schink@finn.de>
2013-07-31 10:27:50 +0200

    Adds work package move permissions

--- lib/redmine.rb
+++ lib/redmine.rb
@@ -89,6 +89,7 @@ Redmine::AccessControl.map do |map|
     map.permission :add_issues, {:issues => [:new, :create, :update_form],
                                  :'issues/previews' => :create}
     map.permission :add_work_packages, { :work_packages => [:new, :new_type, :create] }
+    map.permission :move_work_packages, {:'work_packages/moves' => [:new, :create]}, :require => :loggedin
     map.permission :edit_work_packages, { :issues => [:edit, :update, :bulk_edit, :bulk_update, :update_form, :quoted],
                                           :'issues/previews' => :create}
     map.permission :manage_issue_relations, {:issue_relations => [:create, :destroy]}
