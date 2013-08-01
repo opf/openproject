7a33e08902c5866da3d7fe639ea89b98b37aad00
Jens Ulferts <j.ulferts@finn.de>
2013-08-01 12:51:11 +0200

    removes planning_element_type keys from locales

---
+++ config/setup_load_paths.rb
@@ -0,0 +1,12 @@
+if ENV['MY_RUBY_HOME'] && ENV['MY_RUBY_HOME'].include?('rvm')
+  begin
+    rvm_path     = File.dirname(File.dirname(ENV['MY_RUBY_HOME']))
+    rvm_lib_path = File.join(rvm_path, 'lib')
+    $LOAD_PATH.unshift rvm_lib_path
+    require 'rvm'
+    RVM.use_from_path! File.dirname(File.dirname(__FILE__))
+  rescue LoadError
+    # RVM is unavailable at this point.
+    raise "RVM ruby lib is currently unavailable."
+  end
+end
