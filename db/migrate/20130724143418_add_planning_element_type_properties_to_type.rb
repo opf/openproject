5d14204ed9035f39d43980c5718b1513e96a5bbe
Martin Czuchra <martin.czuchra@googlemail.com>
2013-08-01 11:35:02 +0200

    Remove code that is commented out.

--- db/migrate/20130724143418_add_planning_element_type_properties_to_type.rb
+++ db/migrate/20130724143418_add_planning_element_type_properties_to_type.rb
@@ -1,25 +1,5 @@
 class AddPlanningElementTypePropertiesToType < ActiveRecord::Migration
 
-  # create_table "planning_element_types", :force => true do |t|
-  #   t.string   "name",                              :null => false
-  #   t.boolean  "in_aggregation", :default => true,  :null => false
-  #   t.boolean  "is_milestone",   :default => false, :null => false
-  #   t.boolean  "is_default",     :default => false, :null => false
-  #   t.integer  "position",       :default => 1
-  #   t.integer  "color_id"
-  #   t.datetime "created_at",                        :null => false
-  #   t.datetime "updated_at",                        :null => false
-  # end
-
-  # add_index "planning_element_types", ["color_id"], :name => "index_timelines_planning_element_types_on_color_id"
-
-  # create_table "types", :force => true do |t|
-  #   t.string  "name",          :limit => 30, :default => "",    :null => false
-  #   t.boolean "is_in_chlog",                 :default => false, :null => false
-  #   t.integer "position",                    :default => 1
-  #   t.boolean "is_in_roadmap",               :default => true,  :null => false
-  # end
-
   def up
 
     add_column :types, :in_aggregation, :boolean, :default => true,  :null => false
