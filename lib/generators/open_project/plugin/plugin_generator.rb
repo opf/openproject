fe4e1b7c23a66df7631233fc7d64a43cdcac0cd7
Jens Ulferts <j.ulferts@finn.de>
2013-08-02 16:43:33 +0200

    initial generator

---
+++ lib/generators/open_project/plugin/plugin_generator.rb
@@ -0,0 +1,44 @@
+class OpenProject::PluginGenerator < Rails::Generators::Base
+  source_root File.expand_path('../templates', __FILE__)
+
+  argument :plugin_name, :type => :string, :default => "openproject-new-plugin"
+  argument :root_folder, :type => :string, :default => "vendor/gems"
+
+  # every public method is run when the generator is invoked
+  def generate_plugin
+    plugin_dir
+    lib_dir
+  end
+
+  def full_name
+    @full_name ||= begin
+      "openproject-#{plugin_name}"
+    end
+  end
+
+  private
+  def raise_on_params
+    puts plugin_name
+    puts root_folder
+  end
+
+  def plugin_path
+    "#{root_folder}/openproject-#{plugin_name}"
+  end
+
+  def plugin_dir
+    @plugin_dir ||= begin
+      empty_directory(plugin_path)
+    end
+  end
+
+  def lib_path
+    "#{plugin_path}/lib"
+  end
+
+  def lib_dir
+    @lib_dir ||= begin
+      directory('lib', lib_path)
+    end
+  end
+end
