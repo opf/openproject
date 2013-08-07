a83a777159e2792970c9dc6f1dbfa82ef8622531
Sebastian Schuster <s.schuster@finn.de>
2013-08-07 14:58:12 +0200

    Updated changelog and simplified migration

--- db/migrate/20130807141542_remove_files_attached_to_projects_and_versions.rb
+++ db/migrate/20130807141542_remove_files_attached_to_projects_and_versions.rb
@@ -4,12 +4,7 @@ class RemoveFilesAttachedToProjectsAndVersions < ActiveRecord::Migration
       raise "There are still attachments attached to Versions or Projects, please remove them."
     end
     #undocument this code if you want do delete all existing files attached to projects and versions
-    #path = Rails.root.to_s + '/files/'
-    #Attachment.where(:container_type => ['Version','Project']).each do |attachment|
-    #  file = path + attachment.disk_filename
-    #  File.delete(file) if File.exists?(file)
-    #end
-    #Attachment.where(:container_type => ['Version','Project']).delete_all
+    #Attachment.where(:container_type => ['Version','Project']).destroy_all
   end
 
   def down
