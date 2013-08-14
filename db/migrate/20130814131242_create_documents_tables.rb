988f0f74e66001e273edf4dc147c4e4b07b3c07c
Sebastian Schuster <s.schuster@finn.de>
2013-08-14 16:32:47 +0200

    Removed force create and added index

--- db/migrate/20130814131242_create_documents_tables.rb
+++ db/migrate/20130814131242_create_documents_tables.rb
@@ -1,13 +1,16 @@
 class CreateDocumentsTables < ActiveRecord::Migration
   def up
     unless ActiveRecord::Base.connection.table_exists? 'documents'
-      create_table "documents", :force => true do |t|
+      create_table "documents" do |t|
         t.integer  "project_id",                :default => 0,  :null => false
         t.integer  "category_id",               :default => 0,  :null => false
         t.string   "title",       :limit => 60, :default => "", :null => false
         t.text     "description"
         t.datetime "created_on"
       end
+      add_index "documents", ["category_id"], :name => "index_documents_on_category_id"
+      add_index "documents", ["created_on"], :name => "index_documents_on_created_on"
+      add_index "documents", ["project_id"], :name => "documents_project_id"
     end
   end
 
