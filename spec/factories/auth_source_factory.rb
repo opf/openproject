be98cc698b3e0cb6aa1151f84ac96153426aec51
Michael Frister <michael+git@frister.net>
2013-08-15 08:54:46 +0100

    Add factory for LdapAuthSource

--- spec/factories/auth_source_factory.rb
+++ spec/factories/auth_source_factory.rb
@@ -13,4 +13,10 @@ FactoryGirl.define do
   factory :auth_source do
     name 'Test AuthSource'
   end
+  factory :ldap_auth_source, :class => LdapAuthSource do
+    name 'Test LDAP AuthSource'
+    host '127.0.0.1'
+    port 225  # a reserved port, should not be in use
+    attr_login 'uid'
+  end
 end
