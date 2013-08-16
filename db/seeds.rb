bded0ffe2d9531565cceadba85ed8d72c3cb2d86
Stefan Frank <s.frank@vierundsechzig.de>
2013-08-16 06:53:23 +0200

    seeding now loads an environment-specific seed
    seeds are now loaded from db/seeds/<Rails.env>.rb. If plugins have
    seeds, these are also loaded.

--- db/seeds.rb
+++ db/seeds.rb
@@ -17,3 +17,25 @@
 #
 #   cities = City.create([{ :name => 'Chicago' }, { :name => 'Copenhagen' }])
 #   Mayor.create(:name => 'Emanuel', :city => cities.first)
+
+
+# loads environment-specific seeds: The assumed directory structure in db/ is like this:
+#|___seeds
+#| |___all.rb
+#| |___development.rb
+#| |___staging.rb
+#| |___production.rb
+#|___seeds.rb
+#
+['all', Rails.env].each do |seed|
+  seed_file = "#{Rails.root}/db/seeds/#{seed}.rb"
+  if File.exists?(seed_file)
+    puts "*** Loading #{seed} seed data"
+    require seed_file
+  end
+end
+
+Rails::Application::Railties.engines.each do |engine|
+  puts "*** Loading #{engine.engine_name} seed data"
+  engine.load_seed
+end
