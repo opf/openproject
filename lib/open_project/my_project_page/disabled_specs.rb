9744514ca328622de5757bd8d0607a5c44904822
Christian Ratz <c.ratz@finn.de>
2013-08-16 08:37:28 +0200

    require rspec/example_disabler' only in test env
    as the Gemfile says

--- lib/open_project/my_project_page/disabled_specs.rb
+++ lib/open_project/my_project_page/disabled_specs.rb
@@ -1,4 +1,6 @@
-require "rspec/example_disabler"
-# TODO does rspec add a space randomly to the metadata?! (better make the example-disabler a litte more resilient against this)
-RSpec::ExampleDisabler.disable_example('ProjectsController show ', "plugin openproject-my_project_overview overwrites routes for show.")
-RSpec::ExampleDisabler.disable_example('ProjectsController show', "plugin openproject-my_project_overview overwrites routes for show.")
+if Rails.env.test?
+  require "rspec/example_disabler"
+  # TODO does rspec add a space randomly to the metadata?! (better make the example-disabler a litte more resilient against this)
+  RSpec::ExampleDisabler.disable_example('ProjectsController show ', "plugin openproject-my_project_overview overwrites routes for show.")
+  RSpec::ExampleDisabler.disable_example('ProjectsController show', "plugin openproject-my_project_overview overwrites routes for show.")
+end
