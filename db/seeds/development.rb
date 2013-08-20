01b79790d15dbf75bc0d25494fa587cb883c3a56
Stefan Frank <s.frank@vierundsechzig.de>
2013-08-20 10:12:14 +0200

    Added a sample project to the development-seed.[skip ci]
    using RAILS_ENV=development rake db:seed, you can now generates a sample
    project: The project contains several issues, a timeline and lots of
    planning-elements to play with. Feel free to add some more sample data
    to this pet-project.
    
    The project is re-generated every time you seed the database, so every
    changes to this project will be lost.
    
    The ci is currently skipped, as it currently does not run the seeding.

--- db/seeds/development.rb
+++ db/seeds/development.rb
@@ -1,1 +1,76 @@
 # add seeds specific for the development-environment here
+
+puts "Creating seeded project..."
+if delete_me=Project.find_by_identifier("seeded_project")
+  delete_me.destroy
+end
+
+project = FactoryGirl.create(:public_project, name: "Seeded Project", identifier: "seeded_project", description: Faker::Lorem.paragraph(5))
+project.types << Type.all
+project.save
+
+# this will fail rather miserably, when there are no statuses present
+statuses = IssueStatus.all
+types = project.types.all
+puts "Creating seeded project...done."
+
+puts "Creating issues ..."
+20.times do |count|
+  login = "#{Faker::Name.first_name}#{rand(10000)}"
+
+  user = User.find_by_login(login)
+
+  unless user
+    user = FactoryGirl.create(:user,
+                              login: login,
+                              firstname: Faker::Name.first_name,
+                              lastname: Faker::Name.last_name,
+                              mail: Faker::Internet.email)
+  end
+
+  ## let every user about 5 issues...
+
+  rand(30).times do
+    print "."
+    issue = Issue.create!(project: project,
+                          author: user,
+                          status: statuses.sample,
+                          subject: Faker::Lorem.words(8).join(" "),
+                          description: Faker::Lorem.paragraph(5, true,3),
+                          type: types.sample
+    )
+
+  end
+
+  rand(20).times do
+    start_date = rand(20).days.from_now
+    due_date   = start_date + rand(20).days
+
+    element = PlanningElement.create!(project: project,
+                                      author: user,
+                                      status: statuses.sample,
+                                      subject: Faker::Lorem.words(5).join(" "),
+                                      description: Faker::Lorem.paragraph(5, true,3),
+                                      type: types.sample,
+                                      start_date: start_date,
+                                      due_date: due_date)
+    rand(5).times do
+      offset = start_date
+      element = PlanningElement.create!(project: project,
+                                        author: user,
+                                        status: statuses.sample,
+                                        subject: Faker::Lorem.words(5).join(" "),
+                                        description: Faker::Lorem.paragraph(5, true,3),
+                                        type: types.sample,
+                                        start_date: start_date,
+                                        due_date: due_date)
+    end
+
+
+  end
+
+
+end
+
+puts "\n"
+puts "#{project.work_packages.count} issues created."
