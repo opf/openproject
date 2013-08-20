1d662a8b0657b70924532b3338a66715650be567
Stefan Frank <s.frank@vierundsechzig.de>
2013-08-20 09:11:34 +0200

    removed the duplicate comment [skip ci]

--- db/seeds.rb
+++ db/seeds.rb
@@ -17,28 +17,6 @@
 #
 #   cities = City.create([{ :name => 'Chicago' }, { :name => 'Copenhagen' }])
 #   Mayor.create(:name => 'Emanuel', :city => cities.first)
-
-
-
-#-- encoding: UTF-8
-#-- copyright
-# OpenProject is a project management system.
-#
-# Copyright (C) 2012-2013 the OpenProject Team
-#
-# This program is free software; you can redistribute it and/or
-# modify it under the terms of the GNU General Public License version 3.
-#
-# See doc/COPYRIGHT.rdoc for more details.
-#++
-
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ :name => 'Chicago' }, { :name => 'Copenhagen' }])
-#   Mayor.create(:name => 'Emanuel', :city => cities.first)
 #
 # loads environment-specific seeds. The assumed directory structure in db/ is like this:
 #|___seeds
