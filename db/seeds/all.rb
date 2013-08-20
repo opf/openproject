0af29ae525ef34ed30850600d2b1fb52bfe47e44
Martin Czuchra <martin.czuchra@googlemail.com>
2013-08-20 15:40:53 +0200

    Improves seeds.rb to allow consecutive runs.
    seeds.rb now can be run multiple times and should not break instances
    that already have data in their DBs by using find_or_create_[...].
    This also creates the colors in seeds now.

--- db/seeds/all.rb
+++ db/seeds/all.rb
@@ -1,13 +1,17 @@
 # add seeds here, that need to be available in all environments
-unless Type.exists?(name: "none")
-  Type.connection.schema_cache.clear!
-  Type.reset_column_information
-  Type.create!(name: 'none',
-               color_id: '#000000',
-               is_standard: true,
-               is_default: true,
-               is_in_chlog: true,
-               is_in_roadmap: true,
-               in_aggregation: true,
-               is_milestone: false)
+
+[Type, PlanningElementTypeColor].each do |klass|
+  klass.connection.schema_cache.clear!
+  klass.reset_column_information
 end
+
+PlanningElementTypeColor.ms_project_colors.map(&:save)
+default_color = PlanningElementTypeColor.find_by_name('pjSilver')
+
+Type.find_or_create_by_is_standard(true, name: 'none',
+                                         color_id: default_color.id,
+                                         is_default: true,
+                                         is_in_chlog: true,
+                                         is_in_roadmap: true,
+                                         in_aggregation: true,
+                                         is_milestone: false)
