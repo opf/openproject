c3902e15d4d201da7703dca7730753fcfd41496b
Martin Czuchra <martin.czuchra@googlemail.com>
2013-08-21 14:56:45 +0200

    Adds proper limits on wiki content fields.
    The limits do not apply to postgres, which is why the schema.rb never
    included them. MySQL would have produced a different schema.rb.
    Subsequent migrations would break when the limit was not established
    in MySQL, leading to a much shorter limit. Yes, that is true, no limit
    results in a field of shorter size than the maximum size for fields in
    MySQL. Hooray for smart defaults!

--- db/migrate/000_aggregated_migrations.rb
+++ db/migrate/000_aggregated_migrations.rb
@@ -772,25 +772,25 @@ class AggregatedMigrations < ActiveRecord::Migration
     add_index "watchers", ["watchable_id", "watchable_type"], :name => "index_watchers_on_watchable_id_and_watchable_type"
 
     create_table "wiki_content_versions", :force => true do |t|
-      t.integer  "wiki_content_id",                              :null => false
-      t.integer  "page_id",                                      :null => false
+      t.integer  "wiki_content_id",                                        :null => false
+      t.integer  "page_id",                                                :null => false
       t.integer  "author_id"
-      t.binary   "data"
-      t.string   "compression",     :limit => 6, :default => ""
-      t.string   "comments",                     :default => ""
-      t.datetime "updated_on",                                   :null => false
-      t.integer  "version",                                      :null => false
+      t.binary   "data",            :limit => 16.megabytes
+      t.string   "compression",     :limit => 6,           :default => ""
+      t.string   "comments",                               :default => ""
+      t.datetime "updated_on",                                             :null => false
+      t.integer  "version",                                                :null => false
     end
 
     add_index "wiki_content_versions", ["updated_on"], :name => "index_wiki_content_versions_on_updated_on"
     add_index "wiki_content_versions", ["wiki_content_id"], :name => "wiki_content_versions_wcid"
 
     create_table "wiki_contents", :force => true do |t|
-      t.integer  "page_id",      :null => false
+      t.integer  "page_id",                              :null => false
       t.integer  "author_id"
-      t.text     "text"
-      t.datetime "updated_on",   :null => false
-      t.integer  "lock_version", :null => false
+      t.text     "text",         :limit => 16.megabytes
+      t.datetime "updated_on",                           :null => false
+      t.integer  "lock_version",                         :null => false
     end
 
     add_index "wiki_contents", ["author_id"], :name => "index_wiki_contents_on_author_id"
