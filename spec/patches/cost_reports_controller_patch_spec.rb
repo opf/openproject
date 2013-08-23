392690526f27ed94e84e2e230f31fadf4e084f79
Michael Frister <m.frister@finn.de>
2013-08-23 17:57:00 +0100

    Make specs run (some are not implemented)

--- spec/patches/cost_reports_controller_patch_spec.rb
+++ spec/patches/cost_reports_controller_patch_spec.rb
@@ -1,22 +1,22 @@
 require 'spec_helper'
-require 'xls_report/issues_controller_patch'
 
-describe CostReportsController, "rendering to xls" do
-  
+describe 'CostReportsController', "rendering to xls" do
+  pending 'XlsExport: CostReports support not yet migrated to Rails 3'
+
   it "should respond with the xls if requested in the index" do
     pending
     render :action => :index
     response.should be_redirect
   end
-  
+
   it "should not respond with the xls if requested in a detail view" do
     pending
     render :action => :show
     response.should be_redirect
   end
-  
+
   it "should generate xls from issues" do
     pending
   end
-  
+
 end
