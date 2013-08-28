7239cf1963d971021d2f1560109f9f19b689ac92
Hagen Schink <h.schink@finn.de>
2013-08-28 10:09:22 +0200

    Corrects email notification box layout

--- app/helpers/work_packages_helper.rb
+++ app/helpers/work_packages_helper.rb
@@ -209,12 +209,11 @@ module WorkPackagesHelper
   end
 
   def send_notification_option
-    content_tag(:p,
-                content_tag(:label,
-                            l(:label_notify_member_plural),
-                            :for => 'send_notification') +
-                hidden_field_tag('send_notification', '0', :id => nil) +
-                check_box_tag('send_notification', '1', true))
+    content_tag(:label,
+                l(:label_notify_member_plural),
+                  :for => 'send_notification') +
+    hidden_field_tag('send_notification', '0', :id => nil) +
+    check_box_tag('send_notification', '1', true)
   end
 
   def render_work_package_tree_row(work_package, level, relation)
