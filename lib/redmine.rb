97144cc96ebbcfc2c021019b365e7a2fb7d48578
Hagen Schink <h.schink@finn.de>
2013-08-29 13:42:49 +0200

    Implements destroy for work packages controller

--- lib/redmine.rb
+++ lib/redmine.rb
@@ -91,6 +91,7 @@ Redmine::AccessControl.map do |map|
     map.permission :edit_work_packages, { :issues => [:edit, :update, :bulk_edit, :bulk_update, :update_form, :quoted],
                                           :work_packages => [:edit, :update, :new_type, :preview],
                                           :'issues/previews' => :create}
+    map.permission :delete_work_packages, {:work_packages => :destroy}, :require => :member
     map.permission :manage_issue_relations, {:issue_relations => [:create, :destroy]}
     map.permission :manage_work_package_relations, {:work_package_relations => [:create, :destroy]}
     map.permission :manage_subtasks, {}
