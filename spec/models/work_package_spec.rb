09b764fd51043e13594e74f76aa7c820cdc17d69
Hagen Schink <h.schink@finn.de>
2013-09-04 14:23:32 +0200

    Moves custom field test to spec

--- spec/models/work_package_spec.rb
+++ spec/models/work_package_spec.rb
@@ -90,82 +90,168 @@ describe WorkPackage do
                                              is_required: true) }
 
     before do
-      project.work_package_custom_fields << custom_field
-
-      work_package.save
-    end
-
-    subject { work_package.available_custom_fields }
-
-    it { should include(custom_field) }
-
-    describe "invalid custom field value error messages" do
-      context "short error message" do
-        shared_examples_for "custom field with invalid value" do
-          before do
-            unless custom_field_value.nil?
-              work_package.custom_field_values = { custom_field.id => custom_field_value }
-            end
-            work_package.save
-          end
-
-          subject { work_package.errors[:custom_values] }
-
-          it { should include(I18n.translate('activerecord.errors.messages.invalid')) }
-        end
-
-        context "no value given" do
-          let(:custom_field_value) { nil }
-
-          it_behaves_like "custom field with invalid value"
-        end
-
-        context "empty value given" do
-          let(:custom_field_value) { '' }
-
-          it_behaves_like "custom field with invalid value"
-        end
-
-        context "invalid value given" do
-          let(:custom_field_value) { 'SQLServer' }
-
-          it_behaves_like "custom field with invalid value"
-        end
-      end
-
-      context "full error message" do
-        before do
-          work_package.custom_field_values = { custom_field.id => 'SQLServer' }
-          work_package.save
-        end
-
-        subject { work_package.errors.full_messages.first }
-
-        it { should eq("Database is not included in the list") }
+      def self.change_custom_field_value(value)
+        work_package.custom_field_values = { custom_field.id => value } unless value.nil?
+        work_package.save
       end
     end
 
-    context "valid value given" do
+    context "required custom field exists" do
       before do
-        work_package.custom_field_values = { custom_field.id => 'PostgreSQL' }
+        project.work_package_custom_fields << custom_field
         work_package.save
       end
 
-      context :errors do
-        subject { work_package.errors[:custom_values] }
+      subject { work_package.available_custom_fields }
 
-        it { should be_empty }
-      end
+      it { should include(custom_field) }
 
-      context :save do
-        before do
-          work_package.save!
-          work_package.reload
+      describe "invalid custom field values" do
+        context "short error message" do
+          shared_examples_for "custom field with invalid value" do
+            let(:modified_work_package_subject) { "Should not be saved" }
+
+            before do
+              work_package.subject = modified_work_package_subject
+
+              change_custom_field_value(custom_field_value)
+            end
+
+            describe "error message" do
+              before { work_package.save }
+
+              subject { work_package.errors[:custom_values] }
+
+              it { should include(I18n.translate('activerecord.errors.messages.invalid')) }
+            end
+
+            describe "work package attribute update" do
+              subject { work_package.save }
+
+              it { should be_false }
+            end
+          end
+
+          context "no value given" do
+            let(:custom_field_value) { nil }
+
+            it_behaves_like "custom field with invalid value"
+          end
+
+          context "empty value given" do
+            let(:custom_field_value) { '' }
+
+            it_behaves_like "custom field with invalid value"
+          end
+
+          context "invalid value given" do
+            let(:custom_field_value) { 'SQLServer' }
+
+            it_behaves_like "custom field with invalid value"
+          end
         end
 
-        subject { work_package.custom_value_for(custom_field.id).value }
+        context "full error message" do
+          before { change_custom_field_value('SQLServer') }
+
+          subject { work_package.errors.full_messages.first }
+
+          it { should eq("Database is not included in the list") }
+        end
+      end
+
+      describe "valid value given" do
+        before { change_custom_field_value('PostgreSQL') }
+
+        context :errors do
+          subject { work_package.errors[:custom_values] }
+
+          it { should be_empty }
+        end
+
+        context :save do
+          before do
+            work_package.save!
+            work_package.reload
+          end
+
+          subject { work_package.custom_value_for(custom_field.id).value }
+
+          it { should eq('PostgreSQL') }
+        end
+
+        describe "value change" do
+          before do
+            change_custom_field_value('PostgreSQL')
+            @initial_custom_value = work_package.custom_value_for(custom_field).id
+            change_custom_field_value('MySQL')
+
+            work_package.reload
+          end
+
+          subject { work_package.custom_value_for(custom_field).id }
+
+          it { should eq(@initial_custom_value) }
+        end
+      end
+    end
+
+    describe "work package type change" do
+      let (:custom_field_2) { FactoryGirl.create(:work_package_custom_field) }
+      let(:type_feature) { FactoryGirl.create(:type_feature,
+                                              custom_fields: [custom_field_2]) }
+
+      context "with initial type" do
+        before do
+          custom_field
+          project.types << type_feature
+
+          change_custom_field_value('PostgreSQL')
+          work_package.reload
+
+          work_package.type = type_feature
+          work_package.save!
+        end
+
+        subject { work_package.custom_value_for(custom_field).value }
 
         it { should eq('PostgreSQL') }
+      end
+
+      context "w/o initial type" do
+        let(:project_without_custom_fields) { FactoryGirl.build_stubbed(:project) }
+        let(:work_package_without_type) { FactoryGirl.build_stubbed(:work_package,
+                                                                    project: project_without_custom_fields) }
+
+        subject { work_package_without_type.custom_field_values }
+
+        it { should be_empty }
+
+        context "assign type" do
+          before { work_package_without_type.type = type_feature }
+
+          it { should_not be_empty }
+        end
+      end
+
+      describe "assign type id first" do
+        let(:attribute_hash) { ActiveSupport::OrderedHash.new }
+
+        before do
+          attribute_hash['custom_field_values'] = { custom_field.id => 'MySQL' }
+          attribute_hash['type_id'] = type_feature
+        end
+
+        subject do
+          wp = WorkPackage.new.tap do |i|
+            i.force_attributes = { :project => project }
+          end
+          wp.attributes = attribute_hash
+
+          wp.custom_value_for(custom_field.id).value
+        end
+
+        it { should eq('MySQL') }
       end
     end
   end
