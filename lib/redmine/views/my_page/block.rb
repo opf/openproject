a664ae28a1e771eeb30c3ec1010f378b1a36c006
Sebastian Schuster <s.schuster@finn.de>
2013-09-05 12:31:22 +0200

    Fixed wrong error output

--- lib/redmine/views/my_page/block.rb
+++ lib/redmine/views/my_page/block.rb
@@ -22,7 +22,7 @@ module Redmine
               gem_name = plugin_id.to_s.gsub('openproject_','openproject-') if plugin_id.to_s.starts_with?('openproject_')
               gem_spec = Gem.loaded_specs[gem_name]
               if gem_spec.nil?
-                ActiveSupport::Deprecation.warn "No Gemspec found for plugin: " + plugin_id
+                ActiveSupport::Deprecation.warn "No Gemspec found for plugin: " + plugin_id.to_s
                   + ", expected gem name to match the plugin name but starting with openproject-"
                 nil
               else
