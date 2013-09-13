542e2f91b852b33950dfad57bf84c88c7b28b144
Jens Ulferts <j.ulferts@finn.de>
2013-09-13 15:42:09 +0200

    removes duplicate statements (probably merge relic)

--- app/models/mail_handler.rb
+++ app/models/mail_handler.rb
@@ -428,9 +428,6 @@ class MailHandler < ActionMailer::Base
     if assignee.nil?
       assignee ||= assignable.detect {|a| a.is_a?(Group) && a.name.downcase == keyword}
     end
-    if assignee.nil?
-      assignee ||= assignable.detect {|a| a.name.downcase == keyword}
-    end
 
     assignee
   end
