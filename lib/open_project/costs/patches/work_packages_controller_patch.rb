5bf36685f158f79357833ec1ad6f17becf05c24d
Christian Rijke <crijke@gmail.com>
2013-09-18 16:49:17 +0200

    Migrate variables in work_package patches and views.

--- lib/open_project/costs/patches/work_packages_controller_patch.rb
+++ lib/open_project/costs/patches/work_packages_controller_patch.rb
@@ -16,11 +16,11 @@ module OpenProject::Costs::Patches::WorkPackagesControllerPatch
   module InstanceMethods
     # Authorize the user for the requested action
     def show_with_entries
-      @cost_entries = @work_package.cost_entries.visible(User.current, @work_package.project)
+      @cost_entries = work_package.cost_entries.visible(User.current, work_package.project)
       cost_entries_with_rate = @cost_entries.select{|c| c.costs_visible_by?(User.current)}
       @material_costs = cost_entries_with_rate.blank? ? nil : cost_entries_with_rate.collect(&:real_costs).sum
 
-      @time_entries = @work_package.time_entries.visible(User.current, @work_package.project)
+      @time_entries = work_package.time_entries.visible(User.current, work_package.project)
       time_entries_with_rate = @time_entries.select{|c| c.costs_visible_by?(User.current)}
       @labor_costs = time_entries_with_rate.blank? ? nil : time_entries_with_rate.collect(&:real_costs).sum
 
