3a0af51c83af121a012e4ed522a63b48269ea8d2
Martin Czuchra <martin.czuchra@googlemail.com>
2013-09-20 15:15:55 +0200

    Adds cukes for serverside timeline filters.

--- features/support/env.rb
+++ features/support/env.rb
@@ -103,3 +103,7 @@ end
 # See https://github.com/cucumber/cucumber-rails/blob/master/features/choose_javascript_database_strategy.feature
 Cucumber::Rails::Database.javascript_strategy = :truncation
 
+# Capybara.register_driver :selenium do |app|
+#     Capybara::Selenium::Driver.new(app, :browser => :chrome)
+# end
+#
