168843148587e78514c00254fd62cea9233dd05d
Jens Ulferts <j.ulferts@finn.de>
2013-09-23 11:10:49 +0200

    removes duplicate method, present also in UsersHelper

--- app/helpers/groups_helper.rb
+++ app/helpers/groups_helper.rb
@@ -28,19 +28,10 @@
 #++
 
 module GroupsHelper
-  # Options for the new membership projects combo-box
-  def options_for_membership_project_select(user, projects)
-    options = content_tag('option', "--- #{l(:actionview_instancetag_blank_option)} ---")
-    options << project_tree_options_for_select(projects) do |p|
-      {:disabled => (user.projects.include?(p))}
-    end
-    options
-  end
-
   def group_settings_tabs
-    tabs = [{:name => 'general', :partial => 'groups/general', :label => :label_general},
-            {:name => 'users', :partial => 'groups/users', :label => :label_user_plural},
-            {:name => 'memberships', :partial => 'groups/memberships', :label => :label_project_plural}
-            ]
+    [{:name => 'general', :partial => 'groups/general', :label => :label_general},
+     {:name => 'users', :partial => 'groups/users', :label => :label_user_plural},
+     {:name => 'memberships', :partial => 'groups/memberships', :label => :label_project_plural}
+     ]
   end
 end
