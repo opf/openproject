b666d1bdb8a9949c966c597cacf08c2d4781342c
Hagen Schink <h.schink@finn.de>
2013-09-24 13:00:28 +0200

    Considers host name in settings

--- db/migrate/migration_utils/text_references.rb
+++ db/migrate/migration_utils/text_references.rb
@@ -95,24 +95,30 @@ module Migration
       text
     end
 
-    WORK_PACKAGE_LINK_REGEX = /(?<host>http:\/\/(\w|:)*)(\/timelines)?(\/projects\/(\w|-)*)?\/(issues|planning_elements)\/(?<id>\w*)/
     REL_WORK_PACKAGE_LINK_REGEX = /(?<title>"(\w|\s)*"):(\/timelines)?(\/projects\/(\w|-)*)?\/(issues|planning_elements)\/(?<id>\w*)/
 
     def update_issue_planning_element_links(text, id_map)
       unless text.nil?
-        text.gsub!(WORK_PACKAGE_LINK_REGEX) {|_| update_issue_planning_element_link_match $~, id_map}
+        text.gsub!(work_package_link_regex) {|_| update_issue_planning_element_link_match $~, id_map}
         text.gsub!(REL_WORK_PACKAGE_LINK_REGEX) {|_| update_issue_planning_element_link_match $~, id_map}
       end
 
       text
     end
 
-    RESTORE_WORK_PACKAGE_LINK_REGEX = /(?<host>http:\/\/(\w|:)*)\/work_packages\/(?<id>\w*)/
+    def work_package_link_regex
+      @work_package_link_regex ||= Regexp.new "(?<host>http(s)?:\/\/#{Regexp.escape(host_name)})(\/timelines)?(\/projects\/(\w|-)*)?\/(issues|planning_elements)\/(?<id>\w*)"
+    end
+
     RESTORE_REL_WORK_PACKAGE_LINK_REGEX = /(?<title>"(\w|\s)*"):\/work_packages\/(?<id>\w*)/
 
     def restore_issue_planning_element_links(text, id_map)
-      text.gsub!(RESTORE_WORK_PACKAGE_LINK_REGEX) {|_| restore_issue_planning_element_link_match $~, id_map}
+      text.gsub!(restore_work_package_link_regex) {|_| restore_issue_planning_element_link_match $~, id_map}
       text.gsub!(RESTORE_REL_WORK_PACKAGE_LINK_REGEX) {|_| restore_issue_planning_element_link_match $~, id_map}
+    end
+
+    def restore_work_package_link_regex
+      @restore_work_package_link_regex ||= Regexp.new "(?<host>http(s)?:\/\/#{Regexp.escape(host_name)})\/work_packages\/(?<id>\w*)"
     end
 
     def update_issue_planning_element_link_match(match, id_map)
@@ -136,6 +142,18 @@ module Migration
       id = match[:id].to_s
       id = id_map[id][:new_id] if id_map.has_key? id
       id
+    end
+
+    def host_name
+      @host_name ||= select_host_name
+    end
+
+    def select_host_name
+      settings = select_all <<-SQL
+        SELECT value FROM settings WHERE name = 'host_name'
+      SQL
+
+      settings.first['value']
     end
 
     def update_filter(columns)
