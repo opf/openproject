c4c34d55fb3c485a4a96530631165c404e23fc8b
Sebastian Schuster <s.schuster@finn.de>
2013-09-30 18:46:19 +0200

    Fixed hook paths so rendered partials are unique

--- lib/open_project/costs/hooks/activity_hook.rb
+++ lib/open_project/costs/hooks/activity_hook.rb
@@ -1,4 +1,4 @@
-class OpenProject::Costs::Hooks < Redmine::Hook::ViewListener
+class OpenProject::Costs::Hooks::ActivityHook < Redmine::Hook::ViewListener
   render_on :activity_index_head,
-            :partial => 'hooks/activity_index_head'
+            :partial => 'hooks/costs/activity_index_head'
 end
