e29afed8aaa0da7ed0fa1fe0ccba12425fb01e2c
Sebastian Schuster <s.schuster@finn.de>
2013-09-30 15:38:42 +0200

    Updated changelog and fixed updated fields no longer existing

--- db/migrate/20130930144642_remove_journal_columns.rb
+++ db/migrate/20130930144642_remove_journal_columns.rb
@@ -2,23 +2,15 @@ class RemoveJournalColumns < ActiveRecord::Migration
   def up
 
     change_table :work_package_journals do |t|
-      t.remove :lock_version, :updated_at, :planning_element_status_comment, :planning_element_status_id, :sti_type, :root_id, :lft, :rgt
+      t.remove :lock_version, :planning_element_status_comment, :planning_element_status_id, :sti_type, :root_id, :lft, :rgt
     end
 
     change_table :journals do |t|
       t.remove_references :journable_data, polymorphic: true
     end
 
-    change_table :message_journals do |t|
-      t.remove :updated_on
-    end
-
     change_table :wiki_content_journals do |t|
       t.remove :lock_version
-    end
-
-    change_table :time_entry_journals do |t|
-      t.remove :updated_on
     end
 
   end
@@ -27,7 +19,6 @@ class RemoveJournalColumns < ActiveRecord::Migration
 
     change_table :work_package_journals do |t|
       t.integer  :lock_version,                    :default => 0,  :null => false
-      t.datetime :updated_at
       t.text     :planning_element_status_comment
       t.integer  :planning_element_status_id
       t.string   :sti_type
@@ -40,16 +31,8 @@ class RemoveJournalColumns < ActiveRecord::Migration
       t.references :journable_data, polymorphic: true
     end
 
-    change_table :message_journals do |t|
-      t.datetime :updated_on,                       :null => false
-    end
-
     change_table :wiki_content_journals do |t|
       t.integer  :lock_version,                     :null => false
-    end
-
-    change_table :time_entry_journals do |t|
-      t.datetime :updated_on,      :null => false
     end
 
   end
