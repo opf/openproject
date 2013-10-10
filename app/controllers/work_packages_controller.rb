e73ec9882a19a55b2a10050ebc48aeddd68494c7
Till Breuer <breuer.till@googlemail.com>
2013-10-10 15:55:06 +0200

    Mark query menu item corresponding to current query as selected

--- app/controllers/work_packages_controller.rb
+++ app/controllers/work_packages_controller.rb
@@ -46,6 +46,7 @@ class WorkPackagesController < ApplicationController
                 :authorize, :except => [:index]
   before_filter :find_optional_project,
                 :protect_from_unauthorized_export, :only => [:index, :all]
+  before_filter :retrieve_query, :only => :index
 
   def show
     respond_to do |format|
@@ -197,15 +198,13 @@ class WorkPackagesController < ApplicationController
   end
 
   def index
-    query = retrieve_query
+    sort_init(@query.sort_criteria.empty? ? [DEFAULT_SORT_ORDER] : @query.sort_criteria)
+    sort_update(@query.sortable_columns)
 
-    sort_init(query.sort_criteria.empty? ? [DEFAULT_SORT_ORDER] : query.sort_criteria)
-    sort_update(query.sortable_columns)
-
-    results = query.results(:include => [:assigned_to, :type, :priority, :category, :fixed_version],
+    results = @query.results(:include => [:assigned_to, :type, :priority, :category, :fixed_version],
                             :order => sort_clause)
 
-    work_packages = if query.valid?
+    work_packages = if @query.valid?
                       results.work_packages.page(page_param)
                                            .per_page(per_page_param)
                                            .all
@@ -215,7 +214,7 @@ class WorkPackagesController < ApplicationController
 
     respond_to do |format|
       format.html do
-        render :index, :locals => { :query => query,
+        render :index, :locals => { :query => @query,
                                     :work_packages => work_packages,
                                     :results => results,
                                     :project => @project },
@@ -230,7 +229,7 @@ class WorkPackagesController < ApplicationController
       format.pdf do
         serialized_work_packages = WorkPackage::Exporter.pdf(work_packages,
                                                              @project,
-                                                             query,
+                                                             @query,
                                                              results,
                                                              :show_descriptions => params[:show_descriptions])
 
@@ -390,6 +389,16 @@ class WorkPackagesController < ApplicationController
     end
 
     work_package.add_time_entry(attributes)
+  end
+
+  # Override method from menu_controller mix in to select query menu items which aren't currently dsitinguished by their action names
+  def current_menu_item
+    if !@current_menu_item_determined && action_name == 'index' && @query.persisted? && current = @query.query_menu_item.try(:name)
+      @current_menu_item_determined = true
+      @current_menu_item = current.to_sym
+    else
+      super
+    end
   end
 
   protected
