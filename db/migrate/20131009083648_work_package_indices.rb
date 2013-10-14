feaee73e9c9001ce6314997e79ebdb496b564d6f
Christian Ratz <c.ratz@finn.de>
2013-10-14 13:12:18 +0200

    fixed migration
    - remove forgotten sti_type column from work_package_journals
    - readd columns in down migration

--- db/migrate/20131009083648_work_package_indices.rb
+++ db/migrate/20131009083648_work_package_indices.rb
@@ -3,10 +3,10 @@ class WorkPackageIndices < ActiveRecord::Migration
     # drop obsolete fields
     remove_column :work_packages, :planning_element_status_comment
     remove_column :work_packages, :planning_element_status_id
+    remove_column :work_packages, :sti_type
     remove_column :work_package_journals, :planning_element_status_comment
     remove_column :work_package_journals, :planning_element_status_id
-    remove_column :work_packages, :sti_type
-
+    remove_column :work_package_journals, :sti_type
 
     add_index :work_packages, :type_id
     add_index :work_packages, :status_id
@@ -24,6 +24,9 @@ class WorkPackageIndices < ActiveRecord::Migration
     add_column :work_packages, :planning_element_status_comment, :string
     add_column :work_packages, :planning_element_status_id, :integer
     add_column :work_packages, :sti_type, :string
+    add_column :work_package_journals, :planning_element_status_comment, :string
+    add_column :work_package_journals, :planning_element_status_id, :integer
+    add_column :work_package_journals, :sti_type, :string
 
     remove_index :work_packages, :type_id
     remove_index :work_packages, :status_id
