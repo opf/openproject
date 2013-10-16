f8139b499cfb559aa44344c10847f8fdc82e0f1b
Sebastian Schuster <s.schuster@finn.de>
2013-10-16 11:25:23 +0200

    Merged new migrations into one

--- db/migrate/20130919092624_add_backlog_columns_to_work_package_journal.rb
+++ db/migrate/20130919092624_add_backlog_columns_to_work_package_journal.rb
@@ -2,5 +2,11 @@ class AddBacklogColumnsToWorkPackageJournal < ActiveRecord::Migration
   def change
     add_column :work_package_journals, :story_points, :integer
     add_column :work_package_journals, :remaining_hours, :float
+
+    add_index :work_package_journals, [:fixed_version_id,
+                                       :status_id,
+                                       :project_id,
+                                       :type_id],
+                                       :name => 'work_package_journal_on_burndown_attributes'
   end
 end
--- db/migrate/20131001132542_rename_issue_status_to_status.rb
+++ db/migrate/20131001132542_rename_issue_status_to_status.rb
@@ -1,19 +1,6 @@
 class RenameIssueStatusToStatus < ActiveRecord::Migration
-  def initialize
-    super
-    @issue_done_statuses_for_project_exists = \
-      ActiveRecord::Base.connection.tables.include? 'issue_done_statuses_for_project'
-  end
-
   def change
-    if @issue_done_statuses_for_project_exists
-      rename_table :issue_done_statuses_for_project, :done_statuses_for_project
-      rename_column :done_statuses_for_project, :issue_status_id, :status_id
-    else
-      create_table :done_statuses_for_project, :id => false do |t|
-        t.references :project
-        t.references :status
-      end
-    end
+    rename_table :issue_done_statuses_for_project, :done_statuses_for_project
+    rename_column :done_statuses_for_project, :issue_status_id, :status_id
   end
 end
