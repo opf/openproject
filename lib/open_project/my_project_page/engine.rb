7285f1b061617e2ebf100d271e88a6350ad227b5
Christian Rijke <crijke@gmail.com>
2013-10-18 16:49:25 +0200

    Append migrations to load path.

--- lib/open_project/my_project_page/engine.rb
+++ lib/open_project/my_project_page/engine.rb
@@ -75,6 +75,14 @@ module OpenProject::MyProjectPage
       FactoryGirl.definition_file_paths << File.expand_path(self.root.to_s + '/spec/factories') if defined?(FactoryGirl)
     end
 
+    initializer :append_migrations do |app|
+      unless app.root.to_s.match root.to_s
+        config.paths["db/migrate"].expanded.each do |expanded_path|
+          app.config.paths["db/migrate"] << expanded_path
+        end
+      end
+    end
+
     config.to_prepare do
 
 
