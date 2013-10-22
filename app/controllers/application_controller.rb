b346e6a24216d2e87987d53501c49c326acafc40
Jens Ulferts <j.ulferts@finn.de>
2013-10-22 11:54:56 +0200

    moves find_issue into v1/issues_controller - only used there

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -380,16 +380,6 @@ class ApplicationController < ActionController::Base
     render_404
   end
 
-  # TODO: remove this once all subclasses use find_work_packages
-  def find_issues
-    @issues = WorkPackage.find_all_by_id(params[:id] || params[:ids])
-    raise ActiveRecord::RecordNotFound if @issues.empty?
-    @projects = @issues.collect(&:project).compact.uniq
-    @project = @projects.first if @projects.size == 1
-  rescue ActiveRecord::RecordNotFound
-    render_404
-  end
-
   # Check if project is unique before bulk operations
   def check_project_uniqueness
     unless @project
