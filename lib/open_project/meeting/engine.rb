be8cd04d34e47b3adfb07761f38fbcc67b9e8c0a
Sebastian Schuster <s.schuster@finn.de>
2013-10-23 18:18:52 +0200

    Used more specific initializer step

--- lib/open_project/meeting/engine.rb
+++ lib/open_project/meeting/engine.rb
@@ -68,7 +68,7 @@ module OpenProject::Meeting
       app.config.plugins_to_test_paths << self.root
     end
 
-    initializer :append_migrations do |app|
+    initializer 'meeting.append_migrations' do |app|
       unless app.root.to_s.match root.to_s
         config.paths["db/migrate"].expanded.each do |expanded_path|
           app.config.paths["db/migrate"] << expanded_path
