26547a6477e30120bc26b01f3286f64a87810c9e
Sebastian Schuster <s.schuster@finn.de>
2013-10-23 18:20:39 +0200

    Used more specific initializer step

--- lib/open_project/my_project_page/engine.rb
+++ lib/open_project/my_project_page/engine.rb
@@ -75,7 +75,7 @@ module OpenProject::MyProjectPage
       FactoryGirl.definition_file_paths << File.expand_path(self.root.to_s + '/spec/factories') if defined?(FactoryGirl)
     end
 
-    initializer :append_migrations do |app|
+    initializer 'my_project_page.append_migrations' do |app|
       unless app.root.to_s.match root.to_s
         config.paths["db/migrate"].expanded.each do |expanded_path|
           app.config.paths["db/migrate"] << expanded_path
