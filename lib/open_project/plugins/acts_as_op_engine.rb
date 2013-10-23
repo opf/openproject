703c4b5b83d01e3998ab1bb0598a12df120ecd57
Sebastian Schuster <s.schuster@finn.de>
2013-10-23 18:25:30 +0200

    Also used more specific initializer for all other plugins

--- lib/open_project/plugins/acts_as_op_engine.rb
+++ lib/open_project/plugins/acts_as_op_engine.rb
@@ -126,7 +126,7 @@ module OpenProject::Plugins
           FactoryGirl.definition_file_paths << File.expand_path(self.root.to_s + '/spec/factories') if defined?(FactoryGirl)
         end
 
-        initializer :append_migrations do |app|
+        initializer "#{engine_name}.append_migrations" do |app|
           unless app.root.to_s.match root.to_s
             config.paths["db/migrate"].expanded.each do |expanded_path|
               app.config.paths["db/migrate"] << expanded_path
