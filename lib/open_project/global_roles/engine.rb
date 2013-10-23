da7b2af159b2c21f1836dab82b35b0c5efb8e68e
Sebastian Schuster <s.schuster@finn.de>
2013-10-23 18:13:29 +0200

    Used more specific initializer step

--- lib/open_project/global_roles/engine.rb
+++ lib/open_project/global_roles/engine.rb
@@ -60,7 +60,7 @@ module OpenProject::GlobalRoles
       app.routes_reloader.paths.uniq!
     end
 
-    initializer :append_migrations do |app|
+    initializer 'global_roles.append_migrations' do |app|
       unless app.root.to_s.match root.to_s
         config.paths["db/migrate"].expanded.each do |expanded_path|
           app.config.paths["db/migrate"] << expanded_path
