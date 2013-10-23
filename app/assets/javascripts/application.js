e0fd255f22176c7be737aad12930e79346136acc
Martin Czuchra <martin.czuchra@googlemail.com>
2013-10-23 18:53:15 +0200

    Introduces top-shelf jQuery plugin.

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -61,6 +61,7 @@
 //= require settings
 //= require modal
 //= require keyboard_shortcuts
+//= require top-shelf
 
 //source: http://stackoverflow.com/questions/8120065/jquery-and-prototype-dont-work-together-with-array-prototype-reverse
 if (typeof []._reverse == 'undefined') {
