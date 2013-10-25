12aa51de6231502ad1ae67cd9a44f1f9a96469db
Philipp Tessenow <philipp@tessenow.org>
2013-10-25 16:13:33 +0200

    implement j,k shortcuts to navigate in lists

--- app/assets/javascripts/keyboard_shortcuts.js
+++ app/assets/javascripts/keyboard_shortcuts.js
@@ -118,22 +118,64 @@
     $('#search_wrap .search_field').focus();
   };
 
+  var find_list_in_page = function(){
+    var dom_list, focus_elements;
+    dom_list = $($(document.activeElement).parents('table.list')[0] ||
+                 $('table.list'));
+    if (dom_list.size() === 0) { return null; }
+    focus_elements = [];
+    dom_list.find('tbody tr').each(function(index, tr){
+      focus_elements.push($(tr).find('a')[0]);
+    });
+    return focus_elements;
+  };
 
-  Mousetrap.bind('?',     function(){ show_help_modal();  return false; });
+  var focus_item_offset = function(offset){
+    var list, index;
+    list = find_list_in_page();
+    if (list === null) { return; }
+    index = list.indexOf($(document.activeElement).parents('table.list tr').find('a')[0]);
+    $(list[(index+offset+list.length) % list.length]).focus();
+  };
 
-  Mousetrap.bind('g o',   function(){ go_overview();      return false; });
-  Mousetrap.bind('g m',   function(){ go_my_page();       return false; });
-  Mousetrap.bind('g w p', function(){ go_work_packages(); return false; });
-  Mousetrap.bind('g w i', function(){ go_wiki();          return false; });
-  Mousetrap.bind('g a',   function(){ go_activity();      return false; });
-  Mousetrap.bind('g c',   function(){ go_calendar();      return false; });
-  Mousetrap.bind('g n',   function(){ go_news();          return false; });
-  Mousetrap.bind('g t',   function(){ go_timelines();     return false; });
-  Mousetrap.bind('g e',   function(){ go_edit();          return false; });
-  Mousetrap.bind('g p',   function(){ go_preview();       return false; });
+  var focus_next_item = function(){
+    focus_item_offset(1);
+  };
 
-  Mousetrap.bind('n w p', function(){ new_work_package(); return false; });
+  var focus_previous_item = function(){
+    focus_item_offset(-1);
+  };
 
-  Mousetrap.bind('s p',   function(){ search_project();   return false; });
-  Mousetrap.bind('s g',   function(){ search_global();    return false; });
+
+  Mousetrap.bind('?',     function(){ show_help_modal();     return false; });
+
+  Mousetrap.bind('g o',   function(){ go_overview();         return false; });
+  Mousetrap.bind('g m',   function(){ go_my_page();          return false; });
+  Mousetrap.bind('g w p', function(){ go_work_packages();    return false; });
+  Mousetrap.bind('g w i', function(){ go_wiki();             return false; });
+  Mousetrap.bind('g a',   function(){ go_activity();         return false; });
+  Mousetrap.bind('g c',   function(){ go_calendar();         return false; });
+  Mousetrap.bind('g n',   function(){ go_news();             return false; });
+  Mousetrap.bind('g t',   function(){ go_timelines();        return false; });
+  Mousetrap.bind('g e',   function(){ go_edit();             return false; });
+  Mousetrap.bind('g p',   function(){ go_preview();          return false; });
+
+  Mousetrap.bind('n w p', function(){ new_work_package();    return false; });
+  Mousetrap.bind('j',     function(){ focus_previous_item(); return false; });
+  Mousetrap.bind('k',     function(){ focus_next_item();     return false; });
+
+  Mousetrap.bind('s p',   function(){ search_project();      return false; });
+  Mousetrap.bind('s g',   function(){ search_global();       return false; });
 })(jQuery);
+
+jQuery(function(){
+  // simulated hover effect on table lists when using the keyboard
+  var tables = jQuery('table.list');
+  if (tables.size() === 0) { return; };
+  tables.on('blur', 'tr *', function(){
+    jQuery(this).parents('table.list tr').removeClass('keyboard_hover');
+  });
+  tables.on('focus', 'tr *', function(){
+    jQuery(this).parents('table.list tr').addClass('keyboard_hover');
+  });
+});
