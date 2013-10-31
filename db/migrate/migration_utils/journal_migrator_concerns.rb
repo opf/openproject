fe1d516a59db2860e5fe9e3099f634ddd1faeac7
Philipp Tessenow <philipp@tessenow.org>
2013-10-31 10:29:08 +0100

    soo much whitespace..

--- db/migrate/migration_utils/journal_migrator_concerns.rb
+++ db/migrate/migration_utils/journal_migrator_concerns.rb
@@ -32,11 +32,9 @@ module Migration
     module Attachable
       def migrate_attachments(to_insert, legacy_journal, journal_id)
         attachments = to_insert.keys.select { |d| d =~ attachment_key_regexp }
-
         attachments = remove_attachments_deleted_in_current_version attachments, to_insert
 
         attachments.each do |key|
-
           attachment_id = attachment_key_regexp.match(key)[1]
 
           # if an attachment was added the value contains something like:
@@ -44,40 +42,31 @@ module Migration
           # if it was removed the value is something like
           # ["blubs.png", nil]
           removed_filename, added_filename = *to_insert[key]
-
           if added_filename && !removed_filename
             # The attachment was added
-
             attachable = ActiveRecord::Base.connection.select_all <<-SQL
               SELECT *
               FROM #{attachable_table_name} AS a
               WHERE a.journal_id = #{quote_value(journal_id)} AND a.attachment_id = #{attachment_id};
             SQL
-
             if attachable.size > 1
-
               raise AmbiguousAttachableJournalError, <<-MESSAGE.split("\n").map(&:strip!).join(" ") + "\n"
                 It appears there are ambiguous attachable journal data.
                 Please make sure attachable journal data are consistent and
                 that the unique constraint on journal_id and attachment_id
                 is met.
               MESSAGE
-
             elsif attachable.size == 0
-
               db_execute <<-SQL
                 INSERT INTO #{attachable_table_name}(journal_id, attachment_id, filename)
                 VALUES (#{quote_value(journal_id)}, #{quote_value(attachment_id)}, #{quote_value(added_filename)});
               SQL
             end
-
           elsif removed_filename && !added_filename
             # The attachment was removed
             # we need to make certain that no subsequent journal adds an attachable_journal
             # for this attachment
-
             to_insert.delete_if { |k, v| k =~ /attachments_?#{attachment_id}/ }
-
           else
             raise InvalidAttachableJournalError, <<-MESSAGE.split("\n").map(&:strip!).join(" ") + "\n"
               There is a journal entry for an attachment but neither the old nor the new value contains anything:
@@ -85,24 +74,19 @@ module Migration
               #{legacy_journal}
             MESSAGE
           end
-
         end
-
       end
 
       def remove_attachments_deleted_in_current_version(attachments, to_insert)
         attachments_to_add_remove = []
-
         attachments.each do |key|
           attachment_id = attachment_key_regexp.match(key)[1]
           attachments_to_add_remove << key
           removed_filename, _ = *to_insert[key]
-
           if removed_filename
             attachments_to_add_remove.delete_if { |k, v| k =~ /attachments#{attachment_id}/ }
           end
         end
-
         attachments_to_add_remove
       end
 
@@ -114,7 +98,6 @@ module Migration
         # Attachment journal entries can be written in two ways:
         # attachments123 if the attachment was added
         # attachments_123 if the attachment was removed
-        #
         @attachment_key_regexp ||= /attachments_?(\d+)\z/
       end
     end
@@ -123,13 +106,10 @@ module Migration
       def migrate_custom_values(to_insert, legacy_journal, journal_id)
         keys = to_insert.keys
         values = to_insert.values
-
         custom_values = keys.select { |d| d =~ /custom_values.*/ }
         custom_values.each do |k|
-
           custom_field_id = k.split("_values").last.to_i
           value = values[keys.index k].last
-
           customizable = db_select_all <<-SQL
             SELECT *
             FROM #{customizable_table_name} AS a
@@ -137,22 +117,18 @@ module Migration
           SQL
 
           if customizable.size > 1
-
             raise AmbiguousCustomizableJournalError, <<-MESSAGE.split("\n").map(&:strip!).join(" ") + "\n"
               It appears there are ambiguous customizable journal
               data. Please make sure customizable journal data are
               consistent and that the unique constraint on journal_id and
               custom_field_id is met.
             MESSAGE
-
           elsif customizable.size == 0
-
             db_execute <<-SQL
               INSERT INTO #{customizable_table_name}(journal_id, custom_field_id, value)
               VALUES (#{quote_value(journal_id)}, #{quote_value(custom_field_id)}, #{quote_value(value)});
             SQL
           end
-
         end
       end
 
