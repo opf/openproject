9fffdc205c22e69c3b03e5a72201533a4f5add02
Christian Ratz <c.ratz@finn.de>
2013-11-06 09:10:07 +0100

    First version of new project menu fold behavior

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -906,12 +906,10 @@ $(window).bind('resizeEnd', function() {
   }
 
   var toggle_navigation = function() {
-    var height = $(document).height() - $('#main-menu').offset().top - 32;
     $('#wrapper').toggleClass('hidden-navigation');
-    $('#main-menu, #menu-sidebar').toggleClass('hidden');
     $('#content, #breadcrumb').toggleClass('hidden-navigation');
     $('#toggle-project-menu').removeAttr("style").toggleClass('show');
-    remember_menu_state($('#toggle-project-menu.show').css({height:height}));
+    remember_menu_state($('#toggle-project-menu.show'));
   };
 
   // register toggler, and toggle for the first time if remembered to be closed.
