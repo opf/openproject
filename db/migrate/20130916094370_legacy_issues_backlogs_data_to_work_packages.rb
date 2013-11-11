2b20571725f53d278293b9732095a9e23fd76322
Hagen Schink <h.schink@finn.de>
2013-11-11 14:21:43 +0100

    Fixes legacy backlog data migration

--- db/migrate/20130916094370_legacy_issues_backlogs_data_to_work_packages.rb
+++ db/migrate/20130916094370_legacy_issues_backlogs_data_to_work_packages.rb
@@ -14,20 +14,33 @@ require Rails.root.join("db","migrate","migration_utils","utils").to_s
 class LegacyIssuesBacklogsDataToWorkPackages < ActiveRecord::Migration
 
   def up
-    execute <<-SQL
-      UPDATE work_packages AS W
-      SET position = (SELECT L.position FROM legacy_issues AS L WHERE L.id = W.id),
-          story_points = (SELECT L.story_points FROM legacy_issues AS L WHERE L.id = W.id),
-          remaining_hours = (SELECT L.remaining_hours FROM legacy_issues AS L WHERE L.id = W.id)
-    SQL
+    if legacy_backlog_data_exists?
+      execute <<-SQL
+        UPDATE work_packages AS W
+        SET position = (SELECT L.position FROM legacy_issues AS L WHERE L.id = W.id),
+            story_points = (SELECT L.story_points FROM legacy_issues AS L WHERE L.id = W.id),
+            remaining_hours = (SELECT L.remaining_hours FROM legacy_issues AS L WHERE L.id = W.id)
+      SQL
+    end
   end
 
   def down
-    execute <<-SQL
-      UPDATE legacy_issues AS L
-      SET position = (SELECT W.position FROM work_packages AS W WHERE W.id = L.id),
-          story_points = (SELECT W.story_points FROM work_packages AS W WHERE W.id = L.id),
-          remaining_hours = (SELECT W.remaining_hours FROM work_packages AS W WHERE W.id = L.id)
-    SQL
+    if legacy_backlog_data_exists?
+      execute <<-SQL
+        UPDATE legacy_issues AS L
+        SET position = (SELECT W.position FROM work_packages AS W WHERE W.id = L.id),
+            story_points = (SELECT W.story_points FROM work_packages AS W WHERE W.id = L.id),
+            remaining_hours = (SELECT W.remaining_hours FROM work_packages AS W WHERE W.id = L.id)
+      SQL
+    end
+  end
+
+  private
+
+  class LegacyIssue < ActiveRecord::Base
+  end
+
+  def legacy_backlog_data_exists?
+    LegacyIssue.column_names & ['position', 'story_points', 'remaining_hours'] == 3
   end
 end
