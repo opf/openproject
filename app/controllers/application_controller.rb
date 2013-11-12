7282104f00fd818c2f0ab0117ee2e39480a6b69b
Philipp Tessenow <philipp@tessenow.org>
2013-11-12 09:18:23 +0100

    remove forgotten debug statement
    [ci skip]

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -110,7 +110,6 @@ class ApplicationController < ActionController::Base
   # see: http://stackoverflow.com/questions/711418/how-to-prevent-browser-page-caching-in-rails
   def set_cache_buster
     if OpenProject::Configuration['disable_browser_cache']
-      binding.pry
       response.headers["Cache-Control"] = "no-cache, no-store, max-age=0, must-revalidate"
       response.headers["Pragma"] = "no-cache"
       response.headers["Expires"] = "Fri, 01 Jan 1990 00:00:00 GMT"
