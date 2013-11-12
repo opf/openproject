c6b80df6a75eed8e8dc7d2cb43e3958a6eab8526
Till Breuer <till.breuer@gmx.de>
2013-11-12 11:51:19 +0100

    Use active support concern for work packages controller spec

--- lib/open_project/costs/patches/work_packages_controller_patch.rb
+++ lib/open_project/costs/patches/work_packages_controller_patch.rb
@@ -1,36 +1,31 @@
 require_dependency 'work_packages_controller'
 
 module OpenProject::Costs::Patches::WorkPackagesControllerPatch
-  def self.included(base) # :nodoc:
-    base.send(:include, InstanceMethods)
+  extend ActiveSupport::Concern
 
-    base.class_eval do
-      alias_method_chain :show, :entries
-    end
-
+  included do
+    alias_method_chain :show, :entries
   end
 
-  module InstanceMethods
-    # Authorize the user for the requested action
-    def show_with_entries
-      @cost_entries = work_package.cost_entries.visible(User.current, work_package.project)
-      cost_entries_with_rate = @cost_entries.select{|c| c.costs_visible_by?(User.current)}
-      @material_costs = cost_entries_with_rate.blank? ? nil : cost_entries_with_rate.collect(&:real_costs).sum
+  # Authorize the user for the requested action
+  def show_with_entries
+    @cost_entries = work_package.cost_entries.visible(User.current, work_package.project)
+    cost_entries_with_rate = @cost_entries.select{|c| c.costs_visible_by?(User.current)}
+    @material_costs = cost_entries_with_rate.blank? ? nil : cost_entries_with_rate.collect(&:real_costs).sum
 
-      @time_entries = work_package.time_entries.visible(User.current, work_package.project)
-      time_entries_with_rate = @time_entries.select{|c| c.costs_visible_by?(User.current)}
-      @labor_costs = time_entries_with_rate.blank? ? nil : time_entries_with_rate.collect(&:real_costs).sum
+    @time_entries = work_package.time_entries.visible(User.current, work_package.project)
+    time_entries_with_rate = @time_entries.select{|c| c.costs_visible_by?(User.current)}
+    @labor_costs = time_entries_with_rate.blank? ? nil : time_entries_with_rate.collect(&:real_costs).sum
 
-      unless @material_costs.nil? && @labor_costs.nil?
-        @overall_costs = 0
-        @overall_costs += @material_costs unless @material_costs.nil?
-        @overall_costs += @labor_costs unless @labor_costs.nil?
-      else
-        @overall_costs = nil
-      end
-
-      show_without_entries
+    unless @material_costs.nil? && @labor_costs.nil?
+      @overall_costs = 0
+      @overall_costs += @material_costs unless @material_costs.nil?
+      @overall_costs += @labor_costs unless @labor_costs.nil?
+    else
+      @overall_costs = nil
     end
+
+    show_without_entries
   end
 end
 
