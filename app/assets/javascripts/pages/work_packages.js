cc2874f70fc5200f4fe632ee1d92b183fb45612f
Till Breuer <till.breuer@gmx.de>
2013-11-14 14:38:06 +0100

    Remove inline javascript from work_packages#index

--- app/assets/javascripts/pages/work_packages.js
+++ app/assets/javascripts/pages/work_packages.js
@@ -4,3 +4,14 @@ var resetSelectedMenuItem = function() {
   jQuery('#menu-sidebar .selected').removeClass('selected');
   jQuery('#menu-sidebar .work-packages').addClass('selected');
 }
+
+var initQuerySelectBehaviour = function() {
+  jQuery('#query-select')
+  .select2()
+  .change(function() {
+    document.location.href = window.location.pathname + '?query_id=' + this.value;
+    // TODO use FilterQueryStringBuilder
+  });
+}
+
+jQuery(document).ready(initQuerySelectBehaviour);
