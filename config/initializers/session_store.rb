d7f763ac75f69b45217c527d2e2475f900650288
Philipp Tessenow <philipp@tessenow.org>
2013-11-15 11:07:33 +0100

    use memcache session store in production environment

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -28,9 +28,8 @@
 
 # Be sure to restart your server when you modify this file.
 
-OpenProject::Application.config.session_store :cookie_store, :key => '_open_project_session'
-
-# Use the database for sessions instead of the cookie-based default,
-# which shouldn't be used to store highly confidential information
-# (create the session table with "rails generate session_migration")
-# OpenProject::Application.config.session_store :active_record_store
+if Rails.env == 'production'
+  OpenProject::Application.config.session_store ActionDispatch::Session::CacheStore
+else
+  OpenProject::Application.config.session_store :cookie_store, :key => '_open_project_session'
+end
