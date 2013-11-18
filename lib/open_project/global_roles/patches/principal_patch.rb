3c014b187fa1e17e2c6f62db18089b4e38e0cb0d
Sebastian Schuster <s.schuster@finn.de>
2013-11-18 19:43:48 +0100

    Minor change to the copyright header

--- lib/open_project/global_roles/patches/principal_patch.rb
+++ lib/open_project/global_roles/patches/principal_patch.rb
@@ -1,7 +1,7 @@
 #-- copyright
 # OpenProject is a project management system.
 #
-# Copyright (C) 2010-2013 the OpenProject Team
+# Copyright (C) 2010-2013 the OpenProject Foundation (OPF)
 #
 # This program is free software; you can redistribute it and/or
 # modify it under the terms of the GNU General Public License version 3.
