f66534bb8552b270fb9141cf4dca75d41a4ce0e2
Till Breuer <till.breuer@gmx.de>
2013-11-26 13:39:24 +0100

    Add factory girl syntax methods to spec helper

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -57,6 +57,8 @@ RSpec.configure do |config|
   # config.mock_with :rr
   config.mock_with :rspec
 
+  config.include FactoryGirl::Syntax::Methods
+
   # Remove this line if you're not using ActiveRecord or ActiveRecord fixtures
   config.fixture_path = "#{::Rails.root}/spec/fixtures"
 
