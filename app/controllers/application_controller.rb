11205b36095ea18502f0aaaad4232e96d4df58a2
Jens Ulferts <j.ulferts@finn.de>
2013-11-27 20:36:27 +0100

    removes now unneccessary eager loading
    As the permission check is now handled differently, eager loading
    memberships is no longer necessary.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -166,7 +166,7 @@ class ApplicationController < ActionController::Base
   def find_current_user
     if session[:user_id]
       # existing session
-      (User.active.find(session[:user_id], :include => [:memberships]) rescue nil)
+      (User.active.find(session[:user_id]) rescue nil)
     elsif cookies[OpenProject::Configuration['autologin_cookie_name']] && Setting.autologin?
       # auto-login feature starts a new session
       user = User.try_to_autologin(cookies[OpenProject::Configuration['autologin_cookie_name']])
