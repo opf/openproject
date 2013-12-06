d5ce07822c4baa019c7dfe1fb195e13ffd58ece1
Christian Ratz <c.ratz@finn.de>
2013-12-06 11:28:38 +0100

    updated core dependency

--- lib/open_project/my_project_page/engine.rb
+++ lib/open_project/my_project_page/engine.rb
@@ -38,7 +38,7 @@ module OpenProject::MyProjectPage
         version spec.version
         url 'https://www.openproject.org/projects/my-project-page'
 
-        requires_openproject ">= 3.0.0pre30"
+        requires_openproject ">= 3.0.0pre36"
 
         project_module :my_project_page do
 
