cbd0729098616b9ba7973229edcf8a514dc39966
Hagen Schink <h.schink@finn.de>
2013-12-09 15:42:50 +0100

    Prevents calls to not defined methods

--- app/models/activity/base_activity_provider.rb
+++ app/models/activity/base_activity_provider.rb
@@ -57,12 +57,12 @@ class Activity::BaseActivityProvider
   end
 
   def format_event(event, event_data)
-    event.event_title = event_title event_data
-    event.event_type = event_type if respond_to? :event_type
-    event.event_description = event_description event_data if respond_to? :event_description
-    event.event_datetime = event_datetime event_data if respond_to? :event_datetime
-    event.event_path = event_path event_data
-    event.event_url = event_url event_data
+    event.event_title = event_title event_data if self.class.method_defined? :event_title
+    event.event_type = event_type event_data if self.class.method_defined? :event_type
+    event.event_description = event_description event_data if self.class.method_defined? :event_description
+    event.event_datetime = event_datetime event_data if self.class.method_defined? :event_datetime
+    event.event_path = event_path event_data if self.class.method_defined? :event_path
+    event.event_url = event_url event_data if self.class.method_defined? :event_url
 
     event
   end
