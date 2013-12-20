1d3f4efa4f57f2f37f708ccd48fe5ec4fa44b333
Johannes Wollert <johannes.wollert@gmail.com>
2013-12-20 18:22:21 +0100

    jquery rewrite of all the javascript files

--- app/assets/javascripts/global_roles/principal_roles.js
+++ app/assets/javascripts/global_roles/principal_roles.js
@@ -8,44 +8,40 @@
 //
 // See doc/COPYRIGHT.rdoc for more details.
 //++
+(function ($, undefined) {
+	var principal_roles = {
+		init: function(){
+			principal_roles.set_table_visibility();
+			principal_roles.set_available_roles_visibility();
+		},
 
-var principal_roles = {
-	init: function(){
-		principal_roles.set_table_visibility();
-		principal_roles.set_available_roles_visibility();
-	},
+		set_table_visibility: function(){
+			if ($('#table_principal_roles_body tr').length > 0){
+				$('#table_principal_roles').show();
+				$('#no_data').hide();
+			}
+			else
+			{
+				$('#table_principal_roles').hide();
+				$('#no_data').show();
+			}
+		},
 
-	set_table_visibility: function(){
-		if ($$('#table_principal_roles_body tr').length > 0){
-			$('table_principal_roles').show();
-			$('no_data').hide();
+		set_available_roles_visibility: function(){
+			if ($('.principal_role_option').length > 0){
+				$('#additional_principal_roles').show();
+				$('#no_additional_principal_roles').hide();
+			}
+			else
+			{
+				$('#additional_principal_roles').hide();
+				$('#no_additional_principal_roles').show();
+			}
 		}
-		else
-		{
-			$('table_principal_roles').hide();
-			$('no_data').show();
-		}
-	},
-
-	set_available_roles_visibility: function(){
-		if ($$('.principal_role_option').length > 0){
-			$('additional_principal_roles').show();
-			$('no_additional_principal_roles').hide();
-		}
-		else
-		{
-			$('additional_principal_roles').hide();
-			$('no_additional_principal_roles').show();
-		}
-	},
-
-	role_already_assigned: function(){
-		return ($$('.assigned_global_role_' + element.down('input').value).length > 0)
 	}
-};
 
-document.observe('dom:loaded', principal_roles.init);
-
-Ajax.Responders.register({
-  onComplete: principal_roles.init
-});
+	$(document).ready(function () {
+		$(document).ajaxStop(principal_roles.init);
+		principal_roles.init();
+	});
+}(jQuery));
