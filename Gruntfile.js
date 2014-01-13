72be0d3347ad7e428424e0a19a5fa1abd7adada0
Nils Kenneweg <n.kenneweg@finn.de>
2014-01-13 18:19:25 +0100

    more advanced gruntfile.

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,25 +1,49 @@
 module.exports = function(grunt) {
 
   var jsPath = "app/assets/javascripts/";
-  var jsPaths = ['timelines/**/*.js', 'timelines.js', 'timelines_modal.js', 'timelines_select_boxes.js', 'members_form.js', 'members_select_boxes.js'].map(function (e) { return jsPath + e; });
-
+  var testSource = "mocha/index.html";
   // Project configuration.
   grunt.initConfig({
     pkg: grunt.file.readJSON('package.json'),
     jshint: {
       all: {
         files: {
-          src: ["app/assets/javascripts/"] //jsPaths
+          src: [jsPath]
         }
       }
     },
     mocha_phantomjs: {
-      all: ['mocha/index.html']
+      all: [testSource],
+      small: {
+        src:[testSource],
+        options: {
+          reporter: "dot"
+        }
+      },
+      cov: {
+        src:[testSource],
+        options: {
+          output: "coverage.json",
+          reporter: "json-cov"
+        }
+      },
+      jenkins: {
+        src:[testSource],
+        options: {
+          reporter: "XUnit"
+        }
+      }
+    },
+    jscoverage: {
+      options: {
+        inputDirectory: 'app/assets/javascripts/',
+        outputDirectory: 'app/assets/javascripts_cov/'
+      }
     },
     watch: {
       scripts: {
-        files: jsPaths,
-        tasks: ['jshint', 'mocha_phantomjs'],
+        files: [jsPath] ,
+        tasks: ['jshint', 'mocha_phantomjs:small'],
         options: {
           spawn: false,
         },
@@ -27,11 +51,47 @@ module.exports = function(grunt) {
     },
   });
 
+  grunt.loadNpmTasks("grunt-jscoverage");
   grunt.loadNpmTasks('grunt-contrib-jshint');
   grunt.loadNpmTasks('grunt-contrib-watch');
   grunt.loadNpmTasks('grunt-mocha-phantomjs');
 
   // Default task(s).
   grunt.registerTask('default', ['jshint']);
+
+  var tempPath = "app/assets/javascripts_temp/";
+  var covPath = "app/assets/javascripts_cov/";
+
+  grunt.registerTask('moveFiles', 'switch directories for coverage-enabled files and normal files', function () {
+    var fs = require("fs");
+    fs.renameSync(jsPath, tempPath);
+    fs.renameSync(covPath, jsPath);
+  });
+
+  grunt.registerTask('cleanUpCoverage', 'undo moveFiles', function () {
+    var done = this.async();
+
+    var fs = require("fs");
+    if (fs.existsSync(tempPath)) {
+      fs.renameSync(jsPath, covPath);
+      fs.renameSync(tempPath, jsPath);
+    }
+
+    if (fs.existsSync("coverage.json")) {
+      fs.unlinkSync("coverage.json");
+    }
+
+    rmdir = require("rimraf");
+    rmdir(covPath, function (e) {
+        done(e);
+    });
+  });
+
+  grunt.registerTask('jsonCov2Html', 'convert coverage to html', function () {
+    var exec = require("exec");
+    exec("cat coverage.json | node_modules/json2htmlcov/bin/json2htmlcov > coverage.html");
+  });
+
+  grunt.registerTask('coverage', ['jscoverage', 'moveFiles', 'mocha_phantomjs:cov', 'jsonCov2Html', 'cleanUpCoverage']);
 
 };
