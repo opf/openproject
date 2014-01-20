f6271cb12a67e88e538688cd5a210b7b425cc338
Hagen Schink <h.schink@finn.de>
2014-01-20 10:05:03 +0100

    Fixes cukes

--- features/step_definitions/work_package_steps.rb
+++ features/step_definitions/work_package_steps.rb
@@ -113,8 +113,8 @@ Then /^the work package should be shown with the following values:$/ do |table|
   end
 
   table_attributes.each do |key, value|
-    label = first('th', :text => key)
-    should have_css("td.#{label[:class]}", :text => value)
+    label = find('td.work_package_attribute_header', :text => key)
+    should have_css("td.#{label[:class].split(' ').last}", :text => value)
   end
 
   if table.rows_hash["Type"] || table.rows_hash["Subject"]
