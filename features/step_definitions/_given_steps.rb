3dc0ed3b355aa4dc9d7b70fd8dfb5d6133b7275f
Richard <rm.sylvester@gmail.com>
2014-01-20 15:56:09 +0100

    Removed remaining reference to old taskboard card module.

--- features/step_definitions/_given_steps.rb
+++ features/step_definitions/_given_steps.rb
@@ -266,7 +266,7 @@ Given /^I have selected card label stock (.+)$/ do |stock|
   # to get the ones, shipped with the plugin or
   #   rake openproject:backlogs:current_labels
   # to get current one, downloaded from the internet.
-  OpenProject::Backlogs::TaskboardCard::PageLayout.should be_available
+  # OpenProject::Backlogs::TaskboardCard::PageLayout.should be_available
 end
 
 Given /^I have set my API access key$/ do
