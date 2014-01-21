7c9c0f6442201b7ec2481a717d705a69c09be146
Martin Linkhorst <m.linkhorst@gmail.com>
2014-01-21 15:44:49 +0100

    update newest multitenancy

--- db/migrate/20140121144322_create_subscribem_accounts.subscribem.rb
+++ db/migrate/20140121144322_create_subscribem_accounts.subscribem.rb
@@ -2,10 +2,7 @@
 class CreateSubscribemAccounts < ActiveRecord::Migration
   def change
     create_table :subscribem_accounts do |t|
-      t.string :name
       t.string :subdomain, index: true
-      t.references :owner, index: true
-
       t.timestamps
     end
   end
