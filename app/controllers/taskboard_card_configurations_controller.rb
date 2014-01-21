0eba424adf342a9d1f5ca6f75b4c720b30a10ca2
Richard <rm.sylvester@gmail.com>
2014-01-21 11:45:09 +0100

    did some copy pasting to get configs crud to look like other admin sections.

--- app/controllers/taskboard_card_configurations_controller.rb
+++ app/controllers/taskboard_card_configurations_controller.rb
@@ -2,7 +2,7 @@
 class TaskboardCardConfigurationsController < ApplicationController
   layout 'admin'
 
-  before_filter :load_config, only: [:show, :update, :edit]
+  before_filter :load_config, only: [:show, :update, :edit, :destroy]
   before_filter :load_configs, only: [:index]
 
   def index
@@ -29,10 +29,21 @@ class TaskboardCardConfigurationsController < ApplicationController
 
   def update
     if @config.update_attributes(params[:taskboard_card_configuration])
-      redirect_to pdf_export_taskboard_card_configurations_path
+      flash[:notice] = l(:notice_successful_update)
+      redirect_to :action => 'index'
     else
       render "edit"
     end
+  end
+
+  def destroy
+    # TODO RS: Should not be able to delete the default config. Get that onto the model preferably.
+    if @config.destroy
+      flash[:notice] = l(:notice_successful_delete)
+    end
+      flash[:notice] = l(:error_can_not_delete_taskboard_card_configuration)
+    else
+    redirect_to :action => 'index'
   end
 
   def load_config
