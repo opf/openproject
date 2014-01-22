b2ffeae098c73a7e03e00ed2e75ab7e6e26d834c
Hagen Schink <h.schink@finn.de>
2014-01-22 09:52:25 +0100

    Fixes toggler step

--- features/step_definitions/menu_steps.rb
+++ features/step_definitions/menu_steps.rb
@@ -27,7 +27,7 @@
 #++
 
 When /^I toggle the "([^"]+)" submenu$/ do |menu_name|
-  nodes = all(:css, ".menu_root a[title=\"#{menu_name}\"] .toggler")
+  nodes = all(:css, ".menu_root a[title=\"#{menu_name}\"] + .toggler")
 
   # w/o javascript, all menu elements are expanded by default. So the toggler
   # might not be present.
