2293e86133927382f51541ee7efe4ba1358eff9d
Richard <rm.sylvester@gmail.com>
2014-01-24 11:33:58 +0100

    Manually changed date of migration so that standing data comes in after all the fields are there.

--- db/migrate/20140118141913_insert_default_taskboard_card_configuration.rb
+++ db/migrate/20140118141913_insert_default_taskboard_card_configuration.rb
@@ -5,6 +5,7 @@ class InsertDefaultTaskboardCardConfiguration < ActiveRecord::Migration
       name: "Default",
       per_page: 1,
       page_size: "A4",
+      orientation: "landscape",
       rows: "rows:\n  row1:\n    has_border: false\n    columns:\n      id:\n        has_label: false\n        font_size: 20\n        font_style: bold\n        priority: 1\n        minimum_lines: 2\n        render_if_empty: false\n        width: 30%\n      due_date:\n        has_label: false\n        font_size: 15\n        font_style: italic\n        priority: 1\n        minimum_lines: 2\n        render_if_empty: false\n        width: 70%\n  row2:\n    has_border: false\n    columns:\n      description:\n        has_label: false\n        font_size: 15\n        font_style: normal\n        priority: 4\n        minimum_lines: 5\n        render_if_empty: false\n        width: 100%"
     })
   end
