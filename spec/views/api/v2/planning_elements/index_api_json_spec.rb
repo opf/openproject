4d7e0c6ef5131f9f6191ceee3a0fbf0dd25be470
Nils Kenneweg <n.kenneweg@finn.de>
2014-01-27 15:26:48 +0100

    api test.

--- spec/views/api/v2/planning_elements/index_api_json_spec.rb
+++ spec/views/api/v2/planning_elements/index_api_json_spec.rb
@@ -90,4 +90,39 @@ describe 'api/v2/planning_elements/index.api.rabl' do
 
 
   end
+
+  describe 'with 1 custom field planning element' do
+    let (:custom_field) { FactoryGirl.create(:work_package_custom_field,
+                                             name: 'Database',
+                                             field_format: 'list',
+                                             possible_values: ['MySQL', 'PostgreSQL', 'Oracle'],
+                                             is_for_all: true)}
+
+
+
+    let(:project){FactoryGirl.build(:project_with_types, name: "Sample Project", identifier: "sample_project")}
+    let(:wp1){FactoryGirl.build(:work_package, subject: "Subject #1", project: project)}
+
+    let(:planning_elements) {[wp1]}
+
+    before do
+      project.types[0].custom_fields << custom_field
+      project.save!
+
+      wp1.save!
+      wp1.custom_values[0].value = "MySQL"
+      wp1.save!
+
+      assign(:planning_elements, planning_elements)
+      render
+    end
+
+    subject do
+      response.body
+    end
+
+    it 'should render custom field values' do
+      response.body.should be_json_eql("MySQL".to_json).at_path("planning_elements/0/cf_#{custom_field.id}")
+    end
+  end
 end
