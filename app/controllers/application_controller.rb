a3b543f6d8826479f71a1866a4243deed0cadd4f
Martin Linkhorst <m.linkhorst@gmail.com>
2014-01-28 16:34:34 +0100

    only do http auth when desired

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -45,7 +45,7 @@ require_dependency 'principal'
 
 
 class ApplicationController < ActionController::Base
-  http_basic_authenticate_with name: ENV['HTTP_AUTH_USER'], password: ENV['HTTP_AUTH_PASSWORD']
+  http_basic_authenticate_with name: ENV['HTTP_AUTH_USER'], password: ENV['HTTP_AUTH_PASSWORD'] if ENV.key?('HTTP_AUTH_PASSWORD')
 
   class_attribute :_model_object
   class_attribute :_model_scope
