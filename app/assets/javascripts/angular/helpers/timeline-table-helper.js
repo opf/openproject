13549bee692863470de4de173ab24b23336801f4
Till Breuer <till.breuer@gmx.de>
2014-01-29 16:40:31 +0100

    Add grouping labels for timelines with first level groupings

--- app/assets/javascripts/angular/helpers/timeline-table-helper.js
+++ app/assets/javascripts/angular/helpers/timeline-table-helper.js
@@ -1,7 +1,7 @@
 openprojectApp.factory('TimelineTableHelper', [function() {
   TimelineTableHelper = {
     flattenTimelineTree: function(root, processNodeCallback){
-      nodes = [];
+      var nodes = [];
 
       angular.forEach(root.childNodes, function(node){
         if (processNodeCallback) processNodeCallback(node, root);
@@ -18,19 +18,22 @@ openprojectApp.factory('TimelineTableHelper', [function() {
       if (parent) {
         node.ancestors = [parent];
         if(parent.ancestors) node.ancestors = parent.ancestors.concat(node.ancestors);
+
       }
 
       // first level group
       if(node.payload.objectType === 'Project') {
         node.firstLevelGroup        = node.payload.getFirstLevelGrouping();
         node.firstLevelGroupingName = node.payload.getFirstLevelGroupingName();
+      } else {
+        node.firstLevelGroup        = parent.firstLevelGroup;
       }
     },
 
     getTableRowsFromTimelineTree: function(tree) {
-      rows = TimelineTableHelper.flattenTimelineTree(tree, TimelineTableHelper.addRowDataToNode);
-
       TimelineTableHelper.addRowDataToNode(tree);
+
+      rows = TimelineTableHelper.flattenTimelineTree(tree, TimelineTableHelper.addRowDataToNode);
       rows.unshift(tree);
 
       return rows;
