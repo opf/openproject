37807b17f813aa7911ac8d7e54b84068ff0c9010
Martin Linkhorst <m.linkhorst@gmail.com>
2014-01-31 14:31:51 +0100

    send issue update emails synchronous for now...

--- app/models/journal_observer.rb
+++ app/models/journal_observer.rb
@@ -46,7 +46,10 @@ class JournalObserver < ActiveRecord::Observer
       recipients = issue.recipients + issue.watcher_recipients
       users = User.find_all_by_mails(recipients.uniq)
       users.each do |user|
-        Delayed::Job.enqueue DeliverEmailJob.new(UserMailer.work_package_updated(user, journal, User.current))
+        mail = UserMailer.work_package_updated(user, journal, User.current)
+        mail.deliver
+
+        # Delayed::Job.enqueue DeliverEmailJob.new(mail)
       end
     end
   end
