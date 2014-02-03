763706afcfc79a33fb24d342082be89f53c898ef
Till Breuer <till.breuer@gmx.de>
2014-02-03 16:22:43 +0100

    Move stuff from timelines controller to timeline container directive

--- app/assets/javascripts/angular/controllers/timelines_controller.js
+++ app/assets/javascripts/angular/controllers/timelines_controller.js
@@ -1,27 +1,8 @@
 openprojectApp.controller('TimelinesController', ['$scope', 'Timeline', function($scope, Timeline) {
-
-  getInitialOutlineExpansion = function(timelineOptions) {
-    initialOutlineExpansion = timelineOptions.initial_outline_expansion;
-    if (initialOutlineExpansion && initialOutlineExpansion >= 0) {
-      return initialOutlineExpansion;
-    } else {
-      return 0;
-    }
-  };
-
   // Setup
 
   // Get server-side stuff into scope
   $scope.timelineOptions = gon.timeline_options;
-
-  // Hide charts until tables are drawn
-  $scope.underConstruction = true;
-
-  // Create timeline object
-  $scope.timeline = Timeline.create($scope.timelineOptions);
-
-  // Set initial expansion index
-  $scope.timeline.expansionIndex = getInitialOutlineExpansion($scope.timelineOptions);
 
   // Count timeline containers
   $scope.timelineContainerCount = 0;
--- app/assets/javascripts/angular/directives/timeline-container-directive.js
+++ app/assets/javascripts/angular/directives/timeline-container-directive.js
@@ -1,4 +1,13 @@
 openprojectApp.directive('timelineContainer', [function() {
+  getInitialOutlineExpansion = function(timelineOptions) {
+    initialOutlineExpansion = timelineOptions.initial_outline_expansion;
+    if (initialOutlineExpansion && initialOutlineExpansion >= 0) {
+      return initialOutlineExpansion;
+    } else {
+      return 0;
+    }
+  };
+
   return {
     restrict: 'E',
     replace: true,
@@ -6,6 +15,15 @@ openprojectApp.directive('timelineContainer', [function() {
     template: '<div ng-transclude id="{{timelineContainerElementId}}"/>',
     link: function(scope) {
       scope.timelineContainerElementId = 'timeline-container-' + (++scope.timelineContainerCount);
+
+      // Hide charts until tables are drawn
+      scope.underConstruction = true;
+
+      // Create timeline object
+      scope.timeline = Timeline.create(scope.timelineOptions);
+
+      // Set initial expansion index
+      scope.timeline.expansionIndex = getInitialOutlineExpansion(scope.timelineOptions);
     }
   };
 }]);
