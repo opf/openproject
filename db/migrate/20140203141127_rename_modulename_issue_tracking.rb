7b82cd6cc58a6a1db93297cabe379f7a00940dba
Jonas Heinrich <j.heinrich@finn.de>
2014-02-03 15:27:07 +0100

    added copyrights to migration

--- db/migrate/20140203141127_rename_modulename_issue_tracking.rb
+++ db/migrate/20140203141127_rename_modulename_issue_tracking.rb
@@ -1,3 +1,14 @@
+#-- copyright
+# OpenProject is a project management system.
+#
+# Copyright (C) 2012-2013 the OpenProject Foundation (OPF)
+#
+# This program is free software; you can redistribute it and/or
+# modify it under the terms of the GNU General Public License version 3.
+#
+# See doc/COPYRIGHT.rdoc for more details.
+#++
+
 class RenameModulenameIssueTracking < ActiveRecord::Migration
   def up
     update <<-SQL
