9b999961f40a0b8a361f200a30c481cfe031ee02
Till Breuer <till.breuer@gmx.de>
2014-02-07 09:36:59 +0100

    Re-enable sort headers (WIP)

--- app/assets/javascripts/angular/directives/work_packages/sort-header-directive.js
+++ app/assets/javascripts/angular/directives/work_packages/sort-header-directive.js
@@ -2,8 +2,70 @@ openprojectApp.directive('sortHeader', ['I18n', 'PathHelper', function(I18n, Pat
 
   var defaultSortDirection = 'asc';
 
-  var getQueryString = function(headerName, sortOrder) {
-    return 'set_filter=1&sort=' + headerName + '%2Cparent%3A' + sortOrder;
+  var Sortation = function(encodedSortation) {
+    if (encodedSortation) {
+      this.sortations = encodedSortation.split(',').map(function(sortParam) {
+        fieldAndDirection = sortParam.split(':');
+        return { field: fieldAndDirection[0], direction: fieldAndDirection[1] || defaultSortDirection};
+      });
+    } else {
+      this.sortations = [];
+    }
+  };
+
+  Sortation.prototype.getPrimarySortationCriterion = function() {
+    return this.sortations.first();
+  };
+
+  Sortation.prototype.getDisplayedSortDirectionOfHeader = function(headerName) {
+    var sortDirection, displayedSortation = this.getPrimarySortationCriterion();
+
+    if(displayedSortation && displayedSortation.field === headerName) sortDirection = displayedSortation.direction;
+
+    return sortDirection;
+  };
+
+  Sortation.prototype.getCurrentSortDirectionOfHeader = function(headerName) {
+    var sortDirection;
+
+    angular.forEach(this.sortations, function(sortation){
+      if(sortation && sortation.field === headerName) sortDirection = sortation.direction;
+    });
+
+    return sortDirection;
+  };
+
+  Sortation.prototype.removeSortation = function(elementName) {
+    index = this.sortations.map(function(sortation){
+      return sortation.field;
+    }).indexOf(elementName);
+
+    if (index !== -1) this.sortations.splice(index, 1);
+  };
+
+  Sortation.prototype.addSortation = function(sortation) {
+    this.removeSortation(sortation.field);
+
+    this.sortations.unshift(sortation);
+  };
+
+  Sortation.prototype.getTargetSortationOfHeader = function(headerName) {
+    var targetSortation = angular.copy(this);
+    var targetSortDirection = this.getCurrentSortDirectionOfHeader(headerName) === 'asc' ? 'desc' : 'asc';
+
+    targetSortation.addSortation({field: headerName, direction: targetSortDirection}, targetSortation);
+
+    return targetSortation;
+  };
+
+  Sortation.prototype.encode = function() {
+    return this.sortations.map(function(sortation){
+      if (sortation.direction === 'asc') {
+        return sortation.field;
+      } else {
+        return [sortation.field, sortation.direction].join(':');
+      }
+    }).join(',');
   };
 
   return {
@@ -12,39 +74,21 @@ openprojectApp.directive('sortHeader', ['I18n', 'PathHelper', function(I18n, Pat
     templateUrl: '/templates/work_packages/sort_header.html',
     scope: true,
     link: function(scope, element, attributes) {
-      getAllSortations = function() {
-        if (!scope.currentSortation) return [];
-
-        return scope.currentSortation.split(',').map(function(sortParam) {
-          fieldAndDirection = sortParam.split(':');
-          return { field: fieldAndDirection[0], direction: fieldAndDirection[1] || defaultSortDirection};
-        });
-      };
-
-      getCurrentSortation = function() {
-        return getAllSortations().first();
-      };
-
-      getSortDirectionOfHeader = function(headerName) {
-        var sortDirection;
-        var currentSortation = getCurrentSortation();
-
-        if(currentSortation && currentSortation.field === headerName) sortDirection = currentSortation.direction;
-
-        return sortDirection;
-      };
-
-      sortOrder = 'asc';
       headerName = attributes['headerName'];
+
+      sortation = new Sortation(scope.currentSortation);
+      scope.sortation = sortation;
+      targetSortation = sortation.getTargetSortationOfHeader(headerName);
 
       scope.headerTitle = attributes['headerTitle'];
       scope.sortable = attributes['sortable'];
-
       scope.I18n = I18n;
       scope.path = PathHelper.projectWorkPackagesPath(scope.projectIdentifier);
-      scope.queryString = getQueryString(headerName, sortOrder);
 
-      scope.currentSortDirection = getSortDirectionOfHeader(headerName);
+      scope.currentSortDirection = sortation.getDisplayedSortDirectionOfHeader(headerName);
+
+      scope.queryString = 'set_filter=1&sort=' + targetSortation.encode();
+
     }
   };
 }]);
