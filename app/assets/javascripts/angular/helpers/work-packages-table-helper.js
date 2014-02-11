ad97ca76323b8b4904bec5b2e3c3761419728fd0
Till Breuer <till.breuer@gmx.de>
2014-02-11 12:45:57 +0100

    Manage group index in work packages table helper

--- app/assets/javascripts/angular/helpers/work-packages-table-helper.js
+++ app/assets/javascripts/angular/helpers/work-packages-table-helper.js
@@ -5,15 +5,31 @@ angular.module('openproject.workPackages.helpers')
     /* builds rows from work packages, see IssuesHelper */
     getRows: function(workPackages, groupBy) {
       var rows = [], ancestors = [];
+      var currentGroup, allGroups = [], groupIndex = -1;
 
       angular.forEach(workPackages, function(workPackage, i) {
         while(ancestors.length > 0 && workPackage.parent_id !== ancestors.last().object.id) {
+          // this helper method only reflects hierarchies if nested work packages follow one another
           ancestors.pop();
         }
 
+        // manage groups
+
+        // this helper method assumes that the work packages are passed in in blocks each of which consisting of work packages which belong to one group
+
+        currentGroup = WorkPackagesHelper.getRowObjectContent(workPackage, groupBy);
+
+        if(allGroups.indexOf(currentGroup) === -1) {
+          allGroups.push(currentGroup);
+          groupIndex++;
+        }
+
+        // compose row
+
         var row = {
           level: ancestors.length,
-          groupName: WorkPackagesHelper.getRowObjectContent(workPackage, groupBy),
+          groupIndex: groupIndex,
+          groupName: currentGroup,
           parent: ancestors.last(),
           ancestors: ancestors.slice(0),
           object: workPackage
