0d14efe6b2338679db39de2efeecf81da9e27261
Martin Linkhorst <m.linkhorst@gmail.com>
2014-02-14 11:40:24 +0100

    remove multi tenancy migration. now directly called from the engine's db/migrate path.

--- db/migrate/20140121144322_create_subscribem_accounts.subscribem.rb
+++
@@ -1,9 +0,0 @@
-# This migration comes from subscribem (originally 20140107145130)
-class CreateSubscribemAccounts < ActiveRecord::Migration
-  def change
-    create_table :subscribem_accounts do |t|
-      t.string :subdomain, index: true
-      t.timestamps
-    end
-  end
-end
