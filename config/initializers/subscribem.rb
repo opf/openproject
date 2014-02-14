9f226734a2e3ac99f05e7bb6b5fb6b5d2d42456d
Martin Linkhorst <m.linkhorst@gmail.com>
2014-02-14 11:40:33 +0100

    more configuration!

--- config/initializers/subscribem.rb
+++ config/initializers/subscribem.rb
@@ -1,1 +1,6 @@
-Subscribem.tld_length = ENV.fetch('TLD_LENGTH', 1).to_i
+Subscribem.configure do |c|
+  c.attachment_class     = 'Attachment'.constantize # todo
+  c.default_data_loader  = lambda { Redmine::DefaultData::Loader.load }
+  c.settings_class       = 'Setting'.constantize # todo
+  c.tld_length           = ENV.fetch('TLD_LENGTH', 1).to_i
+end
