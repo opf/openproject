502c7b74d67df19797cc4ed00c6f6b23922ba368
Hagen Schink <h.schink@finn.de>
2014-02-17 09:14:09 +0100

    Removes superfluous patch

--- config/initializers/10-patches.rb
+++ config/initializers/10-patches.rb
@@ -165,53 +165,6 @@ module ActionView
       end
     end
 
-
-    module ActiveRecordHelper
-      def error_messages_for(*params)
-        options = params.extract_options!.symbolize_keys
-
-        if object = options.delete(:object)
-          objects = Array.wrap(object)
-        else
-          objects = params.collect {|object_name| instance_variable_get("@#{object_name}") }.compact
-        end
-
-        count  = objects.inject(0) {|sum, object| sum + object.errors.count }
-        unless count.zero?
-          html = {}
-          [:id, :class].each do |key|
-            if options.include?(key)
-              value = options[key]
-              html[key] = value unless value.blank?
-            else
-              html[key] = 'errorExplanation'
-            end
-          end
-          options[:object_name] ||= params.first
-
-          I18n.with_options :locale => options[:locale], :scope => [:activerecord, :errors, :template] do |locale|
-            header_message = if options.include?(:header_message)
-              options[:header_message]
-            else
-              object_name = options[:object_name].to_s
-              object_name = I18n.t(object_name, :default => object_name.gsub('_', ' '), :scope => [:activerecord, :models], :count => 1)
-              locale.t :header, :count => count, :model => object_name
-            end
-            message = options.include?(:message) ? options[:message] : locale.t(:body)
-
-            contents = ''
-            contents << content_tag(options[:header_tag] || :h2, header_message) unless header_message.blank?
-            contents << content_tag(:p, message) unless message.blank?
-            contents << content_tag(:ul, error_message_list(objects))
-
-            content_tag(:div, contents.html_safe, html)
-          end
-        else
-          ''
-        end
-      end
-    end
-
     module DateHelper
       # distance_of_time_in_words breaks when difference is greater than 30 years
       def distance_of_date_in_words(from_date, to_date = 0, options = {})
