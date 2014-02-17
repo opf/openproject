f5192920c0a9ef772ac0e6270f828332b3e4e609
Till Breuer <till.breuer@gmx.de>
2014-02-17 20:59:49 +0100

    Update work packages table data on switching the grouping

--- app/assets/javascripts/angular/directives/work_packages/query-form-directive.js
+++ app/assets/javascripts/angular/directives/work_packages/query-form-directive.js
@@ -10,6 +10,13 @@ angular.module('openproject.workPackages.directives')
         pre: function(scope) {
           scope.showQueryOptions = false;
 
+          scope.$watch('groupBy', function(oldValue, newValue) {
+            if (newValue !== oldValue) {
+              scope.reloadWorkPackagesTableData();
+            }
+          });
+
+
           scope.reloadWorkPackagesTableData = function() {
             var params =  {
               'c[]': scope.columns
