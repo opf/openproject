cb521b573b8673e1c986256a1d0b351a6b331177
Till Breuer <till.breuer@gmx.de>
2014-02-21 14:52:38 +0100

    Only list filters that haven't been selected as addable

--- app/assets/javascripts/angular/filters/work-packages-filters.js
+++ app/assets/javascripts/angular/filters/work-packages-filters.js
@@ -2,4 +2,16 @@ angular.module('openproject.workPackages.filters')
 
 .filter('allRowsChecked', ['WorkPackagesTableHelper', function(WorkPackagesTableHelper) {
   return WorkPackagesTableHelper.allRowsChecked;
+}])
+
+.filter('subtractFilters', [function() {
+  return function(availableFilters, selectedFilters) {
+    var filters = angular.copy(availableFilters);
+
+    angular.forEach(selectedFilters, function(filter) {
+      delete filters[filter.name];
+    });
+
+    return filters;
+  };
 }]);
