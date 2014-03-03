8db3fb44acaf8c991927a94446a9d9d9e28b7a14
Martin Linkhorst <m.linkhorst@gmail.com>
2014-03-03 14:35:10 +0100

    use strings to inject dependent classes

--- config/initializers/subscribem.rb
+++ config/initializers/subscribem.rb
@@ -1,6 +1,6 @@
 Subscribem.configure do |c|
-  c.attachment_class     = 'Attachment'.constantize # todo
+  c.attachment_class     = 'Attachment'
   c.default_data_loader  = lambda { Redmine::DefaultData::Loader.load }
-  c.settings_class       = 'Setting'.constantize # todo
+  c.settings_class       = 'Setting'
   c.tld_length           = ENV.fetch('TLD_LENGTH', 1).to_i
 end
