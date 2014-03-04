abe83088bd73c16977baae3554e83e7803169021
Till Breuer <till.breuer@gmx.de>
2014-03-04 20:44:07 +0100

    Add directive wrapping slide toggle

---
+++ app/assets/javascripts/angular/directives/components/slide-toggle.js
@@ -0,0 +1,19 @@
+angular.module('openproject.uiComponents')
+
+.directive('slideToggle', [function() {
+  return {
+    restrict: 'A',
+    scope: {
+      speed: '@',
+      collapsed: '='
+    },
+    link: function(scope, element) {
+      var defaultSpeed = 'fast';
+
+      scope.$watch('collapsed', function(state, formerState) {
+        if (state !== formerState) element.slideToggle(scope.speed || defaultSpeed, null);
+      });
+
+    }
+  };
+}]);
