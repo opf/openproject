6ad2293ea188a3f05cc16e43e93065dde7352bb7
Till Breuer <till.breuer@gmx.de>
2014-03-04 20:54:26 +0100

    Consider initial expansion state in slide toggle directive

--- app/assets/javascripts/angular/directives/components/slide-toggle.js
+++ app/assets/javascripts/angular/directives/components/slide-toggle.js
@@ -8,12 +8,13 @@ angular.module('openproject.uiComponents')
       collapsed: '='
     },
     link: function(scope, element) {
+      if (scope.collapsed) element.hide();
+
       var defaultSpeed = 'fast';
 
       scope.$watch('collapsed', function(state, formerState) {
         if (state !== formerState) element.slideToggle(scope.speed || defaultSpeed, null);
       });
-
     }
   };
 }]);
