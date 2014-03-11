b047d8bcf4d83529168eb40c769100de09ce605d
Martin Linkhorst <m.linkhorst@gmail.com>
2014-03-11 16:13:25 +0100

    no repositories on projects

--- app/helpers/projects_helper.rb
+++ app/helpers/projects_helper.rb
@@ -41,7 +41,6 @@ module ProjectsHelper
             {:name => 'members', :action => :manage_members, :partial => 'projects/settings/members', :label => :label_member_plural},
             {:name => 'versions', :action => :manage_versions, :partial => 'projects/settings/versions', :label => :label_version_plural},
             {:name => 'categories', :action => :manage_categories, :partial => 'projects/settings/categories', :label => :label_work_package_category_plural},
-            {:name => 'repository', :action => :manage_repository, :partial => 'projects/settings/repository', :label => :label_repository},
             {:name => 'boards', :action => :manage_boards, :partial => 'projects/settings/boards', :label => :label_board_plural},
             {:name => 'activities', :action => :manage_project_activities, :partial => 'projects/settings/activities', :label => :enumeration_activities},
             {:name => 'types', :action => :manage_types, :partial => 'projects/settings/types', :label => :'label_type_plural'}
