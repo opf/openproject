37e844395127f57edb6cbf6c311cc135068b6e61
Till Breuer <till.breuer@gmx.de>
2014-03-13 17:10:24 +0100

    Display custom field columns in timeline table

--- app/assets/javascripts/angular/directives/timelines/timeline-column-directive.js
+++ app/assets/javascripts/angular/directives/timelines/timeline-column-directive.js
@@ -1,16 +1,71 @@
 angular.module('openproject.timelines.directives')
 
 .constant('WORK_PACKAGE_DATE_COLUMNS', ['start_date', 'due_date'])
-.directive('timelineColumn', ['WORK_PACKAGE_DATE_COLUMNS', 'I18n', function(WORK_PACKAGE_DATE_COLUMNS, I18n) {
+.directive('timelineColumn', ['WORK_PACKAGE_DATE_COLUMNS', 'I18n', 'CustomFieldHelper', function(WORK_PACKAGE_DATE_COLUMNS, I18n, CustomFieldHelper) {
   return {
     restrict: 'A',
     scope: {
       rowObject: '=',
       columnName: '=',
+      timeline: '=',
+      customFields: '='
     },
     templateUrl: '/templates/timelines/timeline_column.html',
     link: function(scope, element) {
       scope.isDateColumn = WORK_PACKAGE_DATE_COLUMNS.indexOf(scope.columnName) !== -1;
+
+      scope.historicalDateKind = getHistoricalDateKind(scope.rowObject, scope.columnName);
+
+      if (CustomFieldHelper.isCustomFieldKey(scope.columnName)) {
+        var customFieldId = CustomFieldHelper.getCustomFieldId(scope.columnName), customFieldName, customFieldFormat;
+
+        // watch custom field because they are loaded after the rows are being iterated
+        scope.$watch('timeline.custom_fields', function() {
+          scope.columnData = getCustomFieldColumnData(scope.rowObject, scope.columnName, scope.customFields, customFieldId, scope.timeline.users);
+        });
+      } else {
+        scope.columnData = getColumnData();
+      }
+
+      function getHistoricalDateKind(object, value) {
+        if (!object.does_historical_differ()) return;
+
+        var newDate = object[value];
+        var oldDate = object.historical()[value];
+
+        if (oldDate && newDate) {
+          return (newDate < oldDate ? 'postponed' : 'preponed');
+        }
+        return "changed";
+      }
+
+
+      function getColumnData() {
+        var map = {
+          "type": "getTypeName",
+          "status": "getStatusName",
+          "responsible": "getResponsibleName",
+          "assigned_to": "getAssignedName",
+          "project": "getProjectName"
+        };
+
+        switch(scope.columnName) {
+          case 'start_date':
+            return scope.rowObject.start_date;
+          case 'due_date':
+            return scope.rowObject.due_date;
+          default:
+            return scope.rowObject[map[scope.columnName]]();
+        }
+      }
+
+      function getCustomFieldColumnData(object, value, customFields, customFieldId, users) {
+        if (customFields && customFields[customFieldId]) {
+          var customField = customFields[customFieldId];
+
+          return CustomFieldHelper.formatCustomFieldValue(object[value], customField.field_format, users);
+        }
+      }
     }
   };
 }]);
--- app/assets/javascripts/angular/helpers/components/custom-field-helper.js
+++ app/assets/javascripts/angular/helpers/components/custom-field-helper.js
@@ -1,13 +1,36 @@
 angular.module('openproject.uiComponents')
 
 .constant('CUSTOM_FIELD_PREFIX', 'cf_')
-.service('CustomFieldHelper', ['CUSTOM_FIELD_PREFIX', function(CUSTOM_FIELD_PREFIX) {
-  return {
+.service('CustomFieldHelper', ['CUSTOM_FIELD_PREFIX', 'I18n', function(CUSTOM_FIELD_PREFIX, I18n) {
+  CustomFieldHelper = {
     isCustomFieldKey: function(key) {
       return key.substr(0, CUSTOM_FIELD_PREFIX.length) === CUSTOM_FIELD_PREFIX;
     },
     getCustomFieldId: function(cfKey) {
       return parseInt(cfKey.substr(CUSTOM_FIELD_PREFIX.length, 10), 10);
+    },
+    booleanCustomFieldValue: function(value) {
+      if (value) {
+        if (value === '1') {
+          return I18n.t('js.general_text_Yes');
+        } else if (value === '0') {
+          return I18n.t('js.general_text_No');
+        }
+      }
+    },
+    formatCustomFieldValue: function(value, fieldFormat, users) {
+      switch(fieldFormat) {
+        case 'bool':
+          return CustomFieldHelper.booleanCustomFieldValue(value);
+        case 'user':
+          if (users[value])
+            return users[value].name;
+          break;
+        default:
+          return value;
+      }
     }
   };
+
+  return CustomFieldHelper;
 }]);
