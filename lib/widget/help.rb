bfba482ff909248ed50adf9b08804342871a2aba
Sebastian Schuster <s.schuster@finn.de>
2014-03-13 20:19:15 +0100

    Added copyright notices

--- lib/widget/help.rb
+++ lib/widget/help.rb
@@ -1,3 +1,22 @@
+#-- copyright
+# ReportingEngine
+#
+# Copyright (C) 2010 - 2014 the OpenProject Foundation (OPF)
+#
+# This program is free software; you can redistribute it and/or
+# modify it under the terms of the GNU General Public License
+# version 3.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+#++
+
 ##
 # Usage: render_widget Widget::Help, :text
 #
@@ -11,7 +30,7 @@ class Widget::Help < Widget::Base
     options.merge!(yield) if block_given?
     sai = options[:show_at_id] ? ", show_at_id: '#{options[:show_at_id]}'" : ""
 
-    icon = tag :img, :src => image_path('icon_info_red.gif'), :id => "target:#{@subject}", :alt => ''
+    icon = tag :img, :src => image_path('reporting_engine/icon_info_red.gif'), :id => "target:#{@subject}", :alt => ''
     tip = content_tag_string :span, l(@subject), tip_config(options[:tooltip]), false
     script = content_tag :script,
       "new Tooltip('target:#{@subject}', 'tip:#{@subject}', {className: 'tooltip'#{sai}});",
