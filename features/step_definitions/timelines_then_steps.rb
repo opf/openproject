fb97ba92f04753c28136fbc1f46e3bfc4fa1ac8e
Till Breuer <till.breuer@gmx.de>
2014-03-13 17:19:07 +0100

    Fix path selector in cucumber timeline steps

--- features/step_definitions/timelines_then_steps.rb
+++ features/step_definitions/timelines_then_steps.rb
@@ -86,7 +86,7 @@ end
 
 Then(/^I should see "(.*?)" in the row of the work package "(.*?)"$/) do |content, wp_name|
   elements = find_lowest_containing_element wp_name, ".tl-main-table"
-  elements[-1].should have_xpath("ancestor::tr/descendant-or-self::*[text()='#{content}']")
+  elements[-1].should have_xpath("ancestor::tr/descendant-or-self::*[contains(text(), '#{content}')]")
 end
 
 Then(/^I should not see "(.*?)" in the row of the work package "(.*?)"$/) do |content, wp_name|
