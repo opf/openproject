3ab122833b6ca0b0ceaf2cadfc15845b481dc0fe
Sebastian Schuster <s.schuster@finn.de>
2014-03-17 11:44:12 +0100

    Added copyright notices, fixed costs dependency

--- db/migrate/20130612104243_reporting_migrate_serialized_yaml.rb
+++ db/migrate/20130612104243_reporting_migrate_serialized_yaml.rb
@@ -1,9 +1,11 @@
 #-- copyright
-# OpenProject is a project management system.
-# Copyright (C) 2011-2013 the OpenProject Foundation (OPF)
+# OpenProject Reporting Plugin
+#
+# Copyright (C) 2010 - 2014 the OpenProject Foundation (OPF)
 #
 # This program is free software; you can redistribute it and/or
-# modify it under the terms of the GNU General Public License version 3.
+# modify it under the terms of the GNU General Public License
+# version 3.
 #
 # This program is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
@@ -13,8 +15,6 @@
 # You should have received a copy of the GNU General Public License
 # along with this program; if not, write to the Free Software
 # Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
-#
-# See doc/COPYRIGHT.md for more details.
 #++
 
 require Rails.root.join("db","migrate","migration_utils","yaml_migrator").to_s
