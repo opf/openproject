11ee346cb4096b90905265412fc554423bfc734c
Till Breuer <till.breuer@gmx.de>
2014-03-17 09:20:22 +0100

    Only set watchers on custom fields for custom field columns

--- app/assets/javascripts/angular/directives/timelines/timeline-column-name-directive.js
+++ app/assets/javascripts/angular/directives/timelines/timeline-column-name-directive.js
@@ -9,16 +9,16 @@ angular.module('openproject.timelines.directives')
       localePrefix: '@'
     },
     link: function(scope, element) {
-      scope.$watch('customFields', function(){
-        if (CustomFieldHelper.isCustomFieldKey(scope.columnName)) {
+      if (CustomFieldHelper.isCustomFieldKey(scope.columnName)) {
+        scope.$watch('customFields', function(){
           var customFieldId = CustomFieldHelper.getCustomFieldId(scope.columnName);
           if (scope.customFields && scope.customFields[customFieldId]) {
             element.html(scope.customFields[customFieldId].name);
           }
-        } else {
-          element.html(I18n.t(scope.localePrefix + '.' + scope.columnName));
-        }
-      });
+        });
+      } else {
+        element.html(I18n.t(scope.localePrefix + '.' + scope.columnName));
+      }
 
     }
   };
