8e56bdbcb885b5b624516de7b04005f8a4ace905
Alex Coles <alex@alexbcoles.com>
2014-03-18 11:55:24 +0100

    Minor style tidy-up setting config.assets.paths
    `Rails.root` is a `Pathname` object.
    
    HT: @plexus (https://twitter.com/plexus/status/445233243164782592)
    
    Signed-off-by: Alex Coles <alex@alexbcoles.com>

--- config/application.rb
+++ config/application.rb
@@ -96,9 +96,11 @@ module OpenProject
 
     # Enable the asset pipeline
     config.assets.enabled = true
-    config.assets.paths << File.join(Rails.root, 'vendor', 'assets', 'components', 'select2')
-    config.assets.paths << File.join(Rails.root, 'vendor', 'assets', 'components', 'jquery-ui', 'themes', 'base')
-    config.assets.paths << File.join(Rails.root, 'vendor', 'assets', 'components', 'jquery.atwho', 'dist')
+
+    bower_assets_path    = Rails.root.join(*%w(vendor assets components))
+    config.assets.paths << bower_assets_path.join(*%w(select2)).to_s
+    config.assets.paths << bower_assets_path.join(*%w(jquery-ui themes base)).to_s
+    config.assets.paths << bower_assets_path.join(*%w(jquery.atwho dist)).to_s
 
     # Enable escaping HTML in JSON.
     config.active_support.escape_html_entities_in_json = true
