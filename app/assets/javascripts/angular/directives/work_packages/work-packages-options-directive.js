3c302a7529fd0c37dd575471a0f928a37aaa35e3
Till Breuer <till.breuer@gmx.de>
2014-03-20 15:37:07 +0100

    Initialize columns on start up, fix js startup errors

--- app/assets/javascripts/angular/directives/work_packages/work-packages-options-directive.js
+++ app/assets/javascripts/angular/directives/work_packages/work-packages-options-directive.js
@@ -5,10 +5,10 @@ angular.module('openproject.workPackages.directives')
     restrict: 'E',
     templateUrl: '/templates/work_packages/work_packages_options.html',
     link: function(scope, element, attributes) {
-      scope.$watch('query.group_by', function() {
+      scope.$watch('query.group_by', function(groupBy) {
         var groupByColumnIndex = scope.columns.map(function(column){
           return column.name;
-        }).indexOf(scope.query.group_by);
+        }).indexOf(groupBy);
 
         scope.groupByColumn = scope.columns[groupByColumnIndex];
       });
