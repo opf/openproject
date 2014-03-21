712e2a23c7cf8b7afd88a1f5e43774f50ee8da9a
Martin Linkhorst <m.linkhorst@gmail.com>
2014-03-21 16:03:50 +0100

    run initializer only when multitenancy plugin is loaded

--- config/initializers/subscribem.rb
+++ config/initializers/subscribem.rb
@@ -3,4 +3,4 @@ Subscribem.configure do |c|
   c.default_data_loader  = lambda { Redmine::DefaultData::Loader.load }
   c.settings_class       = 'Setting'
   c.tld_length           = ENV.fetch('TLD_LENGTH', 1).to_i
-end
+end if defined? Subscribem
