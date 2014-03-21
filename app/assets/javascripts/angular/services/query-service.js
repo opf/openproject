8e7ffdb87257a0a1908c0cf57c49e8133d1bd01b
Till Breuer <till.breuer@gmx.de>
2014-03-21 14:21:50 +0100

    Add a method to retrieve available filter values to query service

--- app/assets/javascripts/angular/services/query-service.js
+++ app/assets/javascripts/angular/services/query-service.js
@@ -1,12 +1,48 @@
 angular.module('openproject.services')
 
-.service('QueryService', ['$http', 'PathHelper', function($http, PathHelper) {
+.service('QueryService', ['$http', 'PathHelper', '$q', 'AVAILABLE_WORK_PACKAGE_FILTERS', 'StatusService', 'TypeService', 'PriorityService', 'UserService', function($http, PathHelper, $q, AVAILABLE_WORK_PACKAGE_FILTERS, StatusService, TypeService, PriorityService, UserService) {
+
+  var availableColumns = [], availableFilterValues = {};
 
   var QueryService = {
     getAvailableColumns: function(projectId) {
       var url = PathHelper.apiAvailableColumnsPath(projectId);
 
       return QueryService.doQuery(url);
+    },
+
+    getAvailableFilterValues: function(filterName) {
+      var modelName = AVAILABLE_WORK_PACKAGE_FILTERS[filterName].modelName;
+
+      if(availableFilterValues[modelName]) {
+        return $q.when(availableFilterValues[modelName]);
+      } else {
+        var retrieveAvailableValues;
+
+        switch(modelName) {
+          case 'status':
+            retrieveAvailableValues = StatusService.getStatuses();
+            break;
+          case 'type':
+            retrieveAvailableValues = TypeService.getTypes();
+            break;
+          case 'priority':
+            retrieveAvailableValues = TypeService.getPriorities();
+            break;
+          case 'user':
+            retrieveAvailableValues = UserService.getUsers();
+            break;
+        }
+
+        return retrieveAvailableValues.then(function(values) {
+          return QueryService.storeAvailableFilterValues(modelName, values);
+        });
+      }
+    },
+
+    storeAvailableFilterValues: function(modelName, values) {
+      availableFilterValues[modelName] = values;
+      return values;
     },
 
     doQuery: function(url, params) {
