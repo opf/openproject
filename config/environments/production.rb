97fc9c0c88273c0aa9127f1db485979e29faec72
Alex Coles <alex@alexbcoles.com>
2014-03-24 14:45:33 +0100

    Add additional assets that should be precompiled
    (per-page JavaScripts, plugins, etc.)
    
    The issue in 265c30a4 likely obscured the fact that these should have
    been explicitly specified.
    
    Signed-off-by: Alex Coles <alex@alexbcoles.com>

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -77,6 +77,16 @@ OpenProject::Application.configure do
 
   # Precompile additional assets (application.js, application.css, and all non-JS/CSS are already added)
   config.assets.precompile += %w( default.css
+                                  activate_error_messages.js
+                                  admin_users.js
+                                  autocompleter.js
+                                  members_form.js
+                                  members_select_boxes.js
+                                  project/responsible_attribute.js
+                                  select_list_move.js
+                                  timelines_select_boxes.js
+                                  types_checkboxes.js
+                                  work_packages.js
                                   rtl.css context_menu_rtl.css
                                   accessibility.js accessibility.css
                                   copy_issue_actions.js repository_navigation.js
