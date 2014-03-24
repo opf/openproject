77581eac4aef43ef3c0996fca5520d9916fcd0f7
Martin Linkhorst <m.linkhorst@gmail.com>
2014-03-24 16:21:54 +0100

    set the host via environment variable, infer TLD_LENGTH from it

--- config/initializers/subscribem.rb
+++ config/initializers/subscribem.rb
@@ -2,5 +2,5 @@ Subscribem.configure do |c|
   c.attachment_class     = 'Attachment'
   c.default_data_loader  = lambda { Redmine::DefaultData::Loader.load }
   c.settings_class       = 'Setting'
-  c.tld_length           = ENV.fetch('TLD_LENGTH', 1).to_i
+  c.host                 = ENV.fetch('HOST_NAME')
 end if defined? Subscribem
