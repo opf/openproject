cbd6df7fb3135cd2a262b519f2b940d0ceb027b9
Philipp Tessenow <philipp@tessenow.org>
2014-03-24 15:16:21 +0100

    implement wp_link 'parsing'

--- lib/open_project/github_integration/notification_handlers.rb
+++ lib/open_project/github_integration/notification_handlers.rb
@@ -16,7 +16,8 @@ module OpenProject::GithubIntegration
     #   number: <pull request number>,
     #   pull_request: <details of the pull request>
     # We observed the following keys to appear. However they are not documented by github
-    #   sender: <the github user who opened a pull request> (might not appear on closed, synchronized, or reopened - we habven't checked)
+    #   sender: <the github user who opened a pull request> (might not appear on closed,
+    #           synchronized, or reopened - we habven't checked)
     #   repo: <the repository in action>
     # }
     def self.pull_request(payload)
@@ -32,8 +33,14 @@ module OpenProject::GithubIntegration
     # Returns:
     #   Array<int>
     def self.parse_work_package(source)
-      # we could use a regexp like this here:
-      #http(?:s?):\/\/openproject\.org\/(?:.*\/?)(?:work_packages|wp)\/([0-9]+)
+      # matches the following things (given that `Setting.host_name` equals 'www.openproject.org')
+      #  - http://www.openproject.org/wp/1234
+      #  - https://www.openproject.org/wp/1234
+      #  - http://www.openproject.org/work_packages/1234
+      #  - https://www.openproject.org/subdirectory/work_packages/1234
+      wp_regex = /http(?:s?):\/\/#{Regexp.escape(Setting.host_name)}\/(?:\S+?\/)*(?:work_packages|wp)\/([0-9]+)/
+
+      source.scan(wp_regex).flatten.map {|s| s.to_i }
     end
 
   end
