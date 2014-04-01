950de3bdf63e92830c4013333cab185d233c104f
Hagen Schink <h.schink@finn.de>
2014-04-01 11:29:29 +0200

    Add specs

---
+++ spec/controllers/meeting_agendas_controller_spec.rb
@@ -0,0 +1,41 @@
+#-- copyright
+# OpenProject Meeting Plugin
+#
+# Copyright (C) 2011-2014 the OpenProject Foundation (OPF)
+#
+# This program is free software; you can redistribute it and/or
+# modify it under the terms of the GNU General Public License version 3.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+#
+# See doc/COPYRIGHT.md for more details.
+#++
+
+require File.dirname(__FILE__) + '/../spec_helper'
+
+describe MeetingAgendasController do
+  let(:meeting) { FactoryGirl.create(:meeting) }
+  let(:user) { FactoryGirl.create(:admin) }
+
+  before { User.stub(:current).and_return(user) }
+
+  describe 'preview' do
+    let(:text) { "Meeting agenda content" }
+
+    it_behaves_like 'valid preview' do
+      let(:preview_texts) { [text] }
+      let(:preview_params) { { meeting_id: meeting.id, meeting_agenda: { text: text } } }
+    end
+
+    it_behaves_like 'authorizes object access' do
+      let(:preview_params) { { meeting_id: meeting.id, meeting_agenda: { } } }
+    end
+  end
+end
