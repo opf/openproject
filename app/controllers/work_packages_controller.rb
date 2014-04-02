17ed76e663042210b2214a616c4d65dfd238d9f7
Till Breuer <till.breuer@gmx.de>
2014-04-02 12:22:05 +0200

    Fix export formats of work_packages#index

--- app/controllers/work_packages_controller.rb
+++ app/controllers/work_packages_controller.rb
@@ -444,12 +444,15 @@ class WorkPackagesController < ApplicationController
   private
 
   def load_work_packages
+    sort_init(@query.sort_criteria.empty? ? [DEFAULT_SORT_ORDER] : @query.sort_criteria)
+    sort_update(@query.sortable_columns)
+
     @results = @query.results(:include => [:assigned_to, :type, :priority, :category, :fixed_version],
                              :order => sort_clause)
     @work_packages = if @query.valid?
-                      results.work_packages.page(page_param)
-                                           .per_page(per_page_param)
-                                           .all
+                      @results.work_packages.page(page_param)
+                                            .per_page(per_page_param)
+                                            .all
                     else
                       []
                     end
