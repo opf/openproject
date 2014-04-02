77a77bc497acae7490a424d9f245c03dd1e0d7d9
Hagen Schink <h.schink@finn.de>
2014-04-02 16:01:06 +0200

    Don't escape selected item twice

--- app/assets/javascripts/members_select_boxes.js
+++ app/assets/javascripts/members_select_boxes.js
@@ -51,7 +51,7 @@ jQuery(document).ready(function($) {
     };
 
     formatItemSelection = function (item) {
-      return OpenProject.Helpers.markupEscape(item.name);
+      return item.name;
     };
     
     $("#members_add_form select.select2-select").each(function (ix, elem){
