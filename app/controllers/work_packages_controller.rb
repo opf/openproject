9ba648a9b8309b9c93dc53f70ed1f262c6491a53
Richard <r.sylvester@finn.de>
2014-04-03 14:35:21 +0200

    Loading work packages except when it's an html request.
    Plays nice with the patched respond_to method in the plugin.

--- app/controllers/work_packages_controller.rb
+++ app/controllers/work_packages_controller.rb
@@ -200,7 +200,7 @@ class WorkPackagesController < ApplicationController
   end
 
   def index
-    load_work_packages
+    load_work_packages unless request.format.html?
 
     respond_to do |format|
       format.html do
