c4ba1634c8e2e86f30cfc1d4766527c659a89c6f
Till Breuer <till.breuer@gmx.de>
2014-04-03 16:05:46 +0200

    Make the jquery-ui dependency with the bower integration

--- app/assets/javascripts/backlogs/backlogs.js
+++ app/assets/javascripts/backlogs/backlogs.js
@@ -33,7 +33,7 @@
 // See doc/COPYRIGHT.rdoc for more details.
 //++
 
-//= require jquery.ui.all
+//= require jquery-ui
 //= require backlogs/jquery.flot/jquery.flot
 //= require backlogs/jquery.flot/excanvas
 //= require backlogs/jquery.jeditable.mini
