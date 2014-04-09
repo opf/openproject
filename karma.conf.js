de1bf01b08e3808a83868d90a0bcae0b87118f06
Alex Coles <alex@alexbcoles.com>
2014-04-09 18:11:26 +0200

    Add, configure karma-junit-reporter (for Jenkins)
    Signed-off-by: Alex Coles <alex@alexbcoles.com>

--- karma.conf.js
+++ karma.conf.js
@@ -92,7 +92,7 @@ module.exports = function(config) {
     // test results reporter to use
     // possible values: 'dots', 'progress'
     // available reporters: https://npmjs.org/browse/keyword/karma-reporter
-    reporters: ['progress'],
+    reporters: ['progress', 'junit'],
 
 
     // web server port
@@ -119,6 +119,11 @@ module.exports = function(config) {
 
     // Continuous Integration mode
     // if true, Karma captures browsers, runs the tests and exits
-    singleRun: false
+    singleRun: false,
+
+
+    junitReporter: {
+      outputFile: 'karma/test-results.xml'
+    }
   });
 };
