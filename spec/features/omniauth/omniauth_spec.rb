c8f689fe319173c5ef531fb45a48cad3c8a31c8a
kgalli <mail@kgalli.de>
2014-04-11 17:42:09 +0200

    Enhancement of integration tests for omniauth authentication
      Includes specs for register a user on the fly.

--- spec/features/omniauth/omniauth_spec.rb
+++ spec/features/omniauth/omniauth_spec.rb
@@ -1,31 +1,116 @@
 require 'spec_helper'
 
 describe 'Omniauth authentication' do
+
+  @omniauth_test_mode = OmniAuth.config.test_mode
+  @capybara_ignore_elements = Capybara.ignore_hidden_elements
+  @omniauth_logger = OmniAuth.config.logger
+
   after do
     User.delete_all
     User.current = nil
-    OmniAuth.config.test_mode = false
-    Capybara.ignore_hidden_elements = true
+    OmniAuth.config.test_mode = @omniauth_test_mode
+    Capybara.ignore_hidden_elements = @capybara_ignore_elements
+    OmniAuth.config.logger = @omniauth_logger
   end
 
-  it 'should sign in existing user' do
-    FactoryGirl.create(:user, force_password_change: false, identity_url: 'developer:foo@bar.com', login: 'bob', mail: 'foo@bar.com')
+  before do
+    OmniAuth.config.logger = Rails.logger
     Capybara.ignore_hidden_elements = false
-
-    visit '/auth/developer'
-    fill_in('name', with: 'bob')
-    fill_in('email', with: 'foo@bar.com')
-    click_link_or_button 'Sign In'
-    expect(page).to have_link('Sign out')
   end
 
-  it 'should fail with generic error message' do
-    # set omniauth to test mode will redirect all calls to omniauth
-    # directly to the callback and by setting the mock_auth provider
-    # to a symbol will force omniauth to fail /auth/failure
-    OmniAuth.config.test_mode = true
-    OmniAuth.config.mock_auth[:developer] = :invalid_credentials
-    visit '/auth/developer'
-    expect(page).to have_content(I18n.t(:error_external_authentication_failed))
+  context 'sign in existing user' do
+    let(:user) do
+      FactoryGirl.create(:user,
+                         force_password_change: false,
+                         identity_url: 'developer:omnibob@example.com',
+                         login: 'omnibob',
+                         mail: 'omnibob@example.com',
+                         firstname: 'omni',
+                         lastname: 'bob'
+                        )
+    end
+
+    it 'should redirect to back url' do
+      visit account_lost_password_path
+      find_link('Omniauth Developer').click
+      fill_in('name', with: user.login)
+      fill_in('email', with: user.mail)
+      click_link_or_button 'Sign In'
+
+      expect(current_url).to eql account_lost_password_url
+    end
+
+    it 'should sign in user' do
+      visit '/auth/developer'
+      fill_in('name', with: user.login)
+      fill_in('email', with: user.mail)
+      click_link_or_button 'Sign In'
+
+      expect(page).to have_content('omni bob')
+      expect(page).to have_link('Sign out')
+    end
+  end
+
+  context 'register on the fly' do
+    let(:user) do
+       User.new(
+         { force_password_change: false,
+           identity_url: 'developer:omnibob@example.com',
+           login: 'omnibob',
+           mail: 'omnibob@example.com',
+           firstname: 'omni',
+           lastname: 'bob'
+         })
+    end
+
+    before do
+      Setting.stub(:self_registration?).and_return(true)
+      Setting.stub(:self_registration).and_return("3")
+    end
+
+    it 'should register new user' do
+      visit '/auth/developer'
+
+      # login form developer strategy
+      fill_in('name', with: user.login)
+      fill_in('email', with: user.mail)
+      click_link_or_button 'Sign In'
+
+      # on register form
+      fill_in('user_lastname', with: user.lastname)
+      click_link_or_button 'Submit'
+
+      expect(page).to have_content(I18n.t(:notice_account_registered_and_logged_in))
+      expect(page).to have_link('Sign out')
+    end
+
+    it 'should redirect to bach url' do
+      visit account_lost_password_path
+      find_link('Omniauth Developer').click
+
+      # login form developer strategy
+      fill_in('name', with: user.login)
+      fill_in('email', with: user.mail)
+      click_link_or_button 'Sign In'
+
+      # on register form
+      fill_in('user_lastname', with: user.lastname)
+      click_link_or_button 'Submit'
+
+      expect(current_url).to eql my_first_login_url(back_url: account_lost_password_url)
+    end
+  end
+
+  context 'error occurs' do
+    it 'should fail with generic error message' do
+      # set omniauth to test mode will redirect all calls to omniauth
+      # directly to the callback and by setting the mock_auth provider
+      # to a symbol will force omniauth to fail /auth/failure
+      OmniAuth.config.test_mode = true
+      OmniAuth.config.mock_auth[:developer] = :invalid_credentials
+      visit '/auth/developer'
+      expect(page).to have_content(I18n.t(:error_external_authentication_failed))
+    end
   end
 end
