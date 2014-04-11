83f723ef20a0370ee0471ab10aa46071d0387d63
Till Breuer <till.breuer@gmx.de>
2014-04-11 18:33:58 +0200

    Install karma-chain-sinon and fix karma conf
    by myabc

--- karma.conf.js
+++ karma.conf.js
@@ -15,7 +15,7 @@ module.exports = function(config) {
 
     // frameworks to use
     // available frameworks: https://npmjs.org/browse/keyword/karma-adapter
-    frameworks: ['mocha', 'chai'],
+    frameworks: ['mocha', 'chai-sinon'],
 
 
     // list of files / patterns to load in the browser
