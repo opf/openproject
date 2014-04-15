2b23ab826a1460d2121ece2d25feddc81c12bb34
Markus Kahl <machisuji@googlemail.com>
2014-04-15 15:55:20 +0100

    load OpenID Connect providers

--- lib/open_project/openid_connect/engine.rb
+++ lib/open_project/openid_connect/engine.rb
@@ -12,5 +12,23 @@ module OpenProject::OpenIDConnect
              :author_url => 'http://finn.de',
              :requires_openproject => '>= 3.1.0pre1'
 
+    initializer "openid_connect.middleware" do |app|
+      # Loading OpenID providers manually since rails doesn't do it automatically,
+      # possibly due to non trivially module-name-convertible paths.
+      require 'omniauth/openid_connect/provider'
+
+      # load pre-defined providers
+      Dir[File.join(File.dirname(__FILE__), "../../omniauth/openid_connect/*.rb")].each do |file|
+        require file.gsub("^.*lib/", "").gsub(".rb", "")
+      end
+
+      OmniAuth::OpenIDConnect::Provider.load_generic_providers
+
+      app.config.middleware.use OmniAuth::Builder do
+        OmniAuth::OpenIDConnect::Provider.available.each do |pro|
+          provider :openid_connect, pro.new.to_hash
+        end
+      end
+    end
   end
 end
