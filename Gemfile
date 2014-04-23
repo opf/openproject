opf/openproject

commit 9721cb95fb4e4e8f7129cf2d6496de96801d89ae

Remove guard as we don't use it

Gemfile
@@ -167,12 +167,8 @@ group :development do
   gem 'pry-debugger', :platforms => :mri_19
   gem 'pry-doc'
   gem 'rails-dev-tweaks', '~> 0.6.1'
-  gem 'guard-rspec'
-  gem 'guard-cucumber'
-  gem 'rb-fsevent', :group => :test
   gem 'thin'
   gem 'faker'
-  gem 'guard-test'
 end
 
 # Use the commented pure ruby gems, if you have not the needed prerequisites on

commit 70ba8bd01e9b0f4e89658f24db3237aba91142bf

Bring back test-unit dependency to fix CI::Reporter issue
Broken in 9721cb95: test-unit was previously present by virtue of
being a guard-test dependency.

Signed-off-by: Alex Coles <alex@alexbcoles.com>

Gemfile
@@ -152,6 +152,7 @@ group :test do
   gem "json_spec"
   gem "activerecord-tableless", "~> 1.0"
   gem "codeclimate-test-reporter", :require => nil
+  gem 'test-unit', '2.5.5'
 end
 
 group :ldap do