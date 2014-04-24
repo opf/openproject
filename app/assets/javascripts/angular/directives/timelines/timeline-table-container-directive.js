a269ce212ed8699caed9fd5352591547515a7cce
Richard <r.sylvester@finn.de>
2014-04-24 10:20:38 +0200

    Removed unused param.

--- app/assets/javascripts/angular/directives/timelines/timeline-table-container-directive.js
+++ app/assets/javascripts/angular/directives/timelines/timeline-table-container-directive.js
@@ -108,7 +108,7 @@ angular.module('openproject.timelines.directives')
           if (scope.rows.length > 0) {
             completeUI();
           } else {
-            timeline.warn(I18n.t('js.label_no_data'), 'warning', this.showWarning, scope);
+            timeline.warn(I18n.t('js.label_no_data'), 'warning', this.showWarning);
           }
         } catch (e) {
           timeline.die(e);
