opf/openproject

commit e208115ca3241509baf8bad4b2c0dd9888422294

Temporarily disable Uglifier mangling/obfuscation
Angular.js code needs to be updated to use explicit DI before
re-enabling this option.

Bug # 7210

Signed-off-by: Alex Coles <alex@alexbcoles.com>

config/environments/production.rb
@@ -47,6 +47,8 @@ OpenProject::Application.configure do
   # Compress JavaScripts and CSS
   config.assets.compress = true
 
+  config.assets.js_compressor = Uglifier.new(mangle: false)
+
   # Don't fallback to assets pipeline if a precompiled asset is missed
   config.assets.compile = false
 

commit 6bd9345074b65134c2a883ad533e46c322ba6893

Fix 'uninitialized constant Uglifier', broken in e208115c
:scream_cat:

Uglifier is not available in production at run-time, only at asset
precompilation-time (hence being in the `assets` group).

Signed-off-by: Alex Coles <alex@alexbcoles.com>

config/environments/production.rb
@@ -47,7 +47,7 @@ OpenProject::Application.configure do
   # Compress JavaScripts and CSS
   config.assets.compress = true
 
-  config.assets.js_compressor = Uglifier.new(mangle: false)
+  config.assets.js_compressor = Uglifier.new(mangle: false) if defined? Uglifier
 
   # Don't fallback to assets pipeline if a precompiled asset is missed
   config.assets.compile = false