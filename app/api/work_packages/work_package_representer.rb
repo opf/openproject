5cd59c4ad66652a58703f6c6bcf4557baf687b3b
Marek Takac <m.takac@finn.de>
2014-04-29 16:54:24 +0200

    Created some decorators

--- app/api/work_packages/work_package_representer.rb
+++ app/api/work_packages/work_package_representer.rb
@@ -24,30 +24,29 @@ module WorkPackages
     property :created_at
     property :updated_at
 
-    link :self do
-      "/work_packages/#{represented.id}"
+    property :_links, exec_context: :decorator
+
+    def _links
+        {
+            root: { href: "/", title: 'OpenProject API entry point' },
+            self: { href: "/work_packages/#{represented.id}", title: "represented.subject" },
+            work_packages: { href: "/work_packages", title: "Work packages" },
+            project_work_packages: { href: "/projects/#{represented.project.identifier}/work_packages", title: "#{represented.project.name} - Work packages" },
+            descendants: { href: "/projects/#{represented.project.identifier}/work_packages?filter[ancestor_id]=#{represented.id}", title: "#{represented.subject} - Descendant work packages" },
+            children: { href: "/projects/#{represented.project.identifier}/work_packages?filter[parent_id]=#{represented.id}", title: "#{represented.subject} - Children work packages" },
+            create: { href: "/projects/#{represented.project.identifier}/work_packages", method: :post, title: "#{represented.project.name} - Create new work package" },
+            update: { href: "/work_packages/#{represented.id}", method: :patch, title: "Update #{represented.subject}" },
+            delete: { href: "/work_packages/#{represented.id}", method: :delete, title: "Delete #{represented.subject}" },
+            project: { href: "/projects/#{represented.project.identifier}", title: "#{represented.project}" },
+            author: { href: "/users/#{represented.author_id}", title: "?" },
+            assignee: { href: "/users/#{represented.assigned_to_id}", title: "?" },
+            responsible: { href: "/users/#{represented.responsible_id}", title: "?" }
+        }
     end
 
-    property :project, class: Project, embedded: true do
-      property :id
-    end
-
-    property :author, class: User, embedded: true do
-      property :mail
-    end
-
-    property :assigned_to, as: :assignee, class: User, embedded: true do
-      property :mail
-    end
-
-    property :responsible, class: User, embedded: true do
-      property :mail
-    end
-
-
-
-    # collection :children, class: WorkPackage, embedded: true do
-    #   property :subject
-    # end
+    property :project, class: Project, decorator:  Projects::ProjectRepresenter, embedded: true
+    property :author, class: User, decorator: Users::UserRepresenter, embedded: true
+    property :assigned_to, as: :assignee, class: User, decorator: Users::UserRepresenter, embedded: true
+    property :responsible, class: User, decorator: Users::UserRepresenter, embedded: true
   end
 end
