1215a61044196bc2f1a80afdd94600ff38e4978a
Alex Coles <alex@alexbcoles.com>
2014-04-29 13:33:49 +0200

    Add skeleton test for ng WorkPackagesController
    Use `$timeout` service with `$timeout.flush()` as a shortcut to build
    and resolve promises.
    
    Signed-off-by: Alex Coles <alex@alexbcoles.com>

---
+++ karma/tests/controllers/work-packages-controller-test.js
@@ -0,0 +1,103 @@
+//-- copyright
+// OpenProject is a project management system.
+// Copyright (C) 2012-2014 the OpenProject Foundation (OPF)
+//
+// This program is free software; you can redistribute it and/or
+// modify it under the terms of the GNU General Public License version 3.
+//
+// OpenProject is a fork of ChiliProject, which is a fork of Redmine. The copyright follows:
+// Copyright (C) 2006-2013 Jean-Philippe Lang
+// Copyright (C) 2010-2013 the ChiliProject Team
+//
+// This program is free software; you can redistribute it and/or
+// modify it under the terms of the GNU General Public License
+// as published by the Free Software Foundation; either version 2
+// of the License, or (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program; if not, write to the Free Software
+// Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+//
+// See doc/COPYRIGHT.rdoc for more details.
+//++
+
+/*jshint expr: true*/
+
+describe('WorkPackagesController', function() {
+  var scope, ctrl, win, testWorkPackageService, testQueryService, testPaginationService;
+
+  beforeEach(module('openproject.workPackages.controllers'));
+  beforeEach(inject(function($rootScope, $controller, $timeout) {
+    scope = $rootScope.$new();
+    win   = {
+     location: { pathname: "" }
+    };
+
+    var workPackageData = {
+      meta: {
+      }
+    };
+    var columnData = {
+    };
+
+    testWorkPackageService = {
+      getWorkPackages: function () {
+      },
+      getWorkPackagesByQueryId: function (params) {
+        return $timeout(function () {
+          return workPackageData;
+        }, 10);
+      },
+      getWorkPackagesFromUrlQueryParams: function () {
+        return $timeout(function () {
+          return workPackageData;
+        }, 10);
+      }
+    };
+    testQueryService = {
+      getQuery: function () {
+        return {
+          serialiseForAngular: function () {
+          }
+        }
+      },
+      initQuery: function () {
+      },
+      getAvailableColumns: function () {
+        return $timeout(function () {
+          return columnData;
+        }, 10);
+      }
+    };
+    testPaginationService = {
+      setPerPageOptions: function () {
+      },
+      setPerPage: function () {
+      },
+      setPage: function () {
+      }
+    };
+
+    ctrl = $controller("WorkPackagesController", {
+      $scope:  scope,
+      $window: win,
+      QueryService:       testQueryService,
+      PaginationService:  testPaginationService,
+      WorkPackageService: testWorkPackageService
+    });
+
+    $timeout.flush();
+  }));
+
+  describe('initialisation', function() {
+    it('should initialise', function() {
+      expect(scope.loading).to.be.false;
+    });
+  });
+
+});
