4d4a4695a247ade5af30291eafb3a4043edb925c
Martin Linkhorst <m.linkhorst@gmail.com>
2014-04-30 11:47:43 +0200

    remove unnecessary calls to presence

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -31,7 +31,7 @@
 
 config = OpenProject::Configuration
 
-session_store     = config['session_store'].presence || :cookie_store
+session_store     = config['session_store'] || :cookie_store
 relative_url_root = config['rails_relative_url_root'].presence
 
 session_options = {
