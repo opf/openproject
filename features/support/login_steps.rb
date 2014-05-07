90efec66bd3b35891fb16cc426acf59ec609872c
kgalli <mail@kgalli.de>
2014-05-07 22:52:12 +0200

    removal of second colon to get tests running again

--- features/support/login_steps.rb
+++ features/support/login_steps.rb
@@ -33,7 +33,7 @@ module LoginSteps
     visit '/login'
     within('#login-form') do
       fill_in User.human_attribute_name(:login), :with => login
-      fill_in 'Password:', :with => password
+      fill_in 'Password', :with => password
       click_button 'Login'
     end
   end
