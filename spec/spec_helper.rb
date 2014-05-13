58a9211413ca10b4bfc741b370f6a2fb7b2e2d38
Till Breuer <till.breuer@gmx.de>
2014-05-13 15:26:06 +0200

    Prevent temporary patch from failing if no common project types exist
    Move and fix requiring of toggable field set spec support

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -46,6 +46,7 @@ require 'capybara/rails'
 # Requires supporting ruby files with custom matchers and macros, etc,
 # in spec/support/ and its subdirectories.
 Dir[Rails.root.join("spec/support/**/*.rb")].each {|f| require f}
+Dir[Rails.root.join("spec/features/support/**/*.rb")].each {|f| require f}
 
 RSpec.configure do |config|
   # ## Mock Framework
