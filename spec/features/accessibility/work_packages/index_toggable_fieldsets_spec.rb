01d82ef01ca813c3329034826cb815d6c0e80fe3
Alex Coles <alex@alexbcoles.com>
2014-05-13 19:38:13 +0200

    Fix require in togglable fieldset spec
    Broken with rename in 86c62ccb.
    
    Signed-off-by: Alex Coles <alex@alexbcoles.com>

--- spec/features/accessibility/work_packages/index_toggable_fieldsets_spec.rb
+++ spec/features/accessibility/work_packages/index_toggable_fieldsets_spec.rb
@@ -27,7 +27,7 @@
 #++
 
 require 'spec_helper'
-require 'features/accessibility/support/toggable_fieldsets'
+require 'features/support/toggable_fieldsets'
 require 'features/work_packages/work_packages_page'
 
 describe 'Work package index' do
