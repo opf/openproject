7bbfc5c06e333be17c6ab431425157d040c808f6
Hagen Schink <h.schink@finn.de>
2014-05-13 12:00:43 +0200

    Update button definition

--- lib/widget/controls/apply.rb
+++ lib/widget/controls/apply.rb
@@ -20,9 +20,10 @@
 
 class Widget::Controls::Apply < Widget::Controls
   def render
-    write link_to content_tag(:span, content_tag(:em, l(:button_apply))), {},
-      :href => "#", :id => "query-icon-apply-button",
-      :class => "button apply reporting_button",
-      :"data-target" => url_for(:action => 'index', :set_filter => '1')
+    write link_to(l(:button_apply),
+                  '#',
+                  id: 'query-icon-apply-button',
+                  :class => 'button_highlight',
+                  :'data-target' => url_for(action: 'index', set_filter:  '1'))
   end
 end
--- lib/widget/controls/delete.rb
+++ lib/widget/controls/delete.rb
@@ -20,10 +20,10 @@
 class Widget::Controls::Delete < Widget::Controls
   def render
     return "" if @subject.new_record? or !@options[:can_delete]
-    button = link_to content_tag(:span, content_tag(:em, l(:button_delete), :class => "button-icon icon-delete")), "#",
-          :class => 'button secondary',
-          :id => 'query-icon-delete',
-          :title => l(:button_delete)
+    button = link_to(l(:button_delete),
+                     '#',
+                     id: 'query-icon-delete',
+                     :class => "button icon-context icon-delete")
     popup = content_tag :div, :id => "delete_form", :style => "display:none", :class => "button_form" do
       question = content_tag :p, l(:label_really_delete_question)
       options = content_tag :p do
