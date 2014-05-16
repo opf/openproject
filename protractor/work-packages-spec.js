39c9dadd684b0f6b425390e5332ceda35e98672f
Alex Coles <alex@alexbcoles.com>
2014-05-16 18:56:46 +0200

    Add Protractor config, example e2e spec
    This initial spec still relies on a running Rails server (with default
    seed data).
    
    User Story # 16494
    
    Signed-off-by: Alex Coles <alex@alexbcoles.com>

---
+++ protractor/work-packages-spec.js
@@ -0,0 +1,82 @@
+//-- copyright
+// OpenProject is a project management system.
+// Copyright (C) 2012-2014 the OpenProject Foundation (OPF)
+//
+// This program is free software; you can redistribute it and/or
+// modify it under the terms of the GNU General Public License version 3.
+//
+// OpenProject is a fork of ChiliProject, which is a fork of Redmine. The copyright follows:
+// Copyright (C) 2006-2013 Jean-Philippe Lang
+// Copyright (C) 2010-2013 the ChiliProject Team
+//
+// This program is free software; you can redistribute it and/or
+// modify it under the terms of the GNU General Public License
+// as published by the Free Software Foundation; either version 2
+// of the License, or (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program; if not, write to the Free Software
+// Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+//
+// See doc/COPYRIGHT.rdoc for more details.
+//++
+
+var chai = require('chai');
+var chaiAsPromised = require('chai-as-promised');
+
+chai.use(chaiAsPromised);
+var expect = chai.expect;
+
+var WorkPackagesPage = require('./pages/work-packages-page.js');
+
+describe('OpenProject', function() {
+  var page = new WorkPackagesPage();
+
+  before(function() {
+    browser.get('http://localhost:3000/');
+
+    var loginButton = $('.login');
+    var loginArea   = $('#nav-login-content');
+
+    expect(loginButton.getText()).to.eventually.equal('Sign in');
+    expect(loginArea.isDisplayed()).to.eventually.be.false;
+
+    loginButton.click();
+    expect(loginArea.isDisplayed()).to.eventually.be.true;
+
+    var usernameField = $('#username-pulldown');
+    var passwordField = $('#password-pulldown');
+    var inputButton   = $('input[name=login]');
+
+    usernameField.sendKeys('admin');
+    passwordField.sendKeys('admin');
+    inputButton.click();
+  });
+
+  it('should show work packages title', function() {
+    page.get();
+
+    expect($('h2').getText()).to.eventually.equal('WORK PACKAGES');
+  });
+
+  it('should show work packages', function() {
+    page.get();
+
+    page.workPackagesTable.all(by.css('th')).getText().then(function(text) {
+      expect(text).to.include.members([
+        'PROJECT',
+        'TYPE',
+        'STATUS',
+        'PRIORITY',
+        'SUBJECT',
+        'ASSIGNEE',
+        'UPDATED ON'
+      ]);
+    });
+  });
+});
