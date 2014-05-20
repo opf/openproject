50f9f1ccc7baea74434fa7748870479509028559
Till Breuer <till.breuer@gmx.de>
2014-05-20 17:33:51 +0200

    Fix typo

--- app/assets/javascripts/angular/directives/work_packages/options-dropdown-directive.js
+++ app/assets/javascripts/angular/directives/work_packages/options-dropdown-directive.js
@@ -61,14 +61,14 @@ angular.module('openproject.workPackages.directives')
         }
       };
 
-      $scope.showColumnsModal  = function(){
-        $scope.$emit('hideAllDropdowns');
+      scope.showColumnsModal  = function(){
+        scope.$emit('hideAllDropdowns');
         columnsModal.activate();
       };
 
       scope.toggleDisplaySums = function(){
         scope.query.displaySums = !scope.query.displaySums;
-      }
+      };
     }
   };
 }]);
