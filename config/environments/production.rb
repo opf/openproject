3a2185ed062d067d937e9c81079feb3baf3e33e7
Alex Coles <alex@alexbcoles.com>
2014-05-22 10:27:50 +0200

    Revert "Temporarily disable digest assets in production"
    This setting is not respected when precompiling.
    
    This reverts commit 74996c0dffdf991da89e5e71fa5105171e4fe707.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -53,7 +53,7 @@ OpenProject::Application.configure do
   config.assets.compile = false
 
   # Generate digests for assets URLs
-  config.assets.digest = false
+  config.assets.digest = true
 
   # Defaults to nil and saved in location specified by config.assets.prefix
   # config.assets.manifest = YOUR_PATH
