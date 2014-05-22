74996c0dffdf991da89e5e71fa5105171e4fe707
Alex Coles <alex@alexbcoles.com>
2014-05-22 09:41:04 +0200

    Temporarily disable digest assets in production
    Rails 4's asset pipeline only pre-compiles assets with digest. This
    causes two issues:
     - `font-url` helper in Sass is not referencing assets with the
       digest. This may be a manifestation of the same problem worked-
       around in b0f758fc.
     - various plugins appear not to be using the `asset_path` helpers.
    
    Signed-off-by: Alex Coles <alex@alexbcoles.com>

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -53,7 +53,7 @@ OpenProject::Application.configure do
   config.assets.compile = false
 
   # Generate digests for assets URLs
-  config.assets.digest = true
+  config.assets.digest = false
 
   # Defaults to nil and saved in location specified by config.assets.prefix
   # config.assets.manifest = YOUR_PATH
