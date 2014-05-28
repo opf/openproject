7165e32bc45f56571b570ffa03b9c1c34f06296c
Marek Takac <m.takac@finn.de>
2014-05-28 17:30:42 +0200

    Cleanup

--- app/api/work_packages/work_packages.rb
+++ app/api/work_packages/work_packages.rb
@@ -4,45 +4,6 @@ module WorkPackages
     resources :work_packages do
 
       params do
-        optional :offset, type: Integer, default: 0, desc: 'Offset'
-        optional :limit, type: Integer, default: 100, desc: 'Limit'
-      end
-      get do
-        limit, offset = params[:limit], params[:offset]
-        work_packages =
-          WorkPackage
-            .includes(:project, :author, :responsible, :assigned_to, :type, :status, :priority)
-            .limit(limit)
-            .offset(offset)
-        work_packages_models = work_packages.map { |wp| WorkPackageModel.new(work_package: wp) }
-        work_packages_representer = WorkPackagesRepresenter.new(work_packages_models)
-        work_packages_representer.to_json
-      end
-
-      params do
-        optional :workPackages, type: Array
-      end
-      patch do
-        response = { _embedded: { results: [] }}
-        params['workPackages'].each do |param|
-          param = ActiveSupport::JSON.decode(param)
-          id = param.delete('id')
-          work_package = WorkPackage.find(id)
-          work_package_model = WorkPackageModel.new(work_package: work_package)
-          work_package_representer = WorkPackageRepresenter.new(work_package_model)
-          work_package_representer.from_json(param.to_json)
-
-          if work_package_representer.represented.valid?
-            work_package_representer.represented.save
-            response[:_embedded][:results] << { status: { code: 200, text: 'Ok' }, workPackage: work_package_representer }
-          else
-            response[:_embedded][:results] << {status: { code: 422, text: 'Unprocessable entity' }, errors: work_package_representer.represented.errors.to_json }
-          end
-        end
-        response.to_json
-      end
-
-      params do
         requires :id, type: Integer, desc: 'Work package id.'
       end
       namespace ':id' do
@@ -51,11 +12,6 @@ module WorkPackages
           @work_package = WorkPackage.find(params[:id])
           work_package_model = WorkPackageModel.new(work_package: @work_package)
           @work_package_representer = WorkPackageRepresenter.new(work_package_model)
-        end
-
-        get do
-          authorize(:work_packages_api, :get, @work_package.project)
-          @work_package_representer.to_json
         end
 
         params do
@@ -84,7 +40,6 @@ module WorkPackages
           else
             @work_package_representer.represented.errors.to_json
           end
-
         end
 
       end
