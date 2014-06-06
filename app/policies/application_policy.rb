c2101b035219d1336d425bcb5c9e423bc4a928b6
Marek Takac <m.takac@finn.de>
2014-06-06 19:19:40 +0200

    Clean up

--- app/policies/application_policy.rb
+++
@@ -1,7 +0,0 @@
-class ApplicationPolicy
-  def initialize(user, record)
-    raise Pundit::NotAuthorizedError, "must be logged in" unless user
-    @user = user
-    @record = record
-  end
-end
--- vendor/assets/components/jquery.atwho/src/wrapper_header.js
+++
@@ -1,8 +0,0 @@
-(function() {
-  (function(factory) {
-    if (typeof define === 'function' && define.amd) {
-      return define(['jquery'], factory);
-    } else {
-      return factory(window.jQuery);
-    }
-  })(function($) {
