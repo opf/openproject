opf/openproject

commit bb65ce4e0e702ea478789ff6295abce0a76e829e

Reenable ng $location service HTML5 mode
Update Router to fall back to full page load (i.e. Rails) when a route
is not matched on the client-side.

Also update Router paths/params correspondingly.

Task # 8869

Signed-off-by: Alex Coles <alex@alexbcoles.com>

app/assets/javascripts/angular/routing.js
@@ -31,11 +31,21 @@ openprojectApp.config([
   '$urlRouterProvider',
   function($stateProvider, $urlRouterProvider) {
 
-  $urlRouterProvider.otherwise("/wp");
+  // All unmatched routes should be handled by Rails
+  // (i.e. invoke a full-page load)
+  //
+  // a better behaviour here would be for Angular's HTML5-mode to provide an
+  // option to turn link hijacking off by default (and use a `target` attribute
+  // to turn it on for selected links).
+
+  $urlRouterProvider.otherwise(function($injector, $location) {
+    $window = $injector.get('$window');
+    $window.location.replace($location.path());
+  });
 
   $stateProvider
     .state('work-packages', {
-      url: "/wp?query_id",
+      url: "/projects/:projectIdentifier/work_packages?query_id",
       abstract: true,
       templateUrl: "/templates/work_packages.html",
       controller: 'WorkPackagesController'
@@ -45,7 +55,7 @@ openprojectApp.config([
       templateUrl: "/templates/work_packages.list.html"
     })
     .state('work-packages.list.details', {
-      url: "/:workPackageId",
+      url: "/{workPackageId:[0-9]+}",
       templateUrl: "/templates/work_packages.list.details.html",
       controller: 'WorkPackageDetailsController'
     });

app/assets/javascripts/angular/openproject-app.js
@@ -124,8 +124,7 @@ window.appBasePath = jQuery('meta[name=app_base_path]').attr('content') || '';
 
 openprojectApp
   .config(['$locationProvider', '$httpProvider', function($locationProvider, $httpProvider) {
-    // Note: Not using this because we want to use $location to get the url params and html5Mode prevents all the links from working normally.
-    // $locationProvider.html5Mode(true);
+    $locationProvider.html5Mode(true);
     $httpProvider.defaults.headers.common['X-CSRF-TOKEN'] = jQuery('meta[name=csrf-token]').attr('content'); // TODO find a more elegant way to keep the session alive
 
     $httpProvider.interceptors.push(function ($q) {

commit 3b5954aabc55d7ddbd3b640e693bda63461e18c2

Disable ng HTML5 mode's link-hijacking
This should avoid some of the problems introduced in bb65ce4e (namely
possible endless redirects on unmatched, non-Rails routes).

* Tidy up router formatting

Signed-off-by: Alex Coles <alex@alexbcoles.com>

app/assets/javascripts/angular/routing.js
@@ -26,34 +26,38 @@
 // See doc/COPYRIGHT.rdoc for more details.
 //++
 
-openprojectApp.config([
-  '$stateProvider',
-  '$urlRouterProvider',
-  function($stateProvider, $urlRouterProvider) {
+openprojectApp
+  .config([
+    '$stateProvider',
+    '$urlRouterProvider',
+    function($stateProvider, $urlRouterProvider) {
 
-  // All unmatched routes should be handled by Rails
-  // (i.e. invoke a full-page load)
-  //
-  // a better behaviour here would be for Angular's HTML5-mode to provide an
-  // option to turn link hijacking off by default (and use a `target` attribute
-  // to turn it on for selected links).
-
-
-  $stateProvider
-    .state('work-packages', {
-      url: "/projects/:projectIdentifier/work_packages?query_id",
-      abstract: true,
-      templateUrl: "/templates/work_packages.html",
-      controller: 'WorkPackagesController'
-    })
-    .state('work-packages.list', {
-      url: "",
-      controller: 'WorkPackagesListController',
-      templateUrl: "/templates/work_packages.list.html"
-    })
-    .state('work-packages.list.details', {
-      url: "/{workPackageId:[0-9]+}",
-      templateUrl: "/templates/work_packages.list.details.html",
-      controller: 'WorkPackageDetailsController'
-    });
-}]);
+      $stateProvider
+        .state('work-packages', {
+          url: "/projects/:projectIdentifier/work_packages?query_id",
+          abstract: true,
+          templateUrl: "/templates/work_packages.html",
+          controller: 'WorkPackagesController'
+        })
+        .state('work-packages.list', {
+          url: "",
+          controller: 'WorkPackagesListController',
+          templateUrl: "/templates/work_packages.list.html"
+        })
+        .state('work-packages.list.details', {
+          url: "/{workPackageId:[0-9]+}",
+          templateUrl: "/templates/work_packages.list.details.html",
+          controller: 'WorkPackageDetailsController'
+        });
+    }
+  ])
+  .run([
+    '$location',
+    '$rootElement',
+    function($location, $rootElement) {
+      // Our application is still a hybrid one, meaning most routes are still
+      // handled by Rails. As such, we disable the default link-hijacking that
+      // Angular's HTML5-mode turns on.
+      $rootElement.off('click');
+    }
+  ]);