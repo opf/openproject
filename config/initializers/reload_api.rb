cf80a841afc82fbe916a31653ebbb0db367a2b9a
Marek Takac <m.takac@finn.de>
2014-06-12 14:31:53 +0200

    Fixed path in 'initializers/reload_api.rb

--- config/initializers/reload_api.rb
+++ config/initializers/reload_api.rb
@@ -1,7 +1,7 @@
 if Rails.env.development?
   ActiveSupport::Dependencies.explicitly_unloadable_constants << "API"
 
-  api_files = Dir[Rails.root.join('app', 'api', '**', '*.rb')]
+  api_files = Dir[Rails.root.join('lib', 'api', '**', '*.rb')]
   api_reloader = ActiveSupport::FileUpdateChecker.new(api_files) do
     Rails.application.reload_routes!
   end
