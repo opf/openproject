2cee6b864a1bd3e5b4ead5322d37759f9748487e
slawa <hellbrus@yahoo.com>
2014-06-13 16:48:59 +0300

    Spec tests assignee role query result

--- spec/models/query/results_spec.rb
+++ spec/models/query/results_spec.rb
@@ -44,4 +44,50 @@ describe ::Query::Results do
       end
     end
   end
+
+  describe '#work_packages' do
+    let!(:project_1) { FactoryGirl.create :project }
+    let!(:project_2) { FactoryGirl.create :project }
+    let!(:role_pm) { FactoryGirl.create(:role, :name => 'Manager', :permissions => [:view_work_packages, :edit_work_packages, :create_work_packages, :delete_work_packages])}
+    let!(:role_dev) { FactoryGirl.create(:role, :name => 'Developer', :permissions => [:view_work_packages])}
+    let!(:user_1) { FactoryGirl.create(:user, :member_in_project => project_1,
+                                       :member_through_role => [role_dev, role_pm]) }
+    let!(:member) { FactoryGirl.create(:member,
+                                       project: project_2,
+                                       principal: user_1,
+                                       roles: [role_pm]) }
+    let!(:user_2) { FactoryGirl.create(:user, :member_in_project => project_2,
+                                       :member_through_role => role_dev) }
+
+    let!(:wp_p1) {(1..3).collect { FactoryGirl.create(:work_package, :project=> project_1, assigned_to_id: user_1.id) }}
+    let!(:wp_p2) { FactoryGirl.create(:work_package, :project => project_2, assigned_to_id: user_2.id) }
+    let!(:wp2_p2) { FactoryGirl.create(:work_package, :project => project_2, assigned_to_id: user_1.id) }
+
+    before do
+      User.stub(:current).and_return(user_2)
+      query.add_filter("assigned_to_role", "=", ["#{role_dev.id}"])
+      project_2.descendants.stub(:active).and_return([])
+    end
+    context 'when a project is seted' do
+      before do
+        query.stub(:project).and_return(project_2)
+        query.stub(:project_id).and_return(project_2.id)
+      end
+
+      it 'should display only wp for selected project and selected role' do
+        expect(query_results.work_packages).to match_array([wp_p2])
+      end
+    end
+
+    context 'when no project is seted' do
+      before do
+        query.stub(:project_id).and_return(false)
+        query.stub(:project).and_return(false)
+      end
+      it 'should display all wp from projects where User.current has access' do
+        expect(query_results.work_packages).to match_array([wp_p2, wp2_p2])
+      end
+    end
+
+  end
 end
