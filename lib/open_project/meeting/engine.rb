d639caafaa9767a369f492fb6d2157e5ad41efe7
Christian Ratz <c.ratz@finn.de>
2014-06-27 15:38:06 +0200

    Bump version to corresponding core version

--- lib/open_project/meeting/engine.rb
+++ lib/open_project/meeting/engine.rb
@@ -30,7 +30,7 @@ module OpenProject::Meeting
 
     register 'openproject-meeting',
              :author_url => 'http://finn.de',
-             :requires_openproject => '>= 3.0.4' do
+             :requires_openproject => '>= 4.0.0' do
 
       project_module :meetings do
         permission :create_meetings, {:meetings => [:new, :create, :copy]}, :require => :member
--- lib/open_project/meeting/version.rb
+++ lib/open_project/meeting/version.rb
@@ -20,6 +20,6 @@
 
 module OpenProject
   module Meeting
-    VERSION = "3.0.8"
+    VERSION = "4.0.0"
   end
 end
