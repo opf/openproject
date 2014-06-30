9ee5507edc234f4ee6b78aeaa137149d90aa3d49
Till Breuer <till.breuer@gmx.de>
2014-06-30 14:33:38 +0200

    Add copyright headers an some documentation to ng query menu items

--- app/assets/javascripts/angular/layout/query-menu-items.js
+++ app/assets/javascripts/angular/layout/query-menu-items.js
@@ -1,7 +1,43 @@
+//-- copyright
+// OpenProject is a project management system.
+// Copyright (C) 2012-2014 the OpenProject Foundation (OPF)
+//
+// This program is free software; you can redistribute it and/or
+// modify it under the terms of the GNU General Public License version 3.
+//
+// OpenProject is a fork of ChiliProject, which is a fork of Redmine. The copyright follows:
+// Copyright (C) 2006-2013 Jean-Philippe Lang
+// Copyright (C) 2010-2013 the ChiliProject Team
+//
+// This program is free software; you can redistribute it and/or
+// modify it under the terms of the GNU General Public License
+// as published by the Free Software Foundation; either version 2
+// of the License, or (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program; if not, write to the Free Software
+// Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+//
+// See doc/COPYRIGHT.rdoc for more details.
+//++
+
 angular.module('openproject.layout')
 
 .constant('QUERY_MENU_ITEM_TYPE', 'query-menu-item')
 
+/**
+ * queryMenuItemFactory
+ *
+ * Defines a menu item factory for query menu items, which is set up providing type,
+ * container and a link function. The link function makes the item sensitive to its
+ * selected state and provides an event-based way to destroy the menu item, signalled
+ * by the 'openproject.layout.removeMenuItem' event.
+ */
 .factory('queryMenuItemFactory', [
   'menuItemFactory',
   '$stateParams',
@@ -36,6 +72,14 @@ angular.module('openproject.layout')
   });
 }])
 
+/**
+ * queryMenuItem directive
+ *
+ * Patches query menu items generated on the server-side by applying the link function provided
+ * by the queryMenuItemFactory.
+ * The link function makes the query menu item's 'selected' class reflect the application state
+ * and provides an event-based mechanism to remove the item on the fly.
+ */
 .directive('queryMenuItem', [
   'queryMenuItemFactory',
   function(queryMenuItemFactory) {
