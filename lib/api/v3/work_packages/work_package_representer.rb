87809c19163d1caf065e981fdda85412e60be4a9
Marek Takac <m.takac@finn.de>
2014-06-30 16:52:21 +0200

    Added embedded activities to the work package resource

--- lib/api/v3/work_packages/work_package_representer.rb
+++ lib/api/v3/work_packages/work_package_representer.rb
@@ -40,6 +40,11 @@ module API
 
         self.as_strategy = API::Utilities::CamelCasingStrategy.new
 
+        def initialize(options = {}, *expand)
+            @expand = expand
+            super(options)
+        end
+
         property :_type, exec_context: :decorator
 
         link :self do
@@ -74,9 +79,17 @@ module API
         property :created_at, getter: -> (*) { work_package.created_at.utc.iso8601}, render_nil: true
         property :updated_at, getter: -> (*) { work_package.updated_at.utc.iso8601}, render_nil: true
 
+        collection :activities, embedded: true, class: Activities::ActivityModel, decorator: Activities::ActivityRepresenter
+
         def _type
-          "WorkPackage"
+          'WorkPackage'
         end
+
+        private
+
+          def default_url_options
+            ActionController::Base.default_url_options
+          end
       end
     end
   end
