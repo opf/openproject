b35572142bcb776abf895aa42d202d4afcf9fd16
Marek Takac <m.takac@finn.de>
2014-07-04 10:57:00 +0200

    Just moved some stuff around

--- lib/api/v3/work_packages/work_package_representer.rb
+++ lib/api/v3/work_packages/work_package_representer.rb
@@ -55,21 +55,24 @@ module API
             {
                 href: "#{root_url}/api/v3/users/#{represented.work_package.author.id}",
                 title: "#{represented.work_package.author.name} - #{represented.work_package.author.login}"
-            } unless represented.work_package.author.nil?
+            }
+            unless represented.work_package.author.nil?
         end
 
         link :responsible do
             {
                 href: "#{root_url}/api/v3/users/#{represented.work_package.responsible.id}",
                 title: "#{represented.work_package.responsible.name} - #{represented.work_package.responsible.login}"
-            } unless represented.work_package.responsible.nil?
+            }
+            unless represented.work_package.responsible.nil?
         end
 
         link :assignee do
             {
                 href: "#{root_url}/api/v3/users/#{represented.work_package.assigned_to.id}",
                 title: "#{represented.work_package.assigned_to.name} - #{represented.work_package.assigned_to.login}"
-            } unless represented.work_package.assigned_to.nil?
+            }
+            unless represented.work_package.assigned_to.nil?
         end
 
         property :id, getter: -> (*) { work_package.id }, render_nil: true
