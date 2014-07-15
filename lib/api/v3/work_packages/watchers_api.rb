db373fc8bb001340d48e74f8d17b0a0cd0f1ea57
Till Breuer <till.breuer@gmx.de>
2014-07-15 12:47:51 +0200

    Implement API watchers#delete end point

--- lib/api/v3/work_packages/watchers_api.rb
+++ lib/api/v3/work_packages/watchers_api.rb
@@ -3,22 +3,21 @@ module API
     module WorkPackages
       class WatchersAPI < Grape::API
 
+
         resources :watchers do
           params do
             requires :user_id, desc: 'Id of the user watching the work package'
           end
 
-          before do
-            @user = User.find params[:user_id]
-          end
-
           post do
             authorize(:add_work_package_watchers, context: @work_package.project)
 
-            if @work_package.watcher_users.include?(@user)
+            user = User.find params[:user_id]
+
+            if @work_package.watcher_users.include?(user)
               status 200
             else
-              watcher = Watcher.new(user: @user, watchable: @work_package)
+              watcher = Watcher.new(user: user, watchable: @work_package)
 
               if watcher.valid?
                 @work_package.watchers << watcher
@@ -27,8 +26,25 @@ module API
               end
             end
 
-            model = ::API::V3::Users::UserModel.new(@user)
+            model = ::API::V3::Users::UserModel.new(user)
             @representer = ::API::V3::Users::UserRepresenter.new(model).to_json
+          end
+
+          params do
+            requires :user_id, desc: 'Id of the user watching the work package'
+          end
+          namespace ':user_id' do
+            delete do
+              authorize(:delete_work_package_watchers, context: @work_package.project)
+
+              user = User.find_by_id params[:user_id]
+
+              if @work_package.watcher_users.include?(user)
+                @work_package.watcher_users.delete(user)
+              end
+
+              status 204
+            end
           end
 
         end
