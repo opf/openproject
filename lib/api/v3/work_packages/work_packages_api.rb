1d335b51bbd965b14374e29034e26f70279500f7
Hagen Schink <h.schink@finn.de>
2014-07-16 16:23:20 +0200

    Fix indentation and spacing

--- lib/api/v3/work_packages/work_packages_api.rb
+++ lib/api/v3/work_packages/work_packages_api.rb
@@ -13,7 +13,7 @@ module API
             before do
               @work_package = WorkPackage.find(params[:id])
               model = ::API::V3::WorkPackages::WorkPackageModel.new(work_package: @work_package)
-              @representer =  ::API::V3::WorkPackages::WorkPackageRepresenter.new(model, :activities, :users)
+              @representer = ::API::V3::WorkPackages::WorkPackageRepresenter.new(model, :activities, :users)
             end
 
             get do
