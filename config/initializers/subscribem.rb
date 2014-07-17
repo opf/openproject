e26fcd8e87e14e5c1e9b6efcba4118f6a66824db
Michael Frister <michael+git@frister.net>
2014-07-17 11:05:36 +0200

    Add default excluded domains for subscribem

--- config/initializers/subscribem.rb
+++ config/initializers/subscribem.rb
@@ -3,4 +3,6 @@ Subscribem.configure do |c|
   c.default_data_loader  = lambda { Redmine::DefaultData::Loader.load }
   c.settings_class       = 'Setting'
   c.host                 = ENV.fetch('HOST_NAME') { 'openproject-demo.org' }
+  c.excluded_domains     = ENV.fetch('SUBSCRIBEM_EXCLUDED_DOMAINS',
+                                     'localhost openproject.dev').split(' ')
 end if defined? Subscribem
