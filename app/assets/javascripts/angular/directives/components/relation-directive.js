3d7ae83f8747cd46af358945982492cc7c57a334
Hagen Schink <h.schink@finn.de>
2014-07-17 16:03:46 +0200

    Add WP relation directive

---
+++ app/assets/javascripts/angular/directives/components/relation-directive.js
@@ -0,0 +1,74 @@
+//-- copyright
+// OpenProject is a project management system.
+// Copyright (C) 2012-2014 the OpenProject Foundation (OPF)
+//
+// This program is free software; you can redistribute it and/or
+// modify it under the terms of the GNU General Public License version 3.
+//
+// OpenProject is a fork of ChiliProject, which is a fork of Redmine. The copyright follows:
+// Copyright (C) 2006-2013 Jean-Philippe Lang
+// Copyright (C) 2010-2013 the ChiliProject Team
+//
+// This program is free software; you can redistribute it and/or
+// modify it under the terms of the GNU General Public License
+// as published by the Free Software Foundation; either version 2
+// of the License, or (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program; if not, write to the Free Software
+// Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+//
+// See doc/COPYRIGHT.rdoc for more details.
+//++
+
+// TODO move to UI components
+angular.module('openproject.uiComponents')
+
+.directive('workPackageRelation', ['I18n', function(I18n) {
+  return {
+    restrict: 'E',
+    replace: true,
+    scope: { title: '@', relations: '=', btnTitle: '@buttonTitle', btnIcon: '@buttonIcon' },
+    templateUrl: '/templates/components/work_package_relation.html',
+    link: function(scope, element, attrs) {
+      scope.I18n = I18n;
+
+      scope.collapseStateIcon = function(collapsed) {
+        var iconClass = 'icon-arrow-right5-';
+
+        if (collapsed) {
+          iconClass += '3';
+        } else {
+          iconClass += '2';
+        }
+
+        return iconClass;
+      }
+
+      scope.getRelations = function() {
+        if (Array.isArray(scope.relations)) {
+          return scope.relations;
+        } else {
+          return [scope.relations];
+        }
+      }
+
+      scope.relationsExist = function() {
+        return scope.relations
+               && ((Array.isArray(scope.relations) && scope.relations.length > 0)
+                   || !Array.isArray(scope.relations));
+      }
+
+      scope.isSingleRelation = function() {
+        return scope.relations && !Array.isArray(scope.relations)
+      }
+
+      scope.expand = scope.relationsExist() && (scope.isSingleRelation() || scope.relations.length > 0);
+    }
+  };
+}]);
