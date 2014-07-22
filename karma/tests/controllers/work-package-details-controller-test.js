45fae8f53d9b3a8aac2f84f0b40e0ad91bb8b417
Till Breuer <till.breuer@gmx.de>
2014-07-22 11:17:42 +0200

    Split up work package details controller

--- karma/tests/controllers/work-package-details-controller-test.js
+++ karma/tests/controllers/work-package-details-controller-test.js
@@ -32,22 +32,10 @@ describe('WorkPackageDetailsController', function() {
   var scope;
   var buildController;
   var I18n = { t: angular.identity },
-      WorkPackagesHelper = {
-        formatWorkPackageProperty: angular.identity
-      },
-      UserService = {
-        getUser: angular.identity
-      },
-      CustomFieldHelper = {
-        formatCustomFieldValue: angular.identity
-      },
       workPackage = {
         props: {
           status: 'open',
           versionName: null,
-          customProperties: [
-            { format: 'text', name: 'color', value: 'red' },
-          ]
         },
         embedded: {
           activities: [],
@@ -72,7 +60,7 @@ describe('WorkPackageDetailsController', function() {
   }
 
   beforeEach(module('openproject.api', 'openproject.services', 'openproject.workPackages.controllers'));
-  beforeEach(inject(function($rootScope, $controller, $timeout) {
+  beforeEach(inject(function($rootScope, $controller) {
     var workPackageId = 99;
 
     buildController = function() {
@@ -88,15 +76,11 @@ describe('WorkPackageDetailsController', function() {
             return false;
           }
         },
-        UserService: UserService,
-        CustomFieldHelper: CustomFieldHelper,
         WorkPackagesDetailsHelper: {
           attachmentsTitle: function() { return ''; }
         },
         workPackage: buildWorkPackageWithId(workPackageId),
       });
-
-      $timeout.flush();
     };
 
   }));
@@ -106,210 +90,5 @@ describe('WorkPackageDetailsController', function() {
       buildController();
     });
   });
-
-  describe('work package properties', function() {
-    function fetchPresentPropertiesWithName(propertyName) {
-      return scope.presentWorkPackageProperties.filter(function(propertyData) {
-        return propertyData.property === propertyName;
-      });
-    }
-
-    describe('when the property has a value', function() {
-      var propertyName = 'status';
-
-      beforeEach(function() {
-        buildController();
-      });
-
-      it('adds properties to present properties', function() {
-        expect(fetchPresentPropertiesWithName(propertyName)).to.have.length(1);
-      });
-    });
-
-    describe('when the property is among the first 3 properties', function() {
-      var propertyName = 'responsible';
-
-      beforeEach(function() {
-        buildController();
-      });
-
-      it('is added to present properties even if it is empty', function() {
-        expect(fetchPresentPropertiesWithName(propertyName)).to.have.length(1);
-      });
-    });
-
-    describe('when the property is among the second group of 3 properties', function() {
-      var propertyName = 'priority',
-          label        = 'Priority';
-
-      beforeEach(function() {
-        sinon.stub(I18n, 't')
-             .withArgs('js.work_packages.properties.' + propertyName)
-             .returns(label);
-
-        buildController();
-      });
-
-      afterEach(function() {
-        I18n.t.restore();
-      });
-
-      describe('and none of these 3 properties is present', function() {
-        beforeEach(function() {
-          buildController();
-        });
-
-        it('is added to the empty properties', function() {
-          expect(scope.emptyWorkPackageProperties.indexOf(label)).to.be.greaterThan(-1);
-        });
-      });
-
-      describe('and at least one of these 3 properties is present', function() {
-        beforeEach(function() {
-          workPackage.props.percentageDone = '20';
-          buildController();
-        });
-
-        it('is added to the present properties', function() {
-          expect(fetchPresentPropertiesWithName(propertyName)).to.have.length(1);
-        });
-      });
-    });
-
-    describe('when the property is not among the first 6 properties', function() {
-      var propertyName = 'versionName',
-          label        = 'Version';
-
-      beforeEach(function() {
-        sinon.stub(I18n, 't')
-             .withArgs('js.work_packages.properties.' + propertyName)
-             .returns(label);
-
-        buildController();
-      });
-
-      afterEach(function() {
-        I18n.t.restore();
-      });
-
-      it('adds properties that without values to empty properties', function() {
-        expect(scope.emptyWorkPackageProperties.indexOf(label)).to.be.greaterThan(-1);
-      });
-    });
-
-    describe('date property', function() {
-      var startDate = '2014-07-09',
-          dueDate   = '2014-07-10',
-          placeholder = 'placeholder';
-
-
-      describe('when only the due date is present', function() {
-        beforeEach(function() {
-          sinon.stub(I18n, 't')
-               .withArgs('js.label_no_start_date')
-               .returns(placeholder);
-
-          workPackage.props.startDate = null;
-          workPackage.props.dueDate = dueDate;
-
-          buildController();
-        });
-
-        afterEach(function() {
-          I18n.t.restore();
-        });
-
-        it('renders the due date and a placeholder for the start date as date property', function() {
-          expect(fetchPresentPropertiesWithName('date')[0].value).to.equal(placeholder + ' - Jul 10, 2014');
-        });
-      });
-
-      describe('when only the start date is present', function() {
-        beforeEach(function() {
-          sinon.stub(I18n, 't')
-               .withArgs('js.label_no_due_date')
-               .returns(placeholder);
-
-          workPackage.props.startDate = startDate;
-          workPackage.props.dueDate = null;
-
-          buildController();
-        });
-
-        afterEach(function() {
-          I18n.t.restore();
-        });
-
-        it('renders the start date and a placeholder for the due date as date property', function() {
-          expect(fetchPresentPropertiesWithName('date')[0].value).to.equal('Jul 9, 2014 - ' + placeholder);
-        });
-      });
-
-      describe('when both - start and due date are present', function() {
-        beforeEach(function() {
-          workPackage.props.startDate = startDate;
-          workPackage.props.dueDate = dueDate;
-
-          buildController();
-        });
-
-        it('combines them and renders them as date property', function() {
-          expect(fetchPresentPropertiesWithName('date')[0].value).to.equal('Jul 9, 2014 - Jul 10, 2014');
-        });
-      });
-    });
-
-    describe('custom field properties', function() {
-      var customPropertyName = 'color';
-
-      describe('when the property has a value', function() {
-        beforeEach(function() {
-          formatCustomFieldValueSpy = sinon.spy(CustomFieldHelper, 'formatCustomFieldValue');
-
-          buildController();
-        });
-
-        afterEach(function() {
-          CustomFieldHelper.formatCustomFieldValue.restore();
-        });
-
-        it('adds properties to present properties', function() {
-          expect(fetchPresentPropertiesWithName(customPropertyName)).to.have.length(1);
-        });
-
-        it('formats values using the custom field helper', function() {
-          expect(CustomFieldHelper.formatCustomFieldValue.calledWith('red', 'text')).to.be.true;
-        });
-      });
-
-      describe('when the property does not have a value', function() {
-        beforeEach(function() {
-          workPackage.props.customProperties[0].value = null;
-          buildController();
-        });
-
-        it('adds the custom property to empty properties', function() {
-          expect(scope.emptyWorkPackageProperties.indexOf(customPropertyName)).to.be.greaterThan(-1);
-        });
-      });
-
-      describe('user custom property', function() {
-        var userId = '1';
-
-        beforeEach(function() {
-          workPackage.props.customProperties[0].value = userId;
-          workPackage.props.customProperties[0].format = 'user';
-
-          getUserSpy = sinon.spy(UserService, 'getUser');
-          buildController();
-        });
-
-        it('fetches the user using the user service', function() {
-          expect(UserService.getUser.calledWith(userId)).to.be.true;
-        });
-      });
-    });
-  });
-
 
 });
