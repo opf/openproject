8a634a3655a17efc6f1032f5950c96652059a210
Jens Ulferts <j.ulferts@finn.de>
2014-07-25 13:29:10 +0200

    fixes loading of plugin code

--- lib/open_project/plugins.rb
+++ lib/open_project/plugins.rb
@@ -28,6 +28,8 @@
 
 module OpenProject
   module Plugins
-    require 'open_project/plugins/engine'
+    require 'open_project/plugins/patch_registry'
+    require 'open_project/plugins/load_dependency'
+    require 'open_project/plugins/acts_as_op_engine'
   end
 end
