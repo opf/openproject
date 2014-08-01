ec083f53747058ab1026c4b3bcf1bbc24b386b02
slawa <hellbrus@yahoo.com>
2014-08-01 17:26:04 +0300

    Send mail to all meeting participants

--- app/mailers/meeting_mailer.rb
+++ app/mailers/meeting_mailer.rb
@@ -25,13 +25,15 @@ class MeetingMailer < UserMailer
     @meeting_url = meeting_url @meeting
     @project_url = project_url @meeting.project
     @content_type = content_type
-
     open_project_headers 'Project' => @meeting.project.identifier,
                          'Meeting-Id' => @meeting.id
 
-    recipients = @meeting.watcher_recipients.reject{|r| r == @meeting.author.mail}
+    recipients = @meeting.participants.collect{|p| p.mail}.reject{|r| r == @meeting.author.mail}
+    recipients << @meeting.author.mail if @meeting.author.preference[:no_self_notified]
 
     subject = "[#{@meeting.project.name}] #{I18n.t(:"label_#{content_type}")}: #{@meeting.title}"
-    mail :to => @meeting.author.mail, :cc => recipients, :subject => subject
+    recipients.each do |address|
+      mail to: address, subject: subject
+    end
   end
 end
