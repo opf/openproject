88957865b492754d20a60bdfaf54a3ca6f94d416
Hagen Schink <h.schink@finn.de>
2014-08-04 15:30:53 +0200

    Add relations dependency object

---
+++ app/assets/javascripts/angular/work_packages/models/relations-handler.js
@@ -0,0 +1,94 @@
+//-- copyright
+// OpenProject is a project management system.
+// Copyright (C) 2012-2014 the OpenProject Foundation (OPF)
+//
+// This program is free software; you can redistribute it and/or
+// modify it under the terms of the GNU General Public License version 3.
+//
+// OpenProject is a fork of ChiliProject, which is a fork of Redmine. The copyright follows:
+// Copyright (C) 2006-2013 Jean-Philippe Lang
+// Copyright (C) 2010-2013 the ChiliProject Team
+//
+// This program is free software; you can redistribute it and/or
+// modify it under the terms of the GNU General Public License
+// as published by the Free Software Foundation; either version 2
+// of the License, or (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program; if not, write to the Free Software
+// Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+//
+// See doc/COPYRIGHT.rdoc for more details.
+//++
+
+angular.module('openproject.models')
+
+.factory('CommonRelationsHandler', [
+    '$timeout',
+    'WorkPackageService',
+    'ApiHelper',
+    function($timeout, WorkPackageService, ApiHelper) {
+  function CommonRelationsHandler(workPackage,
+                                  relations,
+                                  relationsId) {
+    this.workPackage = workPackage;
+    this.relations = relations;
+    this.relationsId = relationsId;
+  }
+
+  CommonRelationsHandler.prototype = {
+    isEmpty: function() {
+      return !this.relations || this.relations.length === 0;
+    },
+
+    getCount: function() {
+      return (this.relations) ? this.relations.length : 0;
+    },
+
+    canAddRelation: function() {
+      return !!this.workPackage.links.addRelation;
+    },
+
+    addRelation: function(scope) {
+      var inputElement = angular.element('#relation_to_id-' + this.relationsId);
+      var toId = inputElement.val();
+      WorkPackageService.addWorkPackageRelation(this.workPackage, toId, this.relationsId).then(function(relation) {
+          inputElement.val('');
+          scope.$emit('workPackageRefreshRequired', '');
+      }, function(error) {
+        ApiHelper.handleError(scope, error);
+      });
+    },
+
+    applyCustomExtensions: function() {
+      // Massive hack alert - Using old prototype autocomplete ///////////
+      if(this.canAddRelation) {
+        var workPackage = this.workPackage;
+        var relationsId = this.relationsId;
+
+        $timeout(function() {
+          var url = PathHelper.workPackageAutoCompletePath(workPackage.props.projectId, workPackage.props.id);
+          new Ajax.Autocompleter('relation_to_id-' + relationsId,
+                                 'related_issue_candidates-' + relationsId,
+                                 url,
+                                 { minChars: 1,
+                                   frequency: 0.5,
+                                   paramName: 'q',
+                                   updateElement: function(value) {
+                                     document.getElementById('relation_to_id-' + relationsId).value = value.id;
+                                   },
+                                   parameters: 'scope=all'
+                                   });
+        });
+      }
+      ////////////////////////////////////////////////////////////////////
+    }
+  };
+
+  return CommonRelationsHandler;
+}]);
