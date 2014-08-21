907566af8840ad0f50ab47509ba6581f151ae13c
Michael Frister <michael+git@frister.net>
2014-08-21 17:29:20 +0200

    API v1: Add note about removal to 410 response

--- config/routes.rb
+++ config/routes.rb
@@ -65,7 +65,12 @@ OpenProject::Application.routes.draw do
     # This does not care if the route actually existed to
     # avoid maintaining knowledge of the now removed api.
     match '/v1/*rest', via: [:get, :post, :put, :delete],
-                       to: proc { [410, {}, ['']] }
+                       to: proc {
+                         [410,
+                          { 'Content-Type' => 'text/plain' },
+                          ["OpenProject API v1 has been removed.\n" \
+                           'See https://www.openproject.org/news/65']]
+                       }
 
     namespace :v2 do
 
