52a9bbf71afae74efed4d88cc5ec8a062c58df7a
Hagen Schink <h.schink@finn.de>
2014-09-02 09:00:53 +0200

    Remove superfluous scope

--- app/assets/javascripts/angular/directives/components/empty-element-directive.js
+++ app/assets/javascripts/angular/directives/components/empty-element-directive.js
@@ -3,7 +3,6 @@ angular.module('openproject.uiComponents')
 .directive('emptyElement', [function() {
   return {
     restrict: 'E',
-    scope: { execute: '&' },
     templateUrl: "/templates/components/empty_element.html"
   };
 }]);
