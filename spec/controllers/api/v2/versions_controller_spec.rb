ac7870f185e0ffeae92192c70e0c08c213fec67d
Hagen Schink <h.schink@finn.de>
2014-09-03 08:43:12 +0200

    Add specs for API v2 version index

--- spec/controllers/api/v2/versions_controller_spec.rb
+++ spec/controllers/api/v2/versions_controller_spec.rb
@@ -29,31 +29,77 @@
 require 'spec_helper'
 
 describe Api::V2::VersionsController, type: :controller do
-
+  let(:project) { FactoryGirl.create(:project) }
+  let(:version) { FactoryGirl.create(:version, project: project) }
   let(:admin_user) { FactoryGirl.create(:admin) }
 
+  shared_examples_for 'unauthorized access' do
+    let(:project) { FactoryGirl.create(:project) }
+
+    before { get action, request_params }
+
+    it { expect(response.status).to eq(401) }
+  end
+
   describe '#index' do
-    let(:version) { FactoryGirl.create(:version) }
-
-    context 'unauthorized access' do
-      let(:project) { FactoryGirl.create(:project) }
-
-      before { get :index, project_id: project.id, format: :xml }
-
-      it { expect(response.status).to eq(401) }
+    it_behaves_like 'unauthorized access' do
+      let(:action) { :index }
+      let(:request_params) { { project_id: project.id, format: :xml } }
     end
 
     context 'with access' do
-      let(:project) { FactoryGirl.create(:project) }
+      let!(:version) { FactoryGirl.create(:version, project: project) }
 
-      before do
-        allow(User).to receive(:current).and_return admin_user
+      before { allow(User).to receive(:current).and_return admin_user }
+
+      describe 'single project' do
+        before { get :index, project_id: project.id, format: :xml }
+
+        it { expect(assigns(:project)).to eq(project) }
+
+        it { expect(assigns(:projects)).to be_nil }
+
+        it { expect(assigns(:versions)).to include(version) }
       end
 
-      before { get :index, project_id: project.id, format: :xml }
+      describe 'multiple projects' do
+        let(:project_2) { FactoryGirl.create(:project) }
+        let!(:version_2) { FactoryGirl.create(:version, project: project_2) }
 
-      it { expect(response.status).to eq(200) }
-      xit 'should render something'
+        shared_examples_for 'request with multiple projects' do
+          before do
+            get :index,
+                project_id: projects.collect(&:id).join(','),
+                format: :xml
+          end
+
+          it { expect(assigns(:project)).to be_nil }
+
+          it { expect(assigns(:projects)).to match_array(expected_projects) }
+
+          it { expect(assigns(:versions)).to match_array(expected_versions) }
+        end
+
+        context 'user has access to all projects' do
+          it_behaves_like 'request with multiple projects' do
+            let(:projects) { [project, project_2] }
+            let(:expected_projects) { projects }
+            let(:expected_versions) { [version, version_2] }
+          end
+        end
+
+        context 'user has access only to one project' do
+          let(:user) { FactoryGirl.create(:user, member_in_project: project) }
+
+          before { allow(User).to receive(:current).and_return user }
+
+          it_behaves_like 'request with multiple projects' do
+            let(:projects) { [project, project_2] }
+            let(:expected_projects) { [project] }
+            let(:expected_versions) { [version] }
+          end
+        end
+      end
     end
   end
 
