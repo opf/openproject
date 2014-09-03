7a84d371f22e3b999a79e930b8d8036414cae258
Hagen Schink <h.schink@finn.de>
2014-09-03 08:38:02 +0200

    Use simple require for spec helper

--- spec/controllers/api/v2/versions_controller_spec.rb
+++ spec/controllers/api/v2/versions_controller_spec.rb
@@ -26,7 +26,7 @@
 # See doc/COPYRIGHT.rdoc for more details.
 #++
 
-require File.expand_path('../../../../spec_helper', __FILE__)
+require 'spec_helper'
 
 describe Api::V2::VersionsController, type: :controller do
 
