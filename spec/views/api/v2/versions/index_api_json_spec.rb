e31ccf6dfbca71dd9f72d8985cdb28c43a40182e
Hagen Schink <h.schink@finn.de>
2014-09-04 09:51:39 +0200

    Use simple require for spec helper

--- spec/views/api/v2/versions/index_api_json_spec.rb
+++ spec/views/api/v2/versions/index_api_json_spec.rb
@@ -26,7 +26,7 @@
 # See doc/COPYRIGHT.rdoc for more details.
 #++
 
-require File.expand_path('../../../../../spec_helper', __FILE__)
+require 'spec_helper'
 
 describe 'api/v2/versions/index.api.rabl', :type => :view do
   let(:shared_with) { [42, 1, 2, 3] }
