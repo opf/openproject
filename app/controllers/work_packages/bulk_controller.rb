94877253350a72056fa011f0c2aa39c06e08c60e
slawa <hellbrus@yahoo.com>
2014-10-02 14:51:22 +0300

    Move functionality to work package service

--- app/controllers/work_packages/bulk_controller.rb
+++ app/controllers/work_packages/bulk_controller.rb
@@ -39,34 +39,18 @@ class WorkPackages::BulkController < ApplicationController
   include IssuesHelper
 
   def edit
-    @work_packages.sort!
-    @available_statuses = @projects.map{|p|Workflow.available_statuses(p)}.inject{|memo,w|memo & w}
-    @custom_fields = @projects.map{|p|p.all_work_package_custom_fields}.inject{|memo,c|memo & c}
-    @assignables = @projects.map(&:possible_assignees).inject{|memo,a| memo & a}
-    @responsibles = @projects.map(&:possible_responsibles).inject{|memo,a| memo & a}
-    @types = @projects.map(&:types).inject{|memo,t| memo & t}
+    WorkPackageBulkUpdateService.new(@work_packages, @projects).run(params)
+    .available_attributes.each do |key, value|
+      self.instance_variable_set(:"@#{key}", value)
+    end
   end
 
   def update
-    @work_packages.sort!
-
-    unsaved_work_package_ids = []
-
-    @work_packages.each do |work_package|
-      work_package.reload
-      work_package.add_journal(User.current, params[:notes])
-
-      # filter parameters by whitelist and add defaults
-      attributes = parse_params_for_bulk_work_package_attributes params, work_package.project
-      work_package.assign_attributes attributes
-
-      call_hook(:controller_work_package_bulk_before_save, { params: params, work_package: work_package })
-      JournalObserver.instance.send_notification = params[:send_notification] == '0' ? false : true
-      unless work_package.save
-        unsaved_work_package_ids << work_package.id
-      end
+    WorkPackageBulkUpdateService.new(@work_packages, @projects).save(params).each do |key, value|
+      self.instance_variable_set(:"@#{key}", value)
     end
-    set_flash_from_bulk_save(@work_packages, unsaved_work_package_ids)
+
+    set_flash_from_bulk_save(@work_packages, @unsaved_work_package_ids)
     redirect_back_or_default({controller: '/work_packages', action: :index, project_id: @project}, false)
   end
 
@@ -91,6 +75,21 @@ class WorkPackages::BulkController < ApplicationController
     end
   end
 
+  # Sets the `flash` notice or error based the number of work packages that did not save
+  #
+  # @param [Array, WorkPackage] work_packages all of the saved and unsaved WorkPackages
+  # @param [Array, Integer] unsaved_work_package_ids the WorkPackage ids that were not saved
+  def set_flash_from_bulk_save(work_packages, unsaved_work_package_ids)
+    if unsaved_work_package_ids.empty?
+      flash[:notice] = l(:notice_successful_update) unless work_packages.empty?
+    else
+      flash[:error] = l(:notice_failed_to_save_work_packages,
+                        count: unsaved_work_package_ids.size,
+                        total: work_packages.size,
+                        ids: '#' + unsaved_work_package_ids.join(', #'))
+    end
+  end
+
 private
 
   def destroy_work_packages(work_packages)
@@ -101,32 +100,6 @@ private
         # raised by #reload if work package no longer exists
         # nothing to do, work package was already deleted (eg. by a parent)
       end
-    end
-  end
-
-  def parse_params_for_bulk_work_package_attributes(params, project)
-    return {} unless params.has_key? :work_package
-
-    safe_params = permitted_params.update_work_package project: project
-    attributes = safe_params.reject {|k,v| v.blank?}
-    attributes.keys.each {|k| attributes[k] = '' if attributes[k] == 'none'}
-    attributes[:custom_field_values].reject! {|k,v| v.blank?} if attributes[:custom_field_values]
-    attributes.delete :custom_field_values if not attributes.has_key?(:custom_field_values) or attributes[:custom_field_values].empty?
-    attributes
-  end
-
-  # Sets the `flash` notice or error based the number of work packages that did not save
-  #
-  # @param [Array, WorkPackage] work_packages all of the saved and unsaved WorkPackages
-  # @param [Array, Integer] unsaved_work_package_ids the WorkPackage ids that were not saved
-  def set_flash_from_bulk_save(work_packages, unsaved_work_package_ids)
-    if unsaved_work_package_ids.empty?
-      flash[:notice] = l(:notice_successful_update) unless work_packages.empty?
-    else
-      flash[:error] = l(:notice_failed_to_save_work_packages,
-                        :count => unsaved_work_package_ids.size,
-                        :total => work_packages.size,
-                        :ids => '#' + unsaved_work_package_ids.join(', #'))
     end
   end
 
