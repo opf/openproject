89969c103f44f71037c97d146b70d09fd9e93c64
Christian Ratz <c.ratz@finn.de>
2014-10-07 15:19:08 +0200

    Bump version to 3.0.14

--- lib/open_project/version.rb
+++ lib/open_project/version.rb
@@ -34,7 +34,7 @@ module OpenProject
 
     MAJOR = 3
     MINOR = 0
-    PATCH = 13
+    PATCH = 14
     TINY  = PATCH # Redmine compat
 
     # Used by semver to define the special version (if any).
