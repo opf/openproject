e94b40385447fa2d4349ec6ea03086eac5c8c2bc
Martin Linkhorst <m.linkhorst@gmail.com>
2014-10-17 15:57:50 +0200

    Revert "attempt to test against a real tenant"
    This reverts commit 907e317c1f84fb4bd45028c766fe375ff85189da.

--- features/support/env.rb
+++ features/support/env.rb
@@ -36,7 +36,6 @@
 require 'cucumber/rails'
 require 'cucumber/rspec/doubles'
 require 'capybara-screenshot/cucumber'
-require 'subscribem/test_helper'
 
 # json-spec is used to specifiy our json-apis
 require "json_spec/cucumber"
@@ -61,7 +60,7 @@ Capybara.configure do |config|
     config.visible_text_only = true
     # Set App host to not contain subdomain for multitenancy.
     # Multitenancy would try to choose another schema when it finds a subdomain.
-    config.app_host = "http://tenant1.example.org"
+    config.app_host = "http://example.org"
 end
 
 # By default, any exception happening in your Rails application will bubble up
@@ -124,7 +123,6 @@ end
 
 # Remove any modal dialog remaining from the scenarios which finish in an unclean state
 Before do |scenario|
-  Subscribem.ensure_tenant('tenant1')
   page.driver.browser.switch_to.alert.accept rescue Selenium::WebDriver::Error::NoAlertOpenError
 end
 
