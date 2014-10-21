2568c2a2d7cb8b739e1c4522b55266dd1c784fb0
Philipp Tessenow <philipp@tessenow.org>
2014-10-21 14:10:32 +0200

    whitespace fixes and other minor things

--- spec/support/shared/rake.rb
+++ spec/support/shared/rake.rb
@@ -29,16 +29,16 @@
 # Origins from http://robots.thoughtbot.com/test-rake-tasks-like-a-boss
 # Author: Josh Clayton
 
-require "rake"
+require 'rake'
 
-shared_context "rake" do
+shared_context 'rake' do
   let(:rake)      { Rake::Application.new }
   let(:task_name) { self.class.description }
-  let(:task_path) { "lib/tasks/#{task_name.split(":").first}" }
+  let(:task_path) { "lib/tasks/#{task_name.split(':').first}" }
   subject         { rake[task_name] }
 
   def loaded_files_excluding_current_rake_file
-    $".reject {|file| file == Rails.root.join("#{task_path}.rake").to_s }
+    $".reject { |file| file == Rails.root.join("#{task_path}.rake").to_s }
   end
 
   before do
