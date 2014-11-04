25ede2df34d534d550c0889cad927c9b1a07121a
Alex Coles <alex@alexbcoles.com>
2014-11-04 10:57:58 +0100

    Re-run Rubocup auto-correct on pagination_spec_helper.rb
    Signed-off-by: Alex Coles <alex@alexbcoles.com>

--- spec/controllers/api/v2/pagination/pagination_spec_helper.rb
+++ spec/controllers/api/v2/pagination/pagination_spec_helper.rb
@@ -32,15 +32,17 @@ module Api
       module PaginationSpecHelper
         def paginating_index_action(model, scope)
           describe :index, type: :controller do
-            let(:params) { { "page" => "1",
-                             "page_limit" => "10",
-                             "q" => "blubs",
-                             "format" => "json" } }
+            let(:params) {
+              { 'page' => '1',
+                'page_limit' => '10',
+                'q' => 'blubs',
+                'format' => 'json' }
+            }
 
             before do
               expect(model).to receive(scope)
-                   .with(params["q"])
-                   .and_return(model)
+                .with(params['q'])
+                .and_return(model)
 
               get :index, params
             end
