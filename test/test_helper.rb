fa06867d4753bf10a6c5e26a04c00991d0c3466b
Alex Coles <alex@alexbcoles.com>
2014-11-09 16:58:57 +0100

    Revert "ugly hack to reset pk_sequences on the ci. postgres got stuck after importing fixtures.."
    This hack was causing most tests to fail on Postgres with:
    
        ActiveRecord::StatementInvalid: PG::InFailedSqlTransaction: ERROR:  current transaction is aborted, commands ignored until end of transaction block
    
    This reverts commit d8785a3f8645764e275f5507f6b6550ae3720a7b.
    
    Signed-off-by: Alex Coles <alex@alexbcoles.com>

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -83,15 +83,6 @@ class ActiveSupport::TestCase
   def setup
     super
     Rails.cache.clear
-    # Ugly H4X to convince postgresql to update its sequences after bulk loading fixtures
-    # TODO: remove this when we got rid of the fixtures
-    ActiveRecord::Base.descendants.each do |model|
-      if model.table_name \
-         and defined?(ActiveRecord::ConnectionAdapters::PostgreSQLAdapter) \
-         and ActiveRecord::Base.connection.is_a?(ActiveRecord::ConnectionAdapters::PostgreSQLAdapter)
-        ActiveRecord::Base.connection.reset_pk_sequence!(model.table_name)
-      end
-    end
 
     # initializes the mocking features
     RSpec::Mocks.setup
