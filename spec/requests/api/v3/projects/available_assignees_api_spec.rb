f4627ceb9351e6648acfdfdd1516a28279fce511
Hagen Schink <h.schink@finn.de>
2014-11-19 13:55:41 +0100

    Test available assignees/responsibles on project endpoint

--- spec/requests/api/v3/projects/available_assignees_api_spec.rb
+++ spec/requests/api/v3/projects/available_assignees_api_spec.rb
@@ -29,16 +29,16 @@
 require 'spec_helper'
 require 'rack/test'
 
-describe API::V3::WorkPackages::WorkPackagesAPI, type: :request do
+describe API::V3::Projects::ProjectsAPI, type: :request do
   let(:admin) { FactoryGirl.create(:admin) }
 
   describe 'available assignees' do
-    let(:work_package) { FactoryGirl.build_stubbed(:work_package) }
+    let(:project) { FactoryGirl.build_stubbed(:project) }
 
-    before { allow(WorkPackage).to receive(:find).and_return(work_package) }
+    before { allow(Project).to receive(:find).and_return(project) }
 
     shared_context 'request available assignees' do
-      before { get "/api/v3/work_packages/#{work_package.id}/available_assignees" }
+      before { get "/api/v3/projects/#{project.id}/available_assignees" }
     end
 
     it_behaves_like 'safeguarded API' do
@@ -60,7 +60,7 @@ describe API::V3::WorkPackages::WorkPackagesAPI, type: :request do
 
         context 'single user' do
           before do
-            allow(work_package.project).to receive(:possible_assignees).and_return([user])
+            allow(project).to receive(:possible_assignees).and_return([user])
 
             allow(user).to receive(:created_on).and_return(user.created_at)
             allow(user).to receive(:updated_on).and_return(user.created_at)
@@ -71,7 +71,7 @@ describe API::V3::WorkPackages::WorkPackagesAPI, type: :request do
 
         context 'multiple users' do
           before do
-            allow(work_package.project).to receive(:possible_assignees).and_return([user, user2])
+            allow(project).to receive(:possible_assignees).and_return([user, user2])
 
             allow(user).to receive(:created_on).and_return(user.created_at)
             allow(user).to receive(:updated_on).and_return(user.created_at)
@@ -86,14 +86,14 @@ describe API::V3::WorkPackages::WorkPackagesAPI, type: :request do
 
       describe 'groups' do
         let(:group) { FactoryGirl.create(:group) }
-        let(:work_package) { FactoryGirl.create(:work_package) }
+        let(:project) { FactoryGirl.create(:project) }
 
-        before { allow(WorkPackage).to receive(:find).and_return(work_package) }
+        before { allow(Project).to receive(:find).and_return(project) }
 
         context 'with work_package_group_assignment' do
           before do
             allow(Setting).to receive(:work_package_group_assignment?).and_return(true)
-            work_package.project.add_member! group, FactoryGirl.create(:role)
+            project.add_member! group, FactoryGirl.create(:role)
           end
 
           it_behaves_like 'returns available assignees', 1, 1
@@ -102,7 +102,7 @@ describe API::V3::WorkPackages::WorkPackagesAPI, type: :request do
         context 'without work_package_group_assignment' do
           before do
             allow(Setting).to receive(:work_package_group_assignment?).and_return(false)
-            work_package.project.add_member! group, FactoryGirl.create(:role)
+            project.add_member! group, FactoryGirl.create(:role)
           end
 
           it_behaves_like 'returns available assignees', 0, 0
