4633dd5cae4d2cbad21b2ef063b381f7f37bf208
Alex Coles <alex@alexbcoles.com>
2014-11-19 12:03:03 +0100

    Add npm module skeleton; load plugin translations
    Signed-off-by: Alex Coles <alex@alexbcoles.com>

---
+++ index.js
@@ -0,0 +1,5 @@
+console.log('Backlogs loaded');
+
+// standard locales
+I18n.addTranslations('en', require('./config/locales/js-en.yml').en);
+I18n.addTranslations('de', require('./config/locales/js-de.yml').de);
