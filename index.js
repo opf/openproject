414327af11613140114d31ed3a577a523c7cfa1d
Alex Coles <alex@alexbcoles.com>
2014-11-21 13:38:23 +0100

    Move Angular app plugin code under frontend/app
    Analog to core commit:
    https://github.com/opf/openproject/commit/6ec5ca58edc6d8dbf0efd14aab189dead0da6fa2
    
    Signed-off-by: Alex Coles <alex@alexbcoles.com>

--- index.js
+++
@@ -1,5 +0,0 @@
-console.log('Costs loaded');
-
-// standard locales
-I18n.addTranslations('en', require('./config/locales/js-en.yml').en);
-I18n.addTranslations('de', require('./config/locales/js-de.yml').de);
