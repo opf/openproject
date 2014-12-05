c942eb6ca3b6b1ceacc2e648348664c83e3edda0
Martin Linkhorst <martin.linkhorst+github.com@posteo.de>
2014-12-05 11:07:16 +0100

    allow plus sign in user's login attribute

--- app/models/user.rb
+++ app/models/user.rb
@@ -144,7 +144,7 @@ class User < Principal
   validates_uniqueness_of :login, :if => Proc.new { |user| !user.login.blank? }, :case_sensitive => false
   validates_uniqueness_of :mail, :allow_blank => true, :case_sensitive => false
   # Login must contain letters, numbers, underscores only
-  validates_format_of :login, :with => /\A[a-z0-9_\-@\.]*\z/i
+  validates_format_of :login, :with => /\A[a-z0-9_\-@\.+]*\z/i
   validates_length_of :login, :maximum => 256
   validates_length_of :firstname, :lastname, :maximum => 30
   validates_format_of :mail, :with => /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i, :allow_blank => true
