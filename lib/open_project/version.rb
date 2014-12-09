6ce61e5f7a0017713c1b89e07ed72770987f3171
kgalli <mail@kgalli.de>
2014-12-09 17:46:43 +0100

    Bump version to 3.0.17

--- lib/open_project/version.rb
+++ lib/open_project/version.rb
@@ -34,7 +34,7 @@ module OpenProject
 
     MAJOR = 3
     MINOR = 0
-    PATCH = 16
+    PATCH = 17
     TINY  = PATCH # Redmine compat
 
     # Used by semver to define the special version (if any).
