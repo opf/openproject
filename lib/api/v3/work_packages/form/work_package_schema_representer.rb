bce6325cbd1aaf41c5bc11ba2c4b7ae4ca28d486
Jan Sandbrink <j.sandbrink@finn.de>
2015-01-30 13:33:46 +0100

    simplify another schema getter

--- lib/api/v3/work_packages/form/work_package_schema_representer.rb
+++ lib/api/v3/work_packages/form/work_package_schema_representer.rb
@@ -81,13 +81,7 @@ module API
           property :version,
                    exec_context: :decorator,
                    getter: -> (*) {
-                     version_origin = represented
-
-                     if represented.persisted? && represented.fixed_version_id_changed?
-                       version_origin = represented.class.find(represented.id)
-                     end
-
-                     SchemaAllowedVersionsRepresenter.new(version_origin.assignable_versions,
+                     SchemaAllowedVersionsRepresenter.new(represented.assignable_versions,
                                                           current_user: current_user)
                    }
 
