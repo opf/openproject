1bb1e379aa784d8f296c8090c1a13488c7649550
Jan Sandbrink <j.sandbrink@finn.de>
2015-02-11 16:24:59 +0100

    OKAY HOUND!

--- lib/api/v3/work_packages/schema/work_package_schema_representer.rb
+++ lib/api/v3/work_packages/schema/work_package_schema_representer.rb
@@ -35,7 +35,7 @@ module API
     module WorkPackages
       module Schema
         class WorkPackageSchemaRepresenter < ::API::Decorators::Single
-          I18n_work_package = 'activerecord.attributes.work_package'
+          I18N_WORK_PACKAGE = 'activerecord.attributes.work_package'
 
           def self.schema(property,
                           type: nil,
@@ -46,7 +46,7 @@ module API
                           max_length: nil)
             raise ArgumentError if property.nil? || type.nil?
 
-            title = I18n.t("#{I18n_work_package}.#{property}") unless title
+            title = I18n.t("#{I18N_WORK_PACKAGE}.#{property}") unless title
 
             schema = ::API::Decorators::PropertySchemaRepresenter.new(type: type,
                                                                       name: title)
@@ -69,7 +69,7 @@ module API
             raise ArgumentError if property.nil? || href_callback.nil?
 
             type = property.to_s.camelize unless type
-            title = I18n.t("#{I18n_work_package}.#{property}") unless title
+            title = I18n.t("#{I18N_WORK_PACKAGE}.#{property}") unless title
 
             property property,
                      exec_context: :decorator,
@@ -129,7 +129,7 @@ module API
 
           schema :percentage_done,
                  type: 'Integer',
-                 title: I18n.t("#{I18n_work_package}.done_ratio"),
+                 title: I18n.t("#{I18N_WORK_PACKAGE}.done_ratio"),
                  writable: false
 
           schema :created_at,
@@ -154,7 +154,7 @@ module API
 
           schema_with_allowed_link :assignee,
                                    type: 'User',
-                                   title: I18n.t("#{I18n_work_package}.assigned_to"),
+                                   title: I18n.t("#{I18N_WORK_PACKAGE}.assigned_to"),
                                    required: false,
                                    href_callback: -> (*) {
                                      api_v3_paths.available_assignees(represented.project.id)
@@ -162,7 +162,7 @@ module API
 
           schema_with_allowed_link :responsible,
                                    type: 'User',
-                                   title: I18n.t("#{I18n_work_package}.responsible"),
+                                   title: I18n.t("#{I18N_WORK_PACKAGE}.responsible"),
                                    required: false,
                                    href_callback: -> (*) {
                                      api_v3_paths.available_responsibles(represented.project.id)
@@ -174,7 +174,7 @@ module API
                      assignable_statuses = represented.assignable_statuses_for(current_user)
                      representer = ::API::Decorators::AllowedValuesByCollectionRepresenter.new(
                        type: 'Status',
-                       name: I18n.t("#{I18n_work_package}.status"),
+                       name: I18n.t("#{I18N_WORK_PACKAGE}.status"),
                        current_user: current_user,
                        value_representer: API::V3::Statuses::StatusRepresenter,
                        link_factory: -> (status) {
@@ -196,7 +196,7 @@ module API
                    getter: -> (*) {
                      representer = ::API::Decorators::AllowedValuesByCollectionRepresenter.new(
                        type: 'Version',
-                       name: I18n.t("#{I18n_work_package}.fixed_version"),
+                       name: I18n.t("#{I18N_WORK_PACKAGE}.fixed_version"),
                        current_user: current_user,
                        value_representer: API::V3::Versions::VersionRepresenter,
                        link_factory: -> (version) {
@@ -220,7 +220,7 @@ module API
                    getter: -> (*) {
                      representer = ::API::Decorators::AllowedValuesByCollectionRepresenter.new(
                        type: 'Priority',
-                       name: I18n.t("#{I18n_work_package}.priority"),
+                       name: I18n.t("#{I18N_WORK_PACKAGE}.priority"),
                        current_user: current_user,
                        value_representer: API::V3::Priorities::PriorityRepresenter,
                        link_factory: -> (priority) {
