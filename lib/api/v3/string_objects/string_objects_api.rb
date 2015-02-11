1074833a9fd67bac1e976aa8accdd0c8315c5942
Jan Sandbrink <j.sandbrink@finn.de>
2015-02-11 17:18:07 +0100

    define string objects endpoint

--- lib/api/v3/string_objects/string_objects_api.rb
+++ lib/api/v3/string_objects/string_objects_api.rb
@@ -27,30 +27,18 @@
 # See doc/COPYRIGHT.rdoc for more details.
 #++
 
-# Root class of the API v3
-# This is the place for all API v3 wide configuration, helper methods, exceptions
-# rescuing, mounting of differnet API versions etc.
-
 module API
   module V3
-    class Root < Grape::API
-      version 'v3', using: :path
+    module StringObjects
+      class StringObjectsAPI < Grape::API
+        resources :string_objects do
 
-      mount ::API::V3::Activities::ActivitiesAPI
-      mount ::API::V3::Attachments::AttachmentsAPI
-      mount ::API::V3::Categories::CategoriesAPI
-      mount ::API::V3::Priorities::PrioritiesAPI
-      mount ::API::V3::Projects::ProjectsAPI
-      mount ::API::V3::Queries::QueriesAPI
-      mount ::API::V3::Render::RenderAPI
-      mount ::API::V3::Statuses::StatusesAPI
-      mount ::API::V3::StringObjects::StringObjectsAPI
-      mount ::API::V3::Users::UsersAPI
-      mount ::API::V3::Versions::VersionsAPI
-      mount ::API::V3::WorkPackages::WorkPackagesAPI
-
-      get '/' do
-        RootRepresenter.new({})
+          namespace ':value' do
+            get do
+              StringObjectRepresenter.new(params[:value])
+            end
+          end
+        end
       end
     end
   end
