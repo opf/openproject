df7f30d5a3f8068e0bf00b4f4549ba7175376284
Jan Sandbrink <j.sandbrink@finn.de>
2015-03-09 15:19:48 +0100

    add story points and remaining time to schema

--- lib/open_project/backlogs/engine.rb
+++ lib/open_project/backlogs/engine.rb
@@ -132,8 +132,30 @@ module OpenProject::Backlogs
       :ProjectsController, :ProjectsHelper, :Query, :User, :VersionsController, :Version]
 
     extend_api_response(:v3, :work_packages, :work_package) do
-      property :story_points, if: -> (*) { backlogs_enabled? }
-      property :remaining_hours, if: -> (*) { backlogs_enabled? }
+      property :story_points,
+               if: -> (*) { backlogs_enabled? }
+
+      property :remaining_time,
+               exec_context: :decorator,
+               getter: -> (*) {
+                 datetime_formatter.format_duration_from_hours(represented.remaining_hours,
+                                                               allow_nil: true)
+               },
+               render_nil: true,
+               if: -> (*) { backlogs_enabled? }
+    end
+
+    extend_api_response(:v3, :work_packages, :schema, :work_package_schema) do
+      schema :story_points,
+             type: 'Integer',
+             required: false,
+             show_if: -> (*) { represented.project.backlogs_enabled? }
+
+      schema :remaining_time,
+             type: 'Duration',
+             name_source: :remaining_hours,
+             required: false,
+             show_if: -> (*) { represented.project.backlogs_enabled? }
     end
 
     config.to_prepare do
