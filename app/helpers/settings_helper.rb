52dc7bcca246d8880ff4137b6dfe5d8e2ef28fbe
Alex Coles <alex@alexbcoles.com>
2015-03-12 13:40:10 -0300

    Extract methods from #settings_multiselect
    Signed-off-by: Alex Coles <alex@alexbcoles.com>

--- app/helpers/settings_helper.rb
+++ app/helpers/settings_helper.rb
@@ -76,33 +76,8 @@ module SettingsHelper
 
   def settings_matrix(settings, choices, options = {})
     content_tag(:table, class: 'form--matrix') do
-      content_tag(:thead) do
-        content_tag(:tr, class: 'form--matrix-header-row') do
-          content_tag(:th, I18n.t(options[:label_choices] || :label_choices),
-                      class: 'form--matrix-header-cell') +
-            settings.map do |setting|
-              content_tag(:th, class: 'form--matrix-header-cell') do
-                hidden_field_tag("settings[#{setting}][]", '') +
-                  I18n.t("setting_#{setting}")
-              end
-            end.join.html_safe
-        end
-      end +
-        content_tag(:tbody) do
-          choices.map do |choice|
-            text, value = (choice.is_a?(Array)) ? choice : [choice, choice]
-            content_tag(:tr, class: 'form--matrix-row') do
-              content_tag(:td, text, class: 'form--matrix-cell') +
-                settings.map do |setting|
-                  content_tag(:td, class: 'form--matrix-checkbox-cell') do
-                    styled_check_box_tag("settings[#{setting}][]", value,
-                                         Setting.send(setting).include?(value),
-                                         id: "#{setting}_#{value}")
-                  end
-                end.join.html_safe
-            end
-          end.join.html_safe
-        end
+      build_settings_matrix_head(settings, options) +
+        build_settings_matrix_body(settings, choices)
     end
   end
 
@@ -155,6 +130,39 @@ module SettingsHelper
       content_tag(:span, class: 'form--field-container', &block)
     else
       block.call
+    end
+  end
+
+  def build_settings_matrix_head(settings, options = {})
+    content_tag(:thead) do
+      content_tag(:tr, class: 'form--matrix-header-row') do
+        content_tag(:th, I18n.t(options[:label_choices] || :label_choices),
+                    class: 'form--matrix-header-cell') +
+          settings.map do |setting|
+            content_tag(:th, class: 'form--matrix-header-cell') do
+              hidden_field_tag("settings[#{setting}][]", '') +
+                I18n.t("setting_#{setting}")
+            end
+          end.join.html_safe
+      end
+    end
+  end
+
+  def build_settings_matrix_body(settings, choices)
+    content_tag(:tbody) do
+      choices.map do |choice|
+        text, value = (choice.is_a?(Array)) ? choice : [choice, choice]
+        content_tag(:tr, class: 'form--matrix-row') do
+          content_tag(:td, text, class: 'form--matrix-cell') +
+            settings.map do |setting|
+              content_tag(:td, class: 'form--matrix-checkbox-cell') do
+                styled_check_box_tag("settings[#{setting}][]", value,
+                                     Setting.send(setting).include?(value),
+                                     id: "#{setting}_#{value}")
+              end
+            end.join.html_safe
+        end
+      end.join.html_safe
     end
   end
 end
