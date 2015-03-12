12f86e7a8fed952e893da17c1dd005ec78175104
Alex Coles <alex@alexbcoles.com>
2015-03-12 13:04:13 -0300

    Rename form--table class usages to form--matrix
    See core commit:
    https://github.com/opf/openproject/commit/cc5f9d5cc03d7b77b7e5b0a04e3cfb57e3944891
    
    Signed-off-by: Alex Coles <alex@alexbcoles.com>

--- features/support/participant_steps.rb
+++ features/support/participant_steps.rb
@@ -21,5 +21,5 @@
 Then(/^the user "(.*?)" should( not)? be available as a participant$/) do |login, negative|
   user = User.find_by_login(login)
 
-  step(%{I should#{negative} see "#{user.name}" within "#meeting-form table.form--table"})
+  step(%{I should#{negative} see "#{user.name}" within "#meeting-form table.form--matrix"})
 end
