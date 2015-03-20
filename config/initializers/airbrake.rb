0082c4485a23894729900d64b0900fe3622e9184
Martin Linkhorst <martin.linkhorst+github.com@posteo.de>
2015-03-20 14:30:28 +0100

    remove boilerplate from airbrake config file
    * live with the defaults
    * but allow errbit to be configured as a backend

--- config/initializers/airbrake.rb
+++ config/initializers/airbrake.rb
@@ -4,8 +4,5 @@ if ENV['AIRBRAKE_API_KEY']
     config.host    = ENV['AIRBRAKE_HOST'] if ENV['AIRBRAKE_HOST']
     config.port    = ENV.fetch('AIRBRAKE_PORT', 443)
     config.secure  = config.port == 443
-
-    config.rescue_rake_exceptions = true
-    # config.development_environments = []
   end
 end
