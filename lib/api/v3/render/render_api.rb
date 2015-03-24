f50ad955b7c66d2362902eadb14cf1820a4e311b
Jan Sandbrink <j.sandbrink@finn.de>
2015-03-24 13:00:51 +0100

    more namespace qualification

--- lib/api/v3/render/render_api.rb
+++ lib/api/v3/render/render_api.rb
@@ -27,8 +27,6 @@
 # See doc/COPYRIGHT.rdoc for more details.
 #++
 
-require 'api/utilities/text_renderer'
-
 module API
   module V3
     module Render
@@ -118,9 +116,9 @@ module API
               check_content_type
               setup_response
 
-              renderer = Utilities::TextRenderer.new(request_body,
-                                                     object: context_object,
-                                                     format: @format)
+              renderer = ::API::Utilities::TextRenderer.new(request_body,
+                                                            object: context_object,
+                                                            format: @format)
               renderer.to_html
             end
           end
