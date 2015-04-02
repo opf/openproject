3a9b2c14b433cd9e2fc9d1101210d86f3c285aed
Nicolai Moraru <dot.nick.dot.messing@gmail.com>
2015-04-02 13:14:25 +0300

    The right fix for IPv6

--- frontend/gulpfile.js
+++ frontend/gulpfile.js
@@ -133,8 +133,8 @@ gulp.task('webdriver:standalone', ['webdriver:update'], webdriverStandalone);
 
 gulp.task('tests:protractor', ['webdriver:update', 'webpack', 'sass', 'express'], function(done) {
   var address = server.address().address;
-  if (address === '::' || address === '::1' || address === '0.0.0.0') {
-    address = 'localhost';
+  if (server.address().family === 'IPv6') {
+    address = '[' + address + ']';
   }
   gulp.src('tests/integration/**/*_spec.js')
     .pipe(protractor({
