opf/openproject

commit 2efd6f4e4535b14cd8577883e56770118215b46b

Move legacy specs under spec/legacy
Signed-off-by: Alex Coles <alex@alexbcoles.com>

lib/tasks/testing.rake
@@ -115,10 +115,9 @@ namespace :spec do
   task legacy: %w(legacy:unit legacy:functional legacy:integration)
   namespace :legacy do
     %w(unit functional integration).each do |type|
-      desc "Run the code examples in test/#{type}"
+      desc "Run the code examples in spec/legacy/#{type}"
       RSpec::Core::RakeTask.new(type => 'spec:prepare') do |t|
-        t.ruby_opts = '-I test'
-        t.pattern = "test/#{type}/**/*_test.rb"
+        t.pattern = "spec/legacy/#{type}/**/*_spec.rb"
       end
     end
   end

commit 91ace2cc0b5ee976c3749f9a86112522faeb2ede

Fix repository fixture dirs used by scm:setup task
Broken in previous commit 2efd6f4e.

Signed-off-by: Alex Coles <alex@alexbcoles.com>

lib/tasks/testing.rake
@@ -54,14 +54,14 @@ namespace :test do
       task subversion: :create_dir do
         repo_path = 'tmp/test/subversion_repository'
         system "svnadmin create #{repo_path}"
-        system "gunzip < test/fixtures/repositories/subversion_repository.dump.gz | svnadmin load #{repo_path}"
+        system "gunzip < spec/fixtures/repositories/subversion_repository.dump.gz | svnadmin load #{repo_path}"
       end
 
       (supported_scms - [:subversion]).each do |scm|
         desc "Creates a test #{scm} repository"
         task scm => :create_dir do
-          # system "gunzip < test/fixtures/repositories/#{scm}_repository.tar.gz | tar -xv -C tmp/test"
-          system "tar -xvz -C tmp/test -f test/fixtures/repositories/#{scm}_repository.tar.gz"
+          # system "gunzip < spec/fixtures/repositories/#{scm}_repository.tar.gz | tar -xv -C tmp/test"
+          system "tar -xvz -C tmp/test -f spec/fixtures/repositories/#{scm}_repository.tar.gz"
         end
       end
 
@@ -75,7 +75,7 @@ namespace :test do
       Dir.glob('tmp/test/*_repository').each do |dir|
         next unless File.basename(dir) =~ %r{\A(.+)_repository\z} && File.directory?(dir)
         scm = $1
-        next unless fixture = Dir.glob("test/fixtures/repositories/#{scm}_repository.*").first
+        next unless fixture = Dir.glob("spec/fixtures/repositories/#{scm}_repository.*").first
         next if File.stat(dir).ctime > File.stat(fixture).mtime
 
         FileUtils.rm_rf dir