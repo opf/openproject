3fdbe072d02a1b4c154262eb273bb9f97dcd887e
Mihail Maxacov <0xf013@gmail.com>
2015-05-18 13:47:20 +0300

    subject and some description specs

---
+++ spec/features/work_packages/details/inplace_editor/subject_editor_spec.rb
@@ -0,0 +1,64 @@
+require 'spec_helper'
+require 'features/work_packages/details/inplace_editor/shared_examples'
+require 'features/work_packages/details/inplace_editor/work_package_field'
+
+describe 'subject inplace editor', js: true do
+  let(:project) { FactoryGirl.create :project_with_types, is_public: true }
+  let(:property_name) { :subject }
+  let(:property_title) { 'Subject' }
+  let!(:work_package) { FactoryGirl.create :work_package, project: project }
+  let(:user) { FactoryGirl.create :admin }
+  let(:field) { WorkPackageField.new page, property_name }
+
+  before do
+    allow(User).to receive(:current).and_return(user)
+    visit project_work_packages_path(project)
+    row = page.find("#work-package-#{work_package.id}")
+    sleep 0.1 # wait for angular to run digest to hook the listeners
+    # TODO: try capybara-angular
+    row.double_click
+  end
+
+  context 'in read state' do
+    it 'has correct content' do
+      expect(field.read_state_text).to eq work_package.send(property_name)
+    end
+  end
+
+
+  it_behaves_like 'an auth aware field'
+  it_behaves_like 'a cancellable field'
+  it_behaves_like 'having a single validation point'
+  it_behaves_like 'a required field'
+
+  context 'in edit state' do
+    before do
+      field.activate_edition
+    end
+
+    it 'renders a text input' do
+      expect(field.input_element).to be_visible
+      expect(field.input_element['type']).to eq 'text'
+    end
+
+    it 'has a correct value for the input' do
+      expect(field.input_element[:value]).to eq work_package.subject
+    end
+
+    context 'on save' do
+      before do
+        field.input_element.set 'Aloha'
+      end
+
+      it 'displays the new value after save' do
+        field.submit_by_click
+        expect(field.read_state_text).to eq 'Aloha'
+      end
+
+      it 'saves the value on ENTER' do
+        field.submit_by_enter
+        expect(field.read_state_text).to eq 'Aloha'
+      end
+    end
+  end
+end
---
+++ spec/features/work_packages/details/inplace_editor/work_package_field.rb
@@ -0,0 +1,61 @@
+class WorkPackageField
+
+  attr_reader :element
+
+  def initialize(page, property_name)
+    @property_name = property_name
+    @element = page.find(field_selector)
+  end
+
+  def read_state_text
+    @element.find('.inplace-edit--read-value span').text
+  end
+
+  def trigger_link
+    @element.find trigger_link_selector
+  end
+
+  def trigger_link_selector
+    'a.inplace-editing--trigger-link'
+  end
+
+  def field_selector
+    ".work-package-field.work-packages--details--#{@property_name}"
+  end
+
+  def activate_edition
+    @element.click
+  end
+
+  def input_element
+    @element.find('.focus-input')
+  end
+
+  def submit_by_click
+    @element.find('.inplace-edit--control--save').click
+  end
+
+  def submit_by_enter
+    input_element.native.send_keys :return
+  end
+
+  def cancel_by_click
+    @element.find('.inplace-edit--control--cancel').click
+  end
+
+  def cancel_by_escape
+    input_element.native.send_keys :escape
+  end
+
+  def editable?
+    !!@element.find('.inplace-edit--write') rescue false
+  end
+
+  def errors_text
+    @element.find('.inplace-edit--errors--text').text
+  end
+
+  def errors_element
+    @element.find('.inplace-edit--errors')
+  end
+end
