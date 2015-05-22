a464c012030a53b7c3247a5310dbd3cd38ccd3cc
Alex Coles <alex@alexbcoles.com>
2015-05-22 15:07:12 +0200

    Force bool coercion in API v2 WorkflowsController
    Boolean values are not properly coerced when using `.select` with
    custom SQL String on PostgreSQL.
    
    Signed-off-by: Alex Coles <alex@alexbcoles.com>

--- app/controllers/api/v2/workflows_controller.rb
+++ app/controllers/api/v2/workflows_controller.rb
@@ -89,9 +89,9 @@ module Api
       end
 
       def scope(transition)
-        if transition.author
+        if ActiveRecord::ConnectionAdapters::Column.value_to_boolean(transition.author)
           :author
-        elsif transition.assignee
+        elsif ActiveRecord::ConnectionAdapters::Column.value_to_boolean(transition.assignee)
           :assignee
         else
           :role
