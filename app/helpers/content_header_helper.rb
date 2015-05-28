95c84611ef0a8701a28007a71ab2b3fa1ec15433
Florian Kraft <f.kraft@finn.de>
2015-05-28 17:51:02 +0200

    make the toolbar submenu helpers work
    Signed-off-by: Florian Kraft <f.kraft@finn.de>

--- app/helpers/content_header_helper.rb
+++ app/helpers/content_header_helper.rb
@@ -1,7 +1,13 @@
 module ContentHeaderHelper
-  class ContentHeader
+
+  class AbstractContent
     include ActionView::Helpers
     attr_accessor :output_buffer
+
+    delegate :key_for, to: OpenProject::AccessKeys
+  end
+
+  class ContentHeader < AbstractContent
 
     def initialize(title:, subtitle: '', helper: nil, &_block)
       @title = title
@@ -39,86 +45,115 @@ module ContentHeaderHelper
     def decode(string)
       raw(strip_tags(string)).strip
     end
+  end
 
-    class ContentToolbar
-      include ActionView::Helpers
+  class ContentToolbar < AbstractContent
 
-      attr_accessor :output_buffer
+    def button(text, location, options = {})
+      options[:class] = %w(button) + Array(options[:class])
+      item text, location, options
+    end
 
-      delegate :key_for, to: OpenProject::AccessKeys
-
-      def button(text, location, options = {})
-        options[:class] = %w(button) + Array(options[:class])
-        item text, location, options
+    def item(text, location, options = {})
+      return '' unless show?(options)
+      link_options = extract_from options
+      toolbar_item do
+        link_to location, link_options do
+          concat button_icon(options)
+          concat button_text(options.merge text: text)
+        end
       end
+    end
 
-      def item(text, location, options = {})
-        return '' unless show?(options)
+    def toolbar_item(options = {})
+      options[:class] = Array(options[:class]) + %w(toolbar-item)
+      options[:role] = 'menuitem'
+      if block_given?
+        content_tag :li, options do
+          yield
+        end
+      else
+        content_tag :li, '', options
+      end
+    end
+
+    def submenu(options = {}, &block)
+      toolbar_item class: '-with-submenu' do
         link_options = extract_from options
-        toolbar_item do
-          link_to location, link_options do
-            concat button_icon(options)
-            concat button_text(text, options)
-          end
+        link_options[:class] += %w(button)
+        link = link_to('#', link_options) do
+          concat button_icon options
+          concat button_text text: options[:title]
+          concat content_tag :i, '', class: 'button--dropdown-indicator'
+        end
+        concat link
+        css = Array(options[:class]) + %w(toolbar-submenu)
+        css += %w(-last) if options[:last]
+        concat content_tag :ul, capture(&block), class: css
+      end
+    end
+
+    def submenu_item(text, location, options = {})
+      unless options[:icon]
+        options[:class] = Array(options[:class]) + %w(no-icon)
+      end
+      item = toolbar_item(options) do
+        link_options = extract_from(options).delete_if { |k, _| k == :icon }
+        link_to location, link_options do
+          concat content_tag :i, '', class: "icon icon-#{options[:icon]}" if options[:icon]
+          concat text
         end
       end
+      concat item
+      ''
+    end
 
-      def toolbar_item
-        opts = { class: 'toolbar-item', role: 'menuitem' }
-        if block_given?
-          content_tag :li, opts  do
-            yield
-          end
-        else
-          content_tag :li, '', opts
-        end
+    protected
+
+    def button_icon(options = {})
+      icon = options.delete :icon
+      return '' if icon.blank?
+      content_tag :i, '', class: "button--icon icon-#{icon}"
+    end
+
+    def button_text(options = {})
+      label_for_blind = options.fetch :label_for_blind, ''
+      text = options.fetch :text, ''
+      concat content_tag :span, text, class: 'button--text'
+      unless label_for_blind.blank?
+        concat content_tag(:span, label_for_blind, class: 'hidden-for-sighted')
       end
+    end
 
-      protected
+    def show?(options = {})
+      options.fetch(:if, true) && !options.fetch(:unless, false)
+    end
 
-      def button_icon(options = {})
-        icon = options.fetch :icon, ''
-        return '' if icon.blank?
-        content_tag :i, '', class: "button--icon icon-#{icon}"
+    def class_from_color(color)
+      case color.to_sym
+      when :alt
+        '-alt-highlight'
+      when :default
+        '-highlight'
+      else
+        ''
       end
+    end
 
-      def button_text(text, options = {})
-        label_for_blind = options.fetch :label_for_blind, ''
-        concat content_tag :span, text, class: 'button--text'
-        unless label_for_blind.blank?
-          concat content_tag(:span, label_for_blind, class: 'hidden-for-sighted')
-        end
-      end
+    def extract_from(options = {})
+      opts = options.dup
+      color, css_classes, key = values_from opts
+      css_classes = Array(css_classes) + Array(class_from_color(color))
+      delete_keys_from(opts).merge(class: css_classes, accesskey: key_for(key))
+    end
 
-      def show?(options = {})
-        options.fetch(:if, true) && !options.fetch(:unless, false)
-      end
+    def values_from(options)
+      [:highlight, :class, :accesskey].map { |sym| options.fetch sym, '' }
+    end
 
-      def class_from_color(color)
-        case color.to_sym
-        when :alt
-          '-alt-highlight'
-        when :default
-          '-highlight'
-        else
-          ''
-        end
-      end
-
-      def extract_from(options = {})
-        color, css_classes, key = values_from options
-        css_classes = Array(css_classes) + Array(class_from_color(color))
-        delete_keys_from(options).merge(class: css_classes, accesskey: key_for(key))
-      end
-
-      def values_from(options)
-        [:highlight, :class, :accesskey].map { |sym| options.fetch sym, '' }
-      end
-
-      def delete_keys_from(options)
-        keys_to_delete = [:unless, :if, :highlight, :class, :accesskey]
-        options.delete_if { |k, _| keys_to_delete.include? k }
-      end
+    def delete_keys_from(options)
+      keys_to_delete = [:unless, :if, :highlight, :class, :accesskey]
+      options.delete_if { |k, _| keys_to_delete.include? k }
     end
   end
 
