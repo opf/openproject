6c8c1c36e766ca5e9f4de5dc7985857b7649582c
Alexander Bach <a.bach@finn.de>
2015-06-05 09:59:57 +0200

    Tiny style fix

--- lib/api/v3/work_packages/work_packages_by_project_api.rb
+++ lib/api/v3/work_packages/work_packages_by_project_api.rb
@@ -51,7 +51,8 @@ module API
           post do
             write_work_package_attributes @work_package
 
-            if write_request_valid?(@work_package, WorkPackages::CreateContract) && @create_service.save
+            if write_request_valid?(@work_package, WorkPackages::CreateContract) &&
+               @create_service.save
               @work_package.reload
 
               WorkPackages::WorkPackageRepresenter.create(@work_package,
