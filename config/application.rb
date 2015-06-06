5916c1995e837a00a7fca095fd0f9ac7093e10b0
Alex Coles <alex@alexbcoles.com>
2015-06-06 14:19:28 +0200

    Update Rails configs: initializers
    Signed-off-by: Alex Coles <alex@alexbcoles.com>

--- config/application.rb
+++ config/application.rb
@@ -111,9 +111,6 @@ module OpenProject
     # Configure the default encoding used in templates for Ruby 1.9.
     config.encoding = 'utf-8'
 
-    # Configure sensitive parameters which will be filtered from the log file.
-    config.filter_parameters += [:password]
-
     # Whitelist assets to be precompiled.
     #
     # This is a workaround for an issue where the precompilation process will
