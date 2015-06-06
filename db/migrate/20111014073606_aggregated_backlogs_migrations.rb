fdb96fb28abae70b59daa8bb0e53a1d9bee17078
Alex Coles <alex@alexbcoles.com>
2015-06-06 11:05:10 +0200

    Fix method signature in aggregated migration
    Analog to core commit:
    https://github.com/opf/openproject/commit/a280631fd6cdce3639ecdf08e843b9f1b43c82a1
    
    Signed-off-by: Alex Coles <alex@alexbcoles.com>

--- db/migrate/20111014073606_aggregated_backlogs_migrations.rb
+++ db/migrate/20111014073606_aggregated_backlogs_migrations.rb
@@ -39,7 +39,7 @@ require 'open_project/plugins/migration_mapping'
 # This migration aggregates the migrations detailed in MIGRATION_FILES
 class AggregatedBacklogsMigrations < ActiveRecord::Migration
 
-  def initialize
+  def initialize(*)
     super
     @issues_table_exists = ActiveRecord::Base.connection.tables.include? 'issues'
   end
