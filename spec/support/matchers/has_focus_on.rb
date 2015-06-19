485c67f747f0428f56e31d5364a4d752d5508cd2
Jens Ulferts <j.ulferts@finn.de>
2015-06-19 08:48:45 +0200

    rework focus specs for accessiblity
    Specing for focus seems to be unreliable when using css selectors in selenium.
    
    Therefore a custom matcher is introduced to try to work around the issues.
    
    Additionally, it makes the specs easier to read.

---
+++ spec/support/matchers/has_focus_on.rb
@@ -0,0 +1,49 @@
+#-- encoding: UTF-8
+#-- copyright
+# OpenProject is a project management system.
+# Copyright (C) 2012-2015 the OpenProject Foundation (OPF)
+#
+# This program is free software; you can redistribute it and/or
+# modify it under the terms of the GNU General Public License version 3.
+#
+# OpenProject is a fork of ChiliProject, which is a fork of Redmine. The copyright follows:
+# Copyright (C) 2006-2013 Jean-Philippe Lang
+# Copyright (C) 2010-2013 the ChiliProject Team
+#
+# This program is free software; you can redistribute it and/or
+# modify it under the terms of the GNU General Public License
+# as published by the Free Software Foundation; either version 2
+# of the License, or (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+#
+# See doc/COPYRIGHT.rdoc for more details.
+#++
+
+# Extending Capybara to circumvent problems with selecting the focused element:
+# http://opensourcetester.co.uk/2011/07/11/selenium-webdriver-focus/
+
+module Capybara
+  class Session
+    def has_focus_on?(selector)
+      starting_time = Time.now
+      return_value = false
+
+      while !return_value && Time.now - starting_time < Capybara.default_wait_time
+
+        focused_element = self.driver.browser.switch_to.active_element
+
+        return_value = find(selector).native == focused_element
+      end
+
+      return_value
+    end
+  end
+end
