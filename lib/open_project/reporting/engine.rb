dd519dfa974fda9199eff42e1d0123660601c020
Jan Sandbrink <j.sandbrink@finn.de>
2015-06-24 11:29:22 +0200

    fix indentation

--- lib/open_project/reporting/engine.rb
+++ lib/open_project/reporting/engine.rb
@@ -27,41 +27,41 @@ module OpenProject::Reporting
              author_url: 'http://finn.de',
              requires_openproject: '>= 4.0.0' do
 
-    view_actions = [:index, :show, :drill_down, :available_values, :display_report_list]
-    edit_actions = [:create, :update, :rename, :delete]
+      view_actions = [:index, :show, :drill_down, :available_values, :display_report_list]
+      edit_actions = [:create, :update, :rename, :delete]
 
-    #register reporting_module including permissions
-    project_module :reporting_module do
-      permission :save_cost_reports, {cost_reports: edit_actions}
-      permission :save_private_cost_reports, {cost_reports: edit_actions}
-    end
+      #register reporting_module including permissions
+      project_module :reporting_module do
+        permission :save_cost_reports, {cost_reports: edit_actions}
+        permission :save_private_cost_reports, {cost_reports: edit_actions}
+      end
 
-    #register additional permissions for viewing time and cost entries through the CostReportsController
-    view_actions.each do |action|
-      Redmine::AccessControl.permission(:view_time_entries).actions << "cost_reports/#{action}"
-      Redmine::AccessControl.permission(:view_own_time_entries).actions << "cost_reports/#{action}"
-      Redmine::AccessControl.permission(:view_cost_entries).actions << "cost_reports/#{action}"
-      Redmine::AccessControl.permission(:view_own_cost_entries).actions << "cost_reports/#{action}"
-    end
+      #register additional permissions for viewing time and cost entries through the CostReportsController
+      view_actions.each do |action|
+        Redmine::AccessControl.permission(:view_time_entries).actions << "cost_reports/#{action}"
+        Redmine::AccessControl.permission(:view_own_time_entries).actions << "cost_reports/#{action}"
+        Redmine::AccessControl.permission(:view_cost_entries).actions << "cost_reports/#{action}"
+        Redmine::AccessControl.permission(:view_own_cost_entries).actions << "cost_reports/#{action}"
+      end
 
-    #menu extensions
-    menu :top_menu, :cost_reports_global, {controller: 'cost_reports', action: 'index', project_id: nil},
-      caption: :cost_reports_title,
-      if: Proc.new {
-        ( User.current.allowed_to?(:view_time_entries, nil, global: true) ||
-          User.current.allowed_to?(:view_own_time_entries, nil, global: true) ||
-          User.current.allowed_to?(:view_cost_entries, nil, global: true) ||
-          User.current.allowed_to?(:view_own_cost_entries, nil, global: true)
-        )
-      }
+      #menu extensions
+      menu :top_menu, :cost_reports_global, {controller: 'cost_reports', action: 'index', project_id: nil},
+        caption: :cost_reports_title,
+        if: Proc.new {
+          ( User.current.allowed_to?(:view_time_entries, nil, global: true) ||
+            User.current.allowed_to?(:view_own_time_entries, nil, global: true) ||
+            User.current.allowed_to?(:view_cost_entries, nil, global: true) ||
+            User.current.allowed_to?(:view_own_cost_entries, nil, global: true)
+          )
+        }
 
-    menu :project_menu, :cost_reports,
-         {controller: 'cost_reports', action: 'index'},
-         param: :project_id,
-         after: :cost_objects,
-         caption: :cost_reports_title,
-         if: Proc.new { |project| project.module_enabled?(:reporting_module) },
-         html: {class: 'icon2 icon-stats'}
+      menu :project_menu, :cost_reports,
+           {controller: 'cost_reports', action: 'index'},
+           param: :project_id,
+           after: :cost_objects,
+           caption: :cost_reports_title,
+           if: Proc.new { |project| project.module_enabled?(:reporting_module) },
+           html: {class: 'icon2 icon-stats'}
     end
 
     initializer "reporting.register_hooks" do
