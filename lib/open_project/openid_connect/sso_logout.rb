76dd0254e5c98603587ebf83668fd206fe29c432
Markus Kahl <machisuji@googlemail.com>
2015-06-29 23:30:27 +0100

    fixed OmniauthLogin reference

--- lib/open_project/openid_connect/sso_logout.rb
+++ lib/open_project/openid_connect/sso_logout.rb
@@ -29,7 +29,7 @@ module OpenProject
         end
 
         # If the user may view the site without being logged in we redirect back to it.
-        site_open = !(Setting.login_required? && Concerns::OmniauthLogin.direct_login?)
+        site_open = !(Setting.login_required? && ::Concerns::OmniauthLogin.direct_login?)
         return_url = site_open && "#{Setting.protocol}://#{Setting.host_name}"
 
         if logout_at_op! return_url
