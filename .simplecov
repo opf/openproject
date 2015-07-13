a99dc23feff97ea6d6e9b4c422ae2b51c7b7bc50
Stefan Botzenhart <sb@basiszwo.com>
2015-07-13 20:05:30 +0200

    Generate code coverage report selectively

--- .simplecov
+++ .simplecov
@@ -1,5 +1,3 @@
-SimpleCov.start :rails
-
 if ENV['CI'] == 'true'
   require 'codecov'
   SimpleCov.formatter = SimpleCov::Formatter::Codecov
--- features/support/env.rb
+++ features/support/env.rb
@@ -33,7 +33,11 @@
 # instead of editing this one. Cucumber will automatically load all features/**/*.rb
 # files.
 
-require 'simplecov'
+if ENV['COVERAGE']
+  require 'simplecov'
+  SimpleCov.start 'rails'
+end
+
 require 'cucumber/rails'
 require 'cucumber/rspec/doubles'
 require 'capybara-screenshot/cucumber'
@@ -85,7 +89,7 @@ ActionController::Base.allow_rescue = false
 # Remove/comment out the lines below if your app doesn't have a database.
 # For some databases (like MongoDB and CouchDB) you may need to use :truncation instead.
 begin
-  DatabaseCleaner.strategy = :truncation
+  DatabaseCleaner.strategy = :transaction
 rescue NameError
   raise 'You need to add database_cleaner to your Gemfile (in the :test group) if you wish to use it.'
 end
