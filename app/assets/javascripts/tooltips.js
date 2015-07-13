7fd856808ecad9e9407d86a9352b87426c61b0cc
Solotchi Veaceslav <slawa85@gmail.com>
2015-07-13 14:43:28 +0300

    Create custom tooltip that support html

---
+++ app/assets/javascripts/tooltips.js
@@ -0,0 +1,13 @@
+jQuery(function($) {
+  var tooltipElements = $('.advanced--tooltip');
+  var input = $('input[id^="new_password"]');
+  if(tooltipElements.length){
+    tooltipElements.each(function createTooltip() {
+      input.focusin(function () {
+        $(this).parent().parent().addClass('tooltip-visible');
+      }).focusout(function () {
+        $(this).parent().parent().removeClass('tooltip-visible');
+      });
+    });
+  }
+});
