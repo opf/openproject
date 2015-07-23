f03008ec94cf8526c73570443b295618345f00a4
Oliver Günther <mail@oliverguenther.de>
2015-07-23 11:35:51 +0200

    Require admin for destroy_info

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -38,7 +38,7 @@ class ProjectsController < ApplicationController
   before_filter :find_project, :except => [ :index, :level_list, :new, :create ]
   before_filter :authorize, :only => [ :show, :settings, :edit, :update, :modules, :types ]
   before_filter :authorize_global, :only => [:new, :create]
-  before_filter :require_admin, :only => [ :archive, :unarchive, :destroy ]
+  before_filter :require_admin, :only => [ :archive, :unarchive, :destroy, :destroy_info ]
   before_filter :jump_to_project_menu_item, :only => :show
   before_filter :load_project_settings, :only => :settings
   before_filter :determine_base
