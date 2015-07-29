44a43a0c11d40081786c88d89a53fbecf8f73e43
Oliver Günther <mail@oliverguenther.de>
2015-07-29 22:16:49 +0200

    Remove superfluous route

--- config/routes.rb
+++ config/routes.rb
@@ -516,7 +516,6 @@ OpenProject::Application.routes.draw do
   scope controller: 'sys' do
     match '/sys/projects.:format', action: 'projects', via: :get
     match '/sys/projects/:id/repository/update_storage', action: 'update_required_storage', via: :get
-    match '/sys/projects/:id/repository.:format', action: 'create_project_repository', via: :post
   end
 
   # alternate routes for the current user
