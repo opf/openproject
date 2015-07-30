c838a88ec706ed27413cf2fca14513e3aa7a34f6
Jan Sandbrink <j.sandbrink@finn.de>
2015-07-30 17:11:02 +0200

    specify fancy edge-case
    yet unsolved

--- spec/workers/mail_notification_jobs/deliver_work_package_notification_job_spec.rb
+++ spec/workers/mail_notification_jobs/deliver_work_package_notification_job_spec.rb
@@ -146,4 +146,114 @@ describe DeliverWorkPackageNotificationJob, type: :model do
       end
     end
   end
+
+  describe 'mail suppressing aggregation' do
+    # business logic of whether to send or not to send a mail is mainly driven by the presence
+    # of an aggregated journal. However, there is an edge case that could lead to a notification
+    # getting lost. Sadly this is very implementation specific, so I'll describe it:
+    #  Journal 1: comment
+    #  Journal 2: change (this can also be multiple journals)
+    #  Journal 3: comment
+    #
+    # The Job for the first journal will not send any mail, because Journal 2 supersedes it.
+    # However, after adding Journal 3, the aggregation will look like (1), (2, 3). Therefore the
+    # job for Journal 2 will not send a notification. Finally the job for Journal 3 will send a
+    # notification, but only containing the changes of 2 and 3. The comment of journal 1 is lost.
+    # Therefore a Job needs to check whether it has hidden another job and execute
+    # its notifications too.
+
+    before do
+      change = { subject: 'new subject' }
+      note = { notes: 'a comment' }
+
+      expect(work_package.update_by!(author, note)).to be_truthy
+      work_package.reload
+      expect(work_package.update_by!(author, change)).to be_truthy
+      work_package.reload
+      expect(work_package.update_by!(author, note)).to be_truthy
+    end
+
+    let(:timeout) { Setting.journal_aggregation_time_minutes.to_i.minutes }
+    let(:journal_1) { work_package.journals[1] }
+    let(:journal_2) { work_package.journals[2] }
+    let(:journal_3) { work_package.journals[3] }
+
+    context 'all changes happen within the timeout of journal 1' do
+      # The job for 1 will know, that Journal 3 took its addition.
+      # The job for 2 will know, that it has become part of Journal 3.
+      #  -> no special behaviour required
+
+      it 'Job 1 sends one mail for journal 1' do
+        expect(UserMailer).to receive(:work_package_updated).once
+        described_class.new(journal_1.id, author.id).perform
+      end
+
+      it 'Job 2 sends no mails' do
+        expect(UserMailer).not_to receive(:work_package_updated)
+        described_class.new(journal_2.id, author.id).perform
+      end
+
+      it 'Job 3 sends one mail for journal (2,3)' do
+        expect(UserMailer).to receive(:work_package_updated).once
+        described_class.new(journal_3.id, author.id).perform
+      end
+    end
+
+    context 'journal 3 created after timeout of 1, but inside of timeout for 2' do
+      # Job 1 will not send a mail because it does not know about journal 3
+      #   (thinking 2 will take its mail)
+      # Job 3 will have to take over the mail for Job 1
+      # Even if Job 1 knew of journal 3 (due to late execution), it was not allowed to send a mail
+      #   (that would cause Job 3 to deliver a duplicate mail)
+
+      before do
+        journal_2.created_at = journal_1.created_at + (timeout / 2)
+        journal_3.created_at = journal_1.created_at + timeout + 5.seconds
+        journal_2.save!
+        journal_3.save!
+      end
+
+      it 'Job 1 sends no mails' do
+        expect(UserMailer).not_to receive(:work_package_updated)
+        described_class.new(journal_1.id, author.id).perform
+      end
+
+      it 'Job 2 sends no mails' do
+        expect(UserMailer).not_to receive(:work_package_updated)
+        described_class.new(journal_2.id, author.id).perform
+      end
+
+      it 'Job 3 sends mails for journals 1 + (2,3)' do
+        expect(UserMailer).to receive(:work_package_updated).twice
+        described_class.new(journal_3.id, author.id).perform
+      end
+    end
+
+    context 'journal 3 created after timeout of 1 and 2' do
+      # This is a normal case, just to ensure that Job 3 will recognize it is not responsible for
+      # (1,2).
+
+      before do
+        journal_2.created_at = journal_1.created_at + (timeout / 2)
+        journal_3.created_at = journal_2.created_at + timeout + 5.seconds
+        journal_2.save!
+        journal_3.save!
+      end
+
+      it 'Job 1 sends no mails' do
+        expect(UserMailer).not_to receive(:work_package_updated)
+        described_class.new(journal_1.id, author.id).perform
+      end
+
+      it 'Job 2 sends one mail for journal (1, 2)' do
+        expect(UserMailer).to receive(:work_package_updated).once
+        described_class.new(journal_2.id, author.id).perform
+      end
+
+      it 'Job 3 sends one mail for journal 3' do
+        expect(UserMailer).to receive(:work_package_updated).once
+        described_class.new(journal_3.id, author.id).perform
+      end
+    end
+  end
 end
