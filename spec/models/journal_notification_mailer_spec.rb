70de5df26753cdd0fb4d335c905592e73b8e3c32
Jan Sandbrink <j.sandbrink@finn.de>
2015-08-03 15:43:47 +0200

    add specs for notification mailer part of edge case

--- spec/models/journal_notification_mailer_spec.rb
+++ spec/models/journal_notification_mailer_spec.rb
@@ -55,9 +55,11 @@ describe JournalNotificationMailer do
 
     allow(User).to receive(:current).and_return(user)
     allow(Setting).to receive(:notified_events).and_return(notifications)
+
+    allow(Delayed::Job).to receive(:enqueue)
   end
 
-  shared_examples_for 'sends a regular notification' do
+  shared_examples_for 'enqueues a regular notification' do
     it do
       expect(Delayed::Job).to receive(:enqueue)
                                 .with(
@@ -75,7 +77,7 @@ describe JournalNotificationMailer do
     context 'setting enabled' do
       let(:notifications) { [notification_setting] }
 
-      it_behaves_like 'sends a regular notification'
+      it_behaves_like 'enqueues a regular notification'
 
       context 'insufficient work package changes' do
         let(:journal) { another_work_package.journals.last }
@@ -123,7 +125,7 @@ describe JournalNotificationMailer do
       context 'setting enabled' do
         let(:notifications) { ['work_package_updated'] }
 
-        it_behaves_like 'sends a regular notification'
+        it_behaves_like 'enqueues a regular notification'
       end
 
       it 'sends no notification' do
@@ -167,6 +169,127 @@ describe JournalNotificationMailer do
       it 'sends no notification' do
         expect(Delayed::Job).not_to receive(:enqueue)
         call_listener
+      end
+    end
+  end
+
+  describe 'mail suppressing aggregation' do
+    # business logic of whether to send or not to send a mail is mainly driven by the presence
+    # of an aggregated journal. However, there is an edge case that could lead to a notification
+    # getting lost. Sadly this is very implementation specific, so I'll describe it:
+    #  Journal 1: comment
+    #  Journal 2: change (this can also be multiple journals)
+    #  Journal 3: comment
+    #
+    # The Job for the first journal will not send any mail, because Journal 2 supersedes it.
+    # However, after adding Journal 3, the aggregation will look like (1), (2, 3). Therefore the
+    # job for Journal 2 will not send a notification. Finally the job for Journal 3 will send a
+    # notification, but only containing the changes of 2 and 3. The comment of journal 1 is lost.
+    # Therefore two things have to happen:
+    # - someone needs to send notifications for the hidden journal
+    #   (done by JournalNotificationMailer)
+    # - in case a journal is hidden, its Job is not allowed to enqueue a mail for it
+    #   (because someone else will do it on behalf)
+    #   This is important since late exec of a Job might cause it to _not_ skip notifications
+
+    let(:author) { FactoryGirl.create(:user) }
+    let(:notifications) { ['work_package_updated'] }
+    let(:timeout) { Setting.journal_aggregation_time_minutes.to_i.minutes }
+
+    let(:journal_1) { work_package.journals[1] }
+    let(:journal_2) { work_package.journals[2] }
+    let(:journal_3) { work_package.journals[3] }
+
+    shared_context 'updated until Journal 1' do
+      before do
+        expect(work_package.update_by!(author, { notes: 'a comment' })).to be_truthy
+      end
+    end
+
+    shared_context 'updated until Journal 2' do
+      include_context 'updated until Journal 1'
+
+      before do
+        work_package.reload
+        expect(work_package.update_by!(author, { subject: 'new subject' })).to be_truthy
+      end
+    end
+
+    shared_context 'updated until Journal 3' do
+      include_context 'updated until Journal 2'
+
+      before do
+        work_package.reload
+        expect(work_package.update_by!(author, { notes: 'a comment' })).to be_truthy
+      end
+    end
+
+    context 'all changes happen within the timeout of journal 1' do
+      # The job for 1 will know, that Journal 3 took its addition.
+      # The job for 2 will know, that it has become part of Journal 3.
+      #  -> no special behaviour required
+
+      describe 'Journal 1' do
+        include_context 'updated until Journal 1'
+
+        it_behaves_like 'enqueues a regular notification'
+      end
+
+      describe 'Journal 2' do
+        include_context 'updated until Journal 2'
+
+        it_behaves_like 'enqueues a regular notification'
+      end
+
+      describe 'Journal 3' do
+        include_context 'updated until Journal 3'
+
+        it_behaves_like 'enqueues a regular notification'
+      end
+    end
+
+    context 'journal 3 created after timeout of 1, but inside of timeout for 2' do
+      describe 'Journal 3' do
+        include_context 'updated until Journal 3'
+
+        before do
+          journal_2.created_at = journal_1.created_at + (timeout / 2)
+          journal_3.created_at = journal_1.created_at + timeout + 5.seconds
+          journal_2.save!
+          journal_3.save!
+        end
+
+        it 'immediately delivers a mail on behalf of Journal 1' do
+          expect(Delayed::Job).to receive(:enqueue)
+                                    .with(
+                                      an_instance_of(DeliverWorkPackageNotificationJob))
+          call_listener
+        end
+
+        it 'also enqueues a regular mail' do
+          expect(Delayed::Job).to receive(:enqueue)
+                                    .with(
+                                      an_instance_of(EnqueueWorkPackageNotificationJob),
+                                      run_at: anything)
+          call_listener
+        end
+      end
+    end
+
+    context 'journal 3 created after timeout of 1 and 2' do
+      # This is a normal case again, ensuring Journal 3 takes no responsiblity when not neccessary.
+
+      describe 'Journal 3' do
+        include_context 'updated until Journal 3'
+
+        before do
+          journal_2.created_at = journal_1.created_at + (timeout / 2)
+          journal_3.created_at = journal_2.created_at + timeout + 5.seconds
+          journal_2.save!
+          journal_3.save!
+        end
+
+        it_behaves_like 'enqueues a regular notification'
       end
     end
   end
