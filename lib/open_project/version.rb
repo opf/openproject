e010165a38e4264a60a6387779af8ef8678f1962
Jonas Heinrich <j.heinrich@finn.de>
2015-08-06 17:15:29 +0200

    Adapt version to corresponding OpenProject core version.

--- lib/open_project/version.rb
+++ lib/open_project/version.rb
@@ -33,7 +33,7 @@ module OpenProject
   module VERSION #:nodoc:
     MAJOR = 4
     MINOR = 2
-    PATCH = 5
+    PATCH = 6
     TINY  = PATCH # Redmine compat
 
     # Used by semver to define the special version (if any).
