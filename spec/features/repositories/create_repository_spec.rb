31ec2ab0c2af9ab13ee3901c5fb9f7f7e34bc1d1
Jens Ulferts <j.ulferts@finn.de>
2015-08-14 11:19:25 +0200

    using stable expectation

--- spec/features/repositories/create_repository_spec.rb
+++ spec/features/repositories/create_repository_spec.rb
@@ -140,7 +140,7 @@ describe 'Create repository', type: :feature, js: true do
         find('input[type="radio"][value="managed"]').set(true)
         find('button[type="submit"]', text: I18n.t(:button_create)).click
 
-        expect(find('div.flash.notice')).not_to be_nil
+        expect(page).to have_selector('div.flash.notice')
         expect(page).to have_selector('input[name="scm_type"][value="managed"]:checked')
         expect(page).to have_selector('a.icon-delete', text: I18n.t(:button_delete))
       end
