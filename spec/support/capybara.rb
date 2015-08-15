034fded3dae8e89903d9649a644699a8d511c404
Stefan Botzenhart <sb@basiszwo.com>
2015-08-15 13:49:30 +0200

    Upgrade capybara and update capybara config

--- spec/support/capybara.rb
+++ spec/support/capybara.rb
@@ -29,4 +29,46 @@
 require 'capybara/rails'
 
 Capybara.javascript_driver = :webkit
-Capybara.default_wait_time = 2
+
+Capybara::Webkit.configure do |config|
+  # Enable debug mode. Prints a log of everything the driver is doing.
+  # config.debug = true
+
+  # By default, requests to outside domains (anything besides localhost) will
+  # result in a warning. Several methods allow you to change this behavior.
+
+  # Silently return an empty 200 response for any requests to unknown URLs.
+  config.block_unknown_urls
+
+  # Allow pages to make requests to any URL without issuing a warning.
+  # config.allow_unknown_urls
+
+  # Allow a specifc domain without issuing a warning.
+  # config.allow_url("example.com")
+
+  # Allow a specifc URL and path without issuing a warning.
+  # config.allow_url("example.com/some/path")
+
+  # Wildcards are allowed in URL expressions.
+  # config.allow_url("*.example.com")
+
+  # Silently return an empty 200 response for any requests to the given URL.
+  # config.block_url("example.com")
+
+  # Timeout if requests take longer than 5 seconds
+  config.timeout = 2
+
+  # Don't raise errors when SSL certificates can't be validated
+  config.ignore_ssl_errors
+
+  # Don't load images
+  # config.skip_image_loading
+
+  # Use a proxy
+  # config.use_proxy(
+  #   host: "example.com",
+  #   port: 1234,
+  #   user: "proxy",
+  #   pass: "secret"
+  # )
+end
