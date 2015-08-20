d298b22502eb33d7e7f58ed2d880527cba72cd46
Oliver Günther <mail@oliverguenther.de>
2015-08-20 10:31:08 +0200

    Use explicit route for repo_auth

--- config/routes.rb
+++ config/routes.rb
@@ -514,6 +514,7 @@ OpenProject::Application.routes.draw do
   end
 
   scope controller: 'sys' do
+    match '/sys/repo_auth', action: 'repo_auth', via: [:get, :post]
     match '/sys/projects.:format', action: 'projects', via: :get
     match '/sys/projects/:id/repository/update_storage', action: 'update_required_storage', via: :get
     match '/sys/projects/:id/repository.:format', action: 'create_project_repository', via: :post
