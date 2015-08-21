opf/openproject

commit e0d945324735617916f8dfb51a2c97f60cd32b6d

replace deprecated find(:first/:last, ...) finders

app/models/hourly_rate.rb
@@ -58,15 +58,17 @@ class HourlyRate < Rate
     user_id = user_id.id if user_id.is_a?(User)
 
     unless project.nil?
-      rate = find(:first,
-                  conditions: ['user_id = ? and project_id = ? and valid_from <= ?', user_id, project, date],
-                  order: 'valid_from DESC')
+      rate = where(['user_id = ? and project_id = ? and valid_from <= ?', user_id, project, date])
+             .order('valid_from DESC')
+             .first
       if rate.nil?
         project = Project.find(project) unless project.is_a?(Project)
-        rate = find(:first,
-                    conditions: ['user_id = ? and project_id in (?) and valid_from <= ?', user_id, project.ancestors.to_a, date],
-                    include: :project,
-                    order: 'projects.lft DESC, valid_from DESC')
+        rate = where(['user_id = ? and project_id in (?) and valid_from <= ?',
+                      user_id,
+                      project.ancestors.to_a,
+                      date])
+               .includes(:project)
+               .order('projects.lft DESC, valid_from DESC')
       end
     end
     rate ||= DefaultHourlyRate.at_for_user(date, user_id) if include_default

app/models/cost_type.rb
@@ -33,9 +33,7 @@ class CostType < ActiveRecord::Base
 
   # finds the default CostType
   def self.default
-    result = CostType.find(:first, conditions: { default: true })
-    result ||= CostType.find(:first)
-    result
+    CostType.find_by(default: true) || CostType.first
   rescue ActiveRecord::RecordNotFound
     nil
   end
@@ -53,7 +51,9 @@ class CostType < ActiveRecord::Base
   end
 
   def rate_at(date)
-    CostRate.find(:first, conditions: ['cost_type_id = ? and valid_from <= ?', id, date], order: 'valid_from DESC')
+    CostRate.where(['cost_type_id = ? and valid_from <= ?', id, date])
+            .order('valid_from DESC')
+            .first
   rescue ActiveRecord::RecordNotFound
     return nil
   end

app/models/default_hourly_rate.rb
@@ -39,9 +39,7 @@ class DefaultHourlyRate < Rate
   def self.at_for_user(date, user_id)
     user_id = user_id.id if user_id.is_a?(User)
 
-    find(:first,
-         conditions: ['user_id = ? and valid_from <= ?', user_id, date],
-         order: 'valid_from DESC')
+    where(['user_id = ? and valid_from <= ?', user_id, date]).order('valid_from DESC').first
   end
 
   private

features/step_definitions/cost_steps.rb
@@ -75,7 +75,7 @@ Given /^the project "([^\"]+)" has (\d+) [Cc]ost(?: )?[Ee]ntr(?:ies|y) with the 
 end
 
 Given /^the work package "([^\"]+)" has (\d+) [Cc]ost(?: )?[Ee]ntr(?:ies|y) with the following:$/ do |work_package, count, table|
-  i = WorkPackage.find(:last, conditions: ["subject = '#{work_package}'"])
+  i = WorkPackage.where(subject: work_package.subject).last
   as_admin count do
     ce = FactoryGirl.build(:cost_entry, spent_on: (table.rows_hash['date'] ? table.rows_hash['date'].to_date : Date.today),
                                         units: table.rows_hash['units'],
@@ -138,7 +138,7 @@ Given /^there is a standard cost control project named "([^\"]*)"$/ do |name|
 end
 
 Given /^users have times and the cost type "([^\"]*)" logged on the work package "([^\"]*)" with:$/ do |cost_type, work_package, table|
-  i = WorkPackage.find(:last, conditions: ["subject = '#{work_package}'"])
+  i = WorkPackage.where(subject: work_package.subject).last
   raise "No such work_package: #{work_package}" unless i
 
   table.rows_hash.map do |k, v|

commit 5cc2403100a6c3d53be04e940314556aa50bc9b0

Correct missing call to .first in HourlyRate
Broken in e0d94532.

app/models/hourly_rate.rb
@@ -69,6 +69,7 @@ class HourlyRate < Rate
                       date])
                .includes(:project)
                .order('projects.lft DESC, valid_from DESC')
+               .first
       end
     end
     rate ||= DefaultHourlyRate.at_for_user(date, user_id) if include_default