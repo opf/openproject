714371506483e0f902bf25066a15374674efadb7
Tim Habermaas <tim.habermaas@gmail.com>
2015-08-30 23:40:21 +0200

    Fix broken /projects/:id/work_packages/:id URL.
    This also fixes some inconsistencies with sidebar rendering when
    reloading the page.

--- frontend/app/routing.js
+++ frontend/app/routing.js
@@ -79,7 +79,7 @@ angular.module('openproject')
     })
 
     .state('work-packages.show', {
-      url: '/work_packages/{workPackageId:[0-9]+}?query_props',
+      url: '{projectPath:projectPathType}/work_packages/{workPackageId:[0-9]+}?query_props',
       templateUrl: '/templates/work_packages.show.html',
       controller: 'WorkPackageShowController',
       controllerAs: 'vm',
