c8c9ba6196ef9e64f889cfe6698a8feaa5d2156c
kgalli <mail@kgalli.de>
2015-09-09 10:00:05 +0000

    Removal of quotes to fix blacklist of actions

--- lib/open_project/github_integration/notification_handlers.rb
+++ lib/open_project/github_integration/notification_handlers.rb
@@ -37,7 +37,7 @@ module OpenProject::GithubIntegration
     def self.pull_request(payload)
       # Don't add comments on new pushes to the pull request => ignore synchronize.
       # Don't add comments about assignments and labels either.
-      ignored_actions = %w['synchronize' 'assigned' 'unassigned' 'labeled' 'unlabeled']
+      ignored_actions = %w[synchronize assigned unassigned labeled unlabeled]
       return if ignored_actions.include? payload['action']
       comment_on_referenced_work_packages payload['pull_request']['body'], payload
     end
