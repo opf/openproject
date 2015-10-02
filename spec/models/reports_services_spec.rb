476468408965929d43ac493b37ae6ad33dcf6b46
Mohamed Wael Khobalatte <w.khobalatte@openproject.com>
2015-10-02 21:48:15 +0100

    Ensure warning-free use of `raise_error`

--- spec/models/reports_services_spec.rb
+++ spec/models/reports_services_spec.rb
@@ -36,6 +36,6 @@ describe Reports::ReportsService, type: :model do
   end
 
   it 'should raise an error, when given no project' do
-    expect { Reports::ReportsService.new(nil) }.to raise_error
+    expect { Reports::ReportsService.new(nil) }.to raise_error("You must provide a project to report upon")
   end
 end
