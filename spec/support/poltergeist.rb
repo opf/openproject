ac86a7e6dfb6400070a6bf7709d47b76a7e30848
Stefan Botzenhart <sb@basiszwo.com>
2015-10-03 01:52:50 +0200

    Add poltergeist as default driver for capybara

---
+++ spec/support/poltergeist.rb
@@ -0,0 +1,6 @@
+require 'capybara/poltergeist'
+Capybara.javascript_driver = :poltergeist
+
+Capybara.register_driver :poltergeist do |app|
+  Capybara::Poltergeist::Driver.new(app, { js_errors: false })
+end
