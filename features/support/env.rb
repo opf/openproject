49e08b7dc4577aeb26d775111d3cd1ce1b133395
Stefan Botzenhart <sb@basiszwo.com>
2015-10-09 15:35:41 +0200

    Increase poltergeist timeout

--- features/support/env.rb
+++ features/support/env.rb
@@ -87,7 +87,11 @@ end
 
 require 'capybara/poltergeist'
 Capybara.register_driver :poltergeist do |app|
-  options = { js_errors: false, window_size: [1200, 1000] }
+  options = {
+    js_errors: false,
+    window_size: [1200, 1000],
+    timeout: 60
+  }
   Capybara::Poltergeist::Driver.new(app, options)
 end
 Capybara.javascript_driver = :poltergeist
--- spec/support/poltergeist.rb
+++ spec/support/poltergeist.rb
@@ -2,6 +2,10 @@ require 'capybara/poltergeist'
 Capybara.javascript_driver = :poltergeist
 
 Capybara.register_driver :poltergeist do |app|
-  options = { js_errors: false, window_size: [1200, 1000] }
+  options = {
+    js_errors: false,
+    window_size: [1200, 1000],
+    timeout: 60
+  }
   Capybara::Poltergeist::Driver.new(app, options)
 end
