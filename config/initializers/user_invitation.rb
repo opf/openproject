708596c735a34b5dc82cdd63a99907c77368af55
Markus Kahl <machisuji@googlemail.com>
2015-10-16 16:02:51 +0100

    delay user invitation mails

--- config/initializers/user_invitation.rb
+++ config/initializers/user_invitation.rb
@@ -2,5 +2,5 @@
 # The default behaviour is to send the user a sign-up mail
 # when they were invited.
 OpenProject::Notifications.subscribe UserInvitation::EVENT_NAME do |token|
-  UserMailer.user_signed_up(token).deliver_now
+  Delayed::Job.enqueue DeliverInvitationJob.new(token.id)
 end
