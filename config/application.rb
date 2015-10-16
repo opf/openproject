da6e41d5344c251c354c0e5048b7b9b9a88780ca
Oliver Günther <mail@oliverguenther.de>
2015-10-16 11:40:40 +0200

    Load I18n::Backend::Fallbacks everytime
    This commit changes the redmine initializer to always require
    `I18n::Backend::Fallbacks` to be loaded.
    
    This should re-enable default Rails commands such as `rake db:setup`
    and `rake db:reset`.
    
    Relevant work package: https://community.openproject.org/work_packages/3782

--- config/application.rb
+++ config/application.rb
@@ -105,6 +105,9 @@ module OpenProject
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     config.i18n.default_locale = :en
 
+    # Fall back to default locale
+    config.i18n.fallbacks = true
+
     # Configure the default encoding used in templates for Ruby 1.9.
     config.encoding = 'utf-8'
 
