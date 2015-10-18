1c20a37532e7e25d84b15c218c91ad27e2a8ebd7
Oliver Günther <mail@oliverguenther.de>
2015-10-18 21:42:58 +0200

    Set default_locale in application rather than initializer

--- config/initializers/30-redmine.rb
+++ config/initializers/30-redmine.rb
@@ -27,7 +27,6 @@
 # See doc/COPYRIGHT.rdoc for more details.
 #++
 
-I18n.default_locale = 'en'
 # Adds fallback to default locale for untranslated strings
 if Setting.table_exists? # don't want to prevent migrations
   I18n::Backend::Simple.send(:include, I18n::Backend::Fallbacks)
