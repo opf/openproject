c085f83bbbea2e00e362a24996fdfc5b8af320b5
Stefan Botzenhart <sb@basiszwo.com>
2015-11-10 11:11:57 +0100

    Disable core specs

--- spec/disabled_specs.rb
+++ spec/disabled_specs.rb
@@ -18,17 +18,16 @@
 # See doc/COPYRIGHT.md for more details.
 #++
 
-if Rails.env.test?
-  require "rspec/example_disabler"
-  # TODO does rspec add a space randomly to the metadata?!
-  #      (better make the example-disabler a litte more resilient against this)
-  RSpec::ExampleDisabler.disable_example(
-    'ProjectsController show ',
-    "plugin openproject-my_project_overview overwrites routes for show."
-  )
+require "rspec/example_disabler"
 
-  RSpec::ExampleDisabler.disable_example(
-    'ProjectsController show',
-    "plugin openproject-my_project_overview overwrites routes for show."
-  )
-end
+# TODO does rspec add a space randomly to the metadata?!
+#      (better make the example-disabler a litte more resilient against this)
+RSpec::ExampleDisabler.disable_example(
+  'ProjectsController show ',
+  "plugin openproject-my_project_overview overwrites routes for show."
+)
+
+RSpec::ExampleDisabler.disable_example(
+  'ProjectsController show',
+  "plugin openproject-my_project_overview overwrites routes for show."
+)
