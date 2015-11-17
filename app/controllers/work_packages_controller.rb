f508d816554ae2873152f44355a71e41a457f589
Oliver Günther <mail@oliverguenther.de>
2015-11-17 15:15:09 +0100

    Restore journals for atom access

--- app/controllers/work_packages_controller.rb
+++ app/controllers/work_packages_controller.rb
@@ -84,6 +84,14 @@ class WorkPackagesController < ApplicationController
     end
   end
 
+  def journals
+    @journals ||= work_package.journals.changing
+                  .includes(:user)
+                  .order("#{Journal.table_name}.created_at ASC").to_a
+    @journals.reverse! if current_user.wants_comments_in_reverse_order?
+    @journals
+  end
+
   def index
     load_work_packages unless request.format.html?
 
