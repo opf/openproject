a42e89f9874897929851c4f1da3191ef926db7d5
Stefan Botzenhart <sb@basiszwo.com>
2015-11-24 16:58:37 +0100

    Permit parent_id for project

--- app/models/permitted_params.rb
+++ app/models/permitted_params.rb
@@ -285,6 +285,7 @@ class PermittedParams
                                                 :responsible_id,
                                                 :identifier,
                                                 :project_type_id,
+                                                :parent_id,
                                                 custom_fields: [],
                                                 work_package_custom_field_ids: [],
                                                 type_ids: [],
