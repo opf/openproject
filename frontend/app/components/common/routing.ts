bee210995cf81a7750d3dd89504ae8360ae9b7b5
Alex Dik <mail@alexdik.de>
2015-12-17 11:52:34 +0100

    Remove annotations in routing configuration

--- frontend/app/components/common/routing.ts
+++ frontend/app/components/common/routing.ts
@@ -28,242 +28,231 @@
 
 angular
   .module('openproject')
-  .config([
-    '$stateProvider',
-    '$urlRouterProvider',
-    '$urlMatcherFactoryProvider',
-    function ($stateProvider, $urlRouterProvider, $urlMatcherFactoryProvider) {
+  .config(function ($stateProvider, $urlRouterProvider, $urlMatcherFactoryProvider) {
+    $urlRouterProvider.when('/work_packages/', '/work_packages');
+    $urlMatcherFactoryProvider.strictMode(false);
 
-      $urlRouterProvider.when('/work_packages/', '/work_packages');
-      $urlMatcherFactoryProvider.strictMode(false);
-
-      var panels = {
-        get watchers() {
-          return {
-            url: '/watchers',
-            template: '<watchers-panel work-package="workPackage"></watchers-panel>'
-          }
-        },
-
-        get activity() {
-          return {
-            url: '/activity',
-            template: '<activity-panel work-package="workPackage"></activity-panel>'
-          }
-        },
-
-        get activityDetails() {
-          var activity = this.activity;
-          activity.url = '#{activity_no:\d+}';
-
-          return activity;
+    var panels = {
+      get watchers() {
+        return {
+          url: '/watchers',
+          template: '<watchers-panel work-package="workPackage"></watchers-panel>'
         }
-      };
+      },
 
-      $stateProvider
-        .state('work-packages', {
-          url: '',
-          abstract: true,
-          templateUrl: '/templates/work_packages.html',
-          controller: 'WorkPackagesController',
-          resolve: {
-            latestTab: function ($state) {
-              var stateName = 'work-packages.list.details.overview'; // the default tab
+      get activity() {
+        return {
+          url: '/activity',
+          template: '<activity-panel work-package="workPackage"></activity-panel>'
+        }
+      },
 
-              return {
-                getStateName: function () {
-                  return stateName;
-                },
-                registerState: function () {
-                  stateName = $state.current.name;
-                }
-              };
-            }
-          }
-        })
+      get activityDetails() {
+        var activity = this.activity;
+        activity.url = '#{activity_no:\d+}';
 
-        .state('work-packages.new', {
-          url: '/{projects}/{projectPath}/work_packages/new?type&parent_id',
-          templateUrl: '/components/routes/partials/work-packages.new.html',
-          controllerAs: 'vm',
-          reloadOnSearch: false
-        })
+        return activity;
+      }
+    };
 
-        .state('work-packages.copy', {
-          url: '/work_packages/{copiedFromWorkPackageId:[0-9]+}/copy',
-          templateUrl: '/components/routes/partials/work-packages.new.html'
-        })
+    $stateProvider
+      .state('work-packages', {
+        url: '',
+        abstract: true,
+        templateUrl: '/templates/work_packages.html',
+        controller: 'WorkPackagesController',
+        resolve: {
+          latestTab: function ($state) {
+            var stateName = 'work-packages.list.details.overview'; // the default tab
 
-        .state('work-packages.edit', {
-          url: '/{projects}/{projectPath}/work_packages/{workPackageId}/edit',
-          params: {
-            projectPath: {value: null, squash: true},
-            projects: {value: null, squash: true}
-          },
-
-          onEnter: function ($state, $stateParams, inplaceEditAll) {
-            inplaceEditAll.start();
-            $state.go('work-packages.list.details.overview', $stateParams);
-          }
-        })
-
-        .state('work-packages.show', {
-          url: '/work_packages/{workPackageId:[0-9]+}?query_props',
-          templateUrl: '/components/routes/partials/work-packages.show.html',
-          controller: 'WorkPackageShowController',
-          controllerAs: 'vm',
-          resolve: {
-            workPackage: function (WorkPackageService, $stateParams) {
-              return WorkPackageService.getWorkPackage($stateParams.workPackageId);
-            },
-            // TODO hack, get rid of latestTab in ShowController
-            latestTab: function ($state) {
-              var stateName = 'work-package.overview'; // the default tab
-
-              return {
-                getStateName: function () {
-                  return stateName;
-                },
-                registerState: function () {
-                  stateName = $state.current.name;
-                }
-              };
-            }
-          },
-          // HACK
-          // This is to avoid problems with the css depending on which page the
-          // browser starts from (deep-link). As we have CSS rules that change the
-          // layout drastically when on the show action (e.g. position: relative)
-          // and this should not be applied to the other states, we need to remove
-          // the trigger used in the CSS. The correct fix would be to alter the
-          // CSS.
-          onEnter: function ($state, $timeout) {
-            angular.element('body').addClass('action-show');
-
-            $timeout(function () {
-              if ($state.is('work-packages.show')) {
-                $state.go('work-packages.show.activity');
+            return {
+              getStateName: function () {
+                return stateName;
+              },
+              registerState: function () {
+                stateName = $state.current.name;
               }
-            });
-          },
-
-          onExit: function () {
-            angular.element('body').removeClass('action-show');
+            };
           }
-        })
-        .state('work-packages.show.activity', panels.activity)
-        .state('work-packages.show.activity.details', panels.activityDetails)
-        .state('work-packages.show.relations', {
-          url: '/relations',
-          templateUrl: '/templates/work_packages/tabs/relations.html'
-        })
-        .state('work-packages.show.watchers', panels.watchers)
+        }
+      })
 
-        .state('work-packages.list', {
-          url: '/{projects}/{projectPath}/work_packages?query_id&query_props',
-          controller: 'WorkPackagesListController',
-          templateUrl: '/components/routes/partials/work-packages.list.html',
-          params: {
-            // value: null makes the parameter optional
-            // squash: true avoids duplicate slashes when the paramter is not provided
-            projectPath: {value: null, squash: true},
-            projects: {value: null, squash: true}
+      .state('work-packages.new', {
+        url: '/{projects}/{projectPath}/work_packages/new?type&parent_id',
+        templateUrl: '/components/routes/partials/work-packages.new.html',
+        controllerAs: 'vm',
+        reloadOnSearch: false
+      })
+
+      .state('work-packages.copy', {
+        url: '/work_packages/{copiedFromWorkPackageId:[0-9]+}/copy',
+        templateUrl: '/components/routes/partials/work-packages.new.html'
+      })
+
+      .state('work-packages.edit', {
+        url: '/{projects}/{projectPath}/work_packages/{workPackageId}/edit',
+        params: {
+          projectPath: {value: null, squash: true},
+          projects: {value: null, squash: true}
+        },
+
+        onEnter: function ($state, $stateParams, inplaceEditAll) {
+          inplaceEditAll.start();
+          $state.go('work-packages.list.details.overview', $stateParams);
+        }
+      })
+
+      .state('work-packages.show', {
+        url: '/work_packages/{workPackageId:[0-9]+}?query_props',
+        templateUrl: '/components/routes/partials/work-packages.show.html',
+        controller: 'WorkPackageShowController',
+        controllerAs: 'vm',
+        resolve: {
+          workPackage: function (WorkPackageService, $stateParams) {
+            return WorkPackageService.getWorkPackage($stateParams.workPackageId);
           },
-          reloadOnSearch: false,
-          // HACK
-          // This is to avoid problems with the css depending on which page the
-          // browser starts from (deep-link). As we have CSS rules that change the
-          // layout drastically when on the index action (e.g. position: absolute,
-          // heigt of footer, ...), and this should not be applied to the other
-          // states, we need to remove the trigger used in the CSS The correct fix
-          // would be to alter the CSS.
-          onEnter: function () {
-            angular.element('body').addClass('action-index');
-          },
-          onExit: function () {
-            angular.element('body').removeClass('action-index');
+          // TODO hack, get rid of latestTab in ShowController
+          latestTab: function ($state) {
+            var stateName = 'work-package.overview'; // the default tab
+
+            return {
+              getStateName: function () {
+                return stateName;
+              },
+              registerState: function () {
+                stateName = $state.current.name;
+              }
+            };
           }
-        })
-        .state('work-packages.list.new', {
-          url: '/create_new?type&parent_id',
-          templateUrl: '/components/routes/partials/work-packages.list.new.html',
-          reloadOnSearch: false
-        })
-        .state('work-packages.list.copy', {
-          url: '/details/{copiedFromWorkPackageId:[0-9]+}/copy',
-          templateUrl: '/components/routes/partials/work-packages.list.new.html',
-          reloadOnSearch: false
-        })
-        .state('work-packages.list.details', {
-          url: '/details/{workPackageId:[0-9]+}',
-          templateUrl: '/components/routes/partials/work-packages.list.details.html',
-          controller: 'WorkPackageDetailsController',
-          reloadOnSearch: false,
-          resolve: {
-            workPackage: function (WorkPackageService, $stateParams) {
-              return WorkPackageService.getWorkPackage($stateParams.workPackageId);
+        },
+        // HACK
+        // This is to avoid problems with the css depending on which page the
+        // browser starts from (deep-link). As we have CSS rules that change the
+        // layout drastically when on the show action (e.g. position: relative)
+        // and this should not be applied to the other states, we need to remove
+        // the trigger used in the CSS. The correct fix would be to alter the
+        // CSS.
+        onEnter: function ($state, $timeout) {
+          angular.element('body').addClass('action-show');
+
+          $timeout(function () {
+            if ($state.is('work-packages.show')) {
+              $state.go('work-packages.show.activity');
             }
+          });
+        },
+
+        onExit: function () {
+          angular.element('body').removeClass('action-show');
+        }
+      })
+      .state('work-packages.show.activity', panels.activity)
+      .state('work-packages.show.activity.details', panels.activityDetails)
+      .state('work-packages.show.relations', {
+        url: '/relations',
+        templateUrl: '/templates/work_packages/tabs/relations.html'
+      })
+      .state('work-packages.show.watchers', panels.watchers)
+
+      .state('work-packages.list', {
+        url: '/{projects}/{projectPath}/work_packages?query_id&query_props',
+        controller: 'WorkPackagesListController',
+        templateUrl: '/components/routes/partials/work-packages.list.html',
+        params: {
+          // value: null makes the parameter optional
+          // squash: true avoids duplicate slashes when the paramter is not provided
+          projectPath: {value: null, squash: true},
+          projects: {value: null, squash: true}
+        },
+        reloadOnSearch: false,
+        // HACK
+        // This is to avoid problems with the css depending on which page the
+        // browser starts from (deep-link). As we have CSS rules that change the
+        // layout drastically when on the index action (e.g. position: absolute,
+        // heigt of footer, ...), and this should not be applied to the other
+        // states, we need to remove the trigger used in the CSS The correct fix
+        // would be to alter the CSS.
+        onEnter: function () {
+          angular.element('body').addClass('action-index');
+        },
+        onExit: function () {
+          angular.element('body').removeClass('action-index');
+        }
+      })
+      .state('work-packages.list.new', {
+        url: '/create_new?type&parent_id',
+        templateUrl: '/components/routes/partials/work-packages.list.new.html',
+        reloadOnSearch: false
+      })
+      .state('work-packages.list.copy', {
+        url: '/details/{copiedFromWorkPackageId:[0-9]+}/copy',
+        templateUrl: '/components/routes/partials/work-packages.list.new.html',
+        reloadOnSearch: false
+      })
+      .state('work-packages.list.details', {
+        url: '/details/{workPackageId:[0-9]+}',
+        templateUrl: '/components/routes/partials/work-packages.list.details.html',
+        controller: 'WorkPackageDetailsController',
+        reloadOnSearch: false,
+        resolve: {
+          workPackage: function (WorkPackageService, $stateParams) {
+            return WorkPackageService.getWorkPackage($stateParams.workPackageId);
           }
-        })
-        .state('work-packages.list.details.overview', {
-          url: '/overview',
-          controller: 'DetailsTabOverviewController',
-          templateUrl: '/templates/work_packages/tabs/overview.html',
-          controllerAs: 'vm',
-        })
-        .state('work-packages.list.details.activity', panels.activity)
-        .state('work-packages.list.details.activity.details', panels.activityDetails)
-        .state('work-packages.list.details.relations', {
-          url: '/relations',
-          templateUrl: '/templates/work_packages/tabs/relations.html',
-        })
-        .state('work-packages.list.details.watchers', panels.watchers);
-    }])
-
-  .run([
-    '$location',
-    '$rootElement',
-    '$browser',
-    '$rootScope',
-    '$state',
-    function ($location, $rootElement, $browser, $rootScope, $state) {
-      // Our application is still a hybrid one, meaning most routes are still
-      // handled by Rails. As such, we disable the default link-hijacking that
-      // Angular's HTML5-mode turns on.
-      $rootElement.off('click');
-      $rootElement.on('click', 'a[data-ui-route]', function (event) {
-        if (!jQuery('body').has('div[ui-view]').length) {
-          return;
         }
-        if (event.ctrlKey || event.metaKey || event.which === 2) {
-          return;
+      })
+      .state('work-packages.list.details.overview', {
+        url: '/overview',
+        controller: 'DetailsTabOverviewController',
+        templateUrl: '/templates/work_packages/tabs/overview.html',
+        controllerAs: 'vm',
+      })
+      .state('work-packages.list.details.activity', panels.activity)
+      .state('work-packages.list.details.activity.details', panels.activityDetails)
+      .state('work-packages.list.details.relations', {
+        url: '/relations',
+        templateUrl: '/templates/work_packages/tabs/relations.html',
+      })
+      .state('work-packages.list.details.watchers', panels.watchers);
+  })
+
+  .run(function ($location, $rootElement, $browser, $rootScope, $state) {
+    // Our application is still a hybrid one, meaning most routes are still
+    // handled by Rails. As such, we disable the default link-hijacking that
+    // Angular's HTML5-mode turns on.
+    $rootElement.off('click');
+    $rootElement.on('click', 'a[data-ui-route]', function (event) {
+      if (!jQuery('body').has('div[ui-view]').length) {
+        return;
+      }
+      if (event.ctrlKey || event.metaKey || event.which === 2) {
+        return;
+      }
+
+      // NOTE: making use of event delegation, thus jQuery-only.
+      var elm = jQuery(event.target);
+      var absHref = elm.prop('href');
+      var rewrittenUrl = $location.$$rewrite(absHref);
+
+      if (absHref && !elm.attr('target') &&
+        rewrittenUrl && !event.isDefaultPrevented()) {
+
+        event.preventDefault();
+        if (rewrittenUrl !== $browser.url()) {
+          // update location manually
+          $location.$$parse(rewrittenUrl);
+          $rootScope.$apply();
+          // hack to work around FF6 bug 684208 when scenario runner clicks on links
+          //noinspection TypeScriptUnresolvedVariable
+          window.angular['ff-684208-preventDefault'] = true;
         }
+      }
+    });
 
-        // NOTE: making use of event delegation, thus jQuery-only.
-        var elm = jQuery(event.target);
-        var absHref = elm.prop('href');
-        var rewrittenUrl = $location.$$rewrite(absHref);
-
-        if (absHref && !elm.attr('target') &&
-          rewrittenUrl && !event.isDefaultPrevented()) {
-
-          event.preventDefault();
-          if (rewrittenUrl !== $browser.url()) {
-            // update location manually
-            $location.$$parse(rewrittenUrl);
-            $rootScope.$apply();
-            // hack to work around FF6 bug 684208 when scenario runner clicks on links
-            //noinspection TypeScriptUnresolvedVariable
-            window.angular['ff-684208-preventDefault'] = true;
-          }
-        }
-      });
-
-      $rootScope.$on('$stateChangeStart', function (event, toState, toParams) {
-        if (!toParams.projects && toParams.projectPath) {
-          toParams.projects = 'projects';
-          $state.go(toState, toParams);
-        }
-      });
+    $rootScope.$on('$stateChangeStart', function (event, toState, toParams) {
+      if (!toParams.projects && toParams.projectPath) {
+        toParams.projects = 'projects';
+        $state.go(toState, toParams);
+      }
+    });
     }
-  ]);
+  );
