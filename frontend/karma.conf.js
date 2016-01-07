e836558ad3fd049bbc0a461676533b111a60d632
Alex Dik <mail@alexdik.de>
2016-01-07 11:25:44 +0100

    Move karma configuration to test file
    Instead of using command params in the package.json, we
    now use the actual config file.

--- frontend/karma.conf.js
+++ frontend/karma.conf.js
@@ -120,12 +120,12 @@ module.exports = function(config) {
 
     // start these browsers
     // available browser launchers: https://npmjs.org/browse/keyword/karma-launcher
-    browsers: ['PhantomJS'],
+    browsers: ['PhantomJS', 'Firefox'],
 
 
     // Continuous Integration mode
     // if true, Karma captures browsers, runs the tests and exits
-    singleRun: false,
+    singleRun: true,
 
 
 
