bc9f610bd6ef2fe79adaada3d6eafd646f27c430
Alex Dik <mail@alexdik.de>
2016-01-14 21:21:27 +0100

    Implement WP show view button directive

---
+++ frontend/app/components/wp-buttons/wp-view-button/wp-view-button.directive.ts
@@ -0,0 +1,82 @@
+// -- copyright
+// OpenProject is a project management system.
+// Copyright (C) 2012-2015 the OpenProject Foundation (OPF)
+//
+// This program is free software; you can redistribute it and/or
+// modify it under the terms of the GNU General Public License version 3.
+//
+// OpenProject is a fork of ChiliProject, which is a fork of Redmine. The copyright follows:
+// Copyright (C) 2006-2013 Jean-Philippe Lang
+// Copyright (C) 2010-2013 the ChiliProject Team
+//
+// This program is free software; you can redistribute it and/or
+// modify it under the terms of the GNU General Public License
+// as published by the Free Software Foundation; either version 2
+// of the License, or (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program; if not, write to the Free Software
+// Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+//
+// See doc/COPYRIGHT.rdoc for more details.
+// ++
+
+import {WorkPackageNavigationButtonController, wpButtonDirective} from '../wp-buttons.module';
+import {KeepTabService} from '../keep-tab/keep-tab.service';
+
+export class WorkPackageViewButtonController extends WorkPackageNavigationButtonController{
+  public workPackageId:number;
+  public nextWpFunc:Function;
+
+  public accessKey:number = 9;
+  public activeState:string = 'work-packages.show';
+  public labelKey:string = 'js.button_show_view';
+  public buttonId:string = 'work-packages-show-view-button';
+  public iconClass:string = 'icon-view-fullscreen';
+
+  constructor(public $state, public I18n, public loadingIndicator, public keepTab:KeepTabService) {
+    'ngInject';
+
+    super($state, I18n);
+  }
+
+  public performAction() {
+    this.openWorkPackageShowView();
+  }
+
+  public openWorkPackageShowView() {
+    var args = ['work-packages.new', this.$state.params];
+
+    if (!(this.$state.is('work-packages.list.new') && this.$state.params.type)) {
+      var params = {
+        workPackageId: this.$state.params.workPackageId || this.workPackageId || this.nextWpFunc()
+      };
+      args = [this.keepTab.currentShowTab, params];
+
+      angular.extend(params, this.$state.params);
+    }
+
+    var promise = this.$state.go.apply(this.$state, args);
+    this.loadingIndicator.on(promise);
+  }
+}
+
+function wpViewButton(): ng.IDirective {
+  return wpButtonDirective({
+    scope: {
+      workPackageId: '=?',
+      nextWpFunc: '=?'
+    },
+
+    controller: WorkPackageViewButtonController,
+  });
+}
+
+angular
+  .module('openproject.wpButtons')
+  .directive('wpViewButton', wpViewButton);
