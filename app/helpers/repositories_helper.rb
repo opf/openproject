8c277bac84f08cc7efaf95f6ac54bd71ae47a63a
Oliver Günther <mail@oliverguenther.de>
2016-02-08 15:45:20 +0100

    Use folder mapping and Set for identifying action

--- app/helpers/repositories_helper.rb
+++ app/helpers/repositories_helper.rb
@@ -91,24 +91,25 @@ module RepositoriesHelper
     render_changes_tree(tree[:s])
   end
 
+  # Mapping from internal action to (folder|file)-icon type
+  def change_action_mapping
+    {
+      'A' => :add,
+      'B' => :remove
+    }
+  end
+
   # This calculates whether a folder was added, deleted or modified. It is based on the assumption that
   # a folder was added/deleted when all content was added/deleted since the folder changes were not tracked.
   def calculate_folder_action(tree)
-    folderAction = ''
-    unified_actions_in_same_iteration = true
-    tree.keys.sort.each do |changedFile|
-      if c = tree[changedFile][:c]
-        if c.action == 'A' && unified_actions_in_same_iteration && folderAction != 'D'
-          folderAction = 'A'
-        elsif c.action == 'D' && unified_actions_in_same_iteration && folderAction != 'A'
-          folderAction = 'D'
-        else
-          folderAction = ''
-          unified_actions_in_same_iteration = false
-        end
+    seen = Set.new
+    tree.each do |_, entry|
+      if folderStyle = change_action_mapping[entry[:c].try(:action)]
+        seen << folderStyle
       end
     end
-    return folderAction
+
+    seen.size == 1 ? seen.first : :open
   end
 
   def render_changes_tree(tree)
@@ -129,16 +130,7 @@ module RepositoriesHelper
                        path: path_param,
                        rev: @changeset.identifier)
 
-        folderAction = calculate_folder_action(s)
-        case folderAction
-        when 'A'
-          output << "<li class='#{style} icon icon-folder-add'>#{text}</li>"
-        when 'D'
-          output << "<li class='#{style} icon icon-folder-remove'>#{text}</li>"
-        else
-          output << "<li class='#{style} icon icon-folder-open'>#{text}</li>"
-        end
-
+        output << "<li class='#{style} icon icon-folder-#{calculate_folder_action(s)}'>#{text}</li>"
         output << render_changes_tree(s)
       elsif c = tree[file][:c]
         style << " change-#{c.action}"
