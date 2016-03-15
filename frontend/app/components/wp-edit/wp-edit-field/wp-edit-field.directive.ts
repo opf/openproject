6c6bd49aa3e1126cd0c98fe530f78ea99e4888d8
Oliver Günther <mail@oliverguenther.de>
2016-03-15 10:10:06 +0100

    Add class states to outer td element
    Mark the td element with `-editable` when the wp-edit field is editable.

--- frontend/app/components/wp-edit/wp-edit-field/wp-edit-field.directive.ts
+++ frontend/app/components/wp-edit/wp-edit-field/wp-edit-field.directive.ts
@@ -39,7 +39,7 @@ export class WorkPackageEditFieldController {
 
   protected _active:boolean = false;
 
-  constructor(protected wpEditField:WorkPackageEditFieldService) {
+  constructor(protected wpEditField:WorkPackageEditFieldService, protected $element) {
   }
 
   public get workPackage() {
@@ -67,6 +67,15 @@ export class WorkPackageEditFieldController {
 
   public deactivate():boolean {
     return this._active = false;
+  }
+
+  public setErrorState(error = true) {
+    this.errorenous = error;
+    if (error) {
+      this.$element.addClass('-error');
+    } else {
+      this.$element.removeClass('-error');
+    }
   }
 
   protected setupField():ng.IPromise<any> {
@@ -99,6 +108,13 @@ function wpEditFieldLink(scope, element, attrs, controllers:[WorkPackageEditForm
   element.click(event => {
     event.stopImmediatePropagation();
   });
+
+  // Mark the td field if it is inline-editable
+  if (scope.vm.isEditable) {
+    element.addClass('-editable');
+  }
+
+  element.addClass(scope.vm.fieldName);
 }
 
 function wpEditField() {
