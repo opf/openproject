d04f3c0f07b38f08b8ecdd5500326b0735cfd9f7
Alex Dik <mail@alexdik.de>
2016-04-05 12:12:40 +0200

    Move field type directives into their own directory

--- frontend/app/components/wp-edit/field-types/wp-edit-text-field.module.ts
+++ frontend/app/components/wp-edit/field-types/wp-edit-text-field.module.ts
@@ -26,8 +26,8 @@
 // See doc/COPYRIGHT.rdoc for more details.
 // ++
 
-import {Field} from "./wp-edit-field.module";
+import {Field} from "../wp-edit-field/wp-edit-field.module";
 
 export class TextField extends Field {
-  public template:string = '/components/wp-edit/wp-edit-field/wp-edit-text-field.directive.html'
+  public template:string = '/components/wp-edit/field-types/wp-edit-text-field.directive.html'
 }
