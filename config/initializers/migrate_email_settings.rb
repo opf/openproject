d5a508753c9610feb99a9310ca08768c72fde3c2
Cyril Rohr <cyril.rohr@gmail.com>
2016-04-14 16:42:48 +0000

    Move email settings out of the YAMl configuration file, and into the Settings page

---
+++ config/initializers/migrate_email_settings.rb
@@ -0,0 +1,6 @@
+unless Setting.email_delivery_migrated?
+  Rails.logger.info "Migrating existing email settings to the settings table..."
+  OpenProject::Configuration.migrate_mailer_configuration!
+end
+
+OpenProject::Configuration.reload_mailer_configuration!
