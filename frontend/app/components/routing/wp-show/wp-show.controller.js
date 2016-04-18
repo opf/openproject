7fb2dfb468fa5f5107363f0da4c24a93f4b35f2e
Alex Dik <mail@alexdik.de>
2016-04-18 13:19:32 +0200

    Reformat WP show controller code

--- frontend/app/components/routing/wp-show/wp-show.controller.js
+++ frontend/app/components/routing/wp-show/wp-show.controller.js
@@ -30,29 +30,49 @@ angular
   .module('openproject.workPackages.controllers')
   .controller('WorkPackageShowController', WorkPackageShowController);
 
-function WorkPackageShowController($scope, $rootScope, $state, workPackage, I18n,
-    RELATION_TYPES, RELATION_IDENTIFIERS, $q, WorkPackagesHelper, PathHelper, UsersHelper,
-    WorkPackageService, CommonRelationsHandler,
-    ChildrenRelationsHandler, ParentRelationsHandler, WorkPackagesOverviewService,
-    WorkPackageFieldService, EditableFieldsState, WorkPackagesDisplayHelper, NotificationsService,
-    WorkPackageAuthorization, PERMITTED_MORE_MENU_ACTIONS, HookService, $window,
-    WorkPackageAttachmentsService, AuthorisationService, inplaceEditAll) {
+function WorkPackageShowController($scope,
+                                   $rootScope,
+                                   $state,
+                                   $window,
+                                   $q,
+                                   workPackage,
+                                   I18n,
+                                   RELATION_TYPES,
+                                   RELATION_IDENTIFIERS,
+                                   WorkPackagesHelper,
+                                   PathHelper,
+                                   UsersHelper,
+                                   WorkPackageService,
+                                   CommonRelationsHandler,
+                                   ChildrenRelationsHandler,
+                                   ParentRelationsHandler,
+                                   WorkPackagesOverviewService,
+                                   WorkPackageFieldService,
+                                   EditableFieldsState,
+                                   WorkPackagesDisplayHelper,
+                                   NotificationsService,
+                                   WorkPackageAuthorization,
+                                   PERMITTED_MORE_MENU_ACTIONS,
+                                   HookService,
+                                   WorkPackageAttachmentsService,
+                                   AuthorisationService,
+                                   inplaceEditAll) {
 
   $scope.editAll = inplaceEditAll;
   $scope.canEdit = EditableFieldsState.canEdit;
 
   //Show all attributes in Edit-Mode
-  $scope.$watch(function(){
+  $scope.$watch(function () {
     return inplaceEditAll.state;
-  },function(newState, oldState){
-    if(newState !== oldState){
+  }, function (newState, oldState) {
+    if (newState !== oldState) {
       vm.hideEmptyFields = !newState;
     }
   });
 
   // Listen to the event globally, as listeners are not necessarily
   // in the child scope
-  var refreshRequiredFunction = $rootScope.$on('workPackageRefreshRequired', function() {
+  var refreshRequiredFunction = $rootScope.$on('workPackageRefreshRequired', function () {
     refreshWorkPackage();
   });
   $scope.$on('$destroy', refreshRequiredFunction);
@@ -65,12 +85,12 @@ function WorkPackageShowController($scope, $rootScope, $state, workPackage, I18n
   // stuff copied from details toolbar directive...
   function getPermittedActions(authorization, permittedMoreMenuActions) {
     var permittedActions = authorization.permittedActionsWithLinks(permittedMoreMenuActions);
-    var augmentedActions = { };
+    var augmentedActions = {};
 
-    angular.forEach(permittedActions, function(permission) {
+    angular.forEach(permittedActions, function (permission) {
       var css = ['icon-' + permission.key];
 
-      this[permission.key] = { link: permission.link, css: css };
+      this[permission.key] = {link: permission.link, css: css};
     }, augmentedActions);
 
     return augmentedActions;
@@ -78,33 +98,35 @@ function WorkPackageShowController($scope, $rootScope, $state, workPackage, I18n
 
   function getPermittedPluginActions(authorization) {
     var pluginActions = [];
-    angular.forEach(HookService.call('workPackageDetailsMoreMenu'), function(action) {
+    angular.forEach(HookService.call('workPackageDetailsMoreMenu'), function (action) {
       pluginActions = pluginActions.concat(action);
     });
 
     var permittedPluginActions = authorization.permittedActionsWithLinks(pluginActions);
-    var augmentedPluginActions = { };
+    var augmentedPluginActions = {};
 
-    angular.forEach(permittedPluginActions, function(action) {
+    angular.forEach(permittedPluginActions, function (action) {
       var css = [].concat(action.css);
 
       if (css.length === 0) {
         css = ["icon-" + action.key];
       }
 
-      this[action.key] = { link: action.link, css: css };
+      this[action.key] = {link: action.link, css: css};
     }, augmentedPluginActions);
 
     return augmentedPluginActions;
   }
+
   function deleteSelectedWorkPackage() {
     var promise = WorkPackageService.performBulkDelete([$scope.workPackage.props.id], true);
 
-    promise.success(function() {
+    promise.success(function () {
       $state.go('work-packages.list', {projectPath: $scope.projectIdentifier});
     });
   }
-  $scope.triggerMoreMenuAction = function(action, link) {
+
+  $scope.triggerMoreMenuAction = function (action, link) {
     switch (action) {
       case 'delete':
         deleteSelectedWorkPackage();
@@ -116,7 +138,7 @@ function WorkPackageShowController($scope, $rootScope, $state, workPackage, I18n
   };
   var authorization = new WorkPackageAuthorization($scope.workPackage);
   $scope.permittedActions = angular.extend(getPermittedActions(authorization, PERMITTED_MORE_MENU_ACTIONS),
-                                           getPermittedPluginActions(authorization));
+    getPermittedPluginActions(authorization));
   $scope.actionsAvailable = Object.keys($scope.permittedActions).length > 0;
 
   // END stuff copied from details toolbar directive...
@@ -129,7 +151,7 @@ function WorkPackageShowController($scope, $rootScope, $state, workPackage, I18n
 
   function refreshWorkPackage() {
     WorkPackageService.getWorkPackage($scope.workPackage.props.id)
-      .then(function(workPackage) {
+      .then(function (workPackage) {
         setWorkPackageScopeProperties(workPackage);
         $scope.$broadcast('workPackageRefreshed');
       });
@@ -153,14 +175,14 @@ function WorkPackageShowController($scope, $rootScope, $state, workPackage, I18n
   $scope.outputError = outputError; // expose to child controllers
 
 
-  function setWorkPackageScopeProperties(workPackage){
+  function setWorkPackageScopeProperties(workPackage) {
     $scope.workPackage = workPackage;
     $scope.isWatched = workPackage.links.hasOwnProperty('unwatch');
     $scope.displayWatchButton = workPackage.links.hasOwnProperty('unwatch') ||
-                                workPackage.links.hasOwnProperty('watch');
+      workPackage.links.hasOwnProperty('watch');
 
     // watchers
-    if(workPackage.links.watchers) {
+    if (workPackage.links.watchers) {
       $scope.watchers = workPackage.embedded.watchers.embedded.elements;
     }
 
@@ -178,12 +200,12 @@ function WorkPackageShowController($scope, $rootScope, $state, workPackage, I18n
     $scope.attachments = workPackage.embedded.attachments.embedded.elements;
 
     // relations
-    $q.all(WorkPackagesHelper.getParent(workPackage)).then(function(parents) {
+    $q.all(WorkPackagesHelper.getParent(workPackage)).then(function (parents) {
       var relationsHandler = new ParentRelationsHandler(workPackage, parents, 'parent');
       $scope.wpParent = relationsHandler;
     });
 
-    $q.all(WorkPackagesHelper.getChildren(workPackage)).then(function(children) {
+    $q.all(WorkPackagesHelper.getChildren(workPackage)).then(function (children) {
       var relationsHandler = new ChildrenRelationsHandler(workPackage, children);
       $scope.wpChildren = relationsHandler;
     });
@@ -192,10 +214,10 @@ function WorkPackageShowController($scope, $rootScope, $state, workPackage, I18n
       $q.all(WorkPackagesHelper.getRelationsOfType(
         workPackage,
         RELATION_TYPES[key])
-      ).then(function(relations) {
+      ).then(function (relations) {
         var relationsHandler = new CommonRelationsHandler(workPackage,
-                                                          relations,
-                                                          RELATION_IDENTIFIERS[key]);
+          relations,
+          RELATION_IDENTIFIERS[key]);
         $scope[key] = relationsHandler;
       });
     }
@@ -207,15 +229,17 @@ function WorkPackageShowController($scope, $rootScope, $state, workPackage, I18n
     }
   }
 
-  $scope.toggleWatch = function() {
+  $scope.toggleWatch = function () {
     // Toggle early to avoid delay.
     $scope.isWatched = !$scope.isWatched;
     WorkPackageService.toggleWatch($scope.workPackage)
-                      .then(function() { refreshWorkPackage() }, outputError);
+      .then(function () {
+        refreshWorkPackage()
+      }, outputError);
   };
 
   //TODO: Remove
-  $scope.canViewWorkPackageWatchers = function() {
+  $scope.canViewWorkPackageWatchers = function () {
     return !!($scope.workPackage && $scope.workPackage.embedded.watchers !== undefined);
   };
 
@@ -228,11 +252,11 @@ function WorkPackageShowController($scope, $rootScope, $state, workPackage, I18n
 
   function getFocusAnchorLabel(tab, workPackage) {
     var tabLabel = I18n.t('js.work_packages.tabs.' + tab),
-        params = {
-          tab: tabLabel,
-          type: workPackage.embedded.type.props.name,
-          subject: workPackage.props.subject
-        };
+      params = {
+        tab: tabLabel,
+        type: workPackage.embedded.type.props.name,
+        subject: workPackage.props.subject
+      };
 
     return I18n.t('js.label_work_package_details_you_are_here', params);
   }
@@ -249,11 +273,11 @@ function WorkPackageShowController($scope, $rootScope, $state, workPackage, I18n
   vm.hideEmptyFields = true;
   vm.workPackage = $scope.workPackage;
 
-  vm.shouldHideGroup = function(group) {
+  vm.shouldHideGroup = function (group) {
     return WorkPackagesDisplayHelper.shouldHideGroup(vm.hideEmptyFields,
-                                                     vm.groupedFields,
-                                                     group,
-                                                     vm.workPackage);
+      vm.groupedFields,
+      group,
+      vm.workPackage);
   };
   vm.isFieldHideable = WorkPackagesDisplayHelper.isFieldHideable;
   vm.getLabel = WorkPackagesDisplayHelper.getLabel;
@@ -264,12 +288,12 @@ function WorkPackageShowController($scope, $rootScope, $state, workPackage, I18n
 
   activate();
 
-  WorkPackageAttachmentsService.hasAttachments(vm.workPackage).then(function(bool) {
+  WorkPackageAttachmentsService.hasAttachments(vm.workPackage).then(function (bool) {
     vm.filesExist = bool;
   });
 
   function activate() {
-    $scope.$watch('workPackage.schema', function(schema) {
+    $scope.$watch('workPackage.schema', function (schema) {
       if (schema) {
         WorkPackagesDisplayHelper.setFocus();
         vm.workPackage = $scope.workPackage;
@@ -277,25 +301,25 @@ function WorkPackageShowController($scope, $rootScope, $state, workPackage, I18n
     });
     vm.groupedFields = WorkPackagesOverviewService.getGroupedWorkPackageOverviewAttributes();
 
-    $scope.$watchCollection('vm.workPackage.form', function() {
+    $scope.$watchCollection('vm.workPackage.form', function () {
       var schema = WorkPackageFieldService.getSchema(vm.workPackage);
       var otherGroup = _.find(vm.groupedFields, {groupName: 'other'});
       otherGroup.attributes = [];
-      _.forEach(schema.props, function(prop, propName) {
+      _.forEach(schema.props, function (prop, propName) {
         if (propName.match(/^customField/)) {
           otherGroup.attributes.push(propName);
         }
       });
-      otherGroup.attributes.sort(function(a, b) {
-        var getLabel = function(field) {
+      otherGroup.attributes.sort(function (a, b) {
+        var getLabel = function (field) {
           return vm.getLabel(vm.workPackage, field);
         };
         var left = getLabel(a).toLowerCase(),
-            right = getLabel(b).toLowerCase();
+          right = getLabel(b).toLowerCase();
         return left.localeCompare(right);
       });
     });
-    $scope.$on('workPackageUpdatedInEditor', function() {
+    $scope.$on('workPackageUpdatedInEditor', function () {
       NotificationsService.addSuccess(I18n.t('js.notice_successful_update'));
     });
   }
