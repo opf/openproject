opf/openproject

commit 96535b53614129caba48c4d73cdcb14048ccbb49

Remove remainingFilterNames $filter (#4333)
This replaces the filter for remaining filter types, which is executed
many times for any digest and causes a significant performance impact
with a watch on the query and according changes when adding/removing a
filter.

frontend/app/components/query/query.service.js
@@ -333,6 +333,13 @@ function QueryConstructorService(Filter, Sortation, UrlParamsHelper, INITIALLY_S
       });
     },
 
+    getRemainingFilters: function() {
+      var activeFilters = _.indexBy(this.getActiveFilters(), function(f) { return f.modelName });
+      return _.filter(this.availableWorkPackageFilters, function(filter) {
+        return !activeFilters[filter.modelName];
+      });
+    },
+
     getActiveConfiguredFilters: function() {
       return this.getActiveFilters().filter(function(filter){
         return filter.isConfigured();

frontend/app/work_packages/directives/query-filters-directive.js
@@ -38,13 +38,21 @@ module.exports = function($timeout, FiltersHelper, I18n, ADD_FILTER_SELECT_INDEX
           scope.I18n = I18n;
           scope.localisedFilterName = FiltersHelper.localisedFilterName;
           scope.focusElementIndex;
+          scope.remainingFilterNames = [];
 
-          scope.$watch('filterToBeAdded', function(filterName) {
-            if (filterName) {
-              scope.query.addFilter(filterName);
+          scope.$watch('filterToBeAdded', function(filter) {
+            if (filter) {
+              scope.query.addFilter(filter.value);
               scope.filterToBeAdded = undefined;
               var index = scope.query.getActiveFilters().length;
               updateFilterFocus(index);
+              updateRemainingFilters();
+            }
+          });
+
+          scope.$watch('query.filters.length', function(len) {
+            if (len >= 0) {
+              updateRemainingFilters();
             }
           });
 
@@ -54,7 +62,19 @@ module.exports = function($timeout, FiltersHelper, I18n, ADD_FILTER_SELECT_INDEX
             scope.query.deactivateFilter(filter);
 
             updateFilterFocus(index);
+            updateRemainingFilters();
           };
+
+          function updateRemainingFilters() {
+            var remainingFilters = _.map(scope.query.getRemainingFilters(), function(filter) {
+              return {
+                value: filter.modelName,
+                name: FiltersHelper.localisedFilterName(filter)
+              };
+            });
+
+            scope.remainingFilterNames = _.sortBy(remainingFilters, 'name');
+          }
 
           function updateFilterFocus(index) {
             var activeFilterCount = scope.query.getActiveFilters().length;

frontend/app/templates/work_packages/query_filters.html
@@ -160,9 +160,7 @@
           id="add_filter_select"
           focus="{{focusElementIndex == -1}}"
           ng-model="filterToBeAdded"
-          ng-options="filterName as localisedFilterName(query.availableWorkPackageFilters[filterName])
-            for filterName
-            in query.availableWorkPackageFilters | remainingFilterNames:query.filters" />
+          ng-options="filter.name for filter in remainingFilterNames" />
         </select>
         <!-- TODO options | orderObjectBy: 'order' -->
       </div>

frontend/app/work_packages/filters/index.js
@@ -30,53 +30,4 @@ angular.module('openproject.workPackages.filters')
 
 .filter('allRowsChecked', ['WorkPackagesTableHelper', function(WorkPackagesTableHelper) {
   return WorkPackagesTableHelper.allRowsChecked;
-}])
-
-/**
- * filter
- * @name remainingFilterNames
- * @function
- *
- * @description
- * Gets a hash of available filters and selected filters and calculates the difference.
- * Returns the keys of those filters that haven't been selected sorted by the localised
- * filter names.
- *
- * @param {Object} availableFilters The set of available filters, stored with their
- *    filter name.
- * @param {Array} selectedFilters An array with the selected filters.
- *
- * @returns {Array} An array of the filter names of those available filters that haven't
- *    been selected, ordered by the corresponding filters localised names.
-*/
-.filter('remainingFilterNames', ['orderByFilter', 'FiltersHelper', function(orderByFilter, FiltersHelper) {
-
-  function subtractActiveFilters(filters, filtersToSubtract) {
-    var filterDiff = _.clone(filters);
-
-    angular.forEach(filtersToSubtract, function(filter) {
-      if(!filter.deactivated) delete filterDiff[filter.name];
-    });
-
-    return filterDiff;
-  }
-
-  function flattenFiltersHash(filtersHash) {
-    var flattenedHash = [];
-    angular.forEach(filtersHash, function(filterValues, filterName) {
-      flattenedHash.push(angular.extend({filterName: filterName}, filterValues));
-    });
-
-    return flattenedHash;
-  }
-
-  return function(availableFilters, selectedFilters) {
-    if(!availableFilters) return [];
-
-    var filters = subtractActiveFilters(availableFilters, selectedFilters);
-
-    return orderByFilter(flattenFiltersHash(filters), FiltersHelper.localisedFilterName).map(function(filter) {
-      return filter.filterName;
-    });
-  };
 }]);

frontend/tests/unit/tests/work_packages/filters/work-package-filters-test.js
@@ -1,85 +1,0 @@
-//-- copyright
-// OpenProject is a project management system.
-// Copyright (C) 2012-2015 the OpenProject Foundation (OPF)
-//
-// This program is free software; you can redistribute it and/or
-// modify it under the terms of the GNU General Public License version 3.
-//
-// OpenProject is a fork of ChiliProject, which is a fork of Redmine. The copyright follows:
-// Copyright (C) 2006-2013 Jean-Philippe Lang
-// Copyright (C) 2010-2013 the ChiliProject Team
-//
-// This program is free software; you can redistribute it and/or
-// modify it under the terms of the GNU General Public License
-// as published by the Free Software Foundation; either version 2
-// of the License, or (at your option) any later version.
-//
-// This program is distributed in the hope that it will be useful,
-// but WITHOUT ANY WARRANTY; without even the implied warranty of
-// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-// GNU General Public License for more details.
-//
-// You should have received a copy of the GNU General Public License
-// along with this program; if not, write to the Free Software
-// Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
-//
-// See doc/COPYRIGHT.rdoc for more details.
-//++
-
-/*jshint expr: true*/
-
-describe('Work package filters', function() {
-  var availableFilters, selectedFilters;
-
-  beforeEach(module('openproject.workPackages.filters'));
-
-  describe('remainingFilterNames', function() {
-    beforeEach(function(){
-      availableFilters = {
-        subject: {
-          locale_name: 'subject',
-          type: 'text'
-        },
-        created_at: {
-          locale_name: 'created_at',
-          type: 'date_past'
-        },
-      };
-
-      selectedFilters = [Factory.build('Filter', {name: 'subject'})];
-
-    });
-
-    it('should be defined', inject(function($filter) {
-      expect($filter('remainingFilterNames')).not.to.equal(null);
-    }));
-
-    it('should return the names of the remaining filters', inject(function($filter) {
-      expect($filter('remainingFilterNames')(availableFilters, selectedFilters)).to.eql(['created_at']);
-    }));
-
-    describe('when there are filter locales for the remaining filters', function() {
-      var I18n, t, selectedFilters;
-
-      beforeEach(inject(function(_I18n_){
-        I18n = _I18n_;
-        t = sinon.stub(I18n, 't');
-        selectedFilters = [];
-      }));
-
-      afterEach(inject(function() {
-        I18n.t.restore();
-      }));
-
-      it('respects the alphabetical locale ordering', inject(function($filter) {
-        t.withArgs('js.filter_labels.subject').returns('Betreff');
-        t.withArgs('js.filter_labels.created_at').returns('angelegt');
-
-        expect($filter('remainingFilterNames')(availableFilters, selectedFilters)).to.eql(['created_at', 'subject']);
-
-        expect(t).to.have.been.calledTwice;
-      }));
-    });
-  });
-
-});

commit 16c9db2f539414ee81594dc9d6ecab4eab885e08

Select query filter keys correctly
Fixes a regression from https://github.com/opf/openproject/pull/4333

frontend/app/components/query/query.service.js
@@ -334,9 +334,9 @@ function QueryConstructorService(Filter, Sortation, UrlParamsHelper, INITIALLY_S
     },
 
     getRemainingFilters: function() {
-      var activeFilters = _.indexBy(this.getActiveFilters(), function(f) { return f.modelName });
-      return _.filter(this.availableWorkPackageFilters, function(filter) {
-        return !activeFilters[filter.modelName];
+      var activeFilters = _.indexBy(this.getActiveFilters(), function(f) { return f.name });
+      return _.pick(this.availableWorkPackageFilters, function(filter, key) {
+        return !activeFilters[key];
       });
     },
 

frontend/app/work_packages/directives/query-filters-directive.js
@@ -42,7 +42,7 @@ module.exports = function($timeout, FiltersHelper, I18n, ADD_FILTER_SELECT_INDEX
 
           scope.$watch('filterToBeAdded', function(filter) {
             if (filter) {
-              scope.query.addFilter(filter.value);
+              scope.query.addFilter(filter.key);
               scope.filterToBeAdded = undefined;
               var index = scope.query.getActiveFilters().length;
               updateFilterFocus(index);
@@ -66,8 +66,9 @@ module.exports = function($timeout, FiltersHelper, I18n, ADD_FILTER_SELECT_INDEX
           };
 
           function updateRemainingFilters() {
-            var remainingFilters = _.map(scope.query.getRemainingFilters(), function(filter) {
+            var remainingFilters = _.map(scope.query.getRemainingFilters(), function(filter, key) {
               return {
+                key: key,
                 value: filter.modelName,
                 name: FiltersHelper.localisedFilterName(filter)
               };