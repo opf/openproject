96dae12f31d1aa9b7d1b23bd020b8abbf89b8e0c
Cyril Rohr <cyril.rohr@gmail.com>
2016-04-20 14:19:52 +0000

    Actually, don't reload the config either if the settings table does not exist

--- config/initializers/migrate_email_settings.rb
+++ config/initializers/migrate_email_settings.rb
@@ -2,11 +2,13 @@ OpenProject::Application.configure do
   config.after_initialize do
     # settings table may not exist when you run db:migrate at installation
     # time, so just ignore this block when that happens.
-    if Setting.settings_table_exists_yet? && !Setting.email_delivery_migrated?
-      Rails.logger.info "Migrating existing email settings to the settings table..."
-      OpenProject::Configuration.migrate_mailer_configuration!
-    end
+    if Setting.settings_table_exists_yet?
+      unless Setting.email_delivery_migrated?
+        Rails.logger.info "Migrating existing email settings to the settings table..."
+        OpenProject::Configuration.migrate_mailer_configuration!
+      end
 
-    OpenProject::Configuration.reload_mailer_configuration!
+      OpenProject::Configuration.reload_mailer_configuration!
+    end
   end
 end
