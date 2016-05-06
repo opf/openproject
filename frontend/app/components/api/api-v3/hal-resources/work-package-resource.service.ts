d6726eec787e0d12610e7826f363426db1161629
Alex Dik <mail@alexdik.de>
2016-05-06 11:09:39 +0200

    Move WP resource class to module scope

--- frontend/app/components/api/api-v3/hal-resources/work-package-resource.service.ts
+++ frontend/app/components/api/api-v3/hal-resources/work-package-resource.service.ts
@@ -26,22 +26,22 @@
 // See doc/COPYRIGHT.rdoc for more details.
 //++
 
-function wpResource(
-  HalResource:typeof op.HalResource,
-  apiWorkPackages,
-  NotificationsService:any,
-  $q:ng.IQService
+import {HalResource} from './hal-resource.service';
+import {opApiModule} from "../../../../angular-modules";
 
-) {
-  class WorkPackageResource extends HalResource {
-    private form;
-    public schema;
-    public id;
+var $q;
+var apiWorkPackages;
+var NotificationsService;
 
-    public static fromCreateForm(projectIdentifier?:string):ng.IPromise<WorkPackageResource> {
-      var deferred = $q.defer();
+class WorkPackageResource extends HalResource {
+  private form;
+  public schema;
+  public id;
 
-      apiWorkPackages.emptyCreateForm(projectIdentifier)
+  public static fromCreateForm(projectIdentifier?:string):ng.IPromise<WorkPackageResource> {
+    var deferred = $q.defer();
+
+    apiWorkPackages.emptyCreateForm(projectIdentifier)
       .then(resource => {
         var wp = new WorkPackageResource(resource.payload.$source, true);
 
@@ -57,154 +57,165 @@ function wpResource(
       })
       .catch(deferred.reject);
 
-      return deferred.promise;
-    }
-
-    public get isNew():boolean {
-      var id = Number(this.id);
-      return isNaN(id);
-    }
-
-    public requiredValueFor(fieldName):boolean {
-      var fieldSchema = this.schema[fieldName];
-      return !this[fieldName] && fieldSchema.writable && fieldSchema.required;
-    }
-
-    public allowedValuesFor(field):ng.IPromise<op.HalResource[]> {
-      var deferred = $q.defer();
-      this.getForm().then(form => {
-        const allowedValues = form.$embedded.schema[field].allowedValues;
-
-        if (Array.isArray(allowedValues)) {
-          deferred.resolve(allowedValues);
-        } else {
-          return allowedValues.$load().then(loadedValues => {
-            deferred.resolve(loadedValues.elements);
-          });
-        }
-      });
-
-      return deferred.promise;
-    }
-
-    public setAllowedValueFor(field, href) {
-      this.allowedValuesFor(field).then(allowedValues => {
-        this[field] = _.find(allowedValues, (entry:any) => (entry.href === href));
-      });
-    }
-
-    public getForm() {
-      if (!this.form) {
-        this.form = this.$links.update(this);
-        this.form.catch(error => {
-          NotificationsService.addError(error.data.message);
-        });
-      }
-      return this.form;
-    }
-
-    public getSchema() {
-      return this.getForm().then(form => {
-        const schema = form.$embedded.schema;
-
-        angular.forEach(schema, (field, name) => {
-          if (this[name] && field && field.writable && field.$isHal
-            && Array.isArray(field.allowedValues)) {
-
-            this[name] = _.where(field.allowedValues, {name: this[name].name})[0];
-          }
-        });
-
-        return schema;
-      });
-    }
-
-    public save() {
-      const plain = this.$plain();
-
-      delete plain.createdAt;
-      delete plain.updatedAt;
-
-      var deferred = $q.defer();
-
-      // Always resolve form to the latest form
-      // This way, we won't have to actively reset it.
-      this.form = this.$links.update(this.$source);
-
-      this.form
-        .catch(deferred.reject)
-        .then(form => {
-
-          // Override the current schema with
-          // the changes from API
-          this.schema = form.$embedded.schema;
-
-          // Merge attributes from form with resource
-          var payload = this.mergeWithForm(form);
-
-          this.saveResource(payload)
-            .then(workPackage => {
-              angular.extend(this, workPackage);
-
-              deferred.resolve(this);
-            }).catch((error) => {
-              deferred.reject(error);
-            });
-        });
-
-      return deferred.promise;
-    }
-
-    public get isLeaf():boolean {
-      var children = this.$links.children;
-      return !(children && children.length > 0);
-    }
-
-    public isParentOf(otherWorkPackage) {
-      return otherWorkPackage.parent.$links.self.$link.href ===
-        this.$links.self.$link.href;
-    }
-
-    public get isEditable():boolean {
-      return !!this.$links.update || this.isNew;
-    }
-
-    protected saveResource(payload):ng.IPromise<any> {
-      if (this.isNew) {
-        return apiWorkPackages.wpApiPath().post(payload);
-      }
-
-      return this.$links.updateImmediately(payload);
-    }
-
-    private mergeWithForm(form) {
-      var plainPayload = form.payload.$source;
-      var schema = form.$embedded.schema;
-
-      // Merge embedded properties from form payload
-      // Do not use properties on this, since they may be incomplete
-      // e.g., when switching to a type that requires a custom field.
-      angular.forEach(plainPayload, (_value, key) => {
-        if (typeof(schema[key]) === 'object' && schema[key]['writable'] === true) {
-          plainPayload[key] = this[key];
-        }
-      });
-
-      // Merged linked properties from form payload
-      angular.forEach(plainPayload._links, (_value, key) => {
-        if (this[key] && typeof(schema[key]) === 'object' && schema[key]['writable'] === true) {
-          var value = this[key].href === 'null' ? null : this[key].href;
-          plainPayload._links[key] = {href: value};
-        }
-      });
-
-      return plainPayload;
-    }
+    return deferred.promise;
   }
+
+  public get isNew():boolean {
+    var id = Number(this.id);
+    return isNaN(id);
+  }
+
+  public requiredValueFor(fieldName):boolean {
+    var fieldSchema = this.schema[fieldName];
+    return !this[fieldName] && fieldSchema.writable && fieldSchema.required;
+  }
+
+  public allowedValuesFor(field):ng.IPromise<op.HalResource[]> {
+    var deferred = $q.defer();
+    this.getForm().then(form => {
+      const allowedValues = form.$embedded.schema[field].allowedValues;
+
+      if (Array.isArray(allowedValues)) {
+        deferred.resolve(allowedValues);
+      } else {
+        return allowedValues.$load().then(loadedValues => {
+          deferred.resolve(loadedValues.elements);
+        });
+      }
+    });
+
+    return deferred.promise;
+  }
+
+  public setAllowedValueFor(field, href) {
+    this.allowedValuesFor(field).then(allowedValues => {
+      this[field] = _.find(allowedValues, (entry:any) => (entry.href === href));
+    });
+  }
+
+  public getForm() {
+    if (!this.form) {
+      this.form = this.$links.update(this);
+      this.form.catch(error => {
+        NotificationsService.addError(error.data.message);
+      });
+    }
+    return this.form;
+  }
+
+  public getSchema() {
+    return this.getForm().then(form => {
+      const schema = form.$embedded.schema;
+
+      angular.forEach(schema, (field, name) => {
+        if (this[name] && field && field.writable && field.$isHal
+          && Array.isArray(field.allowedValues)) {
+
+          this[name] = _.where(field.allowedValues, {name: this[name].name})[0];
+        }
+      });
+
+      return schema;
+    });
+  }
+
+  public save() {
+    const plain = this.$plain();
+
+    delete plain.createdAt;
+    delete plain.updatedAt;
+
+    var deferred = $q.defer();
+
+    // Always resolve form to the latest form
+    // This way, we won't have to actively reset it.
+    this.form = this.$links.update(this.$source);
+
+    this.form
+      .catch(deferred.reject)
+      .then(form => {
+
+        // Override the current schema with
+        // the changes from API
+        this.schema = form.$embedded.schema;
+
+        // Merge attributes from form with resource
+        var payload = this.mergeWithForm(form);
+
+        this.saveResource(payload)
+          .then(workPackage => {
+            angular.extend(this, workPackage);
+
+            deferred.resolve(this);
+          }).catch((error) => {
+          deferred.reject(error);
+        });
+      });
+
+    return deferred.promise;
+  }
+
+  public get isLeaf():boolean {
+    var children = this.$links.children;
+    return !(children && children.length > 0);
+  }
+
+  public isParentOf(otherWorkPackage) {
+    return otherWorkPackage.parent.$links.self.$link.href ===
+      this.$links.self.$link.href;
+  }
+
+  public get isEditable():boolean {
+    return !!this.$links.update || this.isNew;
+  }
+
+  protected saveResource(payload):ng.IPromise<any> {
+    if (this.isNew) {
+      return apiWorkPackages.wpApiPath().post(payload);
+    }
+
+    return this.$links.updateImmediately(payload);
+  }
+
+  private mergeWithForm(form) {
+    var plainPayload = form.payload.$source;
+    var schema = form.$embedded.schema;
+
+    // Merge embedded properties from form payload
+    // Do not use properties on this, since they may be incomplete
+    // e.g., when switching to a type that requires a custom field.
+    angular.forEach(plainPayload, (_value, key) => {
+      if (typeof(schema[key]) === 'object' && schema[key]['writable'] === true) {
+        plainPayload[key] = this[key];
+      }
+    });
+
+    // Merged linked properties from form payload
+    angular.forEach(plainPayload._links, (_value, key) => {
+      if (this[key] && typeof(schema[key]) === 'object' && schema[key]['writable'] === true) {
+        var value = this[key].href === 'null' ? null : this[key].href;
+        plainPayload._links[key] = {href: value};
+      }
+    });
+
+    return plainPayload;
+  }
+}
+
+function wpResource(_$q_:ng.IQService,
+                    _apiWorkPackages_,
+                    _NotificationsService_:any) {
+  $q = _$q_;
+  apiWorkPackages = _apiWorkPackages_;
+  NotificationsService = _NotificationsService_;
 
   return WorkPackageResource;
 }
 
-angular
-  .module('openproject.api')
-  .factory('WorkPackageResource', wpResource);
+opApiModule
+  .factory('WorkPackageResource', [
+    '$q',
+    'apiWorkPackages',
+    'NotificationsService',
+    wpResource
+  ]);
