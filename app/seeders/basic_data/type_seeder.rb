d3c23808e97b4b049721461a373cf3334eb978e1
Markus Kahl <machisuji@googlemail.com>
2016-05-11 08:01:45 +0100

    added visibility to type seed; refactoring (#4411)
    * added visibility to type seed

--- app/seeders/basic_data/type_seeder.rb
+++ app/seeders/basic_data/type_seeder.rb
@@ -44,19 +44,78 @@ module BasicData
       'Skipping types - already exists/configured'
     end
 
+    ##
+    # Returns the data of all types to seed.
+    #
+    # @return [Array<Hash>] List of attributes for each type.
     def data
       colors = PlanningElementTypeColor.all
       colors = colors.map { |c| { c.name =>  c.id } }.reduce({}, :merge)
+      visibility = visibility_data
 
-      [
-        { name: I18n.t(:default_type_task),       is_default: true,  color_id: colors[I18n.t(:default_color_grey)],        is_in_roadmap: true,  in_aggregation: false, is_milestone: false,  position: 1 },
-        { name: I18n.t(:default_type_milestone),  is_default: true,  color_id: colors[I18n.t(:default_color_green_light)], is_in_roadmap: false, in_aggregation: true, is_milestone: true,    position: 2 },
-        { name: I18n.t(:default_type_phase),      is_default: true,  color_id: colors[I18n.t(:default_color_blue_dark)],   is_in_roadmap: false, in_aggregation: true, is_milestone: false,   position: 3 },
-        { name: I18n.t(:default_type_feature),    is_default: true,  color_id: colors[I18n.t(:default_color_blue)],        is_in_roadmap: true,  in_aggregation: false, is_milestone: false,  position: 4 },
-        { name: I18n.t(:default_type_epic),       is_default: true,  color_id: colors[I18n.t(:default_color_orange)],      is_in_roadmap: true,  in_aggregation: true, is_milestone: false,   position: 5 },
-        { name: I18n.t(:default_type_user_story), is_default: true,  color_id: colors[I18n.t(:default_color_grey_dark)],   is_in_roadmap: true,  in_aggregation: false , is_milestone: false, position: 6 },
-        { name: I18n.t(:default_type_bug),        is_default: true,  color_id: colors[I18n.t(:default_color_red)],         is_in_roadmap: true,  in_aggregation: false , is_milestone: false, position: 7 }
-      ]
+      type_table.map do |name, values|
+        {
+          name:                 I18n.t("default_type_#{name}"),
+          position:             values[0],
+          is_default:           values[1],
+          color_id:             colors[I18n.t(values[2])],
+          is_in_roadmap:        values[3],
+          in_aggregation:       values[4],
+          is_milestone:         values[5],
+          attribute_visibility: visibility[name]
+        }
+      end
+    end
+
+    def type_names
+      [:task, :milestone, :phase, :feature, :epic, :user_story, :bug]
+    end
+
+    def type_table
+      { # position is_default color_id is_in_roadmap in_aggregation is_milestone
+        task:       [1, true, :default_color_grey,        true,  false, false],
+        milestone:  [2, true, :default_color_green_light, false, true,  true],
+        phase:      [3, true, :default_color_blue_dark,   false, true,  false],
+        feature:    [4, true, :default_color_blue,        true,  false, false],
+        epic:       [5, true, :default_color_orange,      true,  true,  false],
+        user_story: [6, true, :default_color_grey_dark,   true,  false, false],
+        bug:        [7, true, :default_color_red,         true,  false, false]
+      }
+    end
+
+    def visibility_table
+      map = ['hidden', 'default', 'visible']
+
+      table = { # columns correspond to #type_names above
+        estimated_time:  [1, 1, 1, 1, 1, 1, 1],
+        spent_time:      [1, 1, 1, 1, 1, 1, 1],
+        percentage_done: [1, 1, 1, 1, 1, 1, 1],
+        assignee:        [2, 1, 1, 2, 2, 2, 2],
+        responsible:     [1, 1, 1, 1, 1, 1, 1],
+        category:        [1, 1, 1, 1, 1, 1, 1],
+        version:         [1, 1, 1, 2, 2, 2, 2],
+        start_date:      [2, 2, 2, 1, 1, 1, 1], # mind that start_date and due_date will
+        due_date:        [2, 2, 2, 1, 1, 1, 1], # affect each other - they're shown together
+      }.map do |key, values|
+        [key, values.map { |i| map[i] }]
+      end
+
+      Hash[table]
+    end
+
+    def visibility_data
+      table = visibility_table
+
+      type_entries = type_names.map do |name|
+        field_entries = table.map do |field, visibilities|
+          i = type_names.index name
+          [field, table[field][i] || 'default']
+        end
+
+        [name, Hash[field_entries]]
+      end
+
+      Hash[type_entries]
     end
   end
 end
