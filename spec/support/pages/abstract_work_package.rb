7516b8a10f806a5ba58a1c9bad522aa36439b3c8
Oliver Günther <mail@oliverguenther.de>
2016-05-13 15:42:47 +0200

    Retry for ensure_page_loaded
    This is an ugly hack due to the multiple loading of legacy
    HALAPIResources and should be removed asap.

--- spec/support/pages/abstract_work_package.rb
+++ spec/support/pages/abstract_work_package.rb
@@ -51,8 +51,16 @@ module Pages
     end
 
     def ensure_page_loaded
-      expect(page).to have_selector('.work-package-details-activities-activity-contents .user',
-                                    text: work_package.journals.last.user.name)
+      tries = 0
+      begin
+        expect(page).to have_selector('.work-package-details-activities-activity-contents .user',
+                                      text: work_package.journals.last.user.name)
+      rescue ::Selenium::WebDriver::Error::StaleElementReferenceError => e
+        # HACK This error may happen since activities are loaded several times
+        # in the old resource, and may cause a reload.
+        tries += 1
+        retry unless tries > 5
+      end
     end
 
     def expect_attributes(attribute_expectations)
