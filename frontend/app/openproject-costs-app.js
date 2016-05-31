295143360b8f6bd4b3a609b2469d79f28a17978b
ulferts <j.ulferts@finn.de>
2016-05-31 11:59:57 +0200

    use core's I18n load mechanism (#240)

--- frontend/app/openproject-costs-app.js
+++ frontend/app/openproject-costs-app.js
@@ -27,11 +27,10 @@
 //++
 
 // load all js locales
+var LoadLocales = require('app/plugins/load-locales.module').LoadLocales;
 var localeFiles = require.context('../../config/locales', false, /js-[\w|-]{2,5}\.yml$/);
-localeFiles.keys().forEach(function(localeFile) {
-  var locale = localeFile.match(/js-([\w|-]{2,5})\.yml/)[1];
-  I18n.addTranslations(locale, localeFiles(localeFile)[locale]);
-});
+
+LoadLocales.files(localeFiles);
 
 var CostsByTypeDisplayField = require('./components/wp-display/field-types/wp-display-costs-by-type-field.module').CostsByTypeDisplayField;
 
