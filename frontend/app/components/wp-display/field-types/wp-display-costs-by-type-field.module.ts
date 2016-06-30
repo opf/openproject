49e8e05b8a85cc82489ada8f28771fd7568d3e64
Jens Ulferts <j.ulferts@finn.de>
2016-06-30 10:54:24 +0200

    define TypeScript interface

--- frontend/app/components/wp-display/field-types/wp-display-costs-by-type-field.module.ts
+++ frontend/app/components/wp-display/field-types/wp-display-costs-by-type-field.module.ts
@@ -28,12 +28,23 @@
 
 import {DisplayField} from 'app/components/wp-display/wp-display-field/wp-display-field.module';
 
+interface ICostsByType {
+  $source: {
+    _links: {
+      costType: {
+        title: string;
+      }
+    }
+  };
+  spentUnits: number;
+}
+
 export class CostsByTypeDisplayField extends DisplayField {
 
   isManualRenderer = true;
 
   public get valueString() {
-    return  _.map(this.value.elements, val => {
+    return  _.map(this.value.elements, (val: ICostsByType) => {
       return val.spentUnits + ' ' + val.$source._links.costType.title;
     }).join(', ');
   };
