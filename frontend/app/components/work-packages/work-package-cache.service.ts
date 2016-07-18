02b077dd897d9dd73bb6b77269225b4b7ec93019
Roman Roelofsen <romanroe@gmail.com>
2016-07-18 10:49:37 +0200

    Added API to WorkPackageCacheService: Observable for new work package

--- frontend/app/components/work-packages/work-package-cache.service.ts
+++ frontend/app/components/work-packages/work-package-cache.service.ts
@@ -37,13 +37,20 @@ export class WorkPackageCacheService {
 
   private workPackageCache: {[id: number]: WorkPackageResource} = {};
 
-  workPackagesSubject = new Rx.ReplaySubject<{[id: number]: WorkPackageResource}>(1);
+  private workPackagesSubject = new Rx.ReplaySubject<{[id: number]: WorkPackageResource}>(1);
+
+  private newWorkPackageCreatedSubject = new Rx.Subject<WorkPackageResource>();
 
   /*@ngInject*/
   constructor(private $rootScope: IScope, private apiWorkPackages: ApiWorkPackagesService) {
   }
 
+  newWorkPackageCreated(wp: WorkPackageResource) {
+    this.newWorkPackageCreatedSubject.onNext(wp);
+  }
+
   updateWorkPackage(wp: WorkPackageResource) {
+
     this.updateWorkPackageList([wp]);
   }
 
@@ -62,11 +69,11 @@ export class WorkPackageCacheService {
   /**
    * Invalidate a set of links in the given work package.
    * This is a temporary fix for un-/reloading a known set of links.
-   * 
+   *
    * @param workPackage
    * @param args A list of links to forcefully $load
    */
-  loadWorkPackageLinks(workPackage, ...args:string[]) {
+  loadWorkPackageLinks(workPackage, ...args: string[]) {
     args.forEach((arg) => {
       workPackage[arg].$load(true);
     });
@@ -81,8 +88,12 @@ export class WorkPackageCacheService {
     }
 
     return this.workPackagesSubject
-        .map(cache => cache[workPackageId])
-        .filter(wp => wp !== undefined);
+      .map(cache => cache[workPackageId])
+      .filter(wp => wp !== undefined);
+  }
+
+  onNewWorkPackage(): Obversable<WorkPackageResource> {
+    return this.newWorkPackageCreatedSubject.asObservable();
   }
 
 }
