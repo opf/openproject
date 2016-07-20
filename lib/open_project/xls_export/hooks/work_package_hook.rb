e8136279c6c05924fae997966ea4bd43f6f7a40d
Markus Kahl <machisuji@gmail.com>
2016-07-20 16:17:23 +0100

    added xls with relations link to hook though it is clear it is not used

--- lib/open_project/xls_export/hooks/work_package_hook.rb
+++ lib/open_project/xls_export/hooks/work_package_hook.rb
@@ -2,12 +2,22 @@ module PrintableIssues
   class IssueHook  < Redmine::Hook::ViewListener
     # Add XLS format link below issue list
     def view_work_packages_index_other_formats(context)
-      (context[:link_formatter].link_to 'XLS', :url => { :project_id => context[:project] }) +
-      ' ' +
-      (context[:link_formatter].link_to I18n.t(:xls_with_descriptions),
-                                        :url => { :project_id => context[:project],
-                                                  :show_descriptions => true,
-                                                  :format => 'xls' })
+      links = [
+        link_to_xls(I18n.t(:label_xls), context),
+        link_to_xls(I18n.t(:label_xls_with_descriptions), context, show_descriptions: true),
+        link_to_xls(I18n.t(:label_xls_with_relations), context, show_relations: true)
+      ]
+
+      links.join(" ")
+    end
+
+    def link_to_xls(context, label, options = {})
+      url = {
+        project_id: context[:project],
+        format: "xls"
+      }
+
+      context[:link_formatter].link_to label, url: url.merge(options)
     end
   end
 end
