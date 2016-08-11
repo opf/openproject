822afabcbd022db647c7c4e7d65f8368da194392
Oliver Günther <mail@oliverguenther.de>
2016-08-11 13:48:41 +0200

    [23715] Keep current project context as stateParam
    This adds a global projectPath parameter to the router configuration,
    which allows us to keep the current project context without explicitly
    specifying it as a parameter.
    
    We do not have frontend movements outside the project context without a
    hard refresh. Should that come, one simply has to explictly specify
    `projectPath: null` to the route request.

--- frontend/app/components/routing/ui-router.config.ts
+++ frontend/app/components/routing/ui-router.config.ts
@@ -95,6 +95,12 @@ openprojectModule
       .state('work-packages', {
         url: '',
         abstract: true,
+        params: {
+          // value: null makes the parameter optional
+          // squash: true avoids duplicate slashes when the paramter is not provided
+          projectPath: {value: null, squash: true},
+          projects: {value: null, squash: true},
+        },
         templateUrl: '/components/routing/main/work-packages.html',
         controller: 'WorkPackagesController'
       })
@@ -107,12 +113,6 @@ openprojectModule
         reloadOnSearch: false,
         onEnter: () => angular.element('body').addClass('full-create'),
         onExit: () => angular.element('body').removeClass('full-create'),
-        params: {
-          // value: null makes the parameter optional
-          // squash: true avoids duplicate slashes when the paramter is not provided
-          projectPath: {value: null, squash: true},
-          projects: {value: null, squash: true}
-        }
       })
 
       .state('work-packages.copy', {
@@ -128,11 +128,6 @@ openprojectModule
 
       .state('work-packages.edit', {
         url: '/{projects}/{projectPath}/work_packages/{workPackageId:[0-9]+}/edit',
-        params: {
-          projectPath: {value: null, squash: true},
-          projects: {value: null, squash: true},
-        },
-
         onEnter: ($state, $timeout, $stateParams, wpEditModeState:WorkPackageEditModeStateService) => {
           wpEditModeState.start();
           // Transitioning to a new state may cause a reported issue
@@ -143,7 +138,7 @@ openprojectModule
       })
 
       .state('work-packages.show', {
-        url: '/work_packages/{workPackageId:[0-9]+}?query_id&query_props',
+        url: '/work_packages/{workPackageId:[0-9]+}',
         templateUrl: '/components/routing/wp-show/wp.show.html',
         controller: 'WorkPackageShowController',
         controllerAs: '$ctrl',
@@ -173,8 +168,6 @@ openprojectModule
         params: {
           // value: null makes the parameter optional
           // squash: true avoids duplicate slashes when the paramter is not provided
-          projectPath: {value: null, squash: true},
-          projects: {value: null, squash: true},
           query_id: {value: null},
           query_props: {value: null}
         },
