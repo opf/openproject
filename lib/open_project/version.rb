7e73ac207dc5f5d98d4f10c0a227fab1d6212b41
Oliver Günther <mail@oliverguenther.de>
2016-08-12 19:28:23 +0200

    Bump VERSION to 5.0.21

--- lib/open_project/version.rb
+++ lib/open_project/version.rb
@@ -34,7 +34,7 @@ module OpenProject
   module VERSION #:nodoc:
     MAJOR = 5
     MINOR = 0
-    PATCH = 20
+    PATCH = 21
     TINY  = PATCH # Redmine compat
 
     # Used by semver to define the special version (if any).
