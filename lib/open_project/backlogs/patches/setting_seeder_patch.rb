acf5bd8ad4378ed0f79b5fa1414b0189e510bd19
Markus Kahl <machisuji@gmail.com>
2016-08-19 12:29:56 +0100

    extracted new seed data from patch; fixed missing backlogs setting seed

--- lib/open_project/backlogs/patches/setting_seeder_patch.rb
+++ lib/open_project/backlogs/patches/setting_seeder_patch.rb
@@ -39,6 +39,8 @@ module OpenProject::Backlogs::Patches::SettingSeederPatch
   end
 
   module InstanceMethods
+    ##
+    # Overrides data to include backlogs as a default project module.
     def data
       original_data = super
 
@@ -47,53 +49,6 @@ module OpenProject::Backlogs::Patches::SettingSeederPatch
       end
 
       original_data
-    end
-
-    def seed_data!
-      backlogs_init_setting! unless backlogs_configured?
-
-      super
-    end
-
-    module_function
-
-    def backlogs_init_setting!
-      Setting[backlogs_setting_name] = backlogs_setting_value
-    end
-
-    def backlogs_configured?
-      setting = Hash(Setting[backlogs_setting_name])
-      setting['story_types'].present? && setting['task_type'].present?
-    end
-
-    def backlogs_setting_name
-      'plugin_openproject_backlogs'
-    end
-
-    def backlogs_setting_value
-      {
-        "story_types" => backlogs_types.map(&:id),
-        "task_type" => backlogs_task_type.try(:id),
-        "points_burn_direction" => "up",
-        "wiki_template" => ""
-      }
-    end
-
-    def backlogs_types
-      Type.where(name: backlogs_type_names)
-    end
-
-    def backlogs_type_names
-      [:default_type_feature, :default_type_epic, :default_type_user_story, :default_type_bug]
-        .map { |code| I18n.t(code) }
-    end
-
-    def backlogs_task_type
-      Type.find_by(name: backlogs_task_type_name)
-    end
-
-    def backlogs_task_type_name
-      I18n.t(:default_type_task)
     end
   end
 end
