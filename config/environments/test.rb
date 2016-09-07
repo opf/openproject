78ab8a9cfdf7f3f1e91b75f9616be08c2af3c563
Oliver Günther <mail@oliverguenther.de>
2016-09-07 21:05:00 +0200

    Rename serve_static_files

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -45,7 +45,7 @@ OpenProject::Application.configure do
   config.eager_load = false
 
   # Configure static asset server for tests with Cache-Control for performance.
-  config.serve_static_files   = true
+  config.public_file_server.enabled   = true
   config.static_cache_control = 'public, max-age=3600'
 
   # Show full error reports and disable caching.
