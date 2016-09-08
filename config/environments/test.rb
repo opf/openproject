04ba32d71cd1234bcc85277754d76983620561ca
Oliver Günther <mail@oliverguenther.de>
2016-09-08 08:58:36 +0200

    Fix blatant mistakes

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -46,7 +46,7 @@ OpenProject::Application.configure do
 
   # Configure static asset server for tests with Cache-Control for performance.
   config.public_file_server.enabled   = true
-  config.config.public_file_server.headers = { 'Cache-Control' => 'public, max-age=3600' }
+  config.public_file_server.headers = { 'Cache-Control' => 'public, max-age=3600' }
 
   # Show full error reports and disable caching.
   config.consider_all_requests_local       = true
