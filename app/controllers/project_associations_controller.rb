1c5e73a9938c85ab5f94419944750afad80f3502
Jens Ulferts <j.ulferts@finn.de>
2016-09-12 10:14:40 +0200

    handle non existent param

--- app/controllers/project_associations_controller.rb
+++ app/controllers/project_associations_controller.rb
@@ -96,7 +96,9 @@ class ProjectAssociationsController < ApplicationController
     @project_association = @project.project_associations.find(params[:id])
     check_visibility
 
-    @project_association.description =  params[:project_association][:description]
+    if params[:project_association]
+      @project_association.description = params[:project_association][:description]
+    end
 
     check_visibility # since projects may not be edited by mass-assignement,
     # this check should be superfluous ... but who knows?!?
