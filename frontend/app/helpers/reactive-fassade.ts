d2d5ff0eefd300e1d99ad2c31bba5c89605c840f
Roman Roelofsen <romanroe@gmail.com>
2016-09-12 16:45:09 +0200

    Improved reactive fassade

--- frontend/app/helpers/reactive-fassade.ts
+++ frontend/app/helpers/reactive-fassade.ts
@@ -1,4 +1,4 @@
-import {scopedObservable, runInScopeDigest} from "./angular-rx-utils";
+import {scopedObservable} from "./angular-rx-utils";
 import Observable = Rx.Observable;
 import IScope = angular.IScope;
 import IPromise = Rx.IPromise;
@@ -9,126 +9,27 @@ export abstract class StoreElement {
 
   public logFn: (msg: any) => any = null;
 
-  log(msg: string) {
+  log(msg: string, reason?: string) {
+    reason = reason === undefined ? "" : " // " + reason;
     if (this.pathInStore && this.logFn) {
-      this.logFn("[" + this.pathInStore + "] " + msg);
+      this.logFn("[" + this.pathInStore + "] " + msg + reason);
     }
   }
 }
 
-
-// type LoaderFn = () => IPromise<any>;
-//
-// export class LoadingState<T> extends StoreElement {
-//
-//   private counter = 0;
-//
-//   private subject = new Rx.ReplaySubject<[number, T]>(1);
-//
-//   private observable: Observable<[number, T]>;
-//
-//   private lastLoadRequestedTimestamp: number = 0;
-//
-//   public minimumTimeoutInMs: number;
-//
-//   private loaderFn: LoaderFn = (): any => {
-//     throw "loaderFn not defined";
-//   };
-//
-//   constructor(minimumTimeoutInMs: number = 5000) {
-//     super();
-//     this.minimumTimeoutInMs = minimumTimeoutInMs;
-//     this.observable = this.subject
-//       .filter(val => val[1] !== null);
-//   }
-//
-//   public clear() {
-//     this.log("clear");
-//     this.lastLoadRequestedTimestamp = 0;
-//     this.setState(this.counter++, null);
-//   }
-//
-//   public setLoaderFn(loaderFn: LoaderFn) {
-//     this.log("setLoaderFn");
-//     this.loaderFn = loaderFn;
-//   }
-//
-//   // Force
-//
-//   public forceLoadAndGet(scope: IScope): IPromise<T> {
-//     const currentCounter = this.counter++;
-//     this.lastLoadRequestedTimestamp = Date.now();
-//
-//     this.log("loader called");
-//     return this.loaderFn().then(val => {
-//       runInScopeDigest(scope, () => {
-//         this.setState(currentCounter, val);
-//       });
-//       return val;
-//     });
-//   }
-//
-//   public forceLoadAndObserve(scope: IScope): Observable<T> {
-//     const currentCounter = this.counter;
-//     this.forceLoadAndGet(null);
-//     return this.scopedObservable(scope)
-//       .skipWhile((val) => {
-//         return val[0] < currentCounter;
-//       })
-//       .map(val => val[1]);
-//   }
-//
-//   // Maybe
-//
-//   public maybeLoadAndGet(scope: IScope): IPromise<T> {
-//     if (this.isTimeoutPassed()) {
-//       return this.forceLoadAndGet(scope);
-//     } else {
-//       return this.get();
-//     }
-//   }
-//
-//   public maybeLoadAndObserve(scope: IScope): Observable<T> {
-//     this.maybeLoadAndGet(null);
-//     return this.observe(scope);
-//   }
-//
-//   // Passive
-//
-//   public get(): IPromise<T> {
-//     return this.observable.take(1).map(val => val[1]).toPromise();
-//   }
-//
-//   public observe(scope: IScope): Observable<T> {
-//     return this.scopedObservable(scope).map(val => val[1]);
-//   }
-//
-//   // --------------------------------------------------------------
-//
-//   private setState(counter: number, val: T) {
-//     this.subject.onNext([counter, val]);
-//   }
-//
-//   private isTimeoutPassed(): boolean {
-//     return (Date.now() - this.lastLoadRequestedTimestamp) > this.minimumTimeoutInMs;
-//   }
-//
-//   private scopedObservable(scope: IScope): Observable<[number, T]> {
-//     return scope ? scopedObservable(scope, this.observable) : this.observable;
-//   }
-// }
-
 interface PromiseLike<T> {
-  then(callback: (value: T) => any): any;
+  then(successCallback: (value: T) => any, errorCallback: (value: T) => any): any;
 }
 
 export class State<T> extends StoreElement {
 
   private timestampOfLastValue = -1;
 
-  private putFromPromiseCalled = false;
+  private timestampOfLastPromise = -1;
 
-  private subject = new Rx.ReplaySubject<T>(1);
+  private subject = new Rx.BehaviorSubject<T>(null);
+
+  private cleared = new Rx.Subject();
 
   private observable: Observable<T>;
 
@@ -142,28 +43,42 @@ export class State<T> extends StoreElement {
    * a value is awaited from a promise (via putFromPromise).
    */
   public isPristine(): boolean {
-    return this.timestampOfLastValue === -1 && !this.putFromPromiseCalled;
+    return this.timestampOfLastValue === -1 && this.timestampOfLastPromise === -1;
   }
 
-  public clear(): this {
-    this.log("State#clear()");
+  public isValueOrPromiseOlderThan(timeoutInMs: number) {
+    const ageValue = Date.now() - this.timestampOfLastValue;
+    const agePromise = Date.now() - this.timestampOfLastPromise;
+    return ageValue > timeoutInMs && agePromise > timeoutInMs;
+  }
+
+  public clear(reason?: string): this {
+    this.log("State#clear()", reason);
     this.setState(null);
     return this;
   }
 
-  public put(value: T): this {
-    this.log("State#put(...)");
+  public put(value: T, reason?: string): this {
+    this.log("State#put(...)", reason);
     this.setState(value);
     return this;
   }
 
   public putFromPromise(promise: PromiseLike<T>): this {
     this.clear();
-    this.putFromPromiseCalled = true;
-    promise.then((value: T) => {
-      this.log("State#putFromPromise(...)");
-      this.setState(value);
-    });
+    this.timestampOfLastPromise = Date.now();
+    promise.then(
+      // success
+      (value: T) => {
+        this.log("State#putFromPromise(...)");
+        this.setState(value);
+      },
+      // error
+      () => {
+        this.log("State#putFromPromise ERROR");
+        this.timestampOfLastPromise = -1;
+      }
+    );
     return this;
   }
 
@@ -182,9 +97,17 @@ export class State<T> extends StoreElement {
     return this.scopedObservable(scope);
   }
 
+  public observeCleared(scope: IScope): Observable<any> {
+    return this.cleared.asObservable();
+  }
+
   private setState(val: T) {
     this.timestampOfLastValue = val !== null && val !== undefined ? Date.now() : -1;
     this.subject.onNext(val);
+
+    if (val === null || val === undefined) {
+      this.cleared.onNext(null);
+    }
   }
 
   private scopedObservable(scope: IScope): Observable<T> {
@@ -239,50 +162,3 @@ function traverse(elem: any, path: string, logFn: (msg: any) => any) {
 export function initStates(states: any, logFn?: (msg: any) => any) {
   return traverse(states, "", logFn);
 }
-
-
-/////////////////////////////////////////////////////////////////
-
-// States
-// const states = {
-//     a1: new State<number>(),
-//     complex: {
-//         b1: new State<string>(),
-//         b2: new State<string>(),
-//         b3: new MultiState<string>()
-//     }
-// };
-// initStates(states);
-// setStateLogFunction(log => console.trace(log));
-
-// Subscriber
-// states.complex.b2.observe(null).subscribe(val => {
-//     console.log("1:" + val);
-// });
-
-// states.complex.b3.get(1).then(val => {
-//     console.log("2:" + val);
-// });
-
-
-// in Actions
-// states.complex.b2.put("a");
-
-/////////////////////////////////////////////////////////////////
-
-
-// const ms = new MultiState<string>();
-//
-// ms.get("1").get().then(val => {
-//   console.log("a:" + val);
-// });
-//
-// ms.get("1").observe(null).subscribe(val => {
-//   console.log("b:" + val);
-// });
-//
-// ms.put("1", "aaa");
-// ms.put("2", "bbb");
-// ms.put("3", "ccc");
-// ms.put("1", "aaaaaaaaaa");
-
