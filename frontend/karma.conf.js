679b4e53699b56903a5bbfdf212c4b8754b979f1
Roman Roelofsen <romanroe@gmail.com>
2016-09-26 15:59:59 +0200

    fixed tests

--- frontend/karma.conf.js
+++ frontend/karma.conf.js
@@ -109,7 +109,7 @@ module.exports = function (config) {
 
     // start these browsers
     // available browser launchers: https://npmjs.org/browse/keyword/karma-launcher
-    browsers: ['PhantomJS'/*, 'Firefox'*/],
+    browsers: ['PhantomJS', 'Firefox'],
 
 
     // Continuous Integration mode
