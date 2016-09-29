d48b56d0b163e2b1e27143a0e8f9582aaf0d16fe
Roman Roelofsen <romanroe@gmail.com>
2016-09-29 16:52:27 +0200

    WIP work package timeline

--- frontend/app/components/wp-table/wp-row/wp-row.directive.js
+++ frontend/app/components/wp-table/wp-row/wp-row.directive.js
@@ -29,35 +29,41 @@
 
 var WorkPackagesTimelineCell = require("../timeline/wp-timeline-cell").WorkPackageTimelineCell;
 
-function wpRow(WorkPackagesTableService){
+function wpRow(WorkPackagesTableService) {
   function setCheckboxTitle(scope) {
     scope.checkboxTitle = I18n.t('js.description_select_work_package',
-                                 { id: scope.workPackage.id });
+      {id: scope.workPackage.id});
   }
 
   function setHiddenWorkPackageLabel(scope) {
     scope.parentWorkPackageHiddenText = I18n.t('js.description_subwork_package',
-                                               { id: scope.row.parent.object.id }) ;
+      {id: scope.row.parent.object.id});
   }
 
   return {
     restrict: 'A',
 
-    controller: function (states, $scope, $element) {
+    controller: function (states, $scope, $element, workPackageTimelineService) {
       "ngInject";
 
       var workPackageId = $scope.row.object.id;
       var timelineTd = $element.find("td.wp-timeline-cell")[0];
-      new WorkPackagesTimelineCell($scope, states, workPackageId, timelineTd).init();
+      new WorkPackagesTimelineCell(
+        workPackageTimelineService,
+        $scope,
+        states,
+        workPackageId,
+        timelineTd
+      ).render();
     },
 
-    link: function(scope) {
+    link: function (scope) {
       scope.workPackage = scope.row.object;
       setCheckboxTitle(scope);
 
       if (scope.row.parent) setHiddenWorkPackageLabel(scope);
 
-      scope.$watch('row.checked', function(checked, formerState) {
+      scope.$watch('row.checked', function (checked, formerState) {
         if (checked !== formerState) {
           WorkPackagesTableService.setAllRowsChecked(scope.rows, scope.row, checked);
         }
