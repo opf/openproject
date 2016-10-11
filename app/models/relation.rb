6c5177d8bf4bd2c4f2de4d669fb6a55d6cf5a496
Jens Ulferts <j.ulferts@finn.de>
2016-10-11 16:20:19 +0200

    fix reverse and sym of new relation types

--- app/models/relation.rb
+++ app/models/relation.rb
@@ -72,19 +72,19 @@ class Relation < ActiveRecord::Base
     },
     TYPE_INCLUDES => {
       name: :label_includes, sym_name: :label_includes, order: 8,
-      sym: TYPE_INCLUDES, reverse: TYPE_PARTOF
+      sym: TYPE_PARTOF
     },
     TYPE_PARTOF => {
       name: :label_part_of, sym_name: :label_part_of, order: 9,
-      sym: TYPE_PARTOF, reverse: TYPE_INCLUDES
+      sym: TYPE_INCLUDES, reverse: TYPE_INCLUDES
     },
     TYPE_REQUIRES => {
       name: :label_requires, sym_name: :label_requires, order: 10,
-      sym: TYPE_REQUIRES, reverse: TYPE_REQUIRED
+      sym: TYPE_REQUIRED
     },
     TYPE_REQUIRED => {
       name: :label_required, sym_name: :label_required, order: 11,
-      sym: TYPE_REQUIRED, reverse: TYPE_REQUIRES
+      sym: TYPE_REQUIRES, reverse: TYPE_REQUIRES
     }
   }.freeze
 
