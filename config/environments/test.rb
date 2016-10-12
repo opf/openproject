6388a708310a7cd5e8f0112d3d3a810cab4ca325
Oliver Günther <mail@oliverguenther.de>
2016-10-12 17:05:50 +0200

    Use eager load to mirror the production environment
    Explictly mirror the production environment regarding code loading.
    This reduces load time for a single spec by a bit, but that disadvantage quickly
    fades when running multiple specs.
    
    Also suggested by https://bibwild.wordpress.com/2016/02/18/struggling-towards-reliable-capybara-javascript-testing/

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -39,10 +39,11 @@ OpenProject::Application.configure do
   # and recreated between test runs. Don't rely on the data there!
   config.cache_classes = true
 
-  # Do not eager load code on boot. This avoids loading your whole application
-  # just for the purpose of running a single test. If you are using a tool that
-  # preloads Rails for running tests, you may have to set it to true.
-  config.eager_load = false
+  # Use eager load to mirror the production environment
+  config.eager_load = true
+
+  # This setting is false by default, but we define it explicitly
+  config.allow_concurrency = false
 
   # Configure static asset server for tests with Cache-Control for performance.
   config.public_file_server.enabled = true
