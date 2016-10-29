216d98e6185a23de0c7294ed9004cc3d5c64891a
Oliver Günther <mail@oliverguenther.de>
2016-10-29 22:53:54 +0200

    Don't access the file more than once in export spec

--- spec/features/work_packages/export_spec.rb
+++ spec/features/work_packages/export_spec.rb
@@ -52,7 +52,11 @@ describe 'work package export', type: :feature do
     allow(User).to receive(:current).and_return current_user
   end
 
+  subject { DownloadedFile.download_content }
+
   def export!
+    DownloadedFile::clear_downloads
+
     work_packages_page.ensure_loaded
     work_packages_page.open_settings!
 
@@ -85,13 +89,13 @@ describe 'work package export', type: :feature do
   it 'shows all work packages with the default filters', js: true, retry: 2 do
     export!
 
-    expect(DownloadedFile::download_content).to have_text(wp_1.description)
-    expect(DownloadedFile::download_content).to have_text(wp_2.description)
-    expect(DownloadedFile::download_content).to have_text(wp_3.description)
-    expect(DownloadedFile::download_content).to have_text(wp_4.description)
+    expect(subject).to have_text(wp_1.description)
+    expect(subject).to have_text(wp_2.description)
+    expect(subject).to have_text(wp_3.description)
+    expect(subject).to have_text(wp_4.description)
 
     # results are ordered by ID (desc) and not grouped by type
-    expect(DownloadedFile::download_content.scan(/Type (A|B)/).flatten).to eq %w(A B A A)
+    expect(subject.scan(/Type (A|B)/).flatten).to eq %w(A B A A)
   end
 
   it 'shows all work packages grouped by ', js: true, retry: 2 do
@@ -104,13 +108,13 @@ describe 'work package export', type: :feature do
 
     export!
 
-    expect(DownloadedFile::download_content).to have_text(wp_1.description)
-    expect(DownloadedFile::download_content).to have_text(wp_2.description)
-    expect(DownloadedFile::download_content).to have_text(wp_3.description)
-    expect(DownloadedFile::download_content).to have_text(wp_4.description)
+    expect(subject).to have_text(wp_1.description)
+    expect(subject).to have_text(wp_2.description)
+    expect(subject).to have_text(wp_3.description)
+    expect(subject).to have_text(wp_4.description)
 
     # grouped by type
-    expect(DownloadedFile::download_content.scan(/Type (A|B)/).flatten).to eq %w(A A A B)
+    expect(subject.scan(/Type (A|B)/).flatten).to eq %w(A A A B)
   end
 
   it 'shows only the work package with the right progress if filtered this way', js: true, retry: 2 do
@@ -121,9 +125,9 @@ describe 'work package export', type: :feature do
 
     export!
 
-    expect(DownloadedFile::download_content).to have_text(wp_1.description)
-    expect(DownloadedFile::download_content).to_not have_text(wp_2.description)
-    expect(DownloadedFile::download_content).to_not have_text(wp_3.description)
+    expect(subject).to have_text(wp_1.description)
+    expect(subject).to_not have_text(wp_2.description)
+    expect(subject).to_not have_text(wp_3.description)
   end
 
   it 'shows only work packages of the filtered type', js: true, retry: 2 do
@@ -134,9 +138,9 @@ describe 'work package export', type: :feature do
 
     export!
 
-    expect(DownloadedFile::download_content).to_not have_text(wp_1.description)
-    expect(DownloadedFile::download_content).to_not have_text(wp_2.description)
-    expect(DownloadedFile::download_content).to have_text(wp_3.description)
+    expect(subject).to_not have_text(wp_1.description)
+    expect(subject).to_not have_text(wp_2.description)
+    expect(subject).to have_text(wp_3.description)
   end
 
   it 'exports selected columns', js: true, retry: 2 do
@@ -144,7 +148,7 @@ describe 'work package export', type: :feature do
 
     export!
 
-    expect(DownloadedFile::download_content).to have_text('Progress (%)')
-    expect(DownloadedFile::download_content).to have_text('25')
+    expect(subject).to have_text('Progress (%)')
+    expect(subject).to have_text('25')
   end
 end
