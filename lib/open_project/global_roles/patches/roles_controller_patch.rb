abb2f2905291ee963cb4122ad2ee71d00f6a4f6d
Oliver Günther <mail@oliverguenther.de>
2016-11-07 16:14:29 +0100

    Fix `.all` statement

--- lib/open_project/global_roles/patches/roles_controller_patch.rb
+++ lib/open_project/global_roles/patches/roles_controller_patch.rb
@@ -56,7 +56,7 @@ module OpenProject::GlobalRoles::Patches
           flash[:notice] = l(:notice_successful_create)
           redirect_to action: 'index'
         else
-          @roles = Role.order('builtin, position').all
+          @roles = Role.order('builtin, position').to_a
           @member_permissions = Role.new.setable_permissions
           @global_permissions = GlobalRole.setable_permissions
           render template: 'roles/new'
