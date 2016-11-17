7dc45bcea82820edf6b09e7dc935a5ef342834cc
Oliver Günther <mail@oliverguenther.de>
2016-11-17 15:44:27 +0100

    [24311] Fix n+2 query in MemberRole

--- app/models/member_role.rb
+++ app/models/member_role.rb
@@ -87,7 +87,10 @@ class MemberRole < ActiveRecord::Base
   end
 
   def remove_role_from_group_users
-    MemberRole.where(inherited_from: id).group_by(&:member).each do |member, member_roles|
+    MemberRole.where(inherited_from: id)
+              .includes(member: [:project, :user])
+              .group_by(&:member)
+              .each do |member, member_roles|
       member_roles.each do |mr| member.remove_member_role_and_destroy_member_if_last(mr) end
       if member && member.user
         Watcher.prune(user: member.user, project: member.project)
