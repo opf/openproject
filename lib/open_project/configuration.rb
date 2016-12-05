c5cd1493777f7f38b7673f960f1f24c47be81025
Oliver Günther <mail@oliverguenther.de>
2016-12-05 08:53:02 +0100

    Insert iframe into modal body instead of loading it on page load
    Fixes https://community.openproject.com/work_packages/24398/

--- lib/open_project/configuration.rb
+++ lib/open_project/configuration.rb
@@ -92,7 +92,7 @@ module OpenProject
 
       'apiv2_enable_basic_auth' => true,
 
-      'onboarding_video_url' => 'https://player.vimeo.com/video/163426858',
+      'onboarding_video_url' => 'https://player.vimeo.com/video/163426858?autoplay=1',
       'onboarding_enabled' => true
     }
 
