71b55d834b2b435e53f8a1edb62db3061f7852a2
Oliver Günther <mail@oliverguenther.de>
2016-12-19 09:19:34 +0100

    Add mousetrap typings

--- frontend/typings/index.d.ts
+++ frontend/typings/index.d.ts
@@ -9,6 +9,7 @@
 /// <reference path="globals/mocha/index.d.ts" />
 /// <reference path="globals/moment-node/index.d.ts" />
 /// <reference path="globals/moment/index.d.ts" />
+/// <reference path="globals/mousetrap/index.d.ts" />
 /// <reference path="globals/ng-dialog/index.d.ts" />
 /// <reference path="globals/promises-a-plus/index.d.ts" />
 /// <reference path="globals/rosie/index.d.ts" />
