6aa7ebfddcadd4c5de02eee66c50d2c45462d3ec
Wieland Lindenthal <w.lindenthal@forkmerge.com>
2017-02-06 19:37:55 +0100

    Apply custom CSS variables

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -695,6 +695,15 @@ module ApplicationHelper
     elements.join(', ').html_safe
   end
 
+  def darken_color(hex_color, amount=0.4)
+    hex_color = hex_color.gsub('#','')
+    rgb = hex_color.scan(/../).map {|color| color.hex}
+    rgb[0] = (rgb[0].to_i * amount).round
+    rgb[1] = (rgb[1].to_i * amount).round
+    rgb[2] = (rgb[2].to_i * amount).round
+    "#%02x%02x%02x" % rgb
+  end
+
   private
 
   def wiki_helper
