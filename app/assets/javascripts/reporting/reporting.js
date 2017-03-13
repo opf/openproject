7ad9870ce284436707f028d32775c2ea1746a857
Henriette Dinger <HDinger@users.noreply.github.com>
2017-03-13 07:52:20 +0100

    Override default texts to enable translations (#112)

--- app/assets/javascripts/reporting/reporting.js
+++ app/assets/javascripts/reporting/reporting.js
@@ -1,8 +1,17 @@
 //= require jquery-tablesorter
 
 (function($){
-
   $(document).ajaxComplete(function() {
+    // Override the default texts to enable translations
+    $.tablesorter.language = {
+          sortAsc      : I18n.t('js.sort.sorted_asc'),
+          sortDesc     : I18n.t('js.sort.sorted_dsc'),
+          sortNone     : I18n.t('js.sort.sorted_no'),
+          sortDisabled : I18n.t('js.sort.sorting_disabled'),
+          nextAsc      : I18n.t('js.sort.activate_asc'),
+          nextDesc     : I18n.t('js.sort.activate_dsc'),
+          nextNone     : I18n.t('js.sort.activate_no')
+    };
     $('#sortable-table').not('.tablesorter').tablesorter();
   });
 })(jQuery);
