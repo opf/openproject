0ce062ef25921c644ebbecd1ab656b0111ca322d
Oliver Günther <mail@oliverguenther.de>
2017-03-21 10:37:12 +0100

    Fix more tests

--- spec/features/work_package_show_spec.rb
+++ spec/features/work_package_show_spec.rb
@@ -48,8 +48,7 @@ RSpec.feature 'Work package show page', selenium: true do
 
     wp_page.visit!
 
-    wp_page.expect_attributes Author: work_package.author.name,
-                              Type: work_package.type.name,
+    wp_page.expect_attributes Type: work_package.type.name,
                               Status: work_package.status.name,
                               Priority: work_package.priority.name,
                               Assignee: work_package.assigned_to.name,
