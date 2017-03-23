4c644685126864f2af8680b23c4a8d836f4b44f5
Henriette Dinger <h.dinger@finn.de>
2017-03-23 10:52:25 +0100

    Style footer

--- lib/widget/entry_table.rb
+++ lib/widget/entry_table.rb
@@ -94,20 +94,20 @@ class Widget::Table::EntryTable < Widget::Table
     content_tag :tfoot do
       content_tag :tr do
         if show_result(@subject, 0) != show_result(@subject)
-          concat content_tag(:th, '', colspan: FIELDS.size)
-          concat content_tag(:th) {
+          concat content_tag(:td, '', colspan: FIELDS.size)
+          concat content_tag(:td) {
             concat content_tag(:div,
                                show_result(@subject),
                                class: 'inner generic-table--footer-outer')
           }
-          concat content_tag(:th) {
+          concat content_tag(:td) {
             concat content_tag(:div,
                                show_result(@subject, 0),
                                class: 'result generic-table--footer-outer')
           }
         else
-          concat content_tag(:th, '', colspan: FIELDS.size + 1)
-          concat content_tag(:th) {
+          concat content_tag(:td, '', colspan: FIELDS.size + 1)
+          concat content_tag(:td) {
             concat content_tag(:div,
                                show_result(@subject),
                                class: 'result generic-table--footer-outer')
