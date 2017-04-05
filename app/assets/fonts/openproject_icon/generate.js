1e7e97a421a269d584b550da0bc14258852160d2
Oliver Günther <mail@oliverguenther.de>
2017-04-05 09:25:39 +0200

    Update icon-font generation with font-path

--- app/assets/fonts/openproject_icon/generate.js
+++ app/assets/fonts/openproject_icon/generate.js
@@ -1,6 +1,7 @@
 #!/usr/bin/env node
 
 const webfontsGenerator = require('webfonts-generator');
+const path = require('path');
 const fs = require('fs');
 const glob = require("glob")
 
@@ -8,6 +9,8 @@ webfontsGenerator({
   files: glob.sync("src/*.svg"),
   "fontName": "openproject-icon-font",
   "cssFontsUrl": "../assets/openproject_icon/",
+  "cssDest": path.join(path.resolve(__dirname, '..', '..', 'stylesheets', 'fonts'), '_openproject_icon_definitions.scss'),
+  "cssTemplate": "openproject-icon-font.template.scss",
   "classPrefix": "icon-",
   "baseSelector": ".icon",
   "types": ['woff2', 'woff'],
@@ -16,8 +19,5 @@ webfontsGenerator({
 }, function(error) {
   if (error) {
     console.log('Failed to build icon font. ', error);
-  } else {
-    fs.rename('openproject-icon-font.css', '../../stylesheets/fonts/_openproject_icon_definitions.scss');
-    console.log('Done!');
   }
 });
