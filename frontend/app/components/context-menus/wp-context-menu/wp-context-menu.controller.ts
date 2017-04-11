a0a1076272401e5b06f71a89579658d609d70336
Oliver Günther <mail@oliverguenther.de>
2017-04-11 15:56:34 +0200

    [25008] Fix relation context menu registry
    [ci skip]

--- frontend/app/components/context-menus/wp-context-menu/wp-context-menu.controller.ts
+++ frontend/app/components/context-menus/wp-context-menu/wp-context-menu.controller.ts
@@ -67,13 +67,13 @@ function wpContextMenuController($scope:any,
         editSelectedWorkPackages(link);
         break;
 
-      case "timeline-relation-add-predecessor":
+      case 'relation-precedes':
         table = $scope.table;
         wp = $scope.row.object;
         table.timelineController.startAddRelationPredecessor(wp);
         break;
 
-      case "timeline-relation-add-follower":
+      case 'relation-follows':
         table = $scope.table;
         wp = $scope.row.object;
         table.timelineController.startAddRelationFollower(wp);
