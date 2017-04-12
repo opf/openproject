c29c3ddb51a457610967b33df690e33fddf0885c
Henriette Dinger <HDinger@users.noreply.github.com>
2017-04-12 15:49:20 +0200

    [25030] Rollback sidebar and hamburger icon (#5355)
    * Revert "Apply new (darker) styling"
    
    This reverts commits 91877f5, 8ed5f9a, 4d430c0d1cd66086f794d36fa871bcea8071a601.
    
    * Revert "Change sidebar behavior and position"
    
    This reverts commit e9178ceddc091821c2a144add7283207075a1216.
    
    * Add new styling
    
    [ci skip]

--- frontend/app/layout/controllers/main-menu-controller.js
+++ frontend/app/layout/controllers/main-menu-controller.js
@@ -34,7 +34,6 @@ module.exports = function($rootScope, $window) {
       menuList = hideSubMenuEntries();
     } else {
       showSubMenuEntries(menuList);
-      focusFirstEntry();
     }
     $rootScope.showNavigation = !$rootScope.showNavigation;
     $rootScope.$broadcast('openproject.layout.navigationToggled', $rootScope.showNavigation);
@@ -60,13 +59,4 @@ function toggleMenus(togglers) {
   togglers.each(function(index) {
     jQuery(this).trigger('click');
   });
-}
-
-function focusFirstEntry() {
-  var element = jQuery('#main-menu .main-item-wrapper > a')[0];
-  if (element)  {
-    window.setTimeout(function(){
-      element.focus();
-    }, 100);
-  }
 }
