2a25a13d593843b8b4703d2e6ba929f925f2062a
Oliver Günther <mail@oliverguenther.de>
2017-04-28 15:34:31 +0200

    Remove outdated alias removed from core

--- lib/open_project/backlogs/patches/permitted_params_patch.rb
+++ lib/open_project/backlogs/patches/permitted_params_patch.rb
@@ -40,21 +40,11 @@ module OpenProject::Backlogs::Patches::PermittedParamsPatch
     base.send(:include, InstanceMethods)
 
     base.class_eval do
-      alias_method_chain :new_work_package, :backlogs
       alias_method_chain :update_work_package, :backlogs
     end
   end
 
   module InstanceMethods
-    def new_work_package_with_backlogs(args = {})
-      permitted_params = new_work_package_without_backlogs(args)
-
-      backlogs_params = params.require(:work_package).permit(:story_points, :remaining_hours)
-      permitted_params.merge!(backlogs_params)
-
-      permitted_params
-    end
-
     def update_work_package_with_backlogs(args = {})
       permitted_params = update_work_package_without_backlogs(args)
 
