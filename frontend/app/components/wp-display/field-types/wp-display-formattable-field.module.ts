b9e4a41d392d445362b391dbe1d7631dcd4240ac
Jens Ulferts <j.ulferts@finn.de>
2017-05-09 09:02:24 +0200

    escape html tags from wp display attr value and set as title for formattable

--- frontend/app/components/wp-display/field-types/wp-display-formattable-field.module.ts
+++ frontend/app/components/wp-display/field-types/wp-display-formattable-field.module.ts
@@ -42,6 +42,12 @@ export class FormattableDisplayField extends DisplayField {
   }
 
   public render(element:HTMLElement, displayText:string):void {
+    // title stripped of html tags
+    if (this.value) {
+      let title = angular.element(this.value).text();
+      element.setAttribute('title', title);
+    }
+
     angular.element(element).addClass('-multiline');
 
     let span = document.createElement('span');
