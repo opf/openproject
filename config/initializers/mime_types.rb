0393d0fe9e647f6a18293589de31aef96394f334
Jens Ulferts <j.ulferts@finn.de>
2017-05-11 09:45:37 +0200

    replace deprecated constant

--- config/initializers/mime_types.rb
+++ config/initializers/mime_types.rb
@@ -33,7 +33,7 @@
 # Mime::Type.register "text/richtext", :rtf
 # Mime::Type.register_alias "text/html", :iphone
 
-Mime::SET << Mime::CSV unless Mime::SET.include?(Mime::CSV)
+Mime::SET << Mime[:csv] unless Mime::SET.include?(Mime[:csv])
 
 Mime::Type.register 'application/pdf', :pdf unless Mime::Type.lookup_by_extension(:pdf)
 Mime::Type.register 'image/png', :png unless Mime::Type.lookup_by_extension(:png)
