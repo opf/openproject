10b01d994ee0a1a1a95eef15a9dcf8849d1cf1f4
Jens Ulferts <j.ulferts@finn.de>
2017-05-29 17:01:09 +0200

    apply `ellipsis class` to query menu items

--- lib/redmine/menu_manager/menu_helper.rb
+++ lib/redmine/menu_manager/menu_helper.rb
@@ -192,7 +192,7 @@ module Redmine::MenuManager::MenuHelper
     link_text = ''.html_safe
     link_text << op_icon(item.icon) if item.icon.present?
     link_text << you_are_here_info(selected)
-    link_text << content_tag(:span, caption, class: 'menu-item--title', lang: menu_item_locale(item))
+    link_text << content_tag(:span, caption, class: 'menu-item--title ellipsis', lang: menu_item_locale(item))
     html_options = item.html_options(selected: selected)
     html_options[:title] ||= selected ? t(:description_current_position) + caption : caption
 
