32381134f414118b36231337cb3160d314517824
Oliver Günther <mail@oliverguenther.de>
2017-05-30 12:51:35 +0200

    Fix relations removal in split screen and add test (#5578)
    [ci skip]

--- spec/features/support/components/ui_autocomplete.rb
+++ spec/features/support/components/ui_autocomplete.rb
@@ -26,28 +26,34 @@
 # See doc/COPYRIGHT.rdoc for more details.
 #++
 
+module Components
+  module UIAutocompleteHelpers
+    def search_autocomplete(element, query:)
+      # Open the element
+      element.click
+      # Insert the text to find
+      element.set(query)
+      sleep(0.5)
+
+      ##
+      # Find the open dropdown
+      page.find('.ui-autocomplete', visible: true)
+    end
+
+    def select_autocomplete(element, query:, select_text: nil)
+      target_dropdown = search_autocomplete(element, query: query)
+
+      ##
+      # If a specific select_text is given, use that to locate the match,
+      # otherwise use the query
+      text = select_text.presence || query
+
+      # click the element to select it
+      target_dropdown.find('.ui-menu-item', text: text).click
+    end
+  end
+end
+
 shared_context 'ui-autocomplete helpers' do
-  def search_autocomplete(element, query:)
-    # Open the element
-    element.click
-    # Insert the text to find
-    element.set(query)
-    sleep(0.5)
-
-    ##
-    # Find the open dropdown
-    page.find('.ui-autocomplete', visible: true)
-  end
-
-  def select_autocomplete(element, query:, select_text: nil)
-    target_dropdown = search_autocomplete(element, query: query)
-
-    ##
-    # If a specific select_text is given, use that to locate the match,
-    # otherwise use the query
-    text = select_text.presence || query
-
-    # click the element to select it
-    target_dropdown.find('.ui-menu-item', text: text).click
-  end
+  include ::Components::UIAutocompleteHelpers
 end
---
+++ spec/support/components/work_packages/relations.rb
@@ -0,0 +1,94 @@
+#-- copyright
+# OpenProject is a project management system.
+# Copyright (C) 2012-2017 the OpenProject Foundation (OPF)
+#
+# This program is free software; you can redistribute it and/or
+# modify it under the terms of the GNU General Public License version 3.
+#
+# OpenProject is a fork of ChiliProject, which is a fork of Redmine. The copyright follows:
+# Copyright (C) 2006-2017 Jean-Philippe Lang
+# Copyright (C) 2010-2013 the ChiliProject Team
+#
+# This program is free software; you can redistribute it and/or
+# modify it under the terms of the GNU General Public License
+# as published by the Free Software Foundation; either version 2
+# of the License, or (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+#
+# See doc/COPYRIGHT.rdoc for more details.
+#++
+
+require 'features/support/components/ui_autocomplete'
+
+module Components
+  module WorkPackages
+    class Relations
+      include Capybara::DSL
+      include RSpec::Matchers
+      include ::Components::UIAutocompleteHelpers
+
+      attr_reader :work_package
+
+      def initialize(work_package)
+        @work_package = work_package
+      end
+
+      def remove_relation(relatable)
+        ## Delete relation
+        created_row = find(".relation-row-#{relatable.id}")
+
+        # Hover row to expose button
+        created_row.hover
+        created_row.find('.relation-row--remove-btn').click
+
+        # Expect relation to be gone
+        expect_no_relation(relatable)
+      end
+
+      def add_relation(type:, to:)
+        # Open create form
+        find('#relation--add-relation').click
+
+        # Select relation type
+        container = find('.wp-relations-create--form', wait: 10)
+
+        # Labels to expect
+        relation_label = I18n.t('js.relation_labels.' + type)
+
+        select relation_label, from: 'relation-type--select'
+
+        # Enter the query and select the child
+        autocomplete = container.find(".wp-relations--autocomplete")
+        select_autocomplete(autocomplete, query: to.subject, select_text: to.subject)
+
+        container.find('.wp-create-relation--save').click
+
+        expect(page).to have_selector('.relation-group--header',
+                                      text: relation_label.upcase,
+                                      wait: 10)
+
+        expect(page).to have_selector('.relation-row--type', text: to.type.name)
+
+        expect(page).to have_selector('.wp-relations--subject-field', text: to.subject)
+
+        ## Test if relation exist
+        work_package.reload
+        relation = work_package.relations.last
+        expect(relation.label_for(work_package).to_s).to eq("label_#{type}")
+        expect(relation.other_work_package(work_package).id).to eq(to.id)
+      end
+
+      def expect_no_relation(relatable)
+        expect(page).to have_no_selector('.wp-relations--subject-field', text: relatable.subject)
+      end
+    end
+  end
+end
