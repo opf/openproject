7d42817f49d0a091cfd697b438253d7e91b6257a
Wieland Lindenthal <w.lindenthal@forkmerge.com>
2017-06-16 09:15:11 +0200

    feature #25517 Allow custom favicon and touch icon upload (#5653)
    * Allow custom favicon and touch icon upload
    
    * fix tiny copy and paste error that lead to error in pull preview
    
    [ci skip]

--- spec/controllers/custom_styles_controller_spec.rb
+++ spec/controllers/custom_styles_controller_spec.rb
@@ -76,7 +76,7 @@ describe CustomStylesController, type: :controller do
       let(:custom_style) { CustomStyle.new }
       let(:params) do
         {
-          custom_style: { logo: 'foo' }
+          custom_style: { logo: 'foo', favicon: 'bar', icon_touch: 'yay' }
         }
       end
 
@@ -90,11 +90,9 @@ describe CustomStylesController, type: :controller do
       end
 
       context 'valid custom_style input' do
-        # let(:valid) { true }
         let(:valid) { true }
 
         it 'redirects to show' do
-          # expect(custom_style).to receive(:logo=)
           expect(response).to redirect_to action: :show
         end
       end
@@ -113,7 +111,7 @@ describe CustomStylesController, type: :controller do
       let(:custom_style) { FactoryGirl.build(:custom_style_with_logo) }
       let(:params) do
         {
-          custom_style: { logo: 'foo' }
+          custom_style: { logo: 'foo', favicon: 'bar', icon_touch: 'yay' }
         }
       end
 
@@ -127,11 +125,9 @@ describe CustomStylesController, type: :controller do
       end
 
       context 'valid custom_style input' do
-        # let(:valid) { true }
         let(:valid) { true }
 
         it 'redirects to show' do
-          # expect(custom_style).to receive(:logo=)
           expect(response).to redirect_to action: :show
         end
       end
@@ -196,6 +192,122 @@ describe CustomStylesController, type: :controller do
         before do
           expect(CustomStyle).to receive(:current).and_return(nil)
           delete :logo_delete
+        end
+
+        it 'renders 404' do
+          expect(response.status).to eq 404
+        end
+      end
+    end
+
+    describe "#favicon_download" do
+      render_views
+
+      before do
+        expect(CustomStyle).to receive(:current).and_return(custom_style)
+        allow(controller).to receive(:send_file) { controller.head 200 }
+        get :favicon_download, params: { digest: "1234", filename: "favicon_image.png" }
+      end
+
+      context "when favicon is present" do
+        let(:custom_style) { FactoryGirl.build(:custom_style_with_favicon) }
+
+        it 'will send a file' do
+          expect(response.status).to eq(200)
+        end
+      end
+
+      context "when no logo is present" do
+        let(:custom_style) { nil }
+
+        it 'renders with error' do
+          expect(controller).to_not receive(:send_file)
+          expect(response.status).to eq(404)
+        end
+      end
+    end
+
+    describe "#favicon_delete" do
+      let(:custom_style) { FactoryGirl.build(:custom_style_with_favicon) }
+
+      before do
+        allow(EnterpriseToken).to receive(:allows_to?).with(:define_custom_style).and_return(true)
+      end
+
+      context 'if it exists' do
+        before do
+          expect(CustomStyle).to receive(:current).and_return(custom_style)
+          expect(custom_style).to receive(:remove_favicon!).and_return(custom_style)
+          delete :favicon_delete
+        end
+
+        it 'removes the favicon from custom_style' do
+          expect(response).to redirect_to action: :show
+        end
+      end
+
+      context 'if it does not exist' do
+        before do
+          expect(CustomStyle).to receive(:current).and_return(nil)
+          delete :favicon_delete
+        end
+
+        it 'renders 404' do
+          expect(response.status).to eq 404
+        end
+      end
+    end
+
+    describe "#touch_icon_download" do
+      render_views
+
+      before do
+        expect(CustomStyle).to receive(:current).and_return(custom_style)
+        allow(controller).to receive(:send_file) { controller.head 200 }
+        get :touch_icon_download, params: { digest: "1234", filename: "touch_icon_image.png" }
+      end
+
+      context "when touch icon is present" do
+        let(:custom_style) { FactoryGirl.build(:custom_style_with_touch_icon) }
+
+        it 'will send a file' do
+          expect(response.status).to eq(200)
+        end
+      end
+
+      context "when no touch icon is present" do
+        let(:custom_style) { nil }
+
+        it 'renders with error' do
+          expect(controller).to_not receive(:send_file)
+          expect(response.status).to eq(404)
+        end
+      end
+    end
+
+    describe "#touch_icon_delete" do
+      let(:custom_style) { FactoryGirl.build(:custom_style_with_touch_icon) }
+
+      before do
+        allow(EnterpriseToken).to receive(:allows_to?).with(:define_custom_style).and_return(true)
+      end
+
+      context 'if it exists' do
+        before do
+          expect(CustomStyle).to receive(:current).and_return(custom_style)
+          expect(custom_style).to receive(:remove_touch_icon!).and_return(custom_style)
+          delete :touch_icon_delete
+        end
+
+        it 'removes the touch icon from custom_style' do
+          expect(response).to redirect_to action: :show
+        end
+      end
+
+      context 'if it does not exist' do
+        before do
+          expect(CustomStyle).to receive(:current).and_return(nil)
+          delete :touch_icon_delete
         end
 
         it 'renders 404' do
