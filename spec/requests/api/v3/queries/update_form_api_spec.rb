5165906e11ba9ca53c0e9e183a9297f9bd95a7de
ulferts <j.ulferts@finn.de>
2017-06-19 11:28:37 +0200

    fix saving boolean cf filters, use representer for filter parsing (#5660)
    * fix saving boolean cf filters, use representer for filter parsing
    
    * patch roar to allow linked_reseource definitions
    
    This enables us to get rid of dedicated representers for links sections when the representer is used for parsing
    
    [ci skip]

--- spec/requests/api/v3/queries/update_form_api_spec.rb
+++ spec/requests/api/v3/queries/update_form_api_spec.rb
@@ -1,4 +1,5 @@
 #-- encoding: UTF-8
+
 #-- copyright
 # OpenProject is a project management system.
 # Copyright (C) 2012-2015 the OpenProject Foundation (OPF)
@@ -35,7 +36,7 @@ describe "POST /api/v3/queries/form", type: :request do
   let(:path) { api_v3_paths.query_form(query.id) }
   let(:user) { FactoryGirl.create(:admin) }
   let(:role) { FactoryGirl.create :existing_role, permissions: permissions }
-  let(:permissions) { [:view_work_packages, :manage_public_queries] }
+  let(:permissions) { %i(view_work_packages manage_public_queries) }
 
   let!(:project) { FactoryGirl.create(:project_with_types) }
 
@@ -188,10 +189,18 @@ describe "POST /api/v3/queries/form", type: :request do
       filters = [
         {
           "_links" => {
-            "filter" => { "href" => "/api/v3/queries/filters/status" },
-            "operator" => { "href" => "/api/v3/queries/operators/%3D" },
+            "filter" => {
+              "href" => "/api/v3/queries/filters/status"
+            },
+            "operator" => {
+              "href" => "/api/v3/queries/operators/%3D",
+              "title" => 'is'
+            },
             "values" => [
-              { "href" => "/api/v3/statuses/#{status.id}" }
+              {
+                "href" => "/api/v3/statuses/#{status.id}",
+                "title" => status.name
+              }
             ]
           }
         }
