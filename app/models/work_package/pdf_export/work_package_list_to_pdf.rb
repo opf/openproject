opf/openproject

commit 267f9b0ea24260b69fba958f330a301b0a6b7f7c

provide common interface to exporters - exclude relation columns

app/models/work_package/pdf_export/work_package_list_to_pdf.rb
@@ -1,4 +1,5 @@
 #-- encoding: UTF-8
+
 #-- copyright
 # OpenProject is a project management system.
 # Copyright (C) 2012-2017 the OpenProject Foundation (OPF)
@@ -27,24 +28,16 @@
 # See doc/COPYRIGHT.rdoc for more details.
 #++
 
-class WorkPackage::PdfExport::WorkPackageListToPdf
+class WorkPackage::PdfExport::WorkPackageListToPdf < WorkPackage::Exporter::Base
   include WorkPackage::PdfExport::Common
 
-  attr_accessor :work_packages,
-                :pdf,
-                :project,
-                :query,
-                :results,
+  attr_accessor :pdf,
                 :options
 
-  def initialize(work_packages, project, query, results, options = {})
-    @cell_padding = options.delete(:cell_padding)
+  def initialize(object, options = {})
+    super
 
-    self.work_packages = work_packages
-    self.project = project
-    self.query = query
-    self.results = results
-    self.options = options
+    @cell_padding = options.delete(:cell_padding)
 
     self.pdf = get_pdf(current_language)
 
@@ -58,7 +51,13 @@ class WorkPackage::PdfExport::WorkPackageListToPdf
 
     write_footers!
 
-    pdf
+    success(pdf.render)
+  rescue Prawn::Errors::CannotFit
+    error(I18n.t(:error_pdf_export_too_many_columns))
+  end
+
+  def project
+    query.project
   end
 
   def write_title!
@@ -94,7 +93,7 @@ class WorkPackage::PdfExport::WorkPackageListToPdf
   end
 
   def column_widths
-    widths = query.columns.map do |col|
+    widths = valid_export_columns.map do |col|
       if col.name == :subject || text_column?(col)
         4.0
       else
@@ -107,7 +106,7 @@ class WorkPackage::PdfExport::WorkPackageListToPdf
   end
 
   def description_colspan
-    query.columns.size
+    valid_export_columns.size
   end
 
   def text_column?(column)
@@ -120,7 +119,7 @@ class WorkPackage::PdfExport::WorkPackageListToPdf
   end
 
   def data_headers
-    query.columns.map(&:caption).map do |caption|
+    valid_export_columns.map(&:caption).map do |caption|
       pdf.make_cell caption, font_style: :bold, background_color: 'CCCCCC'
     end
   end
@@ -129,7 +128,7 @@ class WorkPackage::PdfExport::WorkPackageListToPdf
     previous_group = nil
 
     work_packages.flat_map do |work_package|
-      values = query.columns.map do |column|
+      values = valid_export_columns.map do |column|
         make_column_value work_package, column
       end
 
@@ -147,7 +146,7 @@ class WorkPackage::PdfExport::WorkPackageListToPdf
 
         result.insert 0, [
           pdf.make_cell(label, font_style: :bold,
-                               colspan: query.columns.size,
+                               colspan: valid_export_columns.size,
                                background_color: 'DDDDDD')
         ]
       end

app/models/work_package/pdf_export/work_package_to_pdf.rb
@@ -1,4 +1,5 @@
 #-- encoding: UTF-8
+
 #-- copyright
 # OpenProject is a project management system.
 # Copyright (C) 2012-2017 the OpenProject Foundation (OPF)
@@ -27,14 +28,14 @@
 # See doc/COPYRIGHT.rdoc for more details.
 #++
 
-class WorkPackage::PdfExport::WorkPackageToPdf
+class WorkPackage::PdfExport::WorkPackageToPdf < WorkPackage::Exporter::Base
   include WorkPackage::PdfExport::Common
 
-  attr_accessor :work_package,
-                :pdf
+  attr_accessor :pdf
 
   def initialize(work_package)
-    self.work_package = work_package
+    super
+
     self.pdf = get_pdf(current_language)
   end
 
@@ -45,7 +46,7 @@ class WorkPackage::PdfExport::WorkPackageToPdf
     write_attachments!
     write_footers!
 
-    pdf
+    success(pdf.render)
   end
 
   def make_attribute_row(first_attribute, second_attribute)
@@ -75,7 +76,8 @@ class WorkPackage::PdfExport::WorkPackageToPdf
   def make_attribute_cells(attribute, label_options: {}, value_options: {})
     label = pdf.make_cell(
       WorkPackage.human_attribute_name(attribute) + ':',
-      label_options)
+      label_options
+    )
 
     value_content = field_value work_package, attribute
     value = pdf.make_cell(value_content.to_s, value_options)
@@ -146,10 +148,14 @@ class WorkPackage::PdfExport::WorkPackageToPdf
   end
 
   def write_title!
-    pdf.title = "#{work_package.project} - ##{work_package.type} #{work_package.id}"
+    pdf.title = title
     pdf.font style: :bold, size: 11
     pdf.text "#{work_package.project} - #{work_package.type} # #{work_package.id}: #{work_package.subject}"
     pdf.move_down 20
+  end
+
+  def title
+    "#{work_package.project} - ##{work_package.type} #{work_package.id}"
   end
 
   def write_attributes!

app/controllers/work_packages_controller.rb
@@ -1,4 +1,5 @@
 #-- encoding: UTF-8
+
 #-- copyright
 # OpenProject is a project management system.
 # Copyright (C) 2012-2017 the OpenProject Foundation (OPF)
@@ -28,8 +29,6 @@
 #++
 
 class WorkPackagesController < ApplicationController
-  EXPORT_FORMATS = %w[atom rss xls csv pdf]
-
   include QueriesHelper
   include PaginationHelper
   include OpenProject::ClientPreferenceExtractor
@@ -41,8 +40,8 @@ class WorkPackagesController < ApplicationController
   before_action :find_optional_project,
                 :protect_from_unauthorized_export, only: :index
 
-  before_action :load_query,
-                :load_work_packages, only: :index, unless: ->() { request.format.html? }
+  before_action :load_query, only: :index, unless: ->() { request.format.html? }
+  before_action :load_work_packages, only: :index, if: ->() { request.format.atom? }
 
   def show
     respond_to do |format|
@@ -53,12 +52,8 @@ class WorkPackagesController < ApplicationController
         render :show, locals: { work_package: work_package }, layout: 'angular'
       end
 
-      format.pdf do
-        export = WorkPackage::Exporter.work_package_to_pdf(work_package)
-
-        send_data(export.render,
-                  type: 'application/pdf',
-                  filename: "#{project.identifier}-#{work_package.id}.pdf")
+      format.any(*WorkPackage::Exporter.single_formats) do
+        export_single(request.format.symbol)
       end
 
       format.atom do
@@ -81,22 +76,8 @@ class WorkPackagesController < ApplicationController
                        layout: 'angular'
       end
 
-      format.csv do
-        serialized_work_packages = WorkPackage::Exporter.csv(@work_packages, @query)
-        title = @query.new_record? ? l(:label_work_package_plural) : @query.name
-
-        render csv: serialized_work_packages, filename: "#{title}.csv"
-      end
-
-      format.pdf do
-        export = WorkPackage::Exporter.pdf(
-          @work_packages, @project, @query, @results,
-          show_descriptions: params[:show_descriptions]
-        )
-
-        send_data(export.render,
-                  type: 'application/pdf',
-                  filename: 'export.pdf')
+      format.any(*WorkPackage::Exporter.list_formats) do
+        export_list(request.format.symbol)
       end
 
       format.atom do
@@ -104,11 +85,6 @@ class WorkPackagesController < ApplicationController
                     title: "#{@project || Setting.app_title}: #{l(:label_work_package_plural)}")
       end
     end
-  rescue ActiveRecord::RecordNotFound
-    render_404
-  rescue Prawn::Errors::CannotFit
-    flash[:error] = I18n.t :error_pdf_export_too_many_columns
-    redirect_back(fallback_location: index_work_packages_path)
   end
 
   current_menu_item :index do
@@ -117,17 +93,49 @@ class WorkPackagesController < ApplicationController
 
   protected
 
+  def export_list(mime_type)
+    exporter = WorkPackage::Exporter.for_list(mime_type)
+    export = exporter.list(@query, params)
+
+    if export.error?
+      flash[:error] = export.message
+      redirect_back(fallback_location: index_redirect_path)
+    else
+      send_data(export.content,
+                type: export.mime_type,
+                filename: export.title)
+    end
+  end
+
+  def export_single(mime_type)
+    exporter = WorkPackage::Exporter.for_single(mime_type)
+    export = exporter.single(work_package, params)
+
+    if export.error?
+      flash[:error] = export.message
+      redirect_back(fallback_location: work_package_path(work_packages))
+    else
+      send_data(export.content,
+                type: export.mime_type,
+                filename: export.title)
+    end
+  end
+
   def authorize_on_work_package
     deny_access unless work_package
   end
 
   def protect_from_unauthorized_export
-    if EXPORT_FORMATS.include?(params[:format]) &&
+    if supported_export_formats.include?(params[:format]) &&
        !User.current.allowed_to?(:export_work_packages, @project, global: @project.nil?)
 
       deny_access
       false
     end
+  end
+
+  def supported_export_formats
+    %w[atom rss] + WorkPackage::Exporter.list_formats.map(&:to_s)
   end
 
   def load_query
@@ -138,8 +146,6 @@ class WorkPackagesController < ApplicationController
 
   def per_page_param
     case params[:format]
-    when 'csv', 'pdf'
-      Setting.work_packages_export_limit.to_i
     when 'atom'
       Setting.feeds_limit.to_i
     else
@@ -163,6 +169,14 @@ class WorkPackagesController < ApplicationController
                   .order("#{Journal.table_name}.created_at ASC").to_a
     @journals.reverse_order if current_user.wants_comments_in_reverse_order?
     @journals
+  end
+
+  def index_redirect_path
+    if @project
+      project_work_packages_path(@project)
+    else
+      work_packages_path
+    end
   end
 
   private

app/helpers/timelog_helper.rb
@@ -29,7 +29,6 @@
 
 module TimelogHelper
   include ApplicationHelper
-  include WorkPackage::CsvExporter
 
   def render_timelog_breadcrumb
     links = []
@@ -114,7 +113,7 @@ module TimelogHelper
       # Export custom fields
       headers += custom_fields.map(&:name)
 
-      csv << encode_csv_columns(headers)
+      csv << WorkPackage::Exporter::CSV.encode_csv_columns(headers)
       # csv lines
       entries.each do |entry|
         fields = [format_date(entry.spent_on),
@@ -129,7 +128,7 @@ module TimelogHelper
                  ]
         fields += custom_fields.map { |f| show_value(entry.custom_value_for(f)) }
 
-        csv << encode_csv_columns(fields)
+        csv << WorkPackage::Exporter::CSV.encode_csv_columns(fields)
       end
     }
     export

app/models/work_package/exporter/base.rb
@@ -28,37 +28,42 @@
 # See doc/COPYRIGHT.rdoc for more details.
 #++
 
-module WorkPackage::Exporter
-  def self.for_list(type)
-    @for_list[type]
+class WorkPackage::Exporter::Base
+  attr_accessor :object,
+                :options
+
+  def initialize(object, options = {})
+    self.object = object
+    self.options = options
   end
 
-  def self.register_for_list(type, exporter)
-    @for_list ||= {}
-
-    @for_list[type] = exporter
+  def self.list(query, options = {})
+    new(query, options).list
   end
 
-  def self.list_formats
-    @for_list.keys
+  def self.single(work_package, options = {})
+    new(work_package, options).single
   end
 
-  def self.for_single(type)
-    @for_single[type]
+  def page
+    options[:page] || 1
   end
 
-  def self.register_for_single(type, exporter)
-    @for_single ||= {}
-
-    @for_single[type] = exporter
+  def valid_export_columns
+    query.columns.select do |c|
+      c.is_a?(Queries::WorkPackages::Columns::PropertyColumn) ||
+        c.is_a?(Queries::WorkPackages::Columns::CustomFieldColumn)
+    end
   end
 
-  def self.single_formats
-    @for_single.keys
+  alias :query :object
+  alias :work_package :object
+
+  def work_packages
+    @work_packages ||= query
+                       .results
+                       .sorted_work_packages
+                       .page(page)
+                       .per_page(Setting.work_packages_export_limit.to_i)
   end
-
-  register_for_list(:csv, WorkPackage::Exporter::CSV)
-  register_for_list(:pdf, WorkPackage::Exporter::PDF)
-
-  register_for_single(:pdf, WorkPackage::Exporter::PDF)
 end

app/models/work_package/exporter/csv.rb
@@ -28,27 +28,27 @@
 # See doc/COPYRIGHT.rdoc for more details.
 #++
 
-module WorkPackage::CsvExporter
+class WorkPackage::Exporter::CSV < WorkPackage::Exporter::Base
   include Redmine::I18n
   include CustomFieldsHelper
   include ActionView::Helpers::TextHelper
   include ActionView::Helpers::NumberHelper
 
-  def csv(work_packages, query)
-    export = CSV.generate(col_sep: l(:general_csv_separator)) do |csv|
-      headers = csv_headers(query)
-      csv << encode_csv_columns(headers)
+  def list
+    serialized = CSV.generate(col_sep: l(:general_csv_separator)) do |csv|
+      headers = csv_headers
+      csv << self.class.encode_csv_columns(headers)
 
       work_packages.each do |work_package|
-        row = csv_row(work_package, query)
-        csv << encode_csv_columns(row)
+        row = csv_row(work_package)
+        csv << self.class.encode_csv_columns(row)
       end
     end
 
-    export
+    success(serialized)
   end
 
-  def encode_csv_columns(columns, encoding = l(:general_csv_encoding))
+  def self.encode_csv_columns(columns, encoding = l(:general_csv_encoding))
     columns.map do |cell|
       Redmine::CodesetUtil.from_utf8(cell.to_s, encoding)
     end
@@ -56,11 +56,25 @@ module WorkPackage::CsvExporter
 
   private
 
+  def success(serialized)
+    WorkPackage::Exporter::Success
+      .new format: :csv,
+           title: title,
+           content: serialized,
+           mime_type: 'text/csv'
+  end
+
+  def title
+    title = query.new_record? ? l(:label_work_package_plural) : query.name
+
+    "#{title}.csv"
+  end
+
   # fetch all headers
-  def csv_headers(query)
+  def csv_headers
     headers = []
 
-    query.columns.each_with_index do |column, _|
+    valid_export_columns.each_with_index do |column, _|
       headers << column.caption
     end
 
@@ -76,8 +90,8 @@ module WorkPackage::CsvExporter
   end
 
   # fetch all row values
-  def csv_row(work_package, query)
-    row = query.columns.collect do |column|
+  def csv_row(work_package)
+    row = valid_export_columns.collect do |column|
       csv_format_value(work_package, column)
     end
 

app/models/work_package/exporter/error.rb
@@ -28,37 +28,14 @@
 # See doc/COPYRIGHT.rdoc for more details.
 #++
 
-module WorkPackage::Exporter
-  def self.for_list(type)
-    @for_list[type]
+class WorkPackage::Exporter::Error < WorkPackage::Exporter::Result
+  attr_accessor :message
+
+  def initialize(message)
+    self.message = message
   end
 
-  def self.register_for_list(type, exporter)
-    @for_list ||= {}
-
-    @for_list[type] = exporter
+  def error?
+    true
   end
-
-  def self.list_formats
-    @for_list.keys
-  end
-
-  def self.for_single(type)
-    @for_single[type]
-  end
-
-  def self.register_for_single(type, exporter)
-    @for_single ||= {}
-
-    @for_single[type] = exporter
-  end
-
-  def self.single_formats
-    @for_single.keys
-  end
-
-  register_for_list(:csv, WorkPackage::Exporter::CSV)
-  register_for_list(:pdf, WorkPackage::Exporter::PDF)
-
-  register_for_single(:pdf, WorkPackage::Exporter::PDF)
 end

app/models/work_package/exporter/pdf.rb
@@ -1,4 +1,5 @@
 #-- encoding: UTF-8
+
 #-- copyright
 # OpenProject is a project management system.
 # Copyright (C) 2012-2017 the OpenProject Foundation (OPF)
@@ -27,26 +28,19 @@
 # See doc/COPYRIGHT.rdoc for more details.
 #++
 
-require 'spec_helper'
-
-describe WorkPackagesController, type: :controller do
-  let(:user) { FactoryGirl.create :admin }
-  let(:project) { FactoryGirl.create :project, identifier: "exp" }
-
-  before do
-    allow(User).to receive(:current).and_return user
+class WorkPackage::Exporter::PDF < WorkPackage::Exporter::Base
+  # Returns a PDF string of a list of work_packages
+  def list
+    ::WorkPackage::PdfExport::WorkPackageListToPdf
+      .new(query,
+           options)
+      .render!
   end
 
-  describe "#index .pdf" do
-    before do
-      expect(WorkPackage::Exporter)
-        .to receive(:pdf).and_raise(Prawn::Errors::CannotFit)
-
-      get :index, format: "pdf"
-    end
-
-    it "should redirect to the html index and show an error message" do
-      expect(flash[:error].downcase).to include("too many columns")
-    end
+  # Returns a PDF string of a single work_package
+  def single
+    ::WorkPackage::PdfExport::WorkPackageToPdf
+      .new(work_package)
+      .render!
   end
 end

commit 9f750c82b2b8d580af618ec52779d8ae093a1008

append file type to export
broken in 267f9b0ea24260b69fba958f330a301b0a6b7f7c

app/models/work_package/pdf_export/work_package_list_to_pdf.rb
@@ -61,13 +61,17 @@ class WorkPackage::PdfExport::WorkPackageListToPdf < WorkPackage::Exporter::Base
   end
 
   def write_title!
-    pdf.title = title
+    pdf.title = heading
     pdf.font style: :bold, size: 11
-    pdf.text title
+    pdf.text heading
     pdf.move_down 20
   end
 
   def title
+    "#{heading}.pdf"
+  end
+
+  def heading
     title = query.new_record? ? I18n.t(:label_work_package_plural) : query.name
 
     if project

app/models/work_package/pdf_export/work_package_to_pdf.rb
@@ -148,14 +148,18 @@ class WorkPackage::PdfExport::WorkPackageToPdf < WorkPackage::Exporter::Base
   end
 
   def write_title!
-    pdf.title = title
+    pdf.title = heading
     pdf.font style: :bold, size: 11
-    pdf.text "#{work_package.project} - #{work_package.type} # #{work_package.id}: #{work_package.subject}"
+    pdf.text "#{heading}: #{work_package.subject}"
     pdf.move_down 20
   end
 
-  def title
+  def heading
     "#{work_package.project} - ##{work_package.type} #{work_package.id}"
+  end
+
+  def title
+    "#{heading}.pdf"
   end
 
   def write_attributes!