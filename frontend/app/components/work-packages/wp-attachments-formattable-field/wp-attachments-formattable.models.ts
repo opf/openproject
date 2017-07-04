021d8aa0d70b21add6b3c07e081e40d36fab484c
ulferts <j.ulferts@finn.de>
2017-07-04 08:07:59 +0200

    remove unwanted whitespaces before img attachment added via d&d (#5728)
    Whitespaces on a newline before the image will add a code block leading to scroll bars
    
    [ci skip]

--- frontend/app/components/work-packages/wp-attachments-formattable-field/wp-attachments-formattable.models.ts
+++ frontend/app/components/work-packages/wp-attachments-formattable-field/wp-attachments-formattable.models.ts
@@ -15,7 +15,7 @@ export class EditorModel implements IApplyAttachmentMarkup {
 
   public insertWebLink(url:string, insertMode:InsertMode = InsertMode.LINK):void {
     this.contentToInsert = this.markupModel.createMarkup(url, insertMode);
-  };
+  }
 
   public insertAttachmentLink(url:string,
                               insertMode:InsertMode = InsertMode.ATTACHMENT,
@@ -23,16 +23,42 @@ export class EditorModel implements IApplyAttachmentMarkup {
     this.contentToInsert = (addLineBreak) ?
     this.contentToInsert + this.markupModel.createMarkup(url, insertMode, addLineBreak) :
       this.markupModel.createMarkup(url, insertMode, addLineBreak);
-  };
+  }
 
   private setCaretPosition():void {
     this.currentCaretPosition = (this.textarea[0] as HTMLTextAreaElement).selectionStart;
-  };
+  }
 
   public save():void {
-    this.textarea.val(this.textarea.val().substring(0, this.currentCaretPosition) +
-      this.contentToInsert +
-      this.textarea.val().substring(this.currentCaretPosition, this.textarea.val().length)).change();
+    let insertPosition = this.normalizeInputAndGetInsertPosition();
+    let currentValue = this.textarea.val();
+
+    let newValue = currentValue.substring(0, insertPosition) +
+                   this.contentToInsert +
+                   currentValue.substring(this.currentCaretPosition, currentValue.length);
+
+    this.textarea.val(newValue).change();
+  }
+
+  /*
+   * Assure that no whitespace is left before the inlined image as whitespaces will lead to the image being wrapped in a
+   * <pre><code>image</code><pre> block
+   * Removes two sources of whitespace:
+   *  * The whitespace prepended to the content to insert
+   *  * Whitespace added by the user on a newline
+   */
+
+  private normalizeInputAndGetInsertPosition() {
+    let currentValue = this.textarea.val();
+    let newlineMatch = currentValue.substring(0, this.currentCaretPosition).match(/\n *$/);
+    let newlineIndex = newlineMatch && newlineMatch.index || 0;
+    let whitespacesBeforeCaret = this.currentCaretPosition - newlineIndex;
+
+    if (whitespacesBeforeCaret > 0) {
+      this.contentToInsert = this.contentToInsert.substring(1, this.contentToInsert.length);
+    }
+
+    return (newlineIndex && newlineIndex + 1) || this.currentCaretPosition;
   }
 }
 
@@ -98,7 +124,7 @@ export class DropModel {
     }
 
     return false;
-  };
+  }
 
   public isAttachmentOfCurrentWp():boolean {
     if (this.isWebLink) {
@@ -120,15 +146,15 @@ export class DropModel {
     }
 
     return false;
-  };
+  }
 
   public filesAreValidForUploading():boolean {
     return true;
-  };
+  }
 
   public removeHostInformationFromUrl():string {
     return this.webLinkUrl.replace(window.location.origin, '');
-  };
+  }
 
   protected _isUpload(dt:DataTransfer):boolean {
     if (dt.types && this.filesCount > 0) {
@@ -172,20 +198,19 @@ export class FieldModel implements IApplyAttachmentMarkup {
 
   private addInitialLineBreak():string {
     return (this.contentToInsert.length > 0) ? '\r\n' : '';
-  };
+  }
 
   public insertAttachmentLink(url:string, insertMode:InsertMode, addLineBreak?:boolean):void {
     this.contentToInsert += this.addInitialLineBreak() + this.markupModel.createMarkup(url, insertMode, false);
-  };
+  }
 
   public insertWebLink(url:string, insertMode:InsertMode):void {
     this.contentToInsert += this.addInitialLineBreak() + this.markupModel.createMarkup(url, insertMode, false);
-  };
+  }
 
   public save():void {
     this.workPackage.description.raw = this.contentToInsert;
     this.workPackage.save();
-  };
-
+  }
 }
 
