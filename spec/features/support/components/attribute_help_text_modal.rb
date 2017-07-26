b3efc877cd806fc94053bc54ce6b2e2a56a59ff3
ulferts <j.ulferts@finn.de>
2017-07-26 07:42:33 +0200

    adapt help text modal style (#5788)
    In order to allow using the keyboard to trigger the close button, the ngDialog close functionality is disabled and a custom close button (the X) is added which is tabbable.
    
    The rest is as specified in https://community.openproject.com/projects/openproject/work_packages/25912
    
    Remove close button
    Change label to "Edit" (EN), "Bearbeiten" (DE)
    Decrease space between button and bottom.
    Remove blue border/highlight-effect around text
    Restore help/info icon left to header text
    
    [ci skip]

--- spec/features/support/components/attribute_help_text_modal.rb
+++ spec/features/support/components/attribute_help_text_modal.rb
@@ -26,48 +26,52 @@
 # See doc/COPYRIGHT.rdoc for more details.
 #++
 
-class AttributeHelpTextModal
-  include Capybara::DSL
-  include RSpec::Matchers
+module Components
+  class AttributeHelpTextModal
+    include Capybara::DSL
+    include RSpec::Matchers
 
-  attr_reader :help_text, :context
+    attr_reader :help_text, :context
 
-  def initialize(help_text, context: nil)
-    @context = context
-    @help_text = help_text
-  end
-
-  def container
-    if context
-      page.find(context)
-    else
-      page
+    def initialize(help_text, context: nil)
+      @context = context
+      @help_text = help_text
     end
-  end
 
-  def modal_container
-    container.find('.attribute-help-text--modal')
-  end
-
-  def open!
-    container.find(".help-text--for-#{help_text.attribute_name}").click
-    expect(page).to have_selector('.attribute-help-text--modal .modal--header', text: help_text.attribute_caption)
-  end
-
-  def close!
-    page.find('.help-text--close-button').click
-    expect(page).to have_no_selector('.attribute-help-text--modal .modal--header', text: help_text.attribute_caption)
-  end
-
-  def expect_edit(admin:)
-    if admin
-      expect(page).to have_selector('.help-text--edit-button')
-    else
-      expect(page).to have_no_selector('.help-text--edit-button')
+    def container
+      if context
+        page.find(context)
+      else
+        page
+      end
     end
-  end
 
-  def edit_button
-    page.find('.help-text--edit-button')
+    def modal_container
+      container.find('.attribute-help-text--modal')
+    end
+
+    def open!
+      container.find(".help-text--for-#{help_text.attribute_name}").click
+      expect(page).to have_selector('.attribute-help-text--modal .modal--header', text: help_text.attribute_caption)
+    end
+
+    def close!
+      within modal_container do
+        page.find('.ngdialog-close').click
+      end
+      expect(page).to have_no_selector('.attribute-help-text--modal .modal--header', text: help_text.attribute_caption)
+    end
+
+    def expect_edit(admin:)
+      if admin
+        expect(page).to have_selector('.help-text--edit-button')
+      else
+        expect(page).to have_no_selector('.help-text--edit-button')
+      end
+    end
+
+    def edit_button
+      page.find('.help-text--edit-button')
+    end
   end
 end
