a0ab4ddccc2f20685d860eca4723e361b87d8fb1
Oliver Günther <mail@oliverguenther.de>
2017-09-05 21:08:30 +0200

    Fix deprecation in rack-attack

--- config/initializers/rack-attack.rb
+++ config/initializers/rack-attack.rb
@@ -28,12 +28,12 @@
 
 if OpenProject::Configuration.blacklisted_routes.any?
   # Block logins from a bad user agent
-  Rack::Attack.blacklist('block forbidden routes') do |req|
+  Rack::Attack.blocklist('block forbidden routes') do |req|
     regex = OpenProject::Configuration.blacklisted_routes.map! { |str| Regexp.new(str) }
     regex.any? { |i| i =~ req.path }
   end
 
-  Rack::Attack.blacklisted_response = lambda do |_env|
+  Rack::Attack.blocklisted_response = lambda do |_env|
     # All blacklisted routes would return a 404.
     [404, {}, ['Not found']]
   end
