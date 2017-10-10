dddfb6932efb20bbfe9854dd31b32fb1359fac8b
Wieland Lindenthal <w.lindenthal@forkmerge.com>
2017-10-10 18:13:30 +0200

    Improve accessibility for collapsing and expanding the project description

--- app/assets/javascripts/project/description_handling.js
+++ app/assets/javascripts/project/description_handling.js
@@ -31,12 +31,8 @@ function toggleDescription(el) {
   let clickedRow = jQuery(el.closest('.project'))[0];
   let descriptionRow = clickedRow.nextElementSibling;
 
-  if($el.hasClass("icon-arrow-down1")) {
-    $el.removeClass("icon-arrow-down1").addClass("icon-arrow-up1");
-  } else {
-    $el.removeClass("icon-arrow-up1").addClass("icon-arrow-down1");
-  }
 
   clickedRow.classList.toggle('-no-highlighting');
+  clickedRow.classList.toggle('-expanded');
   descriptionRow.classList.toggle('-expanded');
 }
