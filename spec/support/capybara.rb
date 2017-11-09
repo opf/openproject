5dc501158f836a4c9293d6394e42cd7558a19d6c
Oliver Günther <mail@oliverguenther.de>
2017-11-09 14:48:11 +0100

    Enable debug logging

--- spec/support/capybara.rb
+++ spec/support/capybara.rb
@@ -82,7 +82,7 @@ Capybara.register_driver :selenium do |app|
   end
 
   # If you need to trace the webdriver commands, un-comment this line
-  # Selenium::WebDriver.logger.level = :debug
+  Selenium::WebDriver.logger.level = :debug
 
   Capybara::Selenium::Driver.new(
     app,
