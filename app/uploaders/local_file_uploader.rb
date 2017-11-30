8575bc863208bfe944564f7212f7be9eb59036a5
Oliver Günther <mail@oliverguenther.de>
2017-11-30 12:10:46 +0100

    Fix include in LocalFileUploader
    [ci skip]

--- app/uploaders/local_file_uploader.rb
+++ app/uploaders/local_file_uploader.rb
@@ -29,8 +29,16 @@
 require 'fileutils'
 
 class LocalFileUploader < CarrierWave::Uploader::Base
-  storage :file
   include FileUploader
+  storage :file
+
+  # Delete cache and old rack file after store
+  # cf. https://github.com/carrierwaveuploader/carrierwave/wiki/How-to:-Delete-cache-garbage-directories
+
+  before :store, :remember_cache_id
+  after :store, :delete_tmp_dir
+  after :store, :delete_old_tmp_file
+
 
   def copy_to(attachment)
     attachment.file = local_file
