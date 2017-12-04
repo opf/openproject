a06719e3338ea7eaaf2561288beaddc4f46a2183
Roman Roelofsen <romanroe@gmail.com>
2017-12-04 11:52:42 +0100

    Angular4 migration: fixed accessible_by_keyboard directive

--- frontend/app/ui_components/accessible-by-keyboard-directive-upgraded.ts
+++ frontend/app/ui_components/accessible-by-keyboard-directive-upgraded.ts
@@ -26,13 +26,21 @@
 // See doc/COPYRIGHT.rdoc for more details.
 //++
 
-import {Component, Directive, ElementRef, Injector} from '@angular/core';
+import {Component, Directive, ElementRef, Injector, Input, Output} from '@angular/core';
 import {UpgradeComponent} from '@angular/upgrade/static';
 
 @Directive({
   selector: 'accessible-by-keyboard'
 })
 export class AccessibleByKeyboardDirectiveUpgraded extends UpgradeComponent {
+
+  @Output() execute: any;
+  @Input() isDisabled: boolean;
+  @Input() linkClass: string;
+  @Input() linkTitle: string;
+  @Input() spanClass: string;
+  @Input() linkAriaLabel: string;
+
   constructor(elementRef:ElementRef, injector:Injector) {
     super('accessibleByKeyboard', elementRef, injector);
   }
