e932d533835de381ff7cedd6ddb2d26d39196c5a
Oliver Günther <mail@oliverguenther.de>
2017-12-18 08:52:13 +0100

    Require dep to WP

--- lib/open_project/costs/patches/work_package_patch.rb
+++ lib/open_project/costs/patches/work_package_patch.rb
@@ -17,6 +17,8 @@
 # Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
 #++
 
+require_dependency 'work_package'
+
 module OpenProject::Costs::Patches
   module WorkPackagePatch
     def self.mixin!
