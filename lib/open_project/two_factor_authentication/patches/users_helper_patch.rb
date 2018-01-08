079d95b9eb09f3bb87562623d2bdf7a30c83d980
Henriette Dinger <HDinger@users.noreply.github.com>
2018-01-08 08:04:22 +0100

    Show 2fa tab in Admin/users only when it is enabled (#5)
    [ci skip]

--- lib/open_project/two_factor_authentication/patches/users_helper_patch.rb
+++ lib/open_project/two_factor_authentication/patches/users_helper_patch.rb
@@ -13,7 +13,11 @@ module OpenProject::TwoFactorAuthentication::Patches
       # Adds a 2FA tab to the user administration page
       def user_settings_tabs_with_2fa
         # Core defined data
-        user_settings_tabs_without_2fa + [{ name: 'two_factor_authentication', partial: 'users/two_factor_authentication', label: 'two_factor_authentication.label_two_factor_authentication' }]
+        if OpenProject::TwoFactorAuthentication::TokenStrategyManager.enabled?
+          user_settings_tabs_without_2fa + [{ name: 'two_factor_authentication', partial: 'users/two_factor_authentication', label: 'two_factor_authentication.label_two_factor_authentication' }]
+        else
+          user_settings_tabs_without_2fa
+        end
       end
     end
   end
