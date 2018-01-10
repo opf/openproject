e4d8f3f0a312b2b6b1b106685419a058898c0dec
Oliver Günther <mail@oliverguenther.de>
2018-01-10 11:34:20 +0100

    Fix removed methods from timelines helper

--- app/controllers/project_types_controller.rb
+++ app/controllers/project_types_controller.rb
@@ -28,13 +28,10 @@
 #++
 
 class ProjectTypesController < ApplicationController
-  helper :timelines
-
   before_action :disable_api
   before_action :check_permissions
   accept_key_auth :index, :show
 
-  helper :timelines
   layout 'admin'
 
   def index
