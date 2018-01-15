8945430cf9d20309cb81f3ab57fc5859d8b62431
Oliver Günther <mail@oliverguenther.de>
2018-01-15 15:33:14 +0100

    Replace fuzzy with fuse and stricter options  (#6132)
    * Replace fuzzy with fuse and stricter options
    
    * Fix performance issue on jquery-ui autocompleter
    
    https://bugs.jqueryui.com/ticket/15082
    
    [ci skip]

--- frontend/app/components/projects/project-menu-autocomplete/project-menu-autocomplete.component.ts
+++ frontend/app/components/projects/project-menu-autocomplete/project-menu-autocomplete.component.ts
@@ -180,6 +180,7 @@ export class ProjectMenuAutocompleteController extends ILazyAutocompleterBridge<
    */
   protected augmentedResultSet(items:ProjectAutocompleteItem[], matched:ProjectAutocompleteItem[]) {
     const matches = matched.map(el => el.object.identifier);
+    console.log(matches);
     const matchedParents = _.flatten(matched.map(el => el.object.parents));
 
     const results:ProjectAutocompleteItem[] = [];
