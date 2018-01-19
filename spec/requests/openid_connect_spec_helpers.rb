3e3fa3daaeebfe8e0177052f766936f074dcb547
Oliver Günther <mail@oliverguenther.de>
2018-01-19 13:23:44 +0100

    Fix deprecated param

--- spec/requests/openid_connect_spec_helpers.rb
+++ spec/requests/openid_connect_spec_helpers.rb
@@ -2,8 +2,10 @@ module OpenIDConnectSpecHelpers
   def redirect_from_provider(name = 'heroku')
     # Emulate the provider's redirect with a nonsense code.
     get "/auth/#{name}/callback",
-      :code => 'foobar',
-      :redirect_uri => "http://localhost:3000/auth/#{name}/callack"
+        params: {
+          code: 'foobar',
+          redirect_uri: "http://localhost:3000/auth/#{name}/callack"
+        }
   end
 
   def click_on_signin(pro_name = 'heroku')
