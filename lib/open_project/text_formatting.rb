9d5caaa1208c7648018d23a0ca6894de29518366
Oliver Günther <mail@oliverguenther.de>
2018-01-20 11:15:57 +0100

    Unsafe anchor URL generation in text formatting

--- lib/open_project/text_formatting.rb
+++ lib/open_project/text_formatting.rb
@@ -38,6 +38,7 @@ module OpenProject
     include Redmine::I18n
     include ActionView::Helpers::TextHelper
     include OpenProject::ObjectLinking
+    include OpenProject::SafeParams
     # The WorkPackagesHelper is required to get access to the methods
     # 'work_package_css_classes' and 'work_package_quick_info'.
     include WorkPackagesHelper
@@ -524,9 +525,10 @@ module OpenProject
     #
     def full_url(anchor_name = '')
       return "##{anchor_name}" if current_request.nil?
-      current = request.original_fullpath
-      return current if anchor_name.blank?
-      "#{current}##{anchor_name}"
+      url_for pagination_params_whitelist.merge(anchor: anchor_name, only_path: true)
+    rescue ActionController::UrlGenerationError
+      # In a context outside params, we don't know what the relative anchor url is
+      "##{anchor_name}"
     end
 
     def current_request
