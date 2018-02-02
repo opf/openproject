546143e8ecb2a647b8679829b31c461ff76bfd0b
Wieland Lindenthal <w.lindenthal@forkmerge.com>
2018-02-02 15:24:21 +0100

    DRY migration

--- db/migrate/20180122135443_add_tsv_columns_to_attachments.rb
+++ db/migrate/20180122135443_add_tsv_columns_to_attachments.rb
@@ -29,7 +29,7 @@
 #++
 
 class AddTsvColumnsToAttachments < ActiveRecord::Migration[5.0]
-  def up
+  def change
     if OpenProject::Database.allows_tsv?
       add_column :attachments, :fulltext_tsv, :tsvector
       add_column :attachments, :file_tsv, :tsvector
@@ -37,17 +37,11 @@ class AddTsvColumnsToAttachments < ActiveRecord::Migration[5.0]
       add_index :attachments, :fulltext_tsv, using: "gin"
       add_index :attachments, :file_tsv, using: "gin"
     else
-      warn "Your installation does not support full-text search features. Better use PostgreSQL in version 9.6 or higher."
-    end
-  end
-
-  def down
-    if OpenProject::Database.allows_tsv?
-      remove_index :attachments, :fulltext_tsv
-      remove_index :attachments, :file_tsv
-
-      remove_column :attachments, :fulltext_tsv
-      remove_column :attachments, :file_tsv
+      reversible do |dir|
+        dir.up do
+          warn "Your installation does not support full-text search features. Better use PostgreSQL in version 9.6 or higher."
+        end
+      end
     end
   end
 end
