fd62f0785978b7bab716bd9a9d7ee93f4e742c20
Oliver Günther <mail@oliverguenther.de>
2018-02-26 09:24:21 +0100

    Don't use csp_onclick in rails cell
    We don't have the action_view context present, so we can't use `content_for`.

--- app/assets/javascripts/members_form.js
+++ app/assets/javascripts/members_form.js
@@ -26,34 +26,6 @@
 // See docs/COPYRIGHT.rdoc for more details.
 //++
 
-jQuery(document).ready(function($) {
-  $("#tab-content-members").submit('#members_add_form', function () {
-    var error = $('.errorExplanation, .flash');
-    if (error) {
-      error.remove();
-    }
-  });
-
-  // Show/Hide content when page is loaded
-  if (window.OpenProject.guardedLocalStorage("showFilter") === "true") {
-    showFilter(filter = findFilter());
-  }
-  else {
-    hideFilter(filter = findFilter());
-    // In case showFilter is not set yet
-    window.OpenProject.guardedLocalStorage("showFilter", 'false')
-  }
-
-  // show member form only when there's an error
-  if (jQuery("#errorExplanation").text() != "") {
-    showAddMemberForm();
-  }
-
-  if (jQuery('#add-member-button').attr('data-trigger-initially')) {
-    showAddMemberForm();
-  }
-});
-
 function toggleMemberFilter() {
   if (window.OpenProject.guardedLocalStorage("showFilter") === "true") {
     window.OpenProject.guardedLocalStorage("showFilter", 'false');
@@ -83,3 +55,34 @@ function hideAddMemberForm() {
   jQuery('#add-member-button').focus();
   jQuery('#add-member-button').prop('disabled', false);
 }
+
+jQuery(document).ready(function($) {
+  $("#tab-content-members").submit('#members_add_form', function () {
+    var error = $('.errorExplanation, .flash');
+    if (error) {
+      error.remove();
+    }
+  });
+
+  // Show/Hide content when page is loaded
+  if (window.OpenProject.guardedLocalStorage("showFilter") === "true") {
+    showFilter(filter = findFilter());
+  }
+  else {
+    hideFilter(filter = findFilter());
+    // In case showFilter is not set yet
+    window.OpenProject.guardedLocalStorage("showFilter", 'false');
+  }
+
+  $('.toggle-member-filter-link').click(toggleMemberFilter);
+
+
+  // show member form only when there's an error
+  if (jQuery("#errorExplanation").text() != "") {
+    showAddMemberForm();
+  }
+
+  if (jQuery('#add-member-button').attr('data-trigger-initially')) {
+    showAddMemberForm();
+  }
+});
