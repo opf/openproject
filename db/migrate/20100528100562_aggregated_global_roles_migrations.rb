57ce79e541dfc0f72a36fdb3575537846ae25bef
Jens Ulferts <j.ulferts@finn.de>
2018-03-02 09:45:50 +0100

    declare rails version in migrations

--- db/migrate/20100528100562_aggregated_global_roles_migrations.rb
+++ db/migrate/20100528100562_aggregated_global_roles_migrations.rb
@@ -20,7 +20,7 @@
 require Rails.root.join('db', 'migrate', 'migration_utils', 'migration_squasher').to_s
 require 'open_project/plugins/migration_mapping'
 # This migration aggregates the migrations detailed in MIGRATION_FILES
-class AggregatedGlobalRolesMigrations < ActiveRecord::Migration
+class AggregatedGlobalRolesMigrations < ActiveRecord::Migration[5.0]
   MIGRATION_FILES = <<-MIGRATIONS
     001_sti_for_roles.rb
   MIGRATIONS
