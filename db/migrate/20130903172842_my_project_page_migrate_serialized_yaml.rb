3644255cd6c06a47aea4dc11128e0a96212f9e56
Jens Ulferts <j.ulferts@finn.de>
2018-03-02 09:56:39 +0100

    declare rails version in migrations

--- db/migrate/20130903172842_my_project_page_migrate_serialized_yaml.rb
+++ db/migrate/20130903172842_my_project_page_migrate_serialized_yaml.rb
@@ -20,7 +20,7 @@
 
 require Rails.root.join("db","migrate","migration_utils","legacy_yamler").to_s
 
-class MyProjectPageMigrateSerializedYaml < ActiveRecord::Migration
+class MyProjectPageMigrateSerializedYaml < ActiveRecord::Migration[5.0]
   include Migration::LegacyYamler
 
   def up
