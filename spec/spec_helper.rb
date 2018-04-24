d5a0a058d3eeb576b5362ff280a05f38e3670ec0
Oliver Günther <mail@oliverguenther.de>
2018-04-24 13:29:48 +0200

    Revert "Use doc formatter"
    [ci skip]
    
    This reverts commit ad4a8268fb7d2589c14b44346fcadd3f6c60c5ca.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -84,7 +84,17 @@ RSpec.configure do |config|
   # Many RSpec users commonly either run the entire suite or an individual
   # file, and it's useful to allow more verbose output when running an
   # individual spec file.
-  config.default_formatter = 'doc'
+  if config.files_to_run.one?
+    # Use the documentation formatter for detailed output,
+    # unless a formatter has already been configured
+    # (e.g. via a command-line flag).
+    config.default_formatter = 'doc'
+  else
+    # Otherwise, use the Fuubar formatter if we're not on the CI
+    unless ENV['TEST_ENV_NUMBER'] || ENV['CI']
+      config.default_formatter = Fuubar
+    end
+  end
 
   # Print the 10 slowest examples and example groups at the
   # end of the spec run, to help surface which specs are running
