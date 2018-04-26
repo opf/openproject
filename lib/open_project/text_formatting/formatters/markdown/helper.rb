2783a8222c2f5c2ba720ba4150628eaa18333ffb
Oliver Günther <mail@oliverguenther.de>
2018-04-26 09:40:06 +0200

    [27544] Avoid self-closing tag on html5 custom element
    https://community.openproject.com/wp/27544

--- lib/open_project/text_formatting/formatters/markdown/helper.rb
+++ lib/open_project/text_formatting/formatters/markdown/helper.rb
@@ -39,7 +39,7 @@ module OpenProject::TextFormatting::Formatters
       end
 
       def wikitoolbar_for(field_id)
-        tag 'op-ckeditor-form', 'textarea-selector': "##{field_id}"
+        content_tag 'op-ckeditor-form', '', 'textarea-selector': "##{field_id}"
       end
 
       def initial_page_content(_page)
