d3993266ada67478791a50c8c25d997c36a212e0
Roman Roelofsen <romanroe@gmail.com>
2018-05-04 16:51:31 +0200

    cleanup proxy config

--- frontend/cli_to_rails_proxy.js
+++ frontend/cli_to_rails_proxy.js
@@ -2,20 +2,9 @@ var PROXY_CONFIG = [
   {
     "context": ['/**'],
     "target": "http://localhost:3000",
-    "secure": false,
-    "bypass": function (req, res, proxyOptions) {
-      // if (req.url === "/main.bundle.js") {
-      //   return "/main.bundle.js";
-      // }
-
-      // if (req.headers.accept.indexOf("html") !== -1) {
-      //     console.log("Skipping proxy for browser request.");
-      //     return "/index.html";
-      // }
-      // req.headers["X-Custom-Header"] = "yes";
-
-      // return false;
-    }
+    "secure": false
+    // "bypass": function (req, res, proxyOptions) {
+    // }
   }
 ];
 
