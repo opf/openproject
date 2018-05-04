2007e888c9801e991e3a48d99d3c58bd29006c32
Roman Roelofsen <romanroe@gmail.com>
2018-05-04 16:42:53 +0200

    update Angular 6

--- frontend/cli_to_rails_proxy.js
+++ frontend/cli_to_rails_proxy.js
@@ -1,21 +1,22 @@
-var PROXY_CONFIG = {
-    "/": {
-        "target": "http://localhost:3000",
-        "secure": false,
-        "bypass": function (req, res, proxyOptions) {
-            console.log("bypass?");
-            console.log("req.url", req.url);
+var PROXY_CONFIG = [
+  {
+    "context": ['/**'],
+    "target": "http://localhost:3000",
+    "secure": false,
+    "bypass": function (req, res, proxyOptions) {
+      // if (req.url === "/main.bundle.js") {
+      //   return "/main.bundle.js";
+      // }
 
-            // if (req.headers.accept.indexOf("html") !== -1) {
-            //     console.log("Skipping proxy for browser request.");
-            //     return "/index.html";
-            // }
-            // req.headers["X-Custom-Header"] = "yes";
+      // if (req.headers.accept.indexOf("html") !== -1) {
+      //     console.log("Skipping proxy for browser request.");
+      //     return "/index.html";
+      // }
+      // req.headers["X-Custom-Header"] = "yes";
 
-            // return "/BLA";
-            return false;
-        }
+      // return false;
     }
-};
+  }
+];
 
 module.exports = PROXY_CONFIG;
