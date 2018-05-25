3c0690b7104d481437ab877dc766687d000a610f
Oliver Günther <mail@oliverguenther.de>
2018-05-25 08:20:51 +0200

    Ensure authentication stage is registered during reload
    And just for good measure, ensure stages cannot register the same name
    twice (does not happen while reloading, but still...)

--- config/initializers/authentication_stages.rb
+++ config/initializers/authentication_stages.rb
@@ -1,7 +1,13 @@
-OpenProject::Authentication::Stage
-  .register(
-    :consent,
-    active: ->() { Setting.consent_required? }
-  ) {
-    account_consent_path
-  }
+OpenProject::Application.configure do |application|
+
+  # Ensure stage is entered when reloading in dev mode
+  application.config.to_prepare do
+    OpenProject::Authentication::Stage
+      .register(
+        :consent,
+        active: ->() { Setting.consent_required? }
+      ) {
+        account_consent_path
+      }
+  end
+end
