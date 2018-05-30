237156a260c981081a16d537f6084e3e85e78188
Oliver Günther <mail@oliverguenther.de>
2018-05-30 16:50:20 +0200

    Allow wysiwyg tables to exist with and without header

---
+++ spec/support/components/wysiwyg/wysiwyg_editor.rb
@@ -0,0 +1,40 @@
+module Components
+  class WysiwygEditor
+    include Capybara::DSL
+    attr_reader :context_selector
+
+    def initialize(context = '#content')
+      @context_selector = context
+    end
+
+    def container
+      page.find("#{context_selector} .op-ckeditor--wrapper")
+    end
+
+    def editor_element
+      page.find "#{context_selector} #{input_selector}"
+    end
+
+    def in_editor
+      yield container, editor_element
+    end
+
+    def input_selector
+      'div.ck-content'
+    end
+
+    def expect_value(value)
+      expect(input_element.text).to eq(value)
+    end
+
+    def click_and_type_slowly(text)
+      sleep 0.5
+      input_element.click
+
+      sleep 0.5
+      input_element.send_keys text
+
+      sleep 0.5
+    end
+  end
+end
