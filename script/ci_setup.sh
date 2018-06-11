7603a99f8441e22f4ccc386460d879645974705b
Jens Ulferts <j.ulferts@finn.de>
2018-06-11 17:01:03 +0200

    do not use CLI proxy on CI

--- script/ci_setup.sh
+++ script/ci_setup.sh
@@ -59,6 +59,8 @@ fi
 run "for i in {1..3}; do npm install && break || sleep 15; done"
 
 if [ $1 != 'specs' ] && [ $1 != 'spec_legacy' ]; then
+  # as assets are compiled, the angular cli proxy does not serve assets
+  run "export OPENPROJECT_CLI_PROXY=''"
   run "bundle exec rake assets:precompile"
 else
   # fake result of npm/asset run
