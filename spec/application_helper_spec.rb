087115fd47857941c8b31e7cb2f4c5af1f9a46af
Oliver Günther <mail@oliverguenther.de>
2018-07-23 09:46:30 +0200

    Remove text_formatting setting
    [ci skip]

--- spec/application_helper_spec.rb
+++ spec/application_helper_spec.rb
@@ -38,7 +38,7 @@ describe ApplicationHelper do
   include Rails.application.routes.url_helpers
 
 
-  describe ".format_text", with_settings: { text_formatting: 'markdown' } do
+  describe ".format_text"  do
     let(:project) { FactoryBot.create :valid_project }
     let(:identifier) { project.identifier }
     let(:role) {
