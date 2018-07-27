953ef10e7ad020f404dac43369416e71d9a0450f
Oliver Günther <mail@oliverguenther.de>
2018-07-27 13:13:04 +0200

    Enable caching in dev mode

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -52,7 +52,7 @@ OpenProject::Application.configure do
 
   # Show full error reports and disable caching.
   config.consider_all_requests_local       = true
-  config.action_controller.perform_caching = false
+  config.action_controller.perform_caching = true
 
   # Raise exceptions instead of rendering exception templates.
   config.action_dispatch.show_exceptions = false
