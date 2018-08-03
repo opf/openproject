0de05582e6764a3b5dc44a5a8e142d0137d599c0
Jens Ulferts <j.ulferts@finn.de>
2018-08-03 14:30:50 +0200

    fix autocomplete handling in specs

--- spec/features/support/components/ui_autocomplete.rb
+++ spec/features/support/components/ui_autocomplete.rb
@@ -37,8 +37,23 @@ module Components
 
       ##
       # Find the open dropdown
-      scroll_to_element(element.find('.ui-autocomplete'))
-      element.find('.ui-autocomplete', visible: true)
+      input_selector = "#{element.tag_name}.#{element[:class].split(' ').join('.')}"
+
+      # Get the autocomplete instance belonging to the input
+      autocompleter = page.evaluate_script <<-SCRIPT
+        jQuery('#{input_selector}').autocomplete('instance');
+      SCRIPT
+
+      # If we have an instance, great,
+      # if not we just hope that there is no other autocomplete open on the page.
+      list = if autocompleter
+               autocompleter['classesElementLookup']['ui-autocomplete'][0]
+             else
+               page.find('.ui-autocomplete')
+             end
+
+      scroll_to_element(list)
+      list
     end
 
     def select_autocomplete(element, query:, select_text: nil)
