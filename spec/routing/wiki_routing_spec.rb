271f534b4012cb92ce3a4b6666570243807972eb
Oliver Günther <mail@oliverguenther.de>
2018-08-15 11:01:46 +0200

    [28205] Remove HTML export wiki and rename txt to markdown
    https://community.openproject.com/wp/28205

--- spec/routing/wiki_routing_spec.rb
+++ spec/routing/wiki_routing_spec.rb
@@ -44,20 +44,12 @@ describe WikiController, type: :routing do
                                                                 id: 'blubs.blubs')
     end
 
-    it 'should connect GET /projects/:project_id/wiki/:name.txt to wiki/show' do
-      expect(get('/projects/abc/wiki/blubs.txt')).to route_to(controller: 'wiki',
-                                                              action: 'show',
-                                                              project_id: 'abc',
-                                                              id: 'blubs',
-                                                              format: 'txt')
-    end
-
     it 'should connect GET /projects/:project_id/wiki/:name.html to wiki/show' do
-      expect(get('/projects/abc/wiki/blubs.html')).to route_to(controller: 'wiki',
+      expect(get('/projects/abc/wiki/blubs.markdown')).to route_to(controller: 'wiki',
                                                                action: 'show',
                                                                project_id: 'abc',
                                                                id: 'blubs',
-                                                               format: 'html')
+                                                               format: 'markdown')
     end
 
     it 'should connect GET /projects/:project_id/wiki/new to wiki/new' do
