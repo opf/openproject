3ea64f454c63b5565b61709ce5d18171d97ef7e3
Wieland Lindenthal <w.lindenthal@forkmerge.com>
2018-09-24 16:29:08 +0200

    Move adding "highlighted_attributes" to a separate migration as some people might have run the first migration already.

--- db/migrate/20180903110212_add_highlighting_to_query.rb
+++ db/migrate/20180903110212_add_highlighting_to_query.rb
@@ -1,6 +1,5 @@
 class AddHighlightingToQuery < ActiveRecord::Migration[5.1]
   def change
     add_column :queries, :highlighting_mode, :text
-    add_column :queries, :highlighted_attributes, :text
   end
 end
