bf14d2c6bfe3711e5578f0f13fa0364c8af57741
Oliver Günther <mail@oliverguenther.de>
2018-11-21 10:29:22 +0100

    [29049] Fix missing start-of-week without user_specific_includes
    user_specific_javascript_includes was removed in the transition to
    Angular4, however the start of week attribute was not communicated since
    then.
    
    https://community.openproject.com/wp/29049

--- frontend/src/test/i18n-shim.ts
+++ frontend/src/test/i18n-shim.ts
@@ -3,6 +3,7 @@ import {GlobalI18n} from "core-app/modules/common/i18n/i18n.service";
 export class I18nShim implements GlobalI18n {
 
   public defaultLocale = 'en';
+  public firstDayOfWeek = 1;
   public locale = 'en';
   public translations = {
     en: {}
