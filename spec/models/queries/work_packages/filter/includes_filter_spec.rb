8b1ed6552ec590a4a3e0d95e33c80f82baedfdd3
Wieland Lindenthal <w.lindenthal@forkmerge.com>
2018-11-21 11:24:53 +0100

    Fix spec for IncludesFilter

--- spec/models/queries/work_packages/filter/includes_filter_spec.rb
+++ spec/models/queries/work_packages/filter/includes_filter_spec.rb
@@ -28,9 +28,9 @@
 
 require 'spec_helper'
 
-describe Queries::WorkPackages::Filter::PartOfFilter, type: :model do
+describe Queries::WorkPackages::Filter::IncludesFilter, type: :model do
   it_behaves_like 'filter by work package id' do
-    let(:class_key) { :part_of }
+    let(:class_key) { :includes }
 
     it_behaves_like 'filter for relation' do
       let(:relation_type) { :includes }
