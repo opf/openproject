f07be9f9d0e069b67a29714349c002d427d63ec7
Henriette Dinger <h.dinger@openproject.com>
2018-11-23 09:56:31 +0100

    Add missing class for backlogs setting

--- modules/backlogs/app/helpers/version_settings_helper.rb
+++ modules/backlogs/app/helpers/version_settings_helper.rb
@@ -40,7 +40,9 @@ module VersionSettingsHelper
     content_tag :div, class: 'form--field' do
       [
         styled_label_tag(name_for_setting_attributes('display'), t(:label_column_in_backlog)),
-        styled_select_tag(name_for_setting_attributes('display'), options_for_select(position_display_options, setting.display), container_class: '-xslim' ),
+        content_tag(:div,
+                    styled_select_tag(name_for_setting_attributes('display'), options_for_select(position_display_options, setting.display), container_class: '-xslim' ),
+                    class: 'form--field-container'),
         version_hidden_id_field(setting),
         hidden_field_tag('project_id', project.id)
       ].join.html_safe
