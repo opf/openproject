68bd691e7faf6c2693f33c26743843102ac0070d
Jens Ulferts <j.ulferts@finn.de>
2018-12-17 14:30:51 +0100

    provide index action and documentation on document api

--- modules/documents/spec/requests/api/v3/documents/documents_resource_spec.rb
+++ modules/documents/spec/requests/api/v3/documents/documents_resource_spec.rb
@@ -40,7 +40,9 @@ describe 'API v3 documents resource', type: :request do
     FactoryBot.create(:user, member_in_project: project, member_through_role: role)
   end
   let(:document) { FactoryBot.create(:document, project: project) }
+  let(:invisible_document) { FactoryBot.create(:document, project: other_project) }
   let(:project) { FactoryBot.create(:project) }
+  let(:other_project) { FactoryBot.create(:project) }
   let(:role) { FactoryBot.create(:role, permissions: permissions) }
   let(:permissions) { %i(view_documents) }
 
@@ -50,7 +52,41 @@ describe 'API v3 documents resource', type: :request do
     login_as(current_user)
   end
 
-  describe 'GET /api/v3/wiki_pages/:id' do
+  describe 'GET /api/v3/documents' do
+    let(:path) { api_v3_paths.documents }
+
+    before do
+      document
+      invisible_document
+
+      get path
+    end
+
+    it 'returns 200 OK' do
+      expect(subject.status)
+        .to eql(200)
+    end
+
+    it 'returns a Collection of visible documents' do
+      expect(subject.body)
+        .to be_json_eql('Collection'.to_json)
+        .at_path('_type')
+
+      expect(subject.body)
+        .to be_json_eql(1.to_json)
+        .at_path('total')
+
+      expect(subject.body)
+        .to be_json_eql('Document'.to_json)
+        .at_path('_embedded/elements/0/_type')
+
+      expect(subject.body)
+        .to be_json_eql(document.title.to_json)
+        .at_path('_embedded/elements/0/title')
+    end
+  end
+
+  describe 'GET /api/v3/documents/:id' do
     let(:path) { api_v3_paths.document(document.id) }
 
     before do
@@ -62,7 +98,7 @@ describe 'API v3 documents resource', type: :request do
         .to eql(200)
     end
 
-    it 'returns the wiki page' do
+    it 'returns the document' do
       expect(subject.body)
         .to be_json_eql('Document'.to_json)
         .at_path('_type')
