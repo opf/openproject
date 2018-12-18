8aea1f768a63031e74f3b728363318c1318a7f6e
Jens Ulferts <j.ulferts@finn.de>
2018-12-18 22:51:49 +0100

    i18n

--- frontend/src/app/components/routing/my-page/my-page.component.ts
+++ frontend/src/app/components/routing/my-page/my-page.component.ts
@@ -3,14 +3,18 @@ import {GridDmService} from "core-app/modules/hal/dm-services/grid-dm.service";
 import {GridResource} from "core-app/modules/hal/resources/grid-resource";
 import {PathHelperService} from "core-app/modules/common/path-helper/path-helper.service";
 import {HalResourceService} from "core-app/modules/hal/services/hal-resource.service";
+import {I18nService} from "core-app/modules/common/i18n/i18n.service";
 
 @Component({
   templateUrl: './my-page.component.html'
 })
 export class MyPageComponent implements OnInit {
+  public text = { title: this.i18n.t('js.label_my_page') };
+
   constructor(readonly gridDm:GridDmService,
               readonly pathHelper:PathHelperService,
-              readonly halResourceService:HalResourceService) {}
+              readonly halResourceService:HalResourceService,
+              readonly i18n:I18nService) {}
 
   public grid:GridResource;
 
