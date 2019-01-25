074445c2dd93461e8e091e7aa5af718431c4cfa8
Oliver Günther <mail@oliverguenther.de>
2019-01-25 11:14:51 +0100

    Enable lograge only for production
    [ci skip]

--- config/initializers/lograge.rb
+++ config/initializers/lograge.rb
@@ -1,5 +1,5 @@
 Rails.application.configure do
-  config.lograge.enabled = true
+  config.lograge.enabled = Rails.env.production? || ENV['OPENPROJECT_LOGRAGE_ENABLED']
   config.lograge.base_controller_class = %w[ActionController::Base]
 
   # Add custom data to event payload
