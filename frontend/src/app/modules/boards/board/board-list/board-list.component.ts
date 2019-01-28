31432bc599b65bce2f3c7f51110a46fc9aea832f
Oliver Günther <mail@oliverguenther.de>
2019-01-28 15:48:58 +0100

    Renaming queries
    [ci skip]

--- frontend/src/app/modules/boards/board/board-list/board-list.component.ts
+++ frontend/src/app/modules/boards/board/board-list/board-list.component.ts
@@ -1,4 +1,4 @@
-import {Component, ElementRef, Input, OnInit, ViewChild} from "@angular/core";
+import {Component, ElementRef, Input, OnDestroy, OnInit, Query, ViewChild} from "@angular/core";
 import {QueryDmService} from "core-app/modules/hal/dm-services/query-dm.service";
 import {
   LoadingIndicatorService,
@@ -6,21 +6,27 @@ import {
 } from "core-app/modules/common/loading-indicator/loading-indicator.service";
 import {QueryResource} from "core-app/modules/hal/resources/query-resource";
 import {WorkPackageTableConfigurationObject} from "core-components/wp-table/wp-table-configuration";
-import {Observable, of} from "rxjs";
+import {Observable, of, Subject} from "rxjs";
 import {HalResource} from "core-app/modules/hal/resources/hal-resource";
+import {auditTime, debounce, debounceTime, distinctUntilChanged, tap, withLatestFrom} from "rxjs/operators";
+import {untilComponentDestroyed} from "ng2-rx-componentdestroyed";
 
 @Component({
   selector: 'board-list',
   templateUrl: './board-list.component.html',
   styleUrls: ['./board-list.component.sass']
 })
-export class BoardListComponent implements OnInit {
+export class BoardListComponent implements OnInit, OnDestroy {
   @Input() queryInput:number|QueryResource;
 
   @ViewChild('loadingIndicator') indicator:ElementRef;
 
   /** The query resource being loaded */
   public query$:Observable<QueryResource>;
+
+
+  /** Rename events */
+  public rename$ = new Subject<string>();
 
   constructor(private readonly QueryDm:QueryDmService,
               private readonly loadingIndicator:LoadingIndicatorService) {
@@ -37,6 +43,21 @@ export class BoardListComponent implements OnInit {
       .pipe(
         withLoadingIndicator(this.indicatorInstance, 50)
       );
+
+    this.rename$
+      .pipe(
+        untilComponentDestroyed(this),
+        debounceTime(1000),
+        distinctUntilChanged(),
+        withLatestFrom(this.query$)
+      )
+      .subscribe(([newName, query]) => {
+        query.name = newName;
+        this.QueryDm.patch(query.id, { name: newName });
+      });
+  }
+
+  ngOnDestroy():void {
   }
 
   get columnsQueryProps() {
