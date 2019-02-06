7e347b8c5a62c76ba1408d585a004dfd833c8cad
Oliver Günther <mail@oliverguenther.de>
2019-02-06 12:27:12 +0100

    Delete through table

--- frontend/src/app/modules/boards/boards-module.component.ts
+++ frontend/src/app/modules/boards/boards-module.component.ts
@@ -5,6 +5,7 @@ import {I18nService} from "core-app/modules/common/i18n/i18n.service";
 import {BoardService} from "core-app/modules/boards/board/board.service";
 import {Board} from "core-app/modules/boards/board/board";
 import {BoardCacheService} from "core-app/modules/boards/board/board-cache.service";
+import {NotificationsService} from "core-app/modules/common/notifications/notifications.service";
 
 @Component({
   selector: 'boards-module',
@@ -17,6 +18,9 @@ export class BoardsModuleComponent {
     board: this.I18n.t('js.label_board'),
     boards: this.I18n.t('js.label_board_plural'),
     createdAt: this.I18n.t('js.label_created_on'),
+    delete: this.I18n.t('js.button_delete'),
+    areYouSure: this.I18n.t('js.text_are_you_sure'),
+    deleteSuccessful: this.I18n.t('js.notice_successful_delete')
   };
 
   public boards$:Observable<Board[]> = this.BoardCache.observeAll();
@@ -24,6 +28,7 @@ export class BoardsModuleComponent {
   constructor(private readonly Boards:BoardService,
               private readonly BoardCache:BoardCacheService,
               private readonly I18n:I18nService,
+              private readonly notifications:NotificationsService,
               private readonly state:StateService) {
     this.BoardCache.requireLoaded();
   }
@@ -34,5 +39,19 @@ export class BoardsModuleComponent {
       .then((board) => {
         this.state.go('boards.show', { id: board.id });
       });
+  }
+
+  destroyBoard(board:Board) {
+    if (!window.confirm(this.text.areYouSure)) {
+      return;
+    }
+
+    this.Boards
+      .delete(board)
+      .then(() => {
+        this.BoardCache.clearSome(board.id);
+        this.notifications.addSuccess(this.text.deleteSuccessful);
+      })
+      .catch((error) => this.notifications.addError("Deletion failed: " + error));
   }
 }
