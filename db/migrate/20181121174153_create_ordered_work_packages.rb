77f0b686e37434f9c5e67ad5bb8aa5055336a008
Oliver Günther <mail@oliverguenther.de>
2019-02-06 08:40:25 +0100

    Fix migration for MySQL references
    https://stackoverflow.com/a/45825566/420614

--- db/migrate/20181121174153_create_ordered_work_packages.rb
+++ db/migrate/20181121174153_create_ordered_work_packages.rb
@@ -2,8 +2,9 @@ class CreateOrderedWorkPackages < ActiveRecord::Migration[5.1]
   def change
     create_table :ordered_work_packages do |t|
       t.integer    :position, index: true, null: false
-      t.references :query, foreign_key: { index: true, on_delete: :cascade }
-      t.references :work_package, foreign_key: { index: true, on_delete: :cascade }
+      
+      t.references :query, type: :integer, foreign_key: { index: true, on_delete: :cascade }
+      t.references :work_package, type: :integer, foreign_key: { index: true, on_delete: :cascade }
     end
   end
 end
