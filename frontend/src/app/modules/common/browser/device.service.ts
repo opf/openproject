028c7c27dc163c855b077a279727b9e62834c8ce
Inga Mai <inga.mai@outlook.de>
2019-02-14 16:51:17 +0100

    Satisfy codeclimate; empty constructor deleted

--- frontend/src/app/modules/common/browser/device.service.ts
+++ frontend/src/app/modules/common/browser/device.service.ts
@@ -5,8 +5,6 @@ export class DeviceService {
 
   public mobileWidthTreshold:number = 680;
 
-  constructor() {}
-
   public get isMobile():boolean {
     return (window.innerWidth < this.mobileWidthTreshold);
   }
