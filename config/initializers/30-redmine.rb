09d2256313177ea431726650a16b84811b3ae29c
Jens Ulferts <j.ulferts@finn.de>
2019-03-07 09:02:25 +0100

    have dedicated locale_fallback initializer

--- config/initializers/30-redmine.rb
+++ config/initializers/30-redmine.rb
@@ -1,4 +1,5 @@
 #-- encoding: UTF-8
+
 #-- copyright
 # OpenProject is a project management system.
 # Copyright (C) 2012-2018 the OpenProject Foundation (OPF)
@@ -26,16 +27,5 @@
 #
 # See docs/COPYRIGHT.rdoc for more details.
 #++
-
-# Adds fallback to default locale for untranslated strings
-I18n::Backend::Simple.send(:include, I18n::Backend::Fallbacks)
-
-# As we enabled +config.i18n.fallbacks+, Rails will fall back
-# to the default locale.
-# When other locales are available, fall back to them.
-if Setting.table_exists? # don't want to prevent migrations
-  defaults = Set.new I18n.fallbacks.defaults + Setting.available_languages.map(&:to_sym)
-  I18n.fallbacks.defaults = defaults
-end
 
 require 'open_project'
