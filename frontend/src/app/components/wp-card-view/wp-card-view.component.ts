e8fccf2e9d4b3458915de0d0eba2b1184d20fcd2
Henriette Dinger <h.dinger@openproject.com>
2019-03-12 15:30:48 +0100

    Remove inline editing in cards

--- frontend/src/app/components/wp-card-view/wp-card-view.component.ts
+++ frontend/src/app/components/wp-card-view/wp-card-view.component.ts
@@ -46,6 +46,7 @@ import {WorkPackageChangeset} from "core-components/wp-edit-form/work-package-ch
 import {DragAndDropHelpers} from "core-app/modules/boards/drag-and-drop/drag-and-drop.helpers";
 import {WorkPackageNotificationService} from "core-components/wp-edit/wp-notification.service";
 import {WorkPackageEditFieldComponent} from "app/components/wp-edit/wp-edit-field/wp-edit-field.component";
+import {Highlighting} from "core-components/wp-fast-table/builders/highlighting/highlighting.functions";
 
 
 @Component({
@@ -89,8 +90,6 @@ export class WorkPackageCardViewComponent extends WorkPackageEmbeddedTableCompon
   };
 
   @ViewChild('container') public container:ElementRef;
-  @ViewChild('wpEditField') readonly wpEditField:WorkPackageEditFieldComponent;
-
   /** Whether the card view has an active inline created wp */
   public activeInlineCreateWp?:WorkPackageResource;
 
@@ -161,8 +160,16 @@ export class WorkPackageCardViewComponent extends WorkPackageEmbeddedTableCompon
     );
   }
 
-  public activeEditField() {
-    return this.wpEditField && this.wpEditField.active;
+  public wpTypeAttribute(wp:WorkPackageResource) {
+    return wp.type.name + ':';
+  }
+
+  public wpSubject(wp:WorkPackageResource) {
+    return wp.subject;
+  }
+
+  public typeDotClass(wp:WorkPackageResource) {
+    return Highlighting.dotClass('type',  wp.type.getId());
   }
 
   removeDragged() {
