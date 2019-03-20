09a9d39175c479f3b9e8ea7f7372dec85ac86c4f
Oliver Günther <mail@oliverguenther.de>
2019-03-20 15:07:25 +0100

    Manually copy table from boards to forums to avoid index errors
    [ci skip]

--- db/migrate/tables/forums.rb
+++ db/migrate/tables/forums.rb
@@ -29,20 +29,7 @@
 #++
 
 require_relative 'base'
+require_relative 'boards'
 
-class Tables::Boards < Tables::Base
-  def self.table(migration)
-    create_table migration do |t|
-      t.integer :project_id, null: false
-      t.string :name, default: '', null: false
-      t.string :description
-      t.integer :position, default: 1
-      t.integer :topics_count, default: 0, null: false
-      t.integer :messages_count, default: 0, null: false
-      t.integer :last_message_id
-
-      t.index :last_message_id, name: "index_#{table_name}_on_last_message_id"
-      t.index :project_id, name: "#{table_name}_project_id"
-    end
-  end
+class Tables::Forums < Tables::Boards
 end
