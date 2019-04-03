41df8deef7ba2f5b5ba03e69f1bb26d58feca743
Henriette Dinger <h.dinger@openproject.com>
2019-04-03 09:56:24 +0200

    Throw warning when leaving initially opened edit fields in meetings, too

--- modules/meeting/app/assets/javascripts/meeting/meeting.js
+++ modules/meeting/app/assets/javascripts/meeting/meeting.js
@@ -1,8 +1,27 @@
 jQuery(function($) {
   var formSubmitting = false;
+  var editFormOpen = false;
+  if (jQuery('#edit-meeting_agenda').is(':visible') || jQuery('#edit-meeting_minutes').is(':visible')) {
+    editFormOpen = true;
+  }
+
+  $(window).on("beforeunload", function (e) {
+    // When the form is not open or just submitted,
+    // we can safely leave the page
+    if(!editFormOpen || formSubmitting) {
+      return undefined;
+    } else {
+      // Otherwise we throw a warning
+      // For browser compatibility we need to set the event return value
+      e.returnValue = '';
+      return '';
+    }
+  });
+
   function toggleContentTypeForm(content_type, edit) {
     jQuery('.edit-' + content_type).toggle(edit);
     jQuery('.show-' + content_type).toggle(!edit);
+    editFormOpen = edit;
 
     jQuery('.button--edit-agenda').toggleClass('-active', edit);
     jQuery('.button--edit-agenda').attr('disabled', edit);
@@ -11,17 +30,6 @@ jQuery(function($) {
   $('.button--edit-agenda').click(function() {
     var content_type = $(this).data('contentType');
     toggleContentTypeForm(content_type, true);
-
-
-    $(window).on("beforeunload", function (e) {
-      if (formSubmitting) {
-        return undefined;
-      }
-
-      // For browser compatibility we need to set the event return value
-      e.returnValue = '';
-      return '';
-    });
 
     return false;
   });
