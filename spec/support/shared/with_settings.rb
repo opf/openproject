3a5c68857e107cc38a940f89b8cdb1ea7a8f6454
Markus Kahl <machisuji@gmail.com>
2019-04-26 14:02:17 +0100

    make users explicitly define localized setting values so it's clear which locale is mocked

--- spec/support/shared/with_settings.rb
+++ spec/support/shared/with_settings.rb
@@ -53,6 +53,12 @@ RSpec.configure do |config|
 
         raise "#{k} is not a valid setting" unless Setting.respond_to?(name)
 
+        expect(name).not_to start_with("localized_"), ->() do
+          base = name[10..-1]
+
+          "Don't use `#{name}` in `with_settings`. Do this: `with_settings: { #{base}: { \"en\" => \"#{v}\" } }`"
+        end
+
         allow(Setting).to receive(:[]).with(name).and_return v
         allow(Setting).to receive(:[]).with(name.to_sym).and_return v
       end
