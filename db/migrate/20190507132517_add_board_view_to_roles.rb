509d8002386b2fe1db0acfd9e47d674e15c199d1
Oliver Günther <mail@oliverguenther.de>
2019-05-07 15:29:49 +0200

    [30171][30170] Add board view by default
    - Add show permission to all roles
    - Add to default settings

---
+++ db/migrate/20190507132517_add_board_view_to_roles.rb
@@ -0,0 +1,20 @@
+class AddBoardViewToRoles < ActiveRecord::Migration[5.2]
+  def up
+    Role
+      .joins(:role_permissions)
+      .where("role_permissions.permission = 'view_work_packages'")
+      .references(:role_permissions)
+      .find_each do |role|
+
+      role.add_permission! :show_board_views
+    end
+
+    unless Setting.default_projects_modules.include?('board_view')
+      Setting.default_projects_modules = Setting.default_projects_mdoules + ['board_view']
+    end
+  end
+
+  def down
+    # Nothing to do
+  end
+end
