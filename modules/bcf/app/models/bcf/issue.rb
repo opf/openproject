c4d8f76022e2ac89154eb373a4eb95177f48d233
Wieland Lindenthal <w.lindenthal@forkmerge.com>
2019-05-14 10:33:17 +0200

    Cache markup doc in Bcf::Issue model and add a spec

--- modules/bcf/app/models/bcf/issue.rb
+++ modules/bcf/app/models/bcf/issue.rb
@@ -5,6 +5,10 @@ module Bcf
     belongs_to :work_package
     belongs_to :project
 
+    attr_reader(:markup_doc)
+
+    after_update :invalidate_markup_cache
+
     class << self
       def in_project(project)
         where(project_id: project.try(:id) || project)
@@ -12,19 +16,33 @@ module Bcf
 
       def with_markup
         select '*',
-               extract_xml('/Markup/Topic/Title/text()', 'title'),
-               extract_xml('/Markup/Topic/Description/text()', 'description'),
-               extract_xml('/Markup/Topic/Priority/text()', 'priority_text'),
-               extract_xml('/Markup/Topic/@TopicStatus', 'status_text'),
-               extract_xml('/Markup/Topic/AssignedTo/text()', 'assignee_text'),
-               extract_xml('/Markup/Topic/DueDate/text()', 'due_date_text')
+               extract_first_node('/Markup/Topic/Title/text()', 'title'),
+               extract_first_node('/Markup/Topic/Description/text()', 'description'),
+               extract_first_node('/Markup/Topic/Priority/text()', 'priority_text'),
+               extract_first_node('/Markup/Topic/@TopicStatus', 'status_text'),
+               extract_first_node('/Markup/Topic/AssignedTo/text()', 'assignee_text'),
+               extract_first_node('/Markup/Topic/DueDate/text()', 'due_date_text'),
+               extract_first_node('/Markup/Topic/Index/text()', 'index_text'),
+               extract_nodes('/Markup/Topic/Labels/text()', 'labels')
       end
 
       private
 
-      def extract_xml(path, as)
+      def extract_first_node(path, as)
         "(xpath('#{path}', markup))[1] AS #{as}"
       end
+
+      def extract_nodes(path, as)
+        "(xpath('#{path}', markup)) AS #{as}"
+      end
+    end
+
+    def markup_doc
+      @markup_doc ||= Nokogiri::XML markup
+    end
+
+    def invalidate_markup_cache
+      @markup_doc = nil
     end
 
     has_many :viewpoints, foreign_key: :issue_id, class_name: "Bcf::Viewpoint"
