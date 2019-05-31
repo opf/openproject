6c49117804540cba67f92d13298af197386f0118
ulferts <jens.ulferts@googlemail.com>
2019-05-31 14:48:00 +0200

    do not display author in create payload

--- app/contracts/work_packages/create_contract.rb
+++ app/contracts/work_packages/create_contract.rb
@@ -32,7 +32,10 @@ require 'work_packages/base_contract'
 
 module WorkPackages
   class CreateContract < BaseContract
-    attribute :author_id do
+    # TODO: Think about whether this can be removed
+    # as it is unwriteable. So why bother checking for the correct author
+    attribute :author_id,
+              writeable: false do
       errors.add :author_id, :invalid if model.author != user
     end
 
