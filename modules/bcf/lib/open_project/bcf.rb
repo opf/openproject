1d2f0afb6c6ae12ae3562d62fe686ab628a1f37b
Oliver Günther <mail@oliverguenther.de>
2019-06-04 10:19:55 +0200

    Fix autoloading

--- modules/bcf/lib/open_project/bcf.rb
+++ modules/bcf/lib/open_project/bcf.rb
@@ -2,6 +2,5 @@ module OpenProject
   module Bcf
     require "open_project/bcf/engine"
     require "open_project/bcf/bcf_xml"
-    require "open_project/bcf/bcf_issue_associated_filter"
   end
 end
