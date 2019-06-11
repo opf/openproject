481ed8cd0e3aea8c7c73ab023c7c79a1921257af
Oliver Günther <mail@oliverguenther.de>
2019-06-11 10:04:50 +0200

    More onpush with fixes to ng-select
    [ci skip]

--- frontend/src/app/components/wp-copy/wp-copy-full-view.component.ts
+++ frontend/src/app/components/wp-copy/wp-copy-full-view.component.ts
@@ -26,12 +26,13 @@
 // See doc/COPYRIGHT.rdoc for more details.
 // ++
 
-import {Component} from '@angular/core';
+import {ChangeDetectionStrategy, Component} from '@angular/core';
 import {WorkPackageCopyController} from 'core-components/wp-copy/wp-copy.controller';
 
 @Component({
   selector: 'wp-copy-full-view',
   host: { 'class': 'work-packages-page--ui-view' },
+  changeDetection: ChangeDetectionStrategy.OnPush,
   templateUrl: '../wp-new/wp-new-full-view.html'
 })
 export class WorkPackageCopyFullViewComponent extends WorkPackageCopyController {
