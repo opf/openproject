10436bb2c8d67dda817d5af45d50dcb12bb9baf6
Wieland Lindenthal <w.lindenthal@forkmerge.com>
2019-06-11 16:43:29 +0200

    Bind comments to an issue
    and prefent updating a comment with same UUID in other project.

--- modules/bcf/app/models/bcf/comment.rb
+++ modules/bcf/app/models/bcf/comment.rb
@@ -6,9 +6,10 @@ module Bcf
     belongs_to :issue, foreign_key: :issue_id, class_name: "Bcf::Issue"
 
     validates_presence_of :uuid
+    validates_uniqueness_of :uuid, scope: [:issue_id]
 
-    def self.has_uuid?(uuid)
-      where(uuid: uuid).exists?
+    def self.has_uuid?(uuid, issue_id)
+      where(uuid: uuid, issue_id: issue_id).exists?
     end
   end
 end
