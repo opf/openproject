0311ec1934c184ee0fa25d3cb1ce53a4f5e09b65
Oliver Günther <mail@oliverguenther.de>
2019-07-03 14:51:38 +0200

    Disable BasicAuthFailure when basic auth config not enabled

--- lib/open_project/authentication/strategies/warden/basic_auth_failure.rb
+++ lib/open_project/authentication/strategies/warden/basic_auth_failure.rb
@@ -6,6 +6,11 @@ module OpenProject
         # This strategy is inserted after optional basic auth strategies to
         # indicate that invalid basic auth credentials were provided.
         class BasicAuthFailure < ::Warden::Strategies::BasicAuth
+
+          def valid?
+            OpenProject::Configuration.apiv3_enable_basic_auth? && super
+          end
+
           def authenticate_user(_username, _password)
             nil # always fails
           end
