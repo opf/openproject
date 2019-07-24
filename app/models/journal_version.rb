1dfa9c1cf249e531179889e77f0d41c635c44ba7
Oliver Günther <mail@oliverguenther.de>
2019-07-24 14:07:44 +0200

    [30594] Implement separate version table for journals
    This ensures inserts to the journal versions are atomtic, so we can
    avoid locking the journals table.
    
    https://community.openproject.com/wp/30594

--- app/models/journal_version.rb
+++ app/models/journal_version.rb
@@ -1,5 +1,4 @@
 #-- encoding: UTF-8
-
 #-- copyright
 # OpenProject is a project management system.
 # Copyright (C) 2012-2018 the OpenProject Foundation (OPF)
@@ -28,25 +27,5 @@
 # See docs/COPYRIGHT.rdoc for more details.
 #++
 
-require_relative 'base'
-
-class Tables::Journals < Tables::Base
-  def self.table(migration)
-    create_table migration do |t|
-      t.references :journable, polymorphic: true, index: false, type: :int
-      t.integer :user_id, default: 0, null: false
-      t.text :notes
-      t.datetime :created_at, null: false
-      t.integer :version, default: 0, null: false
-      t.string :activity_type
-
-      t.index :journable_id
-      t.index :created_at
-      t.index :journable_type
-      t.index :user_id
-      t.index :activity_type
-      t.index %i[journable_type journable_id version],
-              unique: true
-    end
-  end
+class JournalVersion < ActiveRecord::Base
 end
