0ca9f4d742c4be6f95d93a0e46ae7100f6facbeb
Oliver Günther <mail@oliverguenther.de>
2019-10-30 15:00:10 +0100

    Fix output destination paths of font
    [ci skip]

--- vendor/openproject-icon-font/generate.js
+++ vendor/openproject-icon-font/generate.js
@@ -9,6 +9,7 @@ webfontsGenerator({
   files: glob.sync("src/*.svg"),
   "fontName": "openproject-icon-font",
   "cssFontsUrl": "../../app/assets/openproject_icon/",
+  "dest": path.resolve(__dirname, '..', '..', 'app', 'assets', 'fonts', 'openproject_icon'),
   "cssDest": path.join(path.resolve(__dirname, '..', '..', 'app', 'assets', 'stylesheets', 'fonts'), '_openproject_icon_definitions.scss'),
   "cssTemplate": "openproject-icon-font.template.scss",
   "classPrefix": "icon-",
@@ -18,8 +19,7 @@ webfontsGenerator({
   "htmlTemplate": "openproject-icon-font.template.lsg",
   "types": ['woff2', 'woff'],
   "fixedWidth": true,
-  "descent": 100,
-  dest: ''
+  "descent": 100
 }, function(error) {
   if (error) {
     console.log('Failed to build icon font. ', error);
