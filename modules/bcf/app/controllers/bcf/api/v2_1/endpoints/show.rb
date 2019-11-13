0d086a5e5d4ac45950014447b743c7111d923564
ulferts <jens.ulferts@googlemail.com>
2019-11-13 09:34:20 +0100

    employ endpoint for projects#show

--- modules/bcf/app/controllers/bcf/api/v2_1/endpoints/show.rb
+++ modules/bcf/app/controllers/bcf/api/v2_1/endpoints/show.rb
@@ -28,34 +28,17 @@
 # See docs/COPYRIGHT.rdoc for more details.
 #++
 
-module Bcf::API::V2_1
-  class ProjectsAPI < ::API::OpenProjectAPI
-    resources :projects do
-      helpers do
-        def visible_projects
-          Project
-            .visible(current_user)
-            .has_module(:bcf)
-        end
-      end
+module Bcf::API::V2_1::Endpoints
+  class Show < API::Utilities::Endpoints::Show
+    def render(instance)
+      render_representer
+        .new(instance)
+    end
 
-      get do
-        visible_projects
-          .map do |project|
-          Bcf::API::V2_1::Projects::SingleRepresenter
-            .new(project)
-        end
-      end
+    private
 
-      route_param :id, regexp: /\A(\d+)\z/ do
-        after_validation do
-          @project = visible_projects
-                     .find(params[:id])
-        end
-
-        get &::Bcf::API::V2_1::Endpoints::Show.new(model: Project).mount
-        put &::Bcf::API::V2_1::Endpoints::Update.new(model: Project).mount
-      end
+    def deduce_render_representer
+      "::Bcf::API::V2_1::#{deduce_api_namespace}::SingleRepresenter".constantize
     end
   end
 end
