6e8887c6a4a84f887c395632c84601ac13532411
ulferts <jens.ulferts@googlemail.com>
2019-12-16 14:37:26 +0100

    replace meeting cuke by feature spec

--- modules/meeting/spec/support/pages/meetings/index.rb
+++ modules/meeting/spec/support/pages/meetings/index.rb
@@ -51,6 +51,54 @@ module Pages::Meetings
       end
     end
 
+    def expect_no_meetings_listed
+      within '#content-wrapper' do
+        expect(page)
+          .to have_content I18n.t(:no_results_title_text)
+      end
+    end
+
+    def expect_meetings_listed(*meetings)
+      within '#content-wrapper' do
+        meetings.each do |meeting|
+          expect(page).to have_selector(".meeting",
+                                        text: meeting.title)
+        end
+      end
+    end
+
+    def expect_meetings_not_listed(*meetings)
+      within '#content-wrapper' do
+        meetings.each do |meeting|
+          expect(page).to have_no_selector(".meeting",
+                                           text: meeting.title)
+        end
+      end
+    end
+
+    def expect_to_be_on_page(number)
+      expect(page)
+        .to have_selector('.pagination--item.-current',
+                          text: number)
+    end
+
+    def to_page(number)
+      within '.pagination--pages' do
+        click_link number.to_s
+      end
+    end
+
+    def to_today
+      click_link 'today'
+    end
+
+    def navigate_by_menu
+      visit project_path(project)
+      within '#main-menu' do
+        click_link 'Meetings'
+      end
+    end
+
     def path
       meetings_path(project)
     end
