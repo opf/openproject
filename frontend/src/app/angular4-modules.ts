e0cb9a86eca6de985cef40f6ba5095cc5b621331
ulferts <jens.ulferts@googlemail.com>
2019-12-20 15:48:20 +0100

    bump fullcalendar

--- frontend/src/app/angular4-modules.ts
+++ frontend/src/app/angular4-modules.ts
@@ -76,7 +76,6 @@ import {OpenprojectRouterModule} from "core-app/modules/router/openproject-route
 import {OpenprojectWorkPackageRoutesModule} from "core-app/modules/work_packages/openproject-work-package-routes.module";
 import {BrowserModule} from "@angular/platform-browser";
 import {OpenprojectCalendarModule} from "core-app/modules/calendar/openproject-calendar.module";
-import {FullCalendarModule} from "ng-fullcalendar";
 import {OpenprojectBoardsModule} from "core-app/modules/boards/openproject-boards.module";
 import {OpenprojectGlobalSearchModule} from "core-app/modules/global_search/openproject-global-search.module";
 import {DeviceService} from "core-app/modules/common/browser/device.service";
@@ -127,7 +126,6 @@ import {OpenprojectProjectsModule} from "core-app/modules/projects/openproject-p
 
     // Calendar module
     OpenprojectCalendarModule,
-    FullCalendarModule,
 
     // Dashboards
     OpenprojectDashboardsModule,
