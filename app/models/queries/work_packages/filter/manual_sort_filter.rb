bd7f4e4814933c6d58c9c1d0a24990575d7ecee0
Henriette Dinger <h.dinger@openproject.com>
2020-01-15 11:31:26 +0100

    Update copyright notice

--- app/models/queries/work_packages/filter/manual_sort_filter.rb
+++ app/models/queries/work_packages/filter/manual_sort_filter.rb
@@ -1,8 +1,8 @@
 #-- encoding: UTF-8
 
 #-- copyright
-# OpenProject is a project management system.
-# Copyright (C) 2012-2017 the OpenProject Foundation (OPF)
+# OpenProject is an open source project management software.
+# Copyright (C) 2012-2020 the OpenProject GmbH
 #
 # This program is free software; you can redistribute it and/or
 # modify it under the terms of the GNU General Public License version 3.
@@ -13,7 +13,7 @@
 #
 # This program is free software; you can redistribute it and/or
 # modify it under the terms of the GNU General Public License
-# as published by the Foperatorree Software Foundation; either version 2
+# as published by the Free Software Foundation; either version 2
 # of the License, or (at your option) any later version.
 #
 # This program is distributed in the hope that it will be useful,
@@ -25,7 +25,7 @@
 # along with this program; if not, write to the Free Software
 # Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
 #
-# See doc/COPYRIGHT.rdoc for more details.
+# See docs/COPYRIGHT.rdoc for more details.
 #++
 
 class Queries::WorkPackages::Filter::ManualSortFilter <
