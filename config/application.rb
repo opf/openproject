e695ba4d3bf1a8bef8f22f402a0d57ab82a89993
ulferts <jens.ulferts@googlemail.com>
2020-02-10 22:01:42 +0100

    enable zeitwerk

--- config/application.rb
+++ config/application.rb
@@ -122,6 +122,7 @@ module OpenProject
     # http://stackoverflow.com/questions/4590229
     config.middleware.use Rack::TempfileReaper
 
+    config.autoloader = :zeitwerk
     # Custom directories with classes and modules you want to be autoloadable.
     config.enable_dependency_loading = true
     config.paths.add Rails.root.join('lib').to_s, eager_load: true
