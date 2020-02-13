15b81403c7f04a3d41b48d1ea34653108da0c8eb
ulferts <jens.ulferts@googlemail.com>
2020-02-13 21:54:22 +0100

    avoid sorting and order on counting

--- app/models/queries/projects/orders/name_order.rb
+++ app/models/queries/projects/orders/name_order.rb
@@ -35,6 +35,10 @@ class Queries::Projects::Orders::NameOrder < Queries::BaseOrder
     :name
   end
 
+  def scope
+    super.select('projects.*', 'lower(projects.name)')
+  end
+
   def order
     with_raise_on_invalid do
       model.order(Arel.sql("lower(projects.name)").send(direction))
--- lib/api/decorators/unpaginated_collection.rb
+++ lib/api/decorators/unpaginated_collection.rb
@@ -31,7 +31,18 @@ module API
   module Decorators
     class UnpaginatedCollection < ::API::Decorators::Collection
       def initialize(models, self_link, current_user:)
-        super(models, models.count, self_link, current_user: current_user)
+        super(models, model_count(models), self_link, current_user: current_user)
+      end
+
+      def model_count(models)
+        if models.respond_to?(:except)
+          # We do not want any order/selecting with counting
+          # when it would result in an invalid SELECT COUNT(DISTINCT *, ).
+          # As both, order and select should have no impact on the count result, we remove them.
+          models.except(:select, :order)
+        else
+          models
+        end.count
       end
     end
   end
