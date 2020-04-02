50a6f50b31ae30a7c6cb6307bdb0f91cd1447c4f
b.satarnejad <b.satarnejad@gmail.com>
2020-04-02 23:32:32 +0200

    Improve news component

--- frontend/src/app/modules/grids/widgets/news/news.component.ts
+++ frontend/src/app/modules/grids/widgets/news/news.component.ts
@@ -1,12 +1,9 @@
 import {AbstractWidgetComponent} from "core-app/modules/grids/widgets/abstract-widget.component";
-import {Component, OnInit, ChangeDetectorRef, Injector, ChangeDetectionStrategy} from '@angular/core';
+import {ChangeDetectionStrategy, Component, Injector, OnInit, ChangeDetectorRef} from '@angular/core';
 import {I18nService} from "core-app/modules/common/i18n/i18n.service";
-import {HalResourceService} from "core-app/modules/hal/services/hal-resource.service";
 import {PathHelperService} from "core-app/modules/common/path-helper/path-helper.service";
 import {TimezoneService} from "core-components/datetime/timezone.service";
 import {NewsResource} from "core-app/modules/hal/resources/news-resource";
-import {UserCacheService} from "core-components/user/user-cache.service";
-import {UserResource} from "core-app/modules/hal/resources/user-resource";
 import {NewsDmService} from "core-app/modules/hal/dm-services/news-dm.service";
 import {CurrentProjectService} from "core-components/projects/current-project.service";
 import {DmListParameter} from "core-app/modules/hal/dm-services/dm.service.interface";
@@ -25,30 +22,30 @@ export class WidgetNewsComponent extends AbstractWidgetComponent implements OnIn
   public entries:NewsResource[] = [];
   private entriesLoaded = false;
 
-  constructor(readonly halResource:HalResourceService,
+  constructor(
+
               readonly pathHelper:PathHelperService,
               readonly i18n:I18nService,
               protected readonly injector:Injector,
               readonly timezone:TimezoneService,
-              readonly userCache:UserCacheService,
               readonly newsDm:NewsDmService,
-              readonly currentProject:CurrentProjectService,
-              readonly cdr:ChangeDetectorRef) {
+             readonly currentProject:CurrentProjectService,
+              readonly cdr:ChangeDetectorRef
+              ) {
     super(i18n, injector);
   }
 
   ngOnInit() {
     this.newsDm
       .list(this.newsDmParams)
-      .then((collection) => {
-        this.entries = collection.elements as NewsResource[];
-        this.entriesLoaded = true;
+      .then(collection => this.setupNews(collection.elements));
+  }
 
-        const users_loaded = this.setAuthors();
 
-        Promise.all(users_loaded)
-          .then(() => this.cdr.detectChanges());
-      });
+  public setupNews(news:any[]) {
+    this.entries = news;
+    this.entriesLoaded = true;
+
   }
 
   public get isEditable() {
@@ -56,23 +53,23 @@ export class WidgetNewsComponent extends AbstractWidgetComponent implements OnIn
   }
 
   public newsPath(news:NewsResource) {
-    return this.pathHelper.newsPath(news.id!);
+   return this.pathHelper.newsPath(news.id!);
   }
 
   public newsProjectPath(news:NewsResource) {
-    return this.pathHelper.projectPath(news.project.idFromLink);
+    return this.pathHelper.projectPath(news.project?.idFromLink);
   }
 
   public newsProjectName(news:NewsResource) {
-    return news.project.name;
+    return news.project?.name;
   }
 
   public newsAuthorName(news:NewsResource) {
-    return news.author.name;
+    return news.author?.name;
   }
 
   public newsAuthorPath(news:NewsResource) {
-    return this.pathHelper.userPath(news.author.id);
+    return this.pathHelper.userPath(news.author?.id);
   }
 
   public newsCreated(news:NewsResource) {
@@ -81,20 +78,6 @@ export class WidgetNewsComponent extends AbstractWidgetComponent implements OnIn
 
   public get noEntries() {
     return !this.entries.length && this.entriesLoaded;
-  }
-
-  public setAuthors() {
-    return this.entries.map((entry) => {
-      if (!entry.author) {
-        return Promise.resolve();
-      }
-
-      return this.userCache
-        .require(entry.author.idFromLink)
-        .then((user:UserResource) => {
-          entry.author = user;
-        });
-    });
   }
 
   private get newsDmParams() {
