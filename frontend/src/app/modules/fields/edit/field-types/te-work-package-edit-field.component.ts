opf/openproject

commit 4c575217bb2ce7184334bc51553c7701deb5da0c

[33392] Allow to cache requests in the course of the changeset
We need to reload requests whenever the changeset changes e.g., the
project.

By providing a cache within the temporary changeset, we can easily cache
requests until the changeset completes, or the form is reloaded.

https://community.openproject.com/wp/33392

frontend/src/app/modules/fields/edit/field-types/te-work-package-edit-field.component.ts
@@ -93,10 +93,10 @@ export class TimeEntryWorkPackageEditFieldComponent extends WorkPackageEditField
             .map((timeEntry) => timeEntry.workPackage.idFromLink)
             .filter((v, i, a) => a.indexOf(v) === i);
 
-          return super.allowedValuesFetch(query);
+          return super.loadAllowedValues(query);
         });
     } else {
-      return super.allowedValuesFetch(query);
+      return super.loadAllowedValues(query);
     }
   }
 

frontend/src/app/modules/fields/changeset/resource-changeset.ts
@@ -25,6 +25,9 @@ export class ResourceChangeset<T extends HalResource|{ [key:string]:unknown; } =
   /** Reference and load promise for the current form */
   protected form$ = input<FormResource>();
 
+  /** Request cache for objects within the changeset for the current form */
+  protected cache:{ [key:string]:Promise<unknown> } = {};
+
   /** Flag whether this is currently being saved */
   public inFlight = false;
 
@@ -94,6 +97,10 @@ export class ResourceChangeset<T extends HalResource|{ [key:string]:unknown; } =
       .toPromise();
   }
 
+  /**
+   * Cache some promised value in the course of this changeset.
+   * Will get cleared automatically by the changeset on destroy/submission
+   */
 
   /**
    * Posts to the form with the current changes
@@ -106,6 +113,7 @@ export class ResourceChangeset<T extends HalResource|{ [key:string]:unknown; } =
       .$links
       .update(payload)
       .then((form:FormResource) => {
+        this.cache = {};
         this.form$.putValue(form);
         this.setNewDefaults(form);
         this.push();
@@ -223,6 +231,7 @@ export class ResourceChangeset<T extends HalResource|{ [key:string]:unknown; } =
   public clear() {
     this.state && this.state.clear();
     this.changeset.clear();
+    this.cache = {};
     this.form$.clear();
   }
 
@@ -250,6 +259,18 @@ export class ResourceChangeset<T extends HalResource|{ [key:string]:unknown; } =
    */
   public get schema():SchemaResource {
     return this.form$.getValueOr(this.pristineResource).schema;
+  }
+
+  /**
+   * Access some promised value
+   * that should be cached for the lifetime duration of the form.
+   */
+  public cacheValue<T>(key:string, request:() => Promise<T>):Promise<T> {
+    if (this.cache[key]) {
+      return this.cache[key] as Promise<T>;
+    }
+    
+    return this.cache[key] = request();
   }
 
   protected get minimalPayload() {

frontend/src/app/modules/fields/edit/field-types/select-edit-field.component.ts
@@ -153,7 +153,7 @@ export class SelectEditFieldComponent extends EditFieldComponent implements OnIn
 
   protected loadValuesFromBackend(query?:string) {
     return from(
-      this.allowedValuesFetch(query)
+      this.loadAllowedValues(query)
     ).pipe(
       tap(collection => {
         // if it is an unpaginated collection or if we get all possible entries when fetching with a blank
@@ -174,7 +174,17 @@ export class SelectEditFieldComponent extends EditFieldComponent implements OnIn
     );
   }
 
-  protected allowedValuesFetch(query?:string) {
+  protected loadAllowedValues(query?:string):Promise<CollectionResource> {
+    // Cache the search without any params
+    if (!query) {
+      const cacheKey = this.schema.allowedValues.$link.href;
+      return this.change.cacheValue(cacheKey, this.fetchAllowedValueQuery.bind(this));
+    }
+
+    return this.fetchAllowedValueQuery(query);
+  }
+
+  protected fetchAllowedValueQuery(query?:string) {
     return this.schema.allowedValues.$link.$fetch(this.allowedValuesFilter(query)) as Promise<CollectionResource>;
   }
 

commit f888ec1a025278c74fc5ab2f97155f7b1cee401d

fix recent wp autocompleter broken in 4c575217bb

frontend/src/app/modules/fields/edit/field-types/te-work-package-edit-field.component.ts
@@ -82,7 +82,7 @@ export class TimeEntryWorkPackageEditFieldComponent extends WorkPackageEditField
   // associated with the time entries so that we have the most recent work packages the user logged time on.
   // As a worst case, the user logged RECENT_TIME_ENTRIES_MAGIC_NUMBER times on one work package so we can not guarantee to actually have
   // a fixed number returned.
-  protected allowedValuesFetch(query?:string) {
+  protected loadAllowedValues(query?:string) {
     if (!this.recentWorkPackageIds) {
       return this
         .timeEntryDm
@@ -93,10 +93,10 @@ export class TimeEntryWorkPackageEditFieldComponent extends WorkPackageEditField
             .map((timeEntry) => timeEntry.workPackage.idFromLink)
             .filter((v, i, a) => a.indexOf(v) === i);
 
-          return super.loadAllowedValues(query);
+          return this.fetchAllowedValueQuery(query);
         });
     } else {
-      return super.loadAllowedValues(query);
+      return this.fetchAllowedValueQuery(query);
     }
   }
 