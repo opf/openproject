b07aacdf42226b695d3896f5dc16655447be51b4
Oliver Günther <mail@oliverguenther.de>
2020-06-02 08:39:31 +0200

    Check for empty ENV when setting admin user
    [ci skip]

--- app/seeders/admin_user_seeder.rb
+++ app/seeders/admin_user_seeder.rb
@@ -52,7 +52,7 @@ class AdminUserSeeder < Seeder
       user.password = 'admin'
       user.firstname = 'OpenProject'
       user.lastname = 'Admin'
-      user.mail = ENV.fetch('ADMIN_EMAIL') { 'admin@example.net' }
+      user.mail = ENV['ADMIN_EMAIL'].presence || 'admin@example.net'
       user.mail_notification = User::USER_MAIL_OPTION_ONLY_MY_EVENTS.first
       user.language = I18n.locale.to_s
       user.status = User::STATUSES[:active]
