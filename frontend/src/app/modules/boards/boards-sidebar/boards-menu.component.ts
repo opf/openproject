25e06f1dec45e7b65ba90cd6f879507a852aaea7
Henriette Darge <h.darge@openproject.com>
2020-06-10 09:48:26 +0200

    Highlight currently active board in the sidebar menu
    [ci skip]

--- frontend/src/app/modules/boards/boards-sidebar/boards-menu.component.ts
+++ frontend/src/app/modules/boards/boards-sidebar/boards-menu.component.ts
@@ -7,6 +7,7 @@ import {AngularTrackingHelpers} from "core-components/angular/tracking-functions
 import {MainMenuNavigationService} from "core-components/main-menu/main-menu-navigation.service";
 import {map} from "rxjs/operators";
 import {CurrentProjectService} from "core-components/projects/current-project.service";
+import {UntilDestroyedMixin} from "core-app/helpers/angular/until-destroyed.mixin";
 
 export const boardsMenuSelector = 'boards-menu';
 
@@ -15,10 +16,12 @@ export const boardsMenuSelector = 'boards-menu';
   templateUrl: './boards-menu.component.html'
 })
 
-export class BoardsMenuComponent implements OnInit {
+export class BoardsMenuComponent extends UntilDestroyedMixin implements OnInit {
   trackById = AngularTrackingHelpers.compareByAttribute('id');
 
   currentProjectIdentifier = this.currentProject.identifier;
+
+  selectedBoardId:string;
 
   public boards$:Observable<Board[]> = this.boardCache.observeAll().pipe(
     map((boards:Board[]) => {
@@ -38,16 +41,27 @@ export class BoardsMenuComponent implements OnInit {
               private readonly boardCache:BoardCacheService,
               private readonly currentProject:CurrentProjectService,
               private readonly mainMenuService:MainMenuNavigationService) {
+    super();
   }
 
   ngOnInit() {
-    // When activating the work packages submenu,
+    // When activating the boards submenu,
     // either initially or through click on the toggle, load the results
     this.mainMenuService
       .onActivate('board_view')
       .subscribe(() => {
         this.focusBackArrow();
         this.boardService.loadAllBoards();
+      });
+
+    this
+      .boardService
+      .currentBoard$
+      .pipe(
+        this.untilDestroyed()
+      )
+      .subscribe((id:string|null) => {
+        this.selectedBoardId = id ? id : '';
       });
   }
 
