e87cadd1a6ecd49fb32e1a5ef9853b8f47cd62f9
Henriette Darge <h.darge@openproject.com>
2020-06-12 15:21:30 +0200

    Pass the baseRoute as a parameter along. This is needed to navigate back to the correct view after deleting a WP coming from the BIM page

--- frontend/src/app/modules/common/back-routing/back-routing.service.ts
+++ frontend/src/app/modules/common/back-routing/back-routing.service.ts
@@ -35,6 +35,7 @@ interface BackRouteOptions {
   name:string;
   params:{};
   parent:string;
+  baseRoute:string;
 }
 
 @Injectable({ providedIn: 'root' })
@@ -50,7 +51,7 @@ export class BackRoutingService {
   public goBack(preferListOverSplit:boolean = false) {
     // Default: back to list
     // When coming from a deep link or a create form
-    const baseRoute = this.$state.current.data.baseRoute || 'work-packages.partitioned.list';
+    const baseRoute = this.backRoute?.baseRoute || this.$state.current.data.baseRoute || 'work-packages.partitioned.list';
 
     if (!this.backRoute || this.backRoute.name.includes('new')) {
       this.$state.go(baseRoute, this.$state.params);
@@ -59,7 +60,7 @@ export class BackRoutingService {
         if (preferListOverSplit) {
           this.$state.go(baseRoute, this.$state.params);
         } else {
-          this.$state.go(this.keepTab.currentDetailsState, this.$state.params);
+          this.$state.go(baseRoute + this.keepTab.currentDetailsSubState, this.$state.params);
         }
       } else {
         this.$state.go(this.backRoute.name, this.backRoute.params);
@@ -82,7 +83,10 @@ export class BackRoutingService {
       toState.data &&
       fromState.data.parent !== toState.data.parent) {
       const paramsFromCopy = { ...transition.params('from') };
-      this.backRoute = { name: fromState.name, params: paramsFromCopy, parent: fromState.data.parent };
+      this.backRoute = { name: fromState.name,
+                         params: paramsFromCopy,
+                         parent: fromState.data.parent,
+                         baseRoute: fromState.data.baseRoute };
     }
   }
 
