5c6ed4ee883ca8d7aa75cbe989f52d1ca382db12
Oliver Günther <mail@oliverguenther.de>
2020-06-15 09:28:34 +0200

    Remove superfluous user updating

--- modules/auth_saml/lib/open_project/auth_saml/engine.rb
+++ modules/auth_saml/lib/open_project/auth_saml/engine.rb
@@ -57,20 +57,6 @@ module OpenProject
         auth_provider-saml.png
       )
 
-      config.after_initialize do
-        # Automatically update the openproject user whenever their info change in the upstream identity provider
-        OpenProject::OmniAuth::Authorization.after_login do |user, auth_hash, context|
-          # see https://github.com/opf/openproject/blob/caa07c5dd470f82e1a76d2bd72d3d55b9d2b0b83/app/controllers/concerns/omniauth_login.rb#L148
-          attributes = context.send(:omniauth_hash_to_user_attributes, auth_hash) || {}
-          attributes = attributes.with_indifferent_access
-
-          # Don't allow unsetting admin if user is already admin
-          attributes.delete(:admin) if user.admin?
-
-          user.update attributes
-        end
-      end
-
       register_auth_providers do
         strategy :saml do
           OpenProject::AuthSaml.configuration.values.map do |h|
