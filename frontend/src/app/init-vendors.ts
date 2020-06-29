60672eea1e6969c8eee0f8e9e89c9421cd97e220
Oliver Günther <mail@oliverguenther.de>
2020-06-29 08:17:55 +0200

    Remove unnecessary jquery-ui dependencies

--- frontend/src/app/init-vendors.ts
+++ frontend/src/app/init-vendors.ts
@@ -54,17 +54,8 @@ require('@uirouter/angular');
 // Jquery UI
 require('jquery-ui/ui/core.js');
 require('jquery-ui/ui/position.js');
-require('jquery-ui/ui/widgets/datepicker.js');
-require('jquery-ui/ui/widgets/dialog.js');
 require('jquery-ui/ui/widgets/autocomplete.js');
-require('jquery-ui/ui/widgets/sortable.js');
 require('jquery-ui/ui/widgets/tooltip.js');
-require('./misc/datepicker-defaults');
-
-require('jquery-ui/ui/i18n/datepicker-en-GB.js');
-require('jquery-ui/ui/i18n/datepicker-de.js');
-
-require('jquery-ui/ui/widgets/dialog.js');
 
 require('expose-loader?moment!moment');
 require('moment/locale/en-gb.js');
