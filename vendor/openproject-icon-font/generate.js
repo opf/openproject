0c53a8219ba3265b17ca3e252cb17928f957a0fc
Oliver Günther <mail@oliverguenther.de>
2020-07-10 11:29:48 +0200

    Add view-split-viewer-table icon
    [ci skip]

--- vendor/openproject-icon-font/generate.js
+++ vendor/openproject-icon-font/generate.js
@@ -8,14 +8,14 @@ const glob = require("glob")
 webfontsGenerator({
   files: glob.sync("src/*.svg"),
   "fontName": "openproject-icon-font",
-  "cssFontsUrl": "../../app/assets/openproject_icon/",
-  "dest": path.resolve(__dirname, '..', '..', 'app', 'assets', 'fonts', 'openproject_icon'),
-  "cssDest": path.join(path.resolve(__dirname, '..', '..', 'app', 'assets', 'stylesheets', 'fonts'), '_openproject_icon_definitions.scss'),
+  "cssFontsUrl": "../../frontend/src/assets/fonts/openproject_icon/",
+  "dest": path.resolve(__dirname, '..', '..', 'frontend', 'src', 'assets', 'fonts', 'openproject_icon'),
+  "cssDest": path.join(path.resolve(__dirname, '..', '..', 'frontend', 'src', 'global_styles', 'fonts'), '_openproject_icon_definitions.scss'),
   "cssTemplate": "openproject-icon-font.template.scss",
   "classPrefix": "icon-",
   "baseSelector": ".icon",
   "html": true,
-  "htmlDest": path.join(path.resolve(__dirname, '..', '..', 'app', 'assets', 'stylesheets', 'fonts'), '_openproject_icon_font.lsg'),
+  "htmlDest": path.join(path.resolve(__dirname, '..', '..', 'frontend', 'src', 'global_styles', 'fonts'), '_openproject_icon_font.lsg'),
   "htmlTemplate": "openproject-icon-font.template.lsg",
   "types": ['woff2', 'woff'],
   "fixedWidth": true,
@@ -24,6 +24,4 @@ webfontsGenerator({
   if (error) {
     console.log('Failed to build icon font. ', error);
   }
-
-  console.log('Failed to build icon font. ', error);
 });
