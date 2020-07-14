cba0f602a0341ba2f0acb3b51a788283a9fc16dc
Aleix Suau <info@macrofonoestudio.es>
2020-07-14 17:53:38 +0200

    'shows Cards view by default' fixed + Removed deprecated test files

--- config/middleware.rb
+++
@@ -1,5 +0,0 @@
-require 'request_headers'
-
-if %w(test).include?(Rails.env)
-  Rails.application.config.middleware.insert_before Rack::Lock, "RequestHeaders", CustomHeadersHelper
-end
--- frontend/src/app/modules/bim/ifc_models/openproject-ifc-models.routes.ts
+++ frontend/src/app/modules/bim/ifc_models/openproject-ifc-models.routes.ts
@@ -65,6 +65,9 @@ export const IFC_ROUTES:Ng2StateDeclaration[] = [
   {
     name: 'bim.partitioned.list',
     url: '/list?{cards:bool}',
+    params: {
+      cards: true
+    },
     data: {
       baseRoute: 'bim.partitioned.list',
       newRoute: 'bim.partitioned.list.new',
@@ -78,6 +81,9 @@ export const IFC_ROUTES:Ng2StateDeclaration[] = [
   {
     name: 'bim.partitioned.split',
     url: '/split?{cards:bool}',
+    params: {
+      cards: true
+    },
     data: {
       baseRoute: 'bim.partitioned.split',
       partition: '-split',
