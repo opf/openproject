6b6adb5a0f53341acf32bd72488a04b406a12c56
bsatarnejad <b.satarnejad@gmail.com>
2020-07-27 14:24:19 +0200

    change the test of selecting a board

--- modules/boards/spec/features/support/board_index_page.rb
+++ modules/boards/spec/features/support/board_index_page.rb
@@ -60,10 +60,9 @@ module Pages
       page.find('.toolbar-item a', text: 'Board').click
 
       if action == nil
-        find('.button', text: 'Basic board').click
+        find('.menu-block', text: 'Basic board').click
       else
-        select action.to_s, from: 'new_board_action_select'
-        find('.button', text: 'Action board').click
+        find('.menu-block', text: action.to_s).click
       end
 
       if expect_empty
