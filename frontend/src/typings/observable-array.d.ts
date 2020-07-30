4522b33a053a4e8950c6447c794464d2bd56569e
Oliver Günther <mail@oliverguenther.de>
2020-07-30 10:47:39 +0200

    Fix sass warnings due to sass upgrade

---
+++ frontend/src/typings/observable-array.d.ts
@@ -0,0 +1,58 @@
+// Declare some globals
+// to work around previously magical global constants
+// provided by typings@global
+
+// Active issue
+// https://github.com/Microsoft/TypeScript/issues/10178
+
+/// <reference path="../../node_modules/@types/jquery/index.d.ts" />
+/// <reference path="../../node_modules/@types/jqueryui/index.d.ts" />
+/// <reference path="../../node_modules/@types/mousetrap/index.d.ts" />
+/// <reference path="../../node_modules/@types/moment-timezone/index.d.ts" />
+/// <reference path="../../node_modules/@types/urijs/index.d.ts" />
+/// <reference path="../../node_modules/@types/webpack-env/index.d.ts" />
+/// <reference path="../../node_modules/@types/es6-shim/index.d.ts" />
+/// <reference path="../../node_modules/@types/dragula/index.d.ts" />
+
+import {ErrorReporter} from "core-app/sentry/sentry-reporter";
+import {Injector} from '@angular/core';
+
+import {OpenProject} from 'core-app/globals/openproject';
+import * as TLodash from 'lodash';
+import * as TMoment from 'moment';
+import {GlobalI18n} from "core-app/modules/common/i18n/i18n.service";
+import {Dragula} from "dragula";
+
+declare module 'observable-array';
+declare module 'dom-autoscroller';
+declare module 'core-vendor/enjoyhint';
+
+declare global {
+  const _:typeof TLodash;
+  const moment:typeof TMoment;
+  const I18n:GlobalI18n;
+  const dragula:Dragula;
+}
+
+declare global {
+  interface Window {
+    appBasePath:string;
+    ng2Injector:Injector;
+    OpenProject:OpenProject;
+    ErrorReporter:ErrorReporter;
+    onboardingTourInstance:any;
+  }
+
+  interface JQuery {
+    topShelf:any;
+    mark:any;
+    tablesorter:any;
+  }
+
+  interface JQueryStatic {
+    metadata:any;
+    tablesorter:any;
+  }
+}
+
+export {};
