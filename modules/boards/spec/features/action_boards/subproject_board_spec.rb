5dbb9eadc53c08776a064b9d7215c512782b162a
Oliver Günther <mail@oliverguenther.de>
2020-08-03 13:55:06 +0200

    Add a spec

--- modules/boards/spec/features/action_boards/subproject_board_spec.rb
+++ modules/boards/spec/features/action_boards/subproject_board_spec.rb
@@ -30,114 +30,133 @@ require 'spec_helper'
 require_relative './../support//board_index_page'
 require_relative './../support/board_page'
 
-describe 'Status action board', type: :feature, js: true do
+describe 'Subproject action board', type: :feature, js: true do
   let(:user) do
     FactoryBot.create(:user,
                       member_in_project: project,
                       member_through_role: role)
   end
   let(:type) { FactoryBot.create(:type_standard) }
-  let(:project) { FactoryBot.create(:project, types: [type], enabled_module_names: %i[work_package_tracking board_view]) }
+  let(:project) { FactoryBot.create(:project, name: 'Parent', types: [type], enabled_module_names: %i[work_package_tracking board_view]) }
+  let(:subproject1) { FactoryBot.create(:project, parent: project, name: 'Child 1', types: [type], enabled_module_names: %i[work_package_tracking]) }
+  let(:subproject2) { FactoryBot.create(:project, parent: project, name: 'Child 2', types: [type], enabled_module_names: %i[work_package_tracking]) }
   let(:role) { FactoryBot.create(:role, permissions: permissions) }
 
   let(:board_index) { Pages::BoardIndex.new(project) }
 
   let(:permissions) {
     %i[show_board_views manage_board_views add_work_packages
-       edit_work_packages view_work_packages manage_public_queries]
+       edit_work_packages view_work_packages manage_public_queries move_work_packages]
   }
 
   let!(:priority) { FactoryBot.create :default_priority }
   let!(:open_status) { FactoryBot.create :default_status, name: 'Open' }
-  let!(:other_status) { FactoryBot.create :status, name: 'Whatever' }
-  let!(:closed_status) { FactoryBot.create :status, is_closed: true, name: 'Closed' }
-  let!(:work_package) { FactoryBot.create :work_package, project: project, subject: 'Foo', status: other_status }
-
-  let(:filters) { ::Components::WorkPackages::Filters.new }
-
-  let!(:workflow_type) {
-    FactoryBot.create(:workflow,
-                      type: type,
-                      role: role,
-                      old_status_id: open_status.id,
-                      new_status_id: closed_status.id)
-  }
-  let!(:workflow_type_back) {
-    FactoryBot.create(:workflow,
-                      type: type,
-                      role: role,
-                      old_status_id: other_status.id,
-                      new_status_id: open_status.id)
-  }
-  let!(:workflow_type_back_open) {
-    FactoryBot.create(:workflow,
-                      type: type,
-                      role: role,
-                      old_status_id: closed_status.id,
-                      new_status_id: open_status.id)
-  }
+  let!(:work_package) { FactoryBot.create :work_package, project: subproject1, subject: 'Foo', status: open_status }
 
   before do
     with_enterprise_token :board_view
     project
+    subproject1
+    subproject2
     login_as(user)
   end
 
-  context 'with full boards permissions' do
-    it 'allows management of boards' do
+  context 'without the move_work_packages permission' do
+    let(:permissions) {
+      %i[show_board_views manage_board_views add_work_packages
+       edit_work_packages view_work_packages manage_public_queries]
+    }
+
+    let(:user) do
+      FactoryBot.create(:user,
+                        member_in_projects: [project, subproject1, subproject2],
+                        member_through_role: role)
+    end
+
+    it 'does not allow to move work packages' do
       board_index.visit!
 
       # Create new board
-      board_page = board_index.create_board action: :Status
+      board_page = board_index.create_board action: :Subproject, expect_empty: true
 
-      # expect lists of default status
-      board_page.expect_list 'Open'
+      # Expect we can add a child 1
+      board_page.add_list option: 'Child 1'
+      board_page.expect_list 'Child 1'
 
-      board_page.add_list option: 'Closed'
-      board_page.expect_list 'Closed'
+      # Expect one work package there
+      board_page.expect_card 'Child 1', 'Foo'
+      board_page.expect_movable 'Child 1', 'Foo', movable: false
+    end
+  end
+
+  context 'with permissions in all subprojects' do
+    let(:user) do
+      FactoryBot.create(:user,
+                        member_in_projects: [project, subproject1, subproject2],
+                        member_through_role: role)
+    end
+
+    let(:only_parent_user) do
+      FactoryBot.create(:user,
+                        member_in_project: project,
+                        member_through_role: role)
+    end
+
+    it 'allows management of subproject work packages' do
+      board_index.visit!
+
+      # Create new board
+      board_page = board_index.create_board action: :Subproject, expect_empty: true
+
+      # Expect we can add a child 1
+      board_page.add_list option: 'Child 1'
+      board_page.expect_list 'Child 1'
+
+      # Expect one work package there
+      board_page.expect_card 'Child 1', 'Foo'
+
+      # Expect move permission to be granted
+      board_page.expect_movable 'Child 1', 'Foo', movable: true
 
       board_page.board(reload: true) do |board|
-        expect(board.name).to eq 'Action board (status)'
+        expect(board.name).to eq 'Action board (Subproject)'
         queries = board.contained_queries
-        expect(queries.count).to eq(2)
+        expect(queries.count).to eq(1)
 
-        open = queries.first
-        closed = queries.last
+        query = queries.first
+        expect(query.name).to eq 'Child 1'
 
-        expect(open.name).to eq 'Open'
-        expect(closed.name).to eq 'Closed'
-
-        expect(open.filters.first.name).to eq :status_id
-        expect(open.filters.first.values).to eq [open_status.id.to_s]
-
-        expect(closed.filters.first.name).to eq :status_id
-        expect(closed.filters.first.values).to eq [closed_status.id.to_s]
+        expect(query.filters.first.name).to eq :only_subproject_id
+        expect(query.filters.first.values).to eq [subproject1.id.to_s]
       end
 
       # Create new list
-      board_page.add_list option: 'Whatever'
-      board_page.expect_list 'Whatever'
+      board_page.add_list option: 'Child 2'
+      board_page.expect_list 'Child 2'
+
+      board_page.expect_cards_in_order 'Child 2'
 
       # Add item
-      board_page.add_card 'Open', 'Task 1'
+      board_page.add_card 'Child 1', 'Task 1'
       sleep 2
 
       # Expect added to query
       queries = board_page.board(reload: true).contained_queries
-      expect(queries.count).to eq 3
-      first = queries.find_by(name: 'Open')
-      second = queries.find_by(name: 'Closed')
+      expect(queries.count).to eq 2
+      first = queries.find_by(name: 'Child 1')
+      second = queries.find_by(name: 'Child 2')
       expect(first.ordered_work_packages.count).to eq(1)
       expect(second.ordered_work_packages).to be_empty
 
       # Expect work package to be saved in query first
-      subjects = WorkPackage.where(id: first.ordered_work_packages.pluck(:work_package_id)).pluck(:subject, :status_id)
-      expect(subjects).to match_array [['Task 1', open_status.id]]
+      subjects = WorkPackage.where(id: first.ordered_work_packages.pluck(:work_package_id)).pluck(:subject, :project_id)
+      expect(subjects).to match_array [['Task 1', subproject1.id]]
 
-      # Move item to Closed
-      board_page.move_card(0, from: 'Open', to: 'Closed')
-      board_page.expect_card('Open', 'Task 1', present: false)
-      board_page.expect_card('Closed', 'Task 1', present: true)
+      # Move item to Child 2 list
+      board_page.move_card(0, from: 'Child 1', to: 'Child 2')
+
+      board_page.expect_card('Child 1', 'Task 1', present: false)
+      board_page.expect_card('Child 2', 'Task 1', present: true)
 
       # Expect work package to be saved in query second
       sleep 2
@@ -146,81 +165,15 @@ describe 'Status action board', type: :feature, js: true do
         expect(second.reload.ordered_work_packages.count).to eq(1)
       end
 
-      subjects = WorkPackage.where(id: second.ordered_work_packages.pluck(:work_package_id)).pluck(:subject, :status_id)
-      expect(subjects).to match_array [['Task 1', closed_status.id]]
+      subjects = WorkPackage.where(id: second.ordered_work_packages.pluck(:work_package_id)).pluck(:subject, :project_id)
+      expect(subjects).to match_array [['Task 1', subproject2.id]]
 
-      # Try to drag to whatever, which has no workflow
-      board_page.move_card(0, from: 'Closed', to: 'Whatever')
-      board_page.expect_and_dismiss_notification(
-        type: :error,
-        message: "Status is invalid because no valid transition exists from old to new status for the current user's roles."
-      )
-      board_page.expect_card('Open', 'Task 1', present: false)
-      board_page.expect_card('Whatever', 'Task 1', present: false)
-      board_page.expect_card('Closed', 'Task 1', present: true)
-
-      # Add filter
-      # Filter for Task
-      filters.expect_filter_count 0
-      filters.open
-
-      # Expect that status is not available for global filter selection
-      filters.expect_available_filter 'Status', present: false
-
-      filters.quick_filter 'Task'
-      board_page.expect_changed
-      sleep 2
-
-      board_page.expect_card('Closed', 'Task 1', present: true)
-      board_page.expect_card('Whatever', work_package.subject, present: false)
-
-      # Expect query props to be present
-      url = URI.parse(page.current_url).query
-      expect(url).to include("query_props=")
-
-      # Save that filter
-      board_page.save
-
-      # Expect filter to be saved in board
-      board_page.board(reload: true) do |board|
-        expect(board.options['filters']).to eq [{ 'search' => { 'operator' => '**', 'values' => ['Task'] } }]
-      end
-
-      # Revisit board
+      # Trying to access the same board as a different user
+      login_as only_parent_user
       board_page.visit!
 
-      # Expect filter to be present
-      filters.expect_filter_count 1
-      filters.open
-      filters.expect_quick_filter 'Task'
-
-      # No query props visible
-      board_page.expect_not_changed
-
-      # Remove query
-      board_page.remove_list 'Whatever'
-      queries = board_page.board(reload: true).contained_queries
-      expect(queries.count).to eq(2)
-      expect(queries.first.name).to eq 'Open'
-      expect(queries.last.name).to eq 'Closed'
-      expect(queries.first.ordered_work_packages).to be_empty
-
-      subjects = WorkPackage.where(id: second.ordered_work_packages.pluck(:work_package_id)).pluck(:subject, :status_id)
-      expect(subjects).to match_array [['Task 1', closed_status.id]]
-
-      # Open remaining in split view
-      wp = second.ordered_work_packages.first.work_package
-      card = board_page.card_for(wp)
-      split_view = card.open_details_view
-      split_view.expect_subject
-      split_view.edit_field(:status).update('Open')
-      split_view.expect_and_dismiss_notification message: 'Successful update.'
-
-      wp.reload
-      expect(wp.status).to eq(open_status)
-
-      board_page.expect_card('Open', 'Task 1', present: true)
-      board_page.expect_card('Closed', 'Task 1', present: false)
+      # We will see an error for the two boards pages
+      expect(page).to have_selector('.notification-box.-error', count: 2)
     end
   end
 end
