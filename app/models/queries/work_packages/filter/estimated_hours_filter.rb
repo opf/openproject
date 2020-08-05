1fbc75b96d93b7574b63fa9d8772d13efaf0fb14
bsatarnejad <b.satarnejad@gmail.com>
2020-08-05 10:01:49 +0200

    writing a test for estimated-hours filter with none and zero values

--- app/models/queries/work_packages/filter/estimated_hours_filter.rb
+++ app/models/queries/work_packages/filter/estimated_hours_filter.rb
@@ -32,4 +32,12 @@ class Queries::WorkPackages::Filter::EstimatedHoursFilter <
   def type
     :integer
   end
+  
+  def where
+    if operator == Queries::Operators::None.to_sym.to_s
+      super + " OR #{WorkPackage.table_name}.estimated_hours=0"
+    else
+      super
+    end
+  end
 end
--- spec/models/queries/work_packages/filter/estimated_hours_filter_spec.rb
+++ spec/models/queries/work_packages/filter/estimated_hours_filter_spec.rb
@@ -46,5 +46,21 @@ describe Queries::WorkPackages::Filter::EstimatedHoursFilter, type: :model do
     end
 
     it_behaves_like 'non ar filter'
+
+    describe '#where' do
+      let!(:work_package_zero_hour) {FactoryBot.create(:work_package, estimated_hours: 0)}
+      let!(:work_package_no_hours) {FactoryBot.create(:work_package, estimated_hours: nil)}
+      let!(:work_package_with_hours) {FactoryBot.create(:work_package, estimated_hours: 1)}
+
+      context 'with the operator being "none"' do
+        before do
+          instance.operator = Queries::Operators::None.to_sym.to_s
+        end
+        it 'finds zero and none values' do
+          expect(WorkPackage.where(instance.where)).to match_array [work_package_zero_hour, work_package_no_hours]
+        end
+      end
+    end
   end
+  
 end
