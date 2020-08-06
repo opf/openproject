442ce4a2a7474d729cae9c4e6a71213740d0f9b9
ulferts <jens.ulferts@googlemail.com>
2020-08-06 10:29:15 +0200

    fix archiving projects via api
    When archiving, we need to assume the old `active` status to see if any modifications can be made. On top of that, whenever that status is changed the archive/unarchive service needs to be called as well so that e.g. the child projects are also archived

--- app/contracts/projects/archiver.rb
+++ app/contracts/projects/archiver.rb
@@ -27,20 +27,30 @@
 #++
 
 module Projects
-  class UnarchiveContract < ModelContract
-    include Projects::Archiver
-
-    def validate
-      validate_admin_only
-      validate_all_ancestors_active
-
-      super
+  module Archiver
+    def validate_admin_only
+      unless user.admin?
+        errors.add :base, :error_unauthorized
+      end
     end
 
-    protected
+    # Check that there is no wp of a non descendant project that is assigned
+    # to one of the project or descendant versions
+    def validate_no_foreign_wp_references
+      version_ids = model.rolled_up_versions.select(:id)
 
-    def validate_model?
-      false
+      exists = WorkPackage
+                 .where.not(project_id: model.self_and_descendants.select(:id))
+                 .where(version_id: version_ids)
+                 .exists?
+
+      errors.add :base, :foreign_wps_reference_version if exists
+    end
+
+    def validate_all_ancestors_active
+      if model.ancestors.any?(&:archived?)
+        errors.add :base, :archived_ancestor
+      end
     end
   end
 end
