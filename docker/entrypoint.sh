329ac1c3c77c91cc2ebbb654ad126dc44cd2cd8d
Cyril Rohr <cyril.rohr@gmail.com>
2020-08-18 12:51:33 +0000

    Allow root commands

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -67,11 +67,18 @@ if [ "$(id -u)" = '0' ]; then
 	mkdir -p "$APP_PATH/log" "$APP_PATH/tmp/pids" "$APP_PATH/files"
 	chown "$APP_USER:$APP_USER" "$APP_PATH"
 	chown -R "$APP_USER:$APP_USER" "$APP_PATH/log" "$APP_PATH/tmp" "$APP_PATH/files" "$APP_PATH/public"
-	if [ "$1" = "./docker/supervisord" ] || [ "$1" = "./docker/proxy" ] || [ "$1" = "bash" ]; then
+
+	# allow to launch any command as root by prepending it with 'root'
+	if [ "$1" = "root" ]; then
+		shift
 		exec "$@"
-	else
-		exec $APP_PATH/docker/gosu $APP_USER "$BASH_SOURCE" "$@"
 	fi
+
+	if [ "$1" = "./docker/supervisord" ] || [ "$1" = "./docker/proxy" ]; then
+		exec "$@"
+	fi
+
+	exec $APP_PATH/docker/gosu $APP_USER "$BASH_SOURCE" "$@"
 fi
 
 exec "$@"
