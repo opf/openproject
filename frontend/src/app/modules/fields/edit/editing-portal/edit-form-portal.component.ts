f517746abea43cbdbc107bdb18171ae10525a029
Aleix Suau <info@macrofonoestudio.es>
2020-09-01 10:23:01 +0200

    Line break removed

--- frontend/src/app/modules/fields/edit/editing-portal/edit-form-portal.component.ts
+++ frontend/src/app/modules/fields/edit/editing-portal/edit-form-portal.component.ts
@@ -56,7 +56,6 @@ export class EditFormPortalComponent implements OnInit, OnDestroy, AfterViewInit
       this.change = this.injector.get<ResourceChangeset>(OpEditingPortalChangesetToken);
     }
 
-
     this.componentClass = this.editField.getSpecificClassFor(this.change.pristineResource._type, this.handler.fieldName, this.schema.type);
     this.fieldInjector = createLocalInjector(this.injector, this.change, this.handler, this.schema);
   }
