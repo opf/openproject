83ba37488afc6f6c48807c0e69c386fa3b8a5e67
Oliver Günther <mail@oliverguenther.de>
2020-09-04 11:07:48 +0200

    [33496] Don't try to lookup custom value by value
    Only for list types, the value will be an integer to look up by id,
    but instead use `CustomValue.new(...).typed_value` to use the formatter
    classes to correctly look up the value.
    
    https://community.openproject.com/wp/33496
    
    [ci skip]

--- modules/reporting/spec/helpers/reporting_helper_spec.rb
+++ modules/reporting/spec/helpers/reporting_helper_spec.rb
@@ -32,29 +32,46 @@ describe ReportingHelper, type: :helper do
   describe '#field_representation_map' do
     context 'for a custom field' do
       context 'for which a custom option exists (e.g. a list field)' do
+        let(:custom_field) do
+          FactoryBot.create(
+            :list_wp_custom_field,
+            name: "Ingredients",
+            possible_values: ["ham"]
+          )
+        end
+
         it 'returns the option value' do
-          option = FactoryBot.build(:custom_option)
+          option = custom_field.possible_values.first
 
-          allow(CustomOption)
-            .to receive(:find_by)
-            .with(id: "345")
-            .and_return(option)
+          expect(field_representation_map("custom_field#{custom_field.id}", option.id))
+            .to eql 'ham'
+        end
 
-          expect(field_representation_map("custom_field22", "345"))
-            .to eql option.value
+        it 'returns not found for an outdated value value' do
+          expect(field_representation_map("custom_field#{custom_field.id}", "1234123"))
+            .to eql '1234123 not found'
+        end
+      end
+
+      context 'for which no custom option exists (e.g. a float field)' do
+        let(:custom_field) do
+          FactoryBot.create(
+            :float_wp_custom_field,
+            name: "Estimate"
+          )
+        end
+
+        it 'returns the option value' do
+          expect(field_representation_map("custom_field#{custom_field.id}", 3.0))
+            .to eql 3.0
         end
       end
     end
 
     context 'for which no custom option exists' do
-      it 'returns the option value' do
-        allow(CustomOption)
-          .to receive(:find_by)
-          .with(id: "345")
-          .and_return(nil)
-
-        expect(field_representation_map("custom_field22", "345"))
-          .to eql "345"
+      it 'returns the not found value' do
+        expect(field_representation_map("custom_field12345", "345"))
+          .to eql "345 not found"
       end
     end
   end
