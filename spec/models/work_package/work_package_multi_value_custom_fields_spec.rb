76b4ed2d491a4f9e75ac2292b23af4e789732970
ulferts <jens.ulferts@googlemail.com>
2020-09-10 15:16:26 +0200

    fix touching the customizable on removing custom_values
    Before the commit, the existing custom_values where attempted to be
    reused but there was no deterministic behaviour with which reusing them
    was attempted. Additionally, the existing values where always changed
    (or they would receive the current value again) whenever values where
    provided, regardless of whether those values actually changes. There
    was no explicit flag that a value was removed.
    
    This poses a couple of problems:
    * Even if no new values where provided the order of the custom values as
      stored in the db might change leading to journal entries that where
      not actually caused by the user.
    * If values where only removed, due to the missing
      explicit removal flag, it became luck whether an existing custom
      value was actually changed. Only then would the customized object be
      touched leading to an update of cached fragments, i.e. representers.
    * Because the values where updated all the time, the performance would
      suffer.
    * Removed custom values where removed right away and not only after
      save.
    
    This commit introduces an explicit flag for removal and does not alter
    existing custom values. This might have the drawback of creating a
    custom value even when a value, chosen to be removed by the user, could
    be reused but this should be negligible.
    
    It also has the drawback of introducing a flag to be maintained.

--- spec/models/work_package/work_package_multi_value_custom_fields_spec.rb
+++ spec/models/work_package/work_package_multi_value_custom_fields_spec.rb
@@ -44,9 +44,11 @@ describe WorkPackage, type: :model do
   end
 
   let(:custom_values) do
-    ["ham", "onions", "pineapple"].map do |str|
-      custom_field.custom_options.find { |co| co.value == str }.try(:id)
-    end
+    custom_field
+      .custom_options
+      .where(value: ["ham", "onions", "pineapple"])
+      .pluck(:id)
+      .map(&:to_s)
   end
 
   let(:work_package) do
@@ -63,7 +65,7 @@ describe WorkPackage, type: :model do
   let(:typed_values) { work_package.typed_custom_value_for(custom_field.id) }
 
   it 'returns the properly typed values' do
-    expect(values.map { |cv| cv.value.to_i }).to eq(custom_values)
+    expect(values.map { |cv| cv.value }).to eq(custom_values)
     expect(typed_values).to eq(%w(ham onions pineapple))
   end
 
@@ -73,6 +75,65 @@ describe WorkPackage, type: :model do
     it 'returns nil properly' do
       expect(values).to eq(nil)
       expect(typed_values).to eq(nil)
+    end
+  end
+
+  describe 'setting and reading values' do
+    shared_examples_for 'custom field values updates' do
+      before do
+        # Reload to reset i.e. the saved_changes filter on custom_values
+        work_package.reload
+      end
+
+      it 'touches the work_package' do
+        expect do
+          work_package.custom_field_values = { custom_field.id => ids }
+          work_package.save
+        end
+          .to(change { work_package.lock_version })
+      end
+
+      it 'sets the values' do
+        work_package.custom_field_values = { custom_field.id => ids }
+        work_package.save
+
+        expect(work_package.send("custom_field_#{custom_field.id}"))
+          .to eql values
+      end
+    end
+
+    context 'when removing some custom values' do
+      it_behaves_like 'custom field values updates' do
+        let(:ids) { [custom_values.first.to_s] }
+        let(:values) { ['ham'] }
+      end
+    end
+
+    context 'when removing all custom values' do
+      it_behaves_like 'custom field values updates' do
+        let(:ids) { [] }
+        let(:values) { [nil] }
+      end
+    end
+
+    context 'when adding values' do
+      it_behaves_like 'custom field values updates' do
+        let(:ids) do
+          CustomOption.where(value: ["ham", "onions", "pineapple", "mushrooms"]).pluck(:id).map(&:to_s)
+        end
+        let(:values) { ["ham", "onions", "pineapple", "mushrooms"] }
+      end
+    end
+
+    context 'when first having no values and then adding some' do
+      let(:custom_values) { [] }
+
+      it_behaves_like 'custom field values updates' do
+        let(:ids) do
+          CustomOption.where(value: ["ham", "mushrooms"]).pluck(:id).map(&:to_s)
+        end
+        let(:values) { ["ham", "mushrooms"] }
+      end
     end
   end
 end
