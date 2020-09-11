c6f3c71ba8b215c37efbaee3b0ada691fc00d5a2
Wieland Lindenthal <w.lindenthal@forkmerge.com>
2020-09-11 18:12:49 +0200

    When setting viewpoint directly request focus for OP window back

--- frontend/src/app/modules/bim/revit_add_in/revit-bridge.service.ts
+++ frontend/src/app/modules/bim/revit_add_in/revit-bridge.service.ts
@@ -71,7 +71,10 @@ export class RevitBridgeService extends ViewerBridgeService {
   public showViewpoint(workPackage:WorkPackageResource, index:number) {
      this.viewpointsService
               .getViewPoint$(workPackage, index)
-              .subscribe((viewpoint:BcfViewpointInterface) =>  this.sendMessageToRevit('ShowViewpoint', this.newTrackingId(), JSON.stringify(viewpoint)));
+              .subscribe((viewpoint:BcfViewpointInterface) => {
+                this.sendMessageToRevit('ShowViewpoint', this.newTrackingId(), JSON.stringify(viewpoint));
+                this.sendMessageToRevit("SetBrowserToForeground", this.newTrackingId(), '');
+              });
   }
 
   sendMessageToRevit(messageType:string, trackingId:string, messagePayload?:any) {
