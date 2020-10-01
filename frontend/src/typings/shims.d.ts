3f8ba44bb0c01a330903d05ec83efeb7979f4c2b
Aleix Suau <info@macrofonoestudio.es>
2020-10-01 17:23:16 +0200

    QDCode library working on the global space

--- frontend/src/typings/shims.d.ts
+++ frontend/src/typings/shims.d.ts
@@ -27,6 +27,7 @@ import {Screenfull} from "screenfull";
 declare module 'observable-array';
 declare module 'dom-autoscroller';
 declare module 'core-vendor/enjoyhint';
+declare module 'core-vendor/qrcode-min';
 
 declare global {
   const _:typeof TLodash;
