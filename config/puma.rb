c32b1908ac6664399ebe615e83a59493e10e9a5d
Markus Kahl <machisuji@gmail.com>
2020-10-08 13:43:57 +0100

    fix ArgumentError when setting RAILX_MAX_THREADs
    actually convert strings to ints so they can be compared

--- config/puma.rb
+++ config/puma.rb
@@ -3,13 +3,13 @@
 # Any libraries that use thread pools should be configured to match
 # the maximum value specified for Puma.
 #
-threads_min_count = ENV.fetch("RAILS_MIN_THREADS") { 4 }
-threads_max_count = ENV.fetch("RAILS_MAX_THREADS") { 16 }
+threads_min_count = ENV.fetch("RAILS_MIN_THREADS") { 4 }.to_i
+threads_max_count = ENV.fetch("RAILS_MAX_THREADS") { 16 }.to_i
 threads threads_min_count, [threads_min_count, threads_max_count].max
 
 # Specifies the `port` that Puma will listen on to receive requests; default is 3000.
 #
-port ENV.fetch("PORT") { 3000 }
+port ENV.fetch("PORT") { 3000 }.to_i
 
 # Specifies the `environment` that Puma will run in.
 #
@@ -21,7 +21,7 @@ environment ENV.fetch("RAILS_ENV") { "development" }
 # Workers do not work on JRuby or Windows (both of which do not support
 # processes).
 #
-workers ENV.fetch("OPENPROJECT_WEB_WORKERS") { 1 }
+workers ENV.fetch("OPENPROJECT_WEB_WORKERS") { 1 }.to_i
 
 # Use the `preload_app!` method when specifying a `workers` number.
 # This directive tells Puma to first boot the application and load code
