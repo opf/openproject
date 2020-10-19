f654f9c70de407a3df93ba3809dccb9917d2342d
Markus Kahl <machisuji@gmail.com>
2020-10-19 14:12:21 +0100

    require login to finish direct upload (and set file name)

--- modules/bim/app/controllers/bim/ifc_models/ifc_models_controller.rb
+++ modules/bim/app/controllers/bim/ifc_models/ifc_models_controller.rb
@@ -37,8 +37,8 @@ module Bim
       before_action :find_ifc_model_object, only: %i[edit update destroy]
       before_action :find_all_ifc_models, only: %i[show defaults index]
 
-      before_action :authorize, except: [:set_direct_upload_file_name, :direct_upload_finished]
-      skip_before_action :verify_authenticity_token, only: [:set_direct_upload_file_name]
+      before_action :authorize
+      skip_before_action :verify_authenticity_token, only: [:set_direct_upload_file_name, :direct_upload_finished]
 
       menu_item :ifc_models
 
