4dd8e433b45094752bfca09ab06210895b1b9731
ulferts <jens.ulferts@googlemail.com>
2020-10-26 21:25:18 +0100

    Fix/bump gems (#8770)
    * bump aws-sdk-core
    
    * bump danger
    
    * bump parser
    
    * bump grape - stream related header setting bug has been fixed
    
    * reapply cache headers on stream as grape insists on niling them
    
    * bump aws-partitions
    
    * bump regexp_parser
    
    * bump rubocop
    
    * bump webmock

--- modules/bim/app/controllers/bim/bcf/api/v2_1/viewpoints/api.rb
+++ modules/bim/app/controllers/bim/bcf/api/v2_1/viewpoints/api.rb
@@ -75,20 +75,11 @@ module Bim::Bcf::API::V2_1
             raise NotImplementedError, 'Bitmaps are not yet implemented.'
           end
 
-          namespace :snapshot do
-            helpers ::API::Helpers::AttachmentRenderer
+          namespace :snapshot, &::API::Helpers::AttachmentRenderer.content_endpoint(&-> {
+            snapshot = @issue.viewpoints.find_by!(uuid: params[:viewpoint_uuid]).snapshot
 
-            get do
-              viewpoint = @issue.viewpoints.find_by!(uuid: params[:viewpoint_uuid])
-              if snapshot = viewpoint.snapshot
-                # Cache that value at max 604799 seconds, which is the max
-                # allowed expiry time for AWS generated links
-                respond_with_attachment snapshot, cache_seconds: 604799
-              else
-                raise ActiveRecord::RecordNotFound
-              end
-            end
-          end
+            snapshot || raise(ActiveRecord::RecordNotFound)
+          })
         end
       end
     end
