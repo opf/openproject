b45057e8b0549b4647b9687768b3d6ed3c10c9d3
Benjamin Bädorf <b.baedorf@openproject.com>
2020-11-16 10:41:43 +0100

    Fix copy command, bumped postgres version

--- docker/prod/setup/postinstall.sh
+++ docker/prod/setup/postinstall.sh
@@ -14,7 +14,7 @@ display_error() {
 echo " ---> POSTINSTALL"
 
 # Add MySQL-to-Postgres migration script to path (used in entrypoint.sh)
-cp ./docker/mysql-to-postgres/bin/migrate-mysql-to-postgres /usr/local/bin/
+cp ./docker/prod/mysql-to-postgres/bin/migrate-mysql-to-postgres /usr/local/bin/
 
 # Ensure we can write in /tmp/op_uploaded_files (cf. #29112)
 mkdir -p /tmp/op_uploaded_files/ && chown -R $APP_USER:$APP_USER /tmp/op_uploaded_files/
