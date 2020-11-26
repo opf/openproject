210e7c7f006ac7eefb8d72674426be593c652dd2
ulferts <jens.ulferts@googlemail.com>
2020-11-26 09:33:44 +0100

    adapt spec expectation to altered toc rendering

--- spec/requests/api/v3/work_package_resource_spec.rb
+++ spec/requests/api/v3/work_package_resource_spec.rb
@@ -287,8 +287,9 @@ describe 'API v3 Work package resource',
             # resolves links
             expect(subject['html'])
               .to have_selector("macro.macro--wp-quickinfo[data-id='#{other_wp.id}']")
-            # resolves macros
-            is_expected.to have_text('Table of contents')
+            # resolves macros, e.g. toc
+            expect(subject['html'])
+              .to have_selector('.op-uc-toc--list-item', text: "OpenProject Masterplan for 2015")
           end
         end
 
