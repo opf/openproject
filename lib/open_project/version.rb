926e745c52f2fd21134c22041b6185146a745ccc
ulferts <jens.ulferts@googlemail.com>
2020-12-17 13:36:19 +0100

    Bumped version to 11.1.1
    [ci skip]

--- lib/open_project/version.rb
+++ lib/open_project/version.rb
@@ -33,8 +33,8 @@ require 'open3'
 module OpenProject
   module VERSION #:nodoc:
     MAJOR = 11
-    MINOR = 2
-    PATCH = 0
+    MINOR = 1
+    PATCH = 1
     TINY  = PATCH # Redmine compat
 
     class << self
