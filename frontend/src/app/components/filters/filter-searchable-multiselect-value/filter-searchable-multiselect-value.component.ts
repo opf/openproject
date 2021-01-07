e0750c898abeba8b08801b5b2eabbc048338c409
bsatarnejad <b.satarnejad@gmail.com>
2021-01-07 13:33:01 +0100

    refactoring the code

--- frontend/src/app/components/filters/filter-searchable-multiselect-value/filter-searchable-multiselect-value.component.ts
+++ frontend/src/app/components/filters/filter-searchable-multiselect-value/filter-searchable-multiselect-value.component.ts
@@ -1,16 +1,7 @@
 
 import {HalResource} from 'core-app/modules/hal/resources/hal-resource';
 import {QueryFilterInstanceResource} from 'core-app/modules/hal/resources/query-filter-instance-resource';
-import {
-  ChangeDetectionStrategy,
-  ChangeDetectorRef,
-  Component,
-  EventEmitter,
-  Input,
-  OnInit,
-  Output,
-  ViewChild
-} from '@angular/core';
+import {AfterViewInit, ChangeDetectionStrategy, ChangeDetectorRef, Component, EventEmitter, Input, OnInit, Output, ViewChild} from '@angular/core';
 import {I18nService} from 'core-app/modules/common/i18n/i18n.service';
 import {AngularTrackingHelpers} from 'core-components/angular/tracking-functions';
 import {HalResourceService} from 'core-app/modules/hal/services/hal-resource.service';
@@ -24,13 +15,13 @@ import { HalResourceNotificationService } from 'core-app/modules/hal/services/ha
 import { CurrentProjectService } from 'core-app/components/projects/current-project.service';
 import { ApiV3FilterBuilder, FilterOperator } from 'core-app/components/api/api-v3/api-v3-filter-builder';
 import { map } from 'rxjs/operators';
-import { forEach } from 'lodash';
-import { param } from 'jquery';
 import { APIv3ResourceCollection } from 'core-app/modules/apiv3/paths/apiv3-resource';
 import { UserResource } from 'core-app/modules/hal/resources/user-resource';
 import { APIv3UserPaths } from 'core-app/modules/apiv3/endpoints/users/apiv3-user-paths';
 import { APIV3WorkPackagePaths } from 'core-app/modules/apiv3/endpoints/work_packages/api-v3-work-package-paths';
 import { WorkPackageResource } from 'core-app/modules/hal/resources/work-package-resource';
+import { UntilDestroyedMixin } from 'core-app/helpers/angular/until-destroyed.mixin';
+import { any } from '@uirouter/core';
 
 export interface FilterConditions {name:string; operator:FilterOperator; values:unknown[]|boolean; }
 
@@ -41,95 +32,30 @@ export interface FilterConditions {name:string; operator:FilterOperator; values:
 })
 
 
-export class FilterSearchableMultiselectValueComponent implements OnInit {
+export class FilterSearchableMultiselectValueComponent extends UntilDestroyedMixin implements OnInit, AfterViewInit {
   @Input() public shouldFocus:boolean = false;
   @Input() public filter:QueryFilterInstanceResource;
   @Input() public filterConditions?:FilterConditions[];
-  @Input('filterResource')public filterResource:'work_packages' | 'users';
+  @Input() public filterResource:'work_packages' | 'users';
   @Input() public filterSearchKey?:string;
   @Output() public filterChanged = new EventEmitter<QueryFilterInstanceResource>();
 
-  @ViewChild('ngSelectInstance', { static: true }) ngSelectInstance:NgSelectComponent;
-
+  private _isEmpty:boolean;
   public _availableOptions:HalResource[] = [];
   public compareByHrefOrString = AngularTrackingHelpers.compareByHrefOrString;
-
-  private _isEmpty:boolean;
-
-  readonly text = {
-    placeholder: this.I18n.t('js.placeholders.selection'),
-  };
-
-   constructor(readonly halResourceService:HalResourceService,
-              readonly halSorting:HalResourceSortingService,
-              readonly apiV3Service:APIV3Service,
-              readonly cdRef:ChangeDetectorRef,
-              readonly I18n:I18nService,
-              protected currentProject:CurrentProjectService,
-              readonly halNotification:HalResourceNotificationService) {
-  }
+  public active:Set<string>;
   public requests = new DebouncedRequestSwitchmap<string, ValueOption>(
     (searchTerm:string) => this.loadAvailable(searchTerm),
     errorNotificationHandler(this.halNotification),
     true
   );
-  public active:Set<string>;
-
-  protected createFilters(filterConditions:FilterConditions[], matching:string) {
-    let filters = new ApiV3FilterBuilder();
-    for (let condition of filterConditions) {
-      filters.add(condition.name, condition.operator, condition.values);
-    }
-    if (matching) {
-      filters.add(this.filterSearchKey ?? '', '**', [matching]);
-    }
-    return filters;
-  }
-  public loadAvailable(matching:string):Observable<HalResource[]> {
-
-    let filters:ApiV3FilterBuilder = this.createFilters(this.filterConditions ?? [], matching);
-
-    let filteredData = (this.apiV3Service[this.filterResource] as
-      APIv3ResourceCollection<UserResource|WorkPackageResource,
-      APIv3UserPaths|APIV3WorkPackagePaths>).filtered(filters)
-      .get()
-      .pipe(
-        map(collection => collection.elements)
-      );
-
-    return filteredData
-      .pipe(
-        map(items => items
-      ));
-  }
-
-  initialization() {
-    this
-    .requests
-    .output$
-    .subscribe((values:unknown[]) => {
-      this.availableOptions = values  as HalResource[];
-      this.cdRef.detectChanges();
-    });
-  }
-
-  ngOnInit() {
-    this.initialization();
-  // Request an empty value to load warning early on
-    this.requests.input$.next('');
-  }
+  readonly text = {
+    placeholder: this.I18n.t('js.placeholders.selection'),
+  };
 
   public get value() {
     return this.filter.values;
   }
-
-  public setValues(val:any) {
-    this.filter.values = _.castArray(val);
-    this.filterChanged.emit(this.filter);
-    this.requests.input$.next('');
-    this.cdRef.detectChanges();
-  }
-
   public get availableOptions() {
     return this._availableOptions;
   }
@@ -142,6 +68,75 @@ export class FilterSearchableMultiselectValueComponent implements OnInit {
     return this._isEmpty = this.value.length === 0;
   }
 
+  @ViewChild('ngSelectInstance', { static: true }) ngSelectInstance:NgSelectComponent;
+
+   constructor(readonly halResourceService:HalResourceService,
+              readonly halSorting:HalResourceSortingService,
+              readonly apiV3Service:APIV3Service,
+              readonly cdRef:ChangeDetectorRef,
+              readonly I18n:I18nService,
+              protected currentProject:CurrentProjectService,
+              readonly halNotification:HalResourceNotificationService) {
+              super();
+  }
+  ngAfterViewInit():void {
+    if (this.ngSelectInstance && this.shouldFocus) {
+      this.ngSelectInstance.focus();
+    }
+  }
+
+  ngOnInit() {
+    this.initialization();
+  // Request an empty value to load warning early on
+    this.requests.input$.next('');
+  }
+
+  initialization() {
+    this
+    .requests
+    .output$.pipe(
+      this.untilDestroyed()
+    )
+    .subscribe((values:HalResource[]) => {
+      this.availableOptions = values;
+      this.cdRef.detectChanges();
+    });
+  }
+
+  public loadAvailable(matching:string):Observable<HalResource[]> {
+    let filters:ApiV3FilterBuilder = this.createFilters(this.filterConditions ?? [], matching);
+
+    let filteredData = (this.apiV3Service[this.filterResource] as
+      APIv3ResourceCollection<UserResource|WorkPackageResource, APIv3UserPaths|APIV3WorkPackagePaths>)
+      .filtered(filters).get()
+      .pipe(map(collection => collection.elements));
+
+    return filteredData;
+  }
+
+  protected createFilters(filterConditions:FilterConditions[], matching:string) {
+    let filters = new ApiV3FilterBuilder();
+
+    for (let condition of filterConditions) {
+      filters.add(condition.name, condition.operator, condition.values);
+    }
+    if (matching) {
+      filters.add(this.filterSearchKey ?? '', '**', [matching]);
+    }
+    return filters;
+  }
+
+  public setValues(val:any) {
+    if (val.length > 0) {
+    this.filter.values = (Array.isArray(val) ? val : [val]); }
+    else {
+      this.filter.values = [] as HalResource[];
+    }
+    this.filterChanged.emit(this.filter);
+    this.requests.input$.next('');
+    this.cdRef.detectChanges();
+  }
+
   public repositionDropdown() {
     if (this.ngSelectInstance) {
         const component = (this.ngSelectInstance) as any;
@@ -150,5 +145,4 @@ export class FilterSearchableMultiselectValueComponent implements OnInit {
         }
     }
   }
-
 }
