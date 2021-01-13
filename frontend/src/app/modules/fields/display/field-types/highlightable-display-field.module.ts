aaad134587e51eb50b4e2836b9630c6ea7096111
Aleix Suau <info@macrofonoestudio.es>
2021-01-13 07:33:20 +0100

    Fix/35144 milestones due dates not highlighted (#8915)
    * Default highlight attrs input values removed
    
    * Set the DisplayField.name back to 'dueDate' if it is a Milestone
    
    * Highlight: return 'dueDate' for all date fields

--- frontend/src/app/modules/fields/display/field-types/highlightable-display-field.module.ts
+++ frontend/src/app/modules/fields/display/field-types/highlightable-display-field.module.ts
@@ -31,16 +31,30 @@ import {WorkPackageViewHighlightingService} from "core-app/modules/work_packages
 import {InjectField} from "core-app/helpers/angular/inject-field.decorator";
 
 export class HighlightableDisplayField extends DisplayField {
-
   /** Optionally test if we can inject highlighting service */
   @InjectField(WorkPackageViewHighlightingService, null) viewHighlighting:WorkPackageViewHighlightingService;
+
+
+  // DisplayFieldRenderer.attributeName returns the 'date' name for the
+  // 'dueDate' field because it is its schema.mappedName (that allows to display
+  // the correct input type). In the query.highlightedAttributes (used to decide
+  // if a field is highlighted) the attribute has the name 'dueDate', so we need
+  // to return the original name to get it highlighted.
+  get highlightName () {
+    if (this.name === 'date') {
+      return 'dueDate';
+    } else {
+      return this.name;
+    }
+  }
 
   public get shouldHighlight() {
     if (this.context.options.colorize === false) {
       return false;
     }
 
-    const shouldHighlight = !!this.viewHighlighting && this.viewHighlighting.shouldHighlightInline(this.name);
+    const shouldHighlight = !!this.viewHighlighting && this.viewHighlighting.shouldHighlightInline(this.highlightName);
+
     return this.context.container !== 'table' || shouldHighlight;
   }
 }
