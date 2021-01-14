a240d6a2e464921d0dad2214b62d896b1c97b520
Aleix Suau <info@macrofonoestudio.es>
2021-01-14 13:50:27 +0100

    Permissions Service + show button if canInviteUsersToProject

---
+++ frontend/src/app/core/services/permissions/permissions.service.spec.ts
@@ -0,0 +1,16 @@
+import { TestBed } from '@angular/core/testing';
+
+import { PermissionsService } from './permissions.service';
+
+describe('PermissionsService', () => {
+  let service: PermissionsService;
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({});
+    service = TestBed.inject(PermissionsService);
+  });
+
+  it('should be created', () => {
+    expect(service).toBeTruthy();
+  });
+});
