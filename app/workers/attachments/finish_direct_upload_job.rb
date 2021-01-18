38cae188adae5432c729de598928bcccfd495f70
ulferts <jens.ulferts@googlemail.com>
2021-01-18 17:28:32 +0100

    Fix/journals on direct uploads (#8933)
    * linting
    
    * use Carrierwave's replacement for filename
    
    This avoids having the name altered by Carrierwave later on in the Attachments::FinishDirectUploadJob job where the file is fetched and then the attachment is stored.
    
    * separate cases for no attachment and no local file
    
    * correct journalizing on direct upload
    
    In case the container of an attachment is journalized, the container needs to have added a journal to it when an attachment is uploaded. Without that, the attachment is only picked up in the journals once the container is altered again (possibly by a different user). This will then lead to a history which does not reflect the actual upload.

--- app/workers/attachments/finish_direct_upload_job.rb
+++ app/workers/attachments/finish_direct_upload_job.rb
@@ -43,14 +43,45 @@ class Attachments::FinishDirectUploadJob < ApplicationJob
     end
 
     begin
-      attachment.downloads = 0
-      attachment.set_file_size local_file unless attachment.filesize && attachment.filesize > 0
-      attachment.set_content_type local_file unless attachment.content_type.present?
-      attachment.set_digest local_file unless attachment.digest.present?
-
-      attachment.save! if attachment.changed?
+      set_attributes_from_file(attachment, local_file)
+      save_attachment(attachment)
+      journalize_container(attachment)
     ensure
       File.unlink(local_file.path) if File.exist?(local_file.path)
     end
+  end
+
+  private
+
+  def set_attributes_from_file(attachment, local_file)
+    attachment.downloads = 0
+    attachment.set_file_size local_file unless attachment.filesize && attachment.filesize > 0
+    attachment.set_content_type local_file unless attachment.content_type.present?
+    attachment.set_digest local_file unless attachment.digest.present?
+  end
+
+  def save_attachment(attachment)
+    User.execute_as(attachment.author) do
+      attachment.save! if attachment.changed?
+    end
+  end
+
+  def journalize_container(attachment)
+    journable = attachment.container
+
+    return unless journable&.class&.journaled?
+
+    # Touching the journable will lead to the journal created next having its own timestamp.
+    # That timestamp will not adequately reflect the time the attachment was uploaded. This job
+    # right here might be executed way later than the time the attachment was uploaded. Ideally,
+    # the journals would be created bearing the time stamps of the attachment's created_at.
+    # This remains a TODO.
+    # But with the timestamp update in place as it is, at least the collapsing of aggregated journals
+    # from days before with the newly uploaded attachment is prevented.
+    journable.touch
+
+    Journals::CreateService
+      .new(journable, attachment.author)
+      .call
   end
 end
