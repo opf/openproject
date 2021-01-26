95ffb1569996b342229da44c26d598ecba6c296d
Benjamin Bädorf <b.baedorf@openproject.com>
2021-01-26 17:58:32 +0100

    Add API calls to summary component

--- frontend/src/app/modules/invite-user-modal/summary/summary.component.ts
+++ frontend/src/app/modules/invite-user-modal/summary/summary.component.ts
@@ -6,6 +6,7 @@ import {
   ElementRef,
 } from '@angular/core';
 import {I18nService} from "core-app/modules/common/i18n/i18n.service";
+import {APIV3Service} from "core-app/modules/api/api-v3.service";
 
 @Component({
   selector: 'op-ium-summary',
@@ -32,13 +33,39 @@ export class SummaryComponent {
   constructor(
     readonly I18n:I18nService,
     readonly elementRef:ElementRef,
+    readonly api: APIV3Service,
   ) {}
+
+  async invite() {
+    const principal = await (async () => {
+      if (this.principal.id) {
+        return this.principal;
+      }
+
+      switch (this.type) {
+        case 'user':
+          return this.api.users.create({
+            email: this.principal.name,
+            firstName: this.principal.email,
+            status: 'invited',
+          });
+        case 'group':
+          return this.api.groups.create({ name: this.principal.name });
+        case 'placeholder':
+          return this.api.placeholders.create({ name: this.principal.name });
+      }
+    })();
+
+    return this.api.memberships.create({
+      principal,
+      project: this.project,
+      roles: [this.role],
+    });
+  }
 
   async onSubmit($e:Event) {
     $e.preventDefault();
 
-    // save here
-
-    this.save.emit();
+    this.save.emit({ principal: this.principal });
   }
 }
