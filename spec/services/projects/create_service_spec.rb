38046fba5b91b4f28931c87a64a6e867f8c79511
Oliver Günther <mail@oliverguenther.de>
2021-02-01 13:53:08 +0100

    Rewrite Projects::CreateService with common example

--- spec/services/projects/create_service_spec.rb
+++ spec/services/projects/create_service_spec.rb
@@ -29,100 +29,28 @@
 #++
 
 require 'spec_helper'
+require 'services/base_services/behaves_like_create_service'
 
 describe Projects::CreateService, type: :model do
-  let(:user) { FactoryBot.build_stubbed(:user) }
-  let(:contract_class) do
-    double('contract_class', '<=': true)
-  end
-  let(:project_valid) { true }
-  let(:instance) do
-    described_class.new(user: user,
-                        contract_class: contract_class)
-  end
-  let(:call_attributes) { { name: 'Some name', identifier: 'Some identifier' } }
-  let(:set_attributes_success) do
-    true
-  end
-  let(:set_attributes_errors) do
-    double('set_attributes_errors')
-  end
-  let(:set_attributes_result) do
-    ServiceResult.new result: created_project,
-                      success: set_attributes_success,
-                      errors: set_attributes_errors
-  end
-  let!(:created_project) do
-    project = FactoryBot.build_stubbed(:project)
-
-    allow(Project)
-      .to receive(:new)
-      .and_return(project)
-
-    allow(project)
-      .to receive(:save)
-      .and_return(project_valid)
-
-    project
-  end
-  let!(:set_attributes_service) do
-    service = double('set_attributes_service_instance')
-
-    allow(Projects::SetAttributesService)
-      .to receive(:new)
-            .with(user: user,
-                  model: created_project,
-                  contract_class: contract_class,
-                  contract_options: {})
-            .and_return(service)
-
-    allow(service)
-      .to receive(:call)
-      .and_return(set_attributes_result)
-  end
-  let(:new_project_role) { FactoryBot.build_stubbed(:role) }
-
-  describe 'call' do
-    subject { instance.call(call_attributes) }
-
-    it 'is successful' do
-      expect(subject.success?).to be_truthy
-    end
-
-    it 'returns the result of the SetAttributesService' do
-      expect(subject)
-        .to eql set_attributes_result
-    end
-
-    it 'persists the project' do
-      expect(created_project)
-        .to receive(:save)
-        .and_return(project_valid)
-
-      subject
-    end
-
-    it 'creates a project' do
-      expect(subject.result)
-        .to eql created_project
-    end
+  it_behaves_like 'BaseServices create service' do
+    let(:new_project_role) { FactoryBot.build_stubbed(:role) }
 
     it 'adds the current user to the project' do
       allow(Role)
-        .to receive(:in_new_project)
+        .to(receive(:in_new_project))
         .and_return(new_project_role)
 
       create_member_instance = double('Members::CreateService instance')
 
       expect(Members::CreateService)
-        .to receive(:new)
+        .to(receive(:new))
         .with(user: user, contract_class: EmptyContract)
         .and_return(create_member_instance)
 
       expect(create_member_instance)
-        .to receive(:call)
+        .to(receive(:call))
         .with(principal: user,
-              project: instance_of(Project),
+              project: model_instance,
               roles: [new_project_role])
 
       subject
@@ -131,53 +59,13 @@ describe Projects::CreateService, type: :model do
     context 'current user is admin' do
       it 'does not add the user to the project' do
         allow(user)
-          .to receive(:admin?)
+          .to(receive(:admin?))
           .and_return(true)
 
-        expect(created_project)
-          .not_to receive(:add_member!)
+        expect(model_instance)
+          .not_to(receive(:add_member!))
 
         subject
-      end
-    end
-
-    context 'if the SetAttributeService is unsuccessful' do
-      let(:set_attributes_success) { false }
-
-      it 'is unsuccessful' do
-        expect(subject.success?).to be_falsey
-      end
-
-      it 'returns the result of the SetAttributesService' do
-        expect(subject)
-          .to eql set_attributes_result
-      end
-
-      it 'does not persist the changes' do
-        expect(created_project)
-          .to_not receive(:save)
-
-        subject
-      end
-
-      it "exposes the contract's errors" do
-        subject
-
-        expect(subject.errors).to eql set_attributes_errors
-      end
-    end
-
-    context 'when the project is invalid' do
-      let(:project_valid) { false }
-
-      it 'is unsuccessful' do
-        expect(subject.success?).to be_falsey
-      end
-
-      it "exposes the project's errors" do
-        subject
-
-        expect(subject.errors).to eql created_project.errors
       end
     end
   end
