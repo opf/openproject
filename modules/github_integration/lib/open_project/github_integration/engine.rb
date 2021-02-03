dd32efe339bf4fd29a765f1082f1191b3f171f0f
Philipp Tessenow <philipp@tessenow.org>
2021-02-03 19:34:20 +0100

    Fixed metadata and a typo in our GitHub Plugin
    I found these while reviewing the code. Probably doesn't hurt to fix
    even tiny things like these :)
    
    https://community.openproject.com/projects/openproject/work_packages/36150

--- modules/github_integration/lib/open_project/github_integration/engine.rb
+++ modules/github_integration/lib/open_project/github_integration/engine.rb
@@ -37,7 +37,7 @@ module OpenProject::GithubIntegration
     include OpenProject::Plugins::ActsAsOpEngine
 
     register 'openproject-github_integration',
-             :author_url => 'http://finn.de',
+             :author_url => 'https://openproject.com',
              bundled: true
 
 
