5299f0c69b80b16e282527d8ba37989ef3631e9c
Benjamin Bädorf <b.baedorf@openproject.com>
2021-02-10 15:07:06 +0100

    Improved code style

--- frontend/src/app/modules/modal/modal.module.ts
+++ frontend/src/app/modules/modal/modal.module.ts
@@ -5,18 +5,12 @@ import {OpModalWrapperAugmentService} from "./modal-wrapper-augment.service";
 import {OpModalHeaderComponent} from "./modal-header.component";
 
 @NgModule({
-  imports: [
-    OpenprojectCommonModule,
-  ],
-  exports: [
-    OpModalHeaderComponent,
-  ],
+  imports: [ OpenprojectCommonModule ],
+  exports: [ OpModalHeaderComponent ],
   providers: [
     OpModalService,
     OpModalWrapperAugmentService,
   ],
-  declarations: [
-    OpModalHeaderComponent,
-  ]
+  declarations: [ OpModalHeaderComponent ]
 })
 export class OpenprojectModalModule { }
