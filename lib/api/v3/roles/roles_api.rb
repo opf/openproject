5c112f39e15d194c449c48cb28dd3ea55a2d77ac
ulferts <jens.ulferts@googlemail.com>
2021-02-10 15:24:14 +0100

    fix role api permission checks
    The authorization on the individual get was handled to early (before instead of after_validation) so that the current user wasn`t set correctly yet. Additionally, there was not dedicated authorization check on the index get action

--- lib/api/v3/roles/roles_api.rb
+++ lib/api/v3/roles/roles_api.rb
@@ -32,13 +32,15 @@ module API
     module Roles
       class RolesAPI < ::API::OpenProjectAPI
         resources :roles do
+          after_validation do
+            authorize_any(%i[view_members manage_members], global: true)
+          end
+
           get &::API::V3::Utilities::Endpoints::Index.new(model: Role).mount
 
           route_param :id, type: Integer, desc: 'Role ID' do
-            before do
-              authorize_any(%i[view_members manage_members], global: true)
-
-              @role = Role.find(params[:id])
+            after_validation do
+              @role = Role.find(declared_params[:id])
             end
 
             get &::API::V3::Utilities::Endpoints::Show.new(model: Role).mount
