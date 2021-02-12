1931b236beeb82f7e3e64a80f49d829df1a862cc
Oliver Günther <mail@oliverguenther.de>
2021-02-12 10:14:02 +0100

    Fix group filter for placeholder users

--- app/models/queries/placeholder_users/filters/group_filter.rb
+++ app/models/queries/placeholder_users/filters/group_filter.rb
@@ -29,9 +29,21 @@
 #++
 
 class Queries::PlaceholderUsers::Filters::GroupFilter < Queries::PlaceholderUsers::Filters::PlaceholderUserFilter
+  include Queries::Filters::Shared::GroupFilter
+
   self.model = PlaceholderUser
 
   def human_name
     PlaceholderUser.human_attribute_name(name)
+  end
+
+  private
+
+  def group_subselect
+    PlaceholderUser.in_group(values).select(:id).to_sql
+  end
+
+  def any_group_subselect
+    PlaceholderUser.within_group([]).select(:id).to_sql
   end
 end
