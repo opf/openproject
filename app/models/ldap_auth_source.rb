c2c2b9c759edf55825ccbc97e91898cbf7e9d977
Oliver Günther <mail@oliverguenther.de>
2021-02-15 11:06:58 +0100

    [36206] Make force_no_page for ldap configurable
    https://community.openproject.com/wp/36206

--- app/models/ldap_auth_source.rb
+++ app/models/ldap_auth_source.rb
@@ -129,7 +129,7 @@ class LdapAuthSource < AuthSource
   def initialize_ldap_con(ldap_user, ldap_password)
     options = { host: host,
                 port: port,
-                force_no_page: true,
+                force_no_page: OpenProject::Configuration.ldap_force_no_page,
                 encryption: ldap_encryption }
     unless ldap_user.blank? && ldap_password.blank?
       options.merge!(auth: { method: :simple, username: ldap_user,
