33b161a287b5f9eea5c0272ee8602e8862b0fe8b
Aleix Suau <info@macrofonoestudio.es>
2021-02-16 08:47:21 +0100

    Docs improvement

--- frontend/src/app/modules/common/dynamic-bootstrap/dynamic-bootstrap.module.ts
+++ frontend/src/app/modules/common/dynamic-bootstrap/dynamic-bootstrap.module.ts
@@ -3,7 +3,7 @@
  * DynamicBootstrapComponent allows to dynamically render an HTML string into any HTML node
  * and bootstrap its Angular components and directives.
  *
- * Use example:
+ * @usageNotes
  * ```
  *  <op-dynamic-bootstrap [HTML]="<macro data-id="1" data-detailed="false" title="Macro nº1"></macro>">
  *  </op-dynamic-bootstrap>
