8b342f36740cf3c47750c382d54557f7381c3d53
Aleix Suau <info@macrofonoestudio.es>
2021-02-16 09:13:37 +0100

    Docs improvement

--- frontend/src/app/modules/common/dynamic-bootstrap/component/dynamic-bootstrap/dynamic-bootstrap.component.ts
+++ frontend/src/app/modules/common/dynamic-bootstrap/component/dynamic-bootstrap/dynamic-bootstrap.component.ts
@@ -1,17 +1,18 @@
-import {ApplicationRef, Component, ElementRef, Input} from '@angular/core';
-import {DomSanitizer, SafeHtml} from "@angular/platform-browser";
-import {DynamicBootstrapper} from "core-app/globals/dynamic-bootstrapper";
 /**
- * Allows to dynamically render an HTML string into any HTML node, bootstrapping
- * dynamically all its Angular components and directives.
+ * Allows to dynamically render an HTML string into any HTML node, dynamically
+ * bootstrapping all its Angular components and directives.
  *
- * @example
  * ```
  *  <op-dynamic-bootstrap HTML="<macro data-id="1" data-detailed="false" title="Macro nº1"></macro>">
  *  </op-dynamic-bootstrap>
  * ```
+ * @module
  * @public
  */
+import {ApplicationRef, Component, ElementRef, Input} from '@angular/core';
+import {DomSanitizer, SafeHtml} from "@angular/platform-browser";
+import {DynamicBootstrapper} from "core-app/globals/dynamic-bootstrapper";
+
 @Component({
   selector: 'op-dynamic-bootstrap',
   templateUrl: './dynamic-bootstrap.component.html',
