af35670694de0fb4da588520c8fe4cd77ef62243
Oliver Günther <mail@oliverguenther.de>
2021-02-16 21:32:30 +0100

    Make placeholder users EE (#9003)
    * Add global permission to manage (but not delete) placeholders
    
    https://community.openproject.com/work_packages/35508
    
    * Show EE upsale and disable creation of placeholders without it
    
    * Review feedback

--- spec/contracts/placeholder_users/create_contract_spec.rb
+++ spec/contracts/placeholder_users/create_contract_spec.rb
@@ -34,8 +34,27 @@ require_relative 'shared_contract_examples'
 describe PlaceholderUsers::CreateContract do
   include_context 'ModelContract shared context'
 
-  it_behaves_like 'placeholder user contract' do
-    let(:placeholder_user) { PlaceholderUser.new(name: placeholder_user_name) }
+  context 'without enterprise' do
+    let(:placeholder_user) { PlaceholderUser.new(name: 'foo') }
     let(:contract) { described_class.new(placeholder_user, current_user) }
+
+    context 'when user with global permission' do
+      let(:current_user) { FactoryBot.create(:user, global_permission: %i[add_placeholder_user]) }
+
+      it_behaves_like 'contract is invalid', base: :error_enterprise_only
+    end
+
+    context 'when user with admin permission' do
+      let(:current_user) { FactoryBot.build_stubbed(:admin) }
+
+      it_behaves_like 'contract is invalid', base: :error_enterprise_only
+    end
+  end
+
+  context 'with enterprise', with_ee: %i[placeholder_users] do
+    it_behaves_like 'placeholder user contract' do
+      let(:placeholder_user) { PlaceholderUser.new(name: placeholder_user_name) }
+      let(:contract) { described_class.new(placeholder_user, current_user) }
+    end
   end
 end
