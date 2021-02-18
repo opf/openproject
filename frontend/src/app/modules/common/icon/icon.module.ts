a387196ab639e38a91c4c3dd71e987cf13475e33
Aleix Suau <info@macrofonoestudio.es>
2021-02-18 18:24:59 +0100

    InviteUserButtonComponent in

---
+++ frontend/src/app/modules/common/icon/icon.module.ts
@@ -0,0 +1,18 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import {OpIcon} from "core-app/modules/common/icon/op-icon";
+
+
+
+@NgModule({
+  declarations: [
+    OpIcon
+  ],
+  imports: [
+    CommonModule
+  ],
+  exports: [
+    OpIcon
+  ],
+})
+export class OpIconModule { }
