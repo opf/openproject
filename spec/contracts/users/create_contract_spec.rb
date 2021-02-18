6caaa9e657fa8ea21d1b980557c95260201c29b7
Wieland Lindenthal <w.lindenthal@forkmerge.com>
2021-02-18 13:44:26 +0100

    Refactoring: Create shared examples for user contracts

--- spec/contracts/users/create_contract_spec.rb
+++ spec/contracts/users/create_contract_spec.rb
@@ -30,81 +30,23 @@
 
 require 'spec_helper'
 require 'contracts/shared/model_contract_shared_context'
+require_relative 'shared_contract_examples'
 
 describe Users::CreateContract do
   include_context 'ModelContract shared context'
 
-  let(:user) { FactoryBot.build_stubbed(:user) }
-  let(:contract) { described_class.new(user, current_user) }
-
-  context 'when admin' do
-    let(:current_user) { FactoryBot.build_stubbed(:admin) }
-
-    it_behaves_like 'contract is valid'
-
-    describe 'requires a password set when active' do
-      before do
-        user.password = nil
-        user.activate
-      end
-
-      it_behaves_like 'contract is invalid', password: :blank
-
-      context 'when password is set' do
-        before do
-          user.password = user.password_confirmation = 'password!password!'
-        end
-
-        it_behaves_like 'contract is valid'
-      end
+  it_behaves_like 'user contract' do
+    let(:user) { User.new(attributes) }
+    let(:contract) { described_class.new(user, current_user) }
+    let(:attributes) do
+      {
+        firstname: user_firstname,
+        lastname: user_lastname,
+        login: user_login,
+        mail: user_mail,
+        password: user_password,
+        password_confirmation: user_password_confirmation
+      }
     end
-  end
-
-  context 'when global user' do
-    shared_let(:current_user) { FactoryBot.create :user, global_permission: :manage_user }
-
-    describe 'can invite user' do
-      before do
-        user.password = user.password_confirmation = nil
-        user.mail = 'foo@example.com'
-        user.invite
-      end
-
-      it_behaves_like 'contract is valid'
-    end
-
-    describe 'cannot set the password' do
-      before do
-        user.password = user.password_confirmation = 'password!password!'
-      end
-
-      it_behaves_like 'contract is invalid', password: :error_readonly
-    end
-
-    describe 'can set the auth_source' do
-      let!(:auth_source) { FactoryBot.create :auth_source }
-
-      before do
-        user.password = user.password_confirmation = nil
-        user.auth_source = auth_source
-      end
-
-      it_behaves_like 'contract is valid'
-    end
-
-    describe 'cannot set the identity url' do
-      before do
-        user.identity_url = 'saml:123412foo'
-      end
-
-      it_behaves_like 'contract is invalid', identity_url: :error_readonly
-
-    end
-  end
-
-  context 'when unauthorized user' do
-    let(:current_user) { FactoryBot.build_stubbed(:user) }
-
-    it_behaves_like 'contract user is unauthorized'
   end
 end
