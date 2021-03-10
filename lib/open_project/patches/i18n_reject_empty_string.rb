c7dad28506665a9e26485f635e5a5f42fc036a1f
ulferts <jens.ulferts@googlemail.com>
2021-03-10 11:02:27 +0100

    fix patch check

--- lib/open_project/patches/i18n_reject_empty_string.rb
+++ lib/open_project/patches/i18n_reject_empty_string.rb
@@ -60,6 +60,6 @@ module OpenProject
   end
 end
 
-OpenProject::Patches.patch_gem_version 'i18n', '1.8.8' do
+OpenProject::Patches.patch_gem_version 'i18n', '1.8.9' do
   I18n.backend.singleton_class.prepend OpenProject::Patches::I18nRejectEmptyString
 end
