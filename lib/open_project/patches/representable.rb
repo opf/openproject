232c0ef0c740c45d805a609a0dea23c7f97fb101
Oliver Günther <mail@oliverguenther.de>
2021-03-15 14:31:38 +0100

    Add patch to allow using our custom setter in case of nil values
    https://github.com/trailblazer/representable/issues/234

--- lib/open_project/patches/representable.rb
+++ lib/open_project/patches/representable.rb
@@ -27,29 +27,55 @@
 #++
 
 require 'representable'
+require 'open_project/patches'
 
 module OpenProject::Patches::Representable
-  def self.included(base)
-    base.class_eval do
-      def self.as_strategy=(strategy)
-        raise 'The :as_strategy option should respond to #call?' unless strategy.respond_to?(:call)
+  module DecoratorPatch
+    def self.included(base)
+      base.class_eval do
+        def self.as_strategy=(strategy)
+          raise 'The :as_strategy option should respond to #call?' unless strategy.respond_to?(:call)
 
-        @as_strategy = strategy
+          @as_strategy = strategy
+        end
+
+        def self.as_strategy
+          @as_strategy
+        end
+
+        def self.property(name, options = {}, &block)
+          options = { as: as_strategy.call(name.to_s) }.merge(options) if as_strategy
+
+          super
+        end
       end
+    end
+  end
 
-      def self.as_strategy
-        @as_strategy
-      end
+  module OverwriteOnNilPatch
+    def self.included(base)
+      base.class_eval do
+        unless const_defined?(:OverwriteOnNil)
+          raise "Missing OverwriteOnNil on Representable gem, check if this patch still applies"
+        end
 
-      def self.property(name, options = {}, &block)
-        options = { as: as_strategy.call(name.to_s) }.merge(options) if as_strategy
+        remove_const :OverwriteOnNil
 
-        super
+        ##
+        # Redefine OverwriteOnNil to ensure we use the correct setter
+        # https://github.com/trailblazer/representable/issues/234
+        const_set(:OverwriteOnNil, ->(input, *) { input })
       end
     end
   end
 end
 
-unless Representable::Decorator.included_modules.include?(OpenProject::Patches::Representable)
-  Representable::Decorator.include OpenProject::Patches::Representable
+OpenProject::Patches.patch_gem_version 'representable', '3.0.4' do
+  unless Representable::Decorator.included_modules.include?(OpenProject::Patches::Representable::DecoratorPatch)
+    Representable::Decorator.include OpenProject::Patches::Representable::DecoratorPatch
+  end
+
+  unless Representable::Decorator.included_modules.include?(OpenProject::Patches::Representable::OverwriteOnNilPatch)
+    Representable.include OpenProject::Patches::Representable::OverwriteOnNilPatch
+  end
 end
