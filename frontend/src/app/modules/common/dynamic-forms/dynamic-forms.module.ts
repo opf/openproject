b055dbb118fb27b015438b0e1aca76dfa7adae06
Aleix Suau <info@macrofonoestudio.es>
2021-03-23 10:04:33 +0100

    Working with formattable

--- frontend/src/app/modules/common/dynamic-forms/dynamic-forms.module.ts
+++ frontend/src/app/modules/common/dynamic-forms/dynamic-forms.module.ts
@@ -19,6 +19,10 @@ import { NgOptionHighlightModule } from "@ng-select/ng-option-highlight";
 import { BooleanInputComponent } from './components/inputs/boolean-input/boolean-input.component';
 import { DateInputComponent } from './components/inputs/date-input/date-input.component';
 import { DatePickerAdapterComponent } from './components/inputs/date-input/components/date-picker-adapter/date-picker-adapter.component';
+import { FormattableTextareaInputComponent } from './components/inputs/formattable-textarea-input/formattable-textarea-input.component';
+import { OpenprojectEditorModule } from "core-app/modules/editor/openproject-editor.module";
+import { OpenprojectFieldsModule } from "core-app/modules/fields/openproject-fields.module";
+import { FormattableControlComponent } from './components/inputs/formattable-textarea-input/components/formattable-control/formattable-control.component';
 
 @NgModule({
   imports: [
@@ -32,11 +36,15 @@ import { DatePickerAdapterComponent } from './components/inputs/date-input/compo
         { name: 'selectInput', component: SelectInputComponent },
         { name: 'booleanInput', component: BooleanInputComponent },
         { name: 'dateInput', component: DateInputComponent },
+        { name: 'formattableInput', component: FormattableTextareaInputComponent },
       ],
     }),
     HttpClientModule,
     NgSelectModule,
     NgOptionHighlightModule,
+    OpenprojectEditorModule,
+    // TODO: Import only necessary fields (EditFieldControlsComponent)
+    OpenprojectFieldsModule,
   ],
   declarations: [
     OpSelectComponent,
@@ -51,6 +59,8 @@ import { DatePickerAdapterComponent } from './components/inputs/date-input/compo
     BooleanInputComponent,
     DateInputComponent,
     DatePickerAdapterComponent,
+    FormattableTextareaInputComponent,
+    FormattableControlComponent,
   ],
   providers: [
     DynamicFormService,
