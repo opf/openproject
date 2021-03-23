90c01393e16faa081410981c13fa6b8bcfec301b
Benjamin Bädorf <b.baedorf@openproject.com>
2021-03-23 11:56:01 +0100

    Add avatar border

--- lib/redmine/menu_manager/top_menu_helper.rb
+++ lib/redmine/menu_manager/top_menu_helper.rb
@@ -87,7 +87,7 @@ module Redmine::MenuManager::TopMenuHelper
   end
 
   def render_user_drop_down(items)
-    avatar = avatar(User.current)
+    avatar = avatar(User.current, class: 'op-app-header--avatar')
     render_menu_dropdown_with_items(
       label: avatar.presence || '',
       label_options: {
