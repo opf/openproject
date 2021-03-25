cf8473a0cc32c89c8195be0e85a4980ad767be67
Oliver Günther <mail@oliverguenther.de>
2021-03-25 13:29:26 +0100

    Add spec for copying multi-value cf

--- spec/factories/custom_field_factory.rb
+++ spec/factories/custom_field_factory.rb
@@ -49,6 +49,7 @@ FactoryBot.define do
 
       factory :list_project_custom_field do
         sequence(:name) { |n| "List project custom field #{n}" }
+        multi_value { false }
         field_format { 'list' }
         possible_values { ['A', 'B', 'C', 'D', 'E', 'F', 'G'] }
       end
