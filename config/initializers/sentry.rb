41bfc5f9046c5af323f887a4a87ddaa165e4352a
Oliver Günther <mail@oliverguenther.de>
2021-03-29 20:23:21 +0200

    Manually insert the sentry middlewares

--- config/initializers/sentry.rb
+++ config/initializers/sentry.rb
@@ -3,13 +3,22 @@ if OpenProject::Logging::SentryLogger.enabled?
   require "sentry-rails"
   require "sentry-delayed_job"
 
+  # We need to manually load the sentry initializer
+  # as we're dynamically loading it
+  # https://github.com/getsentry/sentry-ruby/blob/master/sentry-rails/lib/sentry/rails/railtie.rb#L8-L13
+  OpenProject::Application.configure do |app|
+    # need to be placed at first to capture as many errors as possible
+    app.config.middleware.insert 0, Sentry::Rails::CaptureExceptions
+    # need to be placed at last to smuggle app exceptions via env
+    app.config.middleware.use(Sentry::Rails::RescuedExceptionInterceptor)
+  end
+
   Sentry.init do |config|
     config.dsn = OpenProject::Logging::SentryLogger.sentry_dsn
     config.breadcrumbs_logger = [:active_support_logger]
 
     # Submit events as delayed job
-    # TODO perform_later
-    config.async = lambda { |event, hint| ::SentryJob.perform_now(event, hint) }
+    config.async = lambda { |event, hint| ::SentryJob.perform_later(event, hint) }
 
     # Cleanup backtrace
     config.backtrace_cleanup_callback = lambda do |backtrace|
