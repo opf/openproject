ab999b245907bed2fe4a14d4e74156a2858746e0
Aleix Suau <info@macrofonoestudio.es>
2021-04-01 10:25:20 +0200

    Clean up

--- frontend/src/app/modules/common/dynamic-forms/components/inputs/date-input/date-input.component.ts
+++ frontend/src/app/modules/common/dynamic-forms/components/inputs/date-input/date-input.component.ts
@@ -2,7 +2,7 @@ import { Component } from '@angular/core';
 import { FieldType } from "@ngx-formly/core";
 
 @Component({
-  selector: 'app-date-input',
+  selector: 'op-date-input',
   templateUrl: './date-input.component.html',
   styleUrls: ['./date-input.component.scss'],
 })
