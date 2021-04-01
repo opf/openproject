0d71772c373d99d4897f3f937ad9d844c2a51a0a
Oliver Günther <mail@oliverguenther.de>
2021-04-01 14:57:28 +0200

    User SqlBypass sessions to avoid ActiveRecord for session storage (#9142)
    * Use SqlBypass sessions to avoid ActiveRecord for session storage
    
    * Set default host if Capybara.app_host does not exist

--- app/models/sessions/active_record.rb
+++ app/models/sessions/active_record.rb
@@ -27,35 +27,29 @@
 #
 # See docs/COPYRIGHT.rdoc for more details.
 #++
-require_relative './base_service'
 
+##
+# An AR helper class to access sessions, but not create them.
+# You can still use AR methods to delete records however.
 module Sessions
-  class InitializeSessionService < BaseService
-    class << self
-      ##
-      # Initializes a new session for the given user.
-      # This services provides very little for what it is called,
-      # mainly caused due to the many ways a user can login.
-      def call(user, session)
-        session[:user_id] = user.id
-        session[:updated_at] = Time.now
+  class ActiveRecord < ::ApplicationRecord
+    self.table_name = 'sessions'
 
-        if drop_old_sessions?
-          Rails.logger.info { "Deleting all other sessions for #{user}." }
-          ::Sessions::ActiveRecord.for_user(user).delete_all
-        end
+    scope :for_user, ->(user) do
+      user_id = user.is_a?(User) ? user.id : user.to_i
 
-        ServiceResult.new(success: true, result: session)
-      end
+      where(user_id: user_id)
+    end
 
-      private
+    scope :non_user, -> do
+      where(user_id: nil)
+    end
 
-      ##
-      # We can only drop old sessions if they're stored in the database
-      # and enabled by configuration.
-      def drop_old_sessions?
-        active_record_sessions? && OpenProject::Configuration.drop_old_sessions_on_login?
-      end
+    ##
+    # Mark all records as readonly so they cannot
+    # modify the database
+    def readonly?
+      true
     end
   end
 end
