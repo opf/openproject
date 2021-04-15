bdb84a06372893829f6ada33a4f4d9f8bc45e7ae
Benjamin Bädorf <b.baedorf@openproject.com>
2021-04-15 15:14:15 +0200

    Add basic nix development shell

---
+++ shell.nix
@@ -0,0 +1,38 @@
+{ pkgs ? import <nixpkgs> {} }:
+with pkgs;
+let 
+  op-get-routes = writeShellScriptBin "op-get-routes" ''
+    ${docker-compose}/bin/docker-compose exec backend rails runner 'API::Root.routes.each do |api|
+      method = api.request_method.ljust(10)
+      path = api.path
+      puts "#{method} #{path}"
+    end'
+  '';
+  op-get-test-failures = writeShellScriptBin "op-get-test-failures" ''
+    PASTED="$(${wl-clipboard}/bin/wl-paste)"
+    URL="$${1:-$PASTED}"
+
+    echo "LOADING FROM $URL"
+    exec ${curl}/bin/curl "$URL" | grep 'rspec \.\/' | cut -f3 -d' ' | paste -s -d ' '
+  '';
+in
+  mkShell {
+    # nativeBuildInputs is usually what you want -- tools you need to run
+    nativeBuildInputs = [
+      buildPackages.ruby_2_7
+      postgresql
+      nodejs
+      google-chrome
+      tightvnc
+      docker-compose
+      bundix
+
+      op-get-routes
+      op-get-test-failures
+    ];
+
+    CHROME_BINARY = "${chromium}/bin/chromium";
+    OPENPROJECT_TESTING_NO_HEADLESS = "1";
+    OPENPROJECT_TESTING_AUTO_DEVTOOLS = "1";
+}
+
