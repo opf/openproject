3f3e831f6a8533c71e1f4c620e2d17109a05bbf3
Benjamin Bädorf <hello@benjaminbaedorf.eu>
2021-04-20 11:08:12 +0000

    Add any name attribute filter to principal resource collection (#9170)

--- app/models/queries/principals/filters/any_name_attribute_filter.rb
+++ app/models/queries/principals/filters/any_name_attribute_filter.rb
@@ -28,18 +28,6 @@
 # See docs/COPYRIGHT.rdoc for more details.
 #++
 
-module Queries::Principals
-  register = ::Queries::Register
-  filters = ::Queries::Principals::Filters
-  query = ::Queries::Principals::PrincipalQuery
-  orders = Queries::Principals::Orders
-
-  register.filter query, filters::TypeFilter
-  register.filter query, filters::MemberFilter
-  register.filter query, filters::StatusFilter
-  register.filter query, filters::NameFilter
-  register.filter query, filters::AnyNameAttributeFilter
-  register.filter query, filters::IdFilter
-
-  register.order query, orders::NameOrder
+class Queries::Principals::Filters::AnyNameAttributeFilter < Queries::Principals::Filters::NameFilter
+  include Queries::Filters::Shared::AnyUserNameAttributeFilter
 end
