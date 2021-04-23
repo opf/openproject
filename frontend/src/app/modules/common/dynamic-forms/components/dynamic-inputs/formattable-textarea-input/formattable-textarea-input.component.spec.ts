784a0dbf5e6902dcb2460cddad73fb9f1808e6c7
Aleix Suau <info@macrofonoestudio.es>
2021-04-23 13:28:56 +0200

    FormattableTextareaInputComponent tests

--- frontend/src/app/modules/common/dynamic-forms/components/dynamic-inputs/formattable-textarea-input/formattable-textarea-input.component.spec.ts
+++ frontend/src/app/modules/common/dynamic-forms/components/dynamic-inputs/formattable-textarea-input/formattable-textarea-input.component.spec.ts
@@ -1,25 +1,52 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { fakeAsync, flush, tick } from '@angular/core/testing';
+import {
+  createDynamicInputFixture,
+} from "core-app/modules/common/dynamic-forms/spec/helpers";
+import { By } from "@angular/platform-browser";
+// @ts-ignore
+import(/* webpackChunkName: "ckeditor" */ 'core-vendor/ckeditor/ckeditor.js');
 
-import { FormattableTextareaInputComponent } from './formattable-textarea-input.component';
+fdescribe('FormattableTextareaInputComponent', () => {
+  it('should load the field', fakeAsync(() => {
+    const fieldsConfig = [
+      {
+        "type": "formattableInput" as "formattableInput",
+        "className": "op-form--field inline-edit--field",
+        "key": "testControl",
+        "templateOptions": {
+          "required": true,
+          "label": "testControl",
+          "type": "text",
+          "placeholder": "",
+          "disabled": false,
+          "bindLabel": 'name',
+          "bindValue": 'value',
+          "noWrapLabel": true
+        },
+      }
+    ];
+    const formModel = {
+      testControl: {
+        html: '<p>tesValue</p>',
+        raw: 'tesValue'
+      },
+    };
+    const testModel = {
+      initialValue: formModel.testControl,
+      changedValue: 'testValue2',
+    };
+    const fixture = createDynamicInputFixture(fieldsConfig, formModel);
+    const dynamicInput = fixture.debugElement.query(By.css('.document-editor__editable-container')).nativeElement;
+    const dynamicControl = fixture.componentInstance.dynamicControl.field.formControl;
+    const dynamicDebugElement = fixture.debugElement.query(By.css('op-formattable-control'));
+    const dynamicElement = dynamicDebugElement.nativeElement;
 
-xdescribe('FormattableTextareaInputComponent', () => {
-  let component: FormattableTextareaInputComponent;
-  let fixture: ComponentFixture<FormattableTextareaInputComponent>;
-
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      declarations: [ FormattableTextareaInputComponent ]
-    })
-    .compileComponents();
-  });
-
-  beforeEach(() => {
-    fixture = TestBed.createComponent(FormattableTextareaInputComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
-
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
+    // Test ControlValueAccessor
+    // Write Value
+    expect(dynamicControl.value).toEqual(testModel.initialValue);
+    expect(dynamicElement.classList.contains('ng-untouched')).toBeTrue();
+    expect(dynamicElement.classList.contains('ng-valid')).toBeTrue();
+    expect(dynamicElement.classList.contains('ng-pristine')).toBeTrue();
+  }));
 });
+
