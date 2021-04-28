8cd5f6a8721c421f0eef97b8136e8b84680d016d
Oliver Günther <mail@oliverguenther.de>
2021-04-28 11:08:34 +0200

    Fix invalid selector

--- spec/models/wiki_page_spec.rb
+++ spec/models/wiki_page_spec.rb
@@ -120,7 +120,7 @@ describe WikiPage, type: :model do
 
       it 'ensures that there is still a wiki menu item named like the wiki start page' do
         expect(wiki.wiki_menu_items).to be_one
-        expect(wiki.wiki_menu_items.first.name).to WikiPage.slug(eq(wiki.start_page))
+        expect(wiki.wiki_menu_items.first.name).to eq WikiPage.slug(wiki.start_page)
       end
     end
   end
