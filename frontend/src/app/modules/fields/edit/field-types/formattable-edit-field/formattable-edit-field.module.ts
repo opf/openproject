30e3b8f35400457dbd6c2977daed5a824b437a2e
Oliver Günther <mail@oliverguenther.de>
2021-04-29 16:28:03 +0200

    Add separate module for field controls
    Otherwise it will not be found in the FormattableEditField component

--- frontend/src/app/modules/fields/edit/field-types/formattable-edit-field/formattable-edit-field.module.ts
+++ frontend/src/app/modules/fields/edit/field-types/formattable-edit-field/formattable-edit-field.module.ts
@@ -2,6 +2,8 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormattableEditFieldComponent } from "core-app/modules/fields/edit/field-types/formattable-edit-field/formattable-edit-field.component";
 import { OpenprojectEditorModule } from "core-app/modules/editor/openproject-editor.module";
+import { EditFieldControlsComponent } from "core-app/modules/fields/edit/field-controls/edit-field-controls.component";
+import { EditFieldControlsModule } from "core-app/modules/fields/edit/field-controls/edit-field-controls.module";
 
 
 @NgModule({
@@ -11,6 +13,7 @@ import { OpenprojectEditorModule } from "core-app/modules/editor/openproject-edi
   imports: [
     CommonModule,
     OpenprojectEditorModule,
+    EditFieldControlsModule,
   ],
   exports: [
     FormattableEditFieldComponent,
