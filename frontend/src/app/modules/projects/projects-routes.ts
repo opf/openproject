4242272c981133f1df6d8a09e42b540d8ecb5ebf
Oliver Günther <mail@oliverguenther.de>
2021-04-30 20:10:55 +0200

    Add new project component

--- frontend/src/app/modules/projects/projects-routes.ts
+++ frontend/src/app/modules/projects/projects-routes.ts
@@ -1,5 +1,6 @@
 import { Ng2StateDeclaration, UIRouter } from "@uirouter/angular";
 import { ProjectsComponent } from "core-app/modules/projects/components/projects/projects.component";
+import { NewProjectComponent } from "core-app/modules/projects/components/new-project/new-project.component";
 
 export const PROJECTS_ROUTES:Ng2StateDeclaration[] = [
   {
@@ -7,6 +8,11 @@ export const PROJECTS_ROUTES:Ng2StateDeclaration[] = [
     url: '/settings/generic/',
     parent: 'root',
     component: ProjectsComponent,
+  },
+  {
+    name: 'new_project',
+    url: '/projects/new',
+    component: NewProjectComponent,
   },
 ];
 
