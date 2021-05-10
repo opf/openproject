c151ce25d844492f609269895bff25f09bede530
Oliver Günther <mail@oliverguenther.de>
2021-05-10 14:32:07 +0200

    Fix selector

--- spec/features/work_packages/details/relations/hierarchy_custom_fields_spec.rb
+++ spec/features/work_packages/details/relations/hierarchy_custom_fields_spec.rb
@@ -71,7 +71,7 @@ describe 'creating a child directly after the wp itself was created', js: true d
 
     # Expect CF value to be still visible
     wp_page.expect_and_dismiss_notification(message: 'Successful creation.')
-    expect(wp_page).to have_selector('wp-relations-count', text: 1)
+    expect(wp_page).to have_selector('op-tab-count', text: 1)
     wp_page.expect_attributes "customField#{custom_field.id}": '42'
   end
 end
