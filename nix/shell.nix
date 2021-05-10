9285b145152ed9cd281fd305f41cf7499b18f5e9
Benjamin Bädorf <b.baedorf@openproject.com>
2021-05-10 10:29:48 +0200

    Move nix stuff to subdirectory

--- nix/shell.nix
+++ nix/shell.nix
@@ -1,20 +1,15 @@
 { pkgs ? import <nixpkgs> {} }:
 with pkgs;
 let 
-  op-get-routes = writeShellScriptBin "op-get-routes" ''
-    ${docker-compose}/bin/docker-compose exec backend rails runner 'API::Root.routes.each do |api|
-      method = api.request_method.ljust(10)
-      path = api.path
-      puts "#{method} #{path}"
-    end'
-  '';
   op-get-test-failures = writeShellScriptBin "op-get-test-failures" ''
-    PASTED="$(${wl-clipboard}/bin/wl-paste)"
-    URL="$${1:-$PASTED}"
-
-    echo "LOADING FROM $URL"
-    exec ${curl}/bin/curl "$URL" | grep 'rspec \.\/' | cut -f3 -d' ' | paste -s -d ' '
+    exec ${curl}/bin/curl "$$1" | grep 'rspec \.\/' | cut -f3 -d' ' | paste -s -d ' '
   '';
+
+  gems = bundlerEnv {
+    name = "openproject-dev";
+    inherit ruby;
+    gemdir = ./.;
+  };
 in
   mkShell {
     # nativeBuildInputs is usually what you want -- tools you need to run
