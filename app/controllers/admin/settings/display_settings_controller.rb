a0361d167f3ca0ba9f1b911b7a74a9ed578ac495
Oliver Günther <mail@oliverguenther.de>
2021-05-18 21:03:53 +0200

    [37197] Use setting names in error messages
    This avoids wrongly translated content
    
    https://community.openproject.org/work_packages/37197

--- app/controllers/admin/settings/display_settings_controller.rb
+++ app/controllers/admin/settings/display_settings_controller.rb
@@ -52,7 +52,11 @@ module Admin::Settings
       start_of_year = params[:settings][:first_week_of_year]
 
       if start_of_week.present? ^ start_of_year.present?
-        flash[:error] = I18n.t('settings.display.first_date_of_week_and_year_present')
+        flash[:error] = I18n.t(
+          'settings.display.first_date_of_week_and_year_set',
+          first_week_setting_name: I18n.t(:setting_first_week_of_year),
+          day_of_week_setting_name: I18n.t(:setting_start_of_week)
+        )
         redirect_to action: :show
       end
     end
