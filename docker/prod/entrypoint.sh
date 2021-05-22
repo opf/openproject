dd2e630c26d8c057e3b65e79e8604f801d7eb40f
Cyril Rohr <cyril.rohr@gmail.com>
2021-05-22 14:03:17 +0000

    Add script to auto-upgrade postgres database for all-in-one dockerfile

--- docker/prod/entrypoint.sh
+++ docker/prod/entrypoint.sh
@@ -25,6 +25,7 @@ if [ "$(id -u)" = '0' ]; then
 	export PGBIN="/usr/lib/postgresql/$PGVERSION/bin"
 	export PGCONF_FILE="/etc/postgresql/$PGVERSION/main/postgresql.conf"
 	echo "-----> Setting PGVERSION=$PGVERSION PGBIN=$PGBIN PGCONF_FILE=$PGCONF_FILE"
+	export PATH="$PGBIN:$PATH"
 
 	mkdir -p $APP_DATA_PATH/{files,git,svn}
 	chown -R $APP_USER:$APP_USER $APP_DATA_PATH
