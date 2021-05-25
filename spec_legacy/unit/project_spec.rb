c9a80b86530d9fb9657f7bf5f7632038129177ef
Oliver Günther <mail@oliverguenther.de>
2021-05-25 12:07:44 +0200

    [#37299] Remove validation for project identifier presence (#9296)
    [OP#37299](https://community.openproject.org/wp/37299)

--- spec_legacy/unit/project_spec.rb
+++ spec_legacy/unit/project_spec.rb
@@ -39,9 +39,6 @@ describe Project, type: :model do
   end
 
   it { is_expected.to validate_presence_of :name }
-  it { is_expected.to validate_presence_of :identifier }
-
-  it { is_expected.to validate_uniqueness_of :identifier }
 
   context 'associations' do
     it { is_expected.to have_many :members                                       }
