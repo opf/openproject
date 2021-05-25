efe4bf3289da2bad9391e72d7bbb1a703a879558
Oliver Günther <mail@oliverguenther.de>
2021-05-25 16:16:21 +0200

    Make Grids::DeleteContract base on the global delete contract (#9316)
    Otherwise it is impossible to delete grids that are invalid

--- modules/grids/spec/contracts/grids/delete_contract_spec.rb
+++ modules/grids/spec/contracts/grids/delete_contract_spec.rb
@@ -28,10 +28,48 @@
 # See docs/COPYRIGHT.rdoc for more details.
 #++
 
-require 'grids/base_contract'
+require 'spec_helper'
 
-module Grids
-  class DeleteContract < ::DeleteContract
-    delete_permission -> { model.user_deletable? && Grids::Configuration.writable?(model, user) }
+describe Grids::DeleteContract do
+  let(:user) { FactoryBot.build_stubbed(:user) }
+  let(:grid) do
+    FactoryBot.build_stubbed(:grid)
+  end
+
+  let(:instance) { described_class.new(grid, user) }
+
+  before do
+    allow(Grids::Configuration)
+      .to receive(:writable?)
+            .and_return(writable)
+
+    allow(grid).to receive(:user_deletable?).and_return(user_deletable)
+  end
+
+  context 'when writable' do
+    let(:writable) { true }
+    let(:user_deletable) { true }
+
+    it 'deletes the grid even if no valid widgets' do
+      expect(instance.validate).to be_truthy
+    end
+  end
+
+  context 'when not writable' do
+    let(:writable) { false }
+    let(:user_deletable) { true }
+
+    it 'deletes the grid even if not valid' do
+      expect(instance.validate).to be_falsey
+    end
+  end
+
+  context 'when not deletable' do
+    let(:writable) { true }
+    let(:user_deletable) { false }
+
+    it 'deletes the grid even if not valid' do
+      expect(instance.validate).to be_falsey
+    end
   end
 end
