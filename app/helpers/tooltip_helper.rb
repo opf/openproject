fc090ac02466ce496ccbcc977cda6fb8b226f455
Oliver Günther <mail@oliverguenther.de>
2021-05-31 16:31:41 +0200

    [37136] Disable delete button and check for deletion (#9338)
    https://community.openproject.org/wp/37136

--- app/helpers/tooltip_helper.rb
+++ app/helpers/tooltip_helper.rb
@@ -2,13 +2,13 @@
 
 #-- copyright
 # OpenProject is an open source project management software.
-# Copyright (C) 2012-2020 the OpenProject GmbH
+# Copyright (C) 2012-2021 the OpenProject GmbH
 #
 # This program is free software; you can redistribute it and/or
 # modify it under the terms of the GNU General Public License version 3.
 #
 # OpenProject is a fork of ChiliProject, which is a fork of Redmine. The copyright follows:
-# Copyright (C) 2006-2017 Jean-Philippe Lang
+# Copyright (C) 2006-2013 Jean-Philippe Lang
 # Copyright (C) 2010-2013 the ChiliProject Team
 #
 # This program is free software; you can redistribute it and/or
@@ -28,37 +28,21 @@
 # See docs/COPYRIGHT.rdoc for more details.
 #++
 
-module PlaceholderUsers
-  class RowCell < ::RowCell
-    include AvatarHelper
-    include UsersHelper
-    include PlaceholderUsersHelper
-    include TooltipHelper
+module TooltipHelper
+  include OpenProject::FormTagHelper
 
-    def placeholder_user
-      model
-    end
-
-    def name
-      link_to h(placeholder_user.name), edit_placeholder_user_path(placeholder_user)
-    end
-
-    def button_links
-      [delete_link].compact
-    end
-
-    def delete_link
-      if can_delete_placeholder_user?(placeholder_user, User.current)
-        link_to deletion_info_placeholder_user_path(placeholder_user) do
-          tooltip_tag I18n.t('placeholder_users.delete_tooltip'), icon: 'icon-delete'
-        end
-      else
-        tooltip_tag I18n.t('placeholder_users.right_to_manage_members_missing'), icon: 'icon-help2'
-      end
-    end
-
-    def row_css_class
-      "placeholder_user"
+  ##
+  # Render a tooltip span
+  #
+  # @param text [string] Content of the tooltip
+  # @param placement [string] placement (top, left, right, bottom)
+  # @param span_classes [string] Additional classes on the span
+  # @param icon [string] icon class
+  def tooltip_tag(text, placement: 'left', icon: 'icon-help', span_classes: nil)
+    content_tag :span,
+                class: "tooltip--#{placement} #{span_classes}",
+                data: { tooltip: text } do
+      op_icon "icon #{icon}"
     end
   end
 end
