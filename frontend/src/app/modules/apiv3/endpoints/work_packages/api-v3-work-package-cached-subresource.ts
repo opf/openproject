8ac38c66fb8b124168b7b6a5d2bd7f5611994078
Oliver Günther <mail@oliverguenther.de>
2021-06-03 13:32:28 +0200

    [34884] Ensure schemas are cached in the work package collection request
    https://community.openproject.org/wp/34884

--- frontend/src/app/modules/apiv3/endpoints/work_packages/api-v3-work-package-cached-subresource.ts
+++ frontend/src/app/modules/apiv3/endpoints/work_packages/api-v3-work-package-cached-subresource.ts
@@ -32,14 +32,20 @@ import { Observable } from "rxjs";
 import { APIV3WorkPackagesPaths } from "core-app/modules/apiv3/endpoints/work_packages/api-v3-work-packages-paths";
 import { take, tap } from "rxjs/operators";
 import { WorkPackageCache } from "core-app/modules/apiv3/endpoints/work_packages/work-package.cache";
+import { InjectField } from "core-app/helpers/angular/inject-field.decorator";
+import { States } from "core-components/states.service";
+import { CollectionResource } from "core-app/modules/hal/resources/collection-resource";
+import { SchemaResource } from "core-app/modules/hal/resources/schema-resource";
 
 export class ApiV3WorkPackageCachedSubresource extends APIv3GettableResource<WorkPackageCollectionResource> {
+  @InjectField() private states:States;
 
   public get():Observable<WorkPackageCollectionResource> {
     return this
       .halResourceService
       .get<WorkPackageCollectionResource>(this.path)
       .pipe(
+        tap(collection => collection.schemas && this.updateSchemas(collection.schemas)),
         tap(collection => this.cache.updateWorkPackageList(collection.elements)),
         take(1)
       );
@@ -47,5 +53,11 @@ export class ApiV3WorkPackageCachedSubresource extends APIv3GettableResource<Wor
 
   protected get cache():WorkPackageCache {
     return (this.parent as APIV3WorkPackagesPaths).cache;
+  }
+
+  private updateSchemas(schemas:CollectionResource<SchemaResource>) {
+    schemas.elements.forEach(schema => {
+      this.states.schemas.get(schema.href as string).putValue(schema);
+    });
   }
 }
