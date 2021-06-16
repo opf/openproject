275d319f3924564bf12fb37943f150c6d5074ade
Oliver Günther <mail@oliverguenther.de>
2021-06-16 13:40:43 +0200

    Style items

--- frontend/src/app/features/in-app-notifications/store/in-app-notification.model.ts
+++ frontend/src/app/features/in-app-notifications/store/in-app-notification.model.ts
@@ -1,7 +1,21 @@
 import { ID } from "@datorama/akita";
 
+export interface HalResourceLink {
+  href:string;
+  title:string;
+}
+
 export interface InAppNotification {
   id:ID;
   message:string;
+  date:string;
+  reason:string;
   read?:boolean;
+
+  details?:string[];
+
+  _links:{
+    project:HalResourceLink,
+    resource:HalResourceLink,
+  };
 }
--- frontend/src/app/features/in-app-notifications/store/in-app-notifications.service.ts
+++ frontend/src/app/features/in-app-notifications/store/in-app-notifications.service.ts
@@ -14,13 +14,44 @@ export class InAppNotificationsService {
 
   get():void {
     timer(5000)
-      .subscribe(() => this.add({ id: 3, message: 'A new ticket was assigned to you!' }));
+      .subscribe(() => this.add({
+        id: 1,
+        date: '5 minutes ago',
+        message: 'The following work package was assigned to you: Task #1234: Deploy new website',
+        reason: 'assigned',
+        _links: {
+          project: { href: '/api/v3/projects/1', title: 'My website project' },
+          resource: { href: '/api/v3/work_packages/1234', title: 'Task #1234: Deploy new website' }
+        }
+      }));
 
     timer(10000)
-      .subscribe(() => this.add({ id: 1, message: 'A new ticket was assigned to you!' }));
+      .subscribe(() => this.add({
+        id: 2,
+        message: 'You have been mentioned in work package Task #1234: Deploy new website',
+        date: '3 minutes ago',
+        reason: 'mentioned',
+        details: [
+          'Assignee set to Oliver Günther',
+          'Due date changed from 2021-08-01 to 2021-06-16'
+        ],
+        _links: {
+          project: { href: '/api/v3/projects/1', title: 'My website project' },
+          resource: { href: '/api/v3/work_packages/1234', title: 'Task #1234: Deploy new website' }
+        }
+      }));
 
     timer(20000)
-      .subscribe(() => this.add({ id: 2, message: 'You were mentioned in the following ticket: #1234' }));
+      .subscribe(() => this.add({
+        id: 3,
+        date: '1 minute ago',
+        message: 'The following work package was assigned to you: Bug #5432: Fix styling issues new website',
+        reason: 'assigned',
+        _links: {
+          project: { href: '/api/v3/projects/1', title: 'My website project' },
+          resource: { href: '/api/v3/work_packages/1234', title: 'Bug #5432: Fix styling issues new website' }
+        }
+      }));
   }
 
   add(inAppNotification:InAppNotification):void {
