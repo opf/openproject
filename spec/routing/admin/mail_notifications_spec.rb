d57674f492cc1303e2908e52c9ef6c32bbf2cdaa
Oliver Günther <mail@oliverguenther.de>
2021-06-22 21:54:51 +0200

    Restore update spec

--- spec/routing/admin/mail_notifications_spec.rb
+++ spec/routing/admin/mail_notifications_spec.rb
@@ -33,4 +33,9 @@ describe 'admin mail_notifications routes', type: :routing do
     expect(get('admin/settings/mail_notifications'))
       .to route_to('admin/settings/mail_notifications_settings#show')
   end
+
+  it do
+    expect(patch('admin/settings/mail_notifications'))
+      .to route_to('admin/settings/mail_notifications_settings#update')
+  end
 end
