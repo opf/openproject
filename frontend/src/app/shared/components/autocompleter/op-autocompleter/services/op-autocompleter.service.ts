6a063a512f5d95b9629fc440af05b9d043d1031c
ulferts <jens.ulferts@googlemail.com>
2021-06-23 13:47:38 +0200

    adapt me resource path
    this.apiV3Service.users.me changed its type in 0d6c0b6bc7620de94e00e72b36d6cbc1ec4c8db4

--- frontend/src/app/shared/components/autocompleter/op-autocompleter/services/op-autocompleter.service.ts
+++ frontend/src/app/shared/components/autocompleter/op-autocompleter/services/op-autocompleter.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
-import { ApiV3FilterBuilder, FilterOperator } from 'core-app/shared/helpers/api-v3/api-v3-filter-builder';
-import { debounceTime, distinctUntilChanged, map, switchMap } from 'rxjs/operators';
+import { ApiV3FilterBuilder } from 'core-app/shared/helpers/api-v3/api-v3-filter-builder';
+import { map } from 'rxjs/operators';
 import { APIv3ResourceCollection } from 'core-app/core/apiv3/paths/apiv3-resource';
 import { UserResource } from "core-app/features/hal/resources/user-resource";
 import { APIv3UserPaths } from 'core-app/core/apiv3/endpoints/users/apiv3-user-paths';
@@ -8,7 +8,7 @@ import { APIV3WorkPackagePaths } from 'core-app/core/apiv3/endpoints/work_packag
 import { WorkPackageResource } from "core-app/features/hal/resources/work-package-resource";
 import {HalResource} from "core-app/features/hal/resources/hal-resource";
 import {APIV3Service} from "core-app/core/apiv3/api-v3.service";
-import {of, Observable} from "rxjs";
+import {Observable} from "rxjs";
 import {UntilDestroyedMixin} from "core-app/shared/helpers/angular/until-destroyed.mixin";
 
 @Injectable()
