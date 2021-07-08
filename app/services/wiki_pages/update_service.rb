f911ba45dd16cfa6a55650d18939f9f72509b206
Oliver Günther <mail@oliverguenther.de>
2021-07-08 21:24:31 +0200

    Replace wiki page attach_files with attachable services

--- app/services/wiki_pages/update_service.rb
+++ app/services/wiki_pages/update_service.rb
@@ -28,6 +28,23 @@
 # See docs/COPYRIGHT.rdoc for more details.
 #++
 
-class WikiPages::CreateService < ::BaseServices::Create
+class WikiPages::UpdateService < ::BaseServices::Update
   include Attachments::ReplaceAttachments
+
+  private
+
+  def persist(service_result)
+    service_result = super(service_result)
+
+    page = service_result.result
+    content = page.content
+
+    unless page.save && content.save
+      service_result.errors = page.errors
+      service_result.errors.merge! content.errors
+      service_result.success = false
+    end
+
+    service_result
+  end
 end
