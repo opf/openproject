1c5c84b55e77db457fcbd9ee5f500755cb99fc42
Benjamin Bädorf <b.baedorf@openproject.com>
2021-07-14 13:49:35 +0200

    Update git commit formatter

--- modules/github_integration/frontend/module/git-actions/git-actions.service.ts
+++ modules/github_integration/frontend/module/git-actions/git-actions.service.ts
@@ -39,10 +39,13 @@ export class GitActionsService {
     // This sanitization tries to be harsher than those rules
     return str
       .replace(/ +/g, '-') // Spaces become dashes
+      .replace(/&/g, 'and') // Spaces become dashes
       .replace(/[\000-\039]/g, '') // ASCII control characters are out
       .replace(/\177/g, '') // DEL is out
       .replace(/#\\\/\?\*\~\^\:\{\}@\.\[\]/g, '') // Some other characters with special rules are out
-      .replace(/^[-]+/, ''); // Dashes at the start are removed
+      .replace(/^[-]+/, '') // Dashes at the start are removed
+      .replace(/-+/, '-') // Multiple dashes in a row are deduped
+      .trim();
   }
 
   private formattingInput(workPackage: WorkPackageResource) {
@@ -58,10 +61,9 @@ export class GitActionsService {
   }
 
   private sanitizeShellInput(str:string):string {
-    return `"${str
-      .replace(/\$/g, '\\$')
-      .replace(/"/g, '\\"')
-    }"`;
+    return `'${str
+      .replace(/'/g, '\\\'')
+    }'`;
   }
 
   public branchName(workPackage:WorkPackageResource):string {
@@ -81,6 +83,6 @@ ${url}`;
   public gitCommand(workPackage:WorkPackageResource):string {
     const branch = this.branchName(workPackage);
     const commit = this.commitMessage(workPackage);
-    return `git checkout -b ${this.sanitizeShellInput(branch)} && git commit --allow-empty -m ${this.sanitizeShellInput(commit)}`;
+    return `git checkout -b ${this.sanitizeShellInput(branch)} && git commit --allow-empty -m ${this.sanitizeShellInput(commit)}`.trim();
   }
 }
