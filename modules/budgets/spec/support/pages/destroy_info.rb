5008824622c19384ff48289b4552938405c82602
Henriette Darge <h.darge@openproject.com>
2021-07-20 15:17:12 +0200

    Reassign budgets of WPs before the budget is deleted.

--- modules/budgets/spec/support/pages/destroy_info.rb
+++ modules/budgets/spec/support/pages/destroy_info.rb
@@ -27,32 +27,49 @@
 #++
 
 require 'support/pages/page'
-require_relative 'budget_form'
 
 module Pages
-  class EditBudget < Page
-    include ::Pages::BudgetForm
+  class DestroyInfo < Page
+    attr_accessor :budget
 
-    attr_reader :budget_id # budget == budget
-
-    def initialize(budget_id)
-      @budget_id = budget_id
+    def initialize(budget)
+      self.budget = budget
     end
 
-    def click_copy
-      within '.toolbar-items' do
-        click_link 'Copy'
-      end
+    def expect_loaded
+      expect(page)
+        .to have_content("#{I18n.t(:button_delete)} #{I18n.t(:label_budget_id, id: budget.id)}: #{budget.subject}")
     end
 
-    def click_delete
-      within '.toolbar-items' do
-        click_link 'Delete'
-      end
+    def expect_reassign_option
+      expect(page)
+        .to have_field('todo_reassign')
+    end
+
+    def expect_no_reassign_option
+      expect(page)
+        .not_to have_field('todo_reassign')
+    end
+
+    def select_reassign_option(budget_name)
+      select(budget_name, from: 'reassign_to_id')
+    end
+
+    def expect_delete_option
+      expect(page)
+        .to have_field('todo_delete')
+    end
+
+    def select_delete_option
+      choose('todo_delete')
+    end
+
+    def delete
+      click_button 'Apply'
     end
 
     def path
-      "/budgets/#{budget_id}"
+      destroy_info_budget_path(budget)
     end
   end
 end
