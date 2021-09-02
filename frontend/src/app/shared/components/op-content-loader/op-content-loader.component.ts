7b1d7344a553d913502dbe9d01fc8edb192786f0
bsatarnejad <b.satarnejad@gmail.com>
2021-09-02 06:39:34 +0200

    show noresult box only after data is completely rendered

--- frontend/src/app/shared/components/op-content-loader/op-content-loader.component.ts
+++ frontend/src/app/shared/components/op-content-loader/op-content-loader.component.ts
@@ -1,4 +1,9 @@
-import { ChangeDetectionStrategy, Component, Input, OnInit } from '@angular/core';
+import {
+  ChangeDetectionStrategy,
+  Component, 
+  Input, 
+  OnInit
+} from '@angular/core';
 
 function uid() {
   return Math.random().toString(36).substring(2);
