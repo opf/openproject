c0096af9993ec354b70ceee8a6ea88b8e117b4f1
Henriette Darge <h.darge@openproject.com>
2021-09-14 08:48:27 +0200

    Move "Create" button to toolbar on pages with rendered tabs. Thus the button is shown at the top and easily available.

--- app/helpers/tabs_helper.rb
+++ app/helpers/tabs_helper.rb
@@ -32,11 +32,15 @@ module TabsHelper
   # Renders tabs and their content
   def render_tabs(tabs, form = nil)
     if tabs.any?
-      selected_tab = tabs.detect { |t| t[:name] == params[:tab] } if params[:tab].present?
-      render partial: 'common/tabs', locals: { f: form, tabs: tabs, selected_tab: selected_tab || tabs.first }
+      selected = selected_tab(tabs)
+      render partial: 'common/tabs', locals: { f: form, tabs: tabs, selected_tab: selected }
     else
       content_tag 'p', I18n.t(:label_no_data), class: 'nodata'
     end
+  end
+
+  def selected_tab(tabs)
+    tabs.detect { |t| t[:name] == params[:tab] } || tabs.first
   end
 
   # Render tabs from the ui/extensible tabs manager
