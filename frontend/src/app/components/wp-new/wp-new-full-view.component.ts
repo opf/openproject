00f41811569c069d81afbee5ac728d55a15ef05d
Wieland Lindenthal <w.lindenthal@forkmerge.com>
2021-09-16 17:31:35 +0200

    Fix: Revit Add-in - Correct successState for creating BCF issues (#9673)
    Currently after creating a BCF issue in Revit Add-in
    the redirect goes to the work package module's full
    view. Instead it should redirect to 'bim.partitioned.show'.
    
    This fix here has no corresponding bug work package on
    community.

--- frontend/src/app/components/wp-new/wp-new-full-view.component.ts
+++ frontend/src/app/components/wp-new/wp-new-full-view.component.ts
@@ -36,5 +36,5 @@ import { ChangeDetectionStrategy, Component } from '@angular/core';
   changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class WorkPackageNewFullViewComponent extends WorkPackageCreateComponent {
-  public successState = 'work-packages.show';
+  public successState = this.$state.current.data.successState as string;
 }
