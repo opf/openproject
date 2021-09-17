41bd0dbd5a2fc1c7a0007e86dd40d0667c1bb9db
Oliver Günther <mail@oliverguenther.de>
2021-09-17 14:47:20 +0200

    Avoid Hash#delete on input

--- config/initializers/grape_logging.rb
+++ config/initializers/grape_logging.rb
@@ -1,15 +1,14 @@
 OpenProject::Application.configure do
   config.after_initialize do
     ActiveSupport::Notifications.subscribe('openproject_grape_logger') do |_, _, _, _, payload|
-      time = payload.delete :time
+      time = payload[:time]
       attributes = {
         duration: time[:total],
         db: time[:db],
         view: time[:view]
-      }.merge(payload)
+      }.merge(payload.except(:time))
 
-      string = OpenProject::Logging.formatter.call(attributes)
-      Rails.logger.info string
+      Rails.logger.info OpenProject::Logging.formatter.call(attributes)
     end
   end
 end
