af5e2e84942f81186aab461d7a8b5a2bcfdc8471
Oliver Günther <mail@oliverguenther.de>
2021-09-24 21:46:13 +0200

    Add spec for regular uploads as well

--- modules/bim/spec/features/ifc_models/regular_ifc_upload_spec.rb
+++ modules/bim/spec/features/ifc_models/regular_ifc_upload_spec.rb
@@ -29,9 +29,8 @@
 require 'spec_helper'
 require_relative './ifc_upload_shared_examples'
 
-describe 'direct IFC upload', type: :feature, js: true, with_direct_uploads: :redirect, with_config: { edition: 'bim' } do
+describe 'IFC upload', type: :feature, js: true, with_config: { edition: 'bim' } do
   it_behaves_like 'can upload an IFC file' do
-    # with direct upload, we don't get the model name
-    let(:model_name) { 'model.ifc' }
+    let(:model_name) { 'minimal.ifc' }
   end
 end
