3d29e5e9ef1768feaf27d996506dac963ea23d53
Wieland Lindenthal <w.lindenthal@forkmerge.com>
2021-10-06 08:48:36 +0200

    Allow downloading IFC files

--- modules/bim/app/cells/bim/ifc_models/row_cell.rb
+++ modules/bim/app/cells/bim/ifc_models/row_cell.rb
@@ -44,10 +44,17 @@ module Bim
       ###
 
       def button_links
+        links = []
+
+        # Seeded IFC models currently actually only have the XKT and NOT(!) the IFC original seeded
+        if model.ifc_attachment
+          links << download_link
+        end
+
         if User.current.allowed_to?(:manage_ifc_models, model.project)
-          [edit_link, delete_link]
+          links + [edit_link, delete_link]
         else
-          []
+          links
         end
       end
 
@@ -56,14 +63,23 @@ module Bim
                 bcf_project_ifc_model_path(model.project, model),
                 class: 'icon icon-delete',
                 data: { confirm: I18n.t(:text_are_you_sure) },
+                title: I18n.t(:button_delete),
                 method: :delete
+      end
+
+      def download_link
+        link_to '',
+                ::API::V3::Utilities::PathHelper::ApiV3Path.attachment_content(model.ifc_attachment.id),
+                class: 'icon icon-download',
+                title: I18n.t(:button_download)
       end
 
       def edit_link
         link_to '',
                 edit_bcf_project_ifc_model_path(model.project, model),
                 class: 'icon icon-edit',
-                accesskey: accesskey(:edit)
+                accesskey: accesskey(:edit),
+                title: I18n.t(:button_edit)
       end
     end
   end
