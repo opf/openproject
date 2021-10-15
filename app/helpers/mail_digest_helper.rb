e975a418ed575ef84794b81b28a6d147ca3d87ae
Henriette Darge <h.darge@openproject.com>
2021-10-15 10:53:33 +0200

    Show timestamp text instead of notification details in email

--- app/helpers/mail_digest_helper.rb
+++ app/helpers/mail_digest_helper.rb
@@ -44,37 +44,36 @@ module MailDigestHelper
     summary
   end
 
-  def digest_notification_timestamp_text(notification, html: true, extended_text: false)
+  def digest_notification_timestamp_text(notification, html: true)
     journal = notification.journal
     user = html ? link_to_user(journal.user, only_path: false) : journal.user.name
 
-    timestamp_text(user, journal, extended_text)
+    timestamp_text(user, journal)
   end
 
-  def digest_comment_text(notification)
-    if notification.reason_mentioned?
-      sanitize I18n.t(:'mail.work_packages.mentioned')
-    else
-      sanitize I18n.t(:'mail.work_packages.comment_added')
+  def digest_additional_author_text(notifications)
+    number_of_additional_authors = number_of_authors(notifications) - 1
+
+    if notifications.length > 1 && number_of_additional_authors > 0
+      amount = number_of_additional_authors === 1 ? 'one' : 'other'
+      I18n.t(:"js.notifications.center.and_more_users.#{amount}", count: number_of_additional_authors)
     end
   end
 
   private
 
-  def timestamp_text(user, journal, extended)
+  def timestamp_text(user, journal)
     value = journal.initial? ? "created" : "updated"
-    if extended
-      sanitize(
-        "#{I18n.t(:"mail.work_packages.#{value}")} #{I18n.t(:"mail.work_packages.#{value}_at",
-                                                            user: user,
-                                                            timestamp: journal.created_at.strftime(
-                                                              I18n.t(:'time.formats.time')
-                                                            ))}"
-      )
-    else
-      sanitize(I18n.t(:"mail.work_packages.#{value}_at",
-                      user: user,
-                      timestamp: journal.created_at.strftime(I18n.t(:'time.formats.time'))))
-    end
+    sanitize(
+      I18n.t(:"mail.work_packages.#{value}_at",
+             user: user,
+             timestamp: journal.created_at.strftime(
+               "#{I18n.t(:'date.formats.default')}, #{I18n.t(:'time.formats.time')}"
+             ))
+    )
+  end
+
+  def number_of_authors(notifications)
+    notifications.group_by { |n| n[:actor_id] }.count
   end
 end
