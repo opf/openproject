ca881de9970b742b7af494d871833e7d96922a94
Henriette Darge <h.darge@openproject.com>
2021-10-25 09:27:11 +0200

    Adapt test to extend error message when deleting a type

--- app/controllers/types_controller.rb
+++ app/controllers/types_controller.rb
@@ -118,22 +118,7 @@ class TypesController < ApplicationController
       @type.destroy
       flash[:notice] = I18n.t(:notice_successful_delete)
     else
-      flash[:error] = if @type.is_standard?
-                        t(:error_can_not_delete_standard_type)
-                      else
-                        error_message = [
-                          t(:'error_can_not_delete_type.explanation',
-                            { url: belonging_wps_url(@type.id)}).html_safe
-                        ]
-
-                        archived_projects = @type.projects.filter(&:archived?)
-                        error_message.push(
-                          t(:'error_can_not_delete_type.archived_projects',
-                            { archived_projects: archived_projects.map(&:name).join(', ')}).html_safe
-                        ) if archived_projects.length > 0
-
-                        error_message
-                      end
+      flash[:error] = destroy_error_message
     end
     redirect_to action: 'index'
   end
@@ -175,6 +160,29 @@ class TypesController < ApplicationController
 
   def show_local_breadcrumb
     true
+  end
+
+  def destroy_error_message
+    if @type.is_standard?
+      t(:error_can_not_delete_standard_type)
+    else
+      error_message = [
+        ApplicationController.helpers.sanitize(
+          t(:'error_can_not_delete_type.explanation', { url: belonging_wps_url(@type.id) }),
+          attributes: %w(href target)
+        )
+      ]
+
+      archived_projects = @type.projects.filter(&:archived?)
+      if !archived_projects.empty?
+        error_message.push(
+          t(:'error_can_not_delete_type.archived_projects',
+            { archived_projects: archived_projects.map(&:name).join(', ') })
+        )
+      end
+
+      error_message
+    end
   end
 
   def belonging_wps_url(type_id)
