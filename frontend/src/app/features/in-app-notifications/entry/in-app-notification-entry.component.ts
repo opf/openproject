ef7291fecf5f279dfec0c909f48909482d5355a0
Oliver Günther <mail@oliverguenther.de>
2021-11-04 16:59:33 +0100

    [39506] use shareReplay to ensure notification selected on load
    https://community.openproject.org/wp/39506

--- frontend/src/app/features/in-app-notifications/entry/in-app-notification-entry.component.ts
+++ frontend/src/app/features/in-app-notifications/entry/in-app-notification-entry.component.ts
@@ -24,7 +24,6 @@ import { PathHelperService } from 'core-app/core/path-helper/path-helper.service
 import { take } from 'rxjs/internal/operators/take';
 import { StateService } from '@uirouter/angular';
 import { HalResource } from 'core-app/features/hal/resources/hal-resource';
-import { BackRouteOptions } from 'core-app/features/work-packages/components/back-routing/back-routing.service';
 import { InAppNotification } from 'core-app/core/state/in-app-notifications/in-app-notification.model';
 import { IanCenterService } from 'core-app/features/in-app-notifications/center/state/ian-center.service';
 import { DeviceService } from 'core-app/core/browser/device.service';
@@ -46,6 +45,8 @@ export class InAppNotificationEntryComponent implements OnInit {
   workPackage$:Observable<WorkPackageResource>|null = null;
 
   loading$ = this.storeService.query.selectLoading();
+
+  stateChanged$ = this.storeService.stateChanged$;
 
   // The actor, if any
   actors:PrincipalLike[] = [];
