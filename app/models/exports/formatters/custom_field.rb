718bc2978821792dfdbd01c4ced3dde22342ae0f
Oliver Günther <mail@oliverguenther.de>
2021-11-04 09:50:13 +0100

    Use available_custom_values to avoid loading CFs individually

--- app/models/exports/formatters/custom_field.rb
+++ app/models/exports/formatters/custom_field.rb
@@ -8,6 +8,7 @@ module Exports
       ##
       # Takes a WorkPackage and an attribute and returns the value to be exported.
       def retrieve_value(object)
+        custom_field = find_custom_field(object)
         return '' if custom_field.nil?
 
         object.formatted_custom_value_for(custom_field)
@@ -15,12 +16,9 @@ module Exports
 
       ##
       # Finds a custom field from the attribute identifier
-      def custom_field
-        unless defined?(@custom_field)
-          @custom_field = ::CustomField.find_by(id: attribute.to_s.sub('cf_', '').to_i)
-        end
-
-        @custom_field
+      def find_custom_field(object)
+        id = attribute.to_s.sub('cf_', '').to_i
+        object.available_custom_fields.detect { |cf| cf.id == id }
       end
     end
   end
