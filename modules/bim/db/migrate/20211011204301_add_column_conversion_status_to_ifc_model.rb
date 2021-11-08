687fa928e88a5d6bab7ad1ab5e0c890cad92035f
Wieland Lindenthal <w.lindenthal@forkmerge.com>
2021-11-08 12:11:21 +0100

    Improve readability of migration code:

--- modules/bim/db/migrate/20211011204301_add_column_conversion_status_to_ifc_model.rb
+++ modules/bim/db/migrate/20211011204301_add_column_conversion_status_to_ifc_model.rb
@@ -3,22 +3,16 @@ class AddColumnConversionStatusToIfcModel < ActiveRecord::Migration[6.1]
     add_column(:ifc_models, :conversion_status, :integer, default: 0) # default "pending"
     add_column(:ifc_models, :conversion_error_message, :text)
 
-    # Set IfcModels having a XKT file as "processed"
-    ::Bim::IfcModels::IfcModel
-      .joins(:attachments)
-      .where("attachments.description = 'xkt'")
-      .update_all(conversion_status: 2) # 2 == processed
+    converted_models = ::Bim::IfcModels::IfcModel
+                         .joins(:attachments)
+                         .where("attachments.description = 'xkt'")
 
-    # Set IfcModels not having a XKT file as conversion_status "error"
-    processed_model_ids = ::Bim::IfcModels::IfcModel
-                            .joins(:attachments)
-                            .where("attachments.description = 'xkt'")
-                            .pluck(:id)
+    not_converted_models = ::Bim::IfcModels::IfcModel
+                             .where
+                             .not(id: converted_models)
 
-    ::Bim::IfcModels::IfcModel
-      .where
-      .not("id IN (?)", processed_model_ids)
-      .update_all(conversion_status: 3) # 3 == error
+    converted_models.update_all(conversion_status: 2) # 2 == processed
+    not_converted_models.update_all(conversion_status: 3) # 3 == error
   end
 
   def down
