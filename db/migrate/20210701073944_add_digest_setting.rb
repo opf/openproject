1e736cbdf01dff67ea371e68d67778139341fbb3
Oliver Günther <mail@oliverguenther.de>
2021-11-08 08:10:16 +0100

    Remove more notification migrations

--- db/migrate/20210701073944_add_digest_setting.rb
+++ db/migrate/20210701073944_add_digest_setting.rb
@@ -1,32 +1,10 @@
 class AddDigestSetting < ActiveRecord::Migration[6.1]
   def up
-    insert_default_digest_channel
+    # No-op
   end
 
   def down
     remove_digest_channels
-  end
-
-  def insert_default_digest_channel
-    execute <<~SQL
-      INSERT INTO
-        notification_settings
-        (user_id,
-         channel,
-         involved,
-         mentioned,
-         watched)
-      SELECT
-        id,
-        2,
-        true,
-        true,
-        true
-      FROM
-        users
-      WHERE
-        type = 'User'
-    SQL
   end
 
   # Removes all digest channels. Includes non default channels as those might
