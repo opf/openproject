3939da2da0f80e214fd43376af7b45669a1e2d1e
Oliver Günther <mail@oliverguenther.de>
2021-11-16 13:40:35 +0100

    Handle API errors that respond without JSON

--- frontend/src/app/features/hal/services/hal-error.ts
+++ frontend/src/app/features/hal/services/hal-error.ts
@@ -5,16 +5,16 @@ export class HalError extends Error {
   readonly name = 'HALError';
 
   get message():string {
-    return this.resource.message || this.httpError?.message || 'Unknown error';
+    return this.resource?.message || this.httpError?.message || 'Unknown error';
   }
 
   get errorIdentifier():string {
-    return this.resource.errorIdentifier;
+    return this.resource?.errorIdentifier || 'unknown';
   }
 
   constructor(
     readonly httpError:HttpErrorResponse,
-    readonly resource:ErrorResource,
+    readonly resource:ErrorResource|null,
   ) {
     super();
     Object.setPrototypeOf(this, HalError.prototype);
