f7b5bdc150fbd3858a6854c47c7e5561651a77c1
yogesh-jm <yogsh.jmv@gmail.com>
2021-11-16 15:06:29 +0530

    Remove commented code

--- app/controllers/companies_controller.rb
+++ app/controllers/companies_controller.rb
@@ -44,65 +44,14 @@ class CompaniesController < ApplicationController
     end
 
     # def create
-    #   @category = @project.categories.build
-    #   @category.attributes = permitted_params.category
-  
-    #   if @category.save
-    #     respond_to do |format|
-    #       format.html do
-    #         flash[:notice] = I18n.t(:notice_successful_create)
-    #         redirect_to settings_categories_project_path(@project)
-    #       end
-    #       format.js do
-    #         render locals: { project: @project, category: @category }
-    #       end
-    #     end
-    #   else
-    #     respond_to do |format|
-    #       format.html do
-    #         render action: :new
-    #       end
-    #       format.js do
-    #         render(:update) { |page| page.alert(@category.errors.full_messages.join('\n')) }
-    #       end
-    #     end
-    #   end
     # end
   
-    # def update
-    #   @category.attributes = permitted_params.category
-    #   if @category.save
-    #     flash[:notice] = I18n.t(:notice_successful_update)
-    #     redirect_to settings_categories_project_path(@project)
-    #   else
-    #     render action: 'edit'
-    #   end
     # end
   
     # def destroy
-    #   @issue_count = @category.work_packages.size
-    #   if @issue_count == 0
-    #     # No issue assigned to this category
-    #     @category.destroy
-    #     redirect_to settings_categories_project_path(@project)
-    #     return
-    #   elsif params[:todo]
-    #     reassign_to = @project.categories.find_by(id: params[:reassign_to_id]) if params[:todo] == 'reassign'
-    #     @category.destroy(reassign_to)
-    #     redirect_to settings_categories_project_path(@project)
-    #     return
-    #   end
-    #   @categories = @project.categories - [@category]
     # end
   
     # private
-  
-    # # Wrap ApplicationController's find_model_object method to set
-    # # @category instead of just @category
-    # def find_model_object
-    #   super
-    #   @category = @object
-    # end
   
     # def find_company
     #   @company = Company.find(params[:id])
