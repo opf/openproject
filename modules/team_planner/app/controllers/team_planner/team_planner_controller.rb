6885ee64e39364eac430564a862f6c679d0ed823
Henriette Darge <h.darge@openproject.com>
2021-11-18 12:57:43 +0100

    Remove doubled code

--- modules/team_planner/app/controllers/team_planner/team_planner_controller.rb
+++ modules/team_planner/app/controllers/team_planner/team_planner_controller.rb
@@ -3,10 +3,6 @@ module ::TeamPlanner
     before_action :find_optional_project
     before_action :authorize
 
-    # The team planner permission alone does not suffice
-    # to view work packages
-    before_action :authorize_work_package_permission
-
     menu_item :team_planner_view
 
     def index
@@ -15,14 +11,6 @@ module ::TeamPlanner
 
     current_menu_item :index do
       :team_planner_view
-    end
-
-    private
-
-    def authorize_work_package_permission
-      unless current_user.allowed_to?(:view_work_packages, @project, global: @project.nil?)
-        deny_access
-      end
     end
   end
 end
--- modules/team_planner/spec/routing/team_planner_routing_spec.rb
+++ modules/team_planner/spec/routing/team_planner_routing_spec.rb
@@ -30,7 +30,7 @@ require 'spec_helper'
 
 describe 'Team planner routing', type: :routing do
   it 'routes to team_planner#index' do
-    is_expected
+    expect(subject)
       .to route(:get, '/projects/foobar/team_planner/state')
       .to(controller: 'team_planner/team_planner', action: 'index', project_id: 'foobar', state: 'state')
   end
