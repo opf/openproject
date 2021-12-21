eba369027cd0327521c4d93ca456991840cc94de
Benjamin Bädorf <b.baedorf@openproject.com>
2021-12-21 16:23:16 +0100

    Lint HTML files

--- frontend/.eslintrc.js
+++ frontend/.eslintrc.js
@@ -144,6 +144,7 @@ module.exports = {
          * Any template/HTML related rules you wish to use/reconfigure over and above the
          * recommended set provided by the @angular-eslint project would go here.
          */
+        "@angular-eslint/template/no-call-expression": 2,
       }
     },
     {
