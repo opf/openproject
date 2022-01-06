543e7a7eb6f6e06a7197ba426fcd9bec5691b11c
bsatarnejad <b.satarnejad@gmail.com>
2022-01-06 14:56:05 +0100

    add team planner token and set the engine and routes of upsale

--- modules/team_planner/lib/open_project/team_planner/engine.rb
+++ modules/team_planner/lib/open_project/team_planner/engine.rb
@@ -29,10 +29,10 @@ module OpenProject::TeamPlanner
              name: 'OpenProject Team Planner' do
       project_module :team_planner_view, dependencies: :work_package_tracking do
         permission :view_team_planner,
-                   { 'team_planner/team_planner': %i[index] },
+                   { 'team_planner/team_planner': %i[index upsale] },
                    dependencies: %i[view_work_packages]
         permission :manage_team_planner,
-                   { 'team_planner/team_planner': %i[index] },
+                   { 'team_planner/team_planner': %i[index upsale] },
                    dependencies: %i[view_team_planner add_work_packages edit_work_packages save_queries manage_public_queries]
       end
 
