ba22d521c2ebeda5c030b943f25fcb0dbe28fced
Oliver Günther <mail@oliverguenther.de>
2022-01-17 11:03:36 +0100

    Add contract_actions to team_planner

--- modules/team_planner/lib/open_project/team_planner/engine.rb
+++ modules/team_planner/lib/open_project/team_planner/engine.rb
@@ -30,10 +30,13 @@ module OpenProject::TeamPlanner
       project_module :team_planner_view, dependencies: :work_package_tracking do
         permission :view_team_planner,
                    { 'team_planner/team_planner': %i[index] },
-                   dependencies: %i[view_work_packages]
+                   dependencies: %i[view_work_packages],
+                   contract_actions: { team_planner: %i[read] }
+
         permission :manage_team_planner,
                    { 'team_planner/team_planner': %i[index] },
-                   dependencies: %i[view_team_planner add_work_packages edit_work_packages save_queries manage_public_queries]
+                   dependencies: %i[view_team_planner add_work_packages edit_work_packages save_queries manage_public_queries],
+                   contract_actions: { team_planner: %i[create update destroy] }
       end
 
       menu :project_menu,
