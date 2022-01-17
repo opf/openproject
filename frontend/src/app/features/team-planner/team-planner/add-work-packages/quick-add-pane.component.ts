e058882edb7c6b692d0b267320c6b121b2ae93ae
Henriette Darge <h.darge@openproject.com>
2022-01-17 16:02:42 +0100

    Add empty state for team planner quick add pane

--- frontend/src/app/features/team-planner/team-planner/add-work-packages/quick-add-pane.component.ts
+++ frontend/src/app/features/team-planner/team-planner/add-work-packages/quick-add-pane.component.ts
@@ -1,16 +1,28 @@
 import { Component, OnInit, ChangeDetectionStrategy } from '@angular/core';
+import { I18nService } from 'core-app/core/i18n/i18n.service';
+import { imagePath } from 'core-app/shared/helpers/images/path-helper';
 
 @Component({
   selector: 'op-quick-add-pane',
   templateUrl: './quick-add-pane.component.html',
   styleUrls: ['./quick-add-pane.component.sass'],
-  changeDetection: ChangeDetectionStrategy.OnPush
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class QuickAddPaneComponent implements OnInit {
+  isEmpty = true;
 
-  constructor() { }
+  text = {
+    empty_state: this.I18n.t('js.team_planner.quick_add.empty_state'),
+  };
 
-  ngOnInit(): void {
+  image = {
+    empty_state: imagePath('team-planner/quick-add-empty-state.svg'),
+  };
+
+  constructor(
+    private I18n:I18nService,
+  ) { }
+
+  ngOnInit():void {
   }
-
 }
