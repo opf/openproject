024a45ac25a082ad4ccd77c9f4f60b0f2a56f12f
Henriette Darge <h.darge@openproject.com>
2022-01-18 13:43:33 +0100

    Search for work packages and show the results as cards (for now)

--- frontend/src/app/features/team-planner/team-planner/add-work-packages/quick-add-pane.component.ts
+++ frontend/src/app/features/team-planner/team-planner/add-work-packages/quick-add-pane.component.ts
@@ -1,6 +1,26 @@
-import { Component, OnInit, ChangeDetectionStrategy } from '@angular/core';
+import {
+  ChangeDetectionStrategy,
+  Component,
+  OnInit,
+} from '@angular/core';
 import { I18nService } from 'core-app/core/i18n/i18n.service';
 import { imagePath } from 'core-app/shared/helpers/images/path-helper';
+import {
+  BehaviorSubject,
+  of,
+} from 'rxjs';
+import { WorkPackageResource } from 'core-app/features/hal/resources/work-package-resource';
+import { ApiV3FilterBuilder } from 'core-app/shared/helpers/api-v3/api-v3-filter-builder';
+import {
+  catchError,
+  debounceTime,
+  map,
+} from 'rxjs/operators';
+import { ApiV3Service } from 'core-app/core/apiv3/api-v3.service';
+import { WorkPackageNotificationService } from 'core-app/features/work-packages/services/notifications/work-package-notification.service';
+import { CurrentProjectService } from 'core-app/core/current-project/current-project.service';
+import { UrlParamsHelperService } from 'core-app/features/work-packages/components/wp-query/url-params-helper';
+import { IsolatedQuerySpace } from 'core-app/features/work-packages/directives/query-space/isolated-query-space';
 
 @Component({
   selector: 'op-quick-add-pane',
@@ -9,18 +29,87 @@ import { imagePath } from 'core-app/shared/helpers/images/path-helper';
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class QuickAddPaneComponent implements OnInit {
-  isEmpty = true;
+  isEmpty = new BehaviorSubject<boolean>(true);
+
+  isLoading = new BehaviorSubject<boolean>(false);
 
   text = {
     empty_state: this.I18n.t('js.team_planner.quick_add.empty_state'),
+    placeholder: this.I18n.t('js.team_planner.quick_add.search_placeholder'),
   };
 
   image = {
     empty_state: imagePath('team-planner/quick-add-empty-state.svg'),
   };
 
+  resultingWorkPackages:WorkPackageResource[] = [];
+
+  searchWorkPackages = (searchString:string):void => {
+    this.isLoading.next(true);
+
+    // Return when the search string is empty
+    if (searchString.length === 0) {
+      this.isLoading.next(false);
+      return;
+    }
+
+    const filters:ApiV3FilterBuilder = new ApiV3FilterBuilder();
+    const queryResults = this.querySpace.results.value;
+
+    filters.add('subjectOrId', '**', [searchString]);
+
+    if (queryResults && queryResults.elements.length > 0) {
+      filters.add('id', '!', queryResults.elements.map((wp:WorkPackageResource) => wp.id!));
+    }
+
+    // Add the existing filter, if any
+    const query = this.querySpace.query.value;
+    if (query?.filters) {
+      const currentFilters = this.urlParamsHelper.buildV3GetFilters(query.filters);
+
+      currentFilters.forEach((filter) => {
+        Object.keys(filter).forEach((name) => {
+          if (name !== 'assignee' && name !== 'datesInterval') {
+            filters.add(name, filter[name].operator, filter[name].values);
+          }
+        });
+      });
+    }
+
+    this
+      .apiV3Service
+      .withOptionalProject(this.currentProject.id)
+      .work_packages
+      .filtered(filters)
+      .get()
+      .pipe(
+        debounceTime(250),
+        map((collection) => collection.elements),
+        catchError((error:unknown) => {
+          this.notificationService.handleRawError(error);
+          return of([]);
+        }),
+      )
+      .subscribe((results) => {
+        this.resultingWorkPackages = results;
+
+        this.isEmpty.next(results.length === 0);
+        this.isLoading.next(false);
+      });
+  };
+
+  public autocompleterOptions = {
+    resource: 'work_packages',
+    getOptionsFn: this.searchWorkPackages,
+  };
+
   constructor(
+    private readonly querySpace:IsolatedQuerySpace,
     private I18n:I18nService,
+    private readonly apiV3Service:ApiV3Service,
+    private readonly notificationService:WorkPackageNotificationService,
+    private readonly currentProject:CurrentProjectService,
+    private readonly urlParamsHelper:UrlParamsHelperService,
   ) { }
 
   ngOnInit():void {
