5102685267fe69091908850c9b053311fad11fd9
Benjamin Bädorf <b.baedorf@openproject.com>
2022-01-31 15:10:41 +0100

    Add basic form

--- frontend/src/app/shared/components/project-select/project-select.component.ts
+++ frontend/src/app/shared/components/project-select/project-select.component.ts
@@ -1,21 +1,30 @@
 import { I18nService } from 'core-app/core/i18n/i18n.service';
 import {
   ChangeDetectionStrategy,
-  ChangeDetectorRef,
   Component,
 } from '@angular/core';
+import { ProjectsResourceService } from 'core-app/core/state/projects/projects.service';
 
 @Component({
   selector: 'op-project-select',
   changeDetection: ChangeDetectionStrategy.OnPush,
   templateUrl: './project-select.component.html',
+  styleUrls: ['./project-select.component.sass'],
 })
 export class OpProjectSelectComponent {
   public open = false;
   public searchText = '';
+  public includeSubProjects = false;
+  public selectedProjects:string[] = [];
 
   constructor(
     readonly I18n:I18nService,
-    protected cdRef:ChangeDetectorRef,
+    readonly projectsResourceService:ProjectsResourceService,
   ) { }
+
+  public searchProjects() {
+    console.log(this.searchText);
+  }
+  public clearSelection() {}
+  public save() {}
 }
