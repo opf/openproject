b2a9da0f6b6ca43535a5b0840ba735acf2198dbe
Henriette Darge <h.darge@openproject.com>
2022-03-02 13:17:00 +0100

    Show create button in boards module

--- frontend/src/app/features/boards/boards-sidebar/boards-menu.component.ts
+++ frontend/src/app/features/boards/boards-sidebar/boards-menu.component.ts
@@ -1,4 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import {
+  Component,
+  Injector,
+  OnInit,
+} from '@angular/core';
 import { Observable } from 'rxjs';
 import { BoardService } from 'core-app/features/boards/board/board.service';
 import { Board } from 'core-app/features/boards/board/board';
@@ -8,6 +12,10 @@ import { CurrentProjectService } from 'core-app/core/current-project/current-pro
 import { UntilDestroyedMixin } from 'core-app/shared/helpers/angular/until-destroyed.mixin';
 import { ApiV3Service } from 'core-app/core/apiv3/api-v3.service';
 import { MainMenuNavigationService } from 'core-app/core/main-menu/main-menu-navigation.service';
+import { CurrentUserService } from 'core-app/core/current-user/current-user.service';
+import { I18nService } from 'core-app/core/i18n/i18n.service';
+import { NewBoardModalComponent } from 'core-app/features/boards/new-board-modal/new-board-modal.component';
+import { OpModalService } from 'core-app/shared/components/modal/modal.service';
 
 export const boardsMenuSelector = 'boards-menu';
 
@@ -23,7 +31,7 @@ export class BoardsMenuComponent extends UntilDestroyedMixin implements OnInit {
 
   selectedBoardId:string;
 
-  public boards$:Observable<Board[]> = this
+  boards$:Observable<Board[]> = this
     .apiV3Service
     .boards
     .observeAll()
@@ -31,14 +39,32 @@ export class BoardsMenuComponent extends UntilDestroyedMixin implements OnInit {
       map((boards:Board[]) => boards.sort((a, b) => a.name.localeCompare(b.name))),
     );
 
-  constructor(private readonly boardService:BoardService,
+  canCreateBoards$ = this
+    .currentUserService
+    .hasCapabilities$(
+      'boards/create',
+      this.currentProject.id || undefined,
+      )
+    .pipe(this.untilDestroyed());
+
+  text = {
+    create_new_board: this.I18n.t('js.boards.create_new'),
+  };
+
+  constructor(
+    private readonly boardService:BoardService,
     private readonly apiV3Service:ApiV3Service,
     private readonly currentProject:CurrentProjectService,
-    private readonly mainMenuService:MainMenuNavigationService) {
+    private readonly mainMenuService:MainMenuNavigationService,
+    readonly currentUserService:CurrentUserService,
+    readonly I18n:I18nService,
+    private readonly opModalService:OpModalService,
+    private readonly injector:Injector,
+  ) {
     super();
   }
 
-  ngOnInit() {
+  ngOnInit():void {
     // When activating the boards submenu,
     // either initially or through click on the toggle, load the results
     this.mainMenuService
@@ -59,7 +85,11 @@ export class BoardsMenuComponent extends UntilDestroyedMixin implements OnInit {
       });
   }
 
-  private focusBackArrow() {
+  showNewBoardModal():void {
+    this.opModalService.show(NewBoardModalComponent, this.injector);
+  }
+
+  private focusBackArrow():void {
     const buttonArrowLeft = jQuery('*[data-name="board_view"] .main-menu--arrow-left-to-project');
     buttonArrowLeft.focus();
   }
