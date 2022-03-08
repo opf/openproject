1d04491d0acd1516d2d5b48caba324c5cb93afbe
Christophe Bliard <c.bliard@openproject.com>
2022-03-08 10:53:32 +0100

    stop autoloading ApplicationMailer during initialization

--- config/initializers/register_mail_interceptors.rb
+++ config/initializers/register_mail_interceptors.rb
@@ -31,6 +31,8 @@
 # Register interceptors defined in app/mailers/user_mailer.rb
 # Do this here, so they aren't registered multiple times due to reloading in development mode.
 
-ApplicationMailer.register_interceptor(DefaultHeadersInterceptor)
-# following needs to be the last interceptor
-ApplicationMailer.register_interceptor(DoNotSendMailsWithoutReceiverInterceptor)
+Rails.application.config.action_mailer.interceptors = [
+  "DefaultHeadersInterceptor",
+  # following needs to be the last interceptor
+  "DoNotSendMailsWithoutReceiverInterceptor"
+]
