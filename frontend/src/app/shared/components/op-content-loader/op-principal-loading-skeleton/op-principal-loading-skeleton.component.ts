38e8747bf01ea3f71c3cd78677ef0fd3311b310c
bsatarnejad <b.satarnejad@gmail.com>
2022-03-10 11:07:00 +0100

    create component files

---
+++ frontend/src/app/shared/components/op-content-loader/op-principal-loading-skeleton/op-principal-loading-skeleton.component.ts
@@ -0,0 +1,14 @@
+import {
+    ChangeDetectionStrategy,
+    Component,
+  } from '@angular/core';
+  
+  @Component({
+    selector: 'op-principal-loading-skeleton',
+    templateUrl: './op-principal-loading-skeleton.component.html',
+    changeDetection: ChangeDetectionStrategy.OnPush,
+  })
+  export class OpPrincipalLoadingComponent {
+  
+  }
+  
---
+++ frontend/src/app/shared/components/op-content-loader/op-wp-loading-skeleton/op-wp-loading-skeleton.component.ts
@@ -0,0 +1,14 @@
+import {
+    ChangeDetectionStrategy,
+    Component,
+  } from '@angular/core';
+  
+  @Component({
+    selector: 'op-wp-loading-skeleton',
+    templateUrl: './op-wp-loading-skeleton.component.html',
+    changeDetection: ChangeDetectionStrategy.OnPush,
+  })
+  export class OpWPLoadingComponent {
+  
+  }
+  
