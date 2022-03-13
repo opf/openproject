0ca698163dd43dd1db0290d9484b4f669a393f42
bsatarnejad <b.satarnejad@gmail.com>
2022-03-13 22:01:29 +0100

    fix indentations and make some changes to the view of component

--- frontend/src/app/shared/components/op-content-loader/op-wp-loading-skeleton/op-wp-loading-skeleton.component.ts
+++ frontend/src/app/shared/components/op-content-loader/op-wp-loading-skeleton/op-wp-loading-skeleton.component.ts
@@ -1,14 +1,13 @@
 import {
-    ChangeDetectionStrategy,
-    Component,
-  } from '@angular/core';
-  
-  @Component({
-    selector: 'op-wp-loading-skeleton',
-    templateUrl: './op-wp-loading-skeleton.component.html',
-    changeDetection: ChangeDetectionStrategy.OnPush,
-  })
-  export class OpWPLoadingComponent {
-  
-  }
-  
+  ChangeDetectionStrategy,
+  Component,
+} from '@angular/core';
+
+@Component({
+  selector: 'op-wp-loading-skeleton',
+  templateUrl: './op-wp-loading-skeleton.component.html',
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class OpWPLoadingComponent {
+
+}
