ede01caaa4f750aec760d22fd3b6fb54a00e4de2
ulferts <jens.ulferts@googlemail.com>
2022-03-16 18:12:01 +0100

    use default end points on wp#available_projects_on_...

--- lib/api/v3/work_packages/available_projects_on_create_api.rb
+++ lib/api/v3/work_packages/available_projects_on_create_api.rb
@@ -26,8 +26,6 @@
 # See COPYRIGHT and LICENSE files for more details.
 #++
 
-require 'api/v3/projects/project_collection_representer'
-
 module API
   module V3
     module WorkPackages
@@ -35,31 +33,32 @@ module API
         resource :available_projects do
           after_validation do
             authorize(:add_work_packages, global: true)
+
+            checked_permissions = Projects::ProjectCollectionRepresenter.checked_permissions
+            current_user.preload_projects_allowed_to(checked_permissions)
           end
 
           params do
             optional :for_type, type: Integer
           end
 
-          get do
-            checked_permissions = Projects::ProjectCollectionRepresenter.checked_permissions
-            current_user.preload_projects_allowed_to(checked_permissions)
+          get &::API::V3::Utilities::Endpoints::Index
+                 .new(model: Project,
+                      self_path: -> { api_v3_paths.available_projects_on_create(params[:for_type]) },
+                      scope: -> {
+                        available_projects = WorkPackage
+                                             .allowed_target_projects_on_create(current_user)
+                                             .includes(Projects::ProjectCollectionRepresenter.to_eager_load)
 
-            available_projects = WorkPackage
-                                 .allowed_target_projects_on_create(current_user)
-                                 .includes(Projects::ProjectCollectionRepresenter.to_eager_load)
-
-            query = ::Queries::Projects::ProjectQuery.new(user: current_user)
-            if params[:for_type]
-              query.where('type_id', '=', params[:for_type])
-              available_projects = query.results.merge(available_projects)
-            end
-
-            self_link = api_v3_paths.available_projects_on_create(params[:for_type])
-            Projects::ProjectCollectionRepresenter.new(available_projects,
-                                                       self_link: self_link,
-                                                       current_user: current_user)
-          end
+                        if params[:for_type]
+                          query = ::Queries::Projects::ProjectQuery.new(user: current_user)
+                          query.where('type_id', '=', params[:for_type])
+                          query.results.merge(available_projects)
+                        else
+                          available_projects
+                        end
+                      })
+                 .mount
         end
       end
     end
