98f3e738ca71e8f79aff31996735ca2eaa7a8c9d
bsatarnejad <b.satarnejad@gmail.com>
2022-03-17 09:38:13 +0100

    change styles to make the svg fit into the card

--- frontend/src/app/shared/components/op-content-loader/op-wp-loading-skeleton/op-wp-loading-skeleton.component.ts
+++ frontend/src/app/shared/components/op-content-loader/op-wp-loading-skeleton/op-wp-loading-skeleton.component.ts
@@ -1,13 +1,16 @@
 import {
   ChangeDetectionStrategy,
   Component,
+  ViewEncapsulation,
+  HostBinding,
 } from '@angular/core';
 
 @Component({
   selector: 'op-wp-loading-skeleton',
   templateUrl: './op-wp-loading-skeleton.component.html',
   changeDetection: ChangeDetectionStrategy.OnPush,
+  encapsulation: ViewEncapsulation.None,
 })
 export class OpWPLoadingComponent {
-
+  @HostBinding('class.op-wp-loading-skeleton--loader') className = true;
 }
