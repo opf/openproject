c94eb6cef3e11a32b5c632cf7bc5e2f5f6eef111
Dombi Attila <dombesz.attila@gmail.com>
2022-04-07 23:13:09 +0200

    [41071] Ensure project export includes optional column https://community.openproject.org/wp/41071

--- spec/models/projects/exporter/csv_integration_spec.rb
+++ spec/models/projects/exporter/csv_integration_spec.rb
@@ -47,6 +47,19 @@ describe Projects::Exports::CSV, 'integration', type: :model do
                                project.name, project.description, 'Off track', 'false']
   end
 
+  context 'with status_explanation enabled' do
+    before do
+      Setting.enabled_projects_columns += ["status_explanation"]
+    end
+
+    it 'performs a successful export' do
+      expect(parsed.size).to eq(2)
+      expect(parsed.last).to eq [project.id.to_s, project.identifier,
+                                 project.name, project.description,
+                                 'Off track', '', 'false']
+    end
+  end
+
   describe 'custom field columns selected' do
     before do
       Setting.enabled_projects_columns += custom_fields.map { |cf| "cf_#{cf.id}" }
@@ -57,7 +70,7 @@ describe Projects::Exports::CSV, 'integration', type: :model do
         expect(parsed.size).to eq 2
 
         cf_names = custom_fields.map(&:name)
-        expect(header).to eq ['id', 'Identifier', 'Name', 'Description', 'Status', 'Public', *cf_names]
+        expect(header).to eq ['id', 'Identifier', 'Name', 'Description', 'Status', 'Status description', 'Public', *cf_names]
 
         custom_values = custom_fields.map do |cf|
           case cf
@@ -70,13 +83,14 @@ describe Projects::Exports::CSV, 'integration', type: :model do
           end
         end
         expect(rows.first)
-          .to eq [project.id.to_s, project.identifier, project.name, project.description, 'Off track', 'false', *custom_values]
+          .to eq [project.id.to_s, project.identifier, project.name,
+                  project.description, 'Off track', '', 'false', *custom_values]
       end
     end
 
     context 'when ee not enabled' do
       it 'renders only the default columns' do
-        expect(header).to eq %w[id Identifier Name Description Status Public]
+        expect(header).to eq %w[id Identifier Name Description Status Status\ description Public]
       end
     end
   end
