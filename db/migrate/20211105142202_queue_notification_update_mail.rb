3f44f804950ee98b7f9e510bfeb541d60a9a0170
ulferts <jens.ulferts@googlemail.com>
2022-04-26 21:54:33 +0200

    Update db/migrate/20211105142202_queue_notification_update_mail.rb
    Co-authored-by: Oliver Günther <mail@oliverguenther.de>

--- db/migrate/20211105142202_queue_notification_update_mail.rb
+++ db/migrate/20211105142202_queue_notification_update_mail.rb
@@ -2,7 +2,7 @@ class QueueNotificationUpdateMail < ActiveRecord::Migration[6.1]
   def up
     # On a newly created database, we don't want the update mail to be sent.
     # Users are only created upon seeding.
-    return unless User.exists?
+    return unless User.not_builtin.exists?
 
     ::Announcements::SchedulerJob
       .perform_later subject: :'notifications.update_info_mail.subject',
