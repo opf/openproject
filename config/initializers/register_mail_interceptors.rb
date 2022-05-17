8ec3f05767590b4eba0b5ace8cc27a932111f472
Oliver Günther <mail@oliverguenther.de>
2022-05-17 16:22:57 +0200

    Move interceptors registry into reloader

--- config/initializers/register_mail_interceptors.rb
+++ config/initializers/register_mail_interceptors.rb
@@ -28,7 +28,8 @@
 
 # Register interceptors defined in app/mailers/user_mailer.rb
 # Do this here, so they aren't registered multiple times due to reloading in development mode.
-
-ApplicationMailer.register_interceptor Interceptors::DefaultHeaders
-# following needs to be the last interceptor
-ApplicationMailer.register_interceptor Interceptors::DoNotSendMailsWithoutRecipient
+Rails.application.reloader.to_prepare do
+  ApplicationMailer.register_interceptor Interceptors::DefaultHeaders
+  # following needs to be the last interceptor
+  ApplicationMailer.register_interceptor Interceptors::DoNotSendMailsWithoutRecipient
+end
