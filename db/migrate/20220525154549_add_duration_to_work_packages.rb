2d3457388ac10d83bc213033e8aaec1fc4560506
ulferts <jens.ulferts@googlemail.com>
2022-05-25 14:50:57 +0200

    add duration column to wp journals - hide in activity

--- db/migrate/20220525154549_add_duration_to_work_packages.rb
+++ db/migrate/20220525154549_add_duration_to_work_packages.rb
@@ -1,5 +1,7 @@
 class AddDurationToWorkPackages < ActiveRecord::Migration[6.1]
   def change
     add_column :work_packages, :duration, :integer
+
+    add_column :work_package_journals, :duration, :integer
   end
 end
