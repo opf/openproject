03609dd6e0e31f5e767a0095eecce359f8de4461
ulferts <jens.ulferts@googlemail.com>
2022-06-24 09:25:37 +0200

    support signaling on available parent project endpoint

--- lib/api/v3/projects/available_parents_api.rb
+++ lib/api/v3/projects/available_parents_api.rb
@@ -37,25 +37,25 @@ module API
             authorize_any(%i[add_project add_subprojects edit_project], global: true)
           end
 
-          get &::API::V3::Utilities::Endpoints::Index.new(model: Project,
-                                                          scope: -> do
-                                                            project = if params[:of]
-                                                                        Project.find(params[:of])
-                                                                      else
-                                                                        Project.new
-                                                                      end
+          get &::API::V3::Utilities::Endpoints::SqlFallbackedIndex.new(model: Project,
+                                                                       scope: -> do
+                                                                         project = if params[:of]
+                                                                                     Project.find(params[:of])
+                                                                                   else
+                                                                                     Project.new
+                                                                                   end
 
-                                                            contract_class = if project.new_record?
-                                                                               ::Projects::CreateContract
-                                                                             else
-                                                                               ::Projects::UpdateContract
-                                                                             end
+                                                                         contract_class = if project.new_record?
+                                                                                            ::Projects::CreateContract
+                                                                                          else
+                                                                                            ::Projects::UpdateContract
+                                                                                          end
 
-                                                            contract = contract_class.new(project, current_user)
+                                                                         contract = contract_class.new(project, current_user)
 
-                                                            contract.assignable_parents.includes(:enabled_modules)
-                                                          end)
-                                                     .mount
+                                                                         contract.assignable_parents.includes(:enabled_modules)
+                                                                       end)
+                                                                  .mount
         end
       end
     end
