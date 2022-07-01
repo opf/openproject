8668b2d8a47b3a5a6d4efa2171759e4451da5b2b
ulferts <jens.ulferts@googlemail.com>
2022-07-01 11:10:44 +0200

    wip

--- db/migrate/20220628112428_permission_materialized_view.rb
+++ db/migrate/20220628112428_permission_materialized_view.rb
@@ -64,45 +64,60 @@ class PermissionMaterializedView < ActiveRecord::Migration[7.0]
       -- In public projects, all users not being a member have the non member permission. That includes the anonymous user
       -- which can never be a member anyway.
 
-      SELECT DISTINCT
-        users.id,
-        projects.id,
-        permissions.permission
-      FROM
-        users,
-        (
-          SELECT DISTINCT projects.id, enabled_modules.name FROM projects, enabled_modules
-          WHERE
-                projects.id = enabled_modules.project_id
-          AND
-                projects.public = TRUE
-        ) projects,
-        -- Aggregate the two builtin roles so that all permissions, stored and public with their corresponding module
-        -- are returned.
-        (
-          SELECT DISTINCT permission_module_map.permission, permission_module_map.project_module, roles.builtin FROM
-          roles,
-          role_permissions,
-              (VALUES ('view_project', NULL, true), ('view_work_packages', 'work_package_tracking', false)) AS permission_module_map(permission, project_module, public)
+     SELECT user_id, non_members.project_id, permission
+     FROM
+       (
+         SELECT
+             users.id user_id,
+             projects.id project_id,
+         roles.id role_id
+           FROM
+             users,
+             projects,
+         roles
+         WHERE
+           (
+             (users.type IN ('User', 'PrincipalUser') AND roles.builtin = #{Role::BUILTIN_NON_MEMBER})
+             OR
+             (users.type IN ('AnonymousUser') AND roles.builtin = #{Role::BUILTIN_ANONYMOUS})
+           )
+         AND projects.public
+             AND
+               NOT EXISTS (SELECT 1 FROM members WHERE user_id = users.id AND project_id = projects.id)
+         GROUP BY
+            users.id,
+           projects.id,
+           roles.id
+       ) non_members,
+       (
+         SELECT
+           permission_module_map.permission,
+           project_id,
+           role_id
+         FROM
+           role_permissions,
+           (VALUES ('view_project', NULL, true), ('view_work_packages', 'work_package_tracking', false)) AS permission_module_map(permission, project_module, public),
+           roles,
+           enabled_modules
            WHERE
+             (
+               (permission_module_map.public = FALSE AND role_permissions.permission = permission_module_map.permission)
+               OR
+               (permission_module_map.public = TRUE)
+             )
+           AND
              roles.builtin IN (#{Role::BUILTIN_NON_MEMBER}, #{Role::BUILTIN_ANONYMOUS})
-          AND
-            (
-              (roles.id = role_permissions.role_id AND permission_module_map.public = FALSE AND role_permissions.permission = permission_module_map.permission)
-              OR
-              (permission_module_map.public = TRUE)
-            )
-        ) permissions
-        WHERE
-          (projects.name = permissions.project_module OR permissions.project_module IS NULL)
-        AND
-          (
-            (users.type IN ('User', 'PrincipalUser') AND permissions.builtin = #{Role::BUILTIN_NON_MEMBER})
-            OR
-            (users.type IN ('AnonymousUser') AND permissions.builtin = #{Role::BUILTIN_ANONYMOUS})
-          )
-        AND
-          NOT EXISTS (SELECT 1 FROM members WHERE user_id = users.id AND project_id = projects.id)
+           AND
+             (enabled_modules.name = permission_module_map.project_module OR permission_module_map.project_module IS NULL)
+           GROUP BY
+             permission_module_map.permission,
+             role_id,
+             project_id
+       ) permissions_in_project
+       WHERE
+         non_members.project_id = permissions_in_project.project_id
+         AND
+         non_members.role_id = permissions_in_project.role_id
     SQL
 
     add_index :permissions, [:user_id, :project_id, :permission], unique: true
