d55e9a099c5698e83691c46f67f6e48a23ca76b3
ulferts <jens.ulferts@googlemail.com>
2022-07-04 15:26:09 +0200

    wip - non admin users vs admin users - including active check

--- db/migrate/20220628112428_permission_materialized_view.rb
+++ db/migrate/20220628112428_permission_materialized_view.rb
@@ -3,146 +3,147 @@ class PermissionMaterializedView < ActiveRecord::Migration[7.0]
     execute <<~SQL
       CREATE MATERIALIZED VIEW permissions AS
       SELECT
-        members.user_id,
-        members.project_id,
-        role_permissions.permission
-      FROM
-        members
-      JOIN
-        member_roles ON members.id = member_roles.member_id
-      JOIN
-        roles ON roles.id = member_roles.role_id
-      JOIN
-        role_permissions ON roles.id = role_permissions.role_id
-      JOIN
-        projects ON projects.id = members.project_id AND projects.active = TRUE
-      JOIN
-        enabled_modules ON projects.id = enabled_modules.project_id
-      JOIN
-        (VALUES ('view_project', NULL, true), ('view_work_packages', 'work_package_tracking', false)) AS permission_module_map(permission, project_module, public),
-        ON permission_module_map.permission = role_permissions.permission
-        AND (enabled_modules.name = permission_module_map.project_module OR permission_module_map.project_module IS NULL)
-
-      UNION
-
-      -- active member users get public permissions as long as the corresponding module is active and the project as well
-
-      SELECT DISTINCT
-        members.user_id,
-        members.project_id,
-        permission_module_map.permission
-      FROM
-        members
-      JOIN
-        member_roles ON members.id = member_roles.member_id
-      JOIN
-        projects ON projects.id = members.project_id AND projects.active = TRUE
-      JOIN
-        enabled_modules ON projects.id = enabled_modules.project_id
-      CROSS JOIN
-        (VALUES ('view_project', NULL, true), ('view_work_packages', 'work_package_tracking', false)) AS permission_module_map(permission, project_module, public),
-      WHERE
-        enabled_modules.name = permission_module_map.project_module	OR permission_module_map.project_module IS NULL
-
-      UNION
-
-      -- active admin users get all permissions as long as corresponding module is active and the project as well
-      SELECT
-        users.id,
-        projects.id,
+        user_id,
+        project_id,
         permission
       FROM
-        users, projects
-      CROSS JOIN
-        -- TODO fetch values from OpenProject::AccessControl.permissions.map { |p| [p.name, p.project_module] }
-        (VALUES ('view_work_packages', 'work_package_tracking')) AS permission_module_map(permission, project_module)
-      WHERE
-        users.admin = TRUE
-
-      UNION
-
-      -- In public projects, all users not being a member have the non member permission. That includes the anonymous user
-      -- which can never be a member anyway.
-
-      SELECT user_id, non_members.project_id, permission
-      FROM
-       (
-         SELECT
-           users.id user_id,
-           projects.id project_id,
-           roles.id role_id
-         FROM
-           users,
-           projects,
-           roles
-         WHERE
-           (
-             (users.type IN ('User', 'PrincipalUser') AND roles.builtin = #{Role::BUILTIN_NON_MEMBER})
-             OR
-             (users.type IN ('AnonymousUser') AND roles.builtin = #{Role::BUILTIN_ANONYMOUS})
-           )
-         AND projects.public
-         AND
-           NOT EXISTS (SELECT 1 FROM members WHERE user_id = users.id AND project_id = projects.id)
-         GROUP BY
-            users.id,
-           projects.id,
-           roles.id
-       ) non_members,
-       (
+      (
         SELECT
-          permission_module_map.permission,
-          project_id,
-          roles.id role_id
+          members.user_id,
+          members.project_id,
+          permission
         FROM
-		    (
-			    SELECT
-            permission_module_map.permission,
-            permission_module_map.project_module,
-            enabled_modules.project_id,
-            permission_module_map.public
-          FROM
-            (VALUES ('view_project', NULL, true), ('view_work_packages', 'work_package_tracking', false)) AS permission_module_map(permission, project_module, public),
-            enabled_modules
-          WHERE enabled_modules.name = permission_module_map.project_module
 
-          -- using a UNION to get the permissions that are not linked to a project module. This has two advantages:
-          -- * the edge case of having projects but non has any module enabled is covered
-          -- * due to the existence of a projects.id for all entrys, a costly "OR projects.id NULL" when joining can be avoided.
+        -- All members and non members (of public projects)
+        (
+          -- non members
+          SELECT *
+          FROM
+          (
+            SELECT
+                users.id user_id,
+                projects.id project_id,
+            roles.id role_id
+              FROM
+                users,
+                projects,
+            roles
+            WHERE
+              (
+                (users.type IN ('User', 'PrincipalUser') AND roles.builtin = 1)
+                OR
+                (users.type IN ('AnonymousUser') AND roles.builtin = 2)
+              )
+            AND projects.public
+                AND
+                  NOT EXISTS (SELECT 1 FROM members WHERE user_id = users.id AND project_id = projects.id)
+            GROUP BY
+               users.id,
+              projects.id,
+              roles.id
+          ) non_members
+
           UNION
+          -- actual members
 
           SELECT
-            permission_module_map.permission,
-            permission_module_map.project_module,
-            projects.id,
-            permission_module_map.public
+            members.user_id,
+            members.project_id,
+            member_roles.role_id
           FROM
-            (VALUES ('view_project', NULL, true), ('view_work_packages', 'work_package_tracking', false)) AS permission_module_map(permission, project_module, public),
-            projects
-			    WHERE permission_module_map.project_module IS NULL
-			  ) permission_module_map,
+            members
+          JOIN
+            member_roles ON members.id = member_roles.member_id
+        ) members,
+        -- All permissions active within projects
         (
-          SELECT roles.*, role_permissions.permission from roles LEFT JOIN role_permissions ON roles.id = role_permissions.role_id
-          WHERE roles.builtin IN (#{Role::BUILTIN_NON_MEMBER}, #{Role::BUILTIN_ANONYMOUS})
-        ) roles
-        WHERE
+          SELECT
+            permission_module_map.permission,
+            project_id,
+            roles.id role_id
+          FROM
           (
-            (permission_module_map.public = FALSE AND roles.permission = permission_module_map.permission)
-            OR
-            (permission_module_map.public = TRUE)
-          )
-        GROUP BY
-          permission_module_map.permission,
-          roles.id,
-          project_id
-	    ) permissions_in_project
+            SELECT
+              permission_module_map.permission,
+              permission_module_map.project_module,
+              enabled_modules.project_id,
+              public
+            FROM
+              (VALUES ('view_project', NULL, true), ('view_work_packages', 'work_package_tracking', false)) AS permission_module_map(permission, project_module, public),
+              enabled_modules
+              WHERE enabled_modules.name = permission_module_map.project_module
+
+            UNION
+
+            SELECT
+              permission_module_map.permission,
+              permission_module_map.project_module,
+              projects.id,
+              permission_module_map.public
+            FROM
+              (VALUES ('view_project', NULL, true), ('view_work_packages', 'work_package_tracking', false)) AS permission_module_map(permission, project_module, public),
+              projects
+              WHERE permission_module_map.project_module IS NULL
+          ) permission_module_map,
+          -- Roles and the permissions they grant
+          (
+            SELECT roles.*, role_permissions.permission
+            FROM
+              roles,
+              role_permissions
+            WHERE roles.id = role_permissions.role_id
+          ) roles
+          WHERE
+            (
+              (permission_module_map.public = FALSE AND roles.permission = permission_module_map.permission)
+              OR
+              (permission_module_map.public = TRUE)
+            )
+
+          GROUP BY
+            permission_module_map.permission,
+            roles.id,
+            project_id
+        ) permissions_in_project
+
+        WHERE
+          (members.project_id = permissions_in_project.project_id)
+          AND
+          (members.role_id = permissions_in_project.role_id)
+
+        UNION
+
+        -- All permissions admins have in the projects.
+        -- Cannot be done performantly together with the normal users.
+
+         SELECT
+           users.id,
+           projects.id,
+           permission
+         FROM
+           users, projects, enabled_modules,
+           (VALUES ('view_project', NULL, true), ('view_work_packages', 'work_package_tracking', false)) AS permission_module_map(permission, project_module, public)
+         WHERE
+           users.admin = TRUE
+         AND
+           enabled_modules.project_id = projects.id
+         AND
+           (enabled_modules.name = permission_module_map.project_module OR permission_module_map.project_module IS NULL)
+      ) permissions_in_projects,
+      projects,
+      users
       WHERE
-        non_members.project_id = permissions_in_project.project_id
+        -- Reduce to only those permission sets in which both project and user are active.
+        permissions_in_projects.user_id = users.id
         AND
-        non_members.role_id = permissions_in_project.role_id
+          permissions_in_projects.project_id = projects.id
+        AND
+          users.status = 1
+        AND
+          projects.active
     SQL
 
-    add_index :permissions, [:user_id, :project_id, :permission], unique: true
+    add_index :permissions, %i[user_id project_id permission], unique: true
     add_index :permissions, :user_id
     add_index :permissions, :project_id
     add_index :permissions, :permission
