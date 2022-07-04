0495969ebfc57f091fa7fa9a0d2f2b897977f2e0
ulferts <jens.ulferts@googlemail.com>
2022-07-04 13:32:07 +0200

    wip - union to avoid OR null

--- db/migrate/20220628112428_permission_materialized_view.rb
+++ db/migrate/20220628112428_permission_materialized_view.rb
@@ -19,7 +19,7 @@ class PermissionMaterializedView < ActiveRecord::Migration[7.0]
       JOIN
         enabled_modules ON projects.id = enabled_modules.project_id
       JOIN
-        (VALUES ('view_work_packages', 'work_package_tracking')) AS permission_module_map(permission, project_module)
+        (VALUES ('view_project', NULL, true), ('view_work_packages', 'work_package_tracking', false)) AS permission_module_map(permission, project_module, public),
         ON permission_module_map.permission = role_permissions.permission
         AND (enabled_modules.name = permission_module_map.project_module OR permission_module_map.project_module IS NULL)
 
@@ -40,7 +40,7 @@ class PermissionMaterializedView < ActiveRecord::Migration[7.0]
       JOIN
         enabled_modules ON projects.id = enabled_modules.project_id
       CROSS JOIN
-        (VALUES ('view_project', NULL)) AS permission_module_map(permission, project_module)
+        (VALUES ('view_project', NULL, true), ('view_work_packages', 'work_package_tracking', false)) AS permission_module_map(permission, project_module, public),
       WHERE
         enabled_modules.name = permission_module_map.project_module	OR permission_module_map.project_module IS NULL
 
@@ -64,17 +64,17 @@ class PermissionMaterializedView < ActiveRecord::Migration[7.0]
       -- In public projects, all users not being a member have the non member permission. That includes the anonymous user
       -- which can never be a member anyway.
 
-     SELECT user_id, non_members.project_id, permission
-     FROM
+      SELECT user_id, non_members.project_id, permission
+      FROM
        (
          SELECT
-             users.id user_id,
-             projects.id project_id,
-         roles.id role_id
-           FROM
-             users,
-             projects,
-         roles
+           users.id user_id,
+           projects.id project_id,
+           roles.id role_id
+         FROM
+           users,
+           projects,
+           roles
          WHERE
            (
              (users.type IN ('User', 'PrincipalUser') AND roles.builtin = #{Role::BUILTIN_NON_MEMBER})
@@ -82,46 +82,64 @@ class PermissionMaterializedView < ActiveRecord::Migration[7.0]
              (users.type IN ('AnonymousUser') AND roles.builtin = #{Role::BUILTIN_ANONYMOUS})
            )
          AND projects.public
-             AND
-               NOT EXISTS (SELECT 1 FROM members WHERE user_id = users.id AND project_id = projects.id)
+         AND
+           NOT EXISTS (SELECT 1 FROM members WHERE user_id = users.id AND project_id = projects.id)
          GROUP BY
             users.id,
            projects.id,
            roles.id
        ) non_members,
        (
-SELECT
-         permission_module_map.permission,
-           project_id,
-           roles.id role_id
-FROM
-		   (
-			   SELECT * FROM
-			   (VALUES ('view_project', NULL, true), ('view_work_packages', 'work_package_tracking', false)) AS permission_module_map(permission, project_module, public)
-			   LEFT JOIN enabled_modules
-			   ON enabled_modules.name = permission_module_map.project_module
-			   WHERE permission_module_map.project_module IS NULL OR enabled_modules.project_id IS NOT NULL
-			) permission_module_map,
-           (
-		     SELECT roles.*, role_permissions.permission from roles LEFT JOIN role_permissions ON roles.id = role_permissions.role_id
-			   WHERE roles.builtin IN (#{Role::BUILTIN_NON_MEMBER}, #{Role::BUILTIN_ANONYMOUS})
-		   ) roles
-           WHERE
-             (
-               (permission_module_map.public = FALSE AND roles.permission = permission_module_map.permission)
-               OR
-               (permission_module_map.public = TRUE)
-             )
+        SELECT
+          permission_module_map.permission,
+          project_id,
+          roles.id role_id
+        FROM
+		    (
+			    SELECT
+            permission_module_map.permission,
+            permission_module_map.project_module,
+            enabled_modules.project_id,
+            permission_module_map.public
+          FROM
+            (VALUES ('view_project', NULL, true), ('view_work_packages', 'work_package_tracking', false)) AS permission_module_map(permission, project_module, public),
+            enabled_modules
+          WHERE enabled_modules.name = permission_module_map.project_module
 
-		GROUP BY
-             permission_module_map.permission,
-             roles.id,
-             project_id
-	   ) permissions_in_project
-       WHERE
-         (non_members.project_id = permissions_in_project.project_id OR permissions_in_project.project_id IS NULL)
-         AND
-         non_members.role_id = permissions_in_project.role_id
+          -- using a UNION to get the permissions that are not linked to a project module. This has two advantages:
+          -- * the edge case of having projects but non has any module enabled is covered
+          -- * due to the existence of a projects.id for all entrys, a costly "OR projects.id NULL" when joining can be avoided.
+          UNION
+
+          SELECT
+            permission_module_map.permission,
+            permission_module_map.project_module,
+            projects.id,
+            permission_module_map.public
+          FROM
+            (VALUES ('view_project', NULL, true), ('view_work_packages', 'work_package_tracking', false)) AS permission_module_map(permission, project_module, public),
+            projects
+			    WHERE permission_module_map.project_module IS NULL
+			  ) permission_module_map,
+        (
+          SELECT roles.*, role_permissions.permission from roles LEFT JOIN role_permissions ON roles.id = role_permissions.role_id
+          WHERE roles.builtin IN (#{Role::BUILTIN_NON_MEMBER}, #{Role::BUILTIN_ANONYMOUS})
+        ) roles
+        WHERE
+          (
+            (permission_module_map.public = FALSE AND roles.permission = permission_module_map.permission)
+            OR
+            (permission_module_map.public = TRUE)
+          )
+        GROUP BY
+          permission_module_map.permission,
+          roles.id,
+          project_id
+	    ) permissions_in_project
+      WHERE
+        non_members.project_id = permissions_in_project.project_id
+        AND
+        non_members.role_id = permissions_in_project.role_id
     SQL
 
     add_index :permissions, [:user_id, :project_id, :permission], unique: true
@@ -130,37 +148,9 @@ FROM
     add_index :permissions, :permission
   end
 
-
-#  UNION
-#
-#  -- In public projects, the anonymous user has permissions
-#
-#  SELECT DISTINCT
-#  users.id,
-#    projects.id,
-#    permission_module_map.permission
-#  FROM
-#  users, projects, roles, enabled_modules, role_permissions,
-#  (VALUES ('view_project', NULL, true), ('view_work_packages', 'work_package_tracking', false)) AS permission_module_map(permission, project_module, public)
-#  WHERE
-#  (
-#    (roles.id = role_permissions.role_id AND permission_module_map.public = FALSE AND role_permissions.permission = permission_module_map.permission)
-#    OR
-#    (permission_module_map.public = TRUE)
-#  )
-#  AND
-#  projects.id = enabled_modules.project_id
-#  AND
-#  (enabled_modules.name = permission_module_map.project_module OR permission_module_map.project_module IS NULL)
-#  AND
-#  projects.public = TRUE
-#  AND
-#  users.type IN ('AnonymousUser')
-#  AND
-#  roles.builtin = #{Role::BUILTIN_ANONYMOUS}
-    def down
-      execute <<~SQL
+  def down
+    execute <<~SQL
       DROP MATERIALIZED VIEW permissions
-      SQL
-    end
+    SQL
+  end
 end
