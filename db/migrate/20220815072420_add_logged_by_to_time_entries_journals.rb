acf57e4ae2777b1840d91857ec1da0943245fcdd
ulferts <jens.ulferts@googlemail.com>
2022-08-15 09:48:53 +0200

    rewrite logged_by for time entries on user deletion

---
+++ db/migrate/20220815072420_add_logged_by_to_time_entries_journals.rb
@@ -0,0 +1,17 @@
+class AddLoggedByToTimeEntriesJournals < ActiveRecord::Migration[7.0]
+  def change
+    add_reference :time_entry_journals, :logged_by, foreign_key: { to_table: :users }, index: true
+
+    reversible do |change|
+      change.up do
+        execute <<~SQL.squish
+          UPDATE time_entry_journals
+          SET logged_by_id = user_id
+        SQL
+      end
+    end
+
+    change_column_null :time_entries, :logged_by_id, false
+    change_column_null :time_entry_journals, :logged_by_id, false
+  end
+end
