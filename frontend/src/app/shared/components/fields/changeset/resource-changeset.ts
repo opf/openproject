cf6984e6db18ad28eb3b3a9eb929a6cdc6f639d4
Oliver Günther <mail@oliverguenther.de>
2022-08-17 09:02:47 +0200

    Allow changeset value to be typed

--- frontend/src/app/shared/components/fields/changeset/resource-changeset.ts
+++ frontend/src/app/shared/components/fields/changeset/resource-changeset.ts
@@ -239,15 +239,15 @@ export class ResourceChangeset<T extends HalResource = HalResource> {
    * @param {string} key The attribute to read
    * @return {any} Either the value from the overridden change, or the default value
    */
-  public value(key:string) {
+  public value<T>(key:string):T {
     // Overridden value by user?
     if (this.changeset.contains(key)) {
-      return this.changeset.getValue(key);
+      return this.changeset.getValue(key) as T;
     }
 
     // Return whatever is on the base.
     // TODO this needs to be typed
-    return this.pristineResource[key];
+    return this.pristineResource[key] as T;
   }
 
   /**
