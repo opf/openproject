4e1705480af5aa9620c2042365dbc61eff8ef6fe
Henriette Darge <h.darge@openproject.com>
2022-08-19 13:58:23 +0200

    Remove migration  as it will be handled (and extended) separately

--- db/migrate/20220816065025_change_default_for_non_working_days.rb
+++
@@ -1,5 +0,0 @@
-class ChangeDefaultForNonWorkingDays < ActiveRecord::Migration[7.0]
-  def change
-    change_column_default :work_packages, :ignore_non_working_days, from: true, to: false
-  end
-end
