e816b2a795f0fb5ade20d281bd7f1c85d3e155c5
Oliver Günther <mail@oliverguenther.de>
2022-09-05 13:24:45 +0200

    Remove after_initialize block

--- config/initializers/postgresql_timestamp.rb
+++ config/initializers/postgresql_timestamp.rb
@@ -1,4 +1,2 @@
-Rails.application.config.after_initialize do
-  # Use timestampz to create new timestamp columns, so that we get WITH TIME ZONE support
-  ActiveRecord::ConnectionAdapters::PostgreSQLAdapter.datetime_type = :timestamptz
-end
+# Use timestampz to create new timestamp columns, so that we get WITH TIME ZONE support
+ActiveRecord::ConnectionAdapters::PostgreSQLAdapter.datetime_type = :timestamptz
