a9e29279ea76e1e1cf07d0a9bb2a4f6172933aca
Benjamin Bädorf <b.baedorf@openproject.com>
2022-09-19 14:39:15 +0000

    Feature/storybook (#10797)
    Squash commit of storybook addition to aid design system documentation efforts.
    
    The older commit titles were:
    
    * Initial storybook commit
    
    * Fix documentation.json links
    
    * Don't track documentation.json in git
    
    * Enable sass in storybook
    
    * Initial version of a story that uses angular components
    
    * Remove example stories, clean up button story
    
    * More example stories
    
    * Fix sb build
    
    * Always use dev
    
    * Try without auth header
    
    * Update workflow name
    
    * More logs
    
    * Check if token set
    
    * Use release/storybook branch for testing
    
    * Send ref input to workflow
    
    * Escape input to curl call
    
    * Adding logging
    
    * Different type of escaping
    
    * Fix JOSN
    
    * Use dev branch for opf/design-system storybook publishing
    
    * Add plugin to message path to parent window
    
    * Remove extraneous story
    
    * Add a ton of docs
    
    * Update stories
    
    * Fix syntax error caused by multiple newlines inside of a JSX component
    
    * Add text-field story
    
    * Add basic html stories that don't work yet
    
    * Try to get plain HTML examples working
    
    * HTML Examples work, but slowly revert to components anyway
    
    * Fix HTML examples
    
    * Remove extraneous files
    
    * Put storybook eslint rules back in
    
    * Improve docs
    
    * Show docs tab by default
    
    * Add pullpreview for storybook
    
    * Use the same pullpreview tag for both storybook and normal deployments
    
    * Change name of second pullpreview workflow
    
    * Pin node version to 16.17.0
    
    * Initial update to docs
    
    Added/updated:
    
    Foundation pages:
    
    - Colours (major update)
    - Shadows (minor)
    - Typography (new)
    
    Blocks
    
    - Checkbox (minor)
    - Action bar (major)
    - Buttons (new)
    - Link (major)
    - Modal Dialogue (new)
    - Selector Field (new)
    
    * Make sure all code is available during storybook pp build
    
    * Change storybook pullpreview file name
    
    * Add production target to docker-compose sb pp
    
    * Fix acme check for sb pp
    
    * Only run cd-storybook on dev branch
    
    * Run without https on 8080
    
    * Added intro and new page
    
    - Introduction renamed to "Design System", page rewritten completely
    - Added page "Devices and Accessibility"
    
    * Remove domain from caddy
    
    * Add port to listen command
    
    * Remove double pullpreview workflows
    
    * Added Divider component
    
    * Change sorting of stories
    
    * Update section titles and order for styles and blocks
    
    * add extra action bar story
    
    * Updated organising + new page
    
    - Updated organisation into Styles, Components and Patterns.
    - Added page "Using Storybook" (mostly a skeleton for now)
    
    * Added note about colours not being implemented yet
    
    * Minor
    
    Co-authored-by: Parimal Satyal <88370597+psatyal@users.noreply.github.com>

--- frontend/src/app/spot/components/toggle/toggle.component.ts
+++ frontend/src/app/spot/components/toggle/toggle.component.ts
@@ -29,7 +29,7 @@ export class SpotToggleComponent<T> implements ControlValueAccessor {
 
   @HostBinding('class.spot-toggle') public className = true;
 
-  @Output() checkedChange = new EventEmitter<boolean>();
+  @Output() valueChange = new EventEmitter<T>();
 
   @Input() options:SpotToggleOption<T>[] = [];
 
@@ -56,7 +56,7 @@ export class SpotToggleComponent<T> implements ControlValueAccessor {
 
   onChange = (_:T):void => {};
 
-  onTouched = (_:T):void => {};
+  onTouched: (t:T) => void = (_:T):void => {};
 
   registerOnChange(fn:(_:T) => void):void {
     this.onChange = fn;
---
+++ frontend/src/stories/ActionBar.example.ts
@@ -0,0 +1,6 @@
+import { Component } from '@angular/core';
+
+@Component({
+  templateUrl: './ActionBar.example.html',
+})
+export class SbActionBarExample { }
