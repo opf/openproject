606ed1003b40b55a0fa21e6f0ef43d155a8ec96e
Eric Schubert <e.schubert@openproject.com>
2022-09-29 15:40:52 +0200

    [#43842] added action bar stories
    - reworked styling of action bar to comfort mobile and desktop spacings
    - added additional stories to action bar docs

--- frontend/src/stories/ActionBar.example.ts
+++ frontend/src/stories/ActionBar.example.ts
@@ -1,6 +1,7 @@
-import { Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component } from '@angular/core';
 
 @Component({
   templateUrl: './ActionBar.example.html',
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class SbActionBarExample { }
+export class SbActionBarExample {}
