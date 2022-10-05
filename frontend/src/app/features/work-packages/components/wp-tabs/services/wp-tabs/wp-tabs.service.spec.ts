4ed8bc4e08708966056b0a7ceda5c4b7db479cc3
Eric Schubert <e.schubert@openproject.com>
2022-10-05 09:45:44 +0200

    [#44321] Conditional rendering of work package tabs
    - https://community.openproject.org/work_packages/44321
    - added patchCondition function for service
    - added spec

--- frontend/src/app/features/work-packages/components/wp-tabs/services/wp-tabs/wp-tabs.service.spec.ts
+++ frontend/src/app/features/work-packages/components/wp-tabs/services/wp-tabs/wp-tabs.service.spec.ts
@@ -3,7 +3,6 @@ import { Injector, Input } from '@angular/core';
 
 import { TestBed } from '@angular/core/testing';
 import { WorkPackageResource } from 'core-app/features/hal/resources/work-package-resource';
-import { HalResourceService } from 'core-app/features/hal/services/hal-resource.service';
 import { StateService } from '@uirouter/angular';
 import { WorkPackageTabsService } from 'core-app/features/work-packages/components/wp-tabs/services/wp-tabs/wp-tabs.service';
 import { TabComponent } from '../../components/wp-tab-wrapper/tab';
@@ -12,7 +11,6 @@ describe('WpTabsService', () => {
   let service:WorkPackageTabsService;
   const workPackage:any = { id: 1234 };
   let injector:Injector;
-  let halResourceService:HalResourceService;
 
   class TestComponent implements TabComponent {
     @Input() public workPackage:WorkPackageResource;
@@ -56,9 +54,21 @@ describe('WpTabsService', () => {
 
   describe('getTab()', () => {
     it('returns the displayable tab with the correct identifier', () => {
-      expect(service.getTab('displayable-test-tab', workPackage)?.id).toEqual('displayable-test-tab');
+      expect(service.getTab('displayable-test-tab', workPackage)?.id).toEqual('displayable-test-ta');
       expect(service.getTab('non-existing-tab', workPackage)).toEqual(undefined);
       expect(service.getTab('non-displayable-test-tab', workPackage)).toEqual(undefined);
+    });
+  });
+
+  describe('patchTabDefinition()', () => {
+    it('must change the display condition and return accordingly', () => {
+      service.patchTabCondition('displayable-test-tab', () => false);
+      service.patchTabCondition('not-displayable-test-tab', () => true);
+
+      const displayableTabs = service.getDisplayableTabs(workPackage);
+
+      expect(displayableTabs).toHaveSize(1);
+      expect(displayableTabs).toContain(notDisplayableTab);
     });
   });
 });
