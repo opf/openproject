f228f7b86dca633a207aaa49a45109398d8ad612
Markus Kahl <machisuji@gmail.com>
2022-10-26 13:29:49 +0200

    change id seq type too where necessary

--- db/migrate/20221017073431_remove_orphaned_tokens.rb
+++ db/migrate/20221017073431_remove_orphaned_tokens.rb
@@ -1,11 +1,13 @@
+require_relative "./migration_utils/column"
+
 class RemoveOrphanedTokens < ActiveRecord::Migration[7.0]
   def up
     Token::Base.where.not(user_id: User.select(:id)).delete_all
 
     # Make sure we have bigint columns on both sides so the foreign key can be added.
     # It could be that they are of type numeric if the data was migrated from MySQL once.
-    make_bigint! :users, :id
-    make_bigint! :tokens, :user_id
+    change_column_type! :users, :id, :bigint
+    change_column_type! :tokens, :user_id, :bigint
 
     add_foreign_key :tokens, :users
   end
@@ -14,17 +16,7 @@ class RemoveOrphanedTokens < ActiveRecord::Migration[7.0]
     # Nothing to do
   end
 
-  def make_bigint!(table, field)
-    return if bigint? table, field
-
-    change_column table, field, :bigint
-  end
-
-  def bigint?(table, field)
-    get_column_type(table, field).to_s == "bigint"
-  end
-
-  def get_column_type(table, field)
-    ActiveRecord::Base.connection.columns(table.to_s).find { |col| col.name == field.to_s }&.sql_type
+  def change_column_type!(table, column, type)
+    Migration::MigrationUtils::Column.new(connection, table, column).change_type! type
   end
 end
