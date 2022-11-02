c9f9a534f6ca87d800fd3640041245f80e4428a1
Benjamin Bädorf <b.baedorf@openproject.com>
2022-11-02 10:27:35 +0100

    Change asset loading for storybook
    Storybook assets cannot currently be loaded by merging directories,
    so this fix makes sure files are loaded individually.
    
    https://github.com/storybookjs/storybook/issues/18686

--- frontend/.storybook/main.js
+++ frontend/.storybook/main.js
@@ -1,3 +1,5 @@
+const path = require('path');
+
 module.exports = {
   stories: [
     "../src/**/*.stories.mdx",
@@ -23,11 +25,35 @@ module.exports = {
   },
   staticDirs: [
     // Copy local static assets
-    { from: '../src/stories/assets/', to: '/assets' },
+    '../src/stories/assets/logo_openproject.png',
+    '../src/stories/assets/logo_openproject_spot.png',
     
     // Copy font files to specific locations so the normal core SASS 
     // will load the files correctly without having to use variables
-    { from: '../src/assets/fonts/openproject_icon/', to: '/assets/frontend/' },
-    { from: '../src/assets/fonts/lato/', to: '/assets/frontend/' },
-  ],
+    '../src/assets/fonts/openproject_icon/openproject-icon-font.ttf',
+    '../src/assets/fonts/openproject_icon/openproject-icon-font.svg',
+    '../src/assets/fonts/openproject_icon/openproject-icon-font.eot',
+    '../src/assets/fonts/openproject_icon/openproject-icon-font.woff',
+    '../src/assets/fonts/openproject_icon/openproject-icon-font.woff2',
+
+    '../src/assets/fonts/lato/Lato-Regular.woff',
+    '../src/assets/fonts/lato/Lato-Regular.woff2',
+
+    '../src/assets/fonts/lato/Lato-Bold.woff',
+    '../src/assets/fonts/lato/Lato-Bold.woff2',
+
+    '../src/assets/fonts/lato/Lato-Light.woff',
+    '../src/assets/fonts/lato/Lato-Light.woff2',
+
+    '../src/assets/fonts/lato/Lato-Italic.woff',
+    '../src/assets/fonts/lato/Lato-Italic.woff2',
+
+    '../src/assets/fonts/lato/Lato-BoldItalic.woff',
+    '../src/assets/fonts/lato/Lato-BoldItalic.woff2',
+
+    '../src/assets/fonts/lato/Lato-LightItalic.woff',
+    '../src/assets/fonts/lato/Lato-LightItalic.woff2',
+  ].map((from) => ({
+    from, to: path.join('/assets/frontend/', path.basename(from)),
+  })),
 };
