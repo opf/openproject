67dbb689fdea3d3335fdfa698a6eb312803f31e5
Oliver Günther <mail@oliverguenther.de>
2022-11-08 14:38:51 +0100

    Extend spec to check for double alerts

--- spec/features/notifications/notification_center/notification_center_date_alerts_spec.rb
+++ spec/features/notifications/notification_center/notification_center_date_alerts_spec.rb
@@ -4,83 +4,107 @@ describe "Notification center date alerts", js: true, with_settings: { journal_a
   create_shared_association_defaults_for_work_package_factory
 
   shared_let(:project) { project_with_types }
-  shared_let(:role) { create :role, permissions: %i[view_work_packages edit_work_packages work_package_assigned] }
-  shared_let(:membership) { create :member, principal: user, project: project_with_types, roles: [role] }
-  shared_let(:milestone_type) { create :type_milestone }
+  shared_let(:role) { create(:role, permissions: %i[view_work_packages edit_work_packages work_package_assigned]) }
+  shared_let(:membership) { create(:member, principal: user, project: project_with_types, roles: [role]) }
+  shared_let(:milestone_type) { create(:type_milestone) }
 
-  shared_let(:milestone_wp_past) { create :work_package, project:, type: milestone_type, due_date: 2.days.ago }
-  shared_let(:milestone_wp_future) { create :work_package, project:, type: milestone_type, due_date: 1.day.from_now }
+  shared_let(:milestone_wp_past) do
+    create(:work_package, subject: 'Milestone WP past', project:, type: milestone_type, due_date: 2.days.ago)
+  end
+  shared_let(:milestone_wp_future) do
+    create(:work_package, subject: 'Milestone WP future', project:, type: milestone_type, due_date: 1.day.from_now)
+  end
 
-  shared_let(:wp_start_past) { create :work_package, project:, start_date: 1.day.ago }
-  shared_let(:wp_start_future) { create :work_package, project:, start_date: 2.days.from_now }
+  shared_let(:wp_start_past) { create(:work_package, subject: 'WP start past', project:, start_date: 1.day.ago) }
+  shared_let(:wp_start_future) { create(:work_package, subject: 'WP start future', project:, start_date: 2.days.from_now) }
 
-  shared_let(:wp_due_past) { create :work_package, project:, due_date: 3.days.ago }
-  shared_let(:wp_due_future) { create :work_package, project:, due_date: 3.days.from_now }
+  shared_let(:wp_due_past) { create(:work_package, subject: 'WP due past', project:, due_date: 3.days.ago) }
+  shared_let(:wp_due_future) { create(:work_package, subject: 'WP due future', project:, due_date: 3.days.from_now) }
 
-  shared_let(:wp_double_notification) { create :work_package, project:, due_date: 1.days.from_now }
+  shared_let(:wp_double_notification) { create(:work_package, subject: 'Alert + Mention', project:, due_date: 1.day.from_now) }
+
+  shared_let(:wp_double_alert) do
+    create(:work_package, subject: 'Double alert', project:, start_date: 1.day.ago, due_date: 1.day.from_now)
+  end
 
   shared_let(:notification_milestone_past) do
-    create :notification,
+    create(:notification,
            reason: :date_alert_due_date,
            recipient: user,
            resource: milestone_wp_past,
-           project:
+           project:)
   end
 
   shared_let(:notification_milestone_future) do
-    create :notification,
+    create(:notification,
            reason: :date_alert_due_date,
            recipient: user,
            resource: milestone_wp_future,
-           project:
+           project:)
   end
 
   shared_let(:notification_wp_start_past) do
-    create :notification,
+    create(:notification,
            reason: :date_alert_start_date,
            recipient: user,
            resource: wp_start_past,
-           project:
+           project:)
   end
 
   shared_let(:notification_wp_start_future) do
-    create :notification,
+    create(:notification,
            reason: :date_alert_start_date,
            recipient: user,
            resource: wp_start_future,
-           project:
+           project:)
   end
 
   shared_let(:notification_wp_due_past) do
-    create :notification,
+    create(:notification,
            reason: :date_alert_due_date,
            recipient: user,
            resource: wp_due_past,
-           project:
+           project:)
   end
 
   shared_let(:notification_wp_due_future) do
-    create :notification,
+    create(:notification,
            reason: :date_alert_due_date,
            recipient: user,
            resource: wp_due_future,
-           project:
+           project:)
   end
 
   shared_let(:notification_wp_double_date_alert) do
-    create :notification,
+    create(:notification,
            reason: :date_alert_due_date,
            recipient: user,
            resource: wp_double_notification,
-           project:
+           project:)
   end
 
   shared_let(:notification_wp_double_mention) do
-    create :notification,
+    create(:notification,
            reason: :mentioned,
            recipient: user,
            resource: wp_double_notification,
-           project:
+           project:)
+  end
+
+  shared_let(:notification_wp_double_alerts) do
+    due = create(:notification,
+                 reason: :date_alert_due_date,
+                 recipient: user,
+                 resource: wp_double_alert,
+                 project:)
+
+    start = create(:notification,
+                   reason: :date_alert_start_date,
+                   recipient: user,
+                   resource: wp_double_alert,
+                   project:)
+
+    [start, due]
   end
 
   let(:center) { ::Pages::Notifications::Center.new }
@@ -109,6 +133,15 @@ describe "Notification center date alerts", js: true, with_settings: { journal_a
     side_menu.click_item 'Date alert'
     center.expect_item(notification_wp_double_date_alert, 'Finish date is in 1 day')
     center.expect_no_item(notification_wp_double_mention)
+
+    # Ensure that start is created later than due for implicit ID sorting
+    double_alert_start, double_alert_due = notification_wp_double_alerts
+    expect(double_alert_start.id).to be > double_alert_due.id
+
+    # We see that start is actually the newest ID, hence shown as the primary notification
+    # but the date alert still shows the finish date
+    center.expect_item(double_alert_start, 'Finish date is in 1 day')
+    center.expect_no_item(double_alert_due)
 
     # When a work package is updated to a different date
     wp_double_notification.update_column(:due_date, 5.days.from_now)
