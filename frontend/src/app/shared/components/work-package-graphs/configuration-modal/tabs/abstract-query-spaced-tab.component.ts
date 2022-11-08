9f4562ee7cc1d0d2f78b72db275badeb5bdb14bd
ulferts <jens.ulferts@googlemail.com>
2022-11-08 09:41:24 +0100

    only test

--- frontend/src/app/shared/components/work-package-graphs/configuration-modal/tabs/abstract-query-spaced-tab.component.ts
+++ frontend/src/app/shared/components/work-package-graphs/configuration-modal/tabs/abstract-query-spaced-tab.component.ts
@@ -3,6 +3,9 @@ import { I18nService } from 'core-app/core/i18n/i18n.service';
 import { WorkPackageStatesInitializationService } from 'core-app/features/work-packages/components/wp-list/wp-states-initialization.service';
 import { WpGraphConfigurationService } from 'core-app/shared/components/work-package-graphs/configuration/wp-graph-configuration.service';
 
+
+// some text added just for the CI
+
 export abstract class QuerySpacedTabComponent {
   constructor(readonly I18n:I18nService,
     readonly wpStatesInitialization:WorkPackageStatesInitializationService,
