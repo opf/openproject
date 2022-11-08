f71a441999c40194dabdb015921204a020d76038
Eric Schubert <e.schubert@openproject.com>
2022-11-08 09:37:58 +0100

    [chore] added flag to feature spec

--- modules/storages/spec/features/create_file_links_spec.rb
+++ modules/storages/spec/features/create_file_links_spec.rb
@@ -28,7 +28,7 @@
 
 require_relative '../spec_helper'
 
-describe 'Creating file links in work package', js: true, webmock: true do
+describe 'Creating file links in work package', js: true, webmock: true, with_flag: { storage_file_linking: true } do
   let(:permissions) { %i(view_work_packages edit_work_packages view_file_links manage_file_links) }
   let(:project) { create(:project) }
   let(:current_user) { create(:user, member_in_project: project, member_with_permissions: permissions) }
