9d891d1d58dbd31db9ecf0731c2bb89f83a347c5
Eric Schubert <e.schubert@openproject.com>
2022-11-11 09:44:09 +0100

    [fix] replaced filter with helper function

--- frontend/src/app/features/enterprise/enterprise-trial.service.ts
+++ frontend/src/app/features/enterprise/enterprise-trial.service.ts
@@ -1,12 +1,47 @@
+// -- copyright
+// OpenProject is an open source project management software.
+// Copyright (C) 2012-2022 the OpenProject GmbH
+//
+// This program is free software; you can redistribute it and/or
+// modify it under the terms of the GNU General Public License version 3.
+//
+// OpenProject is a fork of ChiliProject, which is a fork of Redmine. The copyright follows:
+// Copyright (C) 2006-2013 Jean-Philippe Lang
+// Copyright (C) 2010-2013 the ChiliProject Team
+//
+// This program is free software; you can redistribute it and/or
+// modify it under the terms of the GNU General Public License
+// as published by the Free Software Foundation; either version 2
+// of the License, or (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program; if not, write to the Free Software
+// Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+//
+// See COPYRIGHT and LICENSE files for more details.
+//++
+
 import { Injectable } from '@angular/core';
-import { I18nService } from 'core-app/core/i18n/i18n.service';
+import { FormGroup } from '@angular/forms';
 import {
   HttpClient,
   HttpErrorResponse,
 } from '@angular/common/http';
+import { Query } from '@datorama/akita';
+import {
+  filter,
+  map,
+  shareReplay,
+} from 'rxjs/operators';
+
+import { I18nService } from 'core-app/core/i18n/i18n.service';
 import { PathHelperService } from 'core-app/core/path-helper/path-helper.service';
 import { ToastService } from 'core-app/shared/components/toaster/toast.service';
-import { FormGroup } from '@angular/forms';
 import { EXTERNAL_REQUEST_HEADER } from 'core-app/features/hal/http/openproject-header-interceptor';
 import { EnterpriseTrialStore } from 'core-app/features/enterprise/enterprise-trial.store';
 import { GonType } from 'core-app/core/gon/gon.service';
@@ -16,12 +51,7 @@ import {
   IEnterpriseData,
   IEnterpriseTrial,
 } from 'core-app/features/enterprise/enterprise-trial.model';
-import { Query } from '@datorama/akita';
-import {
-  filter,
-  map,
-  shareReplay,
-} from 'rxjs/operators';
+import isDefinedEntity from 'core-app/core/state/is-defined-entity';
 
 @Injectable()
 export class EnterpriseTrialService {
@@ -39,9 +69,7 @@ export class EnterpriseTrialService {
 
   userData$ = this.query
     .select('data')
-    .pipe(
-      filter((data) => data !== undefined),
-    );
+    .pipe(filter(isDefinedEntity));
 
   emailError$ = this
     .query
