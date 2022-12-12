18b73ae4149d6e2b42e1b4cd64be95acbc4af04e
bsatarnejad <b.satarnejad@gmail.com>
2022-12-12 17:03:56 +0100

    open confirmation modal in list of non-working days component while submitting the form

--- frontend/src/app/shared/components/op-non-working-days-list/op-non-working-days-list.component.ts
+++ frontend/src/app/shared/components/op-non-working-days-list/op-non-working-days-list.component.ts
@@ -4,6 +4,7 @@ import {
   ElementRef,
   HostBinding,
   Injector,
+  OnInit,
   ViewChild,
   ViewEncapsulation,
 } from '@angular/core';
@@ -23,6 +24,8 @@ import { DayResourceService } from 'core-app/core/state/days/day.service';
 import { IDay } from 'core-app/core/state/days/day.model';
 import { CalendarViewEvent } from 'core-app/features/calendar/op-work-packages-calendar.service';
 import { opIconElement } from 'core-app/shared/helpers/op-icon-builder';
+import { ConfirmDialogService } from 'core-app/shared/components/modals/confirm-dialog/confirm-dialog.service';
+import { ConfirmDialogOptions } from '../modals/confirm-dialog/confirm-dialog.modal';
 
 export const nonWorkingDaysListSelector = 'op-non-working-days-list';
 
@@ -33,7 +36,7 @@ export const nonWorkingDaysListSelector = 'op-non-working-days-list';
   styleUrls: ['./op-non-working-days-list.component.sass'],
   templateUrl: './op-non-working-days-list.component.html',
 })
-export class OpNonWorkingDaysListComponent {
+export class OpNonWorkingDaysListComponent implements OnInit {
   @ViewChild(FullCalendarComponent) ucCalendar:FullCalendarComponent;
 
   @HostBinding('class.op-non-working-days-list') className = true;
@@ -41,8 +44,20 @@ export class OpNonWorkingDaysListComponent {
   text = {
     empty_state_header: this.I18n.t('js.admin.working_days.calendar.empty_state_header'),
     empty_state_description: this.I18n.t('js.admin.working_days.calendar.empty_state_description'),
+    new_date: this.I18n.t('js.admin.working_days.calendar.new_date'),
     add_non_working_day: this.I18n.t('js.admin.working_days.add_non_working_day'),
+    change_description: this.I18n.t('js.admin.working_days.change_description'),
+    warning: this.I18n.t('js.admin.working_days.warning'),
+    change_button: this.I18n.t('js.admin.working_days.change_button'),
+    change_title: this.I18n.t('js.admin.working_days.change_title'),
+    removed_title: this.I18n.t('js.admin.working_days.removed_title'),
   };
+
+  form_submitted = false;
+
+  nonWorkingDays:IDay[] = [];
+
+  removedNonWorkingDays:string[] = [];
 
   calendarOptions:CalendarOptions = {
     plugins: [listPlugin],
@@ -64,6 +79,7 @@ export class OpNonWorkingDaysListComponent {
       anchor.appendChild(opIconElement('icon', 'icon-delete'));
       anchor.addEventListener('click', () => {
         event.remove();
+        this.removedNonWorkingDays.push(event.id);
       });
       td.appendChild(anchor);
       el.appendChild(td);
@@ -81,8 +97,36 @@ export class OpNonWorkingDaysListComponent {
     readonly pathHelper:PathHelperService,
     readonly apiV3Service:ApiV3Service,
     readonly dayService:DayResourceService,
+    readonly confirmDialogService:ConfirmDialogService,
   ) {
     populateInputsFromDataset(this);
+  }
+
+  ngOnInit():void {
+    document.addEventListener('submit', (evt:Event) => {
+      if (this.form_submitted === false) {
+        this.form_submitted = true;
+        const target = evt.target as HTMLFormElement;
+        const options:ConfirmDialogOptions = {
+          text: {
+            text: this.text.change_description,
+            title: this.text.change_title,
+            button_continue: this.text.change_button,
+          },
+          dangerHighlighting: true,
+          divideContent: true,
+          showListData: this.removedNonWorkingDays.length > 0,
+          warningText: this.text.warning,
+          passedData: this.removedNonWorkingDays,
+          listTitle: this.text.removed_title,
+        };
+        evt.preventDefault();
+        void this.confirmDialogService.confirm(options).then(() => {
+          this.form_submitted = false;
+          target.submit();
+        });
+      }
+    });
   }
 
   public calendarEventsFunction(
@@ -94,6 +138,9 @@ export class OpNonWorkingDaysListComponent {
       .subscribe(
         (days:IDay[]) => {
           const events = this.mapToCalendarEvents(days);
+          // test
+          this.nonWorkingDays = this.nonWorkingDays.concat(days);
+          this.nonWorkingDays = this.nonWorkingDays.filter((item, pos) => this.nonWorkingDays.indexOf(item) === pos);
           successCallback(events);
         },
         failureCallback,
@@ -104,10 +151,17 @@ export class OpNonWorkingDaysListComponent {
     return nonWorkingDays.map((NWD:IDay) => ({
       title: NWD.name,
       start: NWD.date,
+      id: NWD.id,
     })).filter((event) => !!event) as EventInput[];
   }
 
   public addNonWorkingDay():void {
     // opens date picker modal
+
+    // now I am just testing adding new event to the calendar, will be removed
+    const day = { start: '2022-12-22', title: 'test' };
+    const api = this.ucCalendar.getApi();
+    this.nonWorkingDays.push(day as unknown as IDay);
+    api.addEvent({ ...day });
   }
 }
