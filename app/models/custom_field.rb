opf/openproject

commit 8d46af092c33ede1e5f94b64d171c5926c04e2bf

[#43553] Move workpackage in other project error
https://community.openproject.org/work_packages/43553

app/models/custom_field.rb
@@ -28,7 +28,7 @@
 
 class CustomField < ApplicationRecord
   include CustomField::OrderStatements
-
+  scope :required, -> { where(is_required: true) }
   has_many :custom_values, dependent: :delete_all
   # WARNING: the inverse_of option is also required in order
   # for the 'touch: true' option on the custom_field association in CustomOption
@@ -41,7 +41,7 @@ class CustomField < ApplicationRecord
            inverse_of: 'custom_field'
   accepts_nested_attributes_for :custom_options
 
-  acts_as_list scope: 'type = \'#{self.class}\''
+  acts_as_list scope: "type = '#{self.class}'"
 
   validates :field_format, presence: true
   validates :custom_options,
@@ -140,7 +140,7 @@ class CustomField < ApplicationRecord
 
   def value_of(value)
     if list?
-      custom_options.where(value:).pluck(:id).first
+      custom_options.where(value:).pick(:id)
     else
       CustomValue.new(custom_field: self, value:).valid? && value
     end
@@ -303,9 +303,9 @@ class CustomField < ApplicationRecord
 
   def possible_values_from_arg(arg)
     if arg.is_a?(Array)
-      arg.compact.map(&:strip).reject(&:blank?)
+      arg.compact.map(&:strip).compact_blank
     else
-      arg.to_s.split(/[\n\r]+/).map(&:strip).reject(&:blank?)
+      arg.to_s.split(/[\n\r]+/).map(&:strip).compact_blank
     end
   end
 

app/controllers/work_packages/moves_controller.rb
@@ -58,7 +58,7 @@ class WorkPackages::MovesController < ApplicationController
 
     klass
       .new(user: current_user, work_packages: @work_packages)
-      .call(permitted_create_params)
+      .call(attributes_for_create)
   end
 
   def redirect_after_create(result)
@@ -101,26 +101,18 @@ class WorkPackages::MovesController < ApplicationController
     @target_project = @allowed_projects.detect { |p| p.id.to_s == params[:new_project_id].to_s } if params[:new_project_id]
     @target_project ||= @project
     @types = @target_project.types
+    @target_type = @types.find { |t| t.id.to_s == params[:new_type_id].to_s }
     @available_versions = @target_project.assignable_versions
     @available_statuses = Workflow.available_statuses(@project)
     @notes = params[:notes] || ''
   end
 
-  def permitted_create_params
-    params
-      .permit(:assigned_to_id,
-              :responsible_id,
-              :start_date,
-              :due_date,
-              :status_id,
-              :version_id,
-              :priority_id)
-      .to_h
-      .merge(type_id: params[:new_type_id],
-             project_id: params[:new_project_id],
-             journal_notes: params[:notes])
+  def attributes_for_create
+    permitted_params
+      .move_work_package
       .compact_blank
       # 'none' is used in the frontend as a value to unset the property, e.g. the assignee.
       .transform_values { |v| v == 'none' ? nil : v }
+      .to_h
   end
 end

app/models/permitted_params.rb
@@ -124,6 +124,16 @@ class PermittedParams
     permitted_params.merge(custom_field_values(:work_package))
   end
 
+  def move_work_package(args = {})
+    permitted = permitted_attributes(:move_work_package, args)
+    permitted_params = params.permit(*permitted)
+    permitted_params
+      .merge(custom_field_values(required: false))
+      .merge(type_id: params[:new_type_id],
+             project_id: params[:new_project_id],
+             journal_notes: params[:notes])
+  end
+
   def member
     params.require(:member).permit(*self.class.permitted_attributes[:member])
   end
@@ -133,7 +143,7 @@ class PermittedParams
       scopes = app_params[:scopes]
 
       if scopes.present?
-        app_params[:scopes] = scopes.reject(&:blank?).join(" ")
+        app_params[:scopes] = scopes.compact_blank.join(" ")
       end
 
       app_params
@@ -383,12 +393,8 @@ class PermittedParams
 
   protected
 
-  def custom_field_values(key, required: true)
-    # a hash of arbitrary values is not supported by strong params
-    # thus we do it by hand
-    object = required ? params.require(key) : params.fetch(key, {})
-    values = object[:custom_field_values] || ActionController::Parameters.new
-
+  def custom_field_values(key = nil, required: true)
+    values = build_custom_field_values(key, required:)
     # only permit values following the schema
     # 'id as string' => 'value as string'
     values.select! { |k, v| k.to_i > 0 && (v.is_a?(String) || v.is_a?(Array)) }
@@ -512,8 +518,7 @@ class PermittedParams
           :subject,
           Proc.new do |args|
             # avoid costly allowed_to? if the param is not there at all
-            if args[:params]['work_package'] &&
-               args[:params]['work_package'].has_key?('watcher_user_ids') &&
+            if args[:params]['work_package']&.has_key?('watcher_user_ids') &&
                args[:current_user].allowed_to?(:add_work_package_watchers, args[:project])
 
               { watcher_user_ids: [] }
@@ -522,6 +527,15 @@ class PermittedParams
           # attributes unique to :new_work_package
           :journal_notes,
           :lock_version
+        ],
+        move_work_package: %i[
+          assigned_to_id
+          responsible_id
+          start_date
+          due_date
+          status_id
+          version_id
+          priority_id
         ],
         oauth_application: [
           :name,
@@ -622,5 +636,18 @@ class PermittedParams
     (hash || {})
       .keys
       .select { |k| list.any? { |whitelisted| whitelisted.to_s == k.to_s || whitelisted === k } }
+  end
+
+  private
+
+  def build_custom_field_values(key, required:)
+    # When the key is false, it means we do not have a parent key,
+    # so we are searching the whole params hash.
+    key_to_fetch = key || :custom_field_values
+    # a hash of arbitrary values is not supported by strong params
+    # thus we do it by hand
+    object = required ? params.require(key_to_fetch) : params.fetch(key_to_fetch, {})
+    values = key ? object[:custom_field_values] : object
+    values || ActionController::Parameters.new
   end
 end

app/views/work_packages/moves/new.html.erb
@@ -44,7 +44,7 @@ See COPYRIGHT and LICENSE files for more details.
 
 <%= styled_form_tag({action: 'create'},
                     id: 'move_form',
-                    data: { 'refresh-url': url_for(action: 'new'), 'input-selector': '[name="new_project_id"]' },
+                    data: { 'refresh-url': url_for(action: 'new'), 'input-selector': '[name="new_project_id"],[name="new_type_id"]' },
                     class: '-wide-labels augment--refresh-on-form-changes') do %>
   <% @work_packages.each do |wp| %>
     <%= hidden_field_tag 'ids[]', wp.id %>
@@ -78,7 +78,7 @@ See COPYRIGHT and LICENSE files for more details.
               <div class="form--field-container">
                 <%= styled_select_tag("new_type_id",
                        content_tag('option', t(:label_no_change_option), value: '') +
-                       options_from_collection_for_select(@types, "id", "name")) %>
+                       options_from_collection_for_select(@types, "id", "name", @target_type&.id)) %>
               </div>
             </div>
             <div class="form--field">
@@ -143,6 +143,16 @@ See COPYRIGHT and LICENSE files for more details.
                 <%= styled_text_field_tag 'due_date', '', size: 10, class: '-augmented-datepicker' %>
               </div>
             </div>
+            <% if @target_type %>
+              <% @target_type.custom_fields.required.each do |custom_field| %>
+                <div class="form--field">
+                  <%= blank_custom_field_label_tag('', custom_field) %>
+                  <div class="form--field-container">
+                    <%= custom_field_tag_for_bulk_edit('', custom_field, @project) %>
+                  </div>
+                </div>
+              <% end %>
+            <% end %>
           </div>
         </div>
       </fieldset>

spec/features/work_packages/bulk/move_work_package_spec.rb
@@ -6,30 +6,30 @@ describe 'Moving a work package through Rails view', js: true do
   include ::Components::Autocompleter::NgSelectAutocompleteHelpers
 
   let(:dev_role) do
-    create :role,
-           permissions: %i[view_work_packages add_work_packages]
+    create(:role,
+           permissions: %i[view_work_packages add_work_packages])
   end
   let(:mover_role) do
-    create :role,
-           permissions: %i[view_work_packages move_work_packages manage_subtasks add_work_packages]
+    create(:role,
+           permissions: %i[view_work_packages move_work_packages manage_subtasks add_work_packages])
   end
   let(:dev) do
-    create :user,
+    create(:user,
            firstname: 'Dev',
            lastname: 'Guy',
            member_in_project: project,
-           member_through_role: dev_role
+           member_through_role: dev_role)
   end
   let(:mover) do
-    create :admin,
+    create(:admin,
            firstname: 'Manager',
            lastname: 'Guy',
            member_in_project: project,
-           member_through_role: mover_role
+           member_through_role: mover_role)
   end
 
-  let(:type) { create :type, name: 'Bug' }
-  let(:type2) { create :type, name: 'Risk' }
+  let(:type) { create(:type, name: 'Bug') }
+  let(:type2) { create(:type, name: 'Risk') }
 
   let!(:project) { create(:project, name: 'Source', types: [type, type2]) }
   let!(:project2) { create(:project, name: 'Target', types: [type, type2]) }
@@ -89,17 +89,17 @@ describe 'Moving a work package through Rails view', js: true do
                             select_text: 'Target',
                             results_selector: 'body'
         SeleniumHubWaiter.wait
+      end
 
+      it 'moves parent and child wp to a new project' do
         # Clicking move and follow might be broken due to the location.href
         # in the refresh-on-form-changes component
         retry_block do
           click_on 'Move and follow'
           page.find('.inline-edit--container.subject', text: work_package.subject, wait: 10)
-          page.find('#projects-menu', text: 'Target')
+          page.find_by_id('projects-menu', text: 'Target')
         end
-      end
 
-      it 'moves parent and child wp to a new project' do
         # Should move its children
         child_wp.reload
         expect(child_wp.project_id).to eq(project2.id)
@@ -109,7 +109,63 @@ describe 'Moving a work package through Rails view', js: true do
         let!(:project2) { create(:project, name: 'Target', types: [type2]) }
 
         it 'does moves the work package and changes the type' do
+          # Clicking move and follow might be broken due to the location.href
+          # in the refresh-on-form-changes component
+          retry_block do
+            click_on 'Move and follow'
+            page.find('.inline-edit--container.subject', text: work_package.subject, wait: 10)
+            page.find_by_id('projects-menu', text: 'Target')
+          end
+
           # Should NOT have moved
+          child_wp.reload
+          work_package.reload
+          expect(work_package.project_id).to eq(project2.id)
+          expect(work_package.type_id).to eq(type2.id)
+          expect(child_wp.project_id).to eq(project2.id)
+          expect(child_wp.type_id).to eq(type2.id)
+        end
+      end
+
+      context 'when the target project has a type with a required field' do
+        let(:required_cf) { create(:int_wp_custom_field, is_required: true) }
+        let(:type2) { create(:type, name: 'Risk', custom_fields: [required_cf]) }
+        let!(:project2) { create(:project, name: 'Target', types: [type2], work_package_custom_fields: [required_cf]) }
+
+        it 'does not moves the work package when the required field is missing' do
+          select "Risk", from: "Type"
+          expect(page).to have_field(required_cf.name)
+
+          # Clicking move and follow might be broken due to the location.href
+          # in the refresh-on-form-changes component
+          retry_block do
+            click_on 'Move and follow'
+          end
+
+          expect(page)
+            .to have_selector('.flash.error',
+                              text: I18n.t(:'work_packages.bulk.none_could_be_saved',
+                                           total: 1))
+          child_wp.reload
+          work_package.reload
+          expect(work_package.project_id).to eq(project.id)
+          expect(work_package.type_id).to eq(type.id)
+          expect(child_wp.project_id).to eq(project.id)
+          expect(child_wp.type_id).to eq(type.id)
+        end
+
+        it 'does moves the work package when the required field is set' do
+          select "Risk", from: "Type"
+          fill_in required_cf.name, with: '1'
+
+          # Clicking move and follow might be broken due to the location.href
+          # in the refresh-on-form-changes component
+          retry_block do
+            click_on 'Move and follow'
+          end
+
+          expect(page).to have_selector('.flash.notice')
+
           child_wp.reload
           work_package.reload
           expect(work_package.project_id).to eq(project2.id)

commit b8dd2336fe50996ff9a7db9d1fb51b8de59119ce

Fix scope attribute for custom_field
This was eval'd and got broken in 8d46af092c33ede1e5f94b64d171c5926c04e2bf

Instead of relying on eval, just use the type

app/models/custom_field.rb
@@ -41,7 +41,7 @@ class CustomField < ApplicationRecord
            inverse_of: 'custom_field'
   accepts_nested_attributes_for :custom_options
 
-  acts_as_list scope: "type = '#{self.class}'"
+  acts_as_list scope: [:type]
 
   validates :field_format, presence: true
   validates :custom_options,