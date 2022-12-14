78542900e08301be7a9debbb0540ebe4611500b3
Torben Köhler <tkoehler@keycomp.com>
2022-12-14 12:39:20 +0100

    Added shortcut to open and close detail view in work-package table

--- frontend/src/app/features/work-packages/components/wp-buttons/wp-details-view-button/wp-details-view-button.component.ts
+++ frontend/src/app/features/work-packages/components/wp-buttons/wp-details-view-button/wp-details-view-button.component.ts
@@ -35,6 +35,7 @@ import { AbstractWorkPackageButtonComponent } from 'core-app/features/work-packa
 import { I18nService } from 'core-app/core/i18n/i18n.service';
 import { States } from 'core-app/core/states/states.service';
 import { KeepTabService } from '../../wp-single-view-tabs/keep-tab/keep-tab.service';
+import * as Mousetrap from 'mousetrap';
 
 @Component({
   templateUrl: '../wp-button.template.html',
@@ -80,11 +81,17 @@ export class WorkPackageDetailsViewButtonComponent extends AbstractWorkPackageBu
       this.isActive = this.$state.includes(this.activeState);
       this.cdRef.detectChanges();
     });
+    
+    Mousetrap.bind(['i', 'I'], (e) => {
+      this.performAction(e);
+    });
   }
 
   public ngOnDestroy() {
     super.ngOnDestroy();
     this.transitionListener();
+    
+    Mousetrap.unbind(['i', 'I']);
   }
 
   public get label():string {
