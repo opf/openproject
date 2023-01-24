3c853958d2eee43a1acf9185ca9b762324ab8b36
Benjamin Bädorf <b.baedorf@openproject.com>
2023-01-24 10:37:26 +0100

    Use op-single-date-picker for work package bulk edit form

--- app/helpers/custom_fields_helper.rb
+++ app/helpers/custom_fields_helper.rb
@@ -150,7 +150,11 @@ module CustomFieldsHelper
     field_format = OpenProject::CustomFieldFormat.find_by_name(custom_field.field_format)
     case field_format.try(:edit_as)
     when 'date'
-      styled_text_field_tag(field_name, '', id: field_id, size: 10, class: '-augmented-datepicker')
+      angular_component_tag 'op-single-date-picker',
+                            inputs: {
+                              id: field_id,
+                              name: field_name,
+                            }
     when 'text'
       styled_text_area_tag(field_name, '', id: field_id, rows: 3, with_text_formatting: true)
     when 'bool'
