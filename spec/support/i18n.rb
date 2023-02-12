fd439484da73cf8d5b67e0eb337c2f5096d57640
Sebastian Fiedlschuster <git@fiedlschuster.de>
2023-02-12 22:18:08 +0100

    specs: on `beginning_of_week: :locale`, set `Date.beginning_of_week`
    https://github.com/opf/openproject/pull/12066

--- spec/support/i18n.rb
+++ spec/support/i18n.rb
@@ -1,11 +1,24 @@
 RSpec.configure do |config|
-
   # Change `Date.beginning_of_week` in the context of the specs
   # according to the current locale.
   #
+  # Usage:
+  #
+  #    context "with Date.beginning_of_week based on I18n.locale", beginning_of_week: :locale
+  #    context "with Date.beginning_of_week set on :sunday", beginning_of_week: :sunday
+  #
   # See also: https://github.com/opf/openproject/pull/12066
   #
-  config.append_before do
+  config.append_before do |example|
+    beginning_of_week = example.metadata[:beginning_of_week]
+    if beginning_of_week == :locale
+      set_beginning_of_week_based_on_locale
+    elsif beginning_of_week.present? and beginning_of_week.is_a? Symbol
+      Date.beginning_of_week = beginning_of_week
+    end
+  end
+
+  def set_beginning_of_week_based_on_locale
     case I18n.locale
     when :en
       Date.beginning_of_week = :sunday
