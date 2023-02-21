2ae16f9837ddf58062db1619d0492a08c7effa99
Dombi Attila <83396+dombesz@users.noreply.github.com>
2023-02-21 16:49:34 +0200

    Fix rubocop errors

--- spec/support/components/datepicker/month_range_selection.rb
+++ spec/support/components/datepicker/month_range_selection.rb
@@ -7,23 +7,21 @@ module Components
       retry_block do
         # This is for a double-month datepicker
         current_month_element = flatpickr_container.all('.cur-month', wait: 0).first
-        if current_month_element == nil
-          # This is for a single-month datepicker
-          current_month = flatpickr_container.first('.flatpickr-monthDropdown-months').value.to_i + 1
-        else
-          current_month = Date::MONTHNAMES.index(current_month_element.text)
-        end
+        current_month = if current_month_element
+                          Date::MONTHNAMES.index(current_month_element.text)
+                        else
+                          # This is for a single-month datepicker
+                          flatpickr_container.first('.flatpickr-monthDropdown-months').value.to_i + 1
+                        end
 
         if current_month < month
-          while current_month < month
-            flatpickr_container.find('.flatpickr-next-month').click
-            current_month = flatpickr_container.first('.flatpickr-monthDropdown-months').value.to_i + 1
-          end
+          month_difference = month - current_month
+          month_difference.times { flatpickr_container.find('.flatpickr-next-month').click }
+          flatpickr_container.first('.flatpickr-monthDropdown-months').value.to_i + 1
         elsif current_month > month
-          while current_month > month
-            flatpickr_container.find('.flatpickr-prev-month').click
-            current_month = flatpickr_container.first('.flatpickr-monthDropdown-months').value.to_i + 1
-          end
+          month_difference = current_month - month
+          month_difference.times { flatpickr_container.find('.flatpickr-prev-month').click }
+          flatpickr_container.first('.flatpickr-monthDropdown-months').value.to_i + 1
         end
       end
     end
