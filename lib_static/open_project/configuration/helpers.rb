c9ed88d3ff410322f3f1573f6875298f4fe26635
Christophe Bliard <c.bliard@openproject.com>
2023-02-28 10:53:16 +0100

    Can use space-separated string for array settings
    Doing `OPENPROJECT_DISABLED__MODULES="repository wiki"` is the same as
    doing `OPENPROJECT_DISABLED__MODULES="['repository', 'wiki']"``.

--- lib_static/open_project/configuration/helpers.rb
+++ lib_static/open_project/configuration/helpers.rb
@@ -135,14 +135,6 @@ module OpenProject
         menus.to_h
       end
 
-      def disabled_modules
-        array self['disabled_modules']
-      end
-
-      def blacklisted_routes
-        array self['blacklisted_routes']
-      end
-
       ##
       # Whether we're running a bim edition
       def bim?
@@ -199,7 +191,7 @@ module OpenProject
       # In the latter case the string will be split and the values returned as an array.
       def array(value)
         if value.is_a?(String) && value =~ / /
-          value.split ' '
+          value.split
         else
           Array(value)
         end
