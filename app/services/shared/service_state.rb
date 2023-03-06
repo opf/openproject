c750c06d434ef46aad104ae8d071dd2a35398720
ulferts <jens.ulferts@googlemail.com>
2023-03-06 16:46:44 +0100

    disable sending notifications on project copy by default
    With notifications now being sent for individual models as well, e.g. work packages, having the option enabled potentially leads to a quite a lot of notifications being created. Thus changing the default.

--- app/services/shared/service_state.rb
+++ app/services/shared/service_state.rb
@@ -25,7 +25,6 @@
 #
 # See COPYRIGHT and LICENSE files for more details.
 #++
-require "ostruct"
 
 ##
 # Service state object to be passed around services
@@ -34,7 +33,7 @@ require "ostruct"
 # Borrows heavily from interactor gem's context class at
 # https://github.com/collectiveidea/interactor
 module Shared
-  class ServiceState < OpenStruct
+  class ServiceState < Hashie::Mash
     ##
     # Builds the context object unless
     # it's already an instance of this context.
