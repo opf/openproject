0d6a3e2097fae9081993ef8fc93747b844289935
ulferts <jens.ulferts@googlemail.com>
2023-03-07 09:11:47 +0100

    cleanup invalid journals before non-null constraint

--- db/migrate/20230306083203_non_null_data_reference_on_journals.rb
+++ db/migrate/20230306083203_non_null_data_reference_on_journals.rb
@@ -1,5 +1,9 @@
 class NonNullDataReferenceOnJournals < ActiveRecord::Migration[7.0]
   def change
+    reversible do |direction|
+      direction.up { execute "DELETE FROM journals WHERE data_id IS NULL or data_type IS NULL" }
+    end
+
     change_column_null :journals, :data_id, false
     change_column_null :journals, :data_type, false
   end
