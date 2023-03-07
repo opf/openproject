3cfdc9f417c98eb9e9dcf7ef138bfa1ed80d4e92
ulferts <jens.ulferts@googlemail.com>
2023-03-07 11:41:42 +0100

    handle notification deletion on journal cleanup

--- db/migrate/20230306083203_non_null_data_reference_on_journals.rb
+++ db/migrate/20230306083203_non_null_data_reference_on_journals.rb
@@ -1,10 +1,43 @@
 class NonNullDataReferenceOnJournals < ActiveRecord::Migration[7.0]
   def change
     reversible do |direction|
-      direction.up { execute "DELETE FROM journals WHERE data_id IS NULL or data_type IS NULL" }
+      direction.up do
+        change_on_delete_on_notification_fk(true)
+        cleanup_invalid_journals
+      end
+      direction.down do
+        change_on_delete_on_notification_fk(false)
+      end
     end
 
+    change_non_null_data_columns
+  end
+
+  private
+
+  def change_non_null_data_columns
     change_column_null :journals, :data_id, false
     change_column_null :journals, :data_type, false
+  end
+
+  def cleanup_invalid_journals
+    execute <<~SQL.squish
+      DELETE FROM journals
+      WHERE data_id IS NULL OR data_type IS NULL
+    SQL
+  end
+
+  def change_on_delete_on_notification_fk(cascade)
+    options = if cascade
+                { on_delete: :cascade }
+              else
+                {}
+              end
+
+    remove_foreign_key :notifications, :journals
+
+    add_foreign_key :notifications,
+                    :journals,
+                    **options
   end
 end
