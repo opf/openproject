137d2a9e1e972e4859574464bd20b10369014525
Benjamin Bädorf <b.baedorf@openproject.com>
2023-03-07 11:09:20 +0100

    Remove unused files

--- frontend/src/stories/ColorsPreview.tsx
+++
@@ -1,8 +0,0 @@
-import ColorPreview from './ColorPreview.tsx';
-
-export default ({ tokens }) => <div className="colors">
-  {Object.keys(tokens)
-    .filter(key => key.startsWith('spot-color-'))
-    .map(name => <ColorPreview tokens={tokens} name={name}></ColorPreview>)
-  }
-</div>;
