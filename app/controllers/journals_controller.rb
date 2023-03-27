e8eff0b5871f3a9099100d83d9cd9ae0355cbd39
Mir Bhatia <m.bhatia@openproject.com>
2023-03-27 13:13:10 +0200

    Remove unused diff field check

--- app/controllers/journals_controller.rb
+++ app/controllers/journals_controller.rb
@@ -104,8 +104,7 @@ class JournalsController < ApplicationController
 
   # Is this a valid field for diff'ing?
   def valid_field?
-    field_param == 'description' || field_param == 'text'
-  end
+    field_param == 'description'
 
   def journals_index_title
     subject = @project ? @project.name : Setting.app_title
