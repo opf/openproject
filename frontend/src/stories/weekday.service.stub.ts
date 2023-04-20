fe7aaa30e5a34085ae979375d6ae7f99fcfe870d
Yule <b.baedorf@openproject.com>
2023-04-20 14:20:48 +0000

    Add stories and argstable for basic datepickers (#12376)
    * Add stories and argstable for basic datepickers
    
    Closes https://community.openproject.org/work_packages/46645/activity
    
    * Revert storybook-angular to v6
    
    * Fix frontend module dependencies
    
    * Export datepickers from shared module
    
    * Rename shims to stubs
    
    * Add day of the week names to i18n stub
    
    * Removed other date picker stories
    
    Included relevant information from "Common elements" to the now-consolidated "Date picker" story.
    
    * Testing adding Figma iframe embeds
    
    * Add mdx-embed and a Figma example for embedding
    
    * Added Figma examples
    
    * Fix modal banners
    
    * Fix day service stub with new resource stores
    
    * Fix linting errors
    
    ---------
    
    Co-authored-by: Parimal Satyal <88370597+psatyal@users.noreply.github.com>

---
+++ frontend/src/stories/weekday.service.stub.ts
@@ -0,0 +1,59 @@
+// -- copyright
+// OpenProject is an open source project management software.
+// Copyright (C) 2012-2023 the OpenProject GmbH
+//
+// This program is free software; you can redistribute it and/or
+// modify it under the terms of the GNU General Public License version 3.
+//
+// OpenProject is a fork of ChiliProject, which is a fork of Redmine. The copyright follows:
+// Copyright (C) 2006-2013 Jean-Philippe Lang
+// Copyright (C) 2010-2013 the ChiliProject Team
+//
+// This program is free software; you can redistribute it and/or
+// modify it under the terms of the GNU General Public License
+// as published by the Free Software Foundation; either version 2
+// of the License, or (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program; if not, write to the Free Software
+// Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+//
+// See COPYRIGHT and LICENSE files for more details.
+//++
+
+import {
+  Injectable,
+} from '@angular/core';
+import * as moment from 'moment';
+import { IWeekday } from 'core-app/core/state/days/weekday.model';
+import {
+  Observable,
+  of,
+} from 'rxjs';
+
+@Injectable({ providedIn: 'root' })
+export class WeekdayServiceStub {
+  private weekdays:IWeekday[] = [];
+
+  /**
+   * @param date The iso day number (1-7) or a date instance
+   * @return {boolean} whether the given iso day is working or not
+   */
+  public isNonWorkingDay(date:Date|number):boolean {
+    const isoDayOfWeek = (typeof date === 'number') ? date : moment(date).isoWeekday();
+    return !!(this.weekdays || []).find((wd) => wd.day === isoDayOfWeek && !wd.working);
+  }
+
+  public get nonWorkingDays():IWeekday[] {
+    return this.weekdays.filter((day) => !day.working);
+  }
+
+  loadWeekdays():Observable<IWeekday[]> {
+    return of(this.weekdays);
+  }
+}
