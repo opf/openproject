e0c3d7a04527ae2547969d2ecb27aaebeb7a9384
Wieland Lindenthal <w.lindenthal@openproject.com>
2023-04-29 20:25:39 +0200

    WP ID filter should allow IDs outside from current project and its subprojects, e.g. when combined with project filter

--- app/models/queries/work_packages/filter/id_filter.rb
+++ app/models/queries/work_packages/filter/id_filter.rb
@@ -29,4 +29,8 @@
 class Queries::WorkPackages::Filter::IdFilter <
   Queries::WorkPackages::Filter::WorkPackageFilter
   include ::Queries::WorkPackages::Filter::FilterForWpMixin
+
+  def visible_scope
+    WorkPackage.visible
+  end
 end
