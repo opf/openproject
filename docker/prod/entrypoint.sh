b3d20157c5aa5222068d645d83966e8dcc637382
Konstantin Zangerle <konstantin.zangerle@kit.edu>
2023-05-02 10:50:49 +0200

    Reduce container image size

--- docker/prod/entrypoint.sh
+++ docker/prod/entrypoint.sh
@@ -6,35 +6,35 @@ set -o pipefail
 APACHE_PIDFILE=/run/apache2/apache2.pid
 
 # handle legacy configs
-if [ -f "/var/lib/postgresql/9.6/main/PG_VERSION" ]; then
-	echo "ERROR: You are using a legacy volume path for your postgres data. You should mount your postgres volumes at $PGDATA instead of /var/lib/postgresql/9.6/main"
-	exit 2
-fi
-if [ -d "/var/db/openproject" ]; then
-	echo "ERROR: You are using a legacy volume path for your openproject data. You should mount your openproject volume at $APP_DATA_PATH instead of /var/db/openproject"
-	exit 2
-fi
+#if [ -f "/var/lib/postgresql/9.6/main/PG_VERSION" ]; then
+#	echo "ERROR: You are using a legacy volume path for your postgres data. You should mount your postgres volumes at $PGDATA instead of /var/lib/postgresql/9.6/main"
+#	exit 2
+#fi
+#if [ -d "/var/db/openproject" ]; then
+#	echo "ERROR: You are using a legacy volume path for your openproject data. You should mount your openproject volume at $APP_DATA_PATH instead of /var/db/openproject"
+#	exit 2
+#fi
 
 if [ "$(id -u)" = '0' ]; then
-	# reexport PGVERSION and PGBIN env variables according to postgres version of existing cluster (if any)
-	# this must happen in the entrypoint
-	if [ -f "$PGDATA/PG_VERSION" ]; then
-		export PGVERSION="$(cat "$PGDATA/PG_VERSION")"
-		echo "-----> Existing PostgreSQL cluster found in $PGDATA."
-	fi
-	export PGBIN="/usr/lib/postgresql/$PGVERSION/bin"
-	export PGCONF_FILE="/etc/postgresql/$PGVERSION/main/postgresql.conf"
-	echo "-----> Setting PGVERSION=$PGVERSION PGBIN=$PGBIN PGCONF_FILE=$PGCONF_FILE"
-	export PATH="$PGBIN:$PATH"
+#	# reexport PGVERSION and PGBIN env variables according to postgres version of existing cluster (if any)
+#	# this must happen in the entrypoint
+#	if [ -f "$PGDATA/PG_VERSION" ]; then
+#		export PGVERSION="$(cat "$PGDATA/PG_VERSION")"
+#		echo "-----> Existing PostgreSQL cluster found in $PGDATA."
+#	fi
+#	export PGBIN="/usr/lib/postgresql/$PGVERSION/bin"
+#	export PGCONF_FILE="/etc/postgresql/$PGVERSION/main/postgresql.conf"
+#	echo "-----> Setting PGVERSION=$PGVERSION PGBIN=$PGBIN PGCONF_FILE=$PGCONF_FILE"
+#	export PATH="$PGBIN:$PATH"
 
 	mkdir -p $APP_DATA_PATH/{files,git,svn}
-	# The $APP_DATA_PATH may be hosted on a NAS that creates snapshots (or a btrfs filesystem). In such a case, the .snapshot folder cannot be touched.
-  find $APP_DATA_PATH | grep -v .snapshot | xargs -n 1 chown $APP_USER:$APP_USER
-	if [ -d /etc/apache2/sites-enabled ]; then
-		chown -R $APP_USER:$APP_USER /etc/apache2/sites-enabled
-		echo "OpenProject currently expects to be reached on the following domain: ${SERVER_NAME:=localhost}, which does not seem to be how your installation is configured." > /var/www/html/index.html
-		echo "If you are an administrator, please ensure you have correctly set the SERVER_NAME variable when launching your container." >> /var/www/html/index.html
-	fi
+#	# The $APP_DATA_PATH may be hosted on a NAS that creates snapshots (or a btrfs filesystem). In such a case, the .snapshot folder cannot be touched.
+#  find $APP_DATA_PATH | grep -v .snapshot | xargs -n 1 chown $APP_USER:$APP_USER
+#	if [ -d /etc/apache2/sites-enabled ]; then
+#		chown -R $APP_USER:$APP_USER /etc/apache2/sites-enabled
+#		echo "OpenProject currently expects to be reached on the following domain: ${SERVER_NAME:=localhost}, which does not seem to be how your installation is configured." > /var/www/html/index.html
+#		echo "If you are an administrator, please ensure you have correctly set the SERVER_NAME variable when launching your container." >> /var/www/html/index.html
+#	fi
 
 	# Clean up any dangling PID file
 	rm -f $APP_PATH/tmp/pids/*
