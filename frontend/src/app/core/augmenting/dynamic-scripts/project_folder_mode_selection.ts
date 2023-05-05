132612c0574756edaa5058d569f88378f2995ee6
Eric Schubert <e.schubert@openproject.com>
2023-05-05 11:49:26 +0200

    [#48044] Use Location picker modal to insert project folder name and ID into HTML form
    - https://community.openproject.org/work_packages/48044
    - add location picker and button to form
    - add storage data to storage options
    - fill input field with selected location id

--- frontend/src/app/core/augmenting/dynamic-scripts/project_folder_mode_selection.ts
+++ frontend/src/app/core/augmenting/dynamic-scripts/project_folder_mode_selection.ts
@@ -26,21 +26,70 @@
 // See COPYRIGHT and LICENSE files for more details.
 //++
 
+import { from, Observable, switchMap } from 'rxjs';
+import { map } from 'rxjs/operators';
+
+import { OpModalService } from 'core-app/shared/components/modal/modal.service';
+import {
+  LocationPickerModalComponent,
+} from 'core-app/shared/components/storages/location-picker-modal/location-picker-modal.component';
+
+interface PartialStorage {
+  name:string,
+  id:string,
+  _links:{
+    self:{ href:string },
+    type:{ href:string },
+  }
+}
+
 const projectFolderModeRadioButtons = document.querySelectorAll('input[name="storages_project_storage[project_folder_mode]"]');
-const projectFolderSelectionSection = document.getElementById('storages_manual_folder_selection');
+
+const projectFolderSelectionSection = document.getElementById('storages_manual_folder_selection') as HTMLElement;
 const manualProjectFolderModeInput = document.getElementById('storages_project_storage_project_folder_mode_manual') as HTMLInputElement;
+const openLocationPickerButton = document.getElementById('open_location_picker_button') as HTMLButtonElement|null;
+const storageSelector = document.getElementById('storages_project_storage_storage_id') as HTMLSelectElement|null;
+const storageSpan = document.getElementById('active_storage') as HTMLSpanElement|null;
+const storageIdInput = document.getElementById('storages_project_storage_project_folder_id') as HTMLInputElement|null;
+
+function modalService():Observable<OpModalService> {
+  return from(window.OpenProject.getPluginContext())
+    .pipe(map((pluginContext) => pluginContext.services.opModalService));
+}
+
+function parseStorageData():PartialStorage {
+  let json = '';
+  if (storageSelector !== null) {
+    const selectedStorageOption = storageSelector.options[storageSelector.selectedIndex];
+    json = selectedStorageOption.dataset.storage as string;
+  } else if (storageSpan !== null) {
+    json = storageSpan.dataset.storage as string;
+  } else {
+    throw new Error('No storage data available.');
+  }
+
+  const data = JSON.parse(json) as unknown&{ id:string, name:string, href:string, type:string };
+
+  return {
+    id: data.id,
+    name: data.name,
+    _links: {
+      self: { href: data.href },
+      type: { href: data.type },
+    },
+  };
+}
 
 // Show the manual folder selection section if the manual radio button is checked
-if (manualProjectFolderModeInput.checked && projectFolderSelectionSection !== null) {
+if (manualProjectFolderModeInput !== null
+  && manualProjectFolderModeInput.checked
+  && projectFolderSelectionSection !== null
+) {
   projectFolderSelectionSection.style.display = 'flex';
 }
 
 projectFolderModeRadioButtons.forEach((radio:HTMLInputElement) => {
   radio.onchange = () => {
-    if (projectFolderSelectionSection === null) {
-      return;
-    }
-
     // If the manual radio button is selected, show the manual folder selection section
     if (radio.value === 'manual') {
       projectFolderSelectionSection.style.display = 'flex';
@@ -49,3 +98,22 @@ projectFolderModeRadioButtons.forEach((radio:HTMLInputElement) => {
     }
   };
 });
+
+if (openLocationPickerButton !== null) {
+  openLocationPickerButton.onclick = () => {
+    const locals = {
+      projectFolderId: null,
+      storage: parseStorageData(),
+    };
+    modalService()
+      .pipe(
+        switchMap((service) => service.show(LocationPickerModalComponent, 'global', locals)),
+        switchMap((modal) => modal.closingEvent),
+      )
+      .subscribe((modal) => {
+        if (storageIdInput !== null) {
+          storageIdInput.value = modal.location;
+        }
+      });
+  };
+}
