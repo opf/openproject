ee2b9a28904ade22d4040271950b5ea710b2052e
bsatarnejad <b.satarnejad@gmail.com>
2023-05-10 11:56:38 +0200

    check if the legends box is shown

--- spec/features/work_packages/table/baseline/baseline_rendering_spec.rb
+++ spec/features/work_packages/table/baseline/baseline_rendering_spec.rb
@@ -155,6 +155,7 @@ describe 'baseline rendering',
 
   let(:wp_table) { Pages::WorkPackagesTable.new(project) }
   let(:baseline) { Components::WorkPackages::Baseline.new }
+  let(:baseline_legends) { Components::WorkPackages::BaselineLegends.new }
 
   current_user { user }
 
@@ -165,6 +166,8 @@ describe 'baseline rendering',
       wp_table.ensure_work_package_not_listed! wp_bug
 
       baseline.expect_active
+      baseline_legends.expect_loaded
+
       baseline.expect_added wp_task_was_bug
       baseline.expect_removed wp_bug_was_task
       baseline.expect_changed wp_task_changed
@@ -189,6 +192,7 @@ describe 'baseline rendering',
       baseline.expect_unchanged_attributes wp_task,
                                            :type, :subject, :start_date, :due_date,
                                            :version, :priority, :assignee, :accountable
+      baseline.expect_legends
     end
   end
 
