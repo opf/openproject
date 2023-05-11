3d55d0301b9373da7cc2c00467a9af1ae05a2ff1
Tey, Benjamin <ben.tey@outlook.com>
2023-05-11 11:06:37 -0400

    Testing pipelines

--- modules/gitlab_integration/lib/open_project/gitlab_integration/services/upsert_pipeline.rb
+++ modules/gitlab_integration/lib/open_project/gitlab_integration/services/upsert_pipeline.rb
@@ -36,7 +36,7 @@ module OpenProject::GitlabIntegration::Services
   # Returns the upserted `GitlabPipeline`.
   class UpsertPipeline
     def call(payload, merge_request:)
-      GitlabPipeline.find_or_initialize_by(gitlab_id: payload.object_attributes.id)
+      GitlabPipeline.find_or_initialize_by(gitlab_id: payload.object_attributes.iid)
                     .tap do |pipeline|
                       pipeline.update!(gitlab_merge_request: merge_request, **extract_params(payload))
                     end
@@ -48,13 +48,13 @@ module OpenProject::GitlabIntegration::Services
     # to our internal representation.
     def extract_params(payload)
       {
-        gitlab_id: payload.object_attributes.id,
-        gitlab_html_url: "#{payload.project.web_url}-/pipelines/#{payload.object_attributes.id}",
+        gitlab_id: payload.object_attributes.iid,
+        gitlab_html_url: "#{payload.project.web_url}/pipelines/#{payload.object_attributes.iid}",
         project_id: payload.project.id,
         gitlab_user_avatar_url: payload.user.avatar_url,
-        name: payload.object_attributes.status,
+        name: payload.object_attributes.iid,
         status: payload.object_attributes.status,
-        details_url: "#{payload.project.web_url}-/pipelines/#{payload.object_attributes.id}",
+        details_url: "#{payload.project.web_url}/pipelines/#{payload.object_attributes.iid}",
         # ci_details: pending until resolution of the gitlab issue,
         started_at: payload.object_attributes.created_at,
         completed_at: payload.object_attributes.finished_at
