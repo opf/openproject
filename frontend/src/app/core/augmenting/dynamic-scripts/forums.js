4e4ee171922f325dae079bd93b8f3d9dd2d39c86
Oliver Günther <mail@oliverguenther.de>
2023-05-11 08:05:44 +0200

    Forums

--- frontend/src/app/core/augmenting/dynamic-scripts/forums.js
+++ frontend/src/app/core/augmenting/dynamic-scripts/forums.js
@@ -29,35 +29,8 @@
 (function($) {
   jQuery(function() {
 
-    function quoteResult(result) {
-      var reply = $("#reply"),
-        subject = $("#reply_subject"),
-        focusElement = jQuery("#reply #message-form");
 
-      subject.val(result.subject);
 
-      $('ckeditor-augmented-textarea op-ckeditor')
-        .data('editor')
-        .then(function(editor) {
-          editor.setData(result.content);
-        });
-
-      reply.slideDown();
-
-      $('#content-wrapper').animate({
-        scrollTop: focusElement.offset().top
-      }, 1000);
-    }
-
-    $('.boards--quote-button').click(function(evt) {
-      var link = $(this);
-
-      $.getJSON(link.attr('href'))
-        .done(quoteResult);
-
-      evt.preventDefault();
-      return false;
-    });
   });
 
 }(jQuery));
