3be60ba0a256e36c4ab3640cc847d31663eda398
Markus Kahl <machisuji@gmail.com>
2023-05-12 11:55:30 +0200

    use port 5433 for postgres during asset compilation to prevent conflicts

--- docker/prod/setup/postinstall-common.sh
+++ docker/prod/setup/postinstall-common.sh
@@ -14,20 +14,20 @@ popd
 
 # Bundle assets
 su - postgres -c "$PGBIN/initdb -D /tmp/nulldb"
-su - postgres -c "bash -c '$PGBIN/pg_ctl -D /tmp/nulldb -l /tmp/pg_ctl.log -w start -t 30 || cat /tmp/pg_ctl.log'"
-echo "create database assets; create user assets with encrypted password 'p4ssw0rd'; grant all privileges on database assets to assets;" | su - postgres -c psql
+su - postgres -c "bash -c '$PGBIN/pg_ctl -D /tmp/nulldb -l /tmp/pg_ctl.log -w start -o \"-p 5433\" || cat /tmp/pg_ctl.log'"
+echo "create database assets; create user assets with encrypted password 'p4ssw0rd'; grant all privileges on database assets to assets;" | su - postgres -c psql -p 5433
 
 # give some more time for DB to start
 sleep 5
 
 # dump schema
-DATABASE_URL=postgres://assets:p4ssw0rd@127.0.0.1/assets RAILS_ENV=production bundle exec rake db:migrate db:schema:dump db:schema:cache:dump
+DATABASE_URL=postgres://assets:p4ssw0rd@127.0.0.1:5433/assets RAILS_ENV=production bundle exec rake db:migrate db:schema:dump db:schema:cache:dump
 
 # this line requires superuser rights, which is not always available and doesn't matter anyway
 sed -i '/^COMMENT ON EXTENSION/d' db/structure.sql
 
 # precompile assets
-DATABASE_URL=postgres://assets:p4ssw0rd@127.0.0.1/assets RAILS_ENV=production bundle exec rake assets:precompile
+DATABASE_URL=postgres://assets:p4ssw0rd@127.0.0.1:5433/assets RAILS_ENV=production bundle exec rake assets:precompile
 
 su - postgres -c "$PGBIN/pg_ctl -D /tmp/nulldb stop"
 
