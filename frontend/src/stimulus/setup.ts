ea34f1a1aa6f2e62346964a24c23c7a57ae1c065
Oliver Günther <mail@oliverguenther.de>
2023-05-14 20:24:57 +0200

    Replace print_handler with print controller

--- frontend/src/stimulus/setup.ts
+++ frontend/src/stimulus/setup.ts
@@ -2,6 +2,7 @@ import { Application } from '@hotwired/stimulus';
 import { environment } from '../environments/environment';
 import { OpApplicationController } from './controllers/op-application.controller';
 import OpDisableWhenCheckedController from './controllers/disable-when-checked.controller';
+import PrintController from './controllers/print.controller';
 
 declare global {
   interface Window {
@@ -19,3 +20,4 @@ instance.handleError = (error, message, detail) => {
 
 instance.register('application', OpApplicationController);
 instance.register('disable-when-checked', OpDisableWhenCheckedController);
+instance.register('print', PrintController);
