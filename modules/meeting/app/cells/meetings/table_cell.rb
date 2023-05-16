be80aaa7c18ea7463d0e11886f6b09aa23e75f72
Klaus Zanders <k.zanders@openproject.com>
2023-05-16 11:46:16 +0200

    Clean up unneeded options

--- modules/meeting/app/cells/meetings/table_cell.rb
+++ modules/meeting/app/cells/meetings/table_cell.rb
@@ -1,7 +1,5 @@
 module Meetings
   class TableCell < ::TableCell
-    options :params # We read collapsed state from params
-    options :current_user # adds this option to those of the base class
     options :current_project # used to determine if displaying the projects column
 
     sortable_columns :title, :start_time, :duration, :location
