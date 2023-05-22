87b233ad24bbb74e36a331a2202c5d99d7236daf
Oliver Günther <mail@oliverguenther.de>
2023-05-22 20:20:20 +0200

    Move admin controllers to subfolder/namespace

--- frontend/src/stimulus/controllers/op-application.controller.ts
+++ frontend/src/stimulus/controllers/op-application.controller.ts
@@ -8,14 +8,29 @@ export class OpApplicationController extends ApplicationController {
 
   dynamicTargetConnected(target:HTMLElement) {
     const controller = target.dataset.controller as string;
+    const path = this.derivePath(controller);
 
     if (!this.loaded.has(controller)) {
       this.loaded.add(controller);
-      void import(/* webpackChunkName: "[request]" */`./dynamic/${controller}.controller`)
+      void import(/* webpackChunkName: "[request]" */`./dynamic/${path}.controller`)
         .then((imported:{ default:ControllerConstructor }) => this.application.register(controller, imported.default))
         .catch((err:unknown) => {
           console.error('Failed to load dyanmic controller chunk %O: %O', controller, err);
         });
     }
+  }
+
+  /**
+   * Derive dynamic path from controller name.
+   *
+   * Stimlus conventions allow subdirectories to be used by double dashes.
+   * We convert these to slashes for the dynamic import.
+   *
+   * https://stimulus.hotwired.dev/handbook/installing#controller-filenames-map-to-identifiers
+   * @param controller
+   * @private
+   */
+  private derivePath(controller:string):string {
+    return controller.replace('--', '/');
   }
 }
