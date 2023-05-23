d5e1d5555c7449bccf43bdf5ddff097c6385c866
Benjamin Bädorf <b.baedorf@openproject.com>
2023-05-23 14:05:48 +0200

    Change all icons to spot-icon in wp-table
    Part of https://community.openproject.org/work_packages/47363/activity

--- frontend/src/app/features/work-packages/components/wp-fast-table/builders/drag-and-drop/drag-drop-handle-builder.ts
+++ frontend/src/app/features/work-packages/components/wp-fast-table/builders/drag-and-drop/drag-drop-handle-builder.ts
@@ -34,7 +34,7 @@ export class DragDropHandleBuilder {
 
     // Wrap handle as span
     const span = document.createElement('span');
-    span.classList.add('wp-table--drag-and-drop-handle', 'icon-drag-handle');
+    span.classList.add('wp-table--drag-and-drop-handle', 'spot-icon', 'spot-icon_1_25', 'spot-icon_drag-handle');
     td.appendChild(span);
 
     if (RENDER_DRAG_AND_DROP_POSITION) {
