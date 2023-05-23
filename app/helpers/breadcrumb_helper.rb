16b17805eda4eea733791ca83693a9abd1a5c7b4
Benjamin Bädorf <b.baedorf@openproject.com>
2023-05-23 16:31:27 +0200

    Lots of improvements on breadcrumb icons

--- app/helpers/breadcrumb_helper.rb
+++ app/helpers/breadcrumb_helper.rb
@@ -33,24 +33,24 @@ module BreadcrumbHelper
 
   def breadcrumb(*args)
     elements = args.flatten
-    elements.any? ? content_tag('p', (args.join(' &#187; ') + ' &#187; ').html_safe, class: 'op-breadcrumb') : nil
+    elements.any? ? content_tag('p', (args.join(' &#187; ') + ' &#187; ').html_safe, class: 'op-wp-breadcrumb') : nil
   end
 
   def breadcrumb_list(*args)
     elements = args.flatten
     breadcrumb_elements = [content_tag(:li,
                                        elements.shift.to_s,
-                                       class: 'first-breadcrumb-element')]
+                                       class: 'op-wp-breadcrumb--list-item first-breadcrumb-element')]
 
     breadcrumb_elements += elements.map do |element|
       if element
         content_tag(:li,
-                    h(element.to_s),
-                    class: "icon4 icon-small icon-arrow-right5")
+                    spot_icon('arrow-right5', size: '1') + h(element.to_s),
+                    class: 'op-wp-breadcrumb--list-item')
       end
     end
 
-    content_tag(:ul, breadcrumb_elements.join.html_safe, class: 'op-breadcrumb', 'data-qa-selector': 'op-breadcrumb')
+    content_tag(:ul, breadcrumb_elements.join.html_safe, class: 'op-wp-breadcrumb--list', 'data-qa-selector': 'op-breadcrumb')
   end
 
   def breadcrumb_paths(*args)
