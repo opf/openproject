0b6f980ae86af69822c0b60d7f3eb6ec3d9cc481
Oliver Günther <mail@oliverguenther.de>
2023-05-29 13:14:47 +0200

    Add logo

--- config/initializers/lookbook.rb
+++ config/initializers/lookbook.rb
@@ -3,7 +3,9 @@ OpenProject::Application.configure do
 
   config.view_component.generate.preview_path = Rails.root.join("spec/components/previews").to_s
   config.view_component.preview_paths << Rails.root.join("spec/components/previews").to_s
-  config.lookbook.project_name = "OpenProject Lookbook"
+  config.lookbook.project_name = "OpenProject Design System"
+  config.lookbook.project_logo = File.read Rails.root.join('app/assets/images/icon_logo_white.svg')
+  config.lookbook.ui_favicon = File.read Rails.root.join('app/assets/images/icon_logo.svg')
   config.lookbook.page_paths = [Rails.root.join("spec/components/docs/").to_s]
   config.lookbook.ui_theme = "blue"
 
