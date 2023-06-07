2e2eaaad9a33fedcbcf8a79322cf70f285855278
Klaus Zanders <k.zanders@openproject.com>
2023-06-07 11:54:06 +0200

    Fix one more call to `add_journal`

--- app/models/changeset.rb
+++ app/models/changeset.rb
@@ -232,7 +232,7 @@ class Changeset < ApplicationRecord
     return if work_package.status && work_package.status.is_closed?
 
     journal_notes = I18n.t(:text_status_changed_by_changeset, value: text_tag, locale: Setting.default_language)
-    work_package.add_journal(user || User.anonymous, journal_notes)
+    work_package.add_journal(user: user || User.anonymous, notes: journal_notes)
     work_package.status = status
     if Setting.commit_fix_done_ratio.present?
       work_package.done_ratio = Setting.commit_fix_done_ratio.to_i
