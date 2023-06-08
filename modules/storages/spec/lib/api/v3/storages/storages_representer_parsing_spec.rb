13644ace654c22b37a4279d56294b802cbe1c2e4
Kabiru Mwenja <makabby@gmail.com>
2023-06-08 20:10:46 +0300

    tests[#48289] Define missing `StoragesRepresenter` parsing spec
    https://community.openproject.org/work_packages/48289

---
+++ modules/storages/spec/lib/api/v3/storages/storages_representer_parsing_spec.rb
@@ -0,0 +1,61 @@
+#-- copyright
+# OpenProject is an open source project management software.
+# Copyright (C) 2012-2023 the OpenProject GmbH
+#
+# This program is free software; you can redistribute it and/or
+# modify it under the terms of the GNU General Public License version 3.
+#
+# OpenProject is a fork of ChiliProject, which is a fork of Redmine. The copyright follows:
+# Copyright (C) 2006-2013 Jean-Philippe Lang
+# Copyright (C) 2010-2013 the ChiliProject Team
+#
+# This program is free software; you can redistribute it and/or
+# modify it under the terms of the GNU General Public License
+# as published by the Free Software Foundation; either version 2
+# of the License, or (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+#
+# See COPYRIGHT and LICENSE files for more details.
+#++
+
+require 'spec_helper'
+
+RSpec.describe API::V3::Storages::StorageRepresenter, 'parsing' do
+  let(:storage) { build_stubbed(:storage) }
+  let(:current_user) { build_stubbed(:user) }
+
+  describe 'parsing' do
+    subject(:parsed) { representer.from_hash parsed_hash }
+
+    let(:representer) { described_class.new(storage, current_user:) }
+
+    let(:parsed_hash) do
+      {
+        "name" => "Nextcloud Local",
+        "_links" => {
+          "origin" => {
+            "href" => storage.host
+          },
+          "type" => {
+            "href" => API::V3::Storages::URN_STORAGE_TYPE_NEXTCLOUD
+          }
+        }
+      }
+    end
+
+    context 'with basic attributes' do
+      it 'is parsed correctly' do
+        expect(parsed).to have_attributes(name: 'Nextcloud Local', host: storage.host,
+                                          provider_type: "Storages::NextcloudStorage")
+      end
+    end
+  end
+end
