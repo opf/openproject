d05376dc04b300e85f82dcf497948da163756d26
Cyril Rohr <hey@cyrilrohr.com>
2023-06-09 13:44:59 +0000

    Adding some optims

--- docker/ci/entrypoint.sh
+++ docker/ci/entrypoint.sh
@@ -41,16 +41,18 @@ execute_quiet() {
 	fi
 }
 
+reset_dbs() {
+	# must reset main db because for some reason the users table is not empty, after running db:migrate
+	execute_quiet "echo 'drop database if exists appdb ; create database appdb' | $PGBIN/psql -U dev -h 127.0.0.1 -d postgres"
+	# create test databases "app1" to "app$JOBS", far faster than using parallel_rspec tasks for that
+	execute_quiet "cat db/structure.sql | $PGBIN/psql -U dev -h 127.0.0.1 -d appdb"
+	for i in $(seq 1 $JOBS); do
+		execute_quiet "echo 'drop database if exists app$i ; create database app$i with template appdb owner appuser;' | $PGBIN/psql -U dev -h 127.0.0.1 -d postgres"
+	done
+}
+
 # if from within docker
 if [ $(id -u) -eq 0 ]; then
-	if [ ! -d "/tmp/nulldb" ]; then
-		echo "fsync = off" >> /etc/postgresql/$PGVERSION/main/postgresql.conf
-		echo "full_page_writes = off" >> /etc/postgresql/$PGVERSION/main/postgresql.conf
-		su - postgres -c "$PGBIN/initdb -E UTF8 -D /tmp/nulldb"
-		su - postgres -c "$PGBIN/pg_ctl -D /tmp/nulldb -l /dev/null -w start"
-		echo "create database app; create user app with superuser encrypted password 'p4ssw0rd'; grant all privileges on database app to app;" | su - postgres -c $PGBIN/psql
-	fi
-
 	mkdir -p /usr/local/bundle
 	mkdir -p /home/$USER/openproject/frontend/node_modules
 	mkdir -p /home/$USER/openproject/frontend/.angular
@@ -75,24 +77,25 @@ if [ "$1" == "setup-tests" ]; then
 		execute_quiet "rm -rf '$folder' ; mkdir -p '$folder' ; chmod 1777 '$folder'"
 	done
 
+	if [ ! -d "/tmp/nulldb" ]; then
+		# echo "fsync = off" >> /etc/postgresql/$PGVERSION/main/postgresql.conf
+		# echo "full_page_writes = off" >> /etc/postgresql/$PGVERSION/main/postgresql.conf
+		execute_quiet "$PGBIN/initdb -E UTF8 -D /tmp/nulldb"
+		execute_quiet "$PGBIN/pg_ctl -D /tmp/nulldb -w start -o '-h 127.0.0.1 -k /tmp'"
+		echo "create database appdb; create user appuser with superuser encrypted password 'p4ssw0rd'; grant all privileges on database appdb to appuser;" | $PGBIN/psql -U dev -h 127.0.0.1 -d postgres
+	fi
+
 	execute_quiet "time bundle install -j$JOBS --quiet"
 	# create test database "app" and dump schema because db/structure.sql is not checked in
-	execute_quiet "time bundle exec rails db:migrate db:schema:dump"
-	# create test databases "app1" to "app$JOBS", far faster than using parallel_rspec tasks for that
-	for i in $(seq 1 $JOBS); do
-		execute_quiet "echo 'create database app$i with template app owner app;' | $PGBIN/psql $DATABASE_URL"
-	done
-	execute_quiet "time bundle exec rails webdrivers:chromedriver:update webdrivers:geckodriver:update openproject:plugins:register_frontend"
-	# setup frontend deps
-	execute_quiet "cd frontend && npm install"
+	execute_quiet "time bundle exec rails db:migrate db:schema:dump zeitwerk:check openproject:plugins:register_frontend"
 fi
 
 if [ "$1" == "run-units" ]; then
 	shift
+	reset_dbs
 	execute_quiet "cp -f /cache/turbo_runtime_units.log spec/support/ || true"
 	# turbo_tests cannot yet exclude specific directories, so copying spec/features elsewhere (temporarily)
 	execute_quiet "mv spec/features tmp/"
-	execute_quiet "time bin/rails zeitwerk:check"
 	execute "time bundle exec turbo_tests -n $JOBS --runtime-log spec/support/turbo_runtime_units.log spec"
 	execute_quiet "cp -f spec/support/turbo_runtime_units.log /cache/ || true"
 	cleanup
@@ -100,8 +103,11 @@ fi
 
 if [ "$1" == "run-features" ]; then
 	shift
+	reset_dbs
+	# setup frontend deps
+	execute_quiet "cd frontend && npm install"
+	execute_quiet "time bundle exec rails assets:precompile webdrivers:chromedriver:update webdrivers:geckodriver:update"
 	execute_quiet "cp -f /cache/turbo_runtime_features.log spec/support/ || true"
-	execute_quiet "time bundle exec rails assets:precompile"
 	execute_quiet "cp -rp config/frontend_assets.manifest.json public/assets/frontend_assets.manifest.json"
 	execute "time bundle exec turbo_tests -n $JOBS --runtime-log spec/support/turbo_runtime_features.log spec/features"
 	execute_quiet "cp -f spec/support/turbo_runtime_features.log /cache/ || true"
