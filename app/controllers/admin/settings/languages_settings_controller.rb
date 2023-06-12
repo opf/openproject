60693535d2850ce2ac21917245a36c6188717a62
Mir Bhatia <m.bhatia@openproject.com>
2023-06-12 12:20:33 +0200

    Remove unnecessary method

--- app/controllers/admin/settings/languages_settings_controller.rb
+++ app/controllers/admin/settings/languages_settings_controller.rb
@@ -29,26 +29,9 @@
 module Admin::Settings
   class LanguagesSettingsController < ::Admin::SettingsController
     menu_item :settings_languages
-    before_action :validate_start_of_week_year, only: :update
 
     def default_breadcrumb
       t(:label_languages)
-    end
-
-    private
-
-    def validate_start_of_week_year
-      start_of_week = params[:settings][:start_of_week]
-      start_of_year = params[:settings][:first_week_of_year]
-
-      if start_of_week.present? ^ start_of_year.present?
-        flash[:error] = I18n.t(
-          'settings.display.first_date_of_week_and_year_set',
-          first_week_setting_name: I18n.t(:setting_first_week_of_year),
-          day_of_week_setting_name: I18n.t(:setting_start_of_week)
-        )
-        redirect_to action: :show
-      end
     end
   end
 end
