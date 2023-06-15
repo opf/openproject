dd36b5c381347560231f5b5ed0482ad04e3264ed
Cyril Rohr <hey@cyrilrohr.com>
2023-06-15 21:04:54 +0200

    Enable on-demand test runners with scaleway (#12795)
    * Use dockerfile build cache
    * Improve entrypoint to speed up test setup
    * Fix asset helper
    * Disable angular cache until webpack fixes dangling caches. Also experiment with max-old-space-size
    * No need for maintaining test schema under CI. Saves ~30s of db:test:prepare stuff
    * DOCKER_BUILDKIT: 1 for AWS
    * turbo_tests verbose
    * PostgreSQL conf for CI
    * Add comment on how to switch back to AWS self-hosted runners

--- docker/ci/entrypoint.sh
+++ docker/ci/entrypoint.sh
@@ -6,61 +6,55 @@ export JOBS="${CI_JOBS:=$(nproc)}"
 # for parallel rspec
 export PARALLEL_TEST_PROCESSORS=$JOBS
 export PARALLEL_TEST_FIRST_IS_1=true
+export DISABLE_DATABASE_ENVIRONMENT_CHECK=1
+export NODE_OPTIONS="--max-old-space-size=8192"
+LOG_FILE=/tmp/op-output.log
 
-# if from within docker
-if [ $(id -u) -eq 0 ]; then
-	if [ ! -d "/tmp/nulldb" ]; then
-		echo "fsync = off" >> /etc/postgresql/$PGVERSION/main/postgresql.conf
-		echo "full_page_writes = off" >> /etc/postgresql/$PGVERSION/main/postgresql.conf
-		su - postgres -c "$PGBIN/initdb -E UTF8 -D /tmp/nulldb"
-		su - postgres -c "$PGBIN/pg_ctl -D /tmp/nulldb -l /dev/null -w start"
-		echo "create database app; create user app with superuser encrypted password 'p4ssw0rd'; grant all privileges on database app to app;" | su - postgres -c $PGBIN/psql
+run_psql() {
+	$PGBIN/psql -v ON_ERROR_STOP=1 -U dev -h 127.0.0.1 "$@"
+}
+
+cleanup() {
+	exit_code=$?
+	echo "CLEANUP"
+	rm -rf tmp/cache/parallel*
+	if [ -d tmp/features ]; then mv tmp/features spec/ ; fi
+	if [ ! $exit_code -eq "0" ]; then
+		echo "ERROR: exit code $exit_code"
+		cat $LOG_FILE
 	fi
+	rm -f $LOG_FILE
+}
 
-	mkdir -p /usr/local/bundle
-	mkdir -p /home/$USER/openproject/frontend/node_modules
-	mkdir -p /home/$USER/openproject/frontend/.angular
-	mkdir -p /home/$USER/openproject/tmp
-	mkdir -p /cache
-	chown $USER:$USER /usr/local/bundle
-	chown $USER:$USER /home/$USER/openproject/frontend/node_modules
-	chown $USER:$USER /home/$USER/openproject/frontend/.angular
-	chown $USER:$USER /home/$USER/openproject/tmp
-	chown -R $USER:$USER /cache
-fi
-
+trap cleanup INT TERM EXIT
 
 execute() {
 	BANNER=${BANNER:="[execute]"}
 	echo "$BANNER $@" >&2
-	if [ $(id -u) -eq 0 ]; then
-		su $USER -c "$@"
-	else
-		bash -c "$@"
-	fi
+	eval "$@"
 }
 
 execute_quiet() {
-	if ! BANNER="[execute_quiet]" execute "$@" >/tmp/op-output.log ; then
-		cat /tmp/op-output.log
+	if ! BANNER="[execute_quiet]" execute "$@" >$LOG_FILE ; then
 		return 1
 	else
 		return 0
 	fi
 }
 
-cleanup() {
-	echo "CLEANUP"
-	rm -rf tmp/cache/parallel*
-	rm -f /tmp/op-output.log
-	if [ -d tmp/features ]; then mv tmp/features spec/ ; fi
+reset_dbs() {
+	# must reset main db because for some reason the users table is not empty, after running db:migrate
+	execute_quiet "echo 'drop database if exists appdb ; create database appdb' | run_psql -d postgres"
+	execute_quiet "cat db/structure.sql | run_psql -d appdb"
+	# create and load schema for test databases "appdb1" to "appdb$JOBS", far faster than using parallel_rspec tasks for that
+	for i in $(seq 1 $JOBS); do
+		execute_quiet "echo 'drop database if exists appdb$i ; create database appdb$i with template appdb owner appuser;' | run_psql -d postgres"
+	done
 }
 
-trap cleanup INT TERM EXIT
-
 if [ "$1" == "setup-tests" ]; then
-	echo "Preparing environment for running tests..."
 	shift
+	echo "Preparing environment for running tests..."
 
 	execute_quiet "mkdir -p tmp"
 	execute_quiet "cp docker/ci/database.yml config/"
@@ -70,21 +64,23 @@ if [ "$1" == "setup-tests" ]; then
 		execute_quiet "rm -rf '$folder' ; mkdir -p '$folder' ; chmod 1777 '$folder'"
 	done
 
-	execute_quiet "time bundle install -j$JOBS --quiet"
-	# create test database "app" and dump schema
-	execute_quiet "time bundle exec rails db:create db:migrate db:schema:dump webdrivers:chromedriver:update webdrivers:geckodriver:update openproject:plugins:register_frontend"
-	# create parallel test databases "app#n" and load schema
-	execute_quiet "time bundle exec rails parallel:create parallel:load_schema"
-	# setup frontend deps
-	execute_quiet "cd frontend && npm install"
+	if [ ! -d "/tmp/nulldb" ]; then
+		execute_quiet "$PGBIN/initdb -E UTF8 -D /tmp/nulldb"
+		execute_quiet "cp docker/ci/postgresql.conf /tmp/nulldb/"
+		execute_quiet "$PGBIN/pg_ctl -D /tmp/nulldb -l /dev/null -w start"
+		echo "create database appdb; create user appuser with superuser encrypted password 'p4ssw0rd'; grant all privileges on database appdb to appuser;" | run_psql -d postgres
+	fi
+
+	# create test database "app" and dump schema because db/structure.sql is not checked in
+	execute_quiet "time bundle exec rails db:migrate db:schema:dump zeitwerk:check openproject:plugins:register_frontend"
 fi
 
 if [ "$1" == "run-units" ]; then
 	shift
+	reset_dbs
 	execute_quiet "cp -f /cache/turbo_runtime_units.log spec/support/ || true"
 	# turbo_tests cannot yet exclude specific directories, so copying spec/features elsewhere (temporarily)
 	execute_quiet "mv spec/features tmp/"
-	execute_quiet "time bundle exec rails zeitwerk:check"
 	execute "time bundle exec turbo_tests --verbose -n $JOBS --runtime-log spec/support/turbo_runtime_units.log spec"
 	execute_quiet "cp -f spec/support/turbo_runtime_units.log /cache/ || true"
 	cleanup
@@ -92,8 +88,9 @@ fi
 
 if [ "$1" == "run-features" ]; then
 	shift
+	reset_dbs
+	execute_quiet "time bundle exec rails assets:precompile webdrivers:chromedriver:update webdrivers:geckodriver:update"
 	execute_quiet "cp -f /cache/turbo_runtime_features.log spec/support/ || true"
-	execute_quiet "time bundle exec rails assets:precompile"
 	execute_quiet "cp -rp config/frontend_assets.manifest.json public/assets/frontend_assets.manifest.json"
 	execute "time bundle exec turbo_tests --verbose -n $JOBS --runtime-log spec/support/turbo_runtime_features.log spec/features"
 	execute_quiet "cp -f spec/support/turbo_runtime_features.log /cache/ || true"
