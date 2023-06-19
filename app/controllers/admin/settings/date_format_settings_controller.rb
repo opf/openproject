8003dd1189050e6199f0af4fd3c5de39a5ccaeaa
Mir Bhatia <m.bhatia@openproject.com>
2023-06-19 11:25:10 +0200

    Reduce conflicts

--- app/controllers/admin/settings/date_format_settings_controller.rb
+++ app/controllers/admin/settings/date_format_settings_controller.rb
@@ -30,7 +30,7 @@ module Admin::Settings
   class DateFormatSettingsController < ::Admin::SettingsController
     menu_item :date_format
 
-    before_action :validate_start_of_week_year, only: :update
+    before_action :validate_start_of_week_and_first_week_of_year_combination, only: :update
 
     def update # rubocop:disable Lint/UselessMethodDefinition
       super
@@ -46,9 +46,9 @@ module Admin::Settings
 
     private
 
-    def validate_start_of_week_year
-      start_of_week = params[:settings][:start_of_week]
-      start_of_year = params[:settings][:first_week_of_year]
+    def validate_start_of_week_and_first_week_of_year_combination
+      start_of_week = settings_params[:start_of_week]
+      start_of_year = settings_params[:first_week_of_year]
 
       if start_of_week.present? ^ start_of_year.present?
         flash[:error] = I18n.t(
