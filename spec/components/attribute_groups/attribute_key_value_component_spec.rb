d6b9001f2174f21f9ad67a7a16eff040ff94b6bb
Kabiru Mwenja <makabby@gmail.com>
2023-06-20 14:28:42 +0300

    feat[#48620] Define `AttributeGroup` components
    https://community.openproject.org/work_packages/48620

---
+++ spec/components/attribute_groups/attribute_key_value_component_spec.rb
@@ -0,0 +1,44 @@
+# frozen_string_literal: true
+
+#-- copyright
+# OpenProject is an open source project management software.
+# Copyright (C) 2012-2022 the OpenProject GmbH
+#
+# This program is free software; you can redistribute it and/or
+# modify it under the terms of the GNU General Public License version 3.
+#
+# OpenProject is a fork of ChiliProject, which is a fork of Redmine. The copyright follows:
+# Copyright (C) 2006-2013 Jean-Philippe Lang
+# Copyright (C) 2010-2013 the ChiliProject Team
+#
+# This program is free software; you can redistribute it and/or
+# modify it under the terms of the GNU General Public License
+# as published by the Free Software Foundation; either version 2
+# of the License, or (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+#
+# See COPYRIGHT and LICENSE files for more details.
+#++
+
+require "rails_helper"
+
+RSpec.describe AttributeGroups::AttributeKeyValueComponent, type: :component do
+  subject(:component_render) do
+    render_inline(described_class.new(attribute_key: 'Attribute Key', attribute_value: 'Attribute Value'))
+  end
+
+  before { component_render }
+
+  it 'renders the attribute key and value' do
+    expect(page).to have_css('.attributes-key-value--key', text: 'Attribute Key') &
+      have_css('.attributes-key-value--value.-text', text: 'Attribute Value')
+  end
+end
