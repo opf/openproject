6888af60195bfd17e88415b1a83a1c1474382634
Mir Bhatia <m.bhatia@openproject.com>
2023-06-27 20:36:27 +0200

    WIP: Add icalendar_settings_controller spec

---
+++ spec/controllers/admin/settings/icalendar_settings_controller_spec.rb
@@ -0,0 +1,94 @@
+# frozen_string_literal: true
+
+# -- copyright
+# OpenProject is an open source project management software.
+# Copyright (C) 2023 the OpenProject GmbH
+#
+# This program is free software; you can redistribute it and/or
+# modify it under the terms of the GNU General Public License version 3.
+#
+# OpenProject is a fork of ChiliProject, which is a fork of Redmine. The copyright follows:
+# Copyright (C) 2006-2013 Jean-Philippe Lang
+# Copyright (C) 2010-2013 the ChiliProject Team
+#
+# This program is free software; you can redistribute it and/or
+# modify it under the terms of the GNU General Public License
+# as published by the Free Software Foundation; either version 2
+# of the License, or (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+#
+# See COPYRIGHT and LICENSE files for more details.
+# ++
+#
+
+require 'spec_helper'
+
+RSpec.describe Admin::Settings::IcalendarSettingsController do
+  let(:project) { create(:project) }
+  let(:user) do
+    create(:admin,
+           member_in_project: project,
+           member_through_role: role)
+  end
+  let(:role) { create(:role, permissions: '') }
+
+  current_user { user }
+
+  describe 'GET #show' do
+    subject { get 'show' }
+
+    describe 'permissions' do
+      let(:fetch) { subject }
+
+      it_behaves_like 'a controller action with require_admin'
+    end
+
+    it 'renders the icalendar settings template' do
+      subject
+
+      expect(response).to be_successful
+      expect(response).to render_template 'admin/settings/icalendar_settings/show', 'layouts/admin'
+    end
+  end
+
+  describe 'PATCH #update' do
+    subject { patch 'update', params: }
+
+    let(:ical_enabled) { true }
+    let(:base_settings) do
+      { ical_enabled: }
+    end
+    let(:params) { { settings: } }
+
+    context 'with valid params' do
+      let(:settings) { base_settings }
+
+      it 'succeeds' do
+        subject
+
+        expect(response).to redirect_to action: :show
+        expect(flash[:notice]).to eq I18n.t(:notice_successful_update)
+      end
+
+      before do
+        Setting.ical_enabled = '0'
+        user = create(:user)
+      end
+
+      it 'allows a user to share calendars', with_settings do
+        binding.pry
+        subject
+
+        expect(user.allowed_to?(:ical_enabled, project)).to be_truthy
+      end
+    end
+  end
+end
