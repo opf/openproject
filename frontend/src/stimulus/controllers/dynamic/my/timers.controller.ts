ff952ef313cf9c4a751f70288e075e28ebbe4ebb
Oliver Günther <o.guenther@openproject.com>
2023-07-04 15:36:59 +0200

    Update frontend/src/stimulus/controllers/dynamic/my/timers.controller.ts
    Co-authored-by: Dombi Attila <83396+dombesz@users.noreply.github.com>

--- frontend/src/stimulus/controllers/dynamic/my/timers.controller.ts
+++ frontend/src/stimulus/controllers/dynamic/my/timers.controller.ts
@@ -27,16 +27,8 @@ export default class MyTimersController extends Controller {
     const start = moment(value as string);
     const now = moment();
     let offset = moment(now).diff(start, 'seconds');
-    let seconds = '';
-    let hours = '';
-    let minutes = '';
     setInterval(() => {
-      /* eslint-disable no-plusplus */
-      seconds=this.pad(++offset%60);
-      minutes=this.pad(parseInt((offset / 60).toString(), 10) % 60);
-      hours = this.pad(parseInt((offset / 3600).toString(), 10));
-
-      this.elapsedTimeTarget.textContent = `${hours}:${minutes}:${seconds}`;
+      this.elapsedTimeTarget.textContent = moment.utc(now.diff(start, 'seconds')).format("HH:mm:ss")
     }, 1000);
   }
 }
