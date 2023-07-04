37cf152fbc07ece8e39e8c37fafe8d3f4dd83434
Oliver Günther <mail@oliverguenther.de>
2023-07-04 19:40:06 +0200

    Fix formatter using milliseconds

--- frontend/src/app/features/work-packages/components/wp-timer-button/time-formatter.helper.ts
+++ frontend/src/app/features/work-packages/components/wp-timer-button/time-formatter.helper.ts
@@ -3,7 +3,7 @@ import * as moment from 'moment/moment';
 export function formatElapsedTime(startTime:string):string {
   const start = moment(startTime);
   const now = moment();
-  const offset = moment(now).diff(start, 'seconds');
+  const offset = moment(now).diff(start);
 
   return moment.utc(offset).format('HH:mm:ss');
 }
