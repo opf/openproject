aa9c4749c0cb7d21057cccddd9e0d3b4ec76aa7c
Andreas Pfohl <a.pfohl@openproject.com>
2023-07-11 10:23:11 +0200

    [48537] Renamed and changed CopyTemplateFolderCommand

---
+++ modules/storages/app/common/storages/peripherals/storage_interaction/nextcloud/copy_template_folder_command.rb
@@ -0,0 +1,121 @@
+#-- copyright
+# OpenProject is an open source project management software.
+# Copyright (C) 2012-2023 the OpenProject GmbH
+#
+# This program is free software; you can redistribute it and/or
+# modify it under the terms of the GNU General Public License version 3.
+#
+# OpenProject is a fork of ChiliProject, which is a fork of Redmine. The copyright follows:
+# Copyright (C) 2006-2013 Jean-Philippe Lang
+# Copyright (C) 2010-2013 the ChiliProject Team
+#
+# This program is free software; you can redistribute it and/or
+# modify it under the terms of the GNU General Public License
+# as published by the Free Software Foundation; either version 2
+# of the License, or (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+#
+# See COPYRIGHT and LICENSE files for more details.
+#++
+
+module Storages::Peripherals::StorageInteraction::Nextcloud
+  class CopyTemplateFolderCommand
+    using Storages::Peripherals::ServiceResultRefinements
+
+    def initialize(storage)
+      @storage = storage
+    end
+
+    def call(source_path, destination_path)
+      validate_inputs(source_path, destination_path) >>
+        build_origin_paths >>
+        validate_destination >>
+        copy_folder
+    end
+
+    def validate_inputs(source_path, destination_path)
+      if source_path.blank? || destination_path.blank?
+        return Util.error(:error, 'Source and destination paths must be present.')
+      end
+
+      ServiceResult.success(result: { source_path:, destination_path: })
+    end
+
+    def build_origin_paths
+      ->(input) do
+        source = Util.join_uri_path(
+          @storage.host,
+          "remote.php/dav/files",
+          CGI.escapeURIComponent(@storage.username),
+          Util.escape_path(input[:source_path])
+        )
+
+        destination = Util.join_uri_path(
+          @storage.host,
+          "remote.php/dav/files",
+          CGI.escapeURIComponent(@storage.username),
+          Util.escape_path(input[:destination_path])
+        )
+
+        ServiceResult.success(result: { source_url: source, destination_url: destination })
+      end
+    end
+
+    def validate_destination
+      ->(urls) do
+        request = Net::HTTP::Head.new(urls[:destination_url])
+        request.initialize_http_header Util.basic_auth_header(@storage.username, @storage.password)
+
+        response = Util.http(@storage.host).request(request)
+
+        case response
+        when Net::HTTPSuccess
+          Util.error(:conflict, 'Destination folder already exists.')
+        when Net::HTTPUnauthorized
+          Util.error(:not_authorized)
+        when Net::HTTPNotFound
+          ServiceResult.success(result: urls)
+        else
+          Util.error(:unknown)
+        end
+      end
+    end
+
+    # rubocop:disable Metrics/AbcSize
+    def copy_folder
+      ->(urls) do
+        request = Net::HTTP::Copy.new(urls[:source_url])
+        request['Destination'] = urls[:destination_url]
+
+        headers = Util.basic_auth_header(@storage.username, @storage.password)
+        headers['Depth'] = 'infinity'
+        request.initialize_http_header headers
+
+        response = Util.http(@storage.host).request(request)
+
+        case response
+        when Net::HTTPSuccess
+          ServiceResult.success(message: 'Folder was successfully copied.')
+        when Net::HTTPUnauthorized
+          Util.error(:not_authorized)
+        when Net::HTTPNotFound
+          Util.error(:not_found)
+        when Net::HTTPConflict
+          Util.error(:conflict, error_text_from_response(response))
+        else
+          Util.error(:unknown)
+        end
+      end
+    end
+
+    # rubocop:enable Metrics/AbcSize
+  end
+end
