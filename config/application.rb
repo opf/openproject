d90573f8ac57e51ed8ddd874fdb02a39a44bcb8d
Henriette Darge <h.darge@openproject.com>
2023-07-11 12:04:14 +0200

    Introduce Primer CSS and Primer ViewComponents

--- config/application.rb
+++ config/application.rb
@@ -32,6 +32,8 @@ require 'rails/all'
 require 'active_support'
 require 'active_support/dependencies'
 require 'core_extensions'
+require "primer/view_components"
+require "primer/view_components/engine"
 
 # Silence deprecations early on for testing on CI and production
 ActiveSupport::Deprecation.silenced =
