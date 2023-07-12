78d921a83798543fca6526a7fa7e00abbf6390d9
Aaron Contreras <aaronlcaq@gmail.com>
2023-07-12 17:18:16 -0500

    Cover missing routes in routing spec
    There were a couple of routes in the routes file that were not covered
    in the `team_planner_routing_spec`. For completeness-sake, added them
    in.

--- modules/team_planner/spec/routing/team_planner_routing_spec.rb
+++ modules/team_planner/spec/routing/team_planner_routing_spec.rb
@@ -29,6 +29,12 @@
 require 'spec_helper'
 
 RSpec.describe 'Team planner routing' do
+  it 'routes to team_planner#overview' do
+    expect(subject)
+      .to route(:get, '/team_planners')
+            .to(controller: 'team_planner/team_planner', action: :overview)
+  end
+
   it 'routes to team_planner#index' do
     expect(subject)
       .to route(:get, '/projects/foobar/team_planners')
@@ -42,7 +48,13 @@ RSpec.describe 'Team planner routing' do
   end
 
   context 'with :project_id' do
-    it 'routes to team_planner#new' do
+    it 'routes to team_planner#upsale' do
+      expect(subject)
+        .to route(:get, '/projects/foobar/team_planners/upsale')
+              .to(controller: 'team_planner/team_planner', action: :upsale, project_id: 'foobar')
+    end
+
+    it 'routes to team_planner#show' do
       expect(subject)
         .to route(:get, '/projects/foobar/team_planners/new')
               .to(controller: 'team_planner/team_planner', action: :show, project_id: 'foobar')
@@ -50,6 +62,12 @@ RSpec.describe 'Team planner routing' do
   end
 
   context 'without :project_id' do
+    it 'routes to team_planner#upsale' do
+      expect(subject)
+        .to route(:get, '/team_planners/upsale')
+              .to(controller: 'team_planner/team_planner', action: :upsale)
+    end
+
     it 'routes to team_planner#new' do
       expect(subject)
         .to route(:get, '/team_planners/new')
