ae3da7e74211be095691ae858680e6fb84722a2a
Oliver Günther <o.guenther@openproject.com>
2023-07-17 08:32:12 +0200

    Allow saving CKEditor with ctrl+enter or cmd+enter (#13127)
    * Add listener and output for ckeditor component to save with keyboard
    
    * Clean up eslint for outputs
    
    * Extend spec to save with keyboard

--- frontend/src/app/shared/components/editor/components/ckeditor/op-ckeditor.component.ts
+++ frontend/src/app/shared/components/editor/components/ckeditor/op-ckeditor.component.ts
@@ -45,10 +45,10 @@ import {
   ICKEditorWatchdog,
 } from 'core-app/shared/components/editor/components/ckeditor/ckeditor.types';
 import { CKEditorSetupService } from 'core-app/shared/components/editor/components/ckeditor/ckeditor-setup.service';
+import { KeyCodes } from 'core-app/shared/helpers/keyCodes.enum';
+import { debugLog } from 'core-app/shared/helpers/debug_output';
 
 declare module 'codemirror';
-
-const manualModeLocalStorageKey = 'op-ckeditor-uses-manual-mode';
 
 @Component({
   selector: 'op-ckeditor',
@@ -68,13 +68,16 @@ export class OpCkeditorComponent implements OnInit, OnDestroy {
   }
 
   // Output notification once ready
-  @Output() onInitialized = new EventEmitter<ICKEditorInstance>();
+  @Output() initializeDone = new EventEmitter<ICKEditorInstance>();
 
   // Output notification at max once/s for data changes
-  @Output() onContentChange = new EventEmitter<string>();
+  @Output() contentChanged = new EventEmitter<string>();
 
   // Output notification when editor cannot be initialized
-  @Output() onInitializationFailed = new EventEmitter<string>();
+  @Output() initializationFailed = new EventEmitter<string>();
+
+  // Output save requests (ctrl+enter and cmd+enter)
+  @Output() saveRequested = new EventEmitter<string>();
 
   // View container of the replacement used to initialize CKEditor5
   @ViewChild('opCkeditorReplacementContainer', { static: true }) opCkeditorReplacementContainer:ElementRef;
@@ -105,9 +108,9 @@ export class OpCkeditorComponent implements OnInit, OnDestroy {
   // to read back changes as they happen
   private debouncedEmitter = _.debounce(
     () => {
-      this.getTransformedContent(false)
+      void this.getTransformedContent(false)
         .then((val) => {
-          this.onContentChange.emit(val);
+          this.contentChanged.emit(val);
         });
     },
     1000,
@@ -116,11 +119,13 @@ export class OpCkeditorComponent implements OnInit, OnDestroy {
 
   private $element:JQuery;
 
-  constructor(private readonly elementRef:ElementRef,
+  constructor(
+    private readonly elementRef:ElementRef,
     private readonly Notifications:ToastService,
     private readonly I18n:I18nService,
     private readonly configurationService:ConfigurationService,
-    private readonly ckEditorSetup:CKEditorSetupService) {
+    private readonly ckEditorSetup:CKEditorSetupService,
+  ) {
   }
 
   /**
@@ -176,13 +181,14 @@ export class OpCkeditorComponent implements OnInit, OnDestroy {
   ngOnInit() {
     try {
       this.initializeEditor();
-    } catch (error) {
+    } catch (error:unknown) {
       // We will run into this error if, among others, the browser does not fully support
       // CKEditor's requirements on ES6.
 
-      console.error(`Failed to setup CKEditor instance: ${error}`);
-      this.error = error;
-      this.onInitializationFailed.emit(error);
+      const message = (error as Error).toString();
+      console.error('Failed to setup CKEditor instance: %O', error);
+      this.error = message;
+      this.initializationFailed.emit(message);
     }
   }
 
@@ -208,18 +214,21 @@ export class OpCkeditorComponent implements OnInit, OnDestroy {
       })
       .then((watchdog:ICKEditorWatchdog) => {
         this.setupWatchdog(watchdog);
-        this.ckEditorInstance = watchdog.editor;
+        const editor = watchdog.editor;
+        this.ckEditorInstance = editor;
 
-        // Save changes while in wysiwyg mode
-        watchdog.editor.model.document.on('change', this.debouncedEmitter);
 
         // Switch mode
-        watchdog.editor.on('op:source-code-enabled', () => this.enableManualMode());
-        watchdog.editor.on('op:source-code-disabled', () => this.disableManualMode());
+        editor.on('op:source-code-enabled', () => this.enableManualMode());
+        editor.on('op:source-code-disabled', () => this.disableManualMode());
+
+        // Capture CTRL+ENTER commands
+        this.interceptModifiedEnterKeystrokes(editor);
 
         // Emit global dragend events for other drop zones to react.
         // This is needed, as CKEditor does not bubble any drag events
-        const model = watchdog.editor.model as unknown&{ on:(ev:string, callback:() => unknown) => void };
+        const model = watchdog.editor.model;
+        model.document.on('change', this.debouncedEmitter);
         model.on('op:attachment-added', () => document.body.dispatchEvent(new DragEvent('dragend')));
         model.on('op:attachment-removed', () => document.body.dispatchEvent(new DragEvent('dragend')));
 
@@ -230,11 +239,26 @@ export class OpCkeditorComponent implements OnInit, OnDestroy {
         // not vanishing after ending the drag.
         this.$element.on('dragleave', () => document.body.dispatchEvent(new DragEvent('dragleave')));
 
-        this.onInitialized.emit(watchdog.editor);
+        this.initializeDone.emit(watchdog.editor);
         return watchdog.editor;
       });
 
     this.$element.data('editor', editorPromise);
+  }
+
+  private interceptModifiedEnterKeystrokes(editor:ICKEditorInstance) {
+    editor.listenTo(
+      editor.editing.view.document,
+      'keydown',
+      (evt, data) => {
+        if ((data.ctrlKey || data.metaKey) && data.keyCode === KeyCodes.ENTER) {
+          debugLog('Sending save request from CKEditor.');
+          this.saveRequested.emit();
+          evt.stop();
+        }
+      },
+      { priority: 'highest' },
+    );
   }
 
   /**
