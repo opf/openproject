71f6e54f5b1b54da4638eb3fb3445bf1c69efb54
Oliver Günther <mail@oliverguenther.de>
2023-07-17 13:28:07 +0200

    Fix timer button rolling over after 24h

--- frontend/src/app/features/work-packages/components/wp-timer-button/time-formatter.helper.ts
+++ frontend/src/app/features/work-packages/components/wp-timer-button/time-formatter.helper.ts
@@ -1,9 +1,25 @@
 import * as moment from 'moment/moment';
 
+function paddedNumber(input:number):string {
+  if (input < 10) {
+    return `0${input}`;
+  }
+
+  return input.toString();
+}
+
 export function formatElapsedTime(startTime:string):string {
   const start = moment(startTime);
   const now = moment();
-  const offset = moment(now).diff(start);
+  const duration = now.diff(start, 'seconds');
 
-  return moment.utc(offset).format('HH:mm:ss');
+  const hours = Math.floor(duration / 3600);
+  const minutes = Math.floor((duration - (hours * 3600)) / 60);
+  const seconds = duration - (hours * 3600) - (minutes * 60);
+
+  return [
+    paddedNumber(hours),
+    paddedNumber(minutes),
+    paddedNumber(seconds),
+  ].join(':');
 }
