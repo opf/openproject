0f662f7790dc6885c514afc96ef24b83b1750151
bsatarnejad <b.satarnejad@gmail.com>
2023-07-18 13:40:45 +0200

    use toast class instead of flash in toast service and for showing messages after an ajax call

--- frontend/src/app/shared/components/toaster/toast.service.ts
+++ frontend/src/app/shared/components/toaster/toast.service.ts
@@ -42,7 +42,8 @@ import {
 } from 'core-app/features/hal/resources/error-resource';
 
 export function removeSuccessFlashMessages():void {
-  jQuery('.flash.notice').remove();
+  debugger;
+  jQuery('.op-toast.-success').remove();
 }
 
 export type ToastType = 'success'|'error'|'warning'|'info'|'upload'|'loading';
