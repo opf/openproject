25c3ce55cc03c4191af05fea5b90263791ed418b
Klaus Zanders <k.zanders@openproject.com>
2023-07-19 13:14:16 +0200

    Only display the Global section when requesting to show global roles

--- app/services/roles/set_attributes_service.rb
+++ app/services/roles/set_attributes_service.rb
@@ -29,7 +29,7 @@
 module Roles
   class SetAttributesService < ::BaseServices::SetAttributes
     def set_default_attributes(*)
-      model.permissions = Role.non_member.permissions if model.permissions.nil? || model.permissions.empty?
+      model.permissions = Role.non_member.permissions if model.permissions.blank?
     end
   end
 end
