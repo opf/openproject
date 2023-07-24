b4d17711fa453fc98a26088a8f72a39ac0f079d9
Henriette Darge <h.darge@openproject.com>
2023-07-24 13:38:09 +0200

    Attempt to load the primer_view_component.js

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -3,5 +3,6 @@ OpenProject::Application.configure do
     favicon.ico
     locales/*.js
     openapi-explorer.min.js
+    @primer/view-components/app/assets/javascripts/primer_view_components.js
   )
 end
