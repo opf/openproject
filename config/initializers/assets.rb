61e7c53ce2ad7378dd81a444073355f348e37bad
Henriette Darge <h.darge@openproject.com>
2023-07-25 11:37:43 +0200

    Revert "Attempt to load the primer_view_component.js"
    This reverts commit b4d17711fa453fc98a26088a8f72a39ac0f079d9.

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -3,6 +3,5 @@ OpenProject::Application.configure do
     favicon.ico
     locales/*.js
     openapi-explorer.min.js
-    @primer/view-components/app/assets/javascripts/primer_view_components.js
   )
 end
