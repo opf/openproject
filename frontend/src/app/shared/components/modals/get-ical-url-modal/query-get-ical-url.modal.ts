91d2d6e5804242d7c2aedafa112236d9f0a67175
Klaus Zanders <k.zanders@openproject.com>
2023-07-27 17:06:58 +0200

    Update modal texts

--- frontend/src/app/shared/components/modals/get-ical-url-modal/query-get-ical-url.modal.ts
+++ frontend/src/app/shared/components/modals/get-ical-url-modal/query-get-ical-url.modal.ts
@@ -123,7 +123,7 @@ export class QueryGetIcalUrlModalComponent extends OpModalComponent implements O
       return;
     }
 
-    let icalUrl = '';
+    const icalUrl = '';
 
     const tokenName = (this.tokenNameForm.value as TokenNameFormValue)?.name;
 
@@ -136,7 +136,7 @@ export class QueryGetIcalUrlModalComponent extends OpModalComponent implements O
 
     void promise
       .then((response:{ icalUrl:{ href:string } }) => {
-        this.copyToClipboardService.copy(String(response.icalUrl.href));
+        this.copyToClipboardService.copy(String(response.icalUrl.href), this.I18n.t('js.ical_sharing_modal.success_message', { name: tokenName }));
         this.closeMe();
       })
       .catch((error:{ message:string }) => {
