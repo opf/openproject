038200d78efc02cf62722b91e32a922951c2124d
Jonas Jabari <jonas@basemate.com>
2023-07-27 11:06:28 +0200

    fixed module

--- app/components/op_turbo/stream_wrapper_component.rb
+++ app/components/op_turbo/stream_wrapper_component.rb
@@ -26,8 +26,8 @@
 # See COPYRIGHT and LICENSE files for more details.
 #++
 
-module Base
-  class OpTurbo::StreamWrapperComponent < ApplicationComponent
+module OpTurbo
+  class StreamWrapperComponent < ApplicationComponent
     def initialize(template:, action:, target:)
       super()
 
