dbaf090f283494e5030fbcb72027d95b5ade9b46
Aaron Contreras <aaronlcaq@gmail.com>
2023-07-31 06:54:17 -0500

    Replace special `POST /boards` route with `resources` macro
    The special case for the `POST /boards` route was only needed due
    to the collisions that would happen when sharing an index and show
    route with the same path matcher. Once these routes were split, there
    was no longer a path helper naming collission so we can ammend the
    `resources` macro with `only: %i[create]` now as well, making better
    use of it and cleaning up the routes.

--- modules/boards/config/routes.rb
+++ modules/boards/config/routes.rb
@@ -9,12 +9,7 @@ OpenProject::Application.routes.draw do
   scope 'projects/:project_id', as: 'project' do
     resources :boards,
               controller: 'boards/boards',
-              only: %i[index new show],
+              only: %i[index show new create],
               as: :work_package_boards
-
-    # Adding the `create` action into the above `resources` macro would
-    # result in a name collision between it and `get /boards(/*state)`
-    # as it would result in both being named `project_work_package_boards`
-    post '/boards', to: 'boards/boards#create'
   end
 end
