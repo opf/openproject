5f23ebe14a7c324bc575836bcbfd216654897070
Aaron Contreras <aaronlcaq@gmail.com>
2023-08-01 16:56:56 -0500

    Remove leading whitespace in Calendar invite's WorkPackage description

--- modules/calendar/spec/services/create_ical_service_spec.rb
+++ modules/calendar/spec/services/create_ical_service_spec.rb
@@ -61,14 +61,14 @@ RSpec.describe Calendar::CreateICalService, type: :model do
     described_class.new
   end
 
-  let(:freezed_date_time) { DateTime.now }
+  let(:frozen_date_time) { DateTime.now }
 
   let(:formatted_result) do
     subject.result.gsub("\r\n ", "").gsub("\r", "").gsub("\\n", "\n")
   end
 
   before do
-    Timecop.freeze(freezed_date_time)
+    Timecop.freeze(frozen_date_time)
   end
 
   subject do
@@ -95,7 +95,7 @@ RSpec.describe Calendar::CreateICalService, type: :model do
       UID:#{work_package_with_due_date.id}@localhost:3000
       DTSTART;VALUE=DATE:#{work_package_with_due_date.due_date.strftime('%Y%m%d')}
       DTEND;VALUE=DATE:#{(work_package_with_due_date.due_date + 1.day).strftime('%Y%m%d')}
-      DESCRIPTION:Project: #{project.name}\nType: None\nStatus: #{work_package_with_due_date.status.name}\nAssignee: \nPriority: #{work_package_with_due_date.priority.name}\n\nDescription:\n #{work_package_with_due_date.description}
+      DESCRIPTION:Project: #{project.name}\nType: None\nStatus: #{work_package_with_due_date.status.name}\nAssignee: \nPriority: #{work_package_with_due_date.priority.name}\n\nDescription:\n#{work_package_with_due_date.description}
       LOCATION:http://localhost:3000/work_packages/#{work_package_with_due_date.id}
       ORGANIZER;CN=#{work_package_with_due_date.author.name}:mailto:#{work_package_with_due_date.author.mail}
       SUMMARY:#{work_package_with_due_date.name}
@@ -105,7 +105,7 @@ RSpec.describe Calendar::CreateICalService, type: :model do
       UID:#{work_package_with_start_date.id}@localhost:3000
       DTSTART;VALUE=DATE:#{work_package_with_start_date.start_date.strftime('%Y%m%d')}
       DTEND;VALUE=DATE:#{(work_package_with_start_date.start_date + 1.day).strftime('%Y%m%d')}
-      DESCRIPTION:Project: #{project.name}\nType: None\nStatus: #{work_package_with_start_date.status.name}\nAssignee: \nPriority: #{work_package_with_start_date.priority.name}\n\nDescription:\n #{work_package_with_start_date.description}
+      DESCRIPTION:Project: #{project.name}\nType: None\nStatus: #{work_package_with_start_date.status.name}\nAssignee: \nPriority: #{work_package_with_start_date.priority.name}\n\nDescription:\n#{work_package_with_start_date.description}
       LOCATION:http://localhost:3000/work_packages/#{work_package_with_start_date.id}
       ORGANIZER;CN=#{work_package_with_start_date.author.name}:mailto:#{work_package_with_start_date.author.mail}
       SUMMARY:#{work_package_with_start_date.name}
@@ -115,7 +115,7 @@ RSpec.describe Calendar::CreateICalService, type: :model do
       UID:#{work_package_with_start_and_due_date.id}@localhost:3000
       DTSTART;VALUE=DATE:#{work_package_with_start_and_due_date.start_date.strftime('%Y%m%d')}
       DTEND;VALUE=DATE:#{(work_package_with_start_and_due_date.due_date + 1.day).strftime('%Y%m%d')}
-      DESCRIPTION:Project: #{project.name}\nType: None\nStatus: #{work_package_with_start_and_due_date.status.name}\nAssignee: \nPriority: #{work_package_with_start_and_due_date.priority.name}\n\nDescription:\n #{work_package_with_start_and_due_date.description}
+      DESCRIPTION:Project: #{project.name}\nType: None\nStatus: #{work_package_with_start_and_due_date.status.name}\nAssignee: \nPriority: #{work_package_with_start_and_due_date.priority.name}\n\nDescription:\n#{work_package_with_start_and_due_date.description}
       LOCATION:http://localhost:3000/work_packages/#{work_package_with_start_and_due_date.id}
       ORGANIZER;CN=#{work_package_with_start_and_due_date.author.name}:mailto:#{work_package_with_start_and_due_date.author.mail}
       SUMMARY:#{work_package_with_start_and_due_date.name}
@@ -125,7 +125,7 @@ RSpec.describe Calendar::CreateICalService, type: :model do
       UID:#{work_package_with_due_date_and_assignee.id}@localhost:3000
       DTSTART;VALUE=DATE:#{work_package_with_due_date_and_assignee.due_date.strftime('%Y%m%d')}
       DTEND;VALUE=DATE:#{(work_package_with_due_date_and_assignee.due_date + 1.day).strftime('%Y%m%d')}
-      DESCRIPTION:Project: #{project.name}\nType: None\nStatus: #{work_package_with_due_date_and_assignee.status.name}\nAssignee: #{work_package_with_due_date_and_assignee.assigned_to.name}\nPriority: #{work_package_with_due_date_and_assignee.priority.name}\n\nDescription:\n #{work_package_with_due_date_and_assignee.description}
+      DESCRIPTION:Project: #{project.name}\nType: None\nStatus: #{work_package_with_due_date_and_assignee.status.name}\nAssignee: #{work_package_with_due_date_and_assignee.assigned_to.name}\nPriority: #{work_package_with_due_date_and_assignee.priority.name}\n\nDescription:\n#{work_package_with_due_date_and_assignee.description}
       LOCATION:http://localhost:3000/work_packages/#{work_package_with_due_date_and_assignee.id}
       ORGANIZER;CN=#{work_package_with_due_date_and_assignee.author.name}:mailto:#{work_package_with_due_date_and_assignee.author.mail}
       SUMMARY:#{work_package_with_due_date_and_assignee.name}
