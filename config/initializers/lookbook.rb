44ce61bd598342a59121f0f7ce4385e05ebe1847
Oliver Günther <mail@oliverguenther.de>
2023-08-03 10:31:27 +0200

    Add configuration for lookbook on production

--- config/initializers/lookbook.rb
+++ config/initializers/lookbook.rb
@@ -1,4 +1,4 @@
-OpenProject::Application.configure do
+Rails.application.config.after_initialize do
   next unless OpenProject::Configuration.lookbook_enabled?
 
   config.lookbook.project_name = "OpenProject Lookbook"
