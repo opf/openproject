fc99ed2afcf78ce6b77651114496e86e5d55e3b0
Klaus Zanders <k.zanders@openproject.com>
2023-08-08 10:09:02 +0200

    Revert "Leave a TODO to add tests"
    This reverts commit d0b4a0967d765d0f20e168a9354e5b88d27780f8.

--- spec/services/groups/create_inherited_roles_service_integration_spec.rb
+++ spec/services/groups/create_inherited_roles_service_integration_spec.rb
@@ -28,8 +28,6 @@
 
 require 'spec_helper'
 
-# TODO: Add tests for work package inheritance, when we have introduced work package roles
-
 RSpec.describe Groups::CreateInheritedRolesService, 'integration' do
   # The setup for these specs are a bit weird. First, the group is set up with its users already
   # attached. Then, the memberships of the group are added afterwards.
