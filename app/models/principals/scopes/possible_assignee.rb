d6b25ccc507c2dd77430f1bd81b20c182bc8fc53
Klaus Zanders <k.zanders@openproject.com>
2023-08-09 15:19:17 +0200

    Fix query for Possible Assignees

--- app/models/principals/scopes/possible_assignee.rb
+++ app/models/principals/scopes/possible_assignee.rb
@@ -49,7 +49,7 @@ module Principals::Scopes
               .assignable
               .of(project)
               .group('user_id')
-              .having(["COUNT(DISTINCT(project_id, user_id)) = ?", Array(project).count])
+              .having(["COUNT(DISTINCT(entity_type, entity_id, user_id)) = ?", Array(project).count])
               .select('user_id')
         )
       end
