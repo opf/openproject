6445ab1db51fd616c72d09b0253a314df5d50d4e
Oliver Günther <mail@oliverguenther.de>
2023-08-16 11:35:12 +0200

    Modify the renderer ENV to include the host name and relative URL root
    https://community.openproject.org/work_packages/49086
    
    Just modifying the defaults do not work
    see:
      - https://github.com/hotwired/turbo-rails/issues/155
      - https://github.com/rails/rails/issues/41795

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -1,8 +1,13 @@
 # Be sure to restart your server when you modify this file.
 
-# ActiveSupport::Reloader.to_prepare do
-#   ApplicationController.renderer.defaults.merge!(
-#     http_host: 'example.org',
-#     https: false
-#   )
-# end
+# Just modifying the defaults do not work
+# see:
+# - https://github.com/hotwired/turbo-rails/issues/155
+# - https://github.com/rails/rails/issues/41795
+ActiveSupport::Reloader.to_prepare do
+  ApplicationController.renderer.instance_variable_get(:@env).merge!(
+    'HTTP_HOST' => Setting.host_name,
+    'HTTPS' => Setting.https?,
+    'SCRIPT_NAME' => OpenProject::Configuration.rails_relative_url_root
+  )
+end
