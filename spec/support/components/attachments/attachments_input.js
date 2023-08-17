ec42caa72f9ccbba36fed5aa5ffae8f46a16fcb6
Dombi Attila <83396+dombesz@users.noreply.github.com>
2023-08-17 18:06:53 +0300

    [#49507] File Drag and Drop
    https://community.openproject.org/work_packages/49507

--- spec/support/components/attachments/attachments_input.js
+++ spec/support/components/attachments/attachments_input.js
@@ -10,7 +10,67 @@ let name = params[1];
 let position = params[2];
 
 // We might want to drag the file over something, then wait a bit and drag it elsewhere
-let stopover = params[3];
+let stopovers;
+
+if (params[3] === null) {
+  stopovers = [];
+} else if (Array.isArray(params[3])) {
+  stopovers = params[3];
+} else {
+  stopovers = [params[3]];
+}
+
+// Cancel the drop event
+let cancelDrop = params[4];
+
+// Delay drag leave to allow the work package tabs to become active on dragover event
+let delayDragleave = params[5];
+
+function buildDragEvent(type, targetX, targetY, dataTransfer) {
+  let event = new MouseEvent(type, { clientX: targetX, clientY: targetY, bubbles: true });
+
+  // Override the constructor to the DragEvent class
+  Object.setPrototypeOf(event, null);
+  event.dataTransfer = dataTransfer;
+  Object.setPrototypeOf(event, DragEvent.prototype);
+  return event;
+}
+
+function dropOnStopover(stopover, dataTransfer) {
+  // Look up the selector
+  if (typeof stopover === 'string') {
+    stopover = document.querySelector(stopover);
+  }
+
+  // We need coordinates to drop to the element
+  let stopbox = stopover.getBoundingClientRect();
+  let stopX;
+  let stopY;
+
+  stopX = stopbox.left + (stopbox.width / 2);
+  stopY = stopbox.top + (stopbox.height / 2);
+
+  // Fire multiple drag events, to better simulate the mouse movement.
+  let eventTypes = ['dragenter', 'dragover', 'dragleave']
+
+  if (cancelDrop) {
+    // Firing a second 'dragleave' means the drag and drop is canceled.
+    eventTypes.push('dragleave');
+  }
+
+  eventTypes.forEach(function (type) {
+    let event = buildDragEvent(type, stopX, stopY, dataTransfer);
+    if (delayDragleave && type === 'dragleave') {
+      setTimeout(() => {
+        console.log("Dispatching event %O", event);
+        stopover.dispatchEvent(event);
+      }, 500);
+    } else {
+      console.log("Dispatching event %O", event);
+      stopover.dispatchEvent(event);
+    }
+  });
+}
 
 function dropOnTarget(dataTransfer) {
   // Look up the selector
@@ -37,13 +97,7 @@ function dropOnTarget(dataTransfer) {
   }
 
   ['dragenter', 'dragover', 'drop'].forEach(function (type) {
-    let event = new MouseEvent(type, { clientX: targetX, clientY: targetY });
-
-    // Override the constructor to the DragEvent class
-    Object.setPrototypeOf(event, null);
-    event.dataTransfer = dataTransfer;
-    Object.setPrototypeOf(event, DragEvent.prototype);
-
+    let event = buildDragEvent(type, targetX, targetY, dataTransfer);
     console.log("Dispatching event %O", event);
     target.dispatchEvent(event);
   });
@@ -71,31 +125,21 @@ let input = jQuery('<input>')
         setDragImage  : function setDragImage(){}
       };
 
-      // If we have a stopover, do that first and then get the target
-      if (stopover) {
-        // We need coordinates to drop to the element
-        let stopbox = stopover.getBoundingClientRect();
-        let stopX;
-        let stopY;
+      // If we have stopovers, do those first and then get the target
+      if (stopovers.length > 0) {
+        stopovers.forEach((stopover) => dropOnStopover(stopover, dataTransfer));
 
-        stopX = stopbox.left + (stopbox.width / 2);
-        stopY = stopbox.top + (stopbox.height / 2);
-
-        ['dragenter', 'dragover'].forEach(function (type) {
-          let event = new MouseEvent(type, { clientX: stopX, clientY: stopY });
-
-          // Override the constructor to the DragEvent class
-          Object.setPrototypeOf(event, null);
-          event.dataTransfer = dataTransfer;
-          Object.setPrototypeOf(event, DragEvent.prototype);
-
-          console.log("Dispatching event %O", event);
-          stopover.dispatchEvent(event);
-        });
-
-        setTimeout(() => dropOnTarget(dataTransfer), 2000);
+        setTimeout(() => {
+          if (!cancelDrop) {
+            // After we left the stopover DOM elements, the target element should remain visible.
+            // If it's not visible, we raise an error.
+            if (target.offsetParent === null) {
+              throw new Error("Cannot drop the file on an invisible target");
+            };
+            dropOnTarget(dataTransfer);
+          }
+        }, 2000);
       } else {
         dropOnTarget(dataTransfer);
       }
-
     });
