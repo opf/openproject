a83b6c52a52cd4dbda3b65be3ba1c65527e99ec7
Kabiru Mwenja <hey@akabiru.me>
2023-08-17 13:26:34 +0300

    [#49397] Remove project storage members connection status feature flag (#13499)
    https://community.openproject.org/projects/openproject/work_packages/49397

--- modules/storages/lib/open_project/storages/engine.rb
+++ modules/storages/lib/open_project/storages/engine.rb
@@ -45,7 +45,6 @@ module OpenProject::Storages
 
     initializer 'openproject_storages.feature_decisions' do
       OpenProject::FeatureDecisions.add :storage_file_picking_select_all
-      OpenProject::FeatureDecisions.add :storage_project_members_check
     end
 
     initializer 'openproject_storages.event_subscriptions' do
