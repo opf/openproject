f851bf16c76628da64480d9bc61a2129c19062ec
Aaron Contreras <aaronlcaq@gmail.com>
2023-08-21 14:07:37 -0500

    Adjust `require` statement to use interpolation
    Appeases Rubocop complaint for String concatenation being used instead
    of String interpolation.

--- modules/avatars/spec/models/user_spec.rb
+++ modules/avatars/spec/models/user_spec.rb
@@ -1,5 +1,5 @@
-require File.expand_path(File.dirname(__FILE__) + '/../spec_helper')
-require File.expand_path(File.dirname(__FILE__) + '/../shared_examples')
+require File.expand_path("#{File.dirname(__FILE__)}/../spec_helper")
+require File.expand_path("#{File.dirname(__FILE__)}/../shared_examples")
 
 RSpec.describe User do
   let(:user) { build(:user) }
