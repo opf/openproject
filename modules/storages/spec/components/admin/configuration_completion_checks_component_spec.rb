b9a5b7605411e3365a9c81458a024bed3470b86b
Kabiru Mwenja <makabby@gmail.com>
2023-08-24 20:32:57 +0300

    tests[#49218]: load spec helper via require_relative for consistency

--- modules/storages/spec/components/admin/configuration_completion_checks_component_spec.rb
+++ modules/storages/spec/components/admin/configuration_completion_checks_component_spec.rb
@@ -26,7 +26,7 @@
 # See COPYRIGHT and LICENSE files for more details.
 #++
 #
-require 'spec_helper'
+require_relative '../../spec_helper'
 
 RSpec.describe Storages::Admin::ConfigurationCompletionChecksComponent,
                type: :component do
