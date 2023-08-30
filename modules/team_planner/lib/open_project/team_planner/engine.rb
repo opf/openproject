6ec03891bb1cab56332a92c108dcde1267de0104
Aaron Contreras <aaronlcaq@gmail.com>
2023-08-30 09:34:57 -0500

    Chore: Update Copyright in `team_planner/engine.rb`

--- modules/team_planner/lib/open_project/team_planner/engine.rb
+++ modules/team_planner/lib/open_project/team_planner/engine.rb
@@ -1,6 +1,13 @@
-# OpenProject Team Planner module
+# -- copyright
+# OpenProject is an open source project management software.
+# Copyright (C) 2021-2023 the OpenProject GmbH
 #
-# Copyright (C) 2021 OpenProject GmbH
+# This program is free software; you can redistribute it and/or
+# modify it under the terms of the GNU General Public License version 3.
+#
+# OpenProject is a fork of ChiliProject, which is a fork of Redmine. The copyright follows:
+# Copyright (C) 2006-2013 Jean-Philippe Lang
+# Copyright (C) 2010-2013 the ChiliProject Team
 #
 # This program is free software; you can redistribute it and/or
 # modify it under the terms of the GNU General Public License
@@ -15,6 +22,9 @@
 # You should have received a copy of the GNU General Public License
 # along with this program; if not, write to the Free Software
 # Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+#
+# See COPYRIGHT and LICENSE files for more details.
+# ++
 
 module OpenProject::TeamPlanner
   class Engine < ::Rails::Engine
