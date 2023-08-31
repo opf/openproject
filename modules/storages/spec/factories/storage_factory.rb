a0ddb4c38dc216ff2018c0b160ac5b49429c512b
Andreas Pfohl <a.pfohl@openproject.com>
2023-08-31 13:01:33 +0200

    Fixed storage test factory

--- modules/storages/spec/factories/storage_factory.rb
+++ modules/storages/spec/factories/storage_factory.rb
@@ -28,26 +28,28 @@
 
 FactoryBot.define do
   factory :storage, class: '::Storages::Storage' do
+    provider_type { Storages::Storage::PROVIDER_TYPE_NEXTCLOUD }
     sequence(:name) { |n| "Storage #{n}" }
+    sequence(:host) { |n| "https://host#{n}.example.com" }
     creator factory: :user
 
     factory :nextcloud_storage, class: '::Storages::NextcloudStorage' do
       provider_type { Storages::Storage::PROVIDER_TYPE_NEXTCLOUD }
-      sequence(:host) { |n| "https://host#{n}.example.com" }
 
       trait :as_automatically_managed do
         automatically_managed { true }
         username { 'OpenProject' }
         password { 'Password123' }
       end
-
       trait :as_not_automatically_managed do
         automatically_managed { false }
       end
     end
+  end
 
-    factory :one_drive_storage, class: '::Storages::OneDriveStorage' do
-      provider_type { Storages::Storage::PROVIDER_TYPE_ONE_DRIVE }
-    end
+  factory :one_drive_storage, class: '::Storages::OneDriveStorage' do
+    sequence(:name) { |n| "Storage #{n}" }
+    creator factory: :user
+    provider_type { Storages::Storage::PROVIDER_TYPE_ONE_DRIVE }
   end
 end
