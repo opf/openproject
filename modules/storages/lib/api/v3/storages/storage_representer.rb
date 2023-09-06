ebb13e7bf5dd155e48d1b210b7e14c0f3ec39c68
Andreas Pfohl <a.pfohl@openproject.com>
2023-09-06 14:31:28 +0200

    Enabled logging into Azure for OneDrive from FilesTab

--- modules/storages/lib/api/v3/storages/storage_representer.rb
+++ modules/storages/lib/api/v3/storages/storage_representer.rb
@@ -178,12 +178,14 @@ module API::V3::Storages
     end
 
     associated_resource :oauth_application,
-                        skip_render: ->(*) { !current_user.admin? },
+                        skip_render: ->(*) { !current_user.admin? || !represented.provider_type_nextcloud? },
                         getter: ->(*) {
+                          next unless current_user.admin? && represented.provider_type_nextcloud?
+
                           ::API::V3::OAuth::OAuthApplicationsRepresenter.create(represented.oauth_application, current_user:)
                         },
                         link: ->(*) {
-                          next unless current_user.admin?
+                          next unless current_user.admin? && represented.provider_type_nextcloud?
 
                           {
                             href: api_v3_paths.oauth_application(represented.oauth_application.id),
