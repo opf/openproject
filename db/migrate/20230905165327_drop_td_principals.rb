bdc86b118c5a6231cff87115614197a778ed2527
pragun16 <pragun.joshi@gmail.com>
2023-09-08 11:15:40 +0545

    optd8: removed unnecessary migrations to create table named td_principals

--- db/migrate/20230905165327_drop_td_principals.rb
+++
@@ -1,5 +0,0 @@
-class DropTdPrincipals < ActiveRecord::Migration[7.0]
-  def change
-    drop_table :td_principals
-  end
-end
--- db/migrate/20230905170212_add_details_to_work_packages.rb
+++ db/migrate/20230905170212_add_details_to_work_packages.rb
@@ -1,6 +1,5 @@
 class AddDetailsToWorkPackages < ActiveRecord::Migration[7.0]
   def change
     add_column :work_packages, :td_principal, :float
-    add_column :work_packages, :ticket_id, :integer
   end
 end
