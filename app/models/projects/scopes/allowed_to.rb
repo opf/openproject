2957cbfc98ff8645484a09ef5453c4fccb249eca
ulferts <jens.ulferts@googlemail.com>
2023-09-11 14:04:10 +0200

    rewrite to arel

--- app/models/projects/scopes/allowed_to.rb
+++ app/models/projects/scopes/allowed_to.rb
@@ -41,9 +41,9 @@ module Projects::Scopes
         elsif user.anonymous?
           where(id: allowed_to_non_member_relation(user, permissions))
         else
-          where(arel_table[:id].in(allowed_to_member_relation(user, permissions).arel.union(
-                                     allowed_to_non_member_relation(user, permissions).arel
-                                   )))
+          where(arel_table[:id].in(allowed_to_member_relation(user, permissions)
+                                     .arel
+                                     .union(:all, allowed_to_non_member_relation(user, permissions).arel)))
         end
       end
 
@@ -55,41 +55,15 @@ module Projects::Scopes
       end
 
       def allowed_to_non_member_relation(user, permissions)
-        builtin = if user.logged?
-                    Role::BUILTIN_NON_MEMBER
-                  else
-                    Role::BUILTIN_ANONYMOUS
-                  end
-
-        member_permission = if user.logged?
-                              <<~SQL.squish
-                                AND NOT EXISTS (SELECT 1
-                                                FROM members
-                                                WHERE #{allowed_to_members_condition(user)})
-                              SQL
-                            end
-
         joins(allowed_to_enabled_module_join(permissions))
-          .joins(<<~SQL.squish
-            INNER JOIN "roles"
-              ON "roles"."builtin" = #{builtin}
-              AND "projects"."active" = TRUE
-              AND "projects"."public" = TRUE
-              #{member_permission}
-          SQL
-                )
+          .joins(allowed_to_builtin_roles_in_active_project_join(user))
           .joins(allowed_to_role_permission_join(permissions))
           .select(:id)
       end
 
       def allowed_to_member_relation(user, permissions)
         Member
-          .joins(<<~SQL.squish
-            JOIN "projects"
-              ON "projects"."active" = TRUE
-              AND #{allowed_to_members_condition(user)}
-          SQL
-                )
+          .joins(allowed_to_member_in_active_project_join(user))
           .joins(allowed_to_enabled_module_join(permissions))
           .joins(:roles, :member_roles)
           .joins(allowed_to_role_permission_join(permissions))
@@ -98,40 +72,39 @@ module Projects::Scopes
 
       def allowed_to_enabled_module_join(permissions)
         project_module = permissions.filter_map(&:project_module).uniq
+        enabled_module_table = EnabledModule.arel_table
 
         if project_module.any?
-          sql = <<~SQL.squish
-            INNER JOIN "enabled_modules"
-              ON "projects"."id" = "enabled_modules"."project_id"
-              AND "enabled_modules"."name" IN (:enabled_module_names)
-              AND "projects"."active" = TRUE
-          SQL
-
-          OpenProject::SqlSanitization.sanitize(sql,
-                                                enabled_module_names: project_module)
+          arel_table.join(enabled_module_table, Arel::Nodes::InnerJoin)
+                    .on(arel_table[:id].eq(enabled_module_table[:project_id])
+                          .and(enabled_module_table[:name].in(project_module))
+                          .and(arel_table[:active].eq(true)))
+                    .join_sources
         end
       end
 
       def allowed_to_role_permission_join(permissions)
         return if permissions.all?(&:public?)
 
-        condition = permissions.map do |permission|
-          if permission.public?
-            <<~SQL.squish
-              "role_permissions"."permission" = '#{permission.name}'
-            SQL
-          else
-            <<~SQL.squish
-              (enabled_modules.name = '#{permission.project_module}' AND "role_permissions"."permission" = '#{permission.name}')
-            SQL
-          end
-        end.join(' OR ')
+        role_permissions_table = RolePermission.arel_table
+        enabled_modules_table = EnabledModule.arel_table
+        roles_table = Role.arel_table
 
-        <<~SQL.squish
-          JOIN "role_permissions"
-            ON roles.id = role_permissions.role_id
-            AND (#{condition})
-        SQL
+        condition = permissions.inject(Arel::Nodes::False.new) do |or_condition, permission|
+          permission_condition = role_permissions_table[:permission].eq(permission.name)
+
+          unless permission.public?
+            permission_condition = permission_condition.and(enabled_modules_table[:name].eq(permission.project_module))
+          end
+
+          or_condition.or(permission_condition)
+        end
+
+        arel_table
+          .join(role_permissions_table, Arel::Nodes::InnerJoin)
+          .on(roles_table[:id].eq(role_permissions_table[:role_id])
+                              .and(condition))
+          .join_sources
       end
 
       def allowed_to_permissions(permission)
@@ -143,12 +116,56 @@ module Projects::Scopes
       end
 
       def allowed_to_members_condition(user)
-        <<~SQL.squish
-          members.project_id = projects.id
-          AND members.user_id = #{user.id}
-          AND members.entity_type IS NULL
-          AND members.entity_id IS NULL
-        SQL
+        members_table = Member.arel_table
+
+        members_table[:project_id].eq(arel_table[:id])
+                                  .and(members_table[:user_id].eq(user.id))
+                                  .and(members_table[:entity_type].eq(nil))
+                                  .and(members_table[:entity_id].eq(nil))
+      end
+
+      def allowed_to_builtin_roles_in_active_project_join(user)
+        condition = allowed_to_built_roles_in_active_project_condition(user)
+
+        if user.logged?
+          condition = condition.and(allowed_to_no_member_exists_condition(user))
+        end
+
+        roles_table = Role.arel_table
+
+        arel_table.join(roles_table)
+                  .on(condition)
+                  .join_sources
+      end
+
+      def allowed_to_member_in_active_project_join(user)
+        arel_table.join(arel_table)
+                  .on(arel_table[:active].eq(true)
+                                         .and(allowed_to_members_condition(user)))
+                  .join_sources
+      end
+
+      def allowed_to_built_roles_in_active_project_condition(user)
+        builtin = if user.logged?
+                    Role::BUILTIN_NON_MEMBER
+                  else
+                    Role::BUILTIN_ANONYMOUS
+                  end
+
+        roles_table = Role.arel_table
+
+        roles_table[:builtin].eq(builtin)
+                             .and(arel_table[:active])
+                             .and(arel_table[:public])
+      end
+
+      def allowed_to_no_member_exists_condition(user)
+        Member
+          .select(1)
+          .where(allowed_to_members_condition(user))
+          .arel
+          .exists
+          .not
       end
     end
   end
