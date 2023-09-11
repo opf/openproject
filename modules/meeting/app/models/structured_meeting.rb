28948d2863e9a495c933eb93b1941ce9ac8d7110
Oliver Günther <mail@oliverguenther.de>
2023-09-11 14:07:19 +0200

    Add meeting type and restore standard meeting rendering

--- modules/meeting/app/models/structured_meeting.rb
+++ modules/meeting/app/models/structured_meeting.rb
@@ -1,5 +1,3 @@
-# frozen_string_literal: true
-
 #-- copyright
 # OpenProject is an open source project management software.
 # Copyright (C) 2012-2023 the OpenProject GmbH
@@ -28,36 +26,6 @@
 # See COPYRIGHT and LICENSE files for more details.
 #++
 
-module Meetings
-  class RowComponent < ::RowComponent
-    def project_name
-      helpers.link_to_project model.project, {}, {}, false
-    end
-
-    def title
-      link_to model.title, meeting_path(model)
-    end
-
-    def type
-      if model.is_a?(StructuredMeeting)
-        I18n.t('meeting.types.structured')
-      else
-        I18n.t('meeting.types.classic')
-      end
-    end
-
-    def start_time
-      safe_join([helpers.format_date(model.start_time), helpers.format_time(model.start_time, false)], " ")
-    end
-
-    def duration
-      "#{number_with_delimiter model.duration} h"
-    end
-
-    def location
-      helpers.auto_link(model.location,
-                        link: :all,
-                        html: { target: '_blank' })
-    end
-  end
+class StructuredMeeting < Meeting
+  has_many :agenda_items, dependent: :destroy, class_name: 'MeetingAgendaItem'
 end
