7ee330b88a2330518c690971b4bb42b94d35cd6a
Aaron Contreras <aaronlcaq@gmail.com>
2023-09-15 12:25:44 -0500

    Lint: Use symbol style for metadata
    Addresses "Rubocop: RSpec/MetadataStyle"

--- modules/team_planner/spec/features/query_handling_spec.rb
+++ modules/team_planner/spec/features/query_handling_spec.rb
@@ -32,7 +32,7 @@ require 'spec_helper'
 require_relative '../support/pages/team_planner'
 require_relative '../../../../spec/features/views/shared_examples'
 
-RSpec.describe 'Team planner query handling', js: true, with_ee: %i[team_planner_view] do
+RSpec.describe 'Team planner query handling', :js, with_ee: %i[team_planner_view] do
   shared_let(:type_task) { create(:type_task) }
   shared_let(:type_bug) { create(:type_bug) }
   shared_let(:project) do
