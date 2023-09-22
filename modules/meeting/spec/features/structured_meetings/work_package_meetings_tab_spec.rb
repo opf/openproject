d3a6d3d7f79e63af03b26ebe3d9b8b830449baec
Jonas Jabari <j.jabari@openproject.com>
2023-09-22 15:10:06 +0200

    re-enabled temporarly disabled specs

--- modules/meeting/spec/features/structured_meetings/work_package_meetings_tab_spec.rb
+++ modules/meeting/spec/features/structured_meetings/work_package_meetings_tab_spec.rb
@@ -53,132 +53,132 @@ RSpec.describe 'Open the Meetings tab', :js do
       login_as(user)
     end
 
-    # context 'when the user does not have the permissions to see the meetings tab' do
-    #   let(:role) do
-    #     create(:role,
-    #            permissions: %i(view_work_packages))
-    #   end
+    context 'when the user does not have the permissions to see the meetings tab' do
+      let(:role) do
+        create(:role,
+               permissions: %i(view_work_packages))
+      end
 
-    #   it 'does not show the meetings tab' do
-    #     work_package_page.visit!
+      it 'does not show the meetings tab' do
+        work_package_page.visit!
 
-    #     meetings_tab.expect_tab_not_present
-    #   end
-    # end
+        meetings_tab.expect_tab_not_present
+      end
+    end
 
-    # context 'when the meetings module is not enabled for the project' do
-    #   let(:project) { create(:project, disable_modules: 'meetings') }
+    context 'when the meetings module is not enabled for the project' do
+      let(:project) { create(:project, disable_modules: 'meetings') }
 
-    #   it 'does not show the meetings tab' do
-    #     work_package_page.visit!
+      it 'does not show the meetings tab' do
+        work_package_page.visit!
 
-    #     meetings_tab.expect_tab_not_present
-    #   end
-    # end
+        meetings_tab.expect_tab_not_present
+      end
+    end
 
-    # it 'shows the meetings tab when the user is allowed to see it' do
-    #   work_package_page.visit!
-    #   work_package_page.switch_to_tab(tab: 'meetings')
+    it 'shows the meetings tab when the user is allowed to see it' do
+      work_package_page.visit!
+      work_package_page.switch_to_tab(tab: 'meetings')
 
-    #   meetings_tab.expect_tab_content_rendered
-    # end
+      meetings_tab.expect_tab_content_rendered
+    end
 
-    # context 'when the work_package is not referenced in an upcoming meeting' do
-    #   it 'shows an empty message within the upcoming meetings section' do
-    #     work_package_page.visit!
-    #     work_package_page.switch_to_tab(tab: 'meetings')
+    context 'when the work_package is not referenced in an upcoming meeting' do
+      it 'shows an empty message within the upcoming meetings section' do
+        work_package_page.visit!
+        work_package_page.switch_to_tab(tab: 'meetings')
 
-    #     meetings_tab.expect_upcoming_counter_to_be(0)
+        meetings_tab.expect_upcoming_counter_to_be(0)
 
-    #     expect(page).to have_content('This work package is not scheduled in an upcoming meeting agenda yet.')
-    #   end
-    # end
+        expect(page).to have_content('This work package is not scheduled in an upcoming meeting agenda yet.')
+      end
+    end
 
-    # context 'when the work_package is not referenced in a past meeting' do
-    #   it 'shows an empty message within the past meetings section' do
-    #     work_package_page.visit!
-    #     work_package_page.switch_to_tab(tab: 'meetings')
+    context 'when the work_package is not referenced in a past meeting' do
+      it 'shows an empty message within the past meetings section' do
+        work_package_page.visit!
+        work_package_page.switch_to_tab(tab: 'meetings')
 
-    #     meetings_tab.expect_past_counter_to_be(0)
-    #     meetings_tab.switch_to_past_meetings_section
+        meetings_tab.expect_past_counter_to_be(0)
+        meetings_tab.switch_to_past_meetings_section
 
-    #     expect(page).to have_content('This work package was not mentioned in a past meeting.')
-    #   end
-    # end
+        expect(page).to have_content('This work package was not mentioned in a past meeting.')
+      end
+    end
 
-    # context 'when the work_package is already referenced in upcoming meetings' do
-    #   let!(:first_meeting) { create(:structured_meeting, project:) }
-    #   let!(:second_meeting) { create(:structured_meeting, project:) }
+    context 'when the work_package is already referenced in upcoming meetings' do
+      let!(:first_meeting) { create(:structured_meeting, project:) }
+      let!(:second_meeting) { create(:structured_meeting, project:) }
 
-    #   let!(:first_meeting_agenda_item_of_first_meeting) do
-    #     create(:meeting_agenda_item, meeting: first_meeting, work_package:, notes: "A very important note in first meeting!")
-    #   end
-    #   let!(:second_meeting_agenda_item_of_first_meeting) do
-    #     create(:meeting_agenda_item, meeting: first_meeting, work_package:,
-    #                                  notes: "Another very important note in the first meeting!")
-    #   end
-    #   let!(:meeting_agenda_item_of_second_meeting) do
-    #     create(:meeting_agenda_item, meeting: second_meeting, work_package:,
-    #                                  notes: "A very important note in the second meeting!")
-    #   end
+      let!(:first_meeting_agenda_item_of_first_meeting) do
+        create(:meeting_agenda_item, meeting: first_meeting, work_package:, notes: "A very important note in first meeting!")
+      end
+      let!(:second_meeting_agenda_item_of_first_meeting) do
+        create(:meeting_agenda_item, meeting: first_meeting, work_package:,
+                                     notes: "Another very important note in the first meeting!")
+      end
+      let!(:meeting_agenda_item_of_second_meeting) do
+        create(:meeting_agenda_item, meeting: second_meeting, work_package:,
+                                     notes: "A very important note in the second meeting!")
+      end
 
-    #   it 'shows the meeting agenda items in the upcoming meetings section grouped by meeting' do
-    #     work_package_page.visit!
-    #     work_package_page.switch_to_tab(tab: 'meetings')
+      it 'shows the meeting agenda items in the upcoming meetings section grouped by meeting' do
+        work_package_page.visit!
+        work_package_page.switch_to_tab(tab: 'meetings')
 
-    #     meetings_tab.expect_upcoming_counter_to_be(2)
-    #     meetings_tab.expect_past_counter_to_be(0)
+        meetings_tab.expect_upcoming_counter_to_be(2)
+        meetings_tab.expect_past_counter_to_be(0)
 
-    #     within_test_selector("op-meeting-container-#{first_meeting.id}") do
-    #       expect(page).to have_content(first_meeting.title)
-    #       expect(page).to have_content(first_meeting_agenda_item_of_first_meeting.notes)
-    #       expect(page).to have_content(second_meeting_agenda_item_of_first_meeting.notes)
-    #     end
+        within_test_selector("op-meeting-container-#{first_meeting.id}") do
+          expect(page).to have_content(first_meeting.title)
+          expect(page).to have_content(first_meeting_agenda_item_of_first_meeting.notes)
+          expect(page).to have_content(second_meeting_agenda_item_of_first_meeting.notes)
+        end
 
-    #     within_test_selector("op-meeting-container-#{second_meeting.id}") do
-    #       expect(page).to have_content(second_meeting.title)
-    #       expect(page).to have_content(meeting_agenda_item_of_second_meeting.notes)
-    #     end
-    #   end
-    # end
+        within_test_selector("op-meeting-container-#{second_meeting.id}") do
+          expect(page).to have_content(second_meeting.title)
+          expect(page).to have_content(meeting_agenda_item_of_second_meeting.notes)
+        end
+      end
+    end
 
-    # context 'when the work_package was already referenced in past meetings' do
-    #   let!(:first_past_meeting) { create(:structured_meeting, project:, start_time: Date.yesterday - 10.hours) }
-    #   let!(:second_past_meeting) { create(:structured_meeting, project:, start_time: Date.yesterday - 10.hours) }
+    context 'when the work_package was already referenced in past meetings' do
+      let!(:first_past_meeting) { create(:structured_meeting, project:, start_time: Date.yesterday - 10.hours) }
+      let!(:second_past_meeting) { create(:structured_meeting, project:, start_time: Date.yesterday - 10.hours) }
 
-    #   let!(:first_meeting_agenda_item_of_first_past_meeting) do
-    #     create(:meeting_agenda_item, meeting: first_past_meeting, work_package:, notes: "A very important note in first meeting!")
-    #   end
-    #   let!(:second_meeting_agenda_item_of_first_past_meeting) do
-    #     create(:meeting_agenda_item, meeting: first_past_meeting, work_package:,
-    #                                  notes: "Another very important note in the first meeting!")
-    #   end
-    #   let!(:meeting_agenda_item_of_second_past_meeting) do
-    #     create(:meeting_agenda_item, meeting: second_past_meeting, work_package:,
-    #                                  notes: "A very important note in the second meeting!")
-    #   end
+      let!(:first_meeting_agenda_item_of_first_past_meeting) do
+        create(:meeting_agenda_item, meeting: first_past_meeting, work_package:, notes: "A very important note in first meeting!")
+      end
+      let!(:second_meeting_agenda_item_of_first_past_meeting) do
+        create(:meeting_agenda_item, meeting: first_past_meeting, work_package:,
+                                     notes: "Another very important note in the first meeting!")
+      end
+      let!(:meeting_agenda_item_of_second_past_meeting) do
+        create(:meeting_agenda_item, meeting: second_past_meeting, work_package:,
+                                     notes: "A very important note in the second meeting!")
+      end
 
-    #   it 'shows the meeting agenda items in the past meetings section grouped by meeting' do
-    #     work_package_page.visit!
-    #     work_package_page.switch_to_tab(tab: 'meetings')
+      it 'shows the meeting agenda items in the past meetings section grouped by meeting' do
+        work_package_page.visit!
+        work_package_page.switch_to_tab(tab: 'meetings')
 
-    #     meetings_tab.expect_upcoming_counter_to_be(0)
-    #     meetings_tab.expect_past_counter_to_be(2)
+        meetings_tab.expect_upcoming_counter_to_be(0)
+        meetings_tab.expect_past_counter_to_be(2)
 
-    #     meetings_tab.switch_to_past_meetings_section
+        meetings_tab.switch_to_past_meetings_section
 
-    #     within_test_selector("op-meeting-container-#{first_past_meeting.id}") do
-    #       expect(page).to have_content(first_past_meeting.title)
-    #       expect(page).to have_content(first_meeting_agenda_item_of_first_past_meeting.notes)
-    #       expect(page).to have_content(second_meeting_agenda_item_of_first_past_meeting.notes)
-    #     end
+        within_test_selector("op-meeting-container-#{first_past_meeting.id}") do
+          expect(page).to have_content(first_past_meeting.title)
+          expect(page).to have_content(first_meeting_agenda_item_of_first_past_meeting.notes)
+          expect(page).to have_content(second_meeting_agenda_item_of_first_past_meeting.notes)
+        end
 
-    #     within_test_selector("op-meeting-container-#{second_past_meeting.id}") do
-    #       expect(page).to have_content(second_past_meeting.title)
-    #       expect(page).to have_content(meeting_agenda_item_of_second_past_meeting.notes)
-    #     end
-    #   end
-    # end
+        within_test_selector("op-meeting-container-#{second_past_meeting.id}") do
+          expect(page).to have_content(second_past_meeting.title)
+          expect(page).to have_content(meeting_agenda_item_of_second_past_meeting.notes)
+        end
+      end
+    end
 
     context 'when user is allowed to edit meetings' do
       it 'shows the add to meeting button' do
@@ -225,9 +225,9 @@ RSpec.describe 'Open the Meetings tab', :js do
     it_behaves_like 'a meetings tab'
   end
 
-  # describe 'work package split view' do
-  #   let(:work_package_page) { Pages::SplitWorkPackage.new(work_package) }
+  describe 'work package split view' do
+    let(:work_package_page) { Pages::SplitWorkPackage.new(work_package) }
 
-  #   it_behaves_like 'a meetings tab'
-  # end
+    it_behaves_like 'a meetings tab'
+  end
 end
