160223740599774e37dd28478314a7ed58b21abc
Aaron Contreras <aaronlcaq@gmail.com>
2023-09-26 08:48:33 -0500

    Create shared_work_package mailer header

--- app/mailers/sharing_mailer.rb
+++ app/mailers/sharing_mailer.rb
@@ -1,14 +1,16 @@
 # frozen_string_literal: true
 
 class SharingMailer < ApplicationMailer
+  helper :mail_notification
+
   def shared_work_package(_sharer, membership)
-    work_package = membership.entity
+    @work_package = membership.entity
     recipient = membership.principal
 
     with_locale_for(recipient) do
       mail to: recipient.mail,
            subject: I18n.t('mail.sharing.work_packages.subject',
-                           id: work_package.id)
+                           id: @work_package.id)
     end
   end
 end
