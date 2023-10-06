5d2cb41efed8c295442ac90d0cc474a7d855371d
Kabiru Mwenja <makabby@gmail.com>
2023-10-06 17:39:17 +0300

    chore[#50212]: Rename to StorageComponent

--- modules/storages/app/components/storages/admin/storage_component.rb
+++ modules/storages/app/components/storages/admin/storage_component.rb
@@ -29,7 +29,7 @@
 #++
 #
 module Storages::Admin
-  class StorageViewComponent < ApplicationComponent
+  class StorageComponent < ApplicationComponent
     include OpPrimer::ComponentHelpers
     alias_method :storage, :model
 
