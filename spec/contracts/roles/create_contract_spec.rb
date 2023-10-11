fa01eb0e2aba119944798bf8172998801f63e9e0
ulferts <jens.ulferts@googlemail.com>
2023-10-11 14:16:36 +0200

    comment on surprising error duplication

--- spec/contracts/roles/create_contract_spec.rb
+++ spec/contracts/roles/create_contract_spec.rb
@@ -69,6 +69,10 @@ RSpec.describe Roles::CreateContract do
         end
 
         it 'is invalid' do
+          # The inclusion is in here twice because:
+          # * The contract validates the type (to check that only GlobalRole and ProjectRole are created)
+          # * The model validates the type (to check that only Role subclasses are created)
+          # This should not cause an issue for users so we disregard this imperfect duplication.
           expect_valid(false, type: %i(inclusion inclusion))
         end
       end
