2e1ebb827f68817a4acbda662e2be2ddd9622273
Christophe Bliard <c.bliard@openproject.com>
2023-10-13 11:19:49 +0200

    Allow preparing WP attachment with only :add_work_package_attachments permission

--- spec/requests/api/v3/attachments/attachment_resource_shared_examples.rb
+++ spec/requests/api/v3/attachments/attachment_resource_shared_examples.rb
@@ -210,11 +210,12 @@ RSpec.shared_examples 'an APIv3 attachment resource', content_type: :json, type:
   let(:attachment_type) { raise "attachment type goes here, e.g. work_package" }
   let(:permissions) { all_permissions }
 
-  let(:all_permissions) { [create_permission, read_permission, update_permission].flatten.compact }
+  let(:all_permissions) { [create_permission, read_permission, update_permission, delete_permission].flatten.uniq.compact }
 
   let(:create_permission) { raise "permissions go here, e.g. add_work_packages" }
   let(:read_permission) { raise "permissions go here, e.g. view_work_packages" }
   let(:update_permission) { raise "permissions go here, e.g. edit_work_packages" }
+  let(:delete_permission) { update_permission }
 
   let(:missing_permissions_user) { user_with_permissions }
 
@@ -386,6 +387,8 @@ RSpec.shared_examples 'an APIv3 attachment resource', content_type: :json, type:
     end
 
     context 'with required permissions' do
+      let(:permissions) { [read_permission, delete_permission].flatten.uniq.compact }
+
       it_behaves_like 'deletes the attachment'
 
       context 'for a non-existent attachment' do
@@ -396,7 +399,7 @@ RSpec.shared_examples 'an APIv3 attachment resource', content_type: :json, type:
     end
 
     context 'without required permissions' do
-      let(:permissions) { all_permissions - Array(update_permission) }
+      let(:permissions) { all_permissions.without(delete_permission) }
 
       it_behaves_like 'does not delete the attachment'
     end
@@ -617,7 +620,7 @@ RSpec.shared_examples 'an APIv3 attachment resource', content_type: :json, type:
       end
 
       context 'only allowed to add, but not to edit' do
-        let(:permissions) { all_permissions - Array(update_permission) }
+        let(:permissions) { [create_permission, read_permission].flatten.uniq.compact.without(update_permission) }
 
         it_behaves_like 'unauthorized access'
       end
