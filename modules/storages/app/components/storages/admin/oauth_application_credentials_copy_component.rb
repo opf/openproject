b138b2104e4634345376bbb5467f2844666a377a
Kabiru Mwenja <makabby@gmail.com>
2023-10-16 16:47:28 +0300

    chore[#50212]: Add flash warning for credentials copy

--- modules/storages/app/components/storages/admin/oauth_application_credentials_copy_component.rb
+++ modules/storages/app/components/storages/admin/oauth_application_credentials_copy_component.rb
@@ -39,5 +39,15 @@ module Storages::Admin
       super(oauth_application, **options)
       @storage = storage
     end
+
+    def oauth_application_details_link
+      render(
+        Primer::Beta::Link.new(
+          href: Storages::Peripherals::StorageInteraction::Nextcloud::Util.join_uri_path(storage.host,
+                                                                                         'settings/admin/openproject'),
+          target: '_blank'
+        )
+      ) { I18n.t('storages.instructions.oauth_application_details_link_text') }
+    end
   end
 end
