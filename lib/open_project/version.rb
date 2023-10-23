26f47637148935a747e298025b5d49b75d6c56ba
Oliver Günther <mail@oliverguenther.de>
2023-10-23 09:50:23 +0200

    Bumped version to 13.0.8
    [ci skip]

--- lib/open_project/version.rb
+++ lib/open_project/version.rb
@@ -33,7 +33,7 @@ module OpenProject
   module VERSION # :nodoc:
     MAJOR = 13
     MINOR = 0
-    PATCH = 7
+    PATCH = 8
 
     class << self
       # Used by semver to define the special version (if any).
