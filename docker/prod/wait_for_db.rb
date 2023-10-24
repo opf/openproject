a112d60f8327b1395df67bd3759eaada8167c1d8
Oliver Günther <mail@oliverguenther.de>
2023-10-24 13:20:17 +0200

    Ensure the seeder script always waits for DB in case of internal DB access

--- docker/prod/wait_for_db.rb
+++ docker/prod/wait_for_db.rb
@@ -1,14 +1,21 @@
 require 'timeout'
 
 timeout = (ENV['WAIT_FOR_DB_TIMEOUT_SECONDS'] || 120).to_i
+wait_for_migrations = ENV.fetch('WAIT_FOR_MIGRATIONS', 'true').downcase == 'true'
 
 Timeout::timeout(timeout) do
   loop do
-    puts "[#{DateTime.now}] waiting for db to be ready and migrated"
+    puts "[#{DateTime.now}] waiting for db to be ready #{wait_for_migrations ? 'and migrated' : ''}"
     sleep 4
 
     begin
-      ActiveRecord::Migration.check_pending!
+      if wait_for_migrations
+        ActiveRecord::Migration.check_pending!
+      else
+        ActiveRecord::Base.establish_connection
+        ActiveRecord::Base.connection # Calls connection object
+        raise "Connection to DB failed" unless ActiveRecord::Base.connected?
+      end
 
       puts "[#{DateTime.now}] db ready"
       exit 0
