051b4f6a56896a56f41e9ee4b75d48c59dc27014
Dombi Attila <83396+dombesz@users.noreply.github.com>
2023-11-01 15:21:41 +0200

    Update Users::UpdateContract#at_least_one_admin_is_active to catch locked admins only

--- app/contracts/users/update_contract.rb
+++ app/contracts/users/update_contract.rb
@@ -51,7 +51,7 @@ module Users
     def at_least_one_admin_is_active
       return unless
         (model.admin_changed? && !model.admin?) ||
-        (model.status_changed? && model.locked?)
+        (model.admin? && model.status_changed? && model.locked?)
 
       if User.active.admin.where.not(id: model).none?
         errors.add :base, :one_must_be_active
