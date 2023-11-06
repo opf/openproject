ea639619b99da877211279dfd8cbff119ff05d85
Klaus Zanders <k.zanders@openproject.com>
2023-11-06 15:39:29 +0100

    Add a skip_permission_checks option

--- lib_static/plugins/acts_as_attachable/lib/acts_as_attachable.rb
+++ lib_static/plugins/acts_as_attachable/lib/acts_as_attachable.rb
@@ -68,7 +68,8 @@ module Redmine
             allow_uncontainered: allow_uncontainered(options),
             viewable_by_all_users: viewable_by_all_users(options),
             modification_blocked: options[:modification_blocked],
-            extract_tsv: attachable_extract_tsv_option(options)
+            extract_tsv: attachable_extract_tsv_option(options),
+            skip_permission_checks: skip_permission_checks_option(options)
           }
 
           # Because subclasses can have their own attachable_options,
@@ -84,7 +85,8 @@ module Redmine
                           :allow_uncontainered,
                           :viewable_by_all_users,
                           :modification_blocked,
-                          :extract_tsv)
+                          :extract_tsv,
+                          :skip_permission_checks)
         end
 
         def view_permission(options)
@@ -113,6 +115,10 @@ module Redmine
 
         def allow_uncontainered(options)
           options.fetch(:allow_uncontainered, true)
+        end
+
+        def skip_permission_checks_option(options)
+          options.fetch(:skip_permission_checks, false)
         end
 
         def view_permission_default
@@ -199,6 +205,8 @@ module Redmine
           private
 
           def allowed_to_on_attachment?(user, permissions)
+            return true if self.class.attachable_options[:skip_permission_checks]
+
             permission_project_context = (project if respond_to?(:project))
 
             Array(permissions).any? do |permission|
