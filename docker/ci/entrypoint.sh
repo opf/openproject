6dea256cbee997421a9d81603d9b6970df730d77
Cyril Rohr <hey@cyrilrohr.com>
2023-11-06 15:30:36 +0000

    Fix postgres config

--- docker/ci/entrypoint.sh
+++ docker/ci/entrypoint.sh
@@ -108,7 +108,7 @@ run_all() {
 	cleanup
 }
 
-export -f cleanup execute execute_quiet run_psql reset_dbs setup_tests precompile_assets run_units run_features
+export -f cleanup execute execute_quiet run_psql reset_dbs setup_tests precompile_assets run_units run_features run_all
 
 if [ "$1" == "setup-tests" ]; then
 	shift
