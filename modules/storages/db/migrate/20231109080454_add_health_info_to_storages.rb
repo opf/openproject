d652e2ab4989df1cc18aac39697b4de7227dc7d2
Marcello Rocha <m.rocha@openproject.com>
2023-11-10 11:55:15 +0100

    Move the migration to storages module

--- modules/storages/db/migrate/20231109080454_add_health_info_to_storages.rb
+++ modules/storages/db/migrate/20231109080454_add_health_info_to_storages.rb
@@ -1,5 +1,5 @@
 class AddHealthInfoToStorages < ActiveRecord::Migration[7.0]
   def change
-    add_column :storages, :health_info, :jsonb, default: {}
+    add_column :storages, :health_info, :jsonb, null: false, default: {}
   end
 end
