92e1cf89442790a3c4e10a3448c595e0b4b9862d
Henriette Darge <h.darge@openproject.com>
2023-11-15 14:33:53 +0100

    Show a warning if inviting new users would exceed the user limit

---
+++ frontend/src/stimulus/controllers/dynamic/work-packages/share/user-limit.controller.ts
@@ -0,0 +1,69 @@
+/*
+ * -- copyright
+ * OpenProject is an open source project management software.
+ * Copyright (C) 2023 the OpenProject GmbH
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License version 3.
+ *
+ * OpenProject is a fork of ChiliProject, which is a fork of Redmine. The copyright follows:
+ * Copyright (C) 2006-2013 Jean-Philippe Lang
+ * Copyright (C) 2010-2013 the ChiliProject Team
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ *
+ * See COPYRIGHT and LICENSE files for more details.
+ * ++
+ */
+
+import { Controller } from '@hotwired/stimulus';
+import {
+  IUserAutocompleteItem,
+} from 'core-app/shared/components/autocompleter/user-autocompleter/user-autocompleter.component';
+
+export default class UserLimitController extends Controller {
+  static targets = [
+    'limitWarning',
+    'inviteUserForm',
+  ];
+
+  declare readonly limitWarningTarget:HTMLElement;
+  declare readonly hasLimitWarningTarget:HTMLElement;
+  declare readonly inviteUserFormTarget:HTMLElement;
+
+  private autocompleter:HTMLElement;
+  private autocompleterListener = this.triggerLimitWarningIfReached.bind(this);
+
+  connect() {
+    this.autocompleter = this.inviteUserFormTarget.querySelector('opce-user-autocompleter') as HTMLElement;
+    this.autocompleter.addEventListener('change', this.autocompleterListener);
+  }
+
+  disconnect() {
+    this.autocompleter.removeEventListener('change', this.autocompleterListener);
+  }
+
+  triggerLimitWarningIfReached(evt:CustomEvent) {
+    const values = evt.detail as IUserAutocompleteItem[];
+
+    if (this.hasLimitWarningTarget) {
+      if (values.find(({ id }) => typeof (id) === 'string' && id.includes('@'))) {
+        this.limitWarningTarget.classList.remove('d-none');
+      } else {
+        this.limitWarningTarget.classList.add('d-none');
+      }
+    }
+  }
+}
