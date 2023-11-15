451c635ea6dd59473c7b87ec4b66ccf578f40d10
Oliver Günther <mail@oliverguenther.de>
2023-11-15 10:10:58 +0100

    Reload the meeting tab counter on frame or stream changes

--- modules/meeting/frontend/module/main.ts
+++ modules/meeting/frontend/module/main.ts
@@ -34,10 +34,10 @@ import { MeetingsTabComponent } from './meetings-tab/meetings-tab.component';
 import { WorkPackageResource } from 'core-app/features/hal/resources/work-package-resource';
 import { PathHelperService } from 'core-app/core/path-helper/path-helper.service';
 import { HttpClient } from '@angular/common/http';
-import { map } from 'rxjs/operators';
-import { Observable } from 'rxjs';
+import { filter, map, startWith, switchMap, throttleTime } from 'rxjs/operators';
+import { fromEvent, merge, Observable } from 'rxjs';
 import { I18nService } from 'core-app/core/i18n/i18n.service';
-
+import { TurboStreamElement } from 'core-typings/turbo';
 
 export function workPackageMeetingsCount(
   workPackage:WorkPackageResource,
@@ -45,10 +45,33 @@ export function workPackageMeetingsCount(
 ):Observable<number> {
   const pathHelperService = injector.get(PathHelperService);
   const http = injector.get(HttpClient);
-  return http
-    .get(`${pathHelperService.workPackagePath(workPackage.id as string)}/meetings/tab/count`)
+
+  return merge(
+    fromEvent(document, 'turbo:frame-render'),
+    fromEvent(document, 'turbo:before-stream-render'),
+  )
     .pipe(
-      map((res:{ count:number }) => res.count),
+      filter((event:CustomEvent) => {
+        if (event.type === 'turbo:frame-render') {
+          return (event.target as HTMLElement).id?.includes('work-package-meetings-tab');
+        }
+
+        if (event.type === 'turbo:before-stream-render') {
+          const stream:TurboStreamElement = (event.detail as { newStream:TurboStreamElement }).newStream;
+          return stream.target?.includes('work-package-meetings-tab');
+        }
+
+        return false;
+      }),
+      startWith(null),
+      throttleTime(1000),
+      switchMap(() => {
+        return http
+          .get(`${pathHelperService.workPackagePath(workPackage.id as string)}/meetings/tab/count`)
+          .pipe(
+            map((res:{ count:number }) => res.count),
+          );
+      }),
     );
 }
 
