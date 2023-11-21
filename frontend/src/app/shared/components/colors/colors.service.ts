431599cf71d21a50dee6652063e5cd5e3ae07c1f
bsatarnejad <b.satarnejad@gmail.com>
2023-11-21 09:36:51 +0100

    change the font-color of avatar based on its background color

--- frontend/src/app/shared/components/colors/colors.service.ts
+++ frontend/src/app/shared/components/colors/colors.service.ts
@@ -18,4 +18,30 @@ export class ColorsService {
 
     return hash % 360;
   }
+
+  public hexCodeColor(value:string) {
+    const h = this.valueHash(value);
+    const f = (n:number) => {
+      const k = (n + h / 30) % 12;
+      const color = 0.5 - 0.25 * Math.max(Math.min(k - 3, 9 - k, 1), -1);
+      return Math.round(255 * color).toString(16).padStart(2, '0');// convert to Hex and prefix "0" if needed
+    };
+    return `#${f(0)}${f(8)}${f(4)}`;
+  }
+
+  public pickTextColorBasedOnBgColor(bgColor:string) {
+    const color = (bgColor.charAt(0) === '#') ? bgColor.substring(1, 7) : bgColor;
+    const r = parseInt(color.substring(0, 2), 16); // hexToR
+    const g = parseInt(color.substring(2, 4), 16); // hexToG
+    const b = parseInt(color.substring(4, 6), 16); // hexToB
+    const uicolors = [r / 255, g / 255, b / 255];
+    const c = uicolors.map((col) => {
+      if (col <= 0.03928) {
+        return col / 12.92;
+      }
+      return ((col + 0.055) / 1.055) ** 2.4;
+    });
+    const L = (0.2126 * c[0]) + (0.7152 * c[1]) + (0.0722 * c[2]);
+    return (L > 0.179) ? '#000000' : '#FFFFFF';
+  }
 }
