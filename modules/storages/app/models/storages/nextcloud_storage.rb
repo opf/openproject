5cf7901b7844a87fcce2cdbf4a03bc06655a6786
Pavel Balashou <ba1ashpash@gmail.com>
2023-11-27 15:18:28 +0100

    [#51265] Make Nextcloud synchronization more stable.
    https://community.openproject.org/work_packages/51265

--- modules/storages/app/models/storages/nextcloud_storage.rb
+++ modules/storages/app/models/storages/nextcloud_storage.rb
@@ -45,9 +45,14 @@ class Storages::NextcloudStorage < Storages::Storage
                                           timeout_seconds: 0,
                                           transaction: false) do
       where("provider_fields->>'automatically_managed' = 'true'")
+        .order(:created_at)
         .includes(:oauth_client)
         .each do |storage|
         Storages::GroupFolderPropertiesSyncService.new(storage).call
+      rescue StandardError => e
+        OpenProject.logger.error(
+          "Unexpected error during NextcloudStorage ##{storage.id} #{storage.host}: #{e.message}"
+        )
       end
       true
     end
