6b7876b2454a0e9dbff83b77c357fe01b4302bb9
Klaus Zanders <k.zanders@openproject.com>
2023-11-28 10:11:40 +0100

    Honor exisiting builtin roles when seeding

--- app/seeders/basic_data/base_role_seeder.rb
+++ app/seeders/basic_data/base_role_seeder.rb
@@ -28,6 +28,7 @@
 module BasicData
   class BaseRoleSeeder < ModelSeeder
     self.needs = []
+    self.attribute_names_for_lookups = %i[builtin name]
 
     def model_attributes(role_data)
       {
