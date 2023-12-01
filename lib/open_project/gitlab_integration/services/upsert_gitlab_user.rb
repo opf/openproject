419f94b28584d6657658a4704b4293636f14e365
Aleksey Kurepin <lesha@kurepin.com>
2023-12-01 12:51:44 +0300

    fix 500 errors with empty MR and Issue description body, fix empty avatar_url

--- lib/open_project/gitlab_integration/services/upsert_gitlab_user.rb
+++ lib/open_project/gitlab_integration/services/upsert_gitlab_user.rb
@@ -1,4 +1,4 @@
-#-- encoding: UTF-8
+# frozen_string_literal: true
 
 #-- copyright
 # OpenProject is an open source project management software.
@@ -27,34 +27,44 @@
 #
 # See docs/COPYRIGHT.rdoc for more details.
 #++
-module OpenProject::GitlabIntegration::Services
-  ##
-  # Takes user data coming from Gitlab webhook data and stores
-  # them as a `GitlabUser`.
-  # If the `GitlabUser` already exists, it is updated.
-  #
-  # Returns the upserted `GitlabUser`.
-  class UpsertGitlabUser
-    def call(payload)
-      GitlabUser.find_or_initialize_by(gitlab_id: payload.id)
-                .tap do |gitlab_user|
-                  gitlab_user.update!(extract_params(payload))
-                end
-    end
+module OpenProject
+  module GitlabIntegration
+    module Services
+      ##
+      # Takes user data coming from Gitlab webhook data and stores
+      # them as a `GitlabUser`.
+      # If the `GitlabUser` already exists, it is updated.
+      #
+      # Returns the upserted `GitlabUser`.
+      class UpsertGitlabUser
+        def call(payload)
+          GitlabUser.find_or_initialize_by(gitlab_id: payload.id)
+                    .tap do |gitlab_user|
+                      gitlab_user.update!(extract_params(payload))
+                    end
+        end
 
-    private
+        private
 
-    ##
-    # Receives the input from the gitlab webhook and translates them
-    # to our internal representation.
-    def extract_params(payload)
-      {
-        gitlab_id: payload.id,
-        gitlab_name: payload.name, 
-        gitlab_username: payload.username, 
-        gitlab_email: payload.email, 
-        gitlab_avatar_url: payload.avatar_url
-      }
+        ##
+        # Receives the input from the gitlab webhook and translates them
+        # to our internal representation.
+        def extract_params(payload)
+          {
+            gitlab_id: payload.id,
+            gitlab_name: payload.name,
+            gitlab_username: payload.username,
+            gitlab_email: payload.email,
+            gitlab_avatar_url: avatar_url(payload)
+          }
+        end
+
+        def avatar_url(payload)
+          # rubocop:disable Layout/LineLength
+          payload.avatar_url.presence || "data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 24 24' width='24' height='24'%3E%3Cpath d='M12 2.5a5.5 5.5 0 0 1 3.096 10.047 9.005 9.005 0 0 1 5.9 8.181.75.75 0 1 1-1.499.044 7.5 7.5 0 0 0-14.993 0 .75.75 0 0 1-1.5-.045 9.005 9.005 0 0 1 5.9-8.18A5.5 5.5 0 0 1 12 2.5ZM8 8a4 4 0 1 0 8 0 4 4 0 0 0-8 0Z'%3E%3C/path%3E%3C/svg%3E"
+          # rubocop:enable Layout/LineLength
+        end
+      end
     end
   end
 end
