d9a98ee025737b544064cc5b164083074982dd3d
Eric Schubert <e.schubert@openproject.com>
2023-12-05 15:52:02 +0100

    [#51305] added description for api spec errors

--- modules/storages/app/contracts/storages/storages/base_contract.rb
+++ modules/storages/app/contracts/storages/storages/base_contract.rb
@@ -70,7 +70,7 @@ module Storages::Storages
 
     def require_ee_token_for_one_drive
       if ::Storages::Storage.one_drive_without_ee_token?(provider_type)
-        errors.add(:base,  I18n.t('api_v3.errors.code_500_missing_enterprise_token'))
+        errors.add(:base, I18n.t('api_v3.errors.code_500_missing_enterprise_token'))
       end
     end
   end
