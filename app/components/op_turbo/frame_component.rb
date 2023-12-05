d03fadb9a4da70e1672009500fa0803d293b39a9
Mir Bhatia <m.bhatia@openproject.com>
2023-12-05 18:04:37 +0100

    WIP

--- app/components/op_turbo/frame_component.rb
+++ app/components/op_turbo/frame_component.rb
@@ -31,5 +31,9 @@ module OpTurbo
     def turbo_frame_id
       ActionView::RecordIdentifier.dom_id(model, options[:context])
     end
+
+    def lazy_loaded_source
+      history_meeting_path(options[:id])
+    end
   end
 end
