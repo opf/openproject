4866e18c8931f68a65ed384e17700e1949057de5
Kabiru Mwenja <makabby@gmail.com>
2023-12-12 13:09:07 +0300

    chore[Op#51033]: rebase against new redirect URI flow

--- modules/storages/app/components/storages/admin/forms/oauth_client_form_component.rb
+++ modules/storages/app/components/storages/admin/forms/oauth_client_form_component.rb
@@ -48,21 +48,6 @@ module Storages::Admin::Forms
       storage.persisted? ? edit_admin_settings_storage_path(storage) : admin_settings_storages_path
     end
 
-    def submit_button_data_options
-      {}.tap do |data|
-        # For One Drive create action, break from Turbo Frame and follow full page redirect
-        data[:turbo] = false if one_drive_first_time_configuration?
-      end
-    end
-
-    def redirect_uri_or_instructions
-      if oauth_client_configured?
-        oauth_client.redirect_uri
-      else
-        I18n.t("storages.instructions.one_drive.missing_client_id_for_redirect_uri")
-      end
-    end
-
     def storage_provider_credentials_instructions
       I18n.t("storages.instructions.#{storage.short_provider_type}.oauth_configuration",
              application_link_text: send(:"#{storage.short_provider_type}_integration_link")).html_safe
@@ -87,10 +72,6 @@ module Storages::Admin::Forms
 
     def default_form_method
       first_time_configuration? ? :post : :patch
-    end
-
-    def oauth_client_configured?
-      oauth_client.present? && oauth_client.client_id.present? && oauth_client.client_secret.present?
     end
   end
 end
