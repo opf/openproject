aec8707d29048747f2b1f561e160f1142b0878bc
Dombi Attila <83396+dombesz@users.noreply.github.com>
2023-12-16 12:49:27 +0200

    Update the rest of the config files from bin/rails app:update.

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -26,41 +26,51 @@
 # See COPYRIGHT and LICENSE files for more details.
 #++
 
+require 'active_support/core_ext/integer/time'
+
+# The test environment is used exclusively to run your application's
+# test suite. You never need to work with it otherwise. Remember that
+# your test database is "scratch space" for the test suite and is wiped
+# and recreated between test runs. Don't rely on the data there!
+
 OpenProject::Application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
 
   # Access to rack session
   config.middleware.use RackSessionAccess::Middleware
 
-  # The test environment is used exclusively to run your application's
-  # test suite. You never need to work with it otherwise. Remember that
-  # your test database is "scratch space" for the test suite and is wiped
-  # and recreated between test runs. Don't rely on the data there!
-  #
   # Spring requires to have the classes reloaded. On the CI or when Spring is
   # disabled, it does not need to happen.
-  config.cache_classes = ENV['CI'].present? || ENV['DISABLE_SPRING'].present?
+  config.enable_reloading = !(ENV['CI'].present? && ENV['DISABLE_SPRING'].present?)
 
-  # Use eager load to mirror the production environment
-  # on travis
+  # Eager loading loads your entire application. When running a single test locally,
+  # this is usually not necessary, and can slow down your test suite. However, it's
+  # recommended that you enable it in continuous integration systems to ensure eager
+  # loading is working properly before deploying your code.
   config.eager_load = ENV['CI'].present? || ENV['EAGER_LOAD'].present?
 
   # This setting is false by default, but we define it explicitly
   config.allow_concurrency = false
 
-  # Configure static asset server for tests with Cache-Control for performance.
+  # Configure public file server for tests with Cache-Control for performance.
   config.public_file_server.enabled = true
   config.public_file_server.headers = { 'Cache-Control' => 'public, max-age=3600' }
 
   # Show full error reports and disable caching.
   config.consider_all_requests_local       = true
-  config.action_controller.perform_caching = true
+  config.action_controller.perform_caching = false
+  config.cache_store = :null_store
 
-  # Raise exceptions instead of rendering exception templates.
-  config.action_dispatch.show_exceptions = false
+  # Render exception templates for rescuable exceptions and raise for other exceptions.
+  config.action_dispatch.show_exceptions = :rescuable
 
   # Enable request forgery protection in test environment.
   config.action_controller.allow_forgery_protection = true
+
+  # Store uploaded files on the local file system in a temporary directory.
+  config.active_storage.service = :test
+
+  config.action_mailer.perform_caching = false
 
   # Tell Action Mailer not to deliver emails to the real world.
   # The :test delivery method accumulates sent emails in the
@@ -78,8 +88,14 @@ OpenProject::Application.configure do
       :stderr
     end
 
+  # Raise exceptions for disallowed deprecations.
+  config.active_support.disallowed_deprecation = :raise
+
   # Highlight code that triggered database queries in logs.
   config.active_record.verbose_query_logs = true
+
+  # Tell Active Support which deprecation messages to disallow.
+  config.active_support.disallowed_deprecation_warnings = []
 
   # Disable asset digests
   config.assets.compile = true
@@ -87,8 +103,14 @@ OpenProject::Application.configure do
   config.assets.digest = false
   config.assets.debug = false
 
-  # Raises error for missing translations
+  # Raises error for missing translations.
   config.i18n.raise_on_missing_translations = true
+
+  # Annotate rendered view with file names.
+  # config.action_view.annotate_rendered_view_with_filenames = true
+
+  # Raise error when a before_action's only/except options reference missing actions
+  config.action_controller.raise_on_missing_callback_actions = true
 
   config.cache_store = :file_store, Rails.root.join("tmp", "cache", "paralleltests#{ENV.fetch('TEST_ENV_NUMBER', nil)}")
 
--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -1,4 +1,15 @@
+# Be sure to restart your server when you modify this file.
+
 OpenProject::Application.configure do
+  # Version of your assets, change this if you want to expire all your assets.
+  # config.assets.version = "1.0"
+
+  # Add additional assets to the asset load path.
+  # config.assets.paths << Emoji.images_path
+
+  # Precompile additional assets.
+  # application.js, application.css, and all non-JS/CSS in the app/assets
+  # folder are already added.
   config.assets.precompile += %w(
     favicon.ico
     locales/*.js
