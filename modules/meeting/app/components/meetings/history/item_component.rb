23804623376c4f3221d7815eaa3a1056f933eed7
Mir Bhatia <m.bhatia@openproject.com>
2023-12-21 11:01:20 +0100

    Duplicate project activity components

---
+++ modules/meeting/app/components/meetings/history/item_component.rb
@@ -0,0 +1,115 @@
+# frozen_string_literal: true
+
+#-- copyright
+# OpenProject is an open source project management software.
+# Copyright (C) 2012-2022 the OpenProject GmbH
+#
+# This program is free software; you can redistribute it and/or
+# modify it under the terms of the GNU General Public License version 3.
+#
+# OpenProject is a fork of ChiliProject, which is a fork of Redmine. The copyright follows:
+# Copyright (C) 2006-2013 Jean-Philippe Lang
+# Copyright (C) 2010-2013 the ChiliProject Team
+#
+# This program is free software; you can redistribute it and/or
+# modify it under the terms of the GNU General Public License
+# as published by the Free Software Foundation; either version 2
+# of the License, or (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+#
+# See COPYRIGHT and LICENSE files for more details.
+#++
+
+module Meetings
+  class History::ItemComponent < Primer::Component
+    with_collection_parameter :event
+    strip_trailing_whitespace
+
+    def initialize(event:, current_project: nil, display_user: true, activity_page: nil)
+      super()
+      @event = event
+      @current_project = current_project
+      @display_user = display_user
+      @activity_page = activity_page
+    end
+
+    def project_suffix
+      return if activity?(Project)
+      return if activity_is_from_current_project?
+
+      kind = activity_is_from_subproject? ? 'subproject' : 'project'
+      suffix = I18n.t("events.title.#{kind}", name: link_to(@event.project.name, @event.project))
+      "(#{suffix})".html_safe # rubocop:disable Rails/OutputSafety
+    end
+
+    def display_user?
+      @display_user
+    end
+
+    def display_details?
+      return false if @event.journal.initial? && @event.journal.journable_type != 'TimeEntry'
+
+      rendered_details.present?
+    end
+
+    def rendered_details
+      filter_details.filter_map { |detail| @event.journal.render_detail(detail, activity_page: @activity_page) }
+    end
+
+    def comment
+      return unless activity?(WorkPackage)
+
+      @event.event_description
+    end
+
+    def description
+      return if activity?(WorkPackage) || activity?(TimeEntry)
+
+      @event.event_description
+    end
+
+    def time_entry_url
+      return unless activity?(TimeEntry)
+
+      @event.event_url
+    end
+
+    private
+
+    def activity?(type)
+      @event.journal.journable_type == type.to_s
+    end
+
+    def activity_is_from_current_project?
+      @current_project && (@event.project == @current_project)
+    end
+
+    def activity_is_from_subproject?
+      @current_project && (@event.project != @current_project)
+    end
+
+    def filter_details
+      details = @event.journal.details
+
+      details.delete(:user_id) if details[:logged_by_id] == details[:user_id]
+      delete_detail(details, :work_package_id)
+      delete_detail(details, :comments)
+      delete_detail(details, :activity_id)
+      delete_detail(details, :spent_on)
+
+      details
+    end
+
+    def delete_detail(details, field)
+      details.delete(field) if details[field] && details[field].first.nil?
+    end
+  end
+end
---
+++ modules/meeting/app/components/meetings/history/item_subtitle_component.rb
@@ -0,0 +1,67 @@
+# frozen_string_literal: true
+
+#-- copyright
+# OpenProject is an open source project management software.
+# Copyright (C) 2012-2022 the OpenProject GmbH
+#
+# This program is free software; you can redistribute it and/or
+# modify it under the terms of the GNU General Public License version 3.
+#
+# OpenProject is a fork of ChiliProject, which is a fork of Redmine. The copyright follows:
+# Copyright (C) 2006-2013 Jean-Philippe Lang
+# Copyright (C) 2010-2013 the ChiliProject Team
+#
+# This program is free software; you can redistribute it and/or
+# modify it under the terms of the GNU General Public License
+# as published by the Free Software Foundation; either version 2
+# of the License, or (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+#
+# See COPYRIGHT and LICENSE files for more details.
+#++
+
+module Meetings
+  class History::ItemSubtitleComponent < Primer::Component
+    def initialize(user:, datetime:, is_creation:, journable_type:)
+      super()
+      @user = user
+      @datetime = datetime
+      @is_creation = is_creation
+      @journable_type = journable_type
+    end
+
+    def user_html
+      return unless @user
+
+      [
+        helpers.avatar(@user, size: 'mini'),
+        helpers.content_tag('span', helpers.link_to_user(@user), class: %w[spot-caption spot-caption_bold])
+      ].join(' ')
+    end
+
+    def datetime_html
+      helpers.format_time(@datetime)
+    end
+
+    def time_entry?
+      @journable_type == 'TimeEntry'
+    end
+
+    def i18n_key
+      i18n_key = 'activity.item.'.dup
+      i18n_key << (@is_creation ? 'created_' : 'updated_')
+      i18n_key << 'by_' if @user
+      i18n_key << 'on'
+      i18n_key << '_time_entry' if time_entry?
+      i18n_key
+    end
+  end
+end
