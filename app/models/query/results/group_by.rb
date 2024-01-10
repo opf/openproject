87abc1ed0cf38cf9983e5c173fb481fd6719cd8a
Dombi Attila <83396+dombesz@users.noreply.github.com>
2024-01-10 20:41:39 +0200

    Adding a string to an sql literal implicitly is no longer possible, calling to_s

--- app/models/query/results/group_by.rb
+++ app/models/query/results/group_by.rb
@@ -163,7 +163,7 @@ module ::Query::Results::GroupBy
     column = case_insensitive_condition(sortable, column, columns_hash)
 
     if order
-      column + " #{order} "
+      column.to_s + " #{order} "
     else
       column
     end
