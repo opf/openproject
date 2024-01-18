2c66396c97d76cce39e4da617fbeb5ac3521b982
Aaron Contreras <aaronlcaq@gmail.com>
2024-01-18 15:28:06 -0500

    Chore: Alphabetize imports in `display-field.initializer.ts`

--- frontend/src/app/shared/components/fields/display/display-field.initializer.ts
+++ frontend/src/app/shared/components/fields/display/display-field.initializer.ts
@@ -26,31 +26,31 @@
 // See COPYRIGHT and LICENSE files for more details.
 //++
 
-import { DisplayFieldService } from 'core-app/shared/components/fields/display/display-field.service';
-import { TextDisplayField } from 'core-app/shared/components/fields/display/field-types/text-display-field.module';
-import { FloatDisplayField } from 'core-app/shared/components/fields/display/field-types/float-display-field.module';
-import { IntegerDisplayField } from 'core-app/shared/components/fields/display/field-types/integer-display-field.module';
-import { ResourceDisplayField } from 'core-app/shared/components/fields/display/field-types/resource-display-field.module';
-import { ResourcesDisplayField } from 'core-app/shared/components/fields/display/field-types/resources-display-field.module';
-import { FormattableDisplayField } from 'core-app/shared/components/fields/display/field-types/formattable-display-field.module';
+import { BooleanDisplayField } from 'core-app/shared/components/fields/display/field-types/boolean-display-field.module';
+import { CombinedDateDisplayField } from 'core-app/shared/components/fields/display/field-types/combined-date-display.field';
 import { DateDisplayField } from 'core-app/shared/components/fields/display/field-types/date-display-field.module';
 import { DateTimeDisplayField } from 'core-app/shared/components/fields/display/field-types/datetime-display-field.module';
-import { BooleanDisplayField } from 'core-app/shared/components/fields/display/field-types/boolean-display-field.module';
-import { ProgressDisplayField } from 'core-app/shared/components/fields/display/field-types/progress-display-field.module';
-import { WorkPackageDisplayField } from 'core-app/shared/components/fields/display/field-types/work-package-display-field.module';
-import { WorkPackageSpentTimeDisplayField } from 'core-app/shared/components/fields/display/field-types/wp-spent-time-display-field.module';
-import { IdDisplayField } from 'core-app/shared/components/fields/display/field-types/id-display-field.module';
+import { DaysDurationDisplayField } from 'core-app/shared/components/fields/display/field-types/days-duration-display-field.module';
+import { DisplayFieldService } from 'core-app/shared/components/fields/display/display-field.service';
+import { EstimatedTimeDisplayField } from 'core-app/shared/components/fields/display/field-types/estimated-time-display-field.module';
+import { FloatDisplayField } from 'core-app/shared/components/fields/display/field-types/float-display-field.module';
+import { FormattableDisplayField } from 'core-app/shared/components/fields/display/field-types/formattable-display-field.module';
 import { HighlightedResourceDisplayField } from 'core-app/shared/components/fields/display/field-types/highlighted-resource-display-field.module';
+import { IdDisplayField } from 'core-app/shared/components/fields/display/field-types/id-display-field.module';
+import { IntegerDisplayField } from 'core-app/shared/components/fields/display/field-types/integer-display-field.module';
+import { LinkedWorkPackageDisplayField } from 'core-app/shared/components/fields/display/field-types/linked-work-package-display-field.module';
+import { MultipleUserFieldModule } from 'core-app/shared/components/fields/display/field-types/multiple-user-display-field.module';
+import { PlainFormattableDisplayField } from 'core-app/shared/components/fields/display/field-types/plain-formattable-display-field.module';
+import { ProgressDisplayField } from 'core-app/shared/components/fields/display/field-types/progress-display-field.module';
+import { ProjectStatusDisplayField } from 'core-app/shared/components/fields/display/field-types/project-status-display-field.module';
+import { ResourceDisplayField } from 'core-app/shared/components/fields/display/field-types/resource-display-field.module';
+import { ResourcesDisplayField } from 'core-app/shared/components/fields/display/field-types/resources-display-field.module';
+import { TextDisplayField } from 'core-app/shared/components/fields/display/field-types/text-display-field.module';
 import { TypeDisplayField } from 'core-app/shared/components/fields/display/field-types/type-display-field.module';
 import { UserDisplayField } from 'core-app/shared/components/fields/display/field-types/user-display-field.module';
-import { MultipleUserFieldModule } from 'core-app/shared/components/fields/display/field-types/multiple-user-display-field.module';
+import { WorkPackageDisplayField } from 'core-app/shared/components/fields/display/field-types/work-package-display-field.module';
 import { WorkPackageIdDisplayField } from 'core-app/shared/components/fields/display/field-types/wp-id-display-field.module';
-import { ProjectStatusDisplayField } from 'core-app/shared/components/fields/display/field-types/project-status-display-field.module';
-import { PlainFormattableDisplayField } from 'core-app/shared/components/fields/display/field-types/plain-formattable-display-field.module';
-import { LinkedWorkPackageDisplayField } from 'core-app/shared/components/fields/display/field-types/linked-work-package-display-field.module';
-import { CombinedDateDisplayField } from 'core-app/shared/components/fields/display/field-types/combined-date-display.field';
-import { EstimatedTimeDisplayField } from 'core-app/shared/components/fields/display/field-types/estimated-time-display-field.module';
-import { DaysDurationDisplayField } from 'core-app/shared/components/fields/display/field-types/days-duration-display-field.module';
+import { WorkPackageSpentTimeDisplayField } from 'core-app/shared/components/fields/display/field-types/wp-spent-time-display-field.module';
 
 export function initializeCoreDisplayFields(displayFieldService:DisplayFieldService) {
   return () => {
