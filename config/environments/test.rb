3322ed67e1b3354271fc4bf1d77c9df0fbea7f89
Dombi Attila <83396+dombesz@users.noreply.github.com>
2024-01-18 18:27:49 +0200

    Enable actioncontroller caching in test config.

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -58,7 +58,7 @@ Rails.application.configure do
 
   # Show full error reports and disable caching.
   config.consider_all_requests_local       = true
-  config.action_controller.perform_caching = false
+  config.action_controller.perform_caching = true
   config.cache_store = :null_store
 
   # Render exception templates for rescuable exceptions and raise for other exceptions.
