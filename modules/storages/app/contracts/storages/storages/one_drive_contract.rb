0d816a93a4b37da4a383c4a1f1409643a657b86b
Kabiru Mwenja <makabby@gmail.com>
2024-01-22 14:34:51 +0300

    fix[Op-51021]: reinstate format

--- modules/storages/app/contracts/storages/storages/one_drive_contract.rb
+++ modules/storages/app/contracts/storages/storages/one_drive_contract.rb
@@ -33,7 +33,7 @@ module Storages::Storages
     attribute :host
     validates :host, absence: true
     attribute :tenant_id
-    validates :tenant_id, presence: true
+    validates :tenant_id, format: { with: /\A(?:[a-f0-9]{8}-[a-f0-9]{4}-[a-f0-9]{4}-[a-f0-9]{4}-[a-f0-9]{12}|consumers)\z/i }
     attribute :drive_id
     validates :drive_id, presence: true, allow_nil: true
   end
