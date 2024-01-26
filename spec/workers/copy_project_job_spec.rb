662cb6558f3d316ff7ce40b132a52dab8201d135
Christophe Bliard <c.bliard@openproject.com>
2024-01-26 18:20:30 +0100

    [52384] Avoid updating ancestor twice
    Processing ancestor twice leads to "Stale record" errors.
    
    It happened in this scenario (see Bug #52384): when creating the child,
    its attributes trigger a parent rescheduling. Once done, the parent is
    part of the dependent results of the rescheduling, and as all results
    (normal + dependent) are then processed by UpdateAncestorsService, the
    parent gets processed twice: a first time when the child is processed,
    and a second time when the parent is processed. As the parent has been
    updated the first time, the second parent update will fail because this
    parent instance is stale.
    
    Solution is to store ids of already updated work packages, and skip the
    update for the ones having already been updated.

--- spec/workers/copy_project_job_spec.rb
+++ spec/workers/copy_project_job_spec.rb
@@ -188,6 +188,44 @@ RSpec.describe CopyProjectJob, type: :model do
     end
   end
 
+  context 'when project has work package hierarchies with derived values' do
+    shared_let(:source_project) { create(:project, name: 'Source project') }
+
+    before_all do
+      set_factory_default(:project, source_project)
+      set_factory_default(:project_with_types, source_project)
+    end
+
+    let(:admin) { create(:admin) }
+    let(:params) { { name: 'Copy', identifier: 'copy' } }
+
+    let_work_packages(<<~TABLE)
+      hierarchy   | work | start date | end date
+      parent      |   1h | 2024-01-23 | 2024-01-26
+        child     |   3h | 2024-01-23 | 2024-01-26
+    TABLE
+
+    before do
+      WorkPackages::UpdateAncestorsService
+        .new(user: admin, work_package: child)
+        .call(%i[estimated_hours remaining_hours ignore_non_working_days])
+    end
+
+    it 'copies the project without any errord (Bug #52384)' do
+      allow(OpenProject.logger).to receive(:error)
+
+      copy_job = described_class.new
+      copy_job.perform user_id: admin.id,
+                       source_project_id: source_project.id,
+                       target_project_params: params,
+                       associations_to_copy: [:work_packages]
+
+      expect(copy_job.job_status.status).to eq 'success'
+      expect(copy_job.errors).to be_empty
+      expect(OpenProject.logger).not_to have_received(:error)
+    end
+  end
+
   describe 'perform' do
     let(:project) { create(:project, public: false) }
     let(:user) { create(:user) }
