176b844f2dea670ce2259c93ae7489fb5cd68f16
Aaron Contreras <aaronlcaq@gmail.com>
2024-02-23 09:06:44 -0500

    Update query sums test expectations

--- spec/models/query/results_sums_integration_spec.rb
+++ spec/models/query/results_sums_integration_spec.rb
@@ -53,11 +53,10 @@ RSpec.describe Query::Results, 'sums' do
     create(:work_package,
            type:,
            project:,
-           estimated_hours: 5,
-           done_ratio: 10,
+           estimated_hours: 10,
            int_cf.attribute_name => 10,
            float_cf.attribute_name => 3.414,
-           remaining_hours: 3,
+           remaining_hours: 9,
            story_points: 7)
   end
   let!(:work_package2) do
@@ -65,11 +64,10 @@ RSpec.describe Query::Results, 'sums' do
            type:,
            project:,
            assigned_to: current_user,
-           done_ratio: 50,
            estimated_hours: 5,
            int_cf.attribute_name => 10,
            float_cf.attribute_name => 3.414,
-           remaining_hours: 3,
+           remaining_hours: 2.5,
            story_points: 7)
   end
   let!(:work_package3) do
@@ -78,11 +76,10 @@ RSpec.describe Query::Results, 'sums' do
            project:,
            assigned_to: current_user,
            responsible: current_user,
-           done_ratio: 50,
            estimated_hours: 5,
            int_cf.attribute_name => 10,
            float_cf.attribute_name => 3.414,
-           remaining_hours: 3,
+           remaining_hours: 2.5,
            story_points: 7)
   end
   let!(:invisible_work_package1) do
@@ -148,7 +145,7 @@ RSpec.describe Query::Results, 'sums' do
           group_by:)
   end
   let(:query_results) do
-    Query::Results.new query
+    described_class.new query
   end
 
   before do
@@ -158,13 +155,13 @@ RSpec.describe Query::Results, 'sums' do
   describe '#all_total_sums' do
     it 'is a hash of all summable columns' do
       expect(query_results.all_total_sums)
-        .to eql(estimated_hours_column => 15.0,
+        .to eql(estimated_hours_column => 20.0,
                 int_cf_column => 30,
                 float_cf_column => 10.24,
                 material_costs_column => 400.0,
                 labor_costs_column => 600.0,
                 overall_costs_column => 1000.0,
-                remaining_hours_column => 9.0,
+                remaining_hours_column => 14.0,
                 story_points_column => 21)
     end
 
@@ -181,14 +178,14 @@ RSpec.describe Query::Results, 'sums' do
                   material_costs_column => 200.0,
                   labor_costs_column => 300.0,
                   overall_costs_column => 500.0,
-                  remaining_hours_column => 6.0,
+                  remaining_hours_column => 5.0,
                   story_points_column => 14)
       end
     end
   end
 
   describe '#all_sums_for_group' do
-    context 'grouped by assigned_to' do
+    context 'when grouped by assigned_to' do
       let(:group_by) { :assigned_to }
 
       it 'is a hash of sums grouped by user values (and nil) and grouped columns' do
@@ -199,15 +196,15 @@ RSpec.describe Query::Results, 'sums' do
                                     material_costs_column => 200.0,
                                     labor_costs_column => 300.0,
                                     overall_costs_column => 500.0,
-                                    remaining_hours_column => 6.0,
+                                    remaining_hours_column => 5.0,
                                     story_points_column => 14 },
-                  nil => { estimated_hours_column => 5.0,
+                  nil => { estimated_hours_column => 10.0,
                            int_cf_column => 10,
                            float_cf_column => 3.41,
                            material_costs_column => 200.0,
                            labor_costs_column => 300.0,
                            overall_costs_column => 500.0,
-                           remaining_hours_column => 3.0,
+                           remaining_hours_column => 9.0,
                            story_points_column => 7 })
       end
 
@@ -225,12 +222,12 @@ RSpec.describe Query::Results, 'sums' do
                                       labor_costs_column => 0.0,
                                       overall_costs_column => 0.0,
                                       story_points_column => 7,
-                                      remaining_hours_column => 3.0 })
+                                      remaining_hours_column => 2.5 })
         end
       end
     end
 
-    context 'grouped by done_ratio' do
+    context 'when grouped by done_ratio' do
       let(:group_by) { :done_ratio }
 
       it 'is a hash of sums grouped by done_ratio values and grouped columns' do
@@ -241,15 +238,15 @@ RSpec.describe Query::Results, 'sums' do
                           material_costs_column => 200.0,
                           labor_costs_column => 300.0,
                           overall_costs_column => 500.0,
-                          remaining_hours_column => 6.0,
+                          remaining_hours_column => 5.0,
                           story_points_column => 14 },
-                  10 => { estimated_hours_column => 5.0,
+                  10 => { estimated_hours_column => 10.0,
                           int_cf_column => 10,
                           float_cf_column => 3.41,
                           material_costs_column => 200.0,
                           labor_costs_column => 300.0,
                           overall_costs_column => 500.0,
-                          remaining_hours_column => 3.0,
+                          remaining_hours_column => 9.0,
                           story_points_column => 7 })
       end
 
@@ -267,7 +264,7 @@ RSpec.describe Query::Results, 'sums' do
                             labor_costs_column => 0.0,
                             overall_costs_column => 0.0,
                             story_points_column => 7,
-                            remaining_hours_column => 3.0 })
+                            remaining_hours_column => 2.5 })
         end
       end
     end
