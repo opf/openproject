0f7b53030489195da005a1b412beb6a3771ea9fd
Klaus Zanders <k.zanders@openproject.com>
2024-02-27 12:04:21 +0100

    imore tests

--- modules/two_factor_authentication/spec/models/device/webauthn_spec.rb
+++ modules/two_factor_authentication/spec/models/device/webauthn_spec.rb
@@ -1,9 +1,10 @@
 require 'spec_helper'
 
 RSpec.describe TwoFactorAuthentication::Device::Webauthn do
-  subject { create(:two_factor_authentication_device_webauthn) }
+  let(:user) { create(:user) }
+
+  subject { build(:two_factor_authentication_device_webauthn, user:) }
 
   it { is_expected.to validate_presence_of(:webauthn_external_id) }
-  it { is_expected.to validate_uniqueness_of(:webauthn_external_id).scoped_to(:user_id) }
   it { is_expected.to validate_presence_of(:webauthn_public_key) }
 end
