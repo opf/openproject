43150a98b6651225526ba4d1e06e77175903a4e4
Andreas Pfohl <a.pfohl@openproject.com>
2024-02-29 15:28:31 +0100

    Fixed a false behavior in storage HealthService

--- modules/storages/app/services/storages/health/health_service.rb
+++ modules/storages/app/services/storages/health/health_service.rb
@@ -1,3 +1,31 @@
+#-- copyright
+# OpenProject is an open source project management software.
+# Copyright (C) 2012-2024 the OpenProject GmbH
+#
+# This program is free software; you can redistribute it and/or
+# modify it under the terms of the GNU General Public License version 3.
+#
+# OpenProject is a fork of ChiliProject, which is a fork of Redmine. The copyright follows:
+# Copyright (C) 2006-2013 Jean-Philippe Lang
+# Copyright (C) 2010-2013 the ChiliProject Team
+#
+# This program is free software; you can redistribute it and/or
+# modify it under the terms of the GNU General Public License
+# as published by the Free Software Foundation; either version 2
+# of the License, or (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+#
+# See COPYRIGHT and LICENSE files for more details.
+#++
+
 module Storages
   module Health
     class HealthService
@@ -17,10 +45,13 @@ module Storages
       end
 
       def unhealthy(reason:)
+        last_reason = @storage.health_reason
         @storage.mark_as_unhealthy(reason:)
 
-        admin_users.each do |admin|
-          ::Storages::StoragesMailer.notify_unhealthy(admin, @storage).deliver_now
+        if @storage.health_reason != last_reason
+          admin_users.each do |admin|
+            ::Storages::StoragesMailer.notify_unhealthy(admin, @storage).deliver_now
+          end
         end
 
         schedule_mail_job(@storage) unless mail_job_exists?
