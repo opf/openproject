7a20c5fe2fa6c5469d0954668bdf6417df081cb7
ulferts <jens.ulferts@googlemail.com>
2024-02-29 12:09:05 +0100

    default values for existing project_queries

--- db/migrate/20240222155909_rename_columns_on_project_queries.rb
+++ db/migrate/20240222155909_rename_columns_on_project_queries.rb
@@ -1,5 +1,21 @@
 class RenameColumnsOnProjectQueries < ActiveRecord::Migration[7.1]
   def change
     rename_column :project_queries, :columns, :selects
+
+    reversible do |dir|
+      dir.up do
+        execute <<~SQL.squish
+          UPDATE project_queries
+          SET selects = '#{Setting.enabled_projects_columns}'
+        SQL
+      end
+
+      dir.down do
+        execute <<~SQL.squish
+          UPDATE project_queries
+          SET selects = '[]'
+        SQL
+      end
+    end
   end
 end
