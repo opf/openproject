9ac2f1cd18dafa53693a210fe55495316d425d1f
Aaron Contreras <aaronlcaq@gmail.com>
2024-03-01 18:16:56 -0500

    Run `rubocop -a` on app/services

--- app/services/base_services/create.rb
+++ app/services/base_services/create.rb
@@ -30,8 +30,8 @@ module BaseServices
   class Create < Write
     protected
 
-    def service_context(send_notifications:, &block)
-      in_user_context(send_notifications:, &block)
+    def service_context(send_notifications:, &)
+      in_user_context(send_notifications:, &)
     end
 
     def instance(_params)
