0ea5c3f36848a92c6311ffab6304702d8fbbc6e7
Dombi Attila <83396+dombesz@users.noreply.github.com>
2024-03-04 20:15:38 +0200

    Replace the Project details widget with deprecation message

--- modules/dashboards/spec/features/project_details_spec.rb
+++ modules/dashboards/spec/features/project_details_spec.rb
@@ -31,30 +31,10 @@ require 'spec_helper'
 require_relative '../support/pages/dashboard'
 
 RSpec.describe 'Project details widget on dashboard', :js do
-  let!(:version_cf) { create(:version_project_custom_field) }
-  let!(:bool_cf) { create(:boolean_project_custom_field) }
-  let!(:user_cf) { create(:user_project_custom_field) }
-  let!(:int_cf) { create(:integer_project_custom_field) }
-  let!(:float_cf) { create(:float_project_custom_field) }
-  let!(:text_cf) { create(:text_project_custom_field) }
-  let!(:string_cf) { create(:string_project_custom_field) }
-  let!(:date_cf) { create(:date_project_custom_field) }
-
   let(:system_version) { create(:version, sharing: 'system') }
 
   let!(:project) do
-    create(:project, members: { other_user => role }).tap do |p|
-      p.send(int_cf.attribute_setter, 5)
-      p.send(bool_cf.attribute_setter, true)
-      p.send(version_cf.attribute_setter, system_version)
-      p.send(float_cf.attribute_setter, 4.5)
-      p.send(text_cf.attribute_setter, 'Some **long** text')
-      p.send(string_cf.attribute_setter, 'Some small text')
-      p.send(date_cf.attribute_setter, Date.current)
-      p.send(user_cf.attribute_setter, other_user)
-
-      p.save!(validate: false)
-    end
+    create(:project, members: { other_user => role })
   end
 
   let(:permissions) do
@@ -98,22 +78,6 @@ RSpec.describe 'Project details widget on dashboard', :js do
     dashboard_page.expect_and_dismiss_toaster message: I18n.t('js.notice_successful_update')
   end
 
-  def change_cf_value(cf, old_value, new_value)
-    # Open description field
-    cf.activate!
-    sleep(0.1)
-
-    # Change the value
-    cf.expect_value(old_value)
-    cf.set_value new_value
-    cf.save! unless cf.field_type === 'create-autocompleter'
-
-    # The edit field is toggled and the value saved.
-    expect(page).to have_content(new_value)
-    expect(page).to have_selector(cf.selector)
-    expect(page).to have_no_selector(cf.input_selector)
-  end
-
   before do
     login_as current_user
     add_project_details_widget
@@ -122,33 +86,25 @@ RSpec.describe 'Project details widget on dashboard', :js do
   context 'without editing permissions' do
     let(:current_user) { read_only_user }
 
-    it 'can add the widget, but not edit the custom fields' do
+    it 'displays the deprecated message' do
       # As the user lacks the manage_public_queries and save_queries permission, no other widget is present
       details_widget = Components::Grids::GridArea.new('.grid--area.-widgeted:nth-of-type(1)')
 
       within(details_widget.area) do
-        # Expect values
         expect(page)
-          .to have_content("#{int_cf.name}\n5")
-        expect(page)
-          .to have_content("#{bool_cf.name}\nyes")
-        expect(page)
-          .to have_content("#{version_cf.name}\n#{system_version.name}")
-        expect(page)
-          .to have_content("#{float_cf.name}\n4.5")
-        expect(page)
-          .to have_content("#{text_cf.name}\nSome long text")
-        expect(page)
-          .to have_content("#{string_cf.name}\nSome small text")
-        expect(page)
-          .to have_content("#{date_cf.name}\n#{Date.today.strftime('%m/%d/%Y')}")
-        expect(page)
-           .to have_content("#{user_cf.name}\n#{other_user.name.split.map(&:first).join}\n#{other_user.name}")
-
-        # The fields are not editable
-        field = EditField.new dashboard_page, bool_cf.attribute_name(:camel_case)
-        field.expect_read_only
-        field.activate! expect_open: false
+          .to have_content("Project details have now moved to a column on the right edge of this page.")
+        expect(page).to have_content(
+          <<~TEXT.strip
+            Starting with version 13.4, project attributes can be grouped \
+            in sections and enabled and disabled at a project level.
+          TEXT
+        )
+        expect(page).to have_content(
+          <<~TEXT.strip
+            This widget can now be removed or replaced. \
+            It will be deleted in subsequent versions.
+          TEXT
+        )
       end
     end
   end
@@ -156,18 +112,26 @@ RSpec.describe 'Project details widget on dashboard', :js do
   context 'with editing permissions' do
     let(:current_user) { editing_user }
 
-    it 'can edit the custom fields' do
-      int_field = EditField.new dashboard_page, int_cf.attribute_name(:camel_case)
-      change_cf_value int_field, "5", "3"
+    it 'displays the deprecated message' do
+      # As the user lacks the manage_public_queries and save_queries permission, no other widget is present
+      details_widget = Components::Grids::GridArea.new('.grid--area.-widgeted:nth-of-type(1)')
 
-      string_field = EditField.new dashboard_page, string_cf.attribute_name(:camel_case)
-      change_cf_value string_field, 'Some small text', 'Some new text'
-
-      text_field = TextEditorField.new dashboard_page, text_cf.attribute_name(:camel_case)
-      change_cf_value text_field, 'Some long text', 'Some very long text'
-
-      user_field = SelectField.new dashboard_page, user_cf.attribute_name(:camel_case)
-      change_cf_value user_field, other_user.name, editing_user.name
+      within(details_widget.area) do
+        expect(page)
+          .to have_content("Project details have now moved to a column on the right edge of this page.")
+        expect(page).to have_content(
+          <<~TEXT.strip
+            Starting with version 13.4, project attributes can be grouped \
+            in sections and enabled and disabled at a project level.
+          TEXT
+        )
+        expect(page).to have_content(
+          <<~TEXT.strip
+            This widget can now be removed or replaced. \
+            It will be deleted in subsequent versions.
+          TEXT
+        )
+      end
     end
   end
 
