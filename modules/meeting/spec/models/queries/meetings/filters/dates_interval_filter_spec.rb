4304dde0d11d944bfab43d42a41fcaad67eb80d7
Oliver Günther <mail@oliverguenther.de>
2024-03-10 21:03:12 +0100

    Show meetings in calendar widgets

--- modules/meeting/spec/models/queries/meetings/filters/dates_interval_filter_spec.rb
+++ modules/meeting/spec/models/queries/meetings/filters/dates_interval_filter_spec.rb
@@ -26,13 +26,25 @@
 # See COPYRIGHT and LICENSE files for more details.
 #++
 
-module Queries::Meetings
-  ::Queries::Register.register(MeetingQuery) do
-    filter Filters::ProjectFilter
-    filter Filters::TimeFilter
-    filter Filters::AttendedUserFilter
-    filter Filters::InvitedUserFilter
-    filter Filters::AuthorFilter
-    filter Filters::DatesIntervalFilter
+require "spec_helper"
+
+RSpec.describe Queries::Meetings::Filters::DatesIntervalFilter do
+  it_behaves_like "basic query filter" do
+    let(:type) { :date }
+    let(:class_key) { :dates_interval }
+
+    describe "#available?" do
+      it "is true" do
+        expect(instance).to be_available
+      end
+    end
+
+    describe "#allowed_values" do
+      it "is nil" do
+        expect(instance.allowed_values).to be_nil
+      end
+    end
+
+    it_behaves_like "non ar filter"
   end
 end
