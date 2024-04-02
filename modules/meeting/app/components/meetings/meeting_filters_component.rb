e85ffadb264c8ab973472c60de5ca1806ba7775f
Klaus Zanders <k.zanders@openproject.com>
2024-04-02 17:19:50 +0200

    Start introducing query filter component for meetings

---
+++ modules/meeting/app/components/meetings/meeting_filters_component.rb
@@ -0,0 +1,53 @@
+# frozen_string_literal: true
+
+# -- copyright
+# OpenProject is an open source project management software.
+# Copyright (C) 2010-2023 the OpenProject GmbH
+#
+# This program is free software; you can redistribute it and/or
+# modify it under the terms of the GNU General Public License version 3.
+#
+# OpenProject is a fork of ChiliProject, which is a fork of Redmine. The copyright follows:
+# Copyright (C) 2006-2013 Jean-Philippe Lang
+# Copyright (C) 2010-2013 the ChiliProject Team
+#
+# This program is free software; you can redistribute it and/or
+# modify it under the terms of the GNU General Public License
+# as published by the Free Software Foundation; either version 2
+# of the License, or (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+#
+# See COPYRIGHT and LICENSE files for more details.
+# ++
+module Meetings
+  class MeetingFiltersComponent < FiltersComponent
+    def allowed_filters
+      super
+        .select { |f| allowed_filter?(f) }
+        .sort_by(&:human_name)
+    end
+
+    private
+
+    def allowed_filter?(filter)
+      allowlist = [
+        Queries::Meetings::Filters::ProjectFilter,
+        Queries::Meetings::Filters::TimeFilter,
+        Queries::Meetings::Filters::AttendedUserFilter,
+        Queries::Meetings::Filters::InvitedUserFilter,
+        Queries::Meetings::Filters::AuthorFilter,
+        Queries::Meetings::Filters::DatesIntervalFilter
+      ]
+
+      allowlist.detect { |clazz| filter.is_a? clazz }
+    end
+  end
+end
