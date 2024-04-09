f7ba77489c6a4cf406ca6218abf7fe4037d3857a
Oliver Günther <mail@oliverguenther.de>
2024-04-09 14:17:36 +0200

    Add virtual column

--- db/migrate/20240408132459_rename_delay_to_lag.rb
+++ db/migrate/20240408132459_rename_delay_to_lag.rb
@@ -2,5 +2,7 @@ class RenameDelayToLag < ActiveRecord::Migration[7.1]
   def change
     rename_column :relations, :delay, :lag
 
+    # TODO remove after 14.0
+    add_column :relations, :delay, :virtual, type: :integer, as: 'lag', stored: true
   end
 end
