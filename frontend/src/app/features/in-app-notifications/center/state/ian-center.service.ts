f65bb82d0ddab400dbe19c27215e23e0a4d42a9d
Behrokh Satarnejad <b.satarnejad@openproject.com>
2024-04-15 08:18:02 +0200

    use bell icon in notification center toasts

--- frontend/src/app/features/in-app-notifications/center/state/ian-center.service.ts
+++ frontend/src/app/features/in-app-notifications/center/state/ian-center.service.ts
@@ -330,7 +330,7 @@ export class IanCenterService extends UntilDestroyedMixin {
 
       this.activeReloadToast = this.toastService.add({
         type: 'info',
-        icon: 'notification',
+        icon: 'bell',
         message: this.I18n.t('js.notifications.center.new_notifications.message'),
         link: {
           text: this.I18n.t('js.notifications.center.new_notifications.link_text'),
