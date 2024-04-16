341e557721fee4d978ae3c7ef3aac2e6222a46c9
Pavel Balashou <ba1ashpash@gmail.com>
2024-04-16 15:00:26 +0200

    [#54293] Reenable HTTPX persistent connections.
    https://community.openproject.org/work_packages/54293

--- lib/open_project.rb
+++ lib/open_project.rb
@@ -47,20 +47,23 @@ module OpenProject
   end
 
   def self.httpx
-    session = HTTPX
-                .plugin(:oauth)
-                .plugin(:basic_auth)
-                .plugin(:webdav)
-                .with(
-                  timeout: {
-                    connect_timeout: OpenProject::Configuration.httpx_connect_timeout,
-                    operation_timeout: OpenProject::Configuration.httpx_operation_timeout,
-                    request_timeout: OpenProject::Configuration.httpx_request_timeout,
-                    write_timeout: OpenProject::Configuration.httpx_write_timeout,
-                    read_timeout: OpenProject::Configuration.httpx_read_timeout,
-                    keep_alive_timeout: OpenProject::Configuration.httpx_keep_alive_timeout
-                  }
-                )
-    OpenProject::Appsignal.enabled? ? session.plugin(HttpxAppsignal) : session
+    Thread.current[:httpx_session] ||= begin
+      session = HTTPX
+                  .plugin(:oauth)
+                  .plugin(:persistent)
+                  .plugin(:basic_auth)
+                  .plugin(:webdav)
+                  .with(
+                    timeout: {
+                      connect_timeout: OpenProject::Configuration.httpx_connect_timeout,
+                      operation_timeout: OpenProject::Configuration.httpx_operation_timeout,
+                      request_timeout: OpenProject::Configuration.httpx_request_timeout,
+                      write_timeout: OpenProject::Configuration.httpx_write_timeout,
+                      read_timeout: OpenProject::Configuration.httpx_read_timeout,
+                      keep_alive_timeout: OpenProject::Configuration.httpx_keep_alive_timeout
+                    }
+                  )
+      OpenProject::Appsignal.enabled? ? session.plugin(HttpxAppsignal) : session
+    end
   end
 end
