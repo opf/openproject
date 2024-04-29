df36334e8baa41cc91b417c0c8e91f9ad2adcbed
Pavel Balashou <ba1ashpash@gmail.com>
2024-04-29 17:25:00 +0200

    Do not cache HTTPX session object in tests.

--- lib/open_project.rb
+++ lib/open_project.rb
@@ -47,23 +47,32 @@ module OpenProject
   end
 
   def self.httpx
-    Thread.current[:httpx_session] ||= begin
-      session = HTTPX
-                  .plugin(:oauth)
-                  .plugin(:persistent)
-                  .plugin(:basic_auth)
-                  .plugin(:webdav)
-                  .with(
-                    timeout: {
-                      connect_timeout: OpenProject::Configuration.httpx_connect_timeout,
-                      operation_timeout: OpenProject::Configuration.httpx_operation_timeout,
-                      request_timeout: OpenProject::Configuration.httpx_request_timeout,
-                      write_timeout: OpenProject::Configuration.httpx_write_timeout,
-                      read_timeout: OpenProject::Configuration.httpx_read_timeout,
-                      keep_alive_timeout: OpenProject::Configuration.httpx_keep_alive_timeout
-                    }
-                  )
-      OpenProject::Appsignal.enabled? ? session.plugin(HttpxAppsignal) : session
+    # In tests it is not advisible to cache httpx session object,
+    # because, for example, stubs made for this object in one test case can
+    # easy interfere with other test cases using the same session object.
+    if Rails.env.test?
+      httpx_session
+    else
+      Thread.current[:httpx_session] ||= httpx_session
     end
+  end
+
+  private_class_method def self.httpx_session
+    session = HTTPX
+                .plugin(:oauth)
+                .plugin(:persistent)
+                .plugin(:basic_auth)
+                .plugin(:webdav)
+                .with(
+                  timeout: {
+                    connect_timeout: OpenProject::Configuration.httpx_connect_timeout,
+                    operation_timeout: OpenProject::Configuration.httpx_operation_timeout,
+                    request_timeout: OpenProject::Configuration.httpx_request_timeout,
+                    write_timeout: OpenProject::Configuration.httpx_write_timeout,
+                    read_timeout: OpenProject::Configuration.httpx_read_timeout,
+                    keep_alive_timeout: OpenProject::Configuration.httpx_keep_alive_timeout
+                  }
+                )
+    OpenProject::Appsignal.enabled? ? session.plugin(HttpxAppsignal) : session
   end
 end
