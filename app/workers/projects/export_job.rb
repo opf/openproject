0986f7f1af655d0e0eeca106419be0a94b42edb3
Klaus Zanders <k.zanders@openproject.com>
2024-05-08 09:23:53 +0200

    Change serialization of orders to be an array instead of a hash

--- app/workers/projects/export_job.rb
+++ app/workers/projects/export_job.rb
@@ -8,6 +8,10 @@ module Projects
 
     def prepare!
       self.query = ::Queries::Projects::ProjectQuery.from_hash(query)
+
+      puts "*" * 100
+      puts(query.orders)
+      puts "*" * 100
     end
   end
 end
