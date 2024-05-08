fdea33a2c917b206ab2e63febf0979f94aedf1e8
Klaus Zanders <k.zanders@openproject.com>
2024-05-08 10:56:40 +0200

    Remove temporary debug ouput

--- app/workers/projects/export_job.rb
+++ app/workers/projects/export_job.rb
@@ -8,10 +8,6 @@ module Projects
 
     def prepare!
       self.query = ::Queries::Projects::ProjectQuery.from_hash(query)
-
-      puts "*" * 100
-      puts(query.orders)
-      puts "*" * 100
     end
   end
 end
