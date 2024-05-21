6c6019dfcadb98b06fc359288be62eef7c1e202b
Christophe Bliard <c.bliard@openproject.com>
2024-05-21 10:34:01 +0200

    Increase log level of pool size increase to INFO

--- config/initializers/database_pool_size.rb
+++ config/initializers/database_pool_size.rb
@@ -31,7 +31,7 @@ pool_size = config && [OpenProject::Configuration.web_max_threads + 1, config["p
 
 # make sure we have enough connections in the pool for each thread and then some
 if pool_size && pool_size > ActiveRecord::Base.connection_pool.size
-  Rails.logger.debug { "Increasing database pool size to #{pool_size} to match max threads" }
+  Rails.logger.info { "Increasing database pool size to #{pool_size} to match max threads" }
 
   ActiveRecord::Base.establish_connection config.merge(pool: pool_size)
 end
