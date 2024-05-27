5cde7451b64dd8ce87e530d30af68c285d8828e3
Klaus Zanders <k.zanders@openproject.com>
2024-05-27 09:31:31 +0200

    Introduce ProjectQueryRole

--- app/models/role.rb
+++ app/models/role.rb
@@ -34,6 +34,13 @@ class Role < ApplicationRecord
   BUILTIN_WORK_PACKAGE_VIEWER = 3
   BUILTIN_WORK_PACKAGE_COMMENTER = 4
   BUILTIN_WORK_PACKAGE_EDITOR = 5
+  BUILTIN_PROJECT_QUERY_VIEW = 6
+  BUILTIN_PROJECT_QUERY_EDIT = 7
+
+  HIDDEN_ROLE_TYPES = [
+    "WorkPackageRole",
+    "ProjectQueryRole"
+  ]
 
   scope :builtin, ->(*args) {
     compare = "not" if args.first == true
@@ -41,7 +48,7 @@ class Role < ApplicationRecord
   }
 
   # Work Package Roles are intentionally visually hidden from users temporarily
-  scope :visible, -> { where.not(type: "WorkPackageRole") }
+  scope :visible, -> { where.not(type: HIDDEN_ROLE_TYPES) }
   scope :ordered_by_builtin_and_position, -> { order(Arel.sql("builtin, position")) }
 
   before_destroy(prepend: true) do
@@ -84,7 +91,9 @@ class Role < ApplicationRecord
           Role::BUILTIN_ANONYMOUS,
           Role::BUILTIN_WORK_PACKAGE_VIEWER,
           Role::BUILTIN_WORK_PACKAGE_COMMENTER,
-          Role::BUILTIN_WORK_PACKAGE_EDITOR
+          Role::BUILTIN_WORK_PACKAGE_EDITOR,
+          Role::BUILTIN_PROJECT_QUERY_VIEW,
+          Role::BUILTIN_PROJECT_QUERY_EDIT
         ]
       )
       .order(Arel.sql("position"))
