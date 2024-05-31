fcfc97f9c42619162fba36590d86c75a0d601e6b
Ivan Kuchin <i.kuchin@openproject.com>
2024-05-31 19:10:24 +0200

    construct read_ian condition in unsent_reminders_before not as an SQL fragment

--- app/models/notifications/scopes/unsent_reminders_before.rb
+++ app/models/notifications/scopes/unsent_reminders_before.rb
@@ -36,7 +36,7 @@ module Notifications::Scopes
       def unsent_reminders_before(recipient:, time:)
         where(Notification.arel_table[:created_at].lteq(time))
           .where(recipient:)
-          .where("read_ian IS NULL OR read_ian IS FALSE")
+          .where(read_ian: [false, nil])
           .where(mail_reminder_sent: false)
       end
     end
