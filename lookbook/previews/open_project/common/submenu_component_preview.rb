fbfe39fadb5d16f96b6347418aa6b2f56cf28906
Henriette Darge <h.darge@openproject.com>
2024-06-11 11:44:45 +0200

    Add option to make the submenu searchable and use the generic filterListController for it

--- lookbook/previews/open_project/common/submenu_component_preview.rb
+++ lookbook/previews/open_project/common/submenu_component_preview.rb
@@ -2,32 +2,48 @@ module OpenProject
   module Common
     # @logical_path OpenProject/Common
     class SubmenuComponentPreview < Lookbook::Preview
+      # @label Default
       def default
-        # Meant to be rendered inside the left-handed sidebar
-        render OpenProject::Common::SubmenuComponent.new(
-          sidebar_menu_items: [
-            OpenProject::Menu::MenuGroup.new(
-              header: nil,
-              children: [
-                OpenProject::Menu::MenuItem.new(title: I18n.t("members.menu.all"), href: "", selected: true),
-                OpenProject::Menu::MenuItem.new(title: I18n.t("members.menu.locked"), href: "", selected: false),
-                OpenProject::Menu::MenuItem.new(title: I18n.t("members.menu.invited"), href: "", selected: false)
-              ]
-            ),
-            OpenProject::Menu::MenuGroup.new(
-              header: I18n.t("members.menu.project_roles"),
-              children: [
-                OpenProject::Menu::MenuItem.new(title: "ROLE X", href: "", selected: false)
-              ]
-            ),
-            OpenProject::Menu::MenuGroup.new(
-              header: I18n.t("members.menu.groups"),
-              children: [
-                OpenProject::Menu::MenuItem.new(title: "GROUP X", href: "", selected: false)
-              ]
-            )
-          ]
-        )
+        render_with_template(template: "open_project/common/submenu_preview/playground",
+                             locals: { sidebar_menu_items: menu_items, searchable: false })
+      end
+
+      # @label Searchable
+      # Searching is currently not working in the lookbook because stimulus controllers are not loaded correctly.
+      # It will be fine in production.
+      def searchable
+        render_with_template(template: "open_project/common/submenu_preview/playground",
+                             locals: { sidebar_menu_items: menu_items, searchable: true })
+      end
+
+      private
+
+      def menu_items
+        [
+          OpenProject::Menu::MenuGroup.new(
+            header: nil,
+            children: [
+              OpenProject::Menu::MenuItem.new(title: I18n.t("members.menu.all"), href: "", selected: true),
+              OpenProject::Menu::MenuItem.new(title: I18n.t("members.menu.locked"), href: "", selected: false),
+              OpenProject::Menu::MenuItem.new(title: I18n.t("members.menu.invited"), href: "", selected: false)
+            ]
+          ),
+          OpenProject::Menu::MenuGroup.new(
+            header: I18n.t("members.menu.project_roles"),
+            children: [
+              OpenProject::Menu::MenuItem.new(title: "Developer", href: "", selected: false),
+              OpenProject::Menu::MenuItem.new(title: "Manager", href: "", selected: true)
+            ]
+          ),
+          OpenProject::Menu::MenuGroup.new(
+            header: I18n.t("members.menu.groups"),
+            children: [
+              OpenProject::Menu::MenuItem.new(title: "UX", href: "", selected: false),
+              OpenProject::Menu::MenuItem.new(title: "Customer success", href: "", selected: false),
+              OpenProject::Menu::MenuItem.new(title: "Core dev team", href: "", selected: false)
+            ]
+          )
+        ]
       end
     end
   end
