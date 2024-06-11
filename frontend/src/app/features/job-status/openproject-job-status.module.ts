d0d9522da2d20ab3fd25a8196df64fc529a4be24
Oliver Günther <mail@oliverguenther.de>
2024-06-11 19:36:41 +0200

    Replace ui-router in job status

--- frontend/src/app/features/job-status/openproject-job-status.module.ts
+++ frontend/src/app/features/job-status/openproject-job-status.module.ts
@@ -29,31 +29,14 @@
 import { NgModule } from '@angular/core';
 import { OpSharedModule } from 'core-app/shared/shared.module';
 import { OpenprojectModalModule } from 'core-app/shared/components/modal/modal.module';
-import { Ng2StateDeclaration, UIRouterModule } from '@uirouter/angular';
 import { DisplayJobPageComponent } from 'core-app/features/job-status/display-job-page/display-job-page.component';
 import { JobStatusModalComponent } from 'core-app/features/job-status/job-status-modal/job-status.modal';
-
-export const JOB_STATUS_ROUTE:Ng2StateDeclaration[] = [
-  {
-    name: 'job-statuses',
-    url: '/job_statuses/{jobId:[a-z0-9-]+}',
-    parent: 'optional_project',
-    component: DisplayJobPageComponent,
-    data: {
-      bodyClasses: 'router--job-statuses',
-    },
-  },
-];
 
 @NgModule({
   imports: [
     // Commons
     OpSharedModule,
     OpenprojectModalModule,
-
-    // Routes for /job_statuses/:uuid
-    UIRouterModule.forChild({ states: JOB_STATUS_ROUTE }),
-
   ],
   declarations: [
     DisplayJobPageComponent,
