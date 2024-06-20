a45a4c132cd22dd09f3ae4679c390df1decd0343
Eric Schubert <e.schubert@openproject.com>
2024-06-20 14:58:23 +0200

    [#55382] added unit tests for controller and validator

--- modules/storages/spec/requests/storages/admin/connection_validation_spec.rb
+++ modules/storages/spec/requests/storages/admin/connection_validation_spec.rb
@@ -37,9 +37,6 @@ RSpec.describe "connection validation", :skip_csrf do
       allow(double).to receive_messages(validate: validation_result)
       double
     end
-    let(:validation_result) do
-      Storages::ConnectionValidation.new(icon: "check-circle", scheme: :default, description: "Successful!")
-    end
 
     current_user { user }
 
@@ -47,11 +44,93 @@ RSpec.describe "connection validation", :skip_csrf do
       allow(Storages::Peripherals::OneDriveConnectionValidator).to receive(:new).and_return(validator)
     end
 
-    it "returns a connection validation result" do
-      response = post validate_connection_admin_settings_storage_connection_validation_path(storage.id, format: :turbo_stream)
-      expect(response.status).to eq(200)
+    subject do
+      post validate_connection_admin_settings_storage_connection_validation_path(storage.id, format: :turbo_stream)
+    end
 
-      doc = Nokogiri::HTML(response.body)
+    shared_examples_for "a validation result template" do |show_timestamp:, label:, description:|
+      it "returns a turbo update template" do
+        expect(subject.status).to eq(200)
+
+        doc = Nokogiri::HTML(subject.body)
+        expect(doc.xpath(xpath_for_subtitle).text).to eq("Connection validation")
+
+        if show_timestamp
+          expect(doc.xpath(xpath_for_timestamp)).not_to be_empty
+        else
+          expect(doc.xpath(xpath_for_timestamp)).to be_empty
+        end
+
+        if label.present?
+          expect(doc.xpath(xpath_for_label).text).to eq(label)
+        else
+          expect(doc.xpath(xpath_for_label).text).to be_empty
+        end
+
+        if description.present?
+          expect(doc.xpath(xpath_for_description).text).to eq(description)
+        else
+          expect(doc.xpath(xpath_for_description).text).to be_empty
+        end
+      end
+    end
+
+    context "if the a validation result of type :none (no validation executed) is returned" do
+      let(:validation_result) do
+        Storages::ConnectionValidation.new(type: :none, timestamp: Time.current, description: "not configured")
+      end
+
+      it_behaves_like "a validation result template", show_timestamp: false, label: nil, description: "not configured"
+    end
+
+    context "if validator returns an error" do
+      let(:validation_result) do
+        Storages::ConnectionValidation.new(type: :error, timestamp: Time.current, description: "An error occurred")
+      end
+
+      it_behaves_like "a validation result template",
+                      show_timestamp: true, label: "Error", description: "An error occurred"
+    end
+
+    context "if validator returns a warning" do
+      let(:validation_result) do
+        Storages::ConnectionValidation.new(type: :warning,
+                                           timestamp: Time.current,
+                                           description: "There is something weird...")
+      end
+
+      it_behaves_like "a validation result template",
+                      show_timestamp: true, label: "Warning", description: "There is something weird..."
+    end
+
+    context "if validator returns a success" do
+      let(:validation_result) do
+        Storages::ConnectionValidation.new(type: :healthy, timestamp: Time.current, description: nil)
+      end
+
+      it_behaves_like "a validation result template",
+                      show_timestamp: true, label: "Healthy", description: nil
     end
   end
+
+  private
+
+  # rubocop:disable Layout/LineLength
+  def xpath_for_subtitle
+    "//turbo-stream[@target='connection_validation_result']/template/div/div/span[@data-test-selector='validation-result--subtitle']"
+  end
+
+  def xpath_for_timestamp
+    "//turbo-stream[@target='connection_validation_result']/template/div/div/span[@data-test-selector='validation-result--timestamp']"
+  end
+
+  def xpath_for_label
+    "//turbo-stream[@target='connection_validation_result']/template/div/div/span[contains(@class, 'Label')]"
+  end
+
+  def xpath_for_description
+    "//turbo-stream[@target='connection_validation_result']/template/div/div/span[@data-test-selector='validation-result--description']"
+  end
+
+  # rubocop:enable Layout/LineLength
 end
