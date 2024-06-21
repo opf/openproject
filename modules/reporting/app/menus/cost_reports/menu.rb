4961d3845b973de1e8f76047101aedf2f6737143
Henriette Darge <h.darge@openproject.com>
2024-06-21 11:35:21 +0200

    Replace angular sidemenu with rails based Submenu component for Time&Costs  module

--- modules/reporting/app/menus/cost_reports/menu.rb
+++ modules/reporting/app/menus/cost_reports/menu.rb
@@ -1,6 +1,6 @@
-#-- copyright
+# -- copyright
 # OpenProject is an open source project management software.
-# Copyright (C) 2012-2024 the OpenProject GmbH
+# Copyright (C) 2010-2023 the OpenProject GmbH
 #
 # This program is free software; you can redistribute it and/or
 # modify it under the terms of the GNU General Public License version 3.
@@ -24,40 +24,51 @@
 # Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
 #
 # See COPYRIGHT and LICENSE files for more details.
-#++
+# ++
+module CostReports
+  class Menu < Submenu
+    attr_reader :view_type, :project
 
-Rails.application.routes.draw do
-  scope "projects/:project_id" do
-    resources :cost_reports, except: :create do
-      collection do
-        match :index, via: %i[get post]
-        get "menu" => "cost_reports/menus#show", as: :menu_project
-      end
+    def initialize(project: nil, params: nil)
+      @project = project
+      @params = params
 
-      member do
-        get :index, as: :project
-        post :update
-        post :rename
-      end
-    end
-  end
-
-  namespace :cost_reports do
-    resource :menu, only: %[show]
-  end
-
-  resources :cost_reports, except: :create do
-    collection do
-      match :index, via: %i[get post]
-      post :save_as, action: :create
-      get :drill_down
-      match :available_values, via: %i[get post]
+      super(view_type:, project:, params:)
     end
 
-    member do
-      get :index, as: :global
-      post :update
-      post :rename
+    def menu_items
+      [
+        OpenProject::Menu::MenuGroup.new(header: I18n.t("label_public_report_plural"), children: global_queries),
+        OpenProject::Menu::MenuGroup.new(header: I18n.t("label_private_report_plural"), children: custom_queries)
+      ]
+    end
+
+    def global_queries
+      CostQuery.public(project)
+        .pluck(:id, :name)
+        .map { |id, name| menu_item(query_params(id), name) }
+    end
+
+    def custom_queries
+      CostQuery.private(project, User.current)
+        .pluck(:id, :name)
+        .map { |id, name| menu_item(query_params(id), name) }
+    end
+
+    def selected?(query_params)
+      query_params[:id].to_s == params[:id]
+    end
+
+    def query_params(id)
+      { id: }
+    end
+
+    def query_path(query_params)
+      if project.present?
+        project_cost_report_path(project, query_params)
+      else
+        global_cost_report_path(query_params)
+      end
     end
   end
 end
