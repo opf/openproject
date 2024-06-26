b406e5e34edd6f580f858816a2b1683d202f79a0
Ivan Kuchin <i.kuchin@openproject.com>
2024-06-26 17:50:35 +0200

    just mention acts_as_favorable/acts_as_watchable models instead of explicitly adding to registry

--- config/initializers/acts_as_watchable.rb
+++ config/initializers/acts_as_watchable.rb
@@ -1,8 +1,13 @@
 # Be sure to restart your server when you modify this file.
 
-# In development and non-eager loaded mode, we need to register the acts_as_watchable models manually
+# For development and non-eager load mode, we need to load models using acts_as_watchable manually
 # as no eager loading takes place
 Rails.application.config.after_initialize do
-  OpenProject::Acts::Watchable::Registry
-    .add(WorkPackage, Message, Forum, News, Meeting, Wiki, WikiPage)
+  Forum
+  Meeting
+  Message
+  News
+  Wiki
+  WikiPage
+  WorkPackage
 end
