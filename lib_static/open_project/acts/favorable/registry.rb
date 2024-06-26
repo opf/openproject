bde7c1b4e4ed9e5748f56eeeb8e72d68043cc574
Ivan Kuchin <i.kuchin@openproject.com>
2024-06-26 16:56:30 +0200

    find instance method module and acts_as_ method name dynamically
    To prevent copy pasting prbolem

--- lib_static/open_project/acts/favorable/registry.rb
+++ lib_static/open_project/acts/favorable/registry.rb
@@ -43,9 +43,12 @@ module OpenProject
         end
 
         def self.add(*models)
+          instance_methods_module = module_parent.const_get(:InstanceMethods)
+          acts_as_method_name = "acts_as_#{module_parent_name.demodulize.underscore}"
+
           models.each do |model|
-            unless model.ancestors.include?(::OpenProject::Acts::Favorable::InstanceMethods)
-              raise ArgumentError.new("Model #{model} does not include acts_as_favorable")
+            unless model.ancestors.include?(instance_methods_module)
+              raise ArgumentError.new("Model #{model} does not include #{acts_as_method_name}")
             end
 
             self.models << model
