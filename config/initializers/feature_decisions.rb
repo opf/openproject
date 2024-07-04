68f2d77f1123152330b8102997aa4aef1f41e2dd
Klaus Zanders <k.zanders@openproject.com>
2024-07-04 11:18:09 +0200

    Remove feature decision for sharing queries

--- config/initializers/feature_decisions.rb
+++ config/initializers/feature_decisions.rb
@@ -40,4 +40,3 @@ require_relative "../../lib_static/open_project/feature_decisions"
 #   end
 OpenProject::FeatureDecisions.add :favorite_projects
 OpenProject::FeatureDecisions.add :dark_mode
-OpenProject::FeatureDecisions.add :project_list_sharing
