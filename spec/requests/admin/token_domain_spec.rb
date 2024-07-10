0100057231eb22ec3ffea469846bcc267f2adfbf
Oliver Günther <mail@oliverguenther.de>
2024-07-10 13:31:05 +0200

    Adapt token spec to use test.host as domain

--- spec/requests/admin/token_domain_spec.rb
+++ spec/requests/admin/token_domain_spec.rb
@@ -29,10 +29,10 @@
 require "spec_helper"
 
 RSpec.describe "Enterprise Edition token domain",
-               :skip_csrf, type: :rails_request, with_settings: { host_name: "localhost:3001" } do
+               :skip_csrf, type: :rails_request do
   let(:current_user) { create(:admin) }
-  let(:valid_token) { Rails.root.join("spec/fixtures/ee_tokens/v2_1_user_localhost_3001.token").read }
-  let(:invalid_token) { Rails.root.join("spec/fixtures/ee_tokens/v2_1_user_localhost_3000.token").read }
+  let(:valid_token) { Rails.root.join("spec/fixtures/ee_tokens/v2_1_user_test_host.token").read }
+  let(:invalid_token) { Rails.root.join("spec/fixtures/ee_tokens/v2_1_user_localhost_3001.token").read }
   let(:ee_token) { valid_token }
 
   before do
