00ac5c229877828b0a494fd1c8c7b25a84cb9e4f
Oliver Günther <mail@oliverguenther.de>
2024-07-18 19:36:01 +0200

    use turbo-action advance for split view links

--- spec/support/pages/work_packages/primerized_split_work_package.rb
+++ spec/support/pages/work_packages/primerized_split_work_package.rb
@@ -26,18 +26,56 @@
 # See COPYRIGHT and LICENSE files for more details.
 #++
 
-require "support/pages/page"
-require "support/pages/work_packages/split_work_package"
+require "support/pages/work_packages/abstract_work_package"
+require "support/pages/work_packages/split_work_package_create"
 
 module Pages
-  module Notifications
-    class SplitScreen < ::Pages::SplitWorkPackage
-      include ::Components::Autocompleter::NgSelectAutocompleteHelpers
+  class SplitWorkPackage < Pages::AbstractWorkPackage
+    attr_reader :selector
 
-      def initialize(work_package, project = nil)
-        super
-        @selector = ".work-packages--details"
+    def initialize(work_package, project = nil)
+      super
+      @selector = ".work-packages--details"
+    end
+
+    def switch_to_fullscreen
+      find(".work-packages--details-fullscreen-icon").click
+      FullWorkPackage.new(work_package, project)
+    end
+
+    def expect_closed
+      expect(page).to have_no_selector(@selector)
+    end
+
+    def expect_open
+      wait_for_reload if using_cuprite?
+      expect(page).to have_selector(@selector)
+      expect_subject
+    end
+
+    def close
+      find(".work-packages--details-close-icon").click
+    end
+
+    def container
+      find(@selector)
+    end
+
+    protected
+
+    def path(tab = "overview")
+      state = "#{work_package.id}/#{tab}"
+
+      if project
+        project_work_packages_path(project, "details/#{state}")
+      else
+        details_work_packages_path(state)
       end
+    end
+
+    def create_page(args)
+      args.merge!(project: project || work_package.project)
+      SplitWorkPackageCreate.new(**args)
     end
   end
 end
