f009ecb801b660f78381e106a4b9236cced49798
Ivan Kuchin <i.kuchin@openproject.com>
2024-07-24 16:25:31 +0200

    sort register_journal_formatter lines and remove duplicate

--- app/models/journal.rb
+++ app/models/journal.rb
@@ -35,30 +35,28 @@ class Journal < ApplicationRecord
   include ::Acts::Journalized::FormatHooks
   include Journal::Timestamps
 
-  register_journal_formatter OpenProject::JournalFormatter::Diff
-  register_journal_formatter OpenProject::JournalFormatter::Attachment
-  register_journal_formatter OpenProject::JournalFormatter::CustomField
-  register_journal_formatter OpenProject::JournalFormatter::ScheduleManually
-  register_journal_formatter OpenProject::JournalFormatter::IgnoreNonWorkingDays
   register_journal_formatter OpenProject::JournalFormatter::ActiveStatus
-  register_journal_formatter OpenProject::JournalFormatter::ProjectStatusCode
-  register_journal_formatter OpenProject::JournalFormatter::Template
-  register_journal_formatter OpenProject::JournalFormatter::Visibility
-  register_journal_formatter OpenProject::JournalFormatter::SubprojectNamedAssociation
-  register_journal_formatter OpenProject::JournalFormatter::TimeEntryHours
-  register_journal_formatter OpenProject::JournalFormatter::WikiDiff
-  register_journal_formatter OpenProject::JournalFormatter::TimeEntryNamedAssociation
-  register_journal_formatter OpenProject::JournalFormatter::Cause
-  register_journal_formatter OpenProject::JournalFormatter::FileLink
-  register_journal_formatter OpenProject::JournalFormatter::MeetingStartTime
-  register_journal_formatter OpenProject::JournalFormatter::AgendaItemPosition
+  register_journal_formatter OpenProject::JournalFormatter::AgendaItemDiff
   register_journal_formatter OpenProject::JournalFormatter::AgendaItemDuration
-  register_journal_formatter OpenProject::JournalFormatter::AgendaItemDiff
+  register_journal_formatter OpenProject::JournalFormatter::AgendaItemPosition
   register_journal_formatter OpenProject::JournalFormatter::AgendaItemTitle
-  register_journal_formatter OpenProject::JournalFormatter::MeetingWorkPackageId
+  register_journal_formatter OpenProject::JournalFormatter::Attachment
+  register_journal_formatter OpenProject::JournalFormatter::Cause
+  register_journal_formatter OpenProject::JournalFormatter::CustomField
+  register_journal_formatter OpenProject::JournalFormatter::Diff
+  register_journal_formatter OpenProject::JournalFormatter::FileLink
+  register_journal_formatter OpenProject::JournalFormatter::IgnoreNonWorkingDays
+  register_journal_formatter OpenProject::JournalFormatter::MeetingStartTime
   register_journal_formatter OpenProject::JournalFormatter::MeetingState
-  register_journal_formatter OpenProject::JournalFormatter::AgendaItemDiff
-  register_journal_formatter OpenProject::JournalFormatter::AgendaItemTitle
+  register_journal_formatter OpenProject::JournalFormatter::MeetingWorkPackageId
+  register_journal_formatter OpenProject::JournalFormatter::ProjectStatusCode
+  register_journal_formatter OpenProject::JournalFormatter::ScheduleManually
+  register_journal_formatter OpenProject::JournalFormatter::SubprojectNamedAssociation
+  register_journal_formatter OpenProject::JournalFormatter::Template
+  register_journal_formatter OpenProject::JournalFormatter::TimeEntryHours
+  register_journal_formatter OpenProject::JournalFormatter::TimeEntryNamedAssociation
+  register_journal_formatter OpenProject::JournalFormatter::Visibility
+  register_journal_formatter OpenProject::JournalFormatter::WikiDiff
 
   # Attributes related to the cause are stored in a JSONB column so we can easily add new relations and related
   # attributes without a heavy database migration. Fields will be prefixed with `cause_` but are stored in the JSONB
