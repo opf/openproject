118b911e496464bfa4bc54a197e70527eb940307
Pavel Balashou <ba1ashpash@gmail.com>
2024-07-24 11:10:34 +0200

    Refactor complex authenticate method.
    - Merge fail and return.
    - Use guard clauses to reduce nesting a bit.

--- lib_static/open_project/authentication/strategies/warden/jwt_oidc.rb
+++ lib_static/open_project/authentication/strategies/warden/jwt_oidc.rb
@@ -16,7 +16,7 @@ module OpenProject
               ::Doorkeeper::Grape::AuthorizationDecorator.new(request)
             )
             return false if @access_token.blank?
-                
+
             @unverified_payload, @unverified_header = JWT.decode(@access_token, nil, false)
             @unverified_header.present? && @unverified_payload.present?
           rescue JWT::DecodeError
@@ -26,53 +26,46 @@ module OpenProject
           def authenticate!
             issuer = @unverified_payload["iss"]
             provider = OpenProject::OpenIDConnect.providers.find { |p| p.configuration[:issuer] == issuer } if issuer.present?
-            if provider.present?
-              client_id = provider.configuration.fetch(:identifier)
-              alg = @unverified_header.fetch("alg")
-              key =
-                if SUPPORTED_ALG.include?(alg)
-                  kid = @unverified_header.fetch("kid")
-                  jwks_uri = provider.configuration[:jwks_uri]
-                  JSON::JWK::Set::Fetcher.fetch(jwks_uri, kid:).to_key
-                else
-                  fail_with_header!(error: "invalid_token", error_description: "Token signature algorithm is not supported")
-                  return
-                end
-              begin
-                verified_payload, = JWT.decode(
-                  @access_token,
-                  key,
-                  true,
-                  {
-                    algorithm: alg,
-                    verify_iss: true,
-                    verify_aud: true,
-                    iss: issuer,
-                    aud: client_id,
-                    required_claims: ["sub", "iss", "aud"]
-                  }
-                )
-              rescue JWT::ExpiredSignature
-                fail_with_header!(error: "invalid_token", error_description: "The access token expired")
-                return
-              rescue JWT::ImmatureSignature
-                # happens when nbf time is less than current
-                fail_with_header!(error: "invalid_token", error_description: "The access token is used too early")
-
-                return
-              rescue JWT::InvalidIssuerError
-                fail_with_header!(error: "invalid_token", error_description: "The access token issuer is wrong")
-                return
-              rescue JWT::InvalidAudError
-                fail_with_header!(error: "invalid_token", error_description: "The access token audience claim is wrong")
-                return
-              end
-
-              user = User.find_by(identity_url: "#{provider.name}:#{verified_payload['sub']}")
-              success!(user) if user
-            else
-              fail_with_header!(error: "invalid_token", error_description: "The access token issuer is unknown")
+            if provider.blank?
+              return fail_with_header!(error: "invalid_token", error_description: "The access token issuer is unknown")
             end
+
+            client_id = provider.configuration.fetch(:identifier)
+            alg = @unverified_header.fetch("alg")
+            if SUPPORTED_ALG.exclude?(alg)
+              return fail_with_header!(error: "invalid_token", error_description: "Token signature algorithm is not supported")
+            end
+
+            kid = @unverified_header.fetch("kid")
+            jwks_uri = provider.configuration[:jwks_uri]
+            key = JSON::JWK::Set::Fetcher.fetch(jwks_uri, kid:).to_key
+            begin
+              verified_payload, = JWT.decode(
+                @access_token,
+                key,
+                true,
+                {
+                  algorithm: alg,
+                  verify_iss: true,
+                  verify_aud: true,
+                  iss: issuer,
+                  aud: client_id,
+                  required_claims: ["sub", "iss", "aud"]
+                }
+              )
+            rescue JWT::ExpiredSignature
+              return fail_with_header!(error: "invalid_token", error_description: "The access token expired")
+            rescue JWT::ImmatureSignature
+              # happens when nbf time is less than current
+              return fail_with_header!(error: "invalid_token", error_description: "The access token is used too early")
+            rescue JWT::InvalidIssuerError
+              return fail_with_header!(error: "invalid_token", error_description: "The access token issuer is wrong")
+            rescue JWT::InvalidAudError
+              return fail_with_header!(error: "invalid_token", error_description: "The access token audience claim is wrong")
+            end
+
+            user = User.find_by(identity_url: "#{provider.name}:#{verified_payload['sub']}")
+            success!(user) if user
           end
         end
       end
