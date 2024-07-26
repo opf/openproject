e22d5ad78fb78420b6e798b2f214ccae3307ce86
Aaron Contreras <aaronlcaq@gmail.com>
2024-07-26 11:47:49 -0500

    Ensure meetings are displayed in the right order based on filter

--- modules/meeting/app/controllers/work_package_meetings_tab_controller.rb
+++ modules/meeting/app/controllers/work_package_meetings_tab_controller.rb
@@ -127,9 +127,20 @@ class WorkPackageMeetingsTabController < ApplicationController
         .includes(:meeting)
         .where(meeting_id: Meeting.visible(current_user))
         .where(work_package_id: @work_package.id)
-        .order("meetings.start_time": :asc)
+        .order(sort_clause(direction))
 
     comparison = direction == :past ? "<" : ">="
     agenda_items.where("meetings.start_time + (interval '1 hour' * meetings.duration) #{comparison} ?", Time.zone.now)
+  end
+
+  def sort_clause(direction)
+    case direction
+    when :upcoming
+      "meetings.start_time ASC"
+    when :past
+      "meetings.start_time DESC"
+    else
+      raise ArgumentError, "Invalid direction: #{direction}. Must be one of :upcoming or :past."
+    end
   end
 end
