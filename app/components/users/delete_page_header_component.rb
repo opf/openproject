c623aacaab76906d5ece16cbe646ffad3e61f238
Behrokh Satarnejad <b.satarnejad@openproject.com>
2024-07-28 19:11:26 +0200

    add page header component to 2fa new pages and chnage the name of delete account page header component

--- app/components/users/delete_page_header_component.rb
+++ app/components/users/delete_page_header_component.rb
@@ -28,7 +28,7 @@
 # See COPYRIGHT and LICENSE files for more details.
 # ++
 
-class Users::DeleteAccountPageHeaderComponent < ApplicationComponent
+class Users::DeletePageHeaderComponent < ApplicationComponent
   include OpPrimer::ComponentHelpers
   include ApplicationHelper
 
