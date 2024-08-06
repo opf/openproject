f3024059eaeeaa72cbecf44c3742044adbcf3491
Oliver Günther <mail@oliverguenther.de>
2024-08-06 10:28:22 +0200

    Remove excessive stubbing in activity filter spec

--- modules/costs/spec/models/queries/time_entries/filters/activity_filter_spec.rb
+++ modules/costs/spec/models/queries/time_entries/filters/activity_filter_spec.rb
@@ -29,37 +29,10 @@
 require "spec_helper"
 
 RSpec.describe Queries::TimeEntries::Filters::ActivityFilter do
-  let(:time_entry_activity1) { build_stubbed(:time_entry_activity) }
-  let(:time_entry_activity2) { build_stubbed(:time_entry_activity) }
-  let(:activities) { [time_entry_activity1, time_entry_activity2] }
+  shared_let(:activities) { create_list(:time_entry_activity, 2) }
+
   let(:plucked_allowed_values) do
     activities.map { |x| [x.name, x.id] }
-  end
-
-  before do
-    allow(TimeEntryActivity)
-      .to receive(:shared)
-      .and_return(activities)
-
-    allow(TimeEntryActivity)
-      .to receive_message_chain(:where, :or)
-      .and_return(activities)
-
-    without_partial_double_verification do
-      allow(activities)
-        .to receive(:where)
-        .and_return(activities)
-
-      allow(activities)
-        .to receive(:pluck)
-        .with(:id)
-        .and_return(activities.map(&:id))
-
-      allow(activities)
-        .to receive(:pluck)
-        .with(:name, :id)
-        .and_return(activities.map { |x| [x.name, x.id] })
-    end
   end
 
   it_behaves_like "basic query filter" do
