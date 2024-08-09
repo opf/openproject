36dd613e23f7675c754025dfdb76ab5f1808897c
Marcello Rocha <m.rocha@openproject.com>
2024-08-09 18:39:06 +0200

    Appease rubocop on has_many

--- app/models/user.rb
+++ app/models/user.rb
@@ -90,7 +90,7 @@ class User < Principal
            inverse_of: :user,
            dependent: :destroy
 
-  has_many :remote_identities
+  has_many :remote_identities, dependent: :destroy
 
   # Users blocked via brute force prevention
   # use lambda here, so time is evaluated on each query
