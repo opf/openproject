7ef82329e7d0aa412615f9458516a5db6f96b922
Oliver Günther <mail@oliverguenther.de>
2024-08-12 21:47:05 +0200

    Render split screen using turbo-frames

--- frontend/src/turbo/setup.ts
+++ frontend/src/turbo/setup.ts
@@ -4,6 +4,7 @@ import TurboPower from 'turbo_power';
 import { registerDialogStreamAction } from './dialog-stream-action';
 import { addTurboEventListeners } from './turbo-event-listeners';
 import { registerFlashStreamAction } from './flash-stream-action';
+import { applyTurboNavigationPatch } from './turbo-navigation-patch';
 
 // Disable default turbo-drive for now as we don't need it for now AND it breaks angular routing
 Turbo.session.drive = false;
@@ -14,6 +15,10 @@ Turbo.start();
 addTurboEventListeners();
 registerDialogStreamAction();
 registerFlashStreamAction();
+
+// Apply navigational patch
+// https://github.com/hotwired/turbo/issues/1300
+applyTurboNavigationPatch();
 
 // Register turbo power actions
 TurboPower.initialize(Turbo.StreamActions);
