d67afc69e998904bc02b841b8edd13592eff21bd
Kabiru Mwenja <makabby@gmail.com>
2024-08-12 14:31:03 +0300

    [#57059] Confusing UX when cancelling editing Nextcloud OAuth settings
    https://community.openproject.org/work_packages/57059

--- modules/storages/spec/components/storages/admin/oauth_client_info_component_spec.rb
+++ modules/storages/spec/components/storages/admin/oauth_client_info_component_spec.rb
@@ -38,7 +38,7 @@ RSpec.describe Storages::Admin::OAuthClientInfoComponent, type: :component do # 
         component = described_class.new(storage:, oauth_client: storage.oauth_client)
         expect(component.edit_icon_button_options)
           .to include(icon: :sync,
-                      data: { confirm:
+                      data: { turbo_confirm:
                                 "This action will reset the current OAuth credentials. After confirming you will " \
                                 "have to enter new credentials from the storage provider and all users will have " \
                                 "to authorize against Nextcloud again. Are you sure you want to proceed?",
