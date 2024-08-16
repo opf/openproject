9e9b27c8c55f07b3f912e5e933379963ec9bfa79
Oliver Günther <mail@oliverguenther.de>
2024-08-16 07:55:02 +0200

    Rename work package actions
    https://community.openproject.org/work_packages/56900

--- spec/features/work_packages/copy_spec.rb
+++ spec/features/work_packages/copy_spec.rb
@@ -135,7 +135,7 @@ RSpec.describe "Work package copy", :js, :selenium do
 
       # Go to add cost entry page
       find("#action-show-more-dropdown-menu .button").click
-      find(".menu-item", text: "Copy", exact_text: true).click
+      find(".menu-item", text: "Duplicate", exact_text: true).click
 
       to_copy_work_package_page = Pages::FullWorkPackageCreate.new(original_work_package:)
       to_copy_work_package_page.update_attributes Description: "Copied WP Description"
