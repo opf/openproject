54cdb68d6138ecf9b46fd45b662583ca7d32bc0f
Ben Tey <ben.tey@outlook.com>
2024-08-23 15:05:25 -0400

    Changes from v14.4.0.

---
+++ app/components/projects/settings/project_custom_field_sections/custom_field_row_component.rb
@@ -0,0 +1,59 @@
+#-- copyright
+# OpenProject is an open source project management software.
+# Copyright (C) 2012-2023 the OpenProject GmbH
+#
+# This program is free software; you can redistribute it and/or
+# modify it under the terms of the GNU General Public License version 3.
+#
+# OpenProject is a fork of ChiliProject, which is a fork of Redmine. The copyright follows:
+# Copyright (C) 2006-2013 Jean-Philippe Lang
+# Copyright (C) 2010-2013 the ChiliProject Team
+#
+# This program is free software; you can redistribute it and/or
+# modify it under the terms of the GNU General Public License
+# as published by the Free Software Foundation; either version 2
+# of the License, or (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+#
+# See COPYRIGHT and LICENSE files for more details.
+#++
+
+module Projects
+  module Settings
+    module ProjectCustomFieldSections
+      class CustomFieldRowComponent < ApplicationComponent
+        include ApplicationHelper
+        include OpPrimer::ComponentHelpers
+        include OpTurbo::Streamable
+
+        def initialize(project:, project_custom_field:)
+          super
+
+          @project = project
+          @project_custom_field = project_custom_field
+          @project_custom_field_project_mappings = project.project_custom_field_project_mappings
+        end
+
+        private
+
+        def wrapper_uniq_by
+          @project_custom_field.id
+        end
+
+        def active_in_project?
+          @project_custom_field_project_mappings.any? do |mapping|
+            mapping.custom_field_id == @project_custom_field.id
+          end
+        end
+      end
+    end
+  end
+end
---
+++ app/components/settings/project_custom_field_sections/index_component.rb
@@ -0,0 +1,71 @@
+#-- copyright
+# OpenProject is an open source project management software.
+# Copyright (C) 2012-2023 the OpenProject GmbH
+#
+# This program is free software; you can redistribute it and/or
+# modify it under the terms of the GNU General Public License version 3.
+#
+# OpenProject is a fork of ChiliProject, which is a fork of Redmine. The copyright follows:
+# Copyright (C) 2006-2013 Jean-Philippe Lang
+# Copyright (C) 2010-2013 the ChiliProject Team
+#
+# This program is free software; you can redistribute it and/or
+# modify it under the terms of the GNU General Public License
+# as published by the Free Software Foundation; either version 2
+# of the License, or (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+#
+# See COPYRIGHT and LICENSE files for more details.
+#++
+
+module Settings
+  module ProjectCustomFieldSections
+    class IndexComponent < ApplicationComponent
+      include ApplicationHelper
+      include OpPrimer::ComponentHelpers
+      include OpTurbo::Streamable
+
+      def initialize(project_custom_field_sections:)
+        super
+
+        @project_custom_field_sections = project_custom_field_sections
+      end
+
+      def first_and_last
+        [@project_custom_field_sections.first, @project_custom_field_sections.last]
+      end
+
+      private
+
+      def wrapper_data_attributes
+        {
+          controller: "generic-drag-and-drop",
+          "application-target": "dynamic"
+        }
+      end
+
+      def drop_target_config
+        {
+          "is-drag-and-drop-target": true,
+          "target-allowed-drag-type": "section" # the type of dragged items which are allowed to be dropped in this target
+        }
+      end
+
+      def draggable_item_config(section)
+        {
+          "draggable-id": section.id,
+          "draggable-type": "section",
+          "drop-url": drop_admin_settings_project_custom_field_section_path(section)
+        }
+      end
+    end
+  end
+end
