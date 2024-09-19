7d396c8c22302b109b735dd7e84a191c6290262e
Oliver Günther <mail@oliverguenther.de>
2024-09-19 16:03:14 +0200

    Deal with multiple flashes

--- frontend/src/turbo/flash-stream-action.ts
+++ frontend/src/turbo/flash-stream-action.ts
@@ -3,7 +3,8 @@ import { StreamActions, StreamElement } from '@hotwired/turbo';
 export function registerFlashStreamAction() {
   StreamActions.flash = function dialogStreamAction(this:StreamElement) {
     const content = this.templateElement.content;
-    const target = document.getElementById('flash-messages');
-    target?.append(content);
+    const target = document.getElementById('banner-messages') as HTMLElement;
+    target.innerHTML = '';
+    target.append(content);
   };
 }
--- modules/meeting/spec/support/pages/structured_meeting/show.rb
+++ modules/meeting/spec/support/pages/structured_meeting/show.rb
@@ -43,8 +43,6 @@ module Pages::StructuredMeeting
 
     def trigger_change_poll
       script = <<~JS
-        // Remove flashes from the page to prevent race conditions
-        document.querySelectorAll('.op-toast--wrapper').forEach((el) => el.remove());
         var target = document.querySelector('[data-test-selector="meeting-page-header"]');
         var controller = window.Stimulus.getControllerForElementAndIdentifier(target, 'poll-for-changes')
         controller.triggerTurboStream();
