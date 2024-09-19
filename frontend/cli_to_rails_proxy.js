8c70afc6a35c340b600cb06287307768571d39f5
Markus Kahl <machisuji@gmail.com>
2024-09-19 11:17:18 +0100

    Update frontend/cli_to_rails_proxy.js
    Co-authored-by: Ivan Kuchin <i.kuchin@openproject.com>

--- frontend/cli_to_rails_proxy.js
+++ frontend/cli_to_rails_proxy.js
@@ -1,4 +1,4 @@
-const PROXY_HOSTNAME = process.env.PROXY_HOSTNAME || 'localhost';
+const PROXY_HOSTNAME = process.env.PROXY_HOSTNAME || process.env.HOST || 'localhost';
 const PORT = process.env.PORT || '3000';
 
 const PROXY_CONFIG = [
