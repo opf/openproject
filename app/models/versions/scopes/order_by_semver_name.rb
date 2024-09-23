48d78777ceda5abee63b1cc087c4bb76366f4ef3
Ivan Kuchin <i.kuchin@openproject.com>
2024-09-23 13:07:29 +0200

    simplify Version.order_by_semver_name
    Also use order instead of reorder

--- app/models/versions/scopes/order_by_semver_name.rb
+++ app/models/versions/scopes/order_by_semver_name.rb
@@ -32,21 +32,7 @@ module Versions::Scopes
 
     class_methods do
       def order_by_semver_name
-        reorder semver_sql, :name
-      end
-
-      # Returns an sql for ordering which:
-      # * Returns a substring from the beginning of the name up until the first alphabetical character e.g. "1.2.3 "
-      #   from "1.2.3 ABC"
-      # * Replaces all non numerical character groups in that substring by a blank, e.g "1.2.3 " to "1 2 3 "
-      # * Splits the result into an array of individual number parts, e.g. "{1, 2, 3, ''}" from "1 2 3 "
-      # * removes all empty array items, e.g. "{1, 2, 3}" from "{1, 2, 3, ''}"
-      def semver_sql(table_name = Version.table_name)
-        sql = <<~SQL
-          array_remove(regexp_split_to_array(regexp_replace(substring(#{table_name}.name from '^[^a-zA-Z]+'), '\\D+', ' ', 'g'), ' '), '')::int[]
-        SQL
-
-        Arel.sql(sql)
+        order :name
       end
     end
   end
