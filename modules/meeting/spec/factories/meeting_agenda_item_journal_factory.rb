6f60ef737759fa52029977efa881bde5b8027f9a
Christophe Bliard <c.bliard@openproject.com>
2024-09-23 18:07:02 +0200

    Ensure author_id of Journal::MeetingAgendaItemJournal are rewritten too

--- modules/meeting/spec/factories/meeting_agenda_item_journal_factory.rb
+++ modules/meeting/spec/factories/meeting_agenda_item_journal_factory.rb
@@ -1,5 +1,3 @@
-# frozen_string_literal: true
-
 #-- copyright
 # OpenProject is an open source project management software.
 # Copyright (C) the OpenProject GmbH
@@ -28,34 +26,6 @@
 # See COPYRIGHT and LICENSE files for more details.
 #++
 
-require "spec_helper"
-require_module_spec_helper
-require Rails.root.join("spec/services/principals/replace_references_context")
-
-RSpec.describe Principals::ReplaceReferencesService, "#call", type: :model do
-  subject(:service_call) { instance.call(from: principal, to: to_principal) }
-
-  shared_let(:other_user) { create(:user, firstname: "other user") }
-  shared_let(:principal) { create(:user, firstname: "old principal") }
-  shared_let(:to_principal) { create(:user, firstname: "new principal") }
-
-  let(:instance) do
-    described_class.new
-  end
-
-  context "with MeetingAgendaItem" do
-    it_behaves_like "rewritten record",
-                    :meeting_agenda_item,
-                    :author_id
-
-    it_behaves_like "rewritten record",
-                    :meeting_agenda_item,
-                    :presenter_id
-  end
-
-  context "with Journal::MeetingAgendaItemJournal" do
-    it_behaves_like "rewritten record",
-                    :journal_meeting_agenda_item_journal,
-                    :author_id
-  end
+FactoryBot.define do
+  factory :journal_meeting_agenda_item_journal, class: "Journal::MeetingAgendaItemJournal"
 end
