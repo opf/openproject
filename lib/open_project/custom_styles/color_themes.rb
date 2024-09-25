787f1c0f7b83463bf79ff079bc8bf9527984419a
Behrokh Satarnejad <b.satarnejad@openproject.com>
2024-09-25 11:49:04 +0200

    fix failing tests

--- lib/open_project/custom_styles/color_themes.rb
+++ lib/open_project/custom_styles/color_themes.rb
@@ -30,7 +30,7 @@ module OpenProject::CustomStyles
   module ColorThemes
     module_function
 
-    OpenProject::CustomStyles::ColorThemes::DEFAULT_THEME_NAME = "OpenProject default".freeze
+    OpenProject::CustomStyles::ColorThemes::DEFAULT_THEME_NAME = "OpenProject (default)".freeze
 
     DEPRECATED_ALTERNATIVE_COLOR = "#35C53F".freeze
     DEPRECATED_PRIMARY_COLOR = "#1A67A3".freeze
@@ -61,7 +61,7 @@ module OpenProject::CustomStyles
         }
       },
       {
-        theme: "OpenProject Grey",
+        theme: "OpenProject Gray",
         colors: {
           "primary-button-color" => PRIMER_PRIMARY_BUTTON_COLOR,
           "accent-color" => ACCENT_COLOR,
