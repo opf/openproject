fdcfa07b888cb7adf553cc3f4a51b872c932cc41
Henriette Darge <h.darge@openproject.com>
2024-10-08 09:55:33 +0200

    Add HighlightedDate to WorkPackage HoverCard

--- app/components/work_packages/highlighted_type_component.rb
+++ app/components/work_packages/highlighted_type_component.rb
@@ -7,10 +7,10 @@ class WorkPackages::HighlightedTypeComponent < ApplicationComponent
     super
 
     @type = work_package.type
-    @system_arguments = system_arguments
+    @system_arguments = system_arguments.merge({ classes: "__hl_inline_type_#{@type.id}" })
   end
 
   def call
-    render(Primer::Beta::Text.new(classes: "__hl_inline_type_#{@type.id}"), **@system_arguments) { @type.name.upcase }
+    render(Primer::Beta::Text.new(**@system_arguments)) { @type.name.upcase }
   end
 end
