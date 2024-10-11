561e2a55660f58e4799532aa929bb72127e7dfc0
Eric Schubert <e.schubert@openproject.com>
2024-10-11 12:43:22 +0200

    [#57816] fixed contract unit test

--- spec/contracts/custom_fields/hierarchy/insert_item_contract_spec.rb
+++ spec/contracts/custom_fields/hierarchy/insert_item_contract_spec.rb
@@ -67,7 +67,7 @@ RSpec.describe CustomFields::Hierarchy::InsertItemContract do
       it "is invalid" do
         result = subject.call(params)
         expect(result).to be_failure
-        expect(result.errors.to_h).to include(label: ["Label must be unique within the same hierarchy level"])
+        expect(result.errors.to_h).to include(label: ["must be unique within the same hierarchy level"])
       end
     end
 
