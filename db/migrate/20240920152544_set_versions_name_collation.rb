340cf98092944379f0718b65a8f6c44bd718bbaa
Ivan Kuchin <i.kuchin@openproject.com>
2024-10-16 20:01:50 +0200

    use root instead of English specific collation

--- db/migrate/20240920152544_set_versions_name_collation.rb
+++ db/migrate/20240920152544_set_versions_name_collation.rb
@@ -1,7 +1,7 @@
 class SetVersionsNameCollation < ActiveRecord::Migration[7.1]
   def up
     execute <<-SQL.squish
-      CREATE COLLATION IF NOT EXISTS versions_name (provider = icu, locale = 'en-u-kn-true');
+      CREATE COLLATION IF NOT EXISTS versions_name (provider = icu, locale = 'und-u-kn-true');
     SQL
 
     change_column :versions, :name, :string, collation: "versions_name"
