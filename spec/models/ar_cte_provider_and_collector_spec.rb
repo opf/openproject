e8eac5176362c2023b9bbfb1022253e3eccbe153
ulferts <jens.ulferts@googlemail.com>
2025-10-30 15:33:21 +0100

    generate skeleton on CTE aggregation

---
+++ spec/models/ar_cte_provider_and_collector_spec.rb
@@ -0,0 +1,69 @@
+# frozen_string_literal: true
+
+# -- copyright
+# OpenProject is an open source project management software.
+# Copyright (C) the OpenProject GmbH
+#
+# This program is free software; you can redistribute it and/or
+# modify it under the terms of the GNU General Public License version 3.
+#
+# OpenProject is a fork of ChiliProject, which is a fork of Redmine. The copyright follows:
+# Copyright (C) 2006-2013 Jean-Philippe Lang
+# Copyright (C) 2010-2013 the ChiliProject Team
+#
+# This program is free software; you can redistribute it and/or
+# modify it under the terms of the GNU General Public License
+# as published by the Free Software Foundation; either version 2
+# of the License, or (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+#
+# See COPYRIGHT and LICENSE files for more details.
+# ++
+require "spec_helper"
+
+RSpec.describe "ActiveRecord CTE provider and collector" do # rubocop:disable RSpec/DescribeClass
+  create_shared_association_defaults_for_work_package_factory
+
+  shared_let(:work_package_in_cte) { create(:work_package) }
+  shared_let(:work_package_not_in_cte) { create(:work_package) }
+
+  before do
+    OpenProject::ActiveRecordExtensions::Cte::Aggregation.register :cte_aggregation_spec,
+                                                                   <<~SQL.squish
+                                                                     SELECT #{work_package_in_cte.id} id
+                                                                   SQL
+  end
+
+  after do
+    OpenProject::ActiveRecordExtensions::Cte::Aggregation.deregister :cte_aggregation_spec
+  end
+
+  it "returns the value from the CTE" do
+    # Desired SQL
+    #
+    # WITH cte_aggregation_spec AS (
+    #   SELECT :id id
+    # )
+    #
+    # SELECT *
+    # FROM work_packages
+    # WHERE work_packages.id IN (SELECT id FROM cte_aggregation_spec)
+
+    provider = OpenProject::ActiveRecordExtensions::CteProvider.new(with: :cte_aggregation_spec,
+                                                                    on: WorkPackage)
+    scope = WorkPackage.where(id: provider.select(:id))
+
+    collected_scope = OpenProject::ActiveRecordExtensions::CteCollector.new(on: scope)
+
+    expect(collected_scope)
+      .to contain_exactly(work_package_in_cte)
+  end
+end
