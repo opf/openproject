523c4ba989b40424c7c32e2eb7db9fce15af83b6
Yauheni Suhakou <shiroginne@users.noreply.github.com>
2026-06-09 16:39:10 +0200

    Update base_query.rb

--- modules/wikis/app/services/wikis/adapters/base_query.rb
+++ modules/wikis/app/services/wikis/adapters/base_query.rb
@@ -60,7 +60,7 @@ module Wikis::Adapters
 
     def canonical_page_info(identifier:, auth_strategy:)
       Input::PageInfo.build(identifier:).bind do |input_data|
-        Internal::CanonicalPageInfo.new(model: provider).call(input_data:, auth_strategy:)
+        Providers::XWiki::Queries::Internal::CanonicalPageInfo.new(model: provider).call(input_data:, auth_strategy:)
       end
     end
   end
