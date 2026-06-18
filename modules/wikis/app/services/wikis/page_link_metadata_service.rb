bff67b956fefd4f3621ac237a663bf3c5152af78
Marcello Rocha <m.rocha@openproject.com>
2026-06-18 15:55:24 +0200

    appease the rubocop gods

--- modules/wikis/app/services/wikis/page_link_metadata_service.rb
+++ modules/wikis/app/services/wikis/page_link_metadata_service.rb
@@ -40,10 +40,7 @@ module Wikis
     def call
       metadata = relation.group_by(&:provider).flat_map do |provider, page_links|
         build_inputs(page_links).filter_map do |(page_link_id, input_data)|
-          provider.auth_strategy_for(User.current).bind do |auth_strategy|
-            provider.resolve("queries.page_info").call(input_data:, auth_strategy:)
-                    .either(->(info) { [page_link_id, info] }, ->(*) {})
-          end
+          request_page_info(input_data, provider).either(->(info) { [page_link_id, info.title] }, ->(*) {})
         end
       end
 
@@ -55,6 +52,12 @@ module Wikis
 
     attr_reader :relation
 
+    def request_page_info(input_data, provider)
+      provider.auth_strategy_for(User.current).bind do |auth_strategy|
+        provider.resolve("queries.page_info").call(input_data:, auth_strategy:)
+      end
+    end
+
     def build_inputs(page_links)
       page_links.filter_map do |page_link|
         [page_link.id, Adapters::Input::PageInfo.build(identifier: page_link.identifier).value_or(nil)]
@@ -62,14 +65,13 @@ module Wikis
     end
 
     def enrich_models(metadata)
-      identifier_title_map = metadata.map { |(id, info)| [id, info.title] }
-      variable_placeholders = Array.new(identifier_title_map.size, "(?,?)").join(",")
+      variable_placeholders = Array.new(metadata.size, "(?,?)").join(",")
       join_string = <<~SQL.squish
         LEFT JOIN (VALUES #{variable_placeholders}) AS metadata(page_link_id, title)
           ON metadata.page_link_id = wiki_page_links.id
       SQL
 
-      join_expression = OpenProject::SqlSanitization.sanitize(join_string, *identifier_title_map.flatten)
+      join_expression = OpenProject::SqlSanitization.sanitize(join_string, *metadata.flatten)
 
       relation.joins(join_expression).select("wiki_page_links.*, metadata.title as title")
     end
