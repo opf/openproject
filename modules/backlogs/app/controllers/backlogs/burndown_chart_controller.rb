2ab04e46168f2ad2d10d1386086831c3503d27d2
Tobias Dillmann <github@pentokami.de>
2026-06-19 09:45:46 +0200

    [AGILE-296] Burndown chart controller: use recommended helper syntax

--- modules/backlogs/app/controllers/backlogs/burndown_chart_controller.rb
+++ modules/backlogs/app/controllers/backlogs/burndown_chart_controller.rb
@@ -32,7 +32,7 @@ module Backlogs
   class BurndownChartController < BaseController
     menu_item :backlogs
 
-    helper "backlogs/burndown_chart"
+    helper Backlogs::BurndownChartHelper
 
     def show
       @burndown = Burndown.new(@sprint, @project) if @sprint.date_range_set?
