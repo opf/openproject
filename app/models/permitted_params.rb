21f6f0497206e91e8987f68eb6b5b276fb14382e
Oliver Günther <mail@oliverguenther.de>
2026-07-07 11:32:39 +0200

    Fix wiki rename functionality to prevent other attribute changes
    https://community.openproject.org/work_packages/SC-254

--- app/models/permitted_params.rb
+++ app/models/permitted_params.rb
@@ -258,9 +258,7 @@ class PermittedParams
   end
 
   def wiki_page_rename
-    permitted = permitted_attributes(:wiki_page)
-
-    params.require(:page).permit(*permitted)
+    params.require(:page).permit(:title, :redirect_existing_links, :lock_version)
   end
 
   def wiki_page
