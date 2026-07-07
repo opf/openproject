f122b84212bffe4afa55967a9cc78f3514824d5b
Tomas Hykel <mail@thykel.cz>
2026-07-07 22:42:04 +0200

    uncomment the fixed test

--- modules/boards/spec/features/action_boards/version_board_spec.rb
+++ modules/boards/spec/features/action_boards/version_board_spec.rb
@@ -221,9 +221,8 @@ RSpec.describe "Version action board",
       work_package.reload
       expect(work_package.version).to eq(open_version)
 
-      # TODO: Temporary deactivation till PR#24158 lands
-      # board_page.expect_card("Open version", "Task 1", present: true)
-      # board_page.expect_card("A second version", "Task 1", present: false)
+      board_page.expect_card("Open version", "Task 1", present: true)
+      board_page.expect_card("A second version", "Task 1", present: false)
     end
 
     it "allows adding new and closed versions from within the board" do
