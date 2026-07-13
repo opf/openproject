e7f4e9d0a1bebce28e17fd0e80774b37e06aba3f
Bruno Pagno <b.pagno@openproject.com>
2026-07-13 12:02:52 +0200

    [COMMS-855] Sync Version & TargetVersions filters (#24150)
    * filter by target_versions
    
    * hide under feature flag and adjust specs
    
    * version filter behaves like target_versions filter
    
    * use target_versions on legacy version filter
    
    * fix broken spec
    
    * extract further logic to the target_verisons mixin

--- app/models/queries/work_packages/filter/target_versions_filter.rb
+++ app/models/queries/work_packages/filter/target_versions_filter.rb
@@ -30,97 +30,12 @@
 
 class Queries::WorkPackages::Filter::TargetVersionsFilter <
   Queries::WorkPackages::Filter::WorkPackageFilter
-  STATUS_BY_OPERATOR = { "o" => "open", "c" => "closed", "l" => "locked" }.freeze
-
-  def allowed_values
-    @allowed_values ||= versions.pluck(:id).map { |id| [id.to_s, id.to_s] }
-  end
-
-  def available_operators
-    [
-      Queries::Operators::EqualsOr,
-      Queries::Operators::NotEquals,
-      Queries::Operators::All,
-      Queries::Operators::None,
-      Queries::Operators::Versions::OpenStatus,
-      Queries::Operators::Versions::LockedStatus,
-      Queries::Operators::Versions::ClosedStatus
-    ]
-  end
+  include ::Queries::WorkPackages::Filter::FilterOnTargetVersionsMixin
 
   def self.key = :target_version_id
-  def type = :list_optional
   def human_name = WorkPackage.human_attribute_name("target_versions")
-  def ar_object_filter? = true
 
   def available?
     Setting::WorkPackageMultipleVersions.active?
-  end
-
-  def operator_strategy
-    case operator
-    when "o"
-      Queries::Operators::Versions::OpenStatus
-    when "c"
-      Queries::Operators::Versions::ClosedStatus
-    when "l"
-      Queries::Operators::Versions::LockedStatus
-    else
-      super
-    end
-  end
-
-  def value_objects
-    available_versions = versions.index_by(&:id)
-
-    values
-      .filter_map { |version_id| available_versions[version_id.to_i] }
-  end
-
-  def where
-    case operator
-    when "!" # is not
-      "NOT (#{target_version_matching_values})"
-    when "!*" # empty
-      "NOT (#{any_target_version_associated})"
-    when "*" # not empty
-      any_target_version_associated
-    when "o", "c", "l" # version status
-      target_version_with_status(STATUS_BY_OPERATOR[operator])
-    else # "=" is (or)
-      target_version_matching_values
-    end
-  end
-
-  private
-
-  def any_target_version_associated
-    "EXISTS (#{target_associations.select(1).to_sql})"
-  end
-
-  def target_version_matching_values
-    "EXISTS (#{target_associations.where(version_id: values).select(1).to_sql})"
-  end
-
-  def target_version_with_status(status)
-    sub = target_associations
-            .joins(:version)
-            .where(Version.table_name => { status: })
-            .select(1)
-    "EXISTS (#{sub.to_sql})"
-  end
-
-  def target_associations
-    WorkPackageVersion
-      .where(kind: "target")
-      .where("#{WorkPackageVersion.table_name}.work_package_id = #{WorkPackage.table_name}.id")
-  end
-
-  def versions
-    if project
-      project.shared_versions
-    else
-      Version.visible
-    end
   end
 end
