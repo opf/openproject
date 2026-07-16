42b1396935b428093b4b95973a354bf0be7c78b8
Dombi Attila <83396+dombesz@users.noreply.github.com>
2026-07-16 17:26:48 +0300

    [SPPM-318] Missing enterprise check on portfolio and program filter
    https://community.openproject.org/wp/SPPM-318

--- app/models/queries/projects/filters/ancestor_workspace_type_filter.rb
+++ app/models/queries/projects/filters/ancestor_workspace_type_filter.rb
@@ -44,6 +44,7 @@ module Queries::Projects::Filters::AncestorWorkspaceTypeFilter
   def where = nil
 
   def available?
+    (self.class.key == :project || EnterpriseToken.allows_to?(:portfolio_management)) &&
     Project.workspace_type(self.class.key.to_s).visible.exists?
   end
 
