1d00913978edbf86e2a3031f360d550ed0ee5c6a
as-op <a.sandorf@openproject.com>
2026-07-23 16:37:17 +0200

    Replace the button "Edit configuration" with an action menu popup

---
+++ spec/features/admin/import/jira/import_runs_spec.rb
@@ -0,0 +1,52 @@
+# frozen_string_literal: true
+
+#-- copyright
+# OpenProject is an open source project management software.
+# Copyright (C) the OpenProject GmbH
+#
+# This program is free software; you can redistribute it and/or
+# modify it under the terms of the GNU General Public License version 3.
+#
+# OpenProject is a fork of ChiliProject, which is a fork of Redmine. The copyright follows:
+# Copyright (C) 2006-2013 Jean-Philippe Lang
+# Copyright (C) 2010-2013 the ChiliProject Team
+#
+# This program is free software; you can redistribute it and/or
+# modify it under the terms of the GNU General Public License
+# as published by the Free Software Foundation; either version 2
+# of the License, or (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+#
+# See COPYRIGHT and LICENSE files for more details.
+#++
+
+require "spec_helper"
+
+RSpec.describe "Jira import runs page", :js do
+  shared_let(:admin) { create(:admin) }
+  let!(:jira) { create(:jira, name: "My Jira") }
+
+  current_user { admin }
+
+  before { visit admin_import_jira_path(jira) }
+
+  describe "header action menu" do
+    it "opens the edit configuration entry from the actions menu" do
+      expect(page).to have_no_link("Edit configuration")
+
+      page.find_test_selector("jira-configuration-action-menu").click
+
+      within "anchored-position" do
+        expect(page).to have_link("Edit configuration", href: edit_admin_import_jira_path(jira))
+      end
+    end
+  end
+end
