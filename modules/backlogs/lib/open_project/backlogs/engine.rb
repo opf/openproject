bdbadac0df138fe283fcbcf9be50b0b94076edc4
Tobias Dillmann <github@pentokami.de>
2026-07-24 15:17:17 +0200

    Show image in MAS enterprise banner

--- modules/backlogs/lib/open_project/backlogs/engine.rb
+++ modules/backlogs/lib/open_project/backlogs/engine.rb
@@ -137,6 +137,8 @@ module OpenProject::Backlogs
            before: :settings_storage
     end
 
+    assets %w(enterprise/multiple-active-sprints-light.png enterprise/multiple-active-sprints-dark.png)
+
     patches %i[PermittedParams
                WorkPackage
                Project]
