0791be0ba042ce7f9fe64405aa06c3e2bc4807e0
Jan Sandbrink <j.sandbrink@openproject.com>
2026-07-28 15:58:28 +0200

    Add MCP tool to list WP comments
    This only offers a limited version of what
    the regular activities APIs would offer, mostly based on
    the assumption that the amount of data returned by the regular
    APIs would not have a practical application and be hard to digest.
    
    On the other hand interaction with comments is essential for
    all kinds of automations, so MCP should also offer it.

--- config/initializers/mcp.rb
+++ config/initializers/mcp.rb
@@ -49,6 +49,7 @@ Rails.application.config.after_initialize do
                     McpTools::CurrentUser,
                     McpTools::ListStatuses,
                     McpTools::ListTypes,
+                    McpTools::ListWorkPackageComments,
                     McpTools::ListWorkPackageRelations,
                     McpTools::SearchCustomFields,
                     McpTools::SearchCustomFieldItems,
