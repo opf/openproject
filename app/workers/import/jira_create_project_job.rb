8dfa8c303c7447b01468e1d24e57dbbe6030ff07
as-op <a.sandorf@openproject.com>
2026-07-29 14:21:02 +0200

    remove unused functions

--- app/workers/import/jira_create_project_job.rb
+++ app/workers/import/jira_create_project_job.rb
@@ -31,35 +31,24 @@
 module Import
   class JiraCreateProjectJob < ApplicationJob
     include Import::JiraOpenProjectReferenceCreation
-    include JiraImportCustomFields
 
     def text
       jira_project_name = Import::JiraProject.find(arguments[1]).payload["name"]
       "Create project '#{jira_project_name}'"
     end
 
-    # rubocop:disable Metrics/AbcSize
     def perform(jira_import_id, jira_project_id)
       Journal::NotificationConfiguration.with(false) do
         Journal::EventConfiguration.with(false) do
           @jira_import = Import::JiraImport.find(jira_import_id)
-          jira = @jira_import.jira
-          @jira_id = jira.id
+          @jira_id = @jira_import.jira.id
           @system_user = User.system
-          @jira_client = Import::JiraClient.new(url: jira.url, personal_access_token: jira.personal_access_token)
           jira_project = Import::JiraProject.find(jira_project_id)
 
-          @project_role = Role.find_by!(name: "JiraMember")
-          custom_field_registry = build_custom_field_registry
-
-          ActiveRecord::Base.transaction do
-            project = create_project(jira_project)
-            update_custom_fields_in_project(project, jira_project, custom_field_registry)
-          end
+          ActiveRecord::Base.transaction { create_project(jira_project) }
         end
       end
     end
-    # rubocop:enable Metrics/AbcSize
 
     private
 
@@ -100,32 +89,6 @@ module Import
       end
 
       raise service_call.message
-    end
-    # rubocop:enable Metrics/AbcSize
-
-    def import_issue(jira_issue, project, custom_field_registry)
-      type = import_type(jira_issue, project)
-      status = import_status(jira_issue)
-      update_workflows(type)
-      new_custom_fields = new_custom_fields_in_type(jira_issue, type, custom_field_registry)
-      update_custom_fields_in_type(type, new_custom_fields) if new_custom_fields.any?
-      priority = import_priority(jira_issue) || IssuePriority.default || IssuePriority.active.first
-      raise "Create a priority. OpenProject work package requires a priority!" if priority.blank?
-
-      import_work_package(jira_issue, project, type, status, priority, custom_field_registry)
-    end
-
-    # rubocop:disable Metrics/AbcSize
-    def update_custom_fields_in_project(project, jira_project, custom_field_registry)
-      project_key = jira_project.payload["key"]
-      applicable_cfs = custom_field_registry.flat_map do |entry|
-        entry[:contexts]
-          .select { |ctx| context_applies_to_project?(ctx, project_key) }
-          .map { |ctx| ctx[:custom_field] }
-      end
-      existing_cf_ids = project.work_package_custom_fields.pluck(:id).to_set
-      new_cfs = applicable_cfs.uniq.reject { |cf| existing_cf_ids.include?(cf.id) }
-      project.work_package_custom_fields << new_cfs if new_cfs.any?
     end
     # rubocop:enable Metrics/AbcSize
   end
