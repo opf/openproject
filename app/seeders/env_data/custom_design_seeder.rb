ae127566c8bf48a9dbc76c2599aa9e3e8b28264e
Oliver Günther <mail@oliverguenther.de>
2026-07-29 11:19:19 +0200

    [OP-19826] SSRF protection error when using custom design seeding with URLs
    Do not use Carrierwave downloader after all, but our own httpx/ssrf implementation
    inside a background job, so failures can be retried.
    
    https://community.openproject.org/wp/OP-19826

--- app/seeders/env_data/custom_design_seeder.rb
+++ app/seeders/env_data/custom_design_seeder.rb
@@ -85,10 +85,7 @@ module EnvData
     end
 
     def seed_remote_url(custom_style, key, url)
-      custom_style.public_send(:"remote_#{key}_url=", url)
-      custom_style.save!
-    rescue StandardError => e
-      Rails.logger.error "Failed to seed remote URL for design variable '#{key}': #{e.message}"
+      CustomStyles::SeedRemoteAssetJob.perform_later(custom_style, key, url)
     end
 
     class Base64StringIO < StringIO
@@ -115,7 +112,7 @@ module EnvData
         content_type = metadata.match(%r{data:([^;]+)})&.captures&.first
         raise ArgumentError, "Failed to parse content type from metadata: #{metadata}" if content_type.nil?
 
-        mime_type = MIME::Types[content_type].last
+        mime_type = MIME::Types[content_type].first
         raise ArgumentError, "Unknown mime type: #{content_type}" if mime_type.nil?
 
         mime_type.preferred_extension
