6a7fbe001fc9afe124051e48601c98bb26dd5559
Marcello Rocha <m.rocha@openproject.com>
2026-08-13 12:04:20 +0200

    addresses feedback by renaming variables and reducing the usage of the fetch {} idiom

--- modules/wikis/app/services/wikis/page_search_service.rb
+++ modules/wikis/app/services/wikis/page_search_service.rb
@@ -73,8 +73,8 @@ module Wikis
     def search_by_query(query)
       Adapters::Input::SearchPages.build(query:).bind do |input_data|
         provider.auth_strategy_for(user).bind do |auth_strategy|
-          matching_pages(input_data:, auth_strategy:).bind do |pages|
-            matching_wikis(input_data:, auth_strategy:).fmap { build_result_tree(pages:, wikis: it) }
+          matching_pages(input_data:, auth_strategy:).bind do |page_hierarchies|
+            matching_wikis(input_data:, auth_strategy:).fmap { |wikis| build_result_tree(page_hierarchies, wikis) }
           end
         end
       end
@@ -89,15 +89,15 @@ module Wikis
     end
 
     def to_tree_node(page:, enabled:)
-      Adapters::Results::PageSearchTreeNode.leaf_page(page.identifier, page.title, enabled)
+      Adapters::Results::PageSearchTreeNode.page(page.identifier, page.title, enabled)
     end
 
-    def build_result_tree(pages:, wikis:)
-      root = Adapters::Results::PageSearchTreeNode.empty_root
+    def build_result_tree(page_hierarchies, wikis)
+      root = Adapters::Results::PageSearchTreeNode.root
 
       existing_nodes = {}
       wikis.each { insert_wiki_node(existing_nodes, it, root) }
-      pages.each { insert_page_hierarchy(existing_nodes, it, root) }
+      page_hierarchies.each { insert_page_hierarchy(existing_nodes, it, root) }
 
       root.children
     end
@@ -108,57 +108,57 @@ module Wikis
       insert_page_node(existing_nodes, page_hierarchy)
     end
 
-    def insert_wiki_node(node_list, wiki, root_node)
-      wiki_node = Adapters::Results::PageSearchTreeNode.empty_wiki(wiki.identifier, wiki.name)
+    def insert_wiki_node(existing_nodes, wiki, root_node)
+      wiki_node = Adapters::Results::PageSearchTreeNode.wiki(wiki.identifier, wiki.name)
 
-      node_list.fetch(wiki_node.key) do
+      unless existing_nodes[wiki_node.key]
         root_node.add_child(wiki_node)
-        node_list[wiki_node.key] = wiki_node
+        existing_nodes[wiki_node.key] = wiki_node
       end
     end
 
-    def insert_ancestor_nodes(node_list, page)
-      page => { wiki:, ancestors: }
+    def insert_ancestor_nodes(existing_nodes, page_hierarchy)
+      page_hierarchy => { wiki:, ancestors: }
 
-      previous_ancestor_node = node_list[node_key(:wiki, wiki.identifier)]
+      previous_ancestor_node = existing_nodes[node_key(:wiki, wiki.identifier)]
 
-      ancestors.reduce(previous_ancestor_node) do |previous, current|
+      ancestors.reverse.reduce(previous_ancestor_node) do |previous, current|
         ancestor_node = to_tree_node(page: current, enabled: false)
 
-        node_list.fetch(ancestor_node.key) do
+        existing_nodes.fetch(ancestor_node.key) do
           previous.add_child(ancestor_node)
-          node_list[ancestor_node.key] = ancestor_node
+          existing_nodes[ancestor_node.key] = ancestor_node
         end
       end
     end
 
-    def insert_page_node(node_list, page_hierarchy)
+    def insert_page_node(existing_nodes, page_hierarchy)
       page_hierarchy => { page:, ancestors:, wiki: }
 
       new_node = to_tree_node(page:, enabled: true)
-      return if enable_if_node_exists(node_list, new_node)
+      return if enable_if_node_exists(existing_nodes, new_node)
 
-      parent_node = find_parent(node_list, ancestors, wiki)
+      parent_node = find_parent(existing_nodes, ancestors, wiki)
       parent_node.add_child(new_node)
-      node_list[new_node.key] = new_node
+      existing_nodes[new_node.key] = new_node
     end
 
-    def find_parent(node_list, ancestors, wiki)
+    def find_parent(existing_nodes, ancestors, wiki)
       key = if ancestors.any?
               node_key(:page, ancestors.first.identifier)
             else
               node_key(:wiki, wiki.identifier)
             end
 
-      node_list[key]
+      existing_nodes[key]
     end
 
     def node_key(type, identifier)
       Adapters::Results::PageSearchTreeNode::NodeKey.new(type, identifier)
     end
 
-    def enable_if_node_exists(node_list, node)
-      existing_node = node_list.fetch(node.key) { return false }
+    def enable_if_node_exists(existing_nodes, node)
+      existing_node = existing_nodes.fetch(node.key) { return false }
       existing_node.enable
     end
   end
