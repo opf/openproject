fe6b59086eb0b43fdc8ea2011e761c4237f7b295
Kabiru Mwenja <k.mwenja@openproject.com>
2026-08-19 11:43:01 +0300

    Cover the target version filter in the API filter hash mapping
    The copy mapper's hash path had no example with the targetVersion key;
    only the live-query path covered the target_version_id mapper entry.

--- spec/services/queries/filters_mapper_spec.rb
+++ spec/services/queries/filters_mapper_spec.rb
@@ -138,6 +138,24 @@ RSpec.describe Queries::Copy::FiltersMapper do
     end
   end
 
+  describe "with a filter hash array holding the target versions filter" do
+    let(:filters) do
+      [
+        { "targetVersion" => { "operator" => "=", "values" => ["3"] } }
+      ]
+    end
+
+    subject { instance.map_filters(filters) }
+
+    before do
+      state.version_id_lookup = { 3 => 33 }
+    end
+
+    it "maps the filter values" do
+      expect(subject[0]["targetVersion"]["values"]).to eq(["33"])
+    end
+  end
+
   describe "with a symbolized filter hash array" do
     let(:filters) do
       [
