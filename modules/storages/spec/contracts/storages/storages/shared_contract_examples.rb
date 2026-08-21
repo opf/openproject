2cf91cf01b89e25c4a39f1e7b335ac92cd559d5e
Yauheni Suhakou <shiroginne@users.noreply.github.com>
2026-08-21 17:05:53 +0200

    Bumped the minimal version for the NC to 32

--- modules/storages/spec/contracts/storages/storages/shared_contract_examples.rb
+++ modules/storages/spec/contracts/storages/storages/shared_contract_examples.rb
@@ -162,7 +162,7 @@ RSpec.shared_examples_for "nextcloud storage contract", :storage_server_helpers,
         let(:capabilities_response_body) { nil } # use default
         let(:capabilities_response_code) { nil } # use default
         let(:capabilities_response_headers) { nil } # use default
-        let(:capabilities_response_major_version) { 22 }
+        let(:capabilities_response_major_version) { 32 }
         let(:check_config_response_body) { nil } # use default
         let(:check_config_response_code) { nil } # use default
         let(:check_config_response_headers) { nil } # use default
@@ -251,8 +251,8 @@ RSpec.shared_examples_for "nextcloud storage contract", :storage_server_helpers,
           include_examples "contract is invalid", host: :not_nextcloud_server
         end
 
-        context "when Nextcloud version is below the required minimal version which is 22" do
-          let(:capabilities_response_major_version) { 21 }
+        context "when Nextcloud version is below the required minimal version which is 32" do
+          let(:capabilities_response_major_version) { 31 }
 
           include_examples "contract is invalid", host: :minimal_nextcloud_version_unmet
         end
