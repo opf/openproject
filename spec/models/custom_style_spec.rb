dcdcdbbd8ece7836e5db3052f1382dfb43df8d51
Behrokh Satarnejad <b.satarnejad@openproject.com>
2026-08-31 14:33:14 +0200

    Add migration and resolve mode-specific logos with fallback to the light logo

--- spec/models/custom_style_spec.rb
+++ spec/models/custom_style_spec.rb
@@ -4,22 +4,22 @@ require "spec_helper"
 
 RSpec.describe CustomStyle do
   describe "#current" do
-    subject { CustomStyle.current }
+    subject { described_class.current }
 
-    context "there is one in DB" do
+    context "when there is one in DB" do
       it "returns an instance" do
-        CustomStyle.create
-        expect(subject).to be_a CustomStyle
+        described_class.create
+        expect(subject).to be_a described_class
       end
 
       it "returns the same instance for subsequent calls" do
-        CustomStyle.create
-        first_instance = CustomStyle.current
+        described_class.create
+        first_instance = described_class.current
         expect(subject).to be first_instance
       end
     end
 
-    context "there is none in DB" do
+    context "when there is none in DB" do
       before do
         RequestStore.delete(:current_custom_style)
       end
@@ -28,67 +28,159 @@ RSpec.describe CustomStyle do
         expect(subject).to be_nil
       end
     end
+  end
 
-    shared_examples "removing an image from a custom style" do
-      let(:image) { raise "define me!" }
-      let(:custom_style) { create "custom_style_with_#{image}" }
+  describe "#logo_for" do
+    context "for desktop logos" do
+      it "returns the light logo in light mode" do
+        custom_style = build(:custom_style_with_logo)
 
-      let!(:file_path) { custom_style.send(image).file.path }
-
-      subject { custom_style.send :"remove_#{image}!" }
-
-      it "deletes the file" do
-        subject
-
-        expect(File.exist?(file_path)).to be false
+        expect(custom_style.logo_for(color_mode: :light)).to eq(custom_style.logo)
       end
 
-      it "clears the file mount column" do
-        subject
+      it "returns the light high contrast logo in light high contrast mode" do
+        custom_style = build(:custom_style_with_logo_light_high_contrast)
 
-        expect(custom_style.reload.send(image).file).to be_nil
+        expect(custom_style.logo_for(color_mode: :light, high_contrast: true))
+          .to eq(custom_style.logo_light_high_contrast)
       end
 
-      it "updates the model" do
-        expect { subject }
-          .to change(custom_style, :updated_at)
-      end
-    end
+      it "falls back to the light logo in light high contrast mode" do
+        custom_style = build(:custom_style_with_logo)
 
-    describe "#remove_favicon" do
-      it_behaves_like "removing an image from a custom style" do
-        let(:image) { "favicon" }
+        expect(custom_style.logo_for(color_mode: :light, high_contrast: true)).to eq(custom_style.logo)
       end
-    end
 
-    describe "#remove_touch_icon" do
-      it_behaves_like "removing an image from a custom style" do
-        let(:image) { "touch_icon" }
+      it "returns the dark logo in dark mode" do
+        custom_style = build(:custom_style_with_logo_dark)
+
+        expect(custom_style.logo_for(color_mode: :dark)).to eq(custom_style.logo_dark)
       end
-    end
 
-    describe "#remove_logo" do
-      it_behaves_like "removing an image from a custom style" do
-        let(:image) { "logo" }
+      it "returns the dark logo in dark high contrast mode" do
+        custom_style = build(:custom_style_with_logo_dark)
+
+        expect(custom_style.logo_for(color_mode: :dark, high_contrast: true)).to eq(custom_style.logo_dark)
+      end
+
+      it "falls back to the light logo in dark mode" do
+        custom_style = build(:custom_style_with_logo)
+
+        expect(custom_style.logo_for(color_mode: :dark)).to eq(custom_style.logo)
       end
     end
 
-    describe "#remove_export_logo" do
-      it_behaves_like "removing an image from a custom style" do
-        let(:image) { "export_logo" }
+    context "for mobile logos" do
+      it "returns the mobile light high contrast logo in light high contrast mode" do
+        custom_style = build(:custom_style_with_logo_mobile_light_high_contrast)
+
+        expect(custom_style.logo_for(color_mode: :light, high_contrast: true, mobile: true))
+          .to eq(custom_style.logo_mobile_light_high_contrast)
+      end
+
+      it "returns the mobile dark logo in dark mode" do
+        custom_style = build(:custom_style_with_logo_mobile_dark)
+
+        expect(custom_style.logo_for(color_mode: :dark, mobile: true)).to eq(custom_style.logo_mobile_dark)
+      end
+
+      it "falls back to the mobile light logo" do
+        custom_style = build(:custom_style_with_logo_mobile)
+
+        expect(custom_style.logo_for(color_mode: :dark, mobile: true)).to eq(custom_style.logo_mobile)
       end
     end
+  end
 
-    describe "#remove_export_cover" do
-      it_behaves_like "removing an image from a custom style" do
-        let(:image) { "export_cover" }
-      end
+  shared_examples "removing an image from a custom style" do
+    let(:image) { raise "define me!" }
+    let(:custom_style) { create "custom_style_with_#{image}" }
+
+    let!(:file_path) { custom_style.send(image).file.path }
+
+    subject { custom_style.send :"remove_#{image}!" }
+
+    it "deletes the file" do
+      subject
+
+      expect(File.exist?(file_path)).to be false
     end
 
-    describe "#remove_export_footer" do
-      it_behaves_like "removing an image from a custom style" do
-        let(:image) { "export_footer" }
-      end
+    it "clears the file mount column" do
+      subject
+
+      expect(custom_style.reload.send(image).file).to be_nil
+    end
+
+    it "updates the model" do
+      expect { subject }
+        .to change(custom_style, :updated_at)
+    end
+  end
+
+  describe "#remove_favicon" do
+    it_behaves_like "removing an image from a custom style" do
+      let(:image) { "favicon" }
+    end
+  end
+
+  describe "#remove_touch_icon" do
+    it_behaves_like "removing an image from a custom style" do
+      let(:image) { "touch_icon" }
+    end
+  end
+
+  describe "#remove_logo" do
+    it_behaves_like "removing an image from a custom style" do
+      let(:image) { "logo" }
+    end
+  end
+
+  describe "#remove_logo_dark" do
+    it_behaves_like "removing an image from a custom style" do
+      let(:image) { "logo_dark" }
+    end
+  end
+
+  describe "#remove_logo_light_high_contrast" do
+    it_behaves_like "removing an image from a custom style" do
+      let(:image) { "logo_light_high_contrast" }
+    end
+  end
+
+  describe "#remove_logo_mobile" do
+    it_behaves_like "removing an image from a custom style" do
+      let(:image) { "logo_mobile" }
+    end
+  end
+
+  describe "#remove_logo_mobile_dark" do
+    it_behaves_like "removing an image from a custom style" do
+      let(:image) { "logo_mobile_dark" }
+    end
+  end
+
+  describe "#remove_logo_mobile_light_high_contrast" do
+    it_behaves_like "removing an image from a custom style" do
+      let(:image) { "logo_mobile_light_high_contrast" }
+    end
+  end
+
+  describe "#remove_export_logo" do
+    it_behaves_like "removing an image from a custom style" do
+      let(:image) { "export_logo" }
+    end
+  end
+
+  describe "#remove_export_cover" do
+    it_behaves_like "removing an image from a custom style" do
+      let(:image) { "export_cover" }
+    end
+  end
+
+  describe "#remove_export_footer" do
+    it_behaves_like "removing an image from a custom style" do
+      let(:image) { "export_footer" }
     end
   end
 end
