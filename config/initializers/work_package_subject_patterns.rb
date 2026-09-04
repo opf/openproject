11b35e381717a2f6aee758aaba466559f45014b7
Jan Sandbrink <j.sandbrink@openproject.com>
2026-09-04 09:06:19 +0200

    Introduce a small DSL to shorten definitions of attributes
    The context got repeated over and over again, so it was moved into a shared
    DSL helper that's shared across a block of attributes. The same block usually shared
    the same way of generating labels in the majority of cases, so that was codified as well.

--- config/initializers/work_package_subject_patterns.rb
+++ config/initializers/work_package_subject_patterns.rb
@@ -29,46 +29,54 @@
 #++
 
 Rails.application.config.to_prepare do
-  # rubocop:disable Layout/LineLength
   mapper = WorkPackageTypes::Patterns::TokenPropertyMapper
-  mapper.add_static_attribute(:id, :work_package, -> { WorkPackage.human_attribute_name(:id) }, ->(wp) { wp.id })
-  mapper.add_static_attribute(:accountable, :work_package, -> { WorkPackage.human_attribute_name(:responsible) }, ->(wp) { wp.responsible })
-  mapper.add_static_attribute(:assignee, :work_package, -> { WorkPackage.human_attribute_name(:assigned_to) }, ->(wp) { wp.assigned_to })
-  mapper.add_static_attribute(:author, :work_package, -> { WorkPackage.human_attribute_name(:author) }, ->(wp) { wp.author })
-  mapper.add_static_attribute(:category, :work_package, -> { WorkPackage.human_attribute_name(:category) }, ->(wp) { wp.category })
-  mapper.add_static_attribute(:creation_date, :work_package, -> { WorkPackage.human_attribute_name(:created_at) }, ->(wp) { wp.created_at }, mapper::DATE)
-  mapper.add_static_attribute(:estimated_time, :work_package, -> { WorkPackage.human_attribute_name(:estimated_hours) }, ->(wp) { wp.estimated_hours }, mapper::DURATION)
-  mapper.add_static_attribute(:remaining_time, :work_package, -> { WorkPackage.human_attribute_name(:remaining_hours) }, ->(wp) { wp.remaining_hours }, mapper::DURATION)
-  mapper.add_static_attribute(:finish_date, :work_package, -> { WorkPackage.human_attribute_name(:due_date) }, ->(wp) { wp.due_date }, mapper::DATE)
-  mapper.add_static_attribute(:observed_in_versions, :work_package, -> { WorkPackage.human_attribute_name(:observed_in_versions) }, ->(wp) { wp.observed_in_versions }, mapper::ARRAY)
-  mapper.add_static_attribute(:priority, :work_package, -> { WorkPackage.human_attribute_name(:priority) }, ->(wp) { wp.priority })
-  mapper.add_static_attribute(:start_date, :work_package, -> { WorkPackage.human_attribute_name(:start_date) }, ->(wp) { wp.start_date }, mapper::DATE)
-  mapper.add_static_attribute(:status, :work_package, -> { WorkPackage.human_attribute_name(:status) }, ->(wp) { wp.status })
-  mapper.add_static_attribute(:type, :work_package, -> { WorkPackage.human_attribute_name(:type) }, ->(wp) { wp.type })
-  mapper.add_static_attribute(:version, :work_package, -> { Setting::WorkPackageMultipleVersions.active? ? WorkPackage.human_attribute_name(:target_versions) : WorkPackage.human_attribute_name(:version) }, ->(wp) { wp.target_versions }, mapper::ARRAY)
+  mapper.configure_static_attributes(context: :work_package, label_model: WorkPackage) do
+    add(:id, ->(wp) { wp.id }, label: :id)
+    add(:accountable, ->(wp) { wp.responsible }, label: :responsible)
+    add(:assignee, ->(wp) { wp.assigned_to }, label: :assigned_to)
+    add(:author, ->(wp) { wp.author }, label: :author)
+    add(:category, ->(wp) { wp.category }, label: :category)
+    add(:creation_date, ->(wp) { wp.created_at }, mapper::DATE, label: :created_at)
+    add(:estimated_time, ->(wp) { wp.estimated_hours }, mapper::DURATION, label: :estimated_hours)
+    add(:remaining_time, ->(wp) { wp.remaining_hours }, mapper::DURATION, label: :remaining_hours)
+    add(:finish_date, ->(wp) { wp.due_date }, mapper::DATE, label: :due_date)
+    add(:observed_in_versions, ->(wp) { wp.observed_in_versions }, mapper::ARRAY, label: :observed_in_versions)
+    add(:priority, ->(wp) { wp.priority }, label: :priority)
+    add(:start_date, ->(wp) { wp.start_date }, mapper::DATE, label: :start_date)
+    add(:status, ->(wp) { wp.status }, label: :status)
+    add(:type, ->(wp) { wp.type }, label: :type)
+    add(:version, ->(wp) { wp.target_versions }, mapper::ARRAY, label: -> { Setting::WorkPackageMultipleVersions.active? ? WorkPackage.human_attribute_name(:target_versions) : WorkPackage.human_attribute_name(:version) }) # rubocop:disable Metrics/LineLength
+  end
 
-  mapper.add_static_attribute(:parent_id, :parent, -> { WorkPackage.human_attribute_name(:id) }, ->(parent) { parent.id })
-  mapper.add_static_attribute(:parent_assignee, :parent, -> { WorkPackage.human_attribute_name(:assigned_to) }, ->(parent) { parent.assigned_to })
-  mapper.add_static_attribute(:parent_author, :parent, -> { WorkPackage.human_attribute_name(:author) }, ->(parent) { parent.author })
-  mapper.add_static_attribute(:parent_category, :parent, -> { WorkPackage.human_attribute_name(:category) }, ->(parent) { parent.category })
-  mapper.add_static_attribute(:parent_creation_date, :parent, -> { WorkPackage.human_attribute_name(:created_at) }, ->(parent) { parent.created_at }, mapper::DATE)
-  mapper.add_static_attribute(:parent_estimated_time, :parent, -> { WorkPackage.human_attribute_name(:estimated_hours) }, ->(parent) { parent.estimated_hours }, mapper::DURATION)
-  mapper.add_static_attribute(:parent_remaining_time, :parent, -> { WorkPackage.human_attribute_name(:remaining_hours) }, ->(parent) { parent.remaining_hours }, mapper::DURATION)
-  mapper.add_static_attribute(:parent_finish_date, :parent, -> { WorkPackage.human_attribute_name(:due_date) }, ->(parent) { parent.due_date }, mapper::DATE)
-  mapper.add_static_attribute(:parent_observed_in_versions, :parent, -> { WorkPackage.human_attribute_name(:observed_in_versions) }, ->(parent) { parent.observed_in_versions }, mapper::ARRAY)
-  mapper.add_static_attribute(:parent_priority, :parent, -> { WorkPackage.human_attribute_name(:priority) }, ->(parent) { parent.priority })
-  mapper.add_static_attribute(:parent_subject, :parent, -> { WorkPackage.human_attribute_name(:subject) }, ->(parent) { parent.subject })
-  mapper.add_static_attribute(:parent_status, :parent, -> { WorkPackage.human_attribute_name(:status) }, ->(parent) { parent.status })
-  mapper.add_static_attribute(:parent_type, :parent, -> { WorkPackage.human_attribute_name(:type) }, ->(parent) { parent.type })
-  mapper.add_static_attribute(:parent_version, :parent, -> { Setting::WorkPackageMultipleVersions.active? ? WorkPackage.human_attribute_name(:target_versions) : WorkPackage.human_attribute_name(:version) }, ->(parent) { parent.target_versions }, mapper::ARRAY)
+  mapper.configure_static_attributes(context: :parent, label_model: WorkPackage) do
+    add(:parent_id, ->(parent) { parent.id }, label: :id)
+    add(:parent_assignee, ->(parent) { parent.assigned_to }, label: :assigned_to)
+    add(:parent_author, ->(parent) { parent.author }, label: :author)
+    add(:parent_category, ->(parent) { parent.category }, label: :category)
+    add(:parent_creation_date, ->(parent) { parent.created_at }, mapper::DATE, label: :created_at)
+    add(:parent_estimated_time, ->(parent) { parent.estimated_hours }, mapper::DURATION, label: :estimated_hours)
+    add(:parent_remaining_time, ->(parent) { parent.remaining_hours }, mapper::DURATION, label: :remaining_hours)
+    add(:parent_finish_date, ->(parent) { parent.due_date }, mapper::DATE, label: :due_date)
+    add(:parent_observed_in_versions, ->(parent) { parent.observed_in_versions }, mapper::ARRAY, label: :observed_in_versions)
+    add(:parent_priority, ->(parent) { parent.priority }, label: :priority)
+    add(:parent_subject, ->(parent) { parent.subject }, label: :subject)
+    add(:parent_status, ->(parent) { parent.status }, label: :status)
+    add(:parent_type, ->(parent) { parent.type }, label: :type)
+    add(:parent_version, ->(parent) { parent.target_versions }, mapper::ARRAY, label: -> { Setting::WorkPackageMultipleVersions.active? ? WorkPackage.human_attribute_name(:target_versions) : WorkPackage.human_attribute_name(:version) }) # rubocop:disable Metrics/LineLength
+  end
 
-  mapper.add_static_attribute(:project_id, :project, -> { Project.human_attribute_name(:id) }, ->(project) { project.id })
-  mapper.add_static_attribute(:project_active, :project, -> { Project.human_attribute_name(:active) }, ->(project) { project.active? })
-  mapper.add_static_attribute(:project_name, :project, -> { Project.human_attribute_name(:name) }, ->(project) { project })
-  mapper.add_static_attribute(:project_status, :project, -> { Project.human_attribute_name(:status_code) }, ->(project) { project.status_code && I18n.t("activerecord.attributes.project.status_codes.#{project.status_code}") })
-  mapper.add_static_attribute(:project_parent, :project, -> { Project.human_attribute_name(:parent) }, ->(project) { project.parent_id })
-  mapper.add_static_attribute(:project_public, :project, -> { Project.human_attribute_name(:public) }, ->(project) { project.public? })
-  # rubocop:enable Layout/LineLength
+  mapper.configure_static_attributes(context: :project, label_model: Project) do
+    add(:project_id, ->(project) { project.id }, label: :id)
+    add(:project_active, ->(project) { project.active? }, label: :active)
+    add(:project_name, ->(project) { project }, label: :name)
+    add(
+      :project_status,
+      ->(project) { project.status_code && I18n.t("activerecord.attributes.project.status_codes.#{project.status_code}") },
+      label: :status_code
+    )
+    add(:project_parent, ->(project) { project.parent_id }, label: :parent)
+    add(:project_public, ->(project) { project.public? }, label: :public)
+  end
 
   mapper.add_custom_fields(->(variant) {
     all_wp_cfs = WorkPackageCustomField.where.not(field_format: %w[text link empty]).order(:name)
