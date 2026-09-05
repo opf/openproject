Syvera-Open-Source-Projects/openproject_syvera

[AGILE-362] Cover destination menu flows
Exercises ordered destination batches through Selenium, covering
confinement, stale menus, rejection, and selection cleanup. Rejects every
visible modal in the disappearing-target case.

https://community.openproject.org/wp/AGILE-362

frontend/src/stimulus/controllers/dynamic/sortable-lists.controller.ts
@@ -226,6 +226,13 @@ export default class SortableListsController extends Controller<HTMLElement> imp
       ?? { kind: 'singular', invoker: itemElement, items: [], ids: [] };
   }
 
+  // Consumer-owned non-optimistic forms do not call performMove, so their
+  // successful move event is the shared boundary at which the live batch is
+  // cleared. Failed requests emit no completion event and keep the selection.
+  clearSelectionAfterMove():void {
+    this.selection?.clearAfterMove();
+  }
+
   availableDestinations(scope:ActionScope, candidates:DestinationIdentity[]):DestinationIdentity[] {
     if (scope.kind === 'singular') {
       return [];

opf/openproject

frontend/src/stimulus/controllers/dynamic/sortable-lists.controller.ts
@@ -255,6 +255,13 @@ export default class SortableListsController extends Controller<HTMLElement> imp
     return this.selection?.selectForAction(itemElement) ?? { kind: 'refused', items: [] };
   }
 
+  // Consumer-owned non-optimistic forms do not call performMove, so their
+  // successful move event is the shared boundary at which the live batch is
+  // cleared. Failed requests emit no completion event and keep the selection.
+  clearSelectionAfterMove():void {
+    this.selection?.clearSilently();
+  }
+
   // Where the batch may move: the candidates every member accepts, minus the
   // one they all already occupy, which would be a move to nowhere.
   availableDestinations(scope:ActionScope, candidates:DestinationIdentity[]):DestinationIdentity[] {