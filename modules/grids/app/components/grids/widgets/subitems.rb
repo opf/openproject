138a733f3c705f7b2ed74910243b36fdb7b2e9e5
Behrokh Satarnejad <b.satarnejad@openproject.com>
2026-09-07 11:22:47 +0200

    Remove current user for checking visible children

--- modules/grids/app/components/grids/widgets/subitems.rb
+++ modules/grids/app/components/grids/widgets/subitems.rb
@@ -46,7 +46,7 @@ module Grids
       end
 
       def render?
-        !project.project? || project.children.visible(current_user).exists?
+        !project.project? || project.children.visible.exists?
       end
 
       def displayed_subitems
