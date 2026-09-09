f60f41de8610f9726647338605b116a9c0fdae2c
ulferts <jens.ulferts@googlemail.com>
2026-09-09 16:41:06 +0200

    squash 20250731144436_add_workspace_type_to_project

--- db/migrate/1000017_aggregated_migrations.rb
+++ db/migrate/1000017_aggregated_migrations.rb
@@ -153,5 +153,6 @@ class AggregatedMigrations < SquashedMigration
     20250612133700_service_account_association_foreign_keys
     20250613141234_add_formula_to_custom_fields
     20250627121119_change_default_main_menu_color
+    20250731144436_add_workspace_type_to_project
   ]
 end
