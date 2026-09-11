Syvera-Open-Source-Projects/openproject_syvera

[AGILE-363] Gate batch position actions
Resolves availability over the invoker's prospective scope and exposes
only moves the live block resolver can execute. Preserves destinations for
contiguous same-list selections.

https://community.openproject.org/wp/AGILE-363

frontend/src/stimulus/controllers/dynamic/sortable-lists.controller.spec.ts
@@ -1210,13 +1210,88 @@ describe('Sortable lists controller', () => {
   });
 
   it('reports per-direction move availability for gating', async () => {
-    const { root, firstSourceItem } = renderFixture();
+    const { root, firstSourceItem } = renderSelectableRoot();
     await ctx.nextFrame();
     const controller = ctx.application.getControllerForElementAndIdentifier(root, 'sortable-lists') as SortableListsControllerType;
 
     // First item: down/bottom available, up/top not.
     expect(controller.moveAvailability(firstSourceItem)).toEqual({
       top: false, up: false, down: true, bottom: true,
+    });
+  });
+
+  describe('batch move availability', () => {
+    const unavailable = { top: false, up: false, down: false, bottom: false };
+    const select = (element:HTMLElement, init:MouseEventInit = {}) => {
+      element.dispatchEvent(new MouseEvent('click', { bubbles: true, cancelable: true, ...init }));
+    };
+
+    it('reports the available directions for a selected contiguous block', async () => {
+      const { root, items } = renderSelectableRoot();
+      await ctx.nextFrame();
+      const controller = ctx.application.getControllerForElementAndIdentifier(root, 'sortable-lists') as SortableListsControllerType;
+      select(items[1]);
+      select(items[2], { metaKey: true });
+
+      expect(controller.moveAvailability(items[1])).toEqual({
+        top: true, up: true, down: false, bottom: false,
+      });
+    });
+
+    it('reports every direction unavailable for a sparse selection', async () => {
+      const { root, items } = renderSelectableRoot();
+      await ctx.nextFrame();
+      const controller = ctx.application.getControllerForElementAndIdentifier(root, 'sortable-lists') as SortableListsControllerType;
+      select(items[0]);
+      select(items[2], { metaKey: true });
+
+      expect(controller.moveAvailability(items[0])).toEqual(unavailable);
+    });
+
+    it('reports every direction unavailable for a cross-list selection', async () => {
+      const { root, items } = renderSelectableRoot();
+      await ctx.nextFrame();
+      const controller = ctx.application.getControllerForElementAndIdentifier(root, 'sortable-lists') as SortableListsControllerType;
+      select(items[2]);
+      select(items[3], { metaKey: true });
+
+      expect(controller.moveAvailability(items[2])).toEqual(unavailable);
+    });
+
+    it('reports one-card availability for an unselected invoker without changing the selection', async () => {
+      const { root, items } = renderSelectableRoot();
+      await ctx.nextFrame();
+      const controller = ctx.application.getControllerForElementAndIdentifier(root, 'sortable-lists') as SortableListsControllerType;
+      select(items[0]);
+      select(items[1], { metaKey: true });
+
+      expect(controller.moveAvailability(items[3])).toEqual({
+        top: false, up: false, down: true, bottom: true,
+      });
+      expect(items.filter((item) => item.hasAttribute('data-batch-selected'))).toEqual([items[0], items[1]]);
+    });
+
+    it('reports normal within-list availability for a confined contiguous block', async () => {
+      const { root, items } = renderSelectableRoot();
+      items[0].setAttribute('data-sortable-lists--item-mobility-value', 'confined');
+      items[1].setAttribute('data-sortable-lists--item-mobility-value', 'confined');
+      await ctx.nextFrame();
+      const controller = ctx.application.getControllerForElementAndIdentifier(root, 'sortable-lists') as SortableListsControllerType;
+      select(items[0]);
+      select(items[1], { metaKey: true });
+
+      expect(controller.moveAvailability(items[0])).toEqual({
+        top: false, up: false, down: true, bottom: true,
+      });
+    });
+
+    it('reports null for a fixed invoker', async () => {
+      const { root, items } = renderSelectableRoot();
+      items[0].setAttribute('data-sortable-lists--item-mobility-value', 'fixed');
+      await ctx.nextFrame();
+      const controller = ctx.application.getControllerForElementAndIdentifier(root, 'sortable-lists') as SortableListsControllerType;
+
+      expect(controller.moveAvailability(items[0])).toBeNull();
     });
   });
 

frontend/src/stimulus/controllers/dynamic/sortable-lists.controller.ts
@@ -51,11 +51,11 @@ import {
   isOrderableItem,
   reorderRows,
   resolveDirectionalPreviousItemId,
+  resolveBlockMoveAvailability,
   resolveItemId,
   resolveItemLabel,
   resolveItemPosition,
   resolveItemType,
-  resolveMoveAvailability,
   restoreRowPositions,
   rowOf,
   rowsRemainAt,
@@ -416,9 +416,16 @@ export default class SortableListsController extends Controller<HTMLElement> imp
   // addressable. Null means the item is not in an owned list (yet). The result
   // is a snapshot for menu gating; the click path re-resolves the live DOM.
   moveAvailability(itemElement:HTMLElement):MoveAvailability|null {
+    const scope = this.actionScopeFor(itemElement);
+    if (scope.kind === 'singular') {
+      return null;
+    }
+
     const list = this.ownerListOf(itemElement);
 
-    return list ? resolveMoveAvailability({ itemElement, rowsContainer: list.rowsContainer }) : null;
+    return list
+      ? resolveBlockMoveAvailability({ itemElements: scope.items, rowsContainer: list.rowsContainer })
+      : null;
   }
 
   moveToDestination(itemElement:HTMLElement, target:DestinationIdentity):void {

opf/openproject

frontend/src/stimulus/controllers/dynamic/sortable-lists.controller.spec.ts
@@ -1210,7 +1210,7 @@ describe('Sortable lists controller', () => {
   });
 
   it('reports per-direction move availability for gating', async () => {
-    const { root, firstSourceItem } = renderFixture();
+    const { root, firstSourceItem } = renderSelectableRoot();
     await ctx.nextFrame();
     const controller = ctx.application.getControllerForElementAndIdentifier(root, 'sortable-lists') as SortableListsControllerType;
 
@@ -1220,16 +1220,78 @@ describe('Sortable lists controller', () => {
     });
   });
 
-  // A card that takes no part in ordering must not be offered a move the
-  // click path would then refuse.
-  it('reports no available direction for a non-movable item', async () => {
-    const { root, firstSourceItem } = renderFixture();
-    firstSourceItem.setAttribute('data-sortable-lists--item-mobility-value', 'fixed');
-    await ctx.nextFrame();
-    const controller = ctx.application.getControllerForElementAndIdentifier(root, 'sortable-lists') as SortableListsControllerType;
+  describe('batch move availability', () => {
+    const unavailable = { top: false, up: false, down: false, bottom: false };
+    const select = (element:HTMLElement, init:MouseEventInit = {}) => {
+      element.dispatchEvent(new MouseEvent('click', { bubbles: true, cancelable: true, ...init }));
+    };
 
-    expect(controller.moveAvailability(firstSourceItem)).toEqual({
-      top: false, up: false, down: false, bottom: false,
+    it('reports the available directions for a selected contiguous block', async () => {
+      const { root, items } = renderSelectableRoot();
+      await ctx.nextFrame();
+      const controller = ctx.application.getControllerForElementAndIdentifier(root, 'sortable-lists') as SortableListsControllerType;
+      select(items[1]);
+      select(items[2], { ctrlKey: true });
+
+      expect(controller.moveAvailability(items[1])).toEqual({
+        top: true, up: true, down: false, bottom: false,
+      });
+    });
+
+    it('reports every direction unavailable for a sparse selection', async () => {
+      const { root, items } = renderSelectableRoot();
+      await ctx.nextFrame();
+      const controller = ctx.application.getControllerForElementAndIdentifier(root, 'sortable-lists') as SortableListsControllerType;
+      select(items[0]);
+      select(items[2], { ctrlKey: true });
+
+      expect(controller.moveAvailability(items[0])).toEqual(unavailable);
+    });
+
+    it('reports every direction unavailable for a cross-list selection', async () => {
+      const { root, items } = renderSelectableRoot();
+      await ctx.nextFrame();
+      const controller = ctx.application.getControllerForElementAndIdentifier(root, 'sortable-lists') as SortableListsControllerType;
+      select(items[2]);
+      select(items[3], { ctrlKey: true });
+
+      expect(controller.moveAvailability(items[2])).toEqual(unavailable);
+    });
+
+    it('reports one-card availability for an unselected invoker without changing the selection', async () => {
+      const { root, items } = renderSelectableRoot();
+      await ctx.nextFrame();
+      const controller = ctx.application.getControllerForElementAndIdentifier(root, 'sortable-lists') as SortableListsControllerType;
+      select(items[0]);
+      select(items[1], { ctrlKey: true });
+
+      expect(controller.moveAvailability(items[3])).toEqual({
+        top: false, up: false, down: true, bottom: true,
+      });
+      expect(items.filter((item) => item.hasAttribute('data-batch-selected'))).toEqual([items[0], items[1]]);
+    });
+
+    it('reports normal within-list availability for a confined contiguous block', async () => {
+      const { root, items } = renderSelectableRoot();
+      items[0].setAttribute('data-sortable-lists--item-mobility-value', 'confined');
+      items[1].setAttribute('data-sortable-lists--item-mobility-value', 'confined');
+      await ctx.nextFrame();
+      const controller = ctx.application.getControllerForElementAndIdentifier(root, 'sortable-lists') as SortableListsControllerType;
+      select(items[0]);
+      select(items[1], { ctrlKey: true });
+
+      expect(controller.moveAvailability(items[0])).toEqual({
+        top: false, up: false, down: true, bottom: true,
+      });
+    });
+
+    it('reports null for a fixed invoker', async () => {
+      const { root, items } = renderSelectableRoot();
+      items[0].setAttribute('data-sortable-lists--item-mobility-value', 'fixed');
+      await ctx.nextFrame();
+      const controller = ctx.application.getControllerForElementAndIdentifier(root, 'sortable-lists') as SortableListsControllerType;
+
+      expect(controller.moveAvailability(items[0])).toBeNull();
     });
   });
 

frontend/src/stimulus/controllers/dynamic/sortable-lists.controller.ts
@@ -55,11 +55,11 @@ import {
   itemAcceptsDestination,
   reorderRows,
   resolveDirectionalPreviousItemId,
+  resolveBlockMoveAvailability,
   resolveItemId,
   resolveItemLabel,
   resolveItemPosition,
   resolveItemType,
-  resolveMoveAvailability,
   restoreRowPositions,
   rowOf,
   rowsRemainAt,
@@ -503,14 +503,19 @@ export default class SortableListsController extends Controller<HTMLElement> imp
   // gating; the click path re-resolves the live DOM.
   moveAvailability(itemElement:HTMLElement):MoveAvailability|null {
     if (!isOrderableItem(itemElement)) {
-      return {
-        top: false, up: false, down: false, bottom: false,
-      };
+      return null;
+    }
+
+    const scope = this.actionScopeFor(itemElement);
+    if (scope.kind === 'refused') {
+      return null;
     }
 
     const list = this.ownerListOf(itemElement);
 
-    return list ? resolveMoveAvailability({ itemElement, rowsContainer: list.rowsContainer }) : null;
+    return list
+      ? resolveBlockMoveAvailability({ itemElements: scope.items, rowsContainer: list.rowsContainer })
+      : null;
   }
 
   moveToDestination(itemElement:HTMLElement, target:DestinationIdentity):void {