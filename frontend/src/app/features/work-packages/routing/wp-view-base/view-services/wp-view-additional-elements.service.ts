f9fffc8292eb65718a41b1abc43521d026387cba
Ivan Kuchin <i.kuchin@openproject.com>
2026-09-11 21:21:21 +0200

    don't send timestamps for non baseline

--- frontend/src/app/features/work-packages/routing/wp-view-base/view-services/wp-view-additional-elements.service.ts
+++ frontend/src/app/features/work-packages/routing/wp-view-base/view-services/wp-view-additional-elements.service.ts
@@ -42,6 +42,7 @@ import { SchemaCacheService } from 'core-app/core/schemas/schema-cache.service';
 import { ApiV3Service } from 'core-app/core/apiv3/api-v3.service';
 import { RelationResource } from 'core-app/features/hal/resources/relation-resource';
 import { HalResourceService } from 'core-app/features/hal/services/hal-resource.service';
+import { DEFAULT_TIMESTAMP } from './wp-view-baseline.service';
 import { WorkPackageViewHierarchiesService } from './wp-view-hierarchy.service';
 import { WorkPackageViewColumnsService } from './wp-view-columns.service';
 import { ApiV3FilterBuilder } from 'core-app/shared/helpers/api-v3/api-v3-filter-builder';
@@ -72,11 +73,13 @@ export class WorkPackageViewAdditionalElementsService {
       this.requireWorkPackageShares(workPackageIds),
       this.requireSumsSchema(results),
     ]).then((wpResults:string[][]) => {
-      this.loadAdditional(wpResults.flat(), query.timestamps);
+      const timestamps = query.timestamps;
+
+      this.loadAdditional(wpResults.flat(), timestamps[0] === DEFAULT_TIMESTAMP ? undefined : timestamps);
     });
   }
 
-  private loadAdditional(wpIds:string[], timestamps:string[]) {
+  private loadAdditional(wpIds:string[], timestamps?:string[]) {
     this
       .apiV3Service
       .work_packages
