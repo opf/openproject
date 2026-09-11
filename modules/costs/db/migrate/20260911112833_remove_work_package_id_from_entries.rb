c0f7faeb1dff7a3928e38da1b5a6f217927710d4
Klaus Zanders <k.zanders@openproject.com>
2026-09-11 14:29:59 +0200

    Let index be dropped implicitly instead of referencing it by the hard coded name

--- modules/costs/db/migrate/20260911112833_remove_work_package_id_from_entries.rb
+++ modules/costs/db/migrate/20260911112833_remove_work_package_id_from_entries.rb
@@ -1,9 +1,9 @@
 # frozen_string_literal: true
 
 class RemoveWorkPackageIdFromEntries < ActiveRecord::Migration[8.1]
+  # The time_entries_issue_id index is dropped implicitly with its column, so it never
+  # has to be looked up by name.
   def change
-    remove_index :time_entries, :work_package_id, name: "time_entries_issue_id"
-
     remove_column :time_entries, :work_package_id, :bigint
     remove_column :time_entry_journals, :work_package_id, :bigint
     remove_column :cost_entries, :work_package_id, :bigint
