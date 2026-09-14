bdab726f7744ec431d71df6238b8c0fdd078b0b2
Jan Sandbrink <j.sandbrink@openproject.com>
2026-09-14 16:13:07 +0200

    Extract common status component
    Also takes care of displaying an icon inside the component as well as
    properly assigning background colors

--- modules/gitlab_integration/app/components/gitlab_integration/status_component.rb
+++ modules/gitlab_integration/app/components/gitlab_integration/status_component.rb
@@ -29,38 +29,15 @@
 #++
 
 module GitlabIntegration
-  class IssueComponent < ApplicationComponent
-    include ApplicationHelper
-    include OpPrimer::ComponentHelpers
+  class StatusComponent < Primer::Component
+    attr_reader :scheme, :icon
 
-    alias_method :issue, :model
+    def initialize(scheme:, icon:, **system_arguments)
+      super()
 
-    private
-
-    def state_scheme
-      case issue.state.to_sym
-      when :opened
-        :success
-      when :closed
-        :done
-      else
-        raise ArgumentError, "Unsupported issue state #{state}"
-      end
-    end
-
-    def state_icon
-      case issue.state.to_sym
-      when :opened
-        :"issue-opened"
-      when :closed
-        :"issue-closed"
-      else
-        raise ArgumentError, "Unsupported issue state #{state}"
-      end
-    end
-
-    def state_label
-      t(".states.#{issue.state}")
+      @scheme = scheme
+      @icon = icon
+      @system_arguments = system_arguments
     end
   end
 end
