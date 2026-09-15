Syvera-Open-Source-Projects/openproject_syvera

[AGILE-362] Submit destination batches
Submits direct and dialog moves through the collection endpoint while
preserving ordered IDs and retry selection. Tracks in-flight movement across
disconnect and reconnect to prevent duplicate requests and stale busy state.

https://community.openproject.org/wp/AGILE-362

frontend/src/stimulus/controllers/dynamic/sortable-lists/item.controller.ts
@@ -182,6 +182,41 @@ export default class ItemController extends Controller<HTMLElement> implements R
     }
   }
 
+  prepareDialog(event:CustomEvent<{ form:HTMLFormElement|null }>):void {
+    const scope = this.root?.selectForAction(this.element);
+    const form = event.detail.form;
+    if (!form || !scope || scope.kind === 'singular') {
+      event.preventDefault();
+      return;
+    }
+
+    form.querySelectorAll('[data-sortable-lists-generated-id]').forEach((input) => input.remove());
+    scope.ids.forEach((id) => {
+      const input = form.ownerDocument.createElement('input');
+      input.type = 'hidden';
+      input.name = 'ids[]';
+      input.value = id;
+      input.dataset.sortableListsGeneratedId = '';
+      form.append(input);
+    });
+  }
+
+  moveToDestination(event:ActionEvent):void {
+    const item = event.currentTarget;
+    if (!this.hasMenuElement || !(item instanceof HTMLElement)) {
+      return;
+    }
+
+    if (this.menuElement.isItemDisabled(item) || this.menuElement.isItemHidden(item)) {
+      return;
+    }
+
+    const candidates = this.destinationCandidates(item);
+    if (candidates.length === 1) {
+      this.root?.moveToDestination(this.element, candidates[0]);
+    }
+  }
+
   // The focus host is the consumer's business: Backlogs puts the tab stop on
   // the card inside the row, another consumer may make the row itself
   // focusable. Both work without the root learning either shape.

opf/openproject

frontend/src/stimulus/controllers/dynamic/sortable-lists/item.controller.ts
@@ -61,7 +61,7 @@ import {
 } from './list-dom';
 import { webLinkHref } from './external-data';
 import { renderDragPreview } from './preview';
-import type { ActionScope } from './selection-orchestrator';
+import { scopeIds, type ActionScope } from './selection-orchestrator';
 
 type CleanupFn = () => void;
 
@@ -177,6 +177,41 @@ export default class ItemController extends Controller<HTMLElement> implements R
     const { direction } = event.params;
     if (isMoveDirection(direction)) {
       this.root?.moveInDirection(this.element, direction);
+    }
+  }
+
+  prepareDialog(event:CustomEvent<{ form:HTMLFormElement|null }>):void {
+    const scope = this.root?.selectForAction(this.element);
+    const form = event.detail.form;
+    if (!form || !scope || scope.kind === 'refused') {
+      event.preventDefault();
+      return;
+    }
+
+    form.querySelectorAll('[data-sortable-lists-generated-id]').forEach((input) => input.remove());
+    scopeIds(scope).forEach((id) => {
+      const input = form.ownerDocument.createElement('input');
+      input.type = 'hidden';
+      input.name = 'ids[]';
+      input.value = id;
+      input.dataset.sortableListsGeneratedId = '';
+      form.append(input);
+    });
+  }
+
+  moveToDestination(event:ActionEvent):void {
+    const item = event.currentTarget;
+    if (!this.hasMenuElement || !(item instanceof HTMLElement)) {
+      return;
+    }
+
+    if (this.menuElement.isItemDisabled(item) || this.menuElement.isItemHidden(item)) {
+      return;
+    }
+
+    const candidates = this.destinationCandidates(item);
+    if (candidates.length === 1) {
+      this.root?.moveToDestination(this.element, candidates[0]);
     }
   }
 