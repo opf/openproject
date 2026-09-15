Syvera-Open-Source-Projects/openproject_syvera

[AGILE-363] Gate batch position actions
Resolves availability over the invoker's prospective scope and exposes
only moves the live block resolver can execute. Preserves destinations for
contiguous same-list selections.

https://community.openproject.org/wp/AGILE-363

frontend/src/stimulus/controllers/dynamic/sortable-lists.controller.ts
@@ -51,11 +51,11 @@ import {
   isOrderableItem,
   reorderRows,
   resolveDirectionalPreviousItemId,
+  resolveBlockMoveAvailability,
   resolveItemId,
   resolveItemLabel,
   resolveItemPosition,
   resolveItemType,
-  resolveMoveAvailability,
   restoreRowPositions,
   rowOf,
   rowsRemainAt,
@@ -416,9 +416,16 @@ export default class SortableListsController extends Controller<HTMLElement> imp
   // addressable. Null means the item is not in an owned list (yet). The result
   // is a snapshot for menu gating; the click path re-resolves the live DOM.
   moveAvailability(itemElement:HTMLElement):MoveAvailability|null {
+    const scope = this.actionScopeFor(itemElement);
+    if (scope.kind === 'singular') {
+      return null;
+    }
+
     const list = this.ownerListOf(itemElement);
 
-    return list ? resolveMoveAvailability({ itemElement, rowsContainer: list.rowsContainer }) : null;
+    return list
+      ? resolveBlockMoveAvailability({ itemElements: scope.items, rowsContainer: list.rowsContainer })
+      : null;
   }
 
   moveToDestination(itemElement:HTMLElement, target:DestinationIdentity):void {

opf/openproject

frontend/src/stimulus/controllers/dynamic/sortable-lists.controller.ts
@@ -55,11 +55,11 @@ import {
   itemAcceptsDestination,
   reorderRows,
   resolveDirectionalPreviousItemId,
+  resolveBlockMoveAvailability,
   resolveItemId,
   resolveItemLabel,
   resolveItemPosition,
   resolveItemType,
-  resolveMoveAvailability,
   restoreRowPositions,
   rowOf,
   rowsRemainAt,
@@ -498,14 +498,19 @@ export default class SortableListsController extends Controller<HTMLElement> imp
   // gating; the click path re-resolves the live DOM.
   moveAvailability(itemElement:HTMLElement):MoveAvailability|null {
     if (!isOrderableItem(itemElement)) {
-      return {
-        top: false, up: false, down: false, bottom: false,
-      };
+      return null;
+    }
+
+    const scope = this.actionScopeFor(itemElement);
+    if (scope.kind === 'refused') {
+      return null;
     }
 
     const list = this.ownerListOf(itemElement);
 
-    return list ? resolveMoveAvailability({ itemElement, rowsContainer: list.rowsContainer }) : null;
+    return list
+      ? resolveBlockMoveAvailability({ itemElements: scope.items, rowsContainer: list.rowsContainer })
+      : null;
   }
 
   moveToDestination(itemElement:HTMLElement, target:DestinationIdentity):void {