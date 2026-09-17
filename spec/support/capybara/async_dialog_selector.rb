7789dfb3eb48a1742c87bc810bb205e664c148cb
Kabiru Mwenja <k.mwenja@openproject.com>
2026-09-17 17:50:13 +0300

    Document the async dialog trigger selector

--- spec/support/capybara/async_dialog_selector.rb
+++ spec/support/capybara/async_dialog_selector.rb
@@ -28,6 +28,21 @@
 # See COPYRIGHT and LICENSE files for more details.
 #++
 
+# Finds links and buttons that open a dialog through the `async-dialog`
+# Stimulus controller, matched by visible text or `aria-label`.
+#
+# @example Assert a menu item opens a dialog for a given endpoint
+#   expect(page).to have_selector(:async_dialog_trigger, "Delete", href: deletion_dialog_admin_label_path(label))
+#
+# @example Shorthand matcher
+#   expect(page).to have_async_dialog_trigger("Label", href: new_dialog_admin_labels_path)
+#
+# @example Click the trigger
+#   find(:async_dialog_trigger, "Rename").click
+#
+# Filters: `href:` (exact match), plus `aria:` and `described_by:` from
+# capybara_accessible_selectors.
+
 Capybara.add_selector(:async_dialog_trigger, locator_type: [String, Symbol]) do
   label "async dialog trigger"
 
@@ -61,10 +76,14 @@ end
 
 module Capybara
   module RSpecMatchers
+    # @param locator [String, Symbol, nil] visible text or aria-label of the trigger
+    # @param options [Hash] selector filters such as `href:`
+    # @return [Capybara::RSpecMatchers::Matchers::HaveSelector]
     def have_async_dialog_trigger(locator = nil, **, &)
       Matchers::HaveSelector.new(:async_dialog_trigger, locator, **, &)
     end
 
+    # Negated form of {#have_async_dialog_trigger}.
     def have_no_async_dialog_trigger(...)
       Matchers::NegatedMatcher.new(have_async_dialog_trigger(...))
     end
