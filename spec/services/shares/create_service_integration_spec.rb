e4d8975b156d3b7a44a4979c0d5444e303dd5d8a
Klaus Zanders <k.zanders@openproject.com>
2026-09-18 16:11:01 +0200

    Fix autogenerated code by Code QL

--- spec/services/shares/create_service_integration_spec.rb
+++ spec/services/shares/create_service_integration_spec.rb
@@ -53,10 +53,10 @@ RSpec.describe Shares::CreateService, "integration", type: :model do
     it "shares the work package with the users already in the group" do
       service_call
 
-      members_by_principal_id = Member.where(principal: users, entity:).includes(:roles).index_by(&:principal_id)
+      members_by_user_id = Member.where(principal: users, entity:).includes(:roles).index_by(&:user_id)
 
       users.each do |user|
-        expect(members_by_principal_id[user.id]&.roles).to contain_exactly(role)
+        expect(members_by_user_id[user.id]&.roles).to contain_exactly(role)
       end
     end
   end
@@ -72,10 +72,10 @@ RSpec.describe Shares::CreateService, "integration", type: :model do
     it "shares the project query with the users already in the group" do
       service_call
 
-      members_by_principal_id = Member.where(principal: users, entity:).includes(:roles).index_by(&:principal_id)
+      members_by_user_id = Member.where(principal: users, entity:).includes(:roles).index_by(&:user_id)
 
       users.each do |user|
-        expect(members_by_principal_id[user.id]&.roles).to contain_exactly(role)
+        expect(members_by_user_id[user.id]&.roles).to contain_exactly(role)
       end
     end
 
@@ -85,10 +85,10 @@ RSpec.describe Shares::CreateService, "integration", type: :model do
 
       service_call
 
-      global_members_by_principal_id = Member.where(principal: users, entity_type: nil).includes(:roles).index_by(&:principal_id)
+      global_members_by_user_id = Member.where(principal: users, entity_type: nil).includes(:roles).index_by(&:user_id)
 
       users.each do |user|
-        expect(global_members_by_principal_id[user.id].roles).to contain_exactly(global_role)
+        expect(global_members_by_user_id[user.id].roles).to contain_exactly(global_role)
       end
     end
   end
