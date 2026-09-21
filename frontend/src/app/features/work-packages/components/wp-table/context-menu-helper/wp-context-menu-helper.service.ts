78cc10552dc141c077ee92dfaf0e24a9e7ce36c3
Niels Lindenthal <lindenthal@users.noreply.github.com>
2026-09-21 15:41:46 +0200

    Fix trash restore action lint

--- frontend/src/app/features/work-packages/components/wp-table/context-menu-helper/wp-context-menu-helper.service.ts
+++ frontend/src/app/features/work-packages/components/wp-table/context-menu-helper/wp-context-menu-helper.service.ts
@@ -181,7 +181,7 @@ export class WorkPackageContextMenuHelperService {
 
     actions.forEach((action) => {
       if (action.link && workPackage[action.link] !== undefined) {
-        action.text = action.text || I18n.t(
+        action.text = action.text ?? I18n.t(
           action.key === 'restore' ? 'js.work_packages.trash.restore' : `js.button_${action.key}`,
         );
         allowedActions.push(action);
