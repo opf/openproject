smithdouglas404/agenticopenproject

Remove and check more html_safe calls

modules/documents/app/components/documents/admin/collaboration_settings/page_header_component.rb
@@ -34,12 +34,8 @@ module Documents
     module CollaborationSettings
       class PageHeaderComponent < ApplicationComponent
         def description
-          I18n.t("documents.admin.collaboration_settings.page_header.description", hocuspocus_server_link:).html_safe
-        end
-
-        def hocuspocus_server_link
-          helpers.static_link_to(:hocuspocus_server_docs,
-                                 label: I18n.t("documents.admin.collaboration_settings.hocuspocus_server"))
+          helpers.link_translate("documents.admin.collaboration_settings.page_header.description",
+                                 links: { hocuspocus_server_link: %i[hocuspocus_server_docs] })
         end
       end
     end

modules/openid_connect/app/components/openid_connect/providers/name_input_and_tenant_form.rb
@@ -47,7 +47,7 @@ module OpenIDConnect
           required: true,
           disabled: provider.seeded_from_env?,
           value: provider.tenant || "common",
-          caption: I18n.t("openid_connect.instructions.tenant").html_safe,
+          caption: I18n.t("openid_connect.instructions.tenant_html"),
           input_width: :medium
         )
       end

opf/openproject

modules/documents/app/components/documents/admin/collaboration_settings/page_header_component.rb
@@ -34,12 +34,8 @@ module Documents
     module CollaborationSettings
       class PageHeaderComponent < ApplicationComponent
         def description
-          I18n.t("documents.admin.collaboration_settings.page_header.description", hocuspocus_server_link:).html_safe
-        end
-
-        def hocuspocus_server_link
-          helpers.static_link_to(:hocuspocus_server_docs,
-                                 label: I18n.t("documents.admin.collaboration_settings.hocuspocus_server"))
+          helpers.link_translate("documents.admin.collaboration_settings.page_header.description_html",
+                                 links: { hocuspocus_server_link: %i[hocuspocus_server_docs] })
         end
       end
     end

modules/openid_connect/app/components/openid_connect/providers/name_input_and_tenant_form.rb
@@ -47,7 +47,7 @@ module OpenIDConnect
           required: true,
           disabled: provider.seeded_from_env?,
           value: provider.tenant || "common",
-          caption: I18n.t("openid_connect.instructions.tenant").html_safe,
+          caption: helpers.t("openid_connect.instructions.tenant_html"),
           input_width: :medium
         )
       end