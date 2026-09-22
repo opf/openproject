e50f235fd37d5435fc77c78bbf5b7ceb8f968d17
Bruno Pagno <b.pagno@openproject.com>
2026-09-22 15:42:40 +0200

    [COMMS-1003] Make labels in workpackages writeable through the API (#25447)
    * make labelable in workpackages writeable in the API
    
    * with specs
    
    * make label - workpackage association work with the API
    
    * docs for labels
    
    * skip rendering links when feature flag is not active
    
    * fix specs to work with feature flag
    
    * remove leftover

--- app/models/concerns/labelable.rb
+++ app/models/concerns/labelable.rb
@@ -37,22 +37,70 @@ module Labelable
 
     scope :labeled_with, ->(label) { joins(:labelings).where(labelings: { label_id: label }) }
 
-    after_save { @labels_was = nil }
+    after_save :persist_label_associations
+
+    validate :validate_label_id_replacements
   end
 
-  def labels=(*)
-    @labels_was ||= label_ids
-    super
+  attr_reader :label_id_replacements
+
+  def label_id_replacements=(new_labels)
+    @label_id_replacements = new_labels.nil? ? nil : normalize_label_ids(new_labels)
   end
 
-  def label_ids=(*)
-    @labels_was ||= label_ids
-    super
+  def override_labels? = !label_id_replacements.nil?
+
+  def effective_labels
+    return labels unless override_labels?
+
+    Label.where(id: label_id_replacements).order(:id)
   end
 
   def label_changes
-    return {} if @labels_was.nil? || @labels_was.sort == label_ids.sort
+    return {} unless override_labels?
+    return {} if label_id_replacements.sort == label_ids.sort
 
-    { "labels" => [@labels_was, label_ids] }
+    { "labels" => [label_ids, label_id_replacements] }
+  end
+
+  private
+
+  def normalize_label_ids(new_labels)
+    Array.wrap(new_labels)
+         .filter_map { |label| label.respond_to?(:id) ? label.id : label.to_s.presence&.to_i }
+         .uniq
+  end
+
+  def validate_label_id_replacements
+    return unless override_labels?
+
+    missing = label_id_replacements - Label.where(id: label_id_replacements).pluck(:id)
+
+    errors.add(:labels, :does_not_exist) if missing.any?
+  end
+
+  def persist_label_associations
+    replace_labels(label_id_replacements) if override_labels?
+
+    self.label_id_replacements = nil
+  end
+
+  def replace_labels(new_label_ids)
+    existing = labelings.pluck(:label_id)
+
+    to_remove = existing - new_label_ids
+    to_add    = new_label_ids - existing
+
+    return if to_remove.empty? && to_add.empty?
+
+    labelings.where(label_id: to_remove).delete_all if to_remove.any?
+    labelings.insert_all(to_add.map { |id| { label_id: id } }) if to_add.any?
+
+    reset_label_associations
+  end
+
+  def reset_label_associations
+    labelings.reset
+    labels.reset
   end
 end
