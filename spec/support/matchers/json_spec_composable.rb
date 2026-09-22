be3ad652656ecd8670be50775c63736dffc73a02
Ivan Kuchin <i.kuchin@openproject.com>
2026-09-22 15:37:56 +0200

    make json_spec matchers composable

---
+++ spec/support/matchers/json_spec_composable.rb
@@ -0,0 +1,42 @@
+# frozen_string_literal: true
+
+#-- copyright
+# OpenProject is an open source project management software.
+# Copyright (C) the OpenProject GmbH
+#
+# This program is free software; you can redistribute it and/or
+# modify it under the terms of the GNU General Public License version 3.
+#
+# OpenProject is a fork of ChiliProject, which is a fork of Redmine. The copyright follows:
+# Copyright (C) 2006-2013 Jean-Philippe Lang
+# Copyright (C) 2010-2013 the ChiliProject Team
+#
+# This program is free software; you can redistribute it and/or
+# modify it under the terms of the GNU General Public License
+# as published by the Free Software Foundation; either version 2
+# of the License, or (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+#
+# See COPYRIGHT and LICENSE files for more details.
+#++
+
+# Make json_spec matchers (be_json_eql, have_json_path, have_json_size,
+# have_json_type, include_json) composable, so usable inside include/match/…
+# or using and/or.
+%i[
+  BeJsonEql
+  HaveJsonPath
+  HaveJsonSize
+  HaveJsonType
+  IncludeJson
+].each do |matcher|
+  JsonSpec::Matchers.const_get(matcher).include(RSpec::Matchers::Composable)
+end
