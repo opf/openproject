465c35430bb7dcc349009bef23288b3580794d62
Klaus Zanders <k.zanders@openproject.com>
2026-09-24 13:32:57 +0200

    Add the field to show the allocations in a work package

---
+++ modules/resource_management/frontend/module/allocated-time-display-field.ts
@@ -0,0 +1,129 @@
+//-- copyright
+// OpenProject is an open source project management software.
+// Copyright (C) the OpenProject GmbH
+//
+// This program is free software; you can redistribute it and/or
+// modify it under the terms of the GNU General Public License version 3.
+//
+// OpenProject is a fork of ChiliProject, which is a fork of Redmine. The copyright follows:
+// Copyright (C) 2006-2013 Jean-Philippe Lang
+// Copyright (C) 2010-2013 the ChiliProject Team
+//
+// This program is free software; you can redistribute it and/or
+// modify it under the terms of the GNU General Public License
+// as published by the Free Software Foundation; either version 2
+// of the License, or (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program; if not, write to the Free Software
+// Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
+//
+// See COPYRIGHT and LICENSE files for more details.
+//++
+
+import { DisplayField } from 'core-app/shared/components/fields/display/display-field.module';
+import { LazyInject } from 'core-app/shared/helpers/angular/lazy-inject.decorator';
+import { TimezoneService } from 'core-app/core/datetime/timezone.service';
+import { TurboRequestsService } from 'core-app/core/turbo/turbo-requests.service';
+import { HalLink } from 'core-app/features/hal/hal-link/hal-link';
+
+export class AllocatedTimeDisplayField extends DisplayField {
+  @LazyInject() timezoneService:TimezoneService;
+
+  @LazyInject() turboRequests:TurboRequestsService;
+
+  private text = {
+    showAllocations: this.I18n.t('js.resource_management.show_allocations'),
+  };
+
+  public get valueString():string {
+    return this.timezoneService.formattedChronicDuration(this.value as string);
+  }
+
+  public render(element:HTMLElement, displayText:string):void {
+    element.innerHTML = '';
+
+    const container = this.showAllocationsLink ? this.buildButton() : document.createElement('span');
+    container.classList.add('d-flex', 'flex-items-center');
+
+    if (this.workDuration) {
+      container.appendChild(this.buildProgressBar());
+      container.appendChild(this.buildSummary(displayText, this.workDuration));
+    } else {
+      container.appendChild(document.createTextNode(displayText));
+    }
+
+    element.appendChild(container);
+  }
+
+  private get showAllocationsLink():HalLink|undefined {
+    return this.resource.showResourceAllocations as HalLink|undefined;
+  }
+
+  private get workDuration():string|null {
+    const work = (this.resource.derivedEstimatedTime ?? this.resource.estimatedTime) as string|null;
+
+    return work && this.timezoneService.toHours(work) > 0 ? work : null;
+  }
+
+  private get ratio():number {
+    const allocatedHours = this.timezoneService.toHours(this.value as string);
+    const workHours = this.timezoneService.toHours(this.workDuration!);
+
+    return Math.round((allocatedHours / workHours) * 100);
+  }
+
+  private get barColorClass():string {
+    if (this.ratio > 100) {
+      return 'color-bg-danger-emphasis';
+    }
+
+    return this.ratio === 100 ? 'color-bg-success-emphasis' : 'color-bg-accent-emphasis';
+  }
+
+  private buildButton():HTMLButtonElement {
+    const button = document.createElement('button');
+    button.type = 'button';
+    button.title = this.text.showAllocations;
+    button.classList.add('p-0', 'border-0', 'bgColor-transparent', 'color-fg-default', 'text-left');
+    button.addEventListener('click', (event) => {
+      event.preventDefault();
+      event.stopPropagation();
+      void this.turboRequests.requestStream(this.showAllocationsLink!.href!);
+    });
+
+    return button;
+  }
+
+  private buildProgressBar():HTMLElement {
+    const bar = document.createElement('span');
+    bar.classList.add('Progress', 'op-rm-allocated-time--bar');
+
+    const item = document.createElement('span');
+    item.classList.add('Progress-item', this.barColorClass);
+    item.style.width = `${Math.min(this.ratio, 100)}%`;
+
+    bar.appendChild(item);
+
+    return bar;
+  }
+
+  private buildSummary(displayText:string, workDuration:string):HTMLElement {
+    const summary = document.createElement('span');
+    summary.classList.add('ml-2', 'flex-shrink-0');
+    summary.textContent = `${displayText} / ${this.timezoneService.formattedChronicDuration(workDuration)} `;
+
+    const percentage = document.createElement('span');
+    percentage.classList.add('color-fg-muted');
+    percentage.textContent = `(${this.ratio}%)`;
+
+    summary.appendChild(percentage);
+
+    return summary;
+  }
+}
---
+++ modules/resource_management/spec/features/work_package_allocated_time_field_spec.rb
@@ -0,0 +1,72 @@
+# frozen_string_literal: true
+
+#-- copyright
+# OpenProject is an open source project management software.
+# Copyright (C) the OpenProject GmbH
+#
+# This program is free software; you can redistribute it and/or
+# modify it under the terms of the GNU General Public License version 3.
+#
+# OpenProject is a fork of ChiliProject, which is a fork of Redmine. The copyright follows:
+# Copyright (C) 2006-2013 Jean-Philippe Lang
+# Copyright (C) 2010-2013 the ChiliProject Team
+#
+# This program is free software; you can redistribute it and/or
+# modify it under the terms of the GNU General Public License
+# as published by the Free Software Foundation; either version 2
+# of the License, or (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+#
+# See COPYRIGHT and LICENSE files for more details.
+#++
+
+require "spec_helper"
+
+RSpec.describe "Allocated time field on the work package page", :js, with_ee: %i[resource_management] do
+  shared_let(:project) { create(:project, enabled_module_names: %w[resource_management work_package_tracking]) }
+  shared_let(:user) do
+    create(:user,
+           member_with_permissions: { project => %i[view_work_packages view_resource_planners allocate_user_resources] })
+  end
+  shared_let(:work_package) { create(:work_package, project:, estimated_hours: 16) }
+
+  let(:wp_page) { Pages::FullWorkPackage.new(work_package, project) }
+  let(:allocated_time_field) { EditField.new(page, "allocatedTime") }
+
+  before do
+    create(:resource_allocation, entity: work_package, principal: user, allocated_time: 8 * 60)
+    create(:resource_allocation, entity: work_package, principal: user, allocated_time: 4 * 60)
+
+    login_as(user)
+    wp_page.visit!
+  end
+
+  it "shows the allocated time against the work and opens the allocations dialog" do
+    allocated_time_field.expect_state_text("12h / 16h (75%)")
+
+    click_button I18n.t("js.resource_management.show_allocations")
+
+    within("##{ResourceAllocations::ListDialogComponent::DIALOG_ID}") do
+      expect(page).to have_text(I18n.t("resource_management.work_package_allocations_dialog.title"))
+    end
+  end
+
+  it "updates after an allocation is removed in the dialog" do
+    click_button I18n.t("js.resource_management.show_allocations")
+
+    within("##{ResourceAllocations::ListDialogComponent::DIALOG_ID}") do
+      all(:button) { it.has_selector?("svg.octicon-kebab-horizontal") }.first.click
+    end
+    accept_confirm { find(:menuitem, I18n.t(:button_delete)).click }
+
+    allocated_time_field.expect_state_text("4h / 16h (25%)")
+  end
+end
