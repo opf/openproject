3c584f8b670d13fcd23a2b77ea929cab83f36fb0
ulferts <jens.ulferts@googlemail.com>
2026-09-25 10:31:13 +0200

    avoid chaining of filters to reset the scope
    The shared filter is one of the few wp filters modifying the query`s scope rather than adding a condition. WorkPackage.unscoped would
    reset the scope. So if a filter was present after the shared filter, the shared filter`s condition was reset

--- app/models/queries/work_packages/filter/work_package_filter.rb
+++ app/models/queries/work_packages/filter/work_package_filter.rb
@@ -43,11 +43,11 @@ class Queries::WorkPackages::Filter::WorkPackageFilter < Queries::Filters::Base
     nil
   end
 
-  def apply_to(_query_scope)
-    # We only return the WorkPackage base scope for now as most of the filters
+  def apply_to(query_scope)
+    # We only pass through the query_scope for now as most of the filters
     # (this one's subclasses) currently do not follow the base filter approach of using the scope.
-    # The intend is to have more and more wp filters use the scope method just like the
+    # The intent is to have more and more wp filters use the scope method just like the
     # rest of the queries (e.g. project)
-    WorkPackage.unscoped
+    query_scope
   end
 end
